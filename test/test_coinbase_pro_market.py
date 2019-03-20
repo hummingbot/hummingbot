96a1305abccd99798172ee3acc58d791db3408e7
Yvonne Zhang <yzhang1994@Gmail.com>
2019-03-20 14:42:06 -0700

    complete authentication

--- test/test_coinbase_pro_market.py
+++ test/test_coinbase_pro_market.py
@@ -52,7 +52,6 @@ class CoinbaseProMarketUnitTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         global MAINNET_RPC_URL
-
         cls.clock: Clock = Clock(ClockMode.REALTIME)
         cls.market: CoinbaseProMarket = CoinbaseProMarket(
             web3_url=MAINNET_RPC_URL,
@@ -96,8 +95,39 @@ class CoinbaseProMarketUnitTest(unittest.TestCase):
     def run_parallel(self, *tasks):
         return self.ev_loop.run_until_complete(self.run_parallel_async(*tasks))
 
-    def test_limit_buy_and_sell(self):
-        self.assertGreater(self.market.get_balance("ETH"), 0.1)
+    def test_get_balances(self):
+        [results] = self.run_parallel(self.market._update_balances())
+        print(results)
+
+    @unittest.skip
+    def test_buy_and_sell(self):
+        # self.assertGreater(self.market.get_balance("ETH"), 0.1)
+        symbol = "ETH-USDC"
+        amount: float = 1.0
+
+        current_bid_price: float = 100
+        # current_bid_price: float = self.market.get_price(symbol, True)
+        bid_price: float = current_bid_price + 0.05 * current_bid_price
+        quantize_bid_price: Decimal = self.market.quantize_order_price(symbol, bid_price)
+        quantized_amount: Decimal = self.market.quantize_order_amount(symbol, amount)
+
+        order_id = self.market.buy(symbol, quantized_amount, OrderType.LIMIT, quantize_bid_price)
+        [order_completed_event] = self.run_parallel(self.market_logger.wait_for(BuyOrderCompletedEvent))
+        order_completed_event: BuyOrderCompletedEvent = order_completed_event
+        trade_events: List[OrderFilledEvent] = [t for t in self.market_logger.event_log
+                                                if isinstance(t, OrderFilledEvent)]
+        base_amount_traded: float = sum(t.amount for t in trade_events)
+        quote_amount_traded: float = sum(t.amount * t.price for t in trade_events)
+
+        self.assertEqual(order_id, order_completed_event.order_id)
+        self.assertEqual(quantized_amount, order_completed_event.base_asset_amount)
+        self.assertEqual("ETH", order_completed_event.base_asset)
+        self.assertEqual("USDC", order_completed_event.quote_asset)
+        self.assertAlmostEqual(base_amount_traded, float(order_completed_event.base_asset_amount))
+        self.assertAlmostEqual(quote_amount_traded, float(order_completed_event.quote_asset_amount))
+        self.assertGreater(order_completed_event.fee_amount, Decimal(0))
+        self.assertTrue(any([isinstance(event, BuyOrderCreatedEvent) and event.order_id == order_id
+                             for event in self.market_logger.event_log]))
 
 
 if __name__ == "__main__":
