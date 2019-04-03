b4d146ef592984b04783f3b08282ea253d66e6f8
Yvonne Zhang <yzhang1994@Gmail.com>
2019-04-03 10:25:54 -0700

    add test secret templates to enable user testing

---
+++ hummingbot/templates/test_templates/web3_wallet_secret_TEMPLATE.py
@@ -0,0 +1,18 @@
+# DDEX test
+web3_test_private_key_ddex = "<INSERT_HERE>"
+test_ddex_web3_provider_list = ["<INSERT_HERE>"]
+test_ddex_erc20_token_address_1 = "<INSERT_HERE>"
+test_ddex_erc20_token_address_2 = "<INSERT_HERE>"
+
+# Radar Relay test
+web3_private_key_radar = "<INSERT_HERE>"
+mn_zerox_token_address = "<INSERT_HERE>"
+mn_weth_token_address = "<INSERT_HERE>"
+
+# Web3 wallet test (Ropsten)
+web3_test_private_key_a = "<INSERT_HERE>"
+web3_test_private_key_b = "<INSERT_HERE>"
+web3_test_private_key_c = "<INSERT_HERE>"
+test_web3_provider_list = ["<INSERT_HERE>"]
+test_erc20_token_address = "<INSERT_HERE>"
+
