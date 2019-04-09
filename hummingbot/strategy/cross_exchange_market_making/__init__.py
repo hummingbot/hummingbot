5cfcd663139cb0f6840a04934c3abfa1c0831bb4
Yvonne Zhang <yzhang1994@Gmail.com>
2019-04-09 10:48:03 -0700

    put stratgies in folders

---
+++ hummingbot/strategy/cross_exchange_market_making/__init__.py
@@ -0,0 +1,8 @@
+from .cross_exchange_market_pair import CrossExchangeMarketPair
+from .cross_exchange_market_making import CrossExchangeMarketMakingStrategy
+
+
+__all__ = [
+    CrossExchangeMarketPair,
+    CrossExchangeMarketMakingStrategy,
+]
