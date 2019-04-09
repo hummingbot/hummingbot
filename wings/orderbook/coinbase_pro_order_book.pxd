606f8a88fa07fdd652038cb686d800803450dff0
Ryan Chow <chowryan@gmail.com>
2019-04-09 14:42:02 -0700

    wip

---
+++ wings/orderbook/coinbase_pro_order_book.pxd
@@ -0,0 +1,4 @@
+from wings.order_book cimport OrderBook
+
+cdef class CoinbaseProOrderBook(OrderBook):
+    pass
--- wings/tracker/coinbase_pro_order_book_tracker.py
+++ wings/tracker/coinbase_pro_order_book_tracker.py
@@ -13,24 +13,26 @@ from typing import (
     Set
 )
 
-from wings.data_source.radar_relay_local_cluster_order_book_data_source import RadarRelayLocalClusterOrderBookDataSource
 from wings.order_book_tracker import OrderBookTracker, OrderBookTrackerDataSourceType
 from wings.data_source.order_book_tracker_data_source import OrderBookTrackerDataSource
-from wings.data_source.radar_relay_api_order_book_data_source import RadarRelayAPIOrderBookDataSource
-from wings.order_book_message import OrderBookMessageType, RadarRelayOrderBookMessage
-from wings.order_book_tracker_entry import RadarRelayOrderBookTrackerEntry
-from wings.orderbook.radar_relay_order_book import RadarRelayOrderBook
-from wings.radar_relay_active_order_tracker import RadarRelayActiveOrderTracker
+from wings.data_source.coinbase_pro_api_order_book_data_source import CoinbaseProAPIOrderBookDataSource
+from wings.order_book_message import (
+    OrderBookMessageType,
+    CoinbaseProOrderBookMessage
+)
+from wings.order_book_tracker_entry import CoinbaseProOrderBookTrackerEntry
+from wings.orderbook.coinbase_pro_order_book import CoinbaseProOrderBook
+from wings.coinbase_pro_active_order_tracker import CoinbaseProActiveOrderTracker
 
 
-class RadarRelayOrderBookTracker(OrderBookTracker):
-    _rrobt_logger: Optional[logging.Logger] = None
+class CoinbaseProOrderBookTracker(OrderBookTracker):
+    _cbpobt_logger: Optional[logging.Logger] = None
 
     @classmethod
     def logger(cls) -> logging.Logger:
-        if cls._rrobt_logger is None:
-            cls._rrobt_logger = logging.getLogger(__name__)
-        return cls._rrobt_logger
+        if cls._cbpobt_logger is None:
+            cls._cbpobt_logger = logging.getLogger(__name__)
+        return cls._cbpobt_logger
 
     def __init__(self,
                  data_source_type: OrderBookTrackerDataSourceType = OrderBookTrackerDataSourceType.EXCHANGE_API,
@@ -43,25 +45,23 @@ class RadarRelayOrderBookTracker(OrderBookTracker):
         self._order_book_diff_stream: asyncio.Queue = asyncio.Queue()
         self._process_msg_deque_task: Optional[asyncio.Task] = None
         self._past_diffs_windows: Dict[str, Deque] = {}
-        self._order_books: Dict[str, RadarRelayOrderBook] = {}
-        self._saved_message_queues: Dict[str, Deque[RadarRelayOrderBookMessage]] = defaultdict(lambda: deque(maxlen=1000))
-        self._active_order_trackers: Dict[str, RadarRelayActiveOrderTracker] = defaultdict(RadarRelayActiveOrderTracker)
+        self._order_books: Dict[str, CoinbaseProOrderBook] = defaultdict(CoinbaseProOrderBook)
+        self._saved_message_queues: Dict[str, Deque[CoinbaseProOrderBookMessage]] = defaultdict(lambda: deque(maxlen=1000))
+        self._active_order_trackers: Dict[str, CoinbaseProActiveOrderTracker] = defaultdict(CoinbaseProActiveOrderTracker)
         self._symbols: Optional[List[str]] = symbols
 
     @property
     def data_source(self) -> OrderBookTrackerDataSource:
         if not self._data_source:
             if self._data_source_type is OrderBookTrackerDataSourceType.EXCHANGE_API:
-                self._data_source = RadarRelayAPIOrderBookDataSource(symbols=self._symbols)
-            elif self._data_source_type is OrderBookTrackerDataSourceType.LOCAL_CLUSTER:
-                self._data_source = RadarRelayLocalClusterOrderBookDataSource()
+                self._data_source = CoinbaseProAPIOrderBookDataSource(symbols=self._symbols)
             else:
                 raise ValueError(f"data_source_type {self._data_source_type} is not supported.")
         return self._data_source
 
     @property
     async def exchange_name(self) -> str:
-        return "radar_relay"
+        return "coinbase_pro"
 
     async def start(self):
         self._order_book_diff_listener_task = asyncio.ensure_future(
@@ -92,13 +92,13 @@ class RadarRelayOrderBookTracker(OrderBookTracker):
         """
         tracking_symbols: Set[str] = set([key for key in self._tracking_tasks.keys()
                                           if not self._tracking_tasks[key].done()])
-        available_pairs: Dict[str, RadarRelayOrderBookTrackerEntry] = await self.data_source.get_tracking_pairs()
+        available_pairs: Dict[str, CoinbaseProOrderBookTrackerEntry] = await self.data_source.get_tracking_pairs()
         available_symbols: Set[str] = set(available_pairs.keys())
         new_symbols: Set[str] = available_symbols - tracking_symbols
         deleted_symbols: Set[str] = tracking_symbols - available_symbols
 
         for symbol in new_symbols:
-            order_book_tracker_entry: RadarRelayOrderBookTrackerEntry = available_pairs[symbol]
+            order_book_tracker_entry: CoinbaseProOrderBookTrackerEntry = available_pairs[symbol]
             self._active_order_trackers[symbol] = order_book_tracker_entry.active_order_tracker
             self._order_books[symbol] = order_book_tracker_entry.order_book
             self._tracking_message_queues[symbol] = asyncio.Queue()
@@ -121,24 +121,18 @@ class RadarRelayOrderBookTracker(OrderBookTracker):
         messages_queued: int = 0
         messages_accepted: int = 0
         messages_rejected: int = 0
-        address_token_map: Dict[str, any] = await self._data_source.get_all_token_info()
         while True:
             try:
-                ob_message: RadarRelayOrderBookMessage = await self._order_book_diff_stream.get()
-                base_token_address: str = ob_message.content["event"]["baseTokenAddress"]
-                quote_token_address: str = ob_message.content["event"]["quoteTokenAddress"]
-                base_token_symbol: str = address_token_map[base_token_address]["symbol"]
-                quote_token_symbol: str = address_token_map[quote_token_address]['symbol']
-                trading_pair_symbol: str = f"{base_token_symbol}-{quote_token_symbol}"
-
-                if trading_pair_symbol not in self._tracking_message_queues:
+                ob_message: CoinbaseProOrderBookMessage = await self._order_book_diff_stream.get()
+                symbol: str = ob_message.symbol
+                if symbol not in self._tracking_message_queues:
                     messages_queued += 1
                     # Save diff messages received before snapshots are ready
-                    self._saved_message_queues[trading_pair_symbol].append(ob_message)
+                    self._saved_message_queues[symbol].append(ob_message)
                     continue
-                message_queue: asyncio.Queue = self._tracking_message_queues[trading_pair_symbol]
+                message_queue: asyncio.Queue = self._tracking_message_queues[symbol]
                 # Check the order book's initial update ID. If it's larger, don't bother.
-                order_book: RadarRelayOrderBook = self._order_books[trading_pair_symbol]
+                order_book: CoinbaseProOrderBook = self._order_books[symbol]
 
                 if order_book.snapshot_uid > ob_message.update_id:
                     messages_rejected += 1
@@ -165,20 +159,20 @@ class RadarRelayOrderBookTracker(OrderBookTracker):
                 await asyncio.sleep(5.0)
 
     async def _track_single_book(self, symbol: str):
-        past_diffs_window: Deque[RadarRelayOrderBookMessage] = deque()
+        past_diffs_window: Deque[CoinbaseProOrderBookMessage] = deque()
         self._past_diffs_windows[symbol] = past_diffs_window
 
         message_queue: asyncio.Queue = self._tracking_message_queues[symbol]
-        order_book: RadarRelayOrderBook = self._order_books[symbol]
-        active_order_tracker: RadarRelayActiveOrderTracker = self._active_order_trackers[symbol]
+        order_book: CoinbaseProOrderBook = self._order_books[symbol]
+        active_order_tracker: CoinbaseProActiveOrderTracker = self._active_order_trackers[symbol]
 
         last_message_timestamp: float = time.time()
         diff_messages_accepted: int = 0
 
         while True:
             try:
-                message: RadarRelayOrderBookMessage = None
-                saved_messages: Deque[RadarRelayOrderBookMessage] = self._saved_message_queues[symbol]
+                message: CoinbaseProOrderBookMessage = None
+                saved_messages: Deque[CoinbaseProOrderBookMessage] = self._saved_message_queues[symbol]
                 # Process saved messages first if there are any
                 if len(saved_messages) > 0:
                     message = saved_messages.popleft()
@@ -201,7 +195,7 @@ class RadarRelayOrderBookTracker(OrderBookTracker):
                         diff_messages_accepted = 0
                     last_message_timestamp = now
                 elif message.type is OrderBookMessageType.SNAPSHOT:
-                    past_diffs: List[RadarRelayOrderBookMessage] = list(past_diffs_window)
+                    past_diffs: List[CoinbaseProOrderBookMessage] = list(past_diffs_window)
                     # only replay diffs later than snapshot, first update active order with snapshot then replay diffs
                     replay_position = bisect.bisect_right(past_diffs, message)
                     replay_diffs = past_diffs[replay_position:]
