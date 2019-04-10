71f658330e79dafa408d81c6af2f69d364cd5011
Yvonne Zhang <yzhang1994@Gmail.com>
2019-04-09 18:28:46 -0700

    Add list_trade function to StrategyBase

--- hummingbot/strategy/strategy_base.pyx
+++ hummingbot/strategy/strategy_base.pyx
@@ -1,20 +1,53 @@
 from typing import List
+from wings.clock cimport Clock
 from wings.time_iterator cimport TimeIterator
-from wings.limit_order cimport LimitOrder
+from wings.market_base cimport MarketBase
 from wings.trade import Trade
+from wings.event_listener cimport EventListener
+from wings.events import MarketEvent
+
+
+cdef class OrderFilledListener(EventListener):
+    cdef:
+        StrategyBase _owner
+
+    def __init__(self, StrategyBase owner):
+        super().__init__()
+        self._owner = owner
+
+    cdef c_call(self, object arg):
+        self._owner.c_record_trade(arg)
 
 
 cdef class StrategyBase(TimeIterator):
     def __init__(self):
         super().__init__()
+        self._past_trades = []
+        self._trade_listener = OrderFilledListener(self)
 
     @property
-    def active_orders(self) -> List[LimitOrder]:
-        raise NotImplementedError
-
-    @property
-    def trades(self) -> List[Trade]:
+    def active_markets(self) -> List[MarketBase]:
         raise NotImplementedError
 
     def format_status(self):
         raise NotImplementedError
+
+    @property
+    def trades(self) -> List[Trade]:
+        return self._past_trades
+
+    cdef c_record_trade(self, object order_filled_event):
+        self._past_trades.append(
+            Trade(order_filled_event.symbol,
+                  order_filled_event.trade_type,
+                  order_filled_event.price,
+                  order_filled_event.amount))
+
+    cdef c_start(self, Clock clock, double timestamp):
+        cdef:
+            MarketBase typed_market
+
+        TimeIterator.c_start(self, clock, timestamp)
+        for active_market in self.active_markets:
+            typed_market = active_market
+            typed_market.c_add_listener(MarketEvent.OrderFilled.value, self._trade_listener)
