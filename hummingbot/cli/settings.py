60bc2cb015365b1d323818644484be1a025a98c4
Yvonne Zhang <yzhang1994@Gmail.com>
2019-04-11 11:00:27 -0700

    Fix prompt value type bug

--- hummingbot/cli/settings.py
+++ hummingbot/cli/settings.py
@@ -399,12 +399,13 @@ global_config_map = {
                                                   required_if=lambda: True),
     # Whether or not to invoke cancel_all on exit if marketing making on a open order book DEX (e.g. Radar Relay)
     "on_chain_cancel_on_exit":          ConfigVar(key="on_chain_cancel_on_exit",
-                                                  prompt=None,
+                                                  prompt="Would you like to cancel transactions on chain if using an "
+                                                         "open order books exchanges? >>> ",
                                                   required_if=lambda: False,
                                                   type_str="bool",
                                                   default=False),
     "exchange_rate_conversion":         ConfigVar(key="exchange_rate_conversion",
-                                                  prompt=None,
+                                                  prompt="Enter your custom exchange rate conversion settings >>> ",
                                                   required_if=lambda: False,
                                                   type_str="list",
                                                   default=[["DAI", 1.0, "COINCAP_API"],
@@ -429,6 +430,8 @@ def parse_cvar_value(cvar: ConfigVar, value: any):
     elif cvar.type == 'bool':
         if type(value) == str and value.lower() in ["true", "yes"]:
             return True
+        elif type(value) == str and value.lower() in ["false", "no"]:
+            return False
         else:
             return bool(value)
     else:
