287cb0cca33849fadd96622879a97ada92ac6f5c
Yvonne Zhang <yzhang1994@Gmail.com>
2019-04-11 18:21:04 -0700

    make changes according to team comments

--- wings/market/coinbase_pro_auth.py
+++ wings/market/coinbase_pro_auth.py
@@ -6,7 +6,7 @@ from typing import Dict
 
 
 class CoinbaseProAuth:
-    def __init__(self, api_key, secret_key, passphrase):
+    def __init__(self, api_key: str, secret_key: str, passphrase: str):
         self.api_key = api_key
         self.secret_key = secret_key
         self.passphrase = passphrase
@@ -19,18 +19,18 @@ class CoinbaseProAuth:
         signature_b64 = base64.b64encode(bytes(signature.digest())).decode('utf8')
 
         return {
-            'signature': signature_b64,
-            'timestamp': timestamp,
-            'key': self.api_key,
-            'passphrase': self.passphrase,
+            "signature": signature_b64,
+            "timestamp": timestamp,
+            "key": self.api_key,
+            "passphrase": self.passphrase,
         }
 
     def get_headers(self, method: str, path_url: str, body: str = "") -> Dict[str, any]:
         header_dict = self.generate_auth_dict(method, path_url, body)
         return {
-            'CB-ACCESS-SIGN': header_dict["signature"],
-            'CB-ACCESS-TIMESTAMP': header_dict["timestamp"],
-            'CB-ACCESS-KEY': header_dict["key"],
-            'CB-ACCESS-PASSPHRASE': header_dict["passphrase"],
-            'Content-Type': 'application/json',
+            "CB-ACCESS-SIGN": header_dict["signature"],
+            "CB-ACCESS-TIMESTAMP": header_dict["timestamp"],
+            "CB-ACCESS-KEY": header_dict["key"],
+            "CB-ACCESS-PASSPHRASE": header_dict["passphrase"],
+            "Content-Type": 'application/json',
         }
