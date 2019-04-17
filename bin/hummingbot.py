d1e787404e5be82cb8b468b86cfa0cda5c9d6fb9
Martin Kou <bitanarch@gmail.com>
2019-04-17 11:59:56 -0700

    Added logic to autoincrement debug console port, to allow multiple instances to run in debug mode.

--- bin/hummingbot.py
+++ bin/hummingbot.py
@@ -16,6 +16,8 @@ else:
     sys.path.insert(0, realpath(join(__file__, "../../")))
 
 import asyncio
+import errno
+import socket
 from typing import (
     List,
     Coroutine
@@ -35,6 +37,20 @@ from hummingbot.cli.ui.stdout_redirection import patch_stdout
 from hummingbot.management.console import start_management_console
 
 
+def detect_available_port(starting_port: int) -> int:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        current_port: int = starting_port
+        while current_port < 65535:
+            try:
+                s.bind(("127.0.0.1", current_port))
+                break
+            except OSError as e:
+                if e.errno == errno.EADDRINUSE:
+                    current_port += 1
+                    continue
+        return current_port
+
+
 async def main():
     await create_yml_files()
     read_configs_from_yml()
@@ -47,7 +63,8 @@ async def main():
                      override_log_level=global_config_map.get("log_level").value)
         tasks: List[Coroutine] = [hb.run()]
         if global_config_map.get("debug_console").value:
-            tasks.append(start_management_console(locals(), host="localhost", port=8211))
+            management_port: int = detect_available_port(8211)
+            tasks.append(start_management_console(locals(), host="localhost", port=management_port))
         await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
