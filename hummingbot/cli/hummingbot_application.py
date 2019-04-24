5e8d75bafd9960251f87f0a254e97d6cb60641e8
Yvonne Zhang <yzhang1994@Gmail.com>
2019-04-24 16:35:10 -0700

    (feat) add in performance analytics

--- hummingbot/cli/hummingbot_application.py
+++ hummingbot/cli/hummingbot_application.py
@@ -663,7 +663,6 @@ class HummingbotApplication:
             self.reporting_module.stop()
         if self.strategy_task is not None and not self.strategy_task.cancelled():
             self.strategy_task.cancel()
-        self.starting_balances = {}
         self.wallet = None
         self.strategy_task = None
         self.strategy = None
@@ -757,4 +756,5 @@ class HummingbotApplication:
                      ["    " + line for line in str(current_df).split("\n")])
 
         self.app.log("\n".join(lines))
+
 
