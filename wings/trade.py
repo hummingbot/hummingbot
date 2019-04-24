f841693bf5ef17b61aaec384f69ffb8233482d1f
Yvonne Zhang <yzhang1994@Gmail.com>
2019-04-24 16:59:38 -0700

    (refactor) delete unused code

--- wings/trade.py
+++ wings/trade.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
 
 from collections import namedtuple
-from typing import (
-    List,
-    Dict
-)
+from typing import List
 from datetime import datetime
 import pandas as pd
 
-from wings.order_book_row import OrderBookRow
 from wings.events import (
     TradeType,
     OrderType,
@@ -23,23 +19,6 @@ class Trade(namedtuple("_Trade", "symbol, side, price, amount, order_type, marke
     order_type: OrderType
     market: str
     timestamp: float
-
-    @classmethod
-    def trades_from_order_book_rows(cls,
-                                    symbol: str,
-                                    side: TradeType,
-                                    order_book_rows: List[OrderBookRow]) -> List["Trade"]:
-        return [Trade(symbol, side, r.price, r.amount) for r in order_book_rows]
-
-    @classmethod
-    def trade_from_binance_execution_report_event(cls, execution_report: Dict[str, any]) -> "Trade":
-        execution_type: str = execution_report.get("x")
-        if execution_type != "TRADE":
-            raise ValueError(f"Invalid execution type '{execution_type}'.")
-        return Trade(execution_report["s"],
-                     TradeType.BUY if execution_report["S"] == "BUY" else TradeType.SELL,
-                     float(execution_report["L"]),
-                     float(execution_report["l"]))
 
     @classmethod
     def to_pandas(cls, trades: List):
