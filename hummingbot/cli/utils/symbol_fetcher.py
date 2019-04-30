db37dc66778bdd63b2a2c59ec533d0a4d8cfcab7
Yvonne Zhang <yzhang1994@Gmail.com>
2019-04-30 10:43:37 -0700

    (fix) fix radar symbol fetcher

--- hummingbot/cli/utils/symbol_fetcher.py
+++ hummingbot/cli/utils/symbol_fetcher.py
@@ -8,7 +8,7 @@ from typing import (
 
 BINANCE_ENDPOINT = "https://api.binance.com/api/v1/exchangeInfo"
 DDEX_ENDPOINT = "https://api.ddex.io/v3/markets"
-RADAR_RELAY_ENDPOINT = "https://api.radarrelay.com/v2/markets/"
+RADAR_RELAY_ENDPOINT = "https://api.radarrelay.com/v2/markets"
 COINBASE_PRO_ENDPOINT = "https://api.pro.coinbase.com/products/"
 API_CALL_TIMEOUT = 5
 
@@ -42,16 +42,25 @@ async def fetch_ddex_symbols() -> List[str]:
 
 
 async def fetch_radar_relay_symbols() -> List[str]:
-    async with aiohttp.ClientSession() as client:
-        async with client.get(RADAR_RELAY_ENDPOINT, timeout=API_CALL_TIMEOUT) as response:
-            if response.status == 200:
-                try:
-                    markets = await response.json()
-                    symbols = list(map(lambda symbol_details: symbol_details.get('id'), markets))
-                    return symbols
-                except Exception:
-                    # Do nothing if the request fails -- there will be no autocomplete for ddex symbols
-                    return []
+    symbols = set()
+    page_count = 1
+    while True:
+        async with aiohttp.ClientSession() as client:
+            async with client.get(f"{RADAR_RELAY_ENDPOINT}?perPage=100&page={page_count}", timeout=API_CALL_TIMEOUT) \
+                    as response:
+                if response.status == 200:
+                    try:
+                        markets = await response.json()
+                        new_symbols = set(map(lambda symbol_details: symbol_details.get('id'), markets))
+                        if len(new_symbols) == 0:
+                            break
+                        else:
+                            symbols = symbols.union(new_symbols)
+                        page_count += 1
+                    except Exception:
+                        # Do nothing if the request fails -- there will be no autocomplete for radar symbols
+                        break
+    return list(symbols)
 
 
 async def fetch_coinbase_pro_symbols() -> List[str]:
@@ -63,7 +72,7 @@ async def fetch_coinbase_pro_symbols() -> List[str]:
                     symbols = list(map(lambda symbol_details: symbol_details.get('id'), markets))
                     return symbols
                 except Exception:
-                    # Do nothing if the request fails -- there will be no autocomplete for ddex symbols
+                    # Do nothing if the request fails -- there will be no autocomplete for coinbase symbols
                     return []
 
 
@@ -78,3 +87,7 @@ async def fetch_all() -> Dict[str, List[str]]:
         "radar_relay": radar_relay_symbols,
         "coinbase_pro": coinbase_pro_symbols,
     }
+
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(fetch_radar_relay_symbols())
