f6f081331c466514721508c917118c66dcbbe291
Yvonne Zhang <yzhang1994@Gmail.com>
2019-05-03 16:19:23 -0700

    (feat) add stop loss feature

--- hummingbot/cli/config/global_config_map.py
+++ hummingbot/cli/config/global_config_map.py
@@ -128,6 +128,20 @@ global_config_map = {
                                                   required_if=lambda: False,
                                                   type_str="list",
                                                   default=[["ETH", "COINCAP_API"],
-                                                           ["DAI", "COINCAP_API"]])
+                                                           ["DAI", "COINCAP_API"]]),
+    "stop_loss_pct":                    ConfigVar(key="stop_upon_loss_pct",
+                                                  prompt="At what percentage of loss would you like the bot to stop "
+                                                         "trading? (Default 0.0) >>> ",
+                                                  default=0.0,
+                                                  type_str="float"),
+    "stop_loss_price_type":             ConfigVar(key="stop_loss_price_type",
+                                                  prompt="Would type of price data would you like to use for stop "
+                                                         "loss (fixed/dynamic) ? >>> ",
+                                                  default="fixed",
+                                                  validator=lambda v: v in {"fixed", "dynamic"}),
+    "stop_loss_base_token":             ConfigVar(key="stop_loss_base_token",
+                                                  prompt="Would base token would you like to use to calculate your "
+                                                         "inventory value? (Default \"USD\") >>> ",
+                                                  default="USD")
 }
 
