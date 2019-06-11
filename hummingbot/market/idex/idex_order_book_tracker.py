d7d2077d122269dc1a0f71482b31228a437520ea
Ryan Chow <chowryan@gmail.com>
2019-06-11 16:14:35 -0700

    [0.9.0] feat / IDEX connector (#348)
    * (feat) initial idex connector
    
    * (feat) save progress
    
    * (feat) make trading rule work for radar
    
    * IDEX market
    
    * (feat) add idex to settings
    
    * (feat) add idex initializer and symbol fetcher
    
    * (fix) fix typo
    
    * fixing idex market
    
    * fix idex cancel all
    
    * remove trading rule refactor commit
    
    * refactor
    
    * refactor
    
    * fix env file
    
    * fix price quantum idex
    
    * (update) setup linux environment
    
    * fix idex balances calculation
    
    * fix datatype in active order tracked
    
    * fix cancel all
    
    * refactor
    
    * idex - order nonce requests
    
    * fix idex market
    
    * fix idex order fill event
    
    * fix idex expiring orders
    
    * fix merge error
    
    * remove print
    
    * refactor

--- hummingbot/market/idex/idex_order_book_tracker.py
+++ hummingbot/market/idex/idex_order_book_tracker.py
@@ -2,9 +2,12 @@
 
 import asyncio
 import bisect
-from collections import deque, defaultdict
 import logging
 import time
+from collections import (
+  defaultdict,
+  deque
+)
 from typing import (
     Deque,
     Dict,
@@ -14,52 +17,56 @@ from typing import (
 )
 
 from hummingbot.logger import HummingbotLogger
-from hummingbot.core.data_type.order_book_tracker import OrderBookTracker, OrderBookTrackerDataSourceType
+from hummingbot.core.data_type.order_book_tracker import (
+    OrderBookTracker,
+    OrderBookTrackerDataSourceType
+)
 from hummingbot.core.data_type.order_book_tracker_data_source import OrderBookTrackerDataSource
-from hummingbot.market.radar_relay.radar_relay_api_order_book_data_source import RadarRelayAPIOrderBookDataSource
-from hummingbot.core.data_type.order_book_message import OrderBookMessageType, RadarRelayOrderBookMessage
-from hummingbot.core.data_type.order_book_tracker_entry import RadarRelayOrderBookTrackerEntry
-from hummingbot.market.radar_relay.radar_relay_order_book import RadarRelayOrderBook
-from hummingbot.market.radar_relay.radar_relay_active_order_tracker import RadarRelayActiveOrderTracker
+from hummingbot.market.idex.idex_order_book import IDEXOrderBook
+from hummingbot.market.idex.idex_active_order_tracker import IDEXActiveOrderTracker
+from hummingbot.market.idex.idex_api_order_book_data_source import IDEXAPIOrderBookDataSource
+from hummingbot.core.data_type.order_book_message import (
+    OrderBookMessageType,
+    IDEXOrderBookMessage
+)
+from hummingbot.core.data_type.order_book_tracker_entry import IDEXOrderBookTrackerEntry
 
 
-class RadarRelayOrderBookTracker(OrderBookTracker):
-    _rrobt_logger: Optional[HummingbotLogger] = None
+class IDEXOrderBookTracker(OrderBookTracker):
+    _iobt_logger: Optional[HummingbotLogger] = None
 
     @classmethod
     def logger(cls) -> HummingbotLogger:
-        if cls._rrobt_logger is None:
-            cls._rrobt_logger = logging.getLogger(__name__)
-        return cls._rrobt_logger
+        if cls._iobt_logger is None:
+            cls._iobt_logger = logging.getLogger(__name__)
+        return cls._iobt_logger
 
     def __init__(self,
                  data_source_type: OrderBookTrackerDataSourceType = OrderBookTrackerDataSourceType.EXCHANGE_API,
                  symbols: Optional[List[str]] = None):
         super().__init__(data_source_type=data_source_type)
-
+        self._past_diffs_windows: Dict[str, Deque] = {}
+        self._order_books: Dict[str, IDEXOrderBook] = {}
+        self._saved_message_queues: Dict[str, Deque[IDEXOrderBookMessage]] = defaultdict(lambda: deque(maxlen=1000))
+        self._order_book_diff_stream: asyncio.Queue = asyncio.Queue()
+        self._order_book_snapshot_stream: asyncio.Queue = asyncio.Queue()
         self._ev_loop: asyncio.BaseEventLoop = asyncio.get_event_loop()
         self._data_source: Optional[OrderBookTrackerDataSource] = None
-        self._order_book_snapshot_stream: asyncio.Queue = asyncio.Queue()
-        self._order_book_diff_stream: asyncio.Queue = asyncio.Queue()
-        self._process_msg_deque_task: Optional[asyncio.Task] = None
-        self._past_diffs_windows: Dict[str, Deque] = {}
-        self._order_books: Dict[str, RadarRelayOrderBook] = {}
-        self._saved_message_queues: Dict[str, Deque[RadarRelayOrderBookMessage]] = defaultdict(lambda: deque(maxlen=1000))
-        self._active_order_trackers: Dict[str, RadarRelayActiveOrderTracker] = defaultdict(RadarRelayActiveOrderTracker)
+        self._active_order_trackers: Dict[str, IDEXActiveOrderTracker] = defaultdict(IDEXActiveOrderTracker)
         self._symbols: Optional[List[str]] = symbols
 
     @property
     def data_source(self) -> OrderBookTrackerDataSource:
         if not self._data_source:
             if self._data_source_type is OrderBookTrackerDataSourceType.EXCHANGE_API:
-                self._data_source = RadarRelayAPIOrderBookDataSource(symbols=self._symbols)
+                self._data_source = IDEXAPIOrderBookDataSource(symbols=self._symbols)
             else:
                 raise ValueError(f"data_source_type {self._data_source_type} is not supported.")
         return self._data_source
 
     @property
     async def exchange_name(self) -> str:
-        return "radar_relay"
+        return "idex"
 
     async def start(self):
         self._order_book_diff_listener_task = asyncio.ensure_future(
@@ -90,13 +97,13 @@ class RadarRelayOrderBookTracker(OrderBookTracker):
         """
         tracking_symbols: Set[str] = set([key for key in self._tracking_tasks.keys()
                                           if not self._tracking_tasks[key].done()])
-        available_pairs: Dict[str, RadarRelayOrderBookTrackerEntry] = await self.data_source.get_tracking_pairs()
+        available_pairs: Dict[str, IDEXOrderBookTrackerEntry] = await self.data_source.get_tracking_pairs()
         available_symbols: Set[str] = set(available_pairs.keys())
         new_symbols: Set[str] = available_symbols - tracking_symbols
         deleted_symbols: Set[str] = tracking_symbols - available_symbols
 
         for symbol in new_symbols:
-            order_book_tracker_entry: RadarRelayOrderBookTrackerEntry = available_pairs[symbol]
+            order_book_tracker_entry: IDEXOrderBookTrackerEntry = available_pairs[symbol]
             self._active_order_trackers[symbol] = order_book_tracker_entry.active_order_tracker
             self._order_books[symbol] = order_book_tracker_entry.order_book
             self._tracking_message_queues[symbol] = asyncio.Queue()
@@ -119,24 +126,20 @@ class RadarRelayOrderBookTracker(OrderBookTracker):
         messages_queued: int = 0
         messages_accepted: int = 0
         messages_rejected: int = 0
-        address_token_map: Dict[str, any] = await self._data_source.get_all_token_info()
+
         while True:
             try:
-                ob_message: RadarRelayOrderBookMessage = await self._order_book_diff_stream.get()
-                base_token_address: str = ob_message.content["event"]["baseTokenAddress"]
-                quote_token_address: str = ob_message.content["event"]["quoteTokenAddress"]
-                base_token_symbol: str = address_token_map[base_token_address]["symbol"]
-                quote_token_symbol: str = address_token_map[quote_token_address]['symbol']
-                trading_pair_symbol: str = f"{base_token_symbol}-{quote_token_symbol}"
+                ob_message: IDEXOrderBookMessage = await self._order_book_diff_stream.get()
+                symbol: str = ob_message.symbol
 
-                if trading_pair_symbol not in self._tracking_message_queues:
+                if symbol not in self._tracking_message_queues:
                     messages_queued += 1
                     # Save diff messages received before snapshots are ready
-                    self._saved_message_queues[trading_pair_symbol].append(ob_message)
+                    self._saved_message_queues[symbol].append(ob_message)
                     continue
-                message_queue: asyncio.Queue = self._tracking_message_queues[trading_pair_symbol]
+                message_queue: asyncio.Queue = self._tracking_message_queues[symbol]
                 # Check the order book's initial update ID. If it's larger, don't bother.
-                order_book: RadarRelayOrderBook = self._order_books[trading_pair_symbol]
+                order_book: IDEXOrderBook = self._order_books[symbol]
 
                 if order_book.snapshot_uid > ob_message.update_id:
                     messages_rejected += 1
@@ -163,20 +166,18 @@ class RadarRelayOrderBookTracker(OrderBookTracker):
                 await asyncio.sleep(5.0)
 
     async def _track_single_book(self, symbol: str):
-        past_diffs_window: Deque[RadarRelayOrderBookMessage] = deque()
+        past_diffs_window: Deque[IDEXOrderBookMessage] = deque()
         self._past_diffs_windows[symbol] = past_diffs_window
-
         message_queue: asyncio.Queue = self._tracking_message_queues[symbol]
-        order_book: RadarRelayOrderBook = self._order_books[symbol]
-        active_order_tracker: RadarRelayActiveOrderTracker = self._active_order_trackers[symbol]
-
-        last_message_timestamp: float = time.time()
+        order_book: IDEXOrderBook = self._order_books[symbol]
+        active_order_tracker: IDEXActiveOrderTracker = self._active_order_trackers[symbol]
+        last_message_timestamp: float = time.time() 
         diff_messages_accepted: int = 0
 
         while True:
             try:
-                message: RadarRelayOrderBookMessage = None
-                saved_messages: Deque[RadarRelayOrderBookMessage] = self._saved_message_queues[symbol]
+                message: IDEXOrderBookMessage = None
+                saved_messages: Deque[IDEXOrderBookMessage] = self._saved_message_queues[symbol]
                 # Process saved messages first if there are any
                 if len(saved_messages) > 0:
                     message = saved_messages.popleft()
@@ -198,8 +199,9 @@ class RadarRelayOrderBookTracker(OrderBookTracker):
                                            diff_messages_accepted, symbol)
                         diff_messages_accepted = 0
                     last_message_timestamp = now
+                    # pass
                 elif message.type is OrderBookMessageType.SNAPSHOT:
-                    past_diffs: List[RadarRelayOrderBookMessage] = list(past_diffs_window)
+                    past_diffs: List[IDEXOrderBookMessage] = list(past_diffs_window)
                     # only replay diffs later than snapshot, first update active order with snapshot then replay diffs
                     replay_position = bisect.bisect_right(past_diffs, message)
                     replay_diffs = past_diffs[replay_position:]
