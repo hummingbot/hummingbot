c7834948d1b3d99d44990e184d635d003d50e925
Anish Neervannan <anish.neervannan@gmail.com>
2019-06-24 16:00:57 -0700

    refactoring

--- hummingbot/client/data_type/currency_amount.py
+++ hummingbot/client/data_type/currency_amount.py
@@ -1,6 +1,24 @@
-from collections import namedtuple
 
+class CurrencyAmount:
 
-class CurrencyAmount(namedtuple("_CurrencyAmount", "token, amount")):
-    token: str
-    amount: float
+    def __init__(self):
+        self._token = None
+        self._amount = None
+
+    def set_token(self, token: str):
+        self._token = token
+
+    def get_token(self) -> str:
+        return self._token
+
+    def set_amount(self, amount: float):
+        self._amount = amount
+
+    def get_amount(self) -> float:
+        return self._amount
+
+    def add_amount(self, amount: float):
+        self._amount += amount
+
+    def subtract_amount(self, amount: float):
+        self._amount -= amount
