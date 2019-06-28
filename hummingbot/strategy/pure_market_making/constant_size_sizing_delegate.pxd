7acdf8eec3afc83f13c87679be29e767c9e5ac20
vignesh <vigneshmani@Vigneshs-MacBook-Pro.local>
2019-06-28 12:04:44 -0700

    (fix)display logs once

--- hummingbot/strategy/pure_market_making/constant_size_sizing_delegate.pxd
+++ hummingbot/strategy/pure_market_making/constant_size_sizing_delegate.pxd
@@ -4,3 +4,4 @@ from .order_sizing_delegate cimport OrderSizingDelegate
 cdef class ConstantSizeSizingDelegate(OrderSizingDelegate):
     cdef:
         double _order_size
+        bint _log_warn
--- hummingbot/strategy/pure_market_making/staggered_multiple_size_sizing_delegate.pyx
+++ hummingbot/strategy/pure_market_making/staggered_multiple_size_sizing_delegate.pyx
@@ -19,6 +19,7 @@ cdef class StaggeredMultipleSizeSizingDelegate(OrderSizingDelegate):
         self._order_start_size = order_start_size
         self._order_step_size = order_step_size
         self._number_of_orders = number_of_orders
+        self._log_warn = True
 
     @classmethod
     def logger(cls) -> HummingbotLogger:
@@ -39,6 +40,10 @@ cdef class StaggeredMultipleSizeSizingDelegate(OrderSizingDelegate):
     @property
     def number_of_orders(self) -> int:
         return self._number_of_orders
+
+    @property
+    def log_warn(self) -> bool:
+        return self._log_warn
 
     cdef object c_get_order_size_proposal(self,
                                           PureMarketMakingStrategyV2 strategy,
@@ -71,21 +76,31 @@ cdef class StaggeredMultipleSizeSizingDelegate(OrderSizingDelegate):
             else:
                 current_order_size = market.c_quantize_order_amount(market_info.symbol, current_order_size)
 
-            if current_order_size == 0:
-                self.logger().network(f"Order size is less than minimum order size for Price: {pricing_proposal.buy_order_prices[idx]} ",
-                                      f"The orders for price of {pricing_proposal.buy_order_prices[idx]} are too small for the market. Check configuration")
+            if self.log_warn:
+                if current_order_size == 0:
+                    self.logger().network(f"Order size is less than minimum order size for Price: {pricing_proposal.buy_order_prices[idx]} ",
+                                          f"The orders for price of {pricing_proposal.buy_order_prices[idx]} are too small for the market. Check configuration")
 
             orders.append(current_order_size)
 
-        if quote_asset_balance < required_quote_asset_balance:
-            self.logger().network(f"Buy(bid) order is not placed because there is not enough Quote asset. "
-                                  f"Quote Asset: {quote_asset_balance}, Required Quote Asset: {required_quote_asset_balance}",
-                                  f"Not enough asset to place the required buy(bid) orders. Check balances.")
+        if self.log_warn:
+            if quote_asset_balance < required_quote_asset_balance:
+                self.logger().network(f"Buy(bid) order is not placed because there is not enough Quote asset. "
+                                      f"Quote Asset: {quote_asset_balance}, Required Quote Asset: {required_quote_asset_balance}",
+                                      f"Not enough asset to place the required buy(bid) orders. Check balances.")
 
-        if (base_asset_balance < required_base_asset_balance):
-            self.logger().network(f"Sell(ask) order is not placed because there is not enough Base asset. "
-                                  f"Base Asset: {base_asset_balance}, Required Base Asset: {required_base_asset_balance}",
-                                  f"Not enough asset to place the required sell(ask) orders. Check balances.")
+            if (base_asset_balance < required_base_asset_balance):
+                self.logger().network(f"Sell(ask) order is not placed because there is not enough Base asset. "
+                                      f"Base Asset: {base_asset_balance}, Required Base Asset: {required_base_asset_balance}",
+                                      f"Not enough asset to place the required sell(ask) orders. Check balances.")
+
+        #After warning once, do not warn again
+        if self.log_warn:
+            self._log_warn = False
+
+        #Reset warnings if both orders are placed again
+        if (quote_asset_balance >= required_quote_asset_balance and not has_active_bid) and (base_asset_balance >= required_base_asset_balance and not has_active_ask):
+            self._log_warn = True
 
         return SizingProposal(
             (orders
