e3875fc95f67dd698ca4eef49fcefa28746aa3a8
carlolm <carlol@coinalpha.com>
2019-07-05 11:58:22 -0700

    (refactor) debian:source scripts

--- scripts/debian/debian-install-source-1.sh
+++ scripts/debian/debian-install-source-1.sh
@@ -1,13 +1,9 @@
 # SETUP DEPENDENCIES
-
 # 1) Install dependencies
-sudo apt-get update
-sudo apt-get install -y build-essential git
-
+sudo apt-get update && \
+sudo apt-get install -y build-essential git && \
 # 2) Install Miniconda3
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-sh Miniconda3-latest-Linux-x86_64.sh
-
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh && \
+sh Miniconda3-latest-Linux-x86_64.sh && \
 # 3) Log out and log back into shell to register "conda" command
 exit
-# Log back into or open a new Linux terminal
--- scripts/debian/debian-install-source-2.sh
+++ scripts/debian/debian-install-source-2.sh
@@ -1,13 +1,9 @@
 # INSTALL HUMMINGBOT
-
 # 4) Clone Hummingbot
-git clone https://github.com/CoinAlpha/hummingbot.git
-
+git clone https://github.com/CoinAlpha/hummingbot.git && \
 # 5) Install Hummingbot
-cd hummingbot && ./install
-
+cd hummingbot && ./install && \
 # 6) Activate environment and compile code
-conda activate hummingbot && ./compile
-
+conda activate hummingbot && ./compile && \
 # 7) Start Hummingbot
 bin/hummingbot.py
