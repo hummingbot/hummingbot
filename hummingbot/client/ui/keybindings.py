e97e2655a9db23f264135e6e2415c9b8369156f2
Yvonne Zhang <yzhang1994@Gmail.com>
2019-07-09 15:29:21 -0700

    (feat) add search to log pane

--- hummingbot/client/ui/keybindings.py
+++ hummingbot/client/ui/keybindings.py
@@ -1,39 +1,66 @@
 #!/usr/bin/env python
 
 import asyncio
-from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.application.current import get_app
-from prompt_toolkit.search import start_search, stop_search
+from prompt_toolkit.filters import (
+    is_searching,
+    to_filter,
+)
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.search import (
+    start_search,
+    stop_search,
+)
 
 
 def load_key_bindings(hb) -> KeyBindings:
     bindings = KeyBindings()
 
     @bindings.add("c-c", "c-c")
-    def _(event):
+    def exit_(event):
         hb.app.log("\n[Double CTRL + C] keyboard exit")
-        asyncio.ensure_future(hb.exit())
+        asyncio.ensure_future(hb.exit_loop())
 
     @bindings.add("c-s")
-    def _(event):
+    def status(event):
         hb.app.log("\n[CTRL + S] Status")
         hb.status()
 
-    @bindings.add("c-f")
-    def _(event):
+    @bindings.add("c-f", filter=to_filter(not is_searching()))
+    def do_find(event):
         start_search(hb.app.log_field.control)
 
-    @bindings.add("c-w")
-    def _(event):
+    @bindings.add("c-f", filter=is_searching)
+    def do_exit_find(event):
         stop_search(hb.app.log_field.control)
-        hb.app.input_field.buffer.focus()
+        get_app().layout.focus(hb.app.input_field.control)
+
+    @bindings.add("c-z")
+    def do_undo(event):
+        get_app().layout.current_buffer.undo()
 
     @bindings.add("c-d")
-    def _(event):
+    def do_find_next(event):
         search_state = get_app().current_search_state
-        text_field = hb.app.log_field.control
-        cursor_position = text_field.buffer.get_search_position(
+
+        cursor_position = get_app().layout.current_buffer.get_search_position(
             search_state, include_current_position=False)
-        text_field.buffer.cursor_position = cursor_position
+        get_app().layout.current_buffer.cursor_position = cursor_position
+
+    @bindings.add("c-c")
+    def do_copy(event):
+        data = get_app().layout.current_buffer.copy_selection()
+        get_app().clipboard.set_data(data)
+
+    @bindings.add("c-v")
+    def do_paste(event):
+        get_app().layout.current_buffer.paste_clipboard_data(get_app().clipboard.get_data())
+
+    @bindings.add("c-a")
+    def do_select_all(event):
+        current_buffer = get_app().layout.current_buffer
+        current_buffer.cursor_position = 0
+        current_buffer.start_selection()
+        current_buffer.cursor_position = len(current_buffer.text)
 
     return bindings
