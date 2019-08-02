ba9b995b240b4ab4007b25bb5f8af71e67fdbcad
Ryan Chow <chowryan@gmail.com>
2019-08-02 16:10:32 -0700

    use entire inventory for order size if not enough funds to create calculated order

--- hummingbot/strategy/pure_market_making/inventory_skew_single_size_sizing_delegate.pyx
+++ hummingbot/strategy/pure_market_making/inventory_skew_single_size_sizing_delegate.pyx
@@ -103,6 +103,15 @@ cdef class InventorySkewSingleSizeSizingDelegate(OrderSizingDelegate):
                                                                       float(ask_order_size),
                                                                       pricing_proposal.sell_order_prices[0])
             required_quote_asset_balance = float(pricing_proposal.buy_order_prices[0]) * float(quantized_bid_order_size)
+            if quote_asset_balance < required_quote_asset_balance:
+                bid_order_size = Decimal(quote_asset_balance / float(pricing_proposal.buy_order_prices[0]))
+                quantized_bid_order_size = market.c_quantize_order_amount(market_info.trading_pair,
+                                                                          float(bid_order_size),
+                                                                          pricing_proposal.buy_order_prices[0])
+            if base_asset_balance < quantized_ask_order_size:
+                quantized_ask_order_size = market.c_quantize_order_amount(market_info.trading_pair,
+                                                                          float(base_asset_balance),
+                                                                          pricing_proposal.sell_order_prices[0])
 
         else:
             quantized_bid_order_size = market.c_quantize_order_amount(market_info.trading_pair,
@@ -117,35 +126,35 @@ cdef class InventorySkewSingleSizeSizingDelegate(OrderSizingDelegate):
             required_quote_asset_balance = (float(pricing_proposal.buy_order_prices[0]) *
                                             (1.0 + float(buy_fees.percent)) *
                                             quantized_bid_order_size)
+        
+        # if self._log_warning_order_size:
+        #     if quantized_bid_order_size == s_decimal_0:
+        #         self.logger().network(f"Buy(bid) order size is less than minimum order size. Buy order will not be placed",
+        #                               f"The order size is too small for the market for buy order. Check order size in configuration.")
+        #         # After warning once, set warning flag to False
+        #         self._log_warning_order_size = False
 
-        if self._log_warning_order_size:
-            if quantized_bid_order_size == s_decimal_0:
-                self.logger().network(f"Buy(bid) order size is less than minimum order size. Buy order will not be placed",
-                                      f"The order size is too small for the market for buy order. Check order size in configuration.")
-                # After warning once, set warning flag to False
-                self._log_warning_order_size = False
+        #     if quantized_ask_order_size == s_decimal_0:
+        #          self.logger().network(f"Sell(ask) order size is less than minimum order size. Sell order will not be placed",
+        #                                f"The order size is too small for the market for sell order. Check order size in configuration.")
+        #          # After warning once, set warning flag to False
+        #          self._log_warning_order_size = False
 
-            if quantized_ask_order_size == s_decimal_0:
-                 self.logger().network(f"Sell(ask) order size is less than minimum order size. Sell order will not be placed",
-                                       f"The order size is too small for the market for sell order. Check order size in configuration.")
-                 # After warning once, set warning flag to False
-                 self._log_warning_order_size = False
+        # if self._log_warning_balance:
+        #     if quote_asset_balance < required_quote_asset_balance:
+        #         self.logger().network(f"Buy(bid) order is not placed because there is not enough Quote asset. "
+        #                               f"Quote Asset: {quote_asset_balance}, Price: {pricing_proposal.buy_order_prices[0]},"
+        #                               f"Size: {quantized_bid_order_size}",
+        #                               f"Not enough asset to place the required buy(bid) order. Check balances.")
+        #         # After warning once, set warning flag to False
+        #         self._log_warning_balance = False
 
-        if self._log_warning_balance:
-            if quote_asset_balance < required_quote_asset_balance:
-                self.logger().network(f"Buy(bid) order is not placed because there is not enough Quote asset. "
-                                      f"Quote Asset: {quote_asset_balance}, Price: {pricing_proposal.buy_order_prices[0]},"
-                                      f"Size: {quantized_bid_order_size}",
-                                      f"Not enough asset to place the required buy(bid) order. Check balances.")
-                # After warning once, set warning flag to False
-                self._log_warning_balance = False
-
-            if base_asset_balance < quantized_ask_order_size:
-                self.logger().network(f"Sell(ask) order is not placed because there is not enough Base asset. "
-                                      f"Base Asset: {base_asset_balance}, Size: {quantized_ask_order_size}",
-                                      f"Not enough asset to place the required sell(ask) order. Check balances.")
-                # After warning once, set warning flag to False
-                self._log_warning_balance = False
+        #     if base_asset_balance < quantized_ask_order_size:
+        #         self.logger().network(f"Sell(ask) order is not placed because there is not enough Base asset. "
+        #                               f"Base Asset: {base_asset_balance}, Size: {quantized_ask_order_size}",
+        #                               f"Not enough asset to place the required sell(ask) order. Check balances.")
+        #         # After warning once, set warning flag to False
+        #         self._log_warning_balance = False
 
 
         # Reset warning flag for balances if there is enough balance to place orders
@@ -160,18 +169,18 @@ cdef class InventorySkewSingleSizeSizingDelegate(OrderSizingDelegate):
             print('*** current_quote_percent', current_quote_percent)
             print('*** current_target_quote_ratio', current_target_quote_ratio)
             print('*** adjustment_quote_ratio', current_target_quote_ratio)
-            print('*** adjusted bid_order_size', bid_order_size, quantized_bid_order_size)
+            print('*** adjusted bid_order_size', quantized_bid_order_size)
         if base_asset_balance >= quantized_ask_order_size and not has_active_ask:
             print('*** current_base_percent', current_base_percent)
             print('*** current_target_base_ratio', current_target_base_ratio)
             print('*** adjustment_base_ratio', current_target_base_ratio)
-            print('*** adjusted ask_order_size', ask_order_size, quantized_ask_order_size)
+            print('*** adjusted ask_order_size', quantized_ask_order_size)
 
         return SizingProposal(
             ([quantized_bid_order_size]
-             if quote_asset_balance >= required_quote_asset_balance and not has_active_bid
+             if not has_active_bid
              else [s_decimal_0]),
             ([quantized_ask_order_size]
-             if base_asset_balance >= quantized_ask_order_size and not has_active_ask else
+             if not has_active_ask else
              [s_decimal_0])
         )
