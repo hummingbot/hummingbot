8e03bfa3e7ffb9e34b7d69c11394d0d992f43b5a
michael chuang <michaelc@coinalpha.com>
2019-08-31 03:44:52 -0700

    (feat) Add trade event emiter task in order book tracker

--- hummingbot/core/data_type/order_book_tracker.py
+++ hummingbot/core/data_type/order_book_tracker.py
@@ -15,6 +15,7 @@ from typing import (
     Tuple,
     List)
 
+from hummingbot.core.event.events import OrderBookTradeEvent, TradeType
 from hummingbot.logger import HummingbotLogger
 from hummingbot.core.data_type.order_book import OrderBook
 from hummingbot.core.data_type.order_book_tracker_entry import OrderBookTrackerEntry
@@ -51,12 +52,15 @@ class OrderBookTracker(ABC):
         self._tracking_message_queues: Dict[str, asyncio.Queue] = {}
         self._past_diffs_windows: Dict[str, Deque] = {}
         self._order_book_diff_listener_task: Optional[asyncio.Task] = None
+        self._order_book_trade_listener_task: Optional[asyncio.Task] = None
         self._order_book_snapshot_listener_task: Optional[asyncio.Task] = None
         self._order_book_diff_router_task: Optional[asyncio.Task] = None
         self._order_book_snapshot_router_task: Optional[asyncio.Task] = None
+        self._emit_trade_event_task: Optional[asyncio.Task] = None
         self._refresh_tracking_task: Optional[asyncio.Task] = None
         self._order_book_diff_stream: asyncio.Queue = asyncio.Queue()
         self._order_book_snapshot_stream: asyncio.Queue = asyncio.Queue()
+        self._order_book_trade_stream: asyncio.Queue = asyncio.Queue()
         self._ev_loop: asyncio.BaseEventLoop = asyncio.get_event_loop()
 
     @property
@@ -66,6 +70,10 @@ class OrderBookTracker(ABC):
 
     @abstractmethod
     async def start(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def stop(self):
         raise NotImplementedError
 
     @property
@@ -200,7 +208,7 @@ class OrderBookTracker(ABC):
                     now: float = time.time()
                     if int(now / 60.0) > int(last_message_timestamp / 60.0):
                         self.logger().debug("Processed %d order book diffs for %s.",
-                                           diff_messages_accepted, symbol)
+                                            diff_messages_accepted, symbol)
                         diff_messages_accepted = 0
                     last_message_timestamp = now
                 elif message.type is OrderBookMessageType.SNAPSHOT:
@@ -211,4 +219,49 @@ class OrderBookTracker(ABC):
                 raise
             except Exception:
                 self.logger().error("Unknown error. Retrying after 5 seconds.", exc_info=True)
+                await asyncio.sleep(5.0)
+
+    async def _emit_trade_event_loop(self):
+        last_message_timestamp: float = time.time()
+        messages_accepted: int = 0
+        messages_rejected: int = 0
+        while True:
+            try:
+                trade_message: OrderBookMessage = await self._order_book_trade_stream.get()
+                symbol: str = trade_message.symbol
+
+                if symbol not in self._order_books:
+                    messages_rejected += 1
+                    continue
+
+                order_book: OrderBook = self._order_books[symbol]
+                order_book.apply_trade(OrderBookTradeEvent(
+                    symbol=trade_message.symbol,
+                    timestamp=trade_message.timestamp,
+                    price=float(trade_message.content["price"]),
+                    amount=float(trade_message.content["amount"]),
+                    type=TradeType.SELL if
+                    trade_message.content["trade_type"] == float(TradeType.SELL.value) else TradeType.SELL
+                ))
+
+                messages_accepted += 1
+
+                # Log some statistics.
+                now: float = time.time()
+                if int(now / 60.0) > int(last_message_timestamp / 60.0):
+                    self.logger().debug("Trade messages processed: %d, rejected: %d",
+                                        messages_accepted,
+                                        messages_rejected)
+                    messages_accepted = 0
+                    messages_rejected = 0
+
+                last_message_timestamp = now
+            except asyncio.CancelledError:
+                raise
+            except Exception:
+                self.logger().network(
+                    f"Unexpected error routing order book messages.",
+                    exc_info=True,
+                    app_warning_msg=f"Unexpected error routing order book messages. Retrying after 5 seconds."
+                )
                 await asyncio.sleep(5.0)
