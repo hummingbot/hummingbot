49b4b8611877d21c776673e6fd4c4b775b11d6e1
michael chuang <michaelc@coinalpha.com>
2019-08-31 03:46:20 -0700

    (feat) Add trade websocket fetcher in Binance API datasource

--- hummingbot/core/data_type/order_book_tracker_data_source.py
+++ hummingbot/core/data_type/order_book_tracker_data_source.py
@@ -17,13 +17,20 @@ class OrderBookTrackerDataSource(metaclass=ABCMeta):
     @abstractmethod
     async def listen_for_order_book_diffs(self, ev_loop: asyncio.BaseEventLoop, output: asyncio.Queue):
         """
-        Object type in the output queue must be OrderBookRecord
+        Object type in the output queue must be OrderBookMessage
         """
         raise NotImplementedError
 
     @abstractmethod
     async def listen_for_order_book_snapshots(self, ev_loop: asyncio.BaseEventLoop, output: asyncio.Queue):
         """
-        Object type in the output queue must be OrderBookRecord
+        Object type in the output queue must be OrderBookMessage
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    async def listen_for_trades(self, ev_loop: asyncio.BaseEventLoop, output: asyncio.Queue):
+        """
+        Object type in the output queue must be OrderBookMessage
         """
         raise NotImplementedError
