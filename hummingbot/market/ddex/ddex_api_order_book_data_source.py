02fa91529817347e43ac2ecfa766f4774acbb291
michael chuang <michaelc@coinalpha.com>
2019-08-31 05:32:37 -0700

    (feat) Add trade listener for DDEX order book tracker

--- hummingbot/market/ddex/ddex_api_order_book_data_source.py
+++ hummingbot/market/ddex/ddex_api_order_book_data_source.py
@@ -149,7 +149,7 @@ class DDEXAPIOrderBookDataSource(OrderBookTrackerDataSource):
                 try:
                     snapshot: Dict[str, any] = await self.get_snapshot(client, trading_pair, 3)
                     snapshot_timestamp: float = time.time()
-                    snapshot_msg: DDEXOrderBookMessage = self.order_book_class.snapshot_message_from_exchange(
+                    snapshot_msg: DDEXOrderBookMessage = DDEXOrderBook.snapshot_message_from_exchange(
                         snapshot,
                         snapshot_timestamp,
                         {"marketId": trading_pair}
@@ -206,6 +206,10 @@ class DDEXAPIOrderBookDataSource(OrderBookTrackerDataSource):
         finally:
             await ws.close()
 
+    async def listen_for_trades(self, ev_loop: asyncio.BaseEventLoop, output: asyncio.Queue):
+        # Trade messages are received from the order book web socket
+        pass
+
     async def listen_for_order_book_diffs(self, ev_loop: asyncio.BaseEventLoop, output: asyncio.Queue):
         while True:
             try:
@@ -223,9 +227,12 @@ class DDEXAPIOrderBookDataSource(OrderBookTrackerDataSource):
                     async for raw_msg in self._inner_messages(ws):
                         msg = ujson.loads(raw_msg)
                         # only process receive and done diff messages from DDEX
-                        if msg["type"] == "receive" or msg["type"] == "done":
-                            diff_msg: DDEXOrderBookMessage = self.order_book_class.diff_message_from_exchange(msg)
+                        if msg["type"] in ["receive", "done"]:
+                            diff_msg: DDEXOrderBookMessage = DDEXOrderBook.diff_message_from_exchange(msg)
                             output.put_nowait(diff_msg)
+                        elif msg["type"] == "trade":
+                            trade_msg: DDEXOrderBookMessage = DDEXOrderBook.trade_message_from_exchange(msg)
+                            output.put_nowait(trade_msg)
             except asyncio.CancelledError:
                 raise
             except Exception:
@@ -246,7 +253,7 @@ class DDEXAPIOrderBookDataSource(OrderBookTrackerDataSource):
                         try:
                             snapshot: Dict[str, any] = await self.get_snapshot(client, trading_pair)
                             snapshot_timestamp: float = time.time()
-                            snapshot_msg: DDEXOrderBookMessage = self.order_book_class.snapshot_message_from_exchange(
+                            snapshot_msg: DDEXOrderBookMessage = DDEXOrderBook.snapshot_message_from_exchange(
                                 snapshot,
                                 snapshot_timestamp,
                                 {"marketId": trading_pair}
