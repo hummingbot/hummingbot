17b051822b5cbc63c575a73d0ff089a6c572b067
Yvonne Zhang <yzhang1994@Gmail.com>
2019-09-18 16:46:03 -0700

    (fix) add return value to wrappers

--- hummingbot/core/utils/async_utils.py
+++ hummingbot/core/utils/async_utils.py
@@ -4,13 +4,13 @@ import logging
 
 def asyncio_ensure_future(*args, **kwargs):
     try:
-        asyncio.ensure_future(*args, **kwargs)
+        return asyncio.ensure_future(*args, **kwargs)
     except Exception as e:
         logging.getLogger().error(f"Unhandled error in background task: {str(e)}", exc_info=True)
 
 
 async def asyncio_gather(*args, **kwargs):
     try:
-        await asyncio.gather(*args, **kwargs)
+        return await asyncio.gather(*args, **kwargs)
     except Exception as e:
         logging.getLogger().debug(f"Unhandled error in background task: {str(e)}", exc_info=True)
