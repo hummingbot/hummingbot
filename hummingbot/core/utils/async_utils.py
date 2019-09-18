d96839c941c518250cd7fbe6857df7cf9758b544
Yvonne Zhang <yzhang1994@Gmail.com>
2019-09-18 15:37:37 -0700

    (feat) create asyncio wrappers

---
+++ hummingbot/core/utils/async_utils.py
@@ -0,0 +1,16 @@
+import asyncio
+import logging
+
+
+def asyncio_ensure_future(*args, **kwargs):
+    try:
+        asyncio.ensure_future(*args, **kwargs)
+    except Exception as e:
+        logging.getLogger().error(f"Unhandled error in background task: {str(e)}", exc_info=True)
+
+
+def asyncio_gather(*args, **kwargs):
+    try:
+        asyncio.gather(*args, **kwargs)
+    except Exception as e:
+        logging.getLogger().debug(f"Unhandled error in background task: {str(e)}", exc_info=True)
