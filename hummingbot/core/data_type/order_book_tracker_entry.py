a76eafdb7de326472f690a7ad67b95f5f3d053a2
svamol <svamol@gmail.com>
2019-09-23 21:59:45 +0530

    added bitroyal to order book and order book tracker, report agregator and conf global template

--- hummingbot/core/data_type/order_book_tracker_entry.py
+++ hummingbot/core/data_type/order_book_tracker_entry.py
@@ -2,6 +2,7 @@
 
 from hummingbot.core.data_type.order_book import OrderBook
 from hummingbot.market.coinbase_pro.coinbase_pro_active_order_tracker import CoinbaseProActiveOrderTracker
+from hummingbot.market.bitroyal.bitroyal_active_order_tracker import BitroyalActiveOrderTracker
 from hummingbot.market.ddex.ddex_active_order_tracker import DDEXActiveOrderTracker
 from hummingbot.market.idex.idex_active_order_tracker import IDEXActiveOrderTracker
 from hummingbot.market.radar_relay.radar_relay_active_order_tracker import RadarRelayActiveOrderTracker
@@ -108,3 +109,17 @@ class CoinbaseProOrderBookTrackerEntry(OrderBookTrackerEntry):
     def active_order_tracker(self) -> CoinbaseProActiveOrderTracker:
         return self._active_order_tracker
 
+
+class BitroyalOrderBookTrackerEntry(OrderBookTrackerEntry):
+    def __init__(self, symbol: str, timestamp: float, order_book: OrderBook,
+                 active_order_tracker: BitroyalActiveOrderTracker):
+        self._active_order_tracker = active_order_tracker
+        super(BitroyalOrderBookTrackerEntry, self).__init__(symbol, timestamp, order_book)
+
+    def __repr__(self) -> str:
+        return f"BitroyalOrderBookTrackerEntry(symbol='{self._symbol}', timestamp='{self._timestamp}', " \
+            f"order_book='{self._order_book}')"
+
+    @property
+    def active_order_tracker(self) -> BitroyalActiveOrderTracker:
+        return self._active_order_tracker
--- hummingbot/logger/report_aggregator.py
+++ hummingbot/logger/report_aggregator.py
@@ -10,6 +10,7 @@ from hummingbot.core.utils.exchange_rate_conversion import ExchangeRateConversio
 from hummingbot.market.bamboo_relay.bamboo_relay_market import BambooRelayMarket
 from hummingbot.market.binance.binance_market import BinanceMarket
 from hummingbot.market.coinbase_pro.coinbase_pro_market import CoinbaseProMarket
+from hummingbot.market.bitroyal.bitroyal_market import BitroyalMarket
 from hummingbot.market.ddex.ddex_market import DDEXMarket
 from hummingbot.market.huobi.huobi_market import HuobiMarket
 from hummingbot.market.idex.idex_market import IDEXMarket
@@ -18,6 +19,7 @@ from hummingbot.market.radar_relay.radar_relay_market import RadarRelayMarket
 MARKETS = {
     "ddex": DDEXMarket,
     "coinbase_pro": CoinbaseProMarket,
+    "bitroyal": BitroyalMarket,
     "binance": BinanceMarket,
     "bamboo_relay": BambooRelayMarket,
     "radar_relay": RadarRelayMarket,
