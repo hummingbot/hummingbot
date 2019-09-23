c0ea772b60e0e13da2cc0ba004a8d0e1e7d5697b
svamol <svamol@gmail.com>
2019-09-23 21:59:45 +0530

    added bitroyal to order book and order book tracker, report agregator and conf global template

--- hummingbot/logger/report_aggregator.py
+++ hummingbot/logger/report_aggregator.py
@@ -12,6 +12,7 @@ from hummingbot.market.bamboo_relay.bamboo_relay_market import BambooRelayMarket
 from hummingbot.market.binance.binance_market import BinanceMarket
 from hummingbot.market.bittrex.bittrex_market import BittrexMarket
 from hummingbot.market.coinbase_pro.coinbase_pro_market import CoinbaseProMarket
+from hummingbot.market.bitroyal.bitroyal_market import BitroyalMarket
 from hummingbot.market.ddex.ddex_market import DDEXMarket
 from hummingbot.market.huobi.huobi_market import HuobiMarket
 from hummingbot.market.idex.idex_market import IDEXMarket
@@ -21,6 +22,7 @@ from hummingbot.market.dolomite.dolomite_market import DolomiteMarket
 MARKETS = {
     "ddex": DDEXMarket,
     "coinbase_pro": CoinbaseProMarket,
+    "bitroyal": BitroyalMarket,
     "binance": BinanceMarket,
     "bamboo_relay": BambooRelayMarket,
     "radar_relay": RadarRelayMarket,
