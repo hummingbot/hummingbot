15e956dd6c2ef7ca4ebefd513de873ca925c6c0a
Ryan Chow <chowryan@gmail.com>
2019-10-01 17:24:18 -0700

    grammar

--- hummingbot/market/huobi/huobi_order_book_tracker.py
+++ hummingbot/market/huobi/huobi_order_book_tracker.py
@@ -134,7 +134,7 @@ class HuobiOrderBookTracker(OrderBookTracker):
             try:
                 message: OrderBookMessage = await message_queue.get()
                 if message.type is OrderBookMessageType.DIFF:
-                    # Huobi websocket messages return the entire order book state so they should be treated as snapshots
+                    # Huobi websocket messages contain the entire order book state so they should be treated as snapshots
                     order_book.apply_snapshot(message.bids, message.asks, message.update_id)
                     diff_messages_accepted += 1
 
