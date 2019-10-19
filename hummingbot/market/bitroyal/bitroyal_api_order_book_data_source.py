bf6cdb78786be5caf62dd1be2cc1cbdb3518612c
bitroyalamol <amol.shirke@bitroyalexchange.com>
2019-10-19 14:37:20 +0000

    hummingbot/market/bitroyal/bitroyal_api_order_book_data_source.py

--- hummingbot/market/bitroyal/bitroyal_api_order_book_data_source.py
+++ hummingbot/market/bitroyal/bitroyal_api_order_book_data_source.py
@@ -4,7 +4,13 @@ import asyncio
 import aiohttp
 import logging
 import pandas as pd
-from typing import Any, AsyncIterable, Dict, List, Optional
+from typing import (
+    Any,
+    AsyncIterable,
+    Dict,
+    List,
+    Optional,
+)
 import time
 import ujson
 import websockets
@@ -14,7 +20,10 @@ from hummingbot.market.bitroyal.bitroyal_order_book import BitroyalOrderBook
 from hummingbot.core.data_type.order_book_tracker_data_source import OrderBookTrackerDataSource
 from hummingbot.core.utils import async_ttl_cache
 from hummingbot.logger import HummingbotLogger
-from hummingbot.core.data_type.order_book_tracker_entry import BitroyalOrderBookTrackerEntry, OrderBookTrackerEntry
+from hummingbot.core.data_type.order_book_tracker_entry import (
+    BitroyalOrderBookTrackerEntry,
+    OrderBookTrackerEntry
+)
 from hummingbot.core.data_type.order_book_message import OrderBookMessage
 from hummingbot.market.bitroyal.bitroyal_active_order_tracker import BitroyalActiveOrderTracker
 
@@ -54,9 +63,8 @@ class BitroyalAPIOrderBookDataSource(OrderBookTrackerDataSource):
                     raise IOError(f"Error fetching active Bitroyal markets. HTTP status is {products_response.status}.")
                 data = await products_response.json()
                 all_markets: pd.DataFrame = pd.DataFrame.from_records(data=data, index="id")
-                all_markets.rename(
-                    {"base_currency": "baseAsset", "quote_currency": "quoteAsset"}, axis="columns", inplace=True
-                )
+                all_markets.rename({"base_currency": "baseAsset", "quote_currency": "quoteAsset"},
+                                   axis="columns", inplace=True)
                 ids: List[str] = list(all_markets.index)
                 volumes: List[float] = []
                 prices: List[float] = []
@@ -74,10 +82,8 @@ class BitroyalAPIOrderBookDataSource(OrderBookTrackerDataSource):
                                 volumes.append(float(data.get("volume", NaN)))
                                 prices.append(float(data.get("price", NaN)))
                             elif ticker_response.status != 429 or retry_counter == MAX_RETRIES:
-                                raise IOError(
-                                    f"Error fetching ticker for {product_id} on Bitroyal. "
-                                    f"HTTP status is {ticker_response.status}."
-                                )
+                                raise IOError(f"Error fetching ticker for {product_id} on Bitroyal. "
+                                              f"HTTP status is {ticker_response.status}.")
                             await asyncio.sleep(0.5)
                 all_markets["volume"] = volumes
                 all_markets["price"] = prices
@@ -120,7 +126,7 @@ class BitroyalAPIOrderBookDataSource(OrderBookTrackerDataSource):
                 self.logger().network(
                     f"Error getting active exchange information.",
                     exc_info=True,
-                    app_warning_msg=f"Error getting active exchange information. Check network connection.",
+                    app_warning_msg=f"Error getting active exchange information. Check network connection."
                 )
         return self._symbols
 
@@ -130,9 +136,8 @@ class BitroyalAPIOrderBookDataSource(OrderBookTrackerDataSource):
         async with client.get(product_order_book_url) as response:
             response: aiohttp.ClientResponse = response
             if response.status != 200:
-                raise IOError(
-                    f"Error fetching Bitroyal market snapshot for {trading_pair}. " f"HTTP status is {response.status}."
-                )
+                raise IOError(f"Error fetching Bitroyal market snapshot for {trading_pair}. "
+                              f"HTTP status is {response.status}.")
             data: Dict[str, Any] = await response.json()
             return data
 
@@ -148,7 +153,9 @@ class BitroyalAPIOrderBookDataSource(OrderBookTrackerDataSource):
                     snapshot: Dict[str, any] = await self.get_snapshot(client, trading_pair)
                     snapshot_timestamp: float = time.time()
                     snapshot_msg: OrderBookMessage = self.order_book_class.snapshot_message_from_exchange(
-                        snapshot, snapshot_timestamp, metadata={"symbol": trading_pair}
+                        snapshot,
+                        snapshot_timestamp,
+                        metadata={"symbol": trading_pair}
                     )
                     order_book: BitroyalOrderBook = BitroyalOrderBook()
                     active_order_tracker: BitroyalActiveOrderTracker = BitroyalActiveOrderTracker()
@@ -156,23 +163,26 @@ class BitroyalAPIOrderBookDataSource(OrderBookTrackerDataSource):
                     order_book.apply_snapshot(bids, asks, snapshot_msg.update_id)
 
                     retval[trading_pair] = BitroyalOrderBookTrackerEntry(
-                        trading_pair, snapshot_timestamp, order_book, active_order_tracker
+                        trading_pair,
+                        snapshot_timestamp,
+                        order_book,
+                        active_order_tracker
                     )
-                    self.logger().info(
-                        f"Initialized order book for {trading_pair}. " f"{index+1}/{number_of_pairs} completed."
-                    )
+                    self.logger().info(f"Initialized order book for {trading_pair}. "
+                                       f"{index+1}/{number_of_pairs} completed.")
                     await asyncio.sleep(0.6)
                 except IOError:
                     self.logger().network(
                         f"Error getting snapshot for {trading_pair}.",
                         exc_info=True,
-                        app_warning_msg=f"Error getting snapshot for {trading_pair}. Check network connection.",
+                        app_warning_msg=f"Error getting snapshot for {trading_pair}. Check network connection."
                     )
                 except Exception:
                     self.logger().error(f"Error initializing order book for {trading_pair}. ", exc_info=True)
             return retval
 
-    async def _inner_messages(self, ws: websockets.WebSocketClientProtocol) -> AsyncIterable[str]:
+    async def _inner_messages(self,
+                              ws: websockets.WebSocketClientProtocol) -> AsyncIterable[str]:
         # Terminate the recv() loop as soon as the next message timed out, so the outer loop can reconnect.
         try:
             while True:
@@ -202,7 +212,7 @@ class BitroyalAPIOrderBookDataSource(OrderBookTrackerDataSource):
                     subscribe_request: Dict[str, Any] = {
                         "type": "subscribe",
                         "product_ids": trading_pairs,
-                        "channels": ["full"],
+                        "channels": ["full"]
                     }
                     await ws.send(ujson.dumps(subscribe_request))
                     async for raw_msg in self._inner_messages(ws):
@@ -230,7 +240,7 @@ class BitroyalAPIOrderBookDataSource(OrderBookTrackerDataSource):
                     f"Unexpected error with WebSocket connection.",
                     exc_info=True,
                     app_warning_msg=f"Unexpected error with WebSocket connection. Retrying in 30 seconds. "
-                    f"Check network connection.",
+                                    f"Check network connection."
                 )
                 await asyncio.sleep(30.0)
 
@@ -244,7 +254,9 @@ class BitroyalAPIOrderBookDataSource(OrderBookTrackerDataSource):
                             snapshot: Dict[str, any] = await self.get_snapshot(client, trading_pair)
                             snapshot_timestamp: float = time.time()
                             snapshot_msg: OrderBookMessage = self.order_book_class.snapshot_message_from_exchange(
-                                snapshot, snapshot_timestamp, metadata={"product_id": trading_pair}
+                                snapshot,
+                                snapshot_timestamp,
+                                metadata={"product_id": trading_pair}
                             )
                             output.put_nowait(snapshot_msg)
                             self.logger().debug(f"Saved order book snapshot for {trading_pair}")
@@ -257,7 +269,7 @@ class BitroyalAPIOrderBookDataSource(OrderBookTrackerDataSource):
                                 f"Unexpected error with WebSocket connection.",
                                 exc_info=True,
                                 app_warning_msg=f"Unexpected error with WebSocket connection. Retrying in 5 seconds. "
-                                f"Check network connection.",
+                                                f"Check network connection."
                             )
                             await asyncio.sleep(5.0)
                     this_hour: pd.Timestamp = pd.Timestamp.utcnow().replace(minute=0, second=0, microsecond=0)
