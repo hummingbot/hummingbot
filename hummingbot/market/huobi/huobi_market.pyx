934de29a54e543fa4e1122a53902ea1a8efc0dc7
Ryan Chow <chowryan@gmail.com>
2019-10-24 11:35:02 -0700

    fix huobi order state error handling

--- hummingbot/market/huobi/huobi_market.pyx
+++ hummingbot/market/huobi/huobi_market.pyx
@@ -66,6 +66,12 @@ SYMBOL_SPLITTER = re.compile(r"^(\w+)(usdt|husd|btc|eth|ht|trx)$")
 HUOBI_ROOT_API = "https://api.huobi.pro/v1/"
 
 
+class HuobiAPIError(IOError):
+    def __init__(self, error_payload: Dict[str, Any]):
+        super().__init__()
+        self.error_payload = error_payload
+
+
 cdef class HuobiMarketTransactionTracker(TransactionTracker):
     cdef:
         HuobiMarket _owner
@@ -290,8 +296,8 @@ cdef class HuobiMarket(MarketBase):
 
             data = parsed_response.get("data")
             if data is None:
-                self.logger().debug(f"Error received from {url}. Response is {parsed_response}.")
-                return {"error": parsed_response}
+                self.logger().error(f"Error received from {url}. Response is {parsed_response}.")
+                raise HuobiAPIError({"error": parsed_response})
             return data
 
     async def _update_account_id(self) -> str:
@@ -412,7 +418,23 @@ cdef class HuobiMarket(MarketBase):
             tracked_orders = list(self._in_flight_orders.values())
             for tracked_order in tracked_orders:
                 exchange_order_id = await tracked_order.get_exchange_order_id()
-                order_update = await self.get_order_status(exchange_order_id)
+                try:
+                    order_update = await self.get_order_status(exchange_order_id)
+                except HuobiAPIError as e:
+                    err_code = e.error_payload.get("error").get("err-code")
+                    self.c_stop_tracking_order(tracked_order.client_order_id)
+                    self.logger().info(f"The limit order {tracked_order.client_order_id} "
+                                       f"has failed according to order status API. - {err_code}")
+                    self.c_trigger_event(
+                        self.MARKET_ORDER_FAILURE_EVENT_TAG,
+                        MarketOrderFailureEvent(
+                            self._current_timestamp,
+                            tracked_order.client_order_id,
+                            tracked_order.order_type
+                        )
+                    )
+                    continue
+
                 if order_update is None:
                     self.logger().network(
                         f"Error fetching status update for the order {tracked_order.client_order_id}: "
@@ -569,17 +591,13 @@ cdef class HuobiMarket(MarketBase):
         }
         if order_type is OrderType.LIMIT:
             params["price"] = str(price)
-        try:
-            exchange_order_id = await self._api_request(
-                "post",
-                path_url=path_url,
-                params=params,
-                data=params,
-                is_auth_required=True
-            )
-        except Exception:
-            raise
-
+        exchange_order_id = await self._api_request(
+            "post",
+            path_url=path_url,
+            params=params,
+            data=params,
+            is_auth_required=True
+        )
         return str(exchange_order_id)
 
     async def execute_buy(self,
@@ -734,18 +752,26 @@ cdef class HuobiMarket(MarketBase):
             if tracked_order is None:
                 raise ValueError(f"Failed to cancel order - {order_id}. Order not found.")
             path_url = f"order/orders/{tracked_order.exchange_order_id}/submitcancel"
-
             response = await self._api_request("post", path_url=path_url, is_auth_required=True)
-            if response.get("error") is not None:
-                order_state = response.get("error").get("order-state")
-                if order_state == 7:
-                    # order-state is canceled
-                    self.c_stop_tracking_order(tracked_order.client_order_id)
-                    self.logger().info(f"The order {tracked_order.client_order_id} has been cancelled according"
-                                       f" to order status API. order_state - {order_state}")
-                    self.c_trigger_event(self.MARKET_ORDER_CANCELLED_EVENT_TAG,
-                                         OrderCancelledEvent(self._current_timestamp,
-                                                             tracked_order.client_order_id))
+
+        except HuobiAPIError as e:
+            order_state = e.error_payload.get("error").get("order-state")
+            if order_state == 7:
+                # order-state is canceled
+                self.c_stop_tracking_order(tracked_order.client_order_id)
+                self.logger().info(f"The order {tracked_order.client_order_id} has been cancelled according"
+                                   f" to order status API. order_state - {order_state}")
+                self.c_trigger_event(self.MARKET_ORDER_CANCELLED_EVENT_TAG,
+                                     OrderCancelledEvent(self._current_timestamp,
+                                                         tracked_order.client_order_id))
+            else:
+                self.logger().network(
+                    f"Failed to cancel order {order_id}: {str(e)}",
+                    exc_info=True,
+                    app_warning_msg=f"Failed to cancel the order {order_id} on Huobi. "
+                                    f"Check API key and network connection."
+                )
+
         except Exception as e:
             self.logger().network(
                 f"Failed to cancel order {order_id}: {str(e)}",
