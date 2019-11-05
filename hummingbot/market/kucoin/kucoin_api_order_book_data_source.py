26bd4460b1e539ab7afef1afb802468f55ce6f8d
vic-en <31972210+vic-en@users.noreply.github.com>
2019-11-05 10:47:29 +0100

    Update Datasource and corrected some indents

--- hummingbot/market/kucoin/kucoin_api_order_book_data_source.py
+++ hummingbot/market/kucoin/kucoin_api_order_book_data_source.py
@@ -1,34 +1,38 @@
 #!/usr/bin/env python
 
-import aiohttp
 import asyncio
 import json
+import aiohttp
 import logging
 import pandas as pd
-import time
 from typing import (
     Any,
     AsyncIterable,
     Dict,
     List,
-    Optional,
+    Optional
 )
+import re
+import time
+import ujson
 import websockets
 from websockets.exceptions import ConnectionClosed
 
-from hummingbot.core.data_type.order_book import OrderBook
-from hummingbot.core.data_type.order_book_message import OrderBookMessage
-from hummingbot.core.data_type.order_book_tracker_data_source import OrderBookTrackerDataSource
-from hummingbot.core.data_type.order_book_tracker_entry import OrderBookTrackerEntry
 from hummingbot.core.utils import async_ttl_cache
 from hummingbot.core.utils.async_utils import safe_gather
+from hummingbot.core.data_type.order_book_tracker_data_source import OrderBookTrackerDataSource
+from hummingbot.core.data_type.order_book_tracker_entry import OrderBookTrackerEntry
+from hummingbot.core.data_type.order_book_message import OrderBookMessage
+from hummingbot.core.data_type.order_book import OrderBook
 from hummingbot.logger import HummingbotLogger
 from hummingbot.market.kucoin.kucoin_order_book import KucoinOrderBook
-from urllib.parse import urlencode
 
-KUCOIN_SYMBOLS_URL = "https://api.kucoin.com/api/v1/symbols"
-KUCOIN_TICKER_URL = "https://api.kucoin.com/api/v1/market/allTickers"
-KUCOIN_DEPTH_URL = "https://api.kucoin.com/api/v1/market/orderbook/level2_100"
+TRADING_PAIR_FILTER = re.compile(r"(BTC|ETH|USDT)$")
+
+SNAPSHOT_REST_URL = "https://api.kucoin.com/api/v1/market/orderbook/level2_100"
+DIFF_STREAM_URL = ""
+TICKER_PRICE_CHANGE_URL = "https://api.kucoin.com/api/v1/market/allTickers"
+EXCHANGE_INFO_URL = "https://api.kucoin.com/api/v1/symbols"
 
 
 class KucoinAPIOrderBookDataSource(OrderBookTrackerDataSource):
@@ -36,17 +40,18 @@ class KucoinAPIOrderBookDataSource(OrderBookTrackerDataSource):
     MESSAGE_TIMEOUT = 30.0
     PING_TIMEOUT = 10.0
 
-    _kuaobds_logger: Optional[HummingbotLogger] = None
+    _kaobds_logger: Optional[HummingbotLogger] = None
 
     @classmethod
     def logger(cls) -> HummingbotLogger:
-        if cls._kuaobds_logger is None:
-            cls._kuaobds_logger = logging.getLogger(__name__)
-        return cls._kuaobds_logger
+        if cls._kaobds_logger is None:
+            cls._kaobds_logger = logging.getLogger(__name__)
+        return cls._kaobds_logger
 
     def __init__(self, symbols: Optional[List[str]] = None):
         super().__init__()
         self._symbols: Optional[List[str]] = symbols
+        self._order_book_create_function = lambda: OrderBook()
 
     @classmethod
     @async_ttl_cache(ttl=60 * 30, maxsize=1)
@@ -57,8 +62,8 @@ class KucoinAPIOrderBookDataSource(OrderBookTrackerDataSource):
         async with aiohttp.ClientSession() as client:
 
             market_response, exchange_response = await safe_gather(
-                client.get(KUCOIN_TICKER_URL),
-                client.get(KUCOIN_SYMBOLS_URL)
+                client.get(TICKER_PRICE_CHANGE_URL),
+                client.get(EXCHANGE_INFO_URL)
             )
             market_response: aiohttp.ClientResponse = market_response
             exchange_response: aiohttp.ClientResponse = exchange_response
@@ -71,43 +76,39 @@ class KucoinAPIOrderBookDataSource(OrderBookTrackerDataSource):
                               f"HTTP status is {exchange_response.status}.")
 
             market_data = await market_response.json()
-            market_data_copy = market_data
             exchange_data = await exchange_response.json()
-
+            
             attr_name_map = {"baseCurrency": "baseAsset", "quoteCurrency": "quoteAsset"}
 
-            trading_pairs: Dict[str, Any] = {
-                item["symbol"]: {attr_name_map[k]: item[k] for k in ["baseCurrency", "quoteCurrency"]}
-                for item in exchange_data["data"]
-                if item["enableTrading"] == true
-            }
+            trading_pairs: Dict[str, Any] = {item["symbol"]: {attr_name_map[k]: item[k] for k in ["baseAsset", "quoteAsset"]}
+                                             for item in exchange_data["data"]
+                                             if item["enableTrading"] == True}
 
-            market_data: List[Dict[str, Any]] = [
-                {**item, **trading_pairs[item["symbol"]]}
-                for item in market_data["data"]["ticker"]
-                if item["symbol"] in trading_pairs
-            ]
+            market_data: List[Dict[str, Any]] = [{**item, **trading_pairs[item["symbol"]]}
+                                                 for item in market_data["data"]["ticker"]
+                                                 if item["symbol"] in trading_pairs]
 
             # Build the data frame.
             all_markets: pd.DataFrame = pd.DataFrame.from_records(data=market_data, index="symbol")
             # calculating the correct USDVolume by multiplying or diving with necessary USD pair
-            split_symbol = all_markets.split('-')
-            if split_symbol[0] == "USDT":
+            # split_symbol = all_markets.split('-')
+            if all_markets.symbol.startswith("USDT"):
                 all_markets.loc[:, "USDVolume"] = 1 / all_markets.volvalue
-            elif split_symbol[1] == "USDT":
+            elif all_markets.symbol.endswith("USDT"):
                 all_markets.loc[:, "USDVolume"] = all_markets.volvalue
             else:
-                for item2 in market_data_copy["data"]["ticker"]:
-                    split_symbol2 = item2["symbol"].split('-')
-                    if split_symbol[1] == split_symbol2[0] and split_symbol2[1] == "USDT":
-		    # Note: USDvolume is only calculated if a usdt pair with the quote currency exists
-                        all_markets.loc[:, "USDVolume"] = all_markets.volvalue * item2["volValue"]
-                    elif split_symbol[1] == split_symbol2[1] and split_symbol2[0] == "USDT":
-                        all_markets.loc[:, "USDVolume"] = all_markets.volvalue / item2["volValue"]
-                    all_markets.loc[:, "volume"] = all_markets.vol
-
+                try:
+                    quote_currency: str = symbol.split('-')[1]
+                    mul: str = quote_currency + "-USDT"
+                    div: str = "USDT-" + quote_currency
+                    if all_markets.loc[mul]:
+                        all_markets.loc[:, "USDVolume"] = all_markets.volvalue * all_markets.loc[mul].last
+                    else:
+                        all_markets.loc[:, "USDVolume"] = all_markets.volvalue / all_markets.loc[div].last
+                except Exception:
+                    self.logger().error(f"Unable to get USD Volume for {trading_pair}. ", exc_info=True)
+            all_markets.loc[:, "volume"] = all_markets.quoteVolume
             return all_markets.sort_values("USDVolume", ascending=False)
-		
 
     async def get_trading_pairs(self) -> List[str]:
         if not self._symbols:
@@ -125,9 +126,8 @@ class KucoinAPIOrderBookDataSource(OrderBookTrackerDataSource):
 
     @staticmethod
     async def get_snapshot(client: aiohttp.ClientSession, trading_pair: str) -> Dict[str, Any]:
-	# urlencode(params) is not proper as it will give  '%3Fsymbol%3DBTC-USDT' which is not a proper GET argument 
-        params: str = "?symbol=" + urllib.parse.quote(trading_pair)
-        async with client.get(KUCOIN_DEPTH_URL + params) as response:
+        params: Dict = {"symbol": trading_pair}
+        async with client.get(SNAPSHOT_REST_URL, params=params) as response:
             response: aiohttp.ClientResponse = response
             if response.status != 200:
                 raise IOError(f"Error fetching Kucoin market snapshot for {trading_pair}. "
@@ -145,20 +145,22 @@ class KucoinAPIOrderBookDataSource(OrderBookTrackerDataSource):
             for index, trading_pair in enumerate(trading_pairs):
                 try:
                     snapshot: Dict[str, Any] = await self.get_snapshot(client, trading_pair)
+                    snapshot_timestamp: float = time.time()
                     snapshot_msg: OrderBookMessage = KucoinOrderBook.snapshot_message_from_exchange(
                         snapshot,
+                        snapshot_timestamp,
                         metadata={"symbol": trading_pair}
                     )
                     order_book: OrderBook = self.order_book_create_function()
                     order_book.apply_snapshot(snapshot_msg.bids, snapshot_msg.asks, snapshot_msg.update_id)
-                    retval[trading_pair] = OrderBookTrackerEntry(trading_pair, snapshot_msg.timestamp, order_book)
+                    retval[trading_pair] = OrderBookTrackerEntry(trading_pair, snapshot_timestamp, order_book)
                     self.logger().info(f"Initialized order book for {trading_pair}. "
-                                       f"{index + 1}/{number_of_pairs} completed.")
+                                       f"{index+1}/{number_of_pairs} completed.")
                     # Kucoin rate limit is 100 https requests per 10 seconds
                     await asyncio.sleep(0.4)
                 except Exception:
                     self.logger().error(f"Error getting snapshot for {trading_pair}. ", exc_info=True)
-                    await asyncio.sleep(10)
+                    await asyncio.sleep(5)
             return retval
 
     async def _inner_messages(self,
@@ -184,16 +186,15 @@ class KucoinAPIOrderBookDataSource(OrderBookTrackerDataSource):
             await ws.close()
 			
 	#get required data to create a websocket request
-        async def ws_connect_data(self):
-            async with aiohttp.ClientSession() as session:
-                    async with session.post('https://api.kucoin.com/api/v1/bullet-public', data=b'') as resp:
-                        response: aiohttp.ClientResponse = resp
-                        if response.status != 200:
-                            raise IOError(f"Error fetching Kucoin websocket connection data."
-						  f"HTTP status is {response.status}.")
-                        data: Dict[str, Any] = await response.json()
-                        return data
-			
+    async def ws_connect_data(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.post('https://api.kucoin.com/api/v1/bullet-public', data=b'') as resp:
+                response: aiohttp.ClientResponse = resp
+                if response.status != 200:
+                    raise IOError(f"Error fetching Kucoin websocket connection data."
+                            f"HTTP status is {response.status}.")
+                data: Dict[str, Any] = await response.json()
+                return data
 
     async def listen_for_trades(self, ev_loop: asyncio.BaseEventLoop, output: asyncio.Queue):
         websocket_data: Dict[str, Any] = await self.ws_connect_data()
@@ -205,11 +206,11 @@ class KucoinAPIOrderBookDataSource(OrderBookTrackerDataSource):
                     ws: websockets.WebSocketClientProtocol = ws
                     for trading_pair in trading_pairs:
                         subscribe_request: Dict[str, Any] = {
-							"id": int(time.time()),                          
-							"type": "subscribe",
-							"topic": f"/market/match:{trading_pair}",
-							"privateChannel": false,                      
-							"response": true
+                                "id": int(time.time()),                          
+				"type": "subscribe",
+				"topic": f"/market/match:{trading_pair}",
+				"privateChannel": False,                      
+				"response": True
                         }
                         await ws.send(json.dumps(subscribe_request))
 
@@ -219,11 +220,11 @@ class KucoinAPIOrderBookDataSource(OrderBookTrackerDataSource):
                             pass
                         elif msg["type"] == "message":
                             trading_pair = msg["data"]["symbol"]
-                            for data in msg["data"]:
-                                trade_message: OrderBookMessage = KucoinOrderBook.trade_message_from_exchange(
+                            data = msg["data"]
+                            trade_message: OrderBookMessage = KucoinOrderBook.trade_message_from_exchange(
                                     data, metadata={"trading_pair": trading_pair}
-                                )
-                                output.put_nowait(trade_message)
+                            )
+                            output.put_nowait(trade_message)
                         else:
                             self.logger().debug(f"Unrecognized message received from Kucoin websocket: {msg}")
             except asyncio.CancelledError:
@@ -243,10 +244,10 @@ class KucoinAPIOrderBookDataSource(OrderBookTrackerDataSource):
                     ws: websockets.WebSocketClientProtocol = ws
                     for trading_pair in trading_pairs:
                         subscribe_request: Dict[str, Any] = {
-							"id": int(time.time()),                          
-							"type": "subscribe",
-							"topic": f"/market/level2:{trading_pair}",
-							"response": true
+                                "id": int(time.time()),                          
+				"type": "subscribe",
+				"topic": f"/market/level2:{trading_pair}",
+				"response": True
                         }
                         await ws.send(json.dumps(subscribe_request))
 
@@ -274,11 +275,13 @@ class KucoinAPIOrderBookDataSource(OrderBookTrackerDataSource):
                     for trading_pair in trading_pairs:
                         try:
                             snapshot: Dict[str, Any] = await self.get_snapshot(client, trading_pair)
-                            snapshot_message: OrderBookMessage = KucoinOrderBook.snapshot_message_from_exchange(
+                            snapshot_timestamp: float = time.time()
+                            snapshot_msg: OrderBookMessage = KucoinOrderBook.snapshot_message_from_exchange(
                                 snapshot,
+                                snapshot_timestamp,
                                 metadata={"symbol": trading_pair}
                             )
-                            output.put_nowait(snapshot_message)
+                            output.put_nowait(snapshot_msg)
                             self.logger().debug(f"Saved order book snapshot for {trading_pair}")
                             await asyncio.sleep(5.0)
                         except asyncio.CancelledError:
@@ -295,4 +298,3 @@ class KucoinAPIOrderBookDataSource(OrderBookTrackerDataSource):
             except Exception:
                 self.logger().error("Unexpected error.", exc_info=True)
                 await asyncio.sleep(5.0)
-
