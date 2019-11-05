d3ceccce290d8e32de5c8404c37b60af34057203
vic-en <31972210+vic-en@users.noreply.github.com>
2019-11-05 10:50:37 +0100

    Change timestamps to handle long Kucoin timestamp

--- hummingbot/market/kucoin/kucoin_order_book.pyx
+++ hummingbot/market/kucoin/kucoin_order_book.pyx
@@ -1,82 +1,60 @@
 #!/usr/bin/env python
-from decimal import Decimal
-
-from aiokafka import ConsumerRecord
-import bz2
 import logging
-from sqlalchemy.engine import RowProxy
 from typing import (
-    Any,
-    Optional,
-    Dict
+    Dict,
+    Optional
 )
 import ujson
+
+from aiokafka import ConsumerRecord
+from sqlalchemy.engine import RowProxy
 
 from hummingbot.logger import HummingbotLogger
 from hummingbot.core.event.events import TradeType
 from hummingbot.core.data_type.order_book cimport OrderBook
-from hummingbot.core.data_type.order_book_message import OrderBookMessage, OrderBookMessageType
+from hummingbot.core.data_type.order_book_message import (
+    OrderBookMessage,
+    OrderBookMessageType
+)
 
-_kuob_logger = None
+_kob_logger = None
 
 
 cdef class KucoinOrderBook(OrderBook):
     @classmethod
     def logger(cls) -> HummingbotLogger:
-        global _kuob_logger
-        if _kuob_logger is None:
-            _kuob_logger = logging.getLogger(__name__)
-        return _kuob_logger
+        global _kob_logger
+        if _kob_logger is None:
+            _kob_logger = logging.getLogger(__name__)
+        return _kob_logger
 
     @classmethod
     def snapshot_message_from_exchange(cls,
-                                       msg: Dict[str, Any],
-                                       timestamp: Optional[float] = None,
+                                       msg: Dict[str, any],
+                                       timestamp: float,
                                        metadata: Optional[Dict] = None) -> OrderBookMessage:
         if metadata:
             msg.update(metadata)
-        msg_ts = int(msg["data"]["time"] * 1e-3)
-        content = {
+        return OrderBookMessage(OrderBookMessageType.SNAPSHOT, {
             "symbol": msg["symbol"],
-            "update_id": msg_ts,
+            "update_id": int(msg["data"]["sequence"]),
             "bids": msg["data"]["bids"],
             "asks": msg["data"]["asks"]
-        }
-        return OrderBookMessage(OrderBookMessageType.SNAPSHOT, content, timestamp or msg_ts)
-
-    @classmethod
-    def trade_message_from_exchange(cls,
-                                   msg: Dict[str, Any],
-                                   timestamp: Optional[float] = None,
-                                   metadata: Optional[Dict] = None) -> OrderBookMessage:
-        if metadata:
-            msg.update(metadata)
-        msg_ts = msg["time"],
-        content = {
-            "symbol": msg["trading_pair"],
-            "trade_type": float(TradeType.SELL.value) if msg["side"] == "buy" else float(TradeType.BUY.value),
-            "trade_id": msg["tradeId"],
-            "update_id": msg_ts,
-            "amount": msg["size"],
-            "price": msg["price"]
-        }
-        return OrderBookMessage(OrderBookMessageType.DIFF, content, timestamp or msg_ts)
+        }, timestamp=timestamp)
 
     @classmethod
     def diff_message_from_exchange(cls,
-                                   msg: Dict[str, Any],
+                                   msg: Dict[str, any],
                                    timestamp: Optional[float] = None,
                                    metadata: Optional[Dict] = None) -> OrderBookMessage:
         if metadata:
             msg.update(metadata)
-        msg_ts = int(msg["ts"] * 1e-3)
-        content = {
+        return OrderBookMessage(OrderBookMessageType.DIFF, {
             "symbol": msg["data"]["symbol"],
-            "update_id": msg_ts,
+            "update_id": msg["data"]["sequenceEnd"],
             "bids": msg["data"]["changes"]["bids"],
             "asks": msg["data"]["changes"]["asks"]
-        }
-        return OrderBookMessage(OrderBookMessageType.DIFF, content, timestamp or msg_ts)
+        }, timestamp=timestamp)
 
     @classmethod
     def snapshot_message_from_db(cls, record: RowProxy, metadata: Optional[Dict] = None) -> OrderBookMessage:
@@ -84,73 +62,84 @@ cdef class KucoinOrderBook(OrderBook):
         msg = record["json"] if type(record["json"])==dict else ujson.loads(record["json"])
         if metadata:
             msg.update(metadata)
-
         return OrderBookMessage(OrderBookMessageType.SNAPSHOT, {
-			"symbol": msg["symbol"],
-            "update_id": msg_ts,
+            "symbol": msg["symbol"],
+            "update_id": int(ts),
             "bids": msg["data"]["bids"],
             "asks": msg["data"]["asks"]
-        }, timestamp=ts * 1e-3)
+        }, timestamp=record["timestamp"] * 1e-3)
 
     @classmethod
     def diff_message_from_db(cls, record: RowProxy, metadata: Optional[Dict] = None) -> OrderBookMessage:
         ts = record["timestamp"]
-        msg = record["json"] if type(record["json"])==dict else ujson.loads(record["json"])
+        msg = ujson.loads(record["json"]) # Kucoin json in DB is TEXT
         if metadata:
             msg.update(metadata)
         return OrderBookMessage(OrderBookMessageType.DIFF, {
-            "symbol": msg["s"],
-            "update_id": int(ts),
-            "bids": msg["b"],
-            "asks": msg["a"]
-        }, timestamp=ts * 1e-3)
+            "symbol": msg["symbol"],
+            "update_id": ts,
+            "bids": msg["data"]["bids"],
+            "asks": msg["data"]["asks"]
+        }, timestamp=record["timestamp"] * 1e-3)
 
     @classmethod
     def snapshot_message_from_kafka(cls, record: ConsumerRecord, metadata: Optional[Dict] = None) -> OrderBookMessage:
         ts = record.timestamp
-        msg = ujson.loads(record.value.decode())
+        msg = ujson.loads(record.value.decode("utf-8"))
         if metadata:
             msg.update(metadata)
         return OrderBookMessage(OrderBookMessageType.SNAPSHOT, {
-			"symbol": msg["symbol"],
-            "update_id": msg_ts,
+            "symbol": msg["symbol"],
+            "update_id": ts,
             "bids": msg["data"]["bids"],
             "asks": msg["data"]["asks"]
-        }, timestamp=ts * 1e-3)
+        }, timestamp=record.timestamp * 1e-3)
 
     @classmethod
     def diff_message_from_kafka(cls, record: ConsumerRecord, metadata: Optional[Dict] = None) -> OrderBookMessage:
-        decompressed = bz2.decompress(record.value)
-        msg = ujson.loads(decompressed)
-        ts = record.timestamp
+        msg = ujson.loads(record.value.decode("utf-8"))
         if metadata:
             msg.update(metadata)
         return OrderBookMessage(OrderBookMessageType.DIFF, {
-            "symbol": msg["s"],
-            "update_id": ts,
-            "bids": msg["bids"],
-            "asks": msg["asks"]
-        }, timestamp=ts * 1e-3)
+            "symbol": msg["symbol"],
+            "update_id": record.timestamp,
+            "bids": msg["data"]["bids"],
+            "asks": msg["data"]["asks"]
+
+        }, timestamp=record.timestamp * 1e-3)
 
     @classmethod
     def trade_message_from_db(cls, record: RowProxy, metadata: Optional[Dict] = None):
         msg = record["json"]
-        ts = record.timestamp
-        data = msg["data"]
         if metadata:
             msg.update(metadata)
         return OrderBookMessage(OrderBookMessageType.TRADE, {
-            "symbol": msg["symbol"]
-            "trade_type": float(TradeType.BUY.value) if data["side"] == "buy"
+            "symbol": msg["symbol"],
+            "trade_type": float(TradeType.BUY.value) if msg["side"] == "buy"
                             else float(TradeType.SELL.value),
-            "trade_id": ts,
-            "update_id": ts,
-            "price": data["price"],
-            "amount": data["size"]
-        }, timestamp=ts * 1e-3)
+            "trade_id": msg["tradeId"],
+            "update_id": msg["sequence"],
+            "price": msg["price"],
+            "amount": msg["size"]
+        }, timestamp=record.timestamp * 1e-9)
+
+    @classmethod
+    def trade_message_from_exchange(cls, msg: Dict[str, any], metadata: Optional[Dict] = None):
+        if metadata:
+            msg.update(metadata)
+        return OrderBookMessage(OrderBookMessageType.TRADE, {
+            "symbol": msg["symbol"],
+            "trade_type": float(TradeType.BUY.value) if msg["side"] == "buy"
+                            else float(TradeType.SELL.value),
+            "trade_id": msg["tradeId"],
+            "update_id": msg["sequence"],
+            "price": msg["price"],
+            "amount": msg["size"]
+        }, timestamp=(int(msg["time"]) * 1e-9))
 
     @classmethod
     def from_snapshot(cls, msg: OrderBookMessage) -> "OrderBook":
         retval = KucoinOrderBook()
         retval.apply_snapshot(msg.bids, msg.asks, msg.update_id)
         return retval
+
--- hummingbot/market/kucoin/kucoin_order_book_tracker.py
+++ hummingbot/market/kucoin/kucoin_order_book_tracker.py
@@ -1,59 +1,64 @@
 #!/usr/bin/env python
 
 import asyncio
+from collections import deque, defaultdict
 import logging
 import time
 from typing import (
+    Deque,
+    Dict,
     List,
     Optional
 )
 
-from hummingbot.core.data_type.order_book import OrderBook
-from hummingbot.core.data_type.order_book_message import (
-    OrderBookMessage,
-    OrderBookMessageType
-)
+from hummingbot.logger import HummingbotLogger
 from hummingbot.core.data_type.order_book_tracker import (
     OrderBookTracker,
-    OrderBookTrackerDataSourceType
-)
+    OrderBookTrackerDataSourceType)
 from hummingbot.core.data_type.order_book_tracker_data_source import OrderBookTrackerDataSource
+from hummingbot.core.data_type.remote_api_order_book_data_source import RemoteAPIOrderBookDataSource
 from hummingbot.core.utils.async_utils import safe_ensure_future
-from hummingbot.logger import HummingbotLogger
 from hummingbot.market.kucoin.kucoin_api_order_book_data_source import KucoinAPIOrderBookDataSource
+from hummingbot.core.data_type.order_book import OrderBook
+from hummingbot.core.data_type.order_book_message import OrderBookMessage, OrderBookMessageType
 
 
 class KucoinOrderBookTracker(OrderBookTracker):
-    _kuobt_logger: Optional[HummingbotLogger] = None
+    _kobt_logger: Optional[HummingbotLogger] = None
 
     @classmethod
     def logger(cls) -> HummingbotLogger:
-        if cls._kuobt_logger is None:
-            cls._kuobt_logger = logging.getLogger(__name__)
-        return cls._kuobt_logger
+        if cls._kobt_logger is None:
+            cls._kobt_logger = logging.getLogger(__name__)
+        return cls._kobt_logger
 
     def __init__(self,
                  data_source_type: OrderBookTrackerDataSourceType = OrderBookTrackerDataSourceType.EXCHANGE_API,
                  symbols: Optional[List[str]] = None):
         super().__init__(data_source_type=data_source_type)
+
         self._order_book_diff_stream: asyncio.Queue = asyncio.Queue()
         self._order_book_snapshot_stream: asyncio.Queue = asyncio.Queue()
+
         self._ev_loop: asyncio.BaseEventLoop = asyncio.get_event_loop()
         self._data_source: Optional[OrderBookTrackerDataSource] = None
+        self._saved_message_queues: Dict[str, Deque[OrderBookMessage]] = defaultdict(lambda: deque(maxlen=1000))
         self._symbols: Optional[List[str]] = symbols
-
-    @property
-    def exchange_name(self) -> str:
-        return "kucoin"
 
     @property
     def data_source(self) -> OrderBookTrackerDataSource:
         if not self._data_source:
-            if self._data_source_type is OrderBookTrackerDataSourceType.EXCHANGE_API:
+            if self._data_source_type is OrderBookTrackerDataSourceType.REMOTE_API:
+                self._data_source = RemoteAPIOrderBookDataSource()
+            elif self._data_source_type is OrderBookTrackerDataSourceType.EXCHANGE_API:
                 self._data_source = KucoinAPIOrderBookDataSource(symbols=self._symbols)
             else:
                 raise ValueError(f"data_source_type {self._data_source_type} is not supported.")
         return self._data_source
+
+    @property
+    def exchange_name(self) -> str:
+        return "kucoin"
 
     async def start(self):
         await super().start()
@@ -91,6 +96,9 @@ class KucoinOrderBookTracker(OrderBookTracker):
                 symbol: str = ob_message.symbol
 
                 if symbol not in self._tracking_message_queues:
+                    messages_queued += 1
+                    # Save diff messages received before snapshots are ready
+                    self._saved_message_queues[symbol].append(ob_message)
                     continue
                 message_queue: asyncio.Queue = self._tracking_message_queues[symbol]
                 # Check the order book's initial update ID. If it's larger, don't bother.
@@ -125,6 +133,9 @@ class KucoinOrderBookTracker(OrderBookTracker):
                 await asyncio.sleep(5.0)
 
     async def _track_single_book(self, symbol: str):
+        past_diffs_window: Deque[OrderBookMessage] = deque()
+        self._past_diffs_windows[symbol] = past_diffs_window
+
         message_queue: asyncio.Queue = self._tracking_message_queues[symbol]
         order_book: OrderBook = self._order_books[symbol]
         last_message_timestamp: float = time.time()
@@ -132,10 +143,20 @@ class KucoinOrderBookTracker(OrderBookTracker):
 
         while True:
             try:
-                message: OrderBookMessage = await message_queue.get()
+                message: OrderBookMessage = None
+                saved_messages: Deque[OrderBookMessage] = self._saved_message_queues[symbol]
+
+                # Process saved messages first if there are any
+                if len(saved_messages) > 0:
+                    message = saved_messages.popleft()
+                else:
+                    message = await message_queue.get()
+
                 if message.type is OrderBookMessageType.DIFF:
-                    # Kucoin websocket messages contain the entire order book state so they should be treated as snapshots
-                    order_book.apply_snapshot(message.bids, message.asks, message.update_id)
+                    order_book.apply_diffs(message.bids, message.asks, message.update_id)
+                    past_diffs_window.append(message)
+                    while len(past_diffs_window) > self.PAST_DIFF_WINDOW_SIZE:
+                        past_diffs_window.popleft()
                     diff_messages_accepted += 1
 
                     # Output some statistics periodically.
@@ -146,7 +167,8 @@ class KucoinOrderBookTracker(OrderBookTracker):
                         diff_messages_accepted = 0
                     last_message_timestamp = now
                 elif message.type is OrderBookMessageType.SNAPSHOT:
-                    order_book.apply_snapshot(message.bids, message.asks, message.update_id)
+                    past_diffs: List[OrderBookMessage] = list(past_diffs_window)
+                    order_book.restore_from_snapshot_and_diffs(message, past_diffs)
                     self.logger().debug("Processed order book snapshot for %s.", symbol)
             except asyncio.CancelledError:
                 raise
