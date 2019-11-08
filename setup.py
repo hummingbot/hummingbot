f26f6fb40cd55529d3fd3477f7455a2e49293fc7
Carlo P. Las Marias <carlol@coinalpha.com>
2019-11-08 08:34:50 +0800

    release / hotfix 0.18.1 (1 of 3) (#1046)
    * (docs) update version
    
    * (docs) 0.18.1 release notes

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if os.name == "posix":
 
 def main():
     cpu_count = os.cpu_count() or 8
-    version = "20191104"
+    version = "20191107"
     packages = [
         "hummingbot",
         "hummingbot.client",
