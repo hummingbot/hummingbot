1d4b1c49f97b5b1686cdf60cad7023a24fbc7ea1
vic-en <victoreni14@gmail.com>
2019-11-15 14:28:14 +0000

    Add User Stream Tracker
    Add Task 2: User Stream Tracker

--- hummingbot/market/kucoin/kucoin_auth.py
+++ hummingbot/market/kucoin/kucoin_auth.py
@@ -1,4 +1,5 @@
 import base64
+import json
 import time
 import hashlib
 import hmac
@@ -6,7 +7,6 @@ from typing import (
     Any,
     Dict
 )
-from urllib.parse import urlencode
 from collections import OrderedDict
 
 
@@ -23,19 +23,19 @@ class KucoinAuth:
     def add_auth_to_params(self,
                            method: str,
                            path_url: str,
-                           args: Dict[str, Any]=None) -> Dict[str, Any]:
+                           args: Dict[str, Any] = None) -> Dict[str, Any]:
         timestamp = int(time.time() * 1000)
         request = {
             "KC-API-KEY": self.api_key,
             "KC-API-PASSPHRASE": self.passphrase,
-            "KC-API-TIMESTAMP": str(timestamp)
+            "KC-API-TIMESTAMP": str(timestamp),
+            "Content-Type": "application/json"
         }
         if args is not None:
             query_string = json.dumps(args)
-        sorted_request = self.keysort(request)
-        payload = "\n".join([str(timestamp), method.upper(), "api/v1/" + path_url, query_string])
-        signature = hmac.new(self.secret_key.encode("utf8"), payload.encode("utf8"), hashlib.sha256)
-        signature_b64 = base64.b64encode(signature.digest())
-        sorted_request["KC-API-SIGN"] = signature_b64
-        return sorted_request
-	
+            payload = str(timestamp) + method.upper() + path_url + query_string
+        else:
+            payload = str(timestamp) + method.upper() + path_url
+        signature = base64.b64encode(hmac.new(self.secret_key.encode("utf-8"), payload.encode("utf-8"), hashlib.sha256).digest())
+        request["KC-API-SIGN"] = str(signature, "utf-8")
+        return request
