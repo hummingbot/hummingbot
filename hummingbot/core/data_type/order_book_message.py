10b423033c622272d1e50dcf6c0da90104b06ee6
nionis <stivennoni77@gmail.com>
2019-11-22 13:50:49 +0200

    (feat) implement bitcoin_com DataSource and OrderBookTracker & Tests

--- hummingbot/core/data_type/order_book_message.py
+++ hummingbot/core/data_type/order_book_message.py
@@ -500,3 +500,62 @@ class BittrexOrderBookMessage(OrderBookMessage):
             If timestamp is the same, the ordering is snapshot < diff < trade
             """
             return self.type.value < other.type.value
+
+
+class BitcoinComOrderBookMessage(OrderBookMessage):
+    def __new__(
+        cls,
+        message_type: OrderBookMessageType,
+        content: Dict[str, any],
+        timestamp: Optional[float] = None,
+        *args,
+        **kwargs,
+    ):
+        if timestamp is None:
+            if message_type is OrderBookMessageType.SNAPSHOT:
+                raise ValueError("timestamp must not be None when initializing snapshot messages.")
+            timestamp = content["timestamp"]
+
+        return super(BitcoinComOrderBookMessage, cls).__new__(
+            cls, message_type, content, timestamp=timestamp, *args, **kwargs
+        )
+
+    @property
+    def update_id(self) -> int:
+        return int(self.timestamp * 1e3)
+
+    @property
+    def trade_id(self) -> int:
+        return int(self.timestamp * 1e3)
+
+    @property
+    def symbol(self) -> str:
+        return self.content["symbol"]
+
+    @property
+    def asks(self) -> List[OrderBookRow]:
+        raise self.content["ask"]
+
+    @property
+    def bids(self) -> List[OrderBookRow]:
+        raise self.content["bid"]
+
+    @property
+    def has_update_id(self) -> bool:
+        return True
+
+    @property
+    def has_trade_id(self) -> bool:
+        return True
+
+    def __eq__(self, other) -> bool:
+        return self.type == other.type and self.timestamp == other.timestamp
+
+    def __lt__(self, other) -> bool:
+        if self.timestamp != other.timestamp:
+            return self.timestamp < other.timestamp
+        else:
+            """
+            If timestamp is the same, the ordering is snapshot < diff < trade
+            """
+            return self.type.value < other.type.value
