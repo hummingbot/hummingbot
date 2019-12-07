9ba587e29b3c10c135ada7147c3797091299fbb4
nionis <stivennoni77@gmail.com>
2019-12-07 14:20:53 +0200

    (feat) implement bitcoin_com hummingbot client

--- hummingbot/client/settings.py
+++ hummingbot/client/settings.py
@@ -33,7 +33,8 @@ EXCHANGES = {
     "idex",
     "radar_relay",
     "dolomite",
-    "bittrex"
+    "bittrex",
+    "bitcoin_com"
 }
 
 DEXES = {
@@ -55,7 +56,8 @@ EXAMPLE_PAIRS = {
     "coinbase_pro": "ETH-USDC",
     "dolomite": "WETH-DAI",
     "huobi": "ETH-USDT",
-    "bittrex": "ZRX-ETH"
+    "bittrex": "ZRX-ETH",
+    "bitcoin_com": "ETHBTC"
 }
 
 EXAMPLE_ASSETS = {
@@ -67,7 +69,8 @@ EXAMPLE_ASSETS = {
     "coinbase_pro": "ETH",
     "huobi": "eth",
     "dolomite": "LRC",
-    "bittrex": "ZRX"
+    "bittrex": "ZRX",
+    "bitcoin_com": "ETH"
 }
 
 MAXIMUM_OUTPUT_PANE_LINE_COUNT = 1000
