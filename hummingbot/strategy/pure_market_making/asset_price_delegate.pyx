da7b4aff71bf927748c919114db4e0d67175f8e8
Nullably <jukares@yahoo.com>
2019-12-09 11:33:34 +0800

    (feat) add the interface and try out Cython

---
+++ hummingbot/strategy/pure_market_making/asset_price_delegate.pyx
@@ -0,0 +1,13 @@
+from decimal import Decimal
+from .pure_market_making_v2 import PureMarketMakingStrategyV2
+
+
+cdef class AssetPriceDelegate :
+    # The following exposed Python functions are meant for unit tests
+    # ---------------------------------------------------------------
+    def get_mid_price(self) -> Decimal:
+        return self.c_get_mid_price()
+    # ---------------------------------------------------------------
+
+    cdef object c_get_mid_price(self):
+        raise NotImplementedError
