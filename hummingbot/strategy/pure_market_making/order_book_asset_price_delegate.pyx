54cfe3542a3fd9daa92842840253eb057b77be04
Nullably <jukares@yahoo.com>
2019-12-12 13:54:24 +0800

    (feat) more external price sources

--- hummingbot/strategy/pure_market_making/order_book_asset_price_delegate.pyx
+++ hummingbot/strategy/pure_market_making/order_book_asset_price_delegate.pyx
@@ -1,11 +1,11 @@
 from .asset_price_delegate cimport AssetPriceDelegate
+from hummingbot.market.paper_trade import create_paper_trade_market
+from hummingbot.strategy.market_trading_pair_tuple import MarketTradingPairTuple
 
 cdef class OrderBookAssetPriceDelegate(AssetPriceDelegate):
-    def __init__(self):
+    def __init__(self, market_info: MarketTradingPairTuple):
         super().__init__()
+        self._market_info = market_info
 
     cdef object c_get_mid_price(self):
-        # cdef:
-        #     MarketBase maker_market = market_info.market
-
-        return 0
+        return self._market_info.get_mid_price()
