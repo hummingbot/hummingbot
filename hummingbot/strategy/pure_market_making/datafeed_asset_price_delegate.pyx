ba28ec855195498cfd960b24cf064345b6e11ef7
Nullably <jukares@yahoo.com>
2019-12-13 10:32:50 +0800

    (feat) add ready property to asset price delegate

--- hummingbot/strategy/pure_market_making/datafeed_asset_price_delegate.pyx
+++ hummingbot/strategy/pure_market_making/datafeed_asset_price_delegate.pyx
@@ -10,3 +10,7 @@ cdef class DataFeedAssetPriceDelegate(AssetPriceDelegate):
     cdef object c_get_mid_price(self):
         ex_rate_conversion = ExchangeRateConversion.get_instance()
         return ex_rate_conversion.convert_token_value_decimal(1, self._base_asset, self._quote_asset)
+
+    @property
+    def ready(self) -> bool:
+        return ExchangeRateConversion.get_instance().ready
