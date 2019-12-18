857fc00a2a9f6ee657436ef73fa6ed4f7e749009
Nullably <jukares@yahoo.com>
2019-12-18 21:57:54 +0800

    (feat) change simple web app to run on a process intead of a thread and see if jenkins will like that

--- test/test_custom_api_feed.py
+++ test/test_custom_api_feed.py
@@ -7,6 +7,7 @@ from aiohttp.web_app import Application
 from decimal import Decimal
 from hummingbot.data_feed.custom_api_data_feed import CustomAPIDataFeed
 from hummingbot.core.network_base import NetworkStatus
+from multiprocessing import Process
 
 
 def async_run(func):
@@ -32,7 +33,6 @@ class SimpleWebApp:
         self._app = None
         self.loop = None
         self._to_return_404 = to_return_404
-        self._start()
 
     def set_params(self, response_price, to_return_404=False):
         self._price = response_price
@@ -45,17 +45,18 @@ class SimpleWebApp:
             return web.Response(text=str(self._price))
 
     def start_web_app(self):
-        self.loop = asyncio.new_event_loop()
+        self.loop = asyncio.get_event_loop()
         asyncio.set_event_loop(self.loop)
         self._app = web.Application(loop=self.loop)
         self._app.add_routes([web.get('/', self.price_response)])
         web.run_app(self._app, port=6001)
 
-    def _start(self):
-        thread = Thread(target=self.start_web_app)
-        thread.daemon = True
-        thread.start()
+    def start(self):
+        p = Process(target=self.start_web_app)
+        p.daemon = True
+        p.start()
         time.sleep(2)
+        return p
 
     def stop(self):
         async_run(self._app.shutdown())
@@ -65,20 +66,24 @@ class CustomAPIFeedUnitTest(unittest.TestCase):
 
     def test_fetch_price(self):
         SimpleWebApp.get_instance().set_params(1, False)
+        p = SimpleWebApp.get_instance().start()
         api_feed = CustomAPIDataFeed(api_url=SimpleWebApp.api_url())
         api_feed.start()
         async_run(api_feed.check_network())
         self.assertTrue(api_feed.network_status == NetworkStatus.CONNECTED)
         price = api_feed.get_price()
         self.assertEqual(price, 1)
+        p.terminate()
 
     def test_fetch_server_error(self):
         SimpleWebApp.get_instance().set_params(1, True)
+        p = SimpleWebApp.get_instance().start()
         api_feed = CustomAPIDataFeed(api_url=SimpleWebApp.api_url())
         api_feed.start()
         with self.assertRaises(Exception) as context:
             async_run(api_feed.check_network())
         self.assertTrue('server error:' in str(context.exception))
+        p.terminate()
 
 
 if __name__ == "__main__":
