d5c8533c6d0dd68cae5937a9a4c1ea40d0cdab79
Joshua Richardson <joshua@webcoders.com.au>
2020-01-28 11:08:15 +1030

    Updates to check the block header bloom filter against topics we are interested in, to save on unncessary RPC eth_getLogs calls. Began work on ZeroeExFillWatcher to watch for 0x Fill logs in blocks.

--- hummingbot/wallet/ethereum/watcher/__init__.py
+++ hummingbot/wallet/ethereum/watcher/__init__.py
@@ -2,6 +2,7 @@ from .account_balance_watcher import AccountBalanceWatcher
 from .erc20_events_watcher import ERC20EventsWatcher
 from .incoming_eth_watcher import IncomingEthWatcher
 from .new_blocks_watcher import NewBlocksWatcher
+from .zeroex_fill_watcher import ZeroExFillWatcher
 from .weth_watcher import WethWatcher
 from .contract_event_logs import ContractEventLogger
 
@@ -12,5 +13,6 @@ __all__ = [
     IncomingEthWatcher,
     NewBlocksWatcher,
     WethWatcher,
+    ZeroExFillWatcher,
     ContractEventLogger,
 ]
