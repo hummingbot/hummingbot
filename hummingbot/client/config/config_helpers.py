5f6f292b100e60514bc438a38ae6b08a0ca46a6a
Nullably <37262506+Nullably@users.noreply.github.com>
2020-02-19 01:00:25 +0800

    (feat) update default min USD value

--- hummingbot/client/config/config_helpers.py
+++ hummingbot/client/config/config_helpers.py
@@ -347,7 +347,7 @@ async def create_yml_files():
 def minimum_order_amount(trading_pair):
     base_asset, quote_asset = trading_pair.split("-")
     global_quote_amount = [[b, m] for b, m in global_config_map.get("min_quote_order_amount").value if b == quote_asset]
-    default_quote, default_amount = "USD", 10
+    default_quote, default_amount = "USD", 11
     if len(global_quote_amount) > 0:
         default_quote, default_amount = global_quote_amount[0]
     quote_amount = ExchangeRateConversion.get_instance().convert_token_value_decimal(default_amount, default_quote,
