584e33868b185130657edfeb62905d1852e32209
Kieran McGowan <kmcgowan2000@gmail.com>
2020-02-26 16:44:06 -0800

    (fix) fix kraken order book diffs

--- hummingbot/market/kraken/kraken_api_order_book_data_source.py
+++ hummingbot/market/kraken/kraken_api_order_book_data_source.py
@@ -236,8 +236,13 @@ class KrakenAPIOrderBookDataSource(OrderBookTrackerDataSource):
                     await ws.send(ws_message)
                     async for raw_msg in self._inner_messages(ws):
                         msg = ujson.loads(raw_msg)
+                        msg_dict = {"trading_pair": msg[-1],
+                                    "asks": msg[1].get("a") or msg[1].get("as") or [],
+                                    "bids": msg[1].get("b") or msg[1].get("bs") or []}
+                        msg_dict["update_id"] = max([*map(lambda x: float(x[2]), msg_dict["bids"] + msg_dict["asks"])],
+                                                    default=0.)
                         order_book_message: OrderBookMessage = KrakenOrderBook.diff_message_from_exchange(
-                            msg, time.time())
+                            msg_dict, time.time())
                         output.put_nowait(order_book_message)
             except asyncio.CancelledError:
                 raise
