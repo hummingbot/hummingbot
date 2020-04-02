06835e23d4a153db6ff85a4a09bd3e6ce5379dae
Nullably <37262506+Nullably@users.noreply.github.com>
2020-04-03 00:17:33 +0800

    (feat) add file_name argument to import and create commands

--- hummingbot/client/command/import_command.py
+++ hummingbot/client/command/import_command.py
@@ -5,7 +5,8 @@ from hummingbot.client.config.config_helpers import (
     get_strategy_config_map,
     load_yml_into_cm,
     short_strategy_name,
-    get_strategy_template_path
+    get_strategy_template_path,
+    format_config_file_name
 )
 from hummingbot.client.settings import CONF_FILE_PATH, CONF_PREFIX
 from hummingbot.client.config.global_config_map import global_config_map
@@ -18,16 +19,28 @@ class ImportCommand:
 
     def import_command(self,  # type: HummingbotApplication
                        file_name):
+        strategy_file_name = None
+        if file_name is not None:
+            strategy_file_name = format_config_file_name(file_name)
+            if strategy_file_name is None:
+                self._notify(f"{file_name} is a not valid yml file.")
+                return
+            if not os.path.exists(os.path.join(CONF_FILE_PATH, strategy_file_name)):
+                self._notify(f"{strategy_file_name} does not exist.")
+                return
         self.app.clear_input()
         self.placeholder_mode = True
         self.app.toggle_hide_input()
-        safe_ensure_future(self.import_config_file())
+        safe_ensure_future(self.import_config_file(strategy_file_name))
 
     async def import_config_file(self,  # type: HummingbotApplication
-                                 ):
+                                 file_name):
         strategy = global_config_map.get("strategy").value
         config_map = get_strategy_config_map(strategy)
-        self.strategy_file_name = await self.prompt_a_file_name(strategy)
+        if file_name is not None:
+            self.strategy_file_name = file_name
+        else:
+            self.strategy_file_name = await self.prompt_a_file_name(strategy)
         strategy_path = os.path.join(CONF_FILE_PATH, self.strategy_file_name)
         template_path = get_strategy_template_path(strategy)
         load_yml_into_cm(strategy_path, template_path, config_map)
