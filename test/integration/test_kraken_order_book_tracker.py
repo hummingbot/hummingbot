24e6a86fe98642b0b3f6c219250f9828406aef1c
Kieran McGowan <kmcgowan2000@gmail.com>
2020-04-04 11:50:15 +1000

    (test) kraken order book tracker test

--- test/integration/test_kraken_order_book_tracker.py
+++ test/integration/test_kraken_order_book_tracker.py
@@ -4,7 +4,6 @@ import sys; sys.path.insert(0, realpath(join(__file__, "../../../")))
 
 from hummingbot.market.kraken.kraken_order_book_tracker import KrakenOrderBookTracker
 from hummingbot.market.kraken.kraken_api_order_book_data_source import KrakenAPIOrderBookDataSource
-from hummingbot.core.utils.async_utils import safe_ensure_future
 import asyncio
 import logging
 import unittest
@@ -15,7 +14,7 @@ class KrakenOrderBookTrackerUnitTest(unittest.TestCase):
     def setUpClass(cls):
         cls.ev_loop: asyncio.BaseEventLoop = asyncio.get_event_loop()
         cls.order_book_tracker: KrakenOrderBookTracker = KrakenOrderBookTracker(trading_pairs=["ETHUSDC", "XBTUSDC"])
-        cls.order_book_tracker_task: asyncio.Task = safe_ensure_future(cls.order_book_tracker.start())
+        cls.order_book_tracker_task: asyncio.Task = cls.order_book_tracker.start()
         cls.ev_loop.run_until_complete(cls.wait_til_tracker_ready())
 
     @classmethod
@@ -36,10 +35,10 @@ class KrakenOrderBookTrackerUnitTest(unittest.TestCase):
         self.assertEqual(self.order_book_tracker.exchange_name, "kraken")
 
     def test_start_stop(self):
-        self.order_book_tracker.start()
         self.assertTrue(asyncio.isfuture(self.order_book_tracker._order_book_snapshot_router_task))
         self.order_book_tracker.stop()
         self.assertIsNone(self.order_book_tracker._order_book_snapshot_router_task)
+        self.order_book_tracker.start()
 
 
 def main():
