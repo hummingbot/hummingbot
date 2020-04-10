04be97b5102331b58a84d0588b146d69a363f220
dennisocana <docana.hummingbot@gmail.com>
2020-04-10 16:07:40 +0800

    (feat) edit config description in help

--- hummingbot/client/ui/parser.py
+++ hummingbot/client/ui/parser.py
@@ -39,22 +39,22 @@ def load_parser(hummingbot) -> ThrowingArgumentParser:
     parser = ThrowingArgumentParser(prog="", add_help=False)
     subparsers = parser.add_subparsers()
 
-    balance_parser = subparsers.add_parser("balance", help="Display all balances from all connected exchanges.")
+    balance_parser = subparsers.add_parser("balance", help="Display all balances from all connected exchanges")
     balance_parser.set_defaults(func=hummingbot.balance)
 
-    connect_parser = subparsers.add_parser("connect", help="Connect or reconnect to an exchange.")
+    connect_parser = subparsers.add_parser("connect", help="Connect or reconnect to an exchange")
     connect_parser.add_argument("exchange", nargs="?", choices=EXCHANGES, help="An exchange name")
     connect_parser.set_defaults(func=hummingbot.connect)
 
-    create_parser = subparsers.add_parser("create", help="Create a new configuration for a new bot.")
+    create_parser = subparsers.add_parser("create", help="Create a new configuration for a new bot")
     create_parser.add_argument("file_name", nargs="?", default=None, help="A new configuration file name")
     create_parser.set_defaults(func=hummingbot.create)
 
-    import_parser = subparsers.add_parser("import", help="Import a configuration to start a bot.")
+    import_parser = subparsers.add_parser("import", help="Import a configuration to start a bot")
     import_parser.add_argument("file_name", nargs="?", default=None, help="A configuration file name")
     import_parser.set_defaults(func=hummingbot.import_command)
 
-    config_parser = subparsers.add_parser("config", help="Create a new bot or import an existing configuration")
+    config_parser = subparsers.add_parser("config", help="Display current strategy configuration")
     config_parser.add_argument("key", nargs="?", default=None, help="Configure a specific variable")
     config_parser.add_argument("value", nargs="?", default=None, help="Value for the variable")
     config_parser.set_defaults(func=hummingbot.config)
