d0b2bb30b8ce980910743bc74ecc01891f390b6f
Nullably <37262506+Nullably@users.noreply.github.com>
2020-04-14 17:26:15 +0800

    (feat) show ethereum connection and ETH balance.

--- hummingbot/client/command/connect_command.py
+++ hummingbot/client/command/connect_command.py
@@ -29,26 +29,36 @@ class ConnectCommand:
         elif option == "ethereum":
             safe_ensure_future(self.connect_ethereum())
         else:
-            safe_ensure_future(self.prompt_api_keys(option))
+            safe_ensure_future(self.connect_exchange(option))
 
-    async def prompt_api_keys(self,  # type: HummingbotApplication
-                              exchange):
+    async def connect_exchange(self,  # type: HummingbotApplication
+                               exchange):
         self.app.clear_input()
         self.placeholder_mode = True
         self.app.hide_input = True
         exchange_configs = [c for c in global_config_map.values() if exchange in c.key]
-        for config in exchange_configs:
-            await self.prompt_a_config(config)
-            Security.update_secure_config(config.key, config.value)
-        api_keys = (await Security.api_keys(exchange)).values()
+        to_connect = True
+        if Security.encrypted_file_exists(exchange_configs[0].key):
+            await Security.wait_til_decryption_done()
+            api_key_config = [c for c in exchange_configs if "api_key" in c.key][0]
+            api_key = Security.decrypted_value(api_key_config.key)
+            answer = await self.app.prompt(prompt=f"Would you like to replace your existing {exchange} API key "
+                                                  f"...{api_key[-4:]} (Yes/No)? >>> ")
+            if answer.lower() not in ("yes", "y"):
+                to_connect = False
+        if to_connect:
+            for config in exchange_configs:
+                await self.prompt_a_config(config)
+                Security.update_secure_config(config.key, config.value)
+            api_keys = (await Security.api_keys(exchange)).values()
+            err_msg = await UserBalances.instance().add_exchange(exchange, *api_keys)
+            if err_msg is None:
+                self._notify(f"\nYou are now connected to {exchange}.")
+            else:
+                self._notify(f"\nError: {err_msg}")
         self.placeholder_mode = False
         self.app.hide_input = False
         self.app.change_prompt(prompt=">>> ")
-        err_msg = await UserBalances.instance().add_exchange(exchange, *api_keys)
-        if err_msg is None:
-            self._notify(f"\nYou are now connected to {exchange}.")
-        else:
-            self._notify(f"\nError: {err_msg}")
 
     async def show_connections(self  # type: HummingbotApplication
                                ):
@@ -67,18 +77,24 @@ class ConnectCommand:
         data = []
         failed_msgs = {}
         err_msgs = await UserBalances.instance().update_all(reconnect=True)
-        for exchange in sorted(OPTIONS):
-            api_keys = (await Security.api_keys(exchange)).values()
+        for option in sorted(OPTIONS):
             keys_added = "No"
             keys_confirmed = 'No'
-            if len(api_keys) > 0:
-                keys_added = "Yes"
-                err_msg = err_msgs.get(exchange)
-                if err_msg is not None:
-                    failed_msgs[exchange] = err_msg
-                else:
-                    keys_confirmed = 'Yes'
-            data.append([exchange, keys_added, keys_confirmed])
+            if option == "ethereum":
+                eth_address = global_config_map["ethereum_wallet"].value
+                if eth_address is not None and eth_address in Security.private_keys():
+                    keys_added = "Yes"
+                    keys_confirmed = "Yes"
+            else:
+                api_keys = (await Security.api_keys(option)).values()
+                if len(api_keys) > 0:
+                    keys_added = "Yes"
+                    err_msg = err_msgs.get(option)
+                    if err_msg is not None:
+                        failed_msgs[option] = err_msg
+                    else:
+                        keys_confirmed = 'Yes'
+            data.append([option, keys_added, keys_confirmed])
         return pd.DataFrame(data=data, columns=columns), failed_msgs
 
     async def connect_ethereum(self,  # type: HummingbotApplication
@@ -86,19 +102,21 @@ class ConnectCommand:
         self.placeholder_mode = True
         self.app.hide_input = True
         ether_wallet = global_config_map["ethereum_wallet"].value
+        to_connect = True
         if ether_wallet is not None:
             answer = await self.app.prompt(prompt=f"Would you like to replace your existing Ethereum wallet "
-                                                  f"{ether_wallet} (Yes/No)? >>> ")
+                                                  f"...{ether_wallet[-4:]} (Yes/No)? >>> ")
             if answer.lower() not in ("yes", "y"):
-                return
-        private_key = await self.app.prompt(prompt="Enter your wallet private key >>> ", is_password=True)
-        try:
-            public_address = Security.add_private_key(private_key)
-            global_config_map["ethereum_wallet"].value = public_address
-            save_to_yml(GLOBAL_CONFIG_PATH, global_config_map)
-            self._notify(f"Wallet {public_address} connected to hummingbot.")
-        except Exception as e:
-            self._notify(f"Failed to connect wallet key: {e}")
+                to_connect = False
+        if to_connect:
+            private_key = await self.app.prompt(prompt="Enter your wallet private key >>> ", is_password=True)
+            try:
+                public_address = Security.add_private_key(private_key)
+                global_config_map["ethereum_wallet"].value = public_address
+                save_to_yml(GLOBAL_CONFIG_PATH, global_config_map)
+                self._notify(f"Wallet {public_address} connected to hummingbot.")
+            except Exception as e:
+                self._notify(f"Failed to connect wallet key: {e}")
         self.placeholder_mode = False
         self.app.hide_input = False
         self.app.change_prompt(prompt=">>> ")
