9d7da5953e0fa3dbcff883d83e2be939ea8699ea
dennisocana <docana.hummingbot@gmail.com>
2020-04-24 11:50:56 +0800

    Revert "(cleanup) remove discovery from markets"
    This reverts commit fe5197d2ed93efb1eb3009738277bf37b863b5a8.

--- hummingbot/market/bitcoin_com/bitcoin_com_market.pyx
+++ hummingbot/market/bitcoin_com/bitcoin_com_market.pyx
@@ -266,6 +266,14 @@ cdef class BitcoinComMarket(MarketBase):
             for key, value in saved_states.items()
         })
 
+    async def get_active_exchange_markets(self) -> pd.DataFrame:
+        """
+        *required
+        Used by the discovery strategy to read order books of all actively trading markets,
+        and find opportunities to profit
+        """
+        return await BitcoinComAPIOrderBookDataSource.get_active_exchange_markets()
+
     cdef c_start(self, Clock clock, double timestamp):
         """
         *required
