e56e305f9232853ac55da4c5ce6fe50de3e387a6
Nullably <37262506+Nullably@users.noreply.github.com>
2020-06-01 15:44:22 +0800

    (refactor) minor fixes to data type

--- hummingbot/strategy/cross_exchange_market_making/cross_exchange_market_making_config_map.py
+++ hummingbot/strategy/cross_exchange_market_making/cross_exchange_market_making_config_map.py
@@ -179,7 +179,7 @@ cross_exchange_market_making_config_map = {
         prompt="Enter conversion rate for taker base asset value to maker base asset value, e.g. "
                "if maker base asset is USD, taker is DAI and 1 USD is worth 1.25 DAI, "
                "the conversion rate is 0.8 (1 / 1.25) >>> ",
-        default=1,
+        default=Decimal("1"),
         validator=lambda v: validate_decimal(v, Decimal(0), Decimal("100"), inclusive=False),
         type_str="decimal"),
     "taker_to_maker_quote_conversion_rate": ConfigVar(
@@ -187,7 +187,7 @@ cross_exchange_market_making_config_map = {
         prompt="Enter conversion rate for taker quote asset value to maker quote asset value, e.g. "
                "if taker quote asset is USD, maker is DAI and 1 USD is worth 1.25 DAI, "
                "the conversion rate is 0.8 (1 / 1.25) >>> ",
-        default=1,
+        default=Decimal("1"),
         validator=lambda v: validate_decimal(v, Decimal(0), Decimal("100"), inclusive=False),
         type_str="decimal"),
 }
