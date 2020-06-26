bc00acf2ed949d77c2672a4efb21bd966a112535
Nullably <37262506+Nullably@users.noreply.github.com>
2020-06-26 15:34:17 +0800

    (feat) add script class file loading

---
+++ conf/price_band_script.py
@@ -0,0 +1,20 @@
+from hummingbot.script.script_base import ScriptBase
+
+
+class PriceBandScript(ScriptBase):
+    def __init__(self):
+        super().__init__()
+
+    def on_tick(self):
+        strategy = self.strategy_parameters.copy()
+        if self.mid_price >= 105:
+            strategy.buy_levels = 0
+        else:
+            strategy.buy_levels = strategy.order_levels
+        if self.mid_price <= 95:
+            strategy.sell_levels = 0
+        else:
+            strategy.sell_levels = strategy.order_levels
+        if strategy != self.strategy_parameters:
+            self.strategy_parameters = strategy
+            self.update_strategy_parameters()
--- hummingbot/script/script_base.py
+++ hummingbot/script/script_base.py
@@ -1,49 +1,38 @@
 import asyncio
 from decimal import Decimal
-from .script_interface import CallUpdateStrategyParameters, OnTick
+from multiprocessing import Queue
+from .script_interface import CallUpdateStrategyParameters, OnTick, StrategyParameters
 
 
 class ScriptBase:
-    def __init__(self, parent_queue, child_queue, queue_check_interval):
-        print("ScriptBase__init__")
+    def __init__(self):
+        self._parent_queue: Queue = None
+        self._child_queue: Queue = None
+        self._queue_check_interval: float = 0.0
+        self.mid_price: Decimal = Decimal("0")
+        self.strategy_parameters: StrategyParameters = None
+
+    def assign_process_init(self, parent_queue: Queue, child_queue: Queue, queue_check_interval: float):
         self._parent_queue = parent_queue
         self._child_queue = child_queue
         self._queue_check_interval = queue_check_interval
-        self.mid_price = Decimal("0")
-        self.strategy_parameters = None
 
-    def on_tick(self):
-        print("on_tick")
-        strategy = self.strategy_parameters.copy()
-        if self.mid_price >= 105:
-            strategy.buy_levels = 0
-        else:
-            strategy.buy_levels = strategy.order_levels
-        if self.mid_price <= 95:
-            strategy.sell_levels = 0
-        else:
-            strategy.sell_levels = strategy.order_levels
-        if strategy != self.strategy_parameters:
-            self.strategy_parameters = strategy
-        self.update_strategy_parameters()
+    # def on_tick(self):
+    #     strategy = self.strategy_parameters.copy()
+    #     if self.mid_price >= 105:
+    #         strategy.buy_levels = 0
+    #     else:
+    #         strategy.buy_levels = strategy.order_levels
+    #     if self.mid_price <= 95:
+    #         strategy.sell_levels = 0
+    #     else:
+    #         strategy.sell_levels = strategy.order_levels
+    #     if strategy != self.strategy_parameters:
+    #         self.strategy_parameters = strategy
+    #         self.update_strategy_parameters()
 
     async def run(self):
         asyncio.ensure_future(self.listen_to_parent())
-
-    def listen_to_parent_sync(self):
-        while True:
-            # if self._parent_queue.empty():
-            #     await asyncio.sleep(0.1)
-            #     continue
-            item = self._parent_queue.get()
-            print(f"child gets {item.__class__}")
-            if item is None:
-                print("child exiting..")
-                break
-            if isinstance(item, OnTick):
-                self.mid_price = item.mid_price
-                self.strategy_parameters = item.strategy_parameters
-                self.on_tick()
 
     async def listen_to_parent(self):
         while True:
@@ -61,5 +50,4 @@ class ScriptBase:
                 self.on_tick()
 
     def update_strategy_parameters(self):
-        print("update_strategy_parameters")
         self._child_queue.put(CallUpdateStrategyParameters(self.strategy_parameters))
