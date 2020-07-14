5dacd2eb9c0a17b7ab680cb47dd9ce30ca117827
Nullably <37262506+Nullably@users.noreply.github.com>
2020-07-15 00:40:34 +0800

    (fix) show balances per exchange in an easy to read format

--- hummingbot/client/command/balance_command.py
+++ hummingbot/client/command/balance_command.py
@@ -3,8 +3,8 @@ from hummingbot.core.utils.async_utils import safe_ensure_future
 from hummingbot.client.config.global_config_map import global_config_map
 from hummingbot.market.celo.celo_cli import CeloCLI
 import pandas as pd
-from numpy import NaN
-from typing import TYPE_CHECKING
+from decimal import Decimal
+from typing import TYPE_CHECKING, Dict
 if TYPE_CHECKING:
     from hummingbot.client.hummingbot_application import HummingbotApplication
 
@@ -15,43 +15,62 @@ class BalanceCommand:
 
     async def show_balances(self):
         self._notify("Updating balances, please wait...")
-        df = await self.balances_df()
-        lines = ["    " + line for line in df.to_string(index=False).split("\n")]
-        self._notify("\n".join(lines))
-        eth_address = global_config_map["ethereum_wallet"].value
-        if eth_address is not None:
-            bal = UserBalances.ethereum_balance()
-            bal = round(bal, 4)
-            self._notify(f"Ethereum balance in ...{eth_address[-4:]} wallet: {bal} ETH")
-            self._notify(f"Note: You may have other ERC 20 tokens in this same address (not shown here).")
+        all_ex_bals = await UserBalances.instance().all_balances_all_exchanges()
+        for exchange, bals in all_ex_bals.items():
+            self._notify(f"\n{exchange}:")
+            df = await self.exchange_balances_df(bals)
+            if df.empty:
+                self._notify("You have no balance on this exchange.")
+            else:
+                lines = ["    " + line for line in df.to_string(index=False).split("\n")]
+                self._notify("\n".join(lines))
+
         celo_address = global_config_map["celo_address"].value
         if celo_address is not None:
             try:
                 if not CeloCLI.unlocked:
                     await self.validate_n_connect_celo()
-                bals = CeloCLI.balances()
-                self._notify("Celo balances:")
-                for token, bal in bals.items():
-                    self._notify(f"  {token} - total: {bal.total} locked: {bal.locked}")
+                df = await self.celo_balances_df()
+                lines = ["    " + line for line in df.to_string(index=False).split("\n")]
+                self._notify("\ncelo:")
+                self._notify("\n".join(lines))
             except Exception as e:
-                self._notify(f"Celo CLI Error: {str(e)}")
+                self._notify(f"\ncelo CLI Error: {str(e)}")
 
-    async def balances_df(self  # type: HummingbotApplication
-                          ):
-        all_ex_bals = await UserBalances.instance().all_balances_all_exchanges()
-        ex_columns = ["Symbol"] + list(all_ex_bals.keys())
+        eth_address = global_config_map["ethereum_wallet"].value
+        if eth_address is not None:
+            df = await self.ethereum_balances_df()
+            lines = ["    " + line for line in df.to_string(index=False).split("\n")]
+            self._notify("\nethereum:")
+            self._notify("\n".join(lines))
+
+    async def exchange_balances_df(self,  # type: HummingbotApplication
+                                   ex_bals: Dict[str, Decimal]):
         rows = []
-        for exchange, bals in all_ex_bals.items():
-            for token, bal in bals.items():
-                if bal == 0:
-                    continue
-                token = token.upper()
-                if not any(r.get("Symbol") == token for r in rows):
-                    rows.append({"Symbol": token})
-                row = [r for r in rows if r["Symbol"] == token][0]
-                row[exchange] = round(bal, 4)
+        for token, bal in ex_bals.items():
+            if bal == 0:
+                continue
+            token = token.upper()
+            rows.append({"asset": token.upper(), "amount": round(bal, 4)})
+        df = pd.DataFrame(data=rows, columns=["asset", "amount"])
+        df.sort_values(by=["asset"], inplace=True)
+        return df
 
-        df = pd.DataFrame(data=rows, columns=ex_columns)
-        df = df.replace(NaN, 0)
-        df.sort_values(by=["Symbol"], inplace=True)
+    async def celo_balances_df(self,  # type: HummingbotApplication
+                               ):
+        rows = []
+        bals = CeloCLI.balances()
+        for token, bal in bals.items():
+            rows.append({"asset": token.upper(), "amount": round(bal, 4)})
+        df = pd.DataFrame(data=rows, columns=["asset", "amount"])
+        df.sort_values(by=["asset"], inplace=True)
+        return df
+
+    async def ethereum_balances_df(self,  # type: HummingbotApplication
+                                   ):
+        rows = []
+        bal = UserBalances.ethereum_balance()
+        rows.append({"asset": "ETH", "amount": round(bal, 4)})
+        df = pd.DataFrame(data=rows, columns=["asset", "amount"])
+        df.sort_values(by=["asset"], inplace=True)
         return df
