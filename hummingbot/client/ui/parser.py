5cac736065953705ac0a129a75e53560752e3391
Daniel Tan <danieltanjiawang@gmail.com>
2020-07-29 19:33:44 +0800

    (feat) include balance limit on a per exchange basis

--- hummingbot/client/ui/parser.py
+++ hummingbot/client/ui/parser.py
@@ -57,6 +57,7 @@ def load_parser(hummingbot) -> ThrowingArgumentParser:
 
     balance_parser = subparsers.add_parser("balance", help=f"Display your asset balances across all connected exchanges")
     balance_parser.add_argument("option", nargs="?", choices=["limit"], default=None, help="Option for balance configuration")
+    balance_parser.add_argument("exchange", nargs="?", default=None, help="Name of Exchange")
     balance_parser.add_argument("asset", nargs="?", default=None, help="Name of Asset Token")
     balance_parser.add_argument("amount", nargs="?", default=None, help="Amount to limit")
     balance_parser.set_defaults(func=hummingbot.balance)
