f6a3e1632f26e75e7ae6fc7b3aebbc18631bbe27
Daniel Tan <danieltanjiawang@gmail.com>
2020-07-31 07:42:05 +0800

    (fix) resolve outstanding issues

--- hummingbot/client/config/global_config_map.py
+++ hummingbot/client/config/global_config_map.py
@@ -8,7 +8,7 @@ from hummingbot.client.settings import (
     DEXES,
     DEFAULT_KEY_FILE_PATH,
     DEFAULT_LOG_FILE_PATH,
-    SCRIPTS_PATH
+    SCRIPTS_PATH, EXCHANGES
 )
 from hummingbot.client.config.config_validators import (
     validate_bool,
@@ -396,5 +396,6 @@ global_config_map = {
                   prompt="Use the `balance limit` command"
                          "e.g. balance limit [EXCHANGE] [ASSET] [AMOUNT]",
                   required_if=lambda: False,
-                  type_str="json"),
+                  type_str="json",
+                  default={exchange: None for exchange in EXCHANGES}),
 }
