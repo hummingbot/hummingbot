7bae4504e5e19739d347cdb9334dba8393f5bf68
Daniel Tan <danieltanjiawang@gmail.com>
2020-07-31 07:42:05 +0800

    (fix) resolve outstanding issues

--- hummingbot/client/command/balance_command.py
+++ hummingbot/client/command/balance_command.py
@@ -10,7 +10,8 @@ from hummingbot.client.config.config_helpers import (
 from hummingbot.market.celo.celo_cli import CeloCLI
 import pandas as pd
 from decimal import Decimal
-from typing import TYPE_CHECKING, Dict
+from typing import TYPE_CHECKING, Dict, Optional
+
 if TYPE_CHECKING:
     from hummingbot.client.hummingbot_application import HummingbotApplication
 
@@ -52,32 +53,36 @@ class BalanceCommand:
 
                 if asset is not None and amount is not None and exchange is not None:
                     exchange_limit_conf = config_var.value[exchange]
+                    if exchange_limit_conf is None:
+                        config_var.value.update({exchange: {}})
                     asset = asset.upper()
                     self._notify(f"Limit for {asset} token, set to {amount}")
-                    exchange_limit_conf[asset] = amount
+                    config_var.value[exchange].update({asset: amount})
+                    save_to_yml(file_path, config_map)
+                else:
+                    self._notify("Error with command arguments. See command details below.")
+                    safe_ensure_future(self.list_options())
                     return
-
-                self._notify("Error with command arguments. See command details below.")
-                safe_ensure_future(self.list_options())
-                return
-
-            save_to_yml(file_path, config_map)
 
     async def list_options(self):
         row = []
         self._notify(f"List of Option(s) of Balance command\n")
         for option in OPTIONS:
-            row.append(f"{option}: {OPTION_DESCRIPTION[option]}")
-            row.append(f"   e.g. {OPTION_HELP[option]}")
+            row.append(f"   - {option}: {OPTION_DESCRIPTION[option]}")
+            row.append(f"       e.g. {OPTION_HELP[option]}")
         self._notify("\n".join(row))
 
     async def show_balances(self):
         self._notify("Updating balances, please wait...")
         all_ex_bals = await UserBalances.instance().all_balances_all_exchanges()
-        all_ex_limits = global_config_map[LIMIT_GLOBAL_CONFIG].value
+        all_ex_limits: Optional[Dict[str, Dict[str, str]]] = global_config_map[LIMIT_GLOBAL_CONFIG].value
+
+        if all_ex_limits is None:
+            all_ex_limits = {}
+
         for exchange, bals in all_ex_bals.items():
             self._notify(f"\n{exchange}:")
-            df = await self.exchange_balances_df(bals, all_ex_limits[exchange])
+            df = await self.exchange_balances_df(bals, all_ex_limits.get(exchange, {}))
             if df.empty:
                 self._notify("You have no balance on this exchange.")
             else:
@@ -105,10 +110,10 @@ class BalanceCommand:
 
     async def exchange_balances_df(self,  # type: HummingbotApplication
                                    exchange_balances: Dict[str, Decimal],
-                                   exchange_limits: Dict[str, str] = None):
+                                   exchange_limits: Dict[str, str]):
         rows = []
         for token, bal in exchange_balances.items():
-            limit = Decimal(exchange_limits.get(token, 0))
+            limit = Decimal(exchange_limits.get(token.upper(), 0)) if exchange_limits is not None else Decimal(0)
             if bal == 0 and limit == 0:
                 continue
             token = token.upper()
@@ -147,9 +152,15 @@ class BalanceCommand:
         return df
 
     async def show_asset_limits(self):
-        self._notify(f"Balance Limits per exchange...")
         config_var = global_config_map[LIMIT_GLOBAL_CONFIG]
         exchange_limit_conf: Dict[str, Dict[str, str]] = config_var.value
+
+        if not any(list(exchange_limit_conf.values())):
+            self._notify("You have not set any limits. See command details below\n")
+            await self.list_options()
+            return
+
+        self._notify(f"Balance Limits per exchange...")
 
         for exchange, asset_limit_config in exchange_limit_conf.items():
             if asset_limit_config is None:
