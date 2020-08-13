d3ce37692b4a4da9716a22d5da6e34b5a5e08bfd
vic-en <victoreni14@gmail.com>
2020-08-13 08:04:01 +0000

    (fix) Remove conversion to exchange pairs in strategy files

--- hummingbot/strategy/dev_1_get_order_book/start.py
+++ hummingbot/strategy/dev_1_get_order_book/start.py
@@ -14,7 +14,7 @@ def start(self):
         raw_market_trading_pair = dev_1_get_order_book_config_map.get("market_trading_pair").value
 
         try:
-            trading_pair: str = self._convert_to_exchange_trading_pair(market, [raw_market_trading_pair])[0]
+            trading_pair: str = raw_market_trading_pair
             assets: Tuple[str, str] = self._initialize_market_assets(market, [trading_pair])[0]
         except ValueError as e:
             self._notify(str(e))
