bbd862fcc4110a65b79a00f79c2fbe51cbc00162
Martín Volpe <martin.volpe@gmail.com>
2020-08-13 21:18:28 +0200

    Updated okex_market

--- hummingbot/market/okex/constants.py
+++ hummingbot/market/okex/constants.py
@@ -3,11 +3,15 @@ from urllib.parse import urljoin
 
 OKEX_BASE_URL = "https://okex.com/api/"
 
+
 OKEX_SYMBOLS_URL = urljoin(OKEX_BASE_URL, "spot/v3/instruments/ticker")
 OKEX_DEPTH_URL = urljoin(OKEX_BASE_URL, "spot/v3/instruments/{trading_pair}/book")
 
+# Auth required
+# Doesn't include base URL as the tail is required to generate the signature
+
+OKEX_PLACE_ORDER = "spot/v3/orders"
+
+
+# WS
 OKCOIN_WS_URI = "wss://real.okex.com:8443/ws/v3"
-
-
-HUOBI_API_ENDPOINT = "https://api.huobi.pro"
-HUOBI_WS_ENDPOINT = "wss://api.huobi.pro/ws/v2"
