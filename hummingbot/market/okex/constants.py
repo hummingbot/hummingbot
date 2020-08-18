e1a82988ef18ba796ded5ed8ce8fc959514c9569
Martín Volpe <martin.volpe@gmail.com>
2020-08-19 00:28:18 +0200

    Can update network status, but test don't work

--- hummingbot/market/okex/constants.py
+++ hummingbot/market/okex/constants.py
@@ -7,8 +7,11 @@ OKEX_BASE_URL = "https://okex.com/"
 OKEX_SYMBOLS_URL = urljoin(OKEX_BASE_URL, "spot/v3/instruments/ticker")
 OKEX_DEPTH_URL = urljoin(OKEX_BASE_URL, "spot/v3/instruments/{trading_pair}/book")
 
-# Auth required
 # Doesn't include base URL as the tail is required to generate the signature
+
+OKEX_SERVER_TIME = '/api/general/v3/time'
+
+# Auth required
 
 OKEX_PLACE_ORDER = "api/spot/v3/orders"
 OKEX_BALANCE_URL = "api/spot/v3/accounts"
