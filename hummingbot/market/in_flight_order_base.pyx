b3efc86989d86db48874f71fe7010caca02070f3
Nullably <37262506+Nullably@users.noreply.github.com>
2020-08-27 18:43:38 +0800

    (feat) almost finish with crypto_com_exchange

--- hummingbot/market/in_flight_order_base.pyx
+++ hummingbot/market/in_flight_order_base.pyx
@@ -6,7 +6,6 @@ from typing import (
     Optional
 )
 from hummingbot.core.data_type.limit_order import LimitOrder
-from hummingbot.market.market_base import MarketBase
 from hummingbot.core.event.events import (
     OrderType,
     TradeType
@@ -17,7 +16,6 @@ s_decimal_0 = Decimal(0)
 
 cdef class InFlightOrderBase:
     def __init__(self,
-                 market_class: MarketBase,
                  client_order_id: str,
                  exchange_order_id: Optional[str],
                  trading_pair: str,
@@ -26,8 +24,6 @@ cdef class InFlightOrderBase:
                  price: Decimal,
                  amount: Decimal,
                  initial_state: str):
-
-        self.market_class = market_class
         self.client_order_id = client_order_id
         self.exchange_order_id = exchange_order_id
         self.trading_pair = trading_pair
