eacc371ad63494d7b3a1c31c1a5d671eefa2b05f
Martin Kou <martin@hummingbot.io>
2020-09-04 21:46:45 -0700

    Bumped version, to avoid pip cache.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if is_posix:
 
 def main():
     cpu_count = os.cpu_count() or 8
-    version = "20200902"
+    version = "20200904"
     packages = [
         "hummingbot",
         "hummingbot.client",
