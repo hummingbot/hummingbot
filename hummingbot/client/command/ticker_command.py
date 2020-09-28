d63ebe86078ef88d4efb28dd495a731c505ce2b0
Dennis Ocana <dennis@coinalpha.com>
2020-09-28 23:43:43 +0800

    (feat) add loop to repeat display ticker

--- hummingbot/client/command/ticker_command.py
+++ hummingbot/client/command/ticker_command.py
@@ -1,10 +1,12 @@
 from hummingbot.core.utils.async_utils import safe_ensure_future
-import pandas as pd
 from hummingbot.core.event.events import PriceType
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from hummingbot.client.hummingbot_application import HummingbotApplication
+
+import pandas as pd
+import asyncio
 
 
 class TickerCommand:
@@ -36,13 +38,16 @@ class TickerCommand:
             trading_pair, order_book = market, market_connector.order_books[market]
         else:
             trading_pair, order_book = next(iter(market_connector.order_books.items()))
+            self._notify(f"  market: {market_connector.name}\n")
 
-        columns = ["Best Bid", "Best Ask", "Mid Price", "Last Trade"]
-        data = [[
-            float(market_connector.get_price_by_type(trading_pair, PriceType.BestBid)),
-            float(market_connector.get_price_by_type(trading_pair, PriceType.BestAsk)),
-            float(market_connector.get_price_by_type(trading_pair, PriceType.MidPrice)),
-            float(market_connector.get_price_by_type(trading_pair, PriceType.LastTrade))
-        ]]
-        df = pd.DataFrame(data=data, columns=columns).to_string(index=False)
-        self._notify(f"  market: {market_connector.name} {trading_pair}\n\n {df}")
+        for i in range(repeat):
+            columns = ["Best Bid", "Best Ask", "Mid Price", "Last Trade"]
+            data = [[
+                float(market_connector.get_price_by_type(trading_pair, PriceType.BestBid)),
+                float(market_connector.get_price_by_type(trading_pair, PriceType.BestAsk)),
+                float(market_connector.get_price_by_type(trading_pair, PriceType.MidPrice)),
+                float(market_connector.get_price_by_type(trading_pair, PriceType.LastTrade))
+            ]]
+            df = pd.DataFrame(data=data, columns=columns).to_string(index=False)
+            self._notify(f" {df}\n")
+            await asyncio.sleep(1)
