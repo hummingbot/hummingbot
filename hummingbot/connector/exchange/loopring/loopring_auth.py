f84baf92ea98cc684341efe78004df3df3e42a0f
TheHolyRoger <theholyroger@theholyroger.com>
2020-10-03 00:00:24 +0100

    `loopring` Connector Flake8 cleanup/fixes

--- hummingbot/connector/exchange/loopring/loopring_auth.py
+++ hummingbot/connector/exchange/loopring/loopring_auth.py
@@ -1,14 +1,15 @@
 from typing import (
-    Optional,
+    # Optional,
     Dict,
     Any
 )
+
 
 class LoopringAuth:
     def __init__(self, api_key: str):
         self.api_key = api_key
 
-    def generate_auth_dict(self) -> Dict[str, Any]: 
+    def generate_auth_dict(self) -> Dict[str, Any]:
         # TODO: remove the uri and data parameters unless we end up doing something with them
         """
         Generates authentication signature and returns it in a dictionary
