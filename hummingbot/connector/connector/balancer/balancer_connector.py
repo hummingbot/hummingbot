96953f24cf502f99324ed973b2163a0cdf39983c
Nullably <37262506+Nullably@users.noreply.github.com>
2020-10-07 15:52:04 +0800

    (feat) connect to balancer gateway api

--- hummingbot/connector/connector/balancer/balancer_connector.py
+++ hummingbot/connector/connector/balancer/balancer_connector.py
@@ -1,18 +1,28 @@
 import logging
 from decimal import Decimal
+import asyncio
+import aiohttp
+from typing import Dict, Any
+import json
+import requests
 
+from hummingbot.connector.in_flight_order_base import InFlightOrderBase
 from hummingbot.core.network_iterator import NetworkStatus
+from hummingbot.core.utils.async_utils import safe_ensure_future, safe_gather
 from hummingbot.logger import HummingbotLogger
 from hummingbot.core.utils.tracking_nonce import get_tracking_nonce
 from hummingbot.core.event.events import (
     MarketEvent,
     BuyOrderCreatedEvent,
     SellOrderCreatedEvent,
+    MarketOrderFailureEvent,
     OrderType,
+    TradeType,
 )
 from hummingbot.connector.connector_base import ConnectorBase
 s_logger = None
 s_decimal_NaN = Decimal("nan")
+GATEWAY_API_URL = "http://localhost:5000"
 
 
 class BalancerConnector(ConnectorBase):
@@ -32,8 +42,16 @@ class BalancerConnector(ConnectorBase):
             s_logger = logging.getLogger(__name__)
         return s_logger
 
-    def __init__(self):
+    def __init__(self,
+                 trading_required: bool = True
+                 ):
+        """
+        :param trading_required: Whether actual trading is needed.
+        """
         super().__init__()
+        self._trading_required = trading_required
+        self._ev_loop = asyncio.get_event_loop()
+        self._shared_client = None
 
     @property
     def name(self):
@@ -48,10 +66,6 @@ class BalancerConnector(ConnectorBase):
     def get_order_price(self, trading_pair: str, is_buy: bool, amount: Decimal) -> Decimal:
         return self.get_quote_price(trading_pair, is_buy, amount)
 
-    def set_balance(self, token, balance):
-        self._account_balances[token] = Decimal(str(balance))
-        self._account_available_balances[token] = Decimal(str(balance))
-
     def buy(self, trading_pair: str, amount: Decimal, order_type: OrderType, price: Decimal):
         return self.place_order(True, trading_pair, amount, price)
 
@@ -61,11 +75,91 @@ class BalancerConnector(ConnectorBase):
     def place_order(self, is_buy: bool, trading_pair: str, amount: Decimal, price: Decimal):
         side = "buy" if is_buy else "sell"
         order_id = f"{side}-{trading_pair}-{get_tracking_nonce()}"
-        event_tag = MarketEvent.BuyOrderCreated if is_buy else MarketEvent.SellOrderCreated
-        event_class = BuyOrderCreatedEvent if is_buy else SellOrderCreatedEvent
-        self.trigger_event(event_tag, event_class(self.current_timestamp, OrderType.LIMIT, trading_pair,
-                                                  amount, price, order_id))
+        safe_ensure_future(self._create_order(TradeType.BUY, order_id, trading_pair, amount, price))
         return order_id
+
+    async def _create_order(self,
+                            trade_type: TradeType,
+                            order_id: str,
+                            trading_pair: str,
+                            amount: Decimal,
+                            price: Decimal):
+        """
+        Calls create-order API end point to place an order, starts tracking the order and triggers order created event.
+        :param trade_type: BUY or SELL
+        :param order_id: Internal order id (also called client_order_id)
+        :param trading_pair: The market to place order
+        :param amount: The order amount (in base token value)
+        :param price: The order price
+        """
+        trading_rule = self._trading_rules[trading_pair]
+
+        amount = self.quantize_order_amount(trading_pair, amount)
+        price = self.quantize_order_price(trading_pair, price)
+        if amount < trading_rule.min_order_size:
+            raise ValueError(f"Buy order amount {amount} is lower than the minimum order size "
+                             f"{trading_rule.min_order_size}.")
+        base_asset, quote_asset = trading_pair.split("-")
+        token_in = quote_asset if trade_type is TradeType.BUY else base_asset
+        token_out = quote_asset if trade_type is not TradeType.BUY else base_asset
+        api_params = {"tokenIn": token_in,
+                      "tokenOut": token_out,
+                      "amount": amount,
+                      "maxPrice": f"{price:f}",
+                      }
+        self.start_tracking_order(order_id, None, trading_pair, trade_type, price, amount)
+        try:
+            order_result = await self._api_request("get", "balancer/trade", api_params)
+            exchange_order_id = str(order_result["result"]["order_id"])
+            tracked_order = self._in_flight_orders.get(order_id)
+            if tracked_order is not None:
+                self.logger().info(f"Created {trade_type.name} order {order_id} for {amount} {trading_pair}.")
+                tracked_order.exchange_order_id = exchange_order_id
+
+            event_tag = MarketEvent.BuyOrderCreated if trade_type is TradeType.BUY else MarketEvent.SellOrderCreated
+            event_class = BuyOrderCreatedEvent if trade_type is TradeType.BUY else SellOrderCreatedEvent
+            self.trigger_event(event_tag, event_class(self.current_timestamp, OrderType.LIMIT, trading_pair, amount,
+                                                      price, order_id))
+        except asyncio.CancelledError:
+            raise
+        except Exception as e:
+            self.stop_tracking_order(order_id)
+            self.logger().network(
+                f"Error submitting {trade_type.name} order to Balancer for "
+                f"{amount} {trading_pair} "
+                f"{price}.",
+                exc_info=True,
+                app_warning_msg=str(e)
+            )
+            self.trigger_event(MarketEvent.OrderFailure,
+                               MarketOrderFailureEvent(self.current_timestamp, order_id, OrderType.LIMIT))
+
+    def start_tracking_order(self,
+                             order_id: str,
+                             exchange_order_id: str,
+                             trading_pair: str,
+                             trade_type: TradeType,
+                             price: Decimal,
+                             amount: Decimal):
+        """
+        Starts tracking an order by simply adding it into _in_flight_orders dictionary.
+        """
+        self._in_flight_orders[order_id] = InFlightOrderBase(
+            client_order_id=order_id,
+            exchange_order_id=exchange_order_id,
+            trading_pair=trading_pair,
+            order_type=OrderType.LIMIT,
+            trade_type=trade_type,
+            price=price,
+            amount=amount
+        )
+
+    def stop_tracking_order(self, order_id: str):
+        """
+        Stops tracking an order by simply removing it from _in_flight_orders dictionary.
+        """
+        if order_id in self._in_flight_orders:
+            del self._in_flight_orders[order_id]
 
     def get_taker_order_type(self):
         return OrderType.LIMIT
@@ -79,5 +173,84 @@ class BalancerConnector(ConnectorBase):
     def ready(self):
         return True
 
+    async def start_network(self):
+        if self._trading_required:
+            self._status_polling_task = safe_ensure_future(self._status_polling_loop())
+
     async def check_network(self) -> NetworkStatus:
         return NetworkStatus.CONNECTED
+
+    async def _status_polling_loop(self):
+        while True:
+            try:
+                self._poll_notifier = asyncio.Event()
+                await self._poll_notifier.wait()
+                await safe_gather(
+                    self._update_balances(),
+                    # self._update_order_status(),
+                )
+                self._last_poll_timestamp = self.current_timestamp
+            except asyncio.CancelledError:
+                raise
+
+    async def _update_balances(self):
+        """
+        Calls REST API to update total and available balances.
+        """
+        local_asset_names = set(self._account_balances.keys())
+        remote_asset_names = set()
+        resp_json = await self._api_request("get", "eth/balances")
+        for token, bal in resp_json["balances"].items():
+            # self._account_available_balances[token] = Decimal(str(bal))
+            self._account_balances[token] = Decimal(str(bal))
+            remote_asset_names.add(token)
+
+        asset_names_to_remove = local_asset_names.difference(remote_asset_names)
+        for asset_name in asset_names_to_remove:
+            del self._account_available_balances[asset_name]
+            del self._account_balances[asset_name]
+
+    async def _http_client(self) -> aiohttp.ClientSession:
+        """
+        :returns Shared client session instance
+        """
+        if self._shared_client is None:
+            self._shared_client = aiohttp.ClientSession()
+        return self._shared_client
+
+    async def _api_request(self,
+                           method: str,
+                           path_url: str,
+                           params: Dict[str, Any] = {}) -> Dict[str, Any]:
+        """
+        Sends an aiohttp request and waits for a response.
+        :param method: The HTTP method, e.g. get or post
+        :param path_url: The path url or the API end point
+        :param is_auth_required: Whether an authentication is required, when True the function will add encrypted
+        signature to the request.
+        :returns A response in json format.
+        """
+        url = f"{GATEWAY_API_URL}/{path_url}"
+        client = await self._http_client()
+        if method == "get":
+            response = await client.get(url)
+        elif method == "post":
+            post_json = json.dumps(params)
+            response = await client.post(url, data=post_json)
+        else:
+            raise NotImplementedError
+
+        try:
+            parsed_response = json.loads(await response.text())
+        except Exception as e:
+            raise IOError(f"Error parsing data from {url}. Error: {str(e)}")
+        if response.status != 200:
+            raise IOError(f"Error fetching data from {url}. HTTP status is {response.status}. "
+                          f"Message: {parsed_response}")
+        print(f"REQUEST: {method} {path_url} {params}")
+        print(f"RESPONSE: {parsed_response}")
+        return parsed_response
+
+    def _get_requests(self, path_url: str, params: Dict[str, Any]):
+        resp = requests.get(url=f"{GATEWAY_API_URL}/{path_url}", params=params)
+        return resp.json()
