4d9e09d93d36f3d9c67d33eececf82ca84b0419e
coco <coco@outlook.com>
2020-10-20 21:44:36 +0800

    quick_start_x.sh has been added

---
+++ quick_start_buy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd ~
+ROOT=$(pwd)
+cd -
+BASEDIR=$(dirname "$0")
+
+export CONDAPATH=$ROOT"/miniconda3"
+export PYTHON=${CONDAPATH}"/envs/hummingbot/bin/python3"
+
+cd $BASEDIR
+${PYTHON} bin/hummingbot_quickstart.py -s pure_market_making -f conf_pop_usdt_buy.yml -p 12345678
+cd -
