0d43fd7989073f60bdd9976718377efed5db05a4
Nullably <37262506+Nullably@users.noreply.github.com>
2020-10-21 23:29:35 +0800

    (feat) create a new db for each strategy config

--- hummingbot/model/sql_connection_manager.py
+++ hummingbot/model/sql_connection_manager.py
@@ -60,9 +60,9 @@ class SQLConnectionManager:
         return get_declarative_base()
 
     @classmethod
-    def get_trade_fills_instance(cls) -> "SQLConnectionManager":
+    def get_trade_fills_instance(cls, db_name: Optional[str] = None) -> "SQLConnectionManager":
         if cls._scm_trade_fills_instance is None:
-            cls._scm_trade_fills_instance = SQLConnectionManager(SQLConnectionType.TRADE_FILLS)
+            cls._scm_trade_fills_instance = SQLConnectionManager(SQLConnectionType.TRADE_FILLS, db_name=db_name)
         return cls._scm_trade_fills_instance
 
     @classmethod
@@ -88,9 +88,13 @@ class SQLConnectionManager:
 
     def __init__(self,
                  connection_type: SQLConnectionType,
-                 db_path: Optional[str] = None):
+                 db_path: Optional[str] = None,
+                 db_name: Optional[str] = None):
         if db_path is None:
-            db_path = join(data_path(), "hummingbot_trades.sqlite")
+            if db_name is None:
+                db_path = join(data_path(), "hummingbot_trades.sqlite")
+            else:
+                db_path = join(data_path(), f"{db_name}.sqlite")
 
         engine_options = {
             "db_engine": global_config_map.get("db_engine").value,
