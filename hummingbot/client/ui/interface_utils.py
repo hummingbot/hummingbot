c88371f5597029a04da205da2a553925f4289313
vic-en <victoreni14@gmail.com>
2020-10-29 16:51:23 +0100

    (feat) remove double % in value displayed for "Return %"

--- hummingbot/client/ui/interface_utils.py
+++ hummingbot/client/ui/interface_utils.py
@@ -69,11 +69,10 @@ async def start_trade_monitor(trade_monitor):
                         cur_price = await get_last_price(market.replace("_PaperTrade", ""), symbol)
                         perf = calculate_performance_metrics(symbol, cur_trades, cur_balances, cur_price)
                         return_pcts.append(perf.return_pct)
-
                         pnls.append(perf.total_pnl)
                     avg_return = sum(return_pcts) / len(return_pcts) if len(return_pcts) > 0 else s_decimal_0
                     total_pnls = sum(pnls)  # Note that this sum doesn't handles cases with different multiple pairs for simplisity
-                    trade_monitor.log(f"Trades: {total_trades}, Total P&L: {smart_round(total_pnls)} {quote_asset}, Return %: {avg_return:.2%}%")
+                    trade_monitor.log(f"Trades: {total_trades}, Total P&L: {smart_round(total_pnls)} {quote_asset}, Return %: {avg_return:.2%}")
                     return_pcts.clear()
                     pnls.clear()
         await asyncio.sleep(2)  # sleeping for longer to manage resources
