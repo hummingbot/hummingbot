6d7fd16caba7e692e3933519c4ea4a4680b30caf
Nullably <37262506+Nullably@users.noreply.github.com>
2020-11-09 14:20:10 +0800

    (fix) fix history keeps reporting, and fix gas not logged

--- hummingbot/core/utils/kill_switch.py
+++ hummingbot/core/utils/kill_switch.py
@@ -41,7 +41,7 @@ class KillSwitch:
                         self._hummingbot_application._notify(f"\n[Kill switch triggered]\n"
                                                              f"Current profitability "
                                                              f"is {self._profitability}. Stopping the bot...")
-                        self._hummingbot_application.trade_performance_report()
+                        self._hummingbot_application.history()
                         self._hummingbot_application.stop()
                         break
 
