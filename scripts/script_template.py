8101377dba251ba763ce62a23d54e115e83f86f1
Paulo <phbrcc@gmail.com>
2020-11-12 17:14:14 -0300

    /scripts/inv_skew_using_spread_script.py

--- scripts/script_template.py
+++ scripts/script_template.py
@@ -18,7 +18,7 @@ def log_to_file(file_name, message):  # log writing function
     with open(file_name, "a+") as f:
         f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " - " + message + "\n")
 
-#
+"""
 # Using the log function:
 #
 #       log_to_file(SCRIPT_LOG_FILE, #message you want to write on the log#)
@@ -29,13 +29,24 @@ def log_to_file(file_name, message):  # log writing function
 #             log_to_file(SCRIPT_LOG_FILE, #message you want to write on the log#)
 #             self.last_stats_logged = time.time()
 # ----------
-
+"""
 
 class Scriptname(ScriptBase):
-    # ----------
-    # This is the main script object. All the actions you want to be performed by the script
-    # must be created inside this class
-
+    """
+    ----------
+    This is the main script object. All the actions you want to be performed by the script
+    must be created inside this class
+    ----------
+    Some useful Functions that can be directly called from the parent class:
+    -------------------
+    self.mid_price - Returns the current mid price, calculated every tick
+    -----------------
+    self.base_asset, self.quote_asset = self.pmm_market_info.trading_pair.split("-")  # save base and quote ticker
+    self.base_balance = self.all_total_balances[f"{self.pmm_market_info.exchange}"].get(self.base_asset, self.base_balance)
+    self.quote_balance = self.all_total_balances[f"{self.pmm_market_info.exchange}"].get(self.quote_asset, self.quote_balance)
+    These functions will call the current pair that is being traded and its balances
+    ----------
+    """
     def __init__(self):
         super().__init__()
         # The init function can be used to declare the variables that will be used only by the script
