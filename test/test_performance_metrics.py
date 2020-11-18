0db32d51d962a32034add59bd54b6dd14e84b3c2
Nullably <37262506+Nullably@users.noreply.github.com>
2020-11-19 00:17:59 +0800

    (feat) add realised and unrealised profit calculation and simple unit test

---
+++ test/test_performance_metrics.py
@@ -0,0 +1,30 @@
+from os.path import join, realpath
+import sys; sys.path.insert(0, realpath(join(__file__, "../../")))
+from decimal import Decimal
+from typing import List
+import unittest
+
+from hummingbot.client.performance import calculate_performance_metrics
+from hummingbot.core.data_type.trade import Trade, TradeType, TradeFee
+
+trading_pair = "HBOT-USDT"
+base, quote = trading_pair.split("-")
+
+
+class PerformanceMetricsUnitTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_calculate_performance_metrics(self):
+        trades: List[Trade] = [
+            Trade(trading_pair, TradeType.BUY, 100, 10, None, trading_pair, 1, TradeFee(0.0, [(quote, 0)])),
+            Trade(trading_pair, TradeType.SELL, 120, 15, None, trading_pair, 1, TradeFee(0.0, [(quote, 0)]))
+        ]
+        cur_bals = {base: 100, quote: 10000}
+        cur_price = 110
+        metrics = calculate_performance_metrics(trading_pair, trades, cur_bals, cur_price)
+        self.assertEqual(Decimal("250"), metrics.trade_pnl)
+        self.assertEqual(Decimal("200"), metrics.realised_pnl)
+        self.assertEqual(Decimal("50"), metrics.unrealised_pnl)
+        print(metrics)
