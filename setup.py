163b629b6e16f39ed785060e03ca49ba9b5d5bb7
michaelc9 <43826836+michaelc9@users.noreply.github.com>
2020-12-07 22:57:59 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if is_posix:
 
 def main():
     cpu_count = os.cpu_count() or 8
-    version = "20201123.ob_msg_refactor"
+    version = "20201123"
     packages = [
         "hummingbot",
         "hummingbot.client",
