dfdb959298db9b6f50fb2fdfdd4c98858fcc1650
Nullably <37262506+Nullably@users.noreply.github.com>
2020-12-16 16:37:20 +0800

    (fix) add a quote-eth uniswap fetching loop

--- hummingbot/strategy/amm_arb/data_types.py
+++ hummingbot/strategy/amm_arb/data_types.py
@@ -46,7 +46,8 @@ class ArbProposal:
         self.first_side: ArbProposalSide = first_side
         self.second_side: ArbProposalSide = second_side
 
-    def profit_pct(self, account_for_fee: bool = False) -> Decimal:
+    def profit_pct(self, account_for_fee: bool = False, first_side_quote_eth_rate: Decimal = None,
+                   second_side_quote_eth_rate: Decimal = None) -> Decimal:
         """
         Returns a profit in percentage value (e.g. 0.01 for 1% profitability)
         """
@@ -58,9 +59,18 @@ class ArbProposal:
             return (sell.quote_price - buy.quote_price) / buy.quote_price
         buy_trade_fee = estimate_fee(buy.market_info.market.name, False)
         sell_trade_fee = estimate_fee(sell.market_info.market.name, False)
-        buy_fee_amount = buy_trade_fee.fee_amount_in_quote(buy.market_info.trading_pair, buy.quote_price, buy.amount)
-        sell_fee_amount = sell_trade_fee.fee_amount_in_quote(sell.market_info.trading_pair, sell.quote_price,
-                                                             sell.amount)
+        buy_quote_eth_rate = first_side_quote_eth_rate if self.first_side.is_buy else second_side_quote_eth_rate
+        sell_quote_eth_rate = first_side_quote_eth_rate if not self.first_side.is_buy else second_side_quote_eth_rate
+        if buy_quote_eth_rate is not None and buy_trade_fee.flat_fees[0][0].upper() == "ETH":
+            buy_fee_amount = buy_trade_fee.flat_fees[0][1] / buy_quote_eth_rate
+        else:
+            buy_fee_amount = buy_trade_fee.fee_amount_in_quote(buy.market_info.trading_pair,
+                                                               buy.quote_price, buy.amount)
+        if sell_quote_eth_rate is not None and sell_trade_fee.flat_fees[0][0].upper() == "ETH":
+            sell_fee_amount = sell_trade_fee.flat_fees[0][1] / sell_quote_eth_rate
+        else:
+            sell_fee_amount = sell_trade_fee.fee_amount_in_quote(sell.market_info.trading_pair, sell.quote_price,
+                                                                 sell.amount)
         sell_gained_net = (sell.amount * sell.quote_price) - sell_fee_amount
         buy_spent_net = (buy.amount * buy.quote_price) + buy_fee_amount
         return (sell_gained_net - buy_spent_net) / buy_spent_net if buy_spent_net != s_decimal_0 else s_decimal_nan
