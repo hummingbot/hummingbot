74cbc572c32afcd3262d2540c13872f37e45ba41
Daniel Tan <danieltanjiawang@gmail.com>
2021-01-15 18:42:50 +0800

    (feat) implement K2OrderBookAPIDataSource [WIP]

---
+++ hummingbot/connector/exchange/k2/k2_constants.py
@@ -0,0 +1,9 @@
+# A single source of truth for constant variables related to the exchange
+
+EXCHANGE_NAME = "k2"
+REST_URL = "http://10.16.132.132:8182/v1"
+WSS_URL = "ws://10.16.132.132:8184/v2"
+
+WSS_MY_TRADES = "SubscribeMyTrades"
+WSS_ORDER_BOOK = "SubscribeOrderBook"
+WSS_TRADES = "SubscribeTrades"
