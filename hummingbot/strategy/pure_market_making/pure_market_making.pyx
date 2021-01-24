74a0253c41ae48d75eff89d1e37ce4b3c002a87b
ladismrkolj <ladismrkolj@gmail.com>
2021-01-24 15:57:14 +0100

    fiexd fee

--- hummingbot/strategy/pure_market_making/pure_market_making.pyx
+++ hummingbot/strategy/pure_market_making/pure_market_making.pyx
@@ -555,14 +555,12 @@ cdef class PureMarketMakingStrategy(StrategyBase):
         for idx in range(0, len(active_orders)):
             order = active_orders[idx]
             if order.is_buy:
-                fee = market.get_fee(self.base_asset, self.quote_asset,
-                        self._limit_order_type, TradeType.BUY, order.quantity, order.price)
+                float fee = 0.001
                 unclosed_value_quote -= (float(order.quantity) * float(order.price))
-                unclosed_value_base += float(order.quantity) * (Decimal(1) - fee.percent)
+                unclosed_value_base += float(order.quantity) * (Decimal(1) - fee)
             else:
-                fee = market.get_fee(self.base_asset, self.quote_asset,
-                        self._limit_order_type, TradeType.SELL, order.quantity, order.price)
-                unclosed_value_quote += (float(order.quantity) * float(order.price)) * (Decimal(1) - fee.percent)
+                float fee = 0.001
+                unclosed_value_quote += (float(order.quantity) * float(order.price)) * (Decimal(1) - fee)
                 unclosed_value_base -= float(order.quantity)
 
         quote_unclosed_value = float(unclosed_value_quote) + (float(unclosed_value_base) * float(price))
