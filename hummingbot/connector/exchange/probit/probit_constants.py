71831439724128690ab0720eef32906a3f7c24d4
Daniel Tan <danieltanjiawang@gmail.com>
2021-02-15 20:39:58 +0800

    (add) add ProbitOrderBookTracker[WIP]

--- hummingbot/connector/exchange/probit/probit_constants.py
+++ hummingbot/connector/exchange/probit/probit_constants.py
@@ -5,9 +5,26 @@ EXCHANGE_NAME = "probit"
 REST_URL = "https://api.probit.com/api/exchange/"
 WSS_URL = "wss://api.probit.com/api/exchange/v1/ws"
 
-API_VERSON = "v1"
+REST_API_VERSON = "v1"
 
-TICKER_PATH_URL = f"{REST_URL+API_VERSON}/ticker"
-MARKETS_PATH_URL = f"{REST_URL+API_VERSON}/market"
-ORDER_BOOK_PATH_URL = f"{REST_URL+API_VERSON}/order_book"
-NEW_ORDER_PATH_URL = f"{REST_URL+API_VERSON}/new_order"
+# REST API Public Endpoints
+TICKER_URL = f"{REST_URL+REST_API_VERSON}/ticker"
+MARKETS_URL = f"{REST_URL+REST_API_VERSON}/market"
+ORDER_BOOK_URL = f"{REST_URL+REST_API_VERSON}/order_book"
+NEW_ORDER_URL = f"{REST_URL+REST_API_VERSON}/new_order"
+
+# REST API Private Endpoints
+NEW_ORDER_URL = f"{REST_URL+REST_API_VERSON}/new_order"
+CANCEL_ORDER_URL = f"{REST_URL+REST_API_VERSON}/cancel_order"
+ORDER_HISTORY_URL = f"{REST_URL+REST_API_VERSON}/order_history"
+TRADE_HISTORY_URL = f"{REST_URL+REST_API_VERSON}/trade_history"
+BALANCE_URL = f"{REST_URL+REST_API_VERSON}/balance"
+ORDER_URL = f"{REST_URL+REST_API_VERSON}/order"
+OPEN_ORDER_URL = f"{REST_URL+REST_API_VERSON}/open_order"
+
+# Order Status Definitions
+ORDER_STATUS = [
+    "open",
+    "filled",
+    "cancelled",
+]
--- hummingbot/connector/exchange/probit/probit_utils.py
+++ hummingbot/connector/exchange/probit/probit_utils.py
@@ -1,7 +1,18 @@
+#!/usr/bin/env python
+
 from typing import (
     Any,
     Dict,
+    List,
+    Tuple
 )
+
+from hummingbot.core.utils.tracking_nonce import get_tracking_nonce
+from hummingbot.core.data_type.order_book_row import OrderBookRow
+from hummingbot.core.data_type.order_book_message import OrderBookMessage
+
+from hummingbot.client.config.config_var import ConfigVar
+from hummingbot.client.config.config_methods import using_exchange
 
 CENTRALIZED = True
 
@@ -10,9 +21,57 @@ EXAMPLE_PAIR = "ETH-USDT"
 DEFAULT_FEES = [0.2, 0.2]
 
 
-def convert_snapshot_message_to_order_book_row(message: Dict[str, Any]):
-    pass
+def get_new_client_order_id(is_buy: bool, trading_pair: str) -> str:
+    side = "B" if is_buy else "S"
+    return f"{side}-{trading_pair}-{get_tracking_nonce()}"
 
 
-def convert_diff_message_to_order_book_row(message: Dict[str, Any]):
-    pass
+def convert_snapshot_message_to_order_book_row(message: OrderBookMessage) -> Tuple[List[OrderBookRow], List[OrderBookRow]]:
+    update_id = message.update_id
+    data = []
+    if "data" in message.content:  # From REST API
+        data: List[Dict[str, Any]] = message.content["data"]
+    elif "order_books" in message.content:  # From Websocket API
+        data: List[Dict[str, Any]] = message.content["order_books"]
+    bids, asks = [], []
+
+    for entry in data:
+        order_row = OrderBookRow(entry["price"], entry["quantity"], update_id)
+        if entry["side"] == "buy":
+            bids.append(order_row)
+        else:  # entry["type"] == "Sell":
+            asks.append(order_row)
+
+    return bids, asks
+
+
+def convert_diff_message_to_order_book_row(message: OrderBookMessage) -> Tuple[List[OrderBookRow], List[OrderBookRow]]:
+    update_id = message.update_id
+    data = message.content["order_books"]
+    bids = []
+    asks = []
+
+    for entry in data:
+        order_row = OrderBookRow(entry["price"], entry["quantity"], update_id)
+        if entry["side"] == "buy":
+            bids.append(order_row)
+        elif entry["side"] == "sell":
+            asks.append(order_row)
+
+    return bids, asks
+
+
+KEYS = {
+    "probit_api_key":
+        ConfigVar(key="probit_api_key",
+                  prompt="Enter your ProBit API key >>> ",
+                  required_if=using_exchange("probit"),
+                  is_secure=True,
+                  is_connect_key=True),
+    "probit_secret_key":
+        ConfigVar(key="probit_secret_key",
+                  prompt="Enter your ProBit secret key >>> ",
+                  required_if=using_exchange("probit"),
+                  is_secure=True,
+                  is_connect_key=True),
+}
