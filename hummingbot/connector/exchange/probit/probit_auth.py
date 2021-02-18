8e2bbad0191af04b5488148910f39967dad1c2a7
Daniel Tan <danieltanjiawang@gmail.com>
2021-02-18 14:55:31 +0800

    (refactor) ProbitAuth module and other Exchange methods

--- hummingbot/connector/exchange/probit/probit_auth.py
+++ hummingbot/connector/exchange/probit/probit_auth.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 
-import aiohttp
 import base64
 import time
-import ujson
-
-import hummingbot.connector.exchange.probit.probit_constants as constants
 
 from typing import Dict, Any
 
@@ -20,49 +16,32 @@ class ProbitAuth():
         self.secret_key: str = secret_key
         self._oauth_token: str = None
         self._oauth_token_expiration_time: int = -1
-        self._http_client: aiohttp.ClientSession = aiohttp.ClientSession()
 
-    def _token_has_expired(self):
+    @property
+    def oauth_token(self):
+        return self._oauth_token
+
+    @property
+    def token_payload(self):
+        payload = f"{self.api_key}:{self.secret_key}".encode()
+        return base64.b64encode(payload).decode()
+
+    def token_has_expired(self):
         now: int = int(time.time())
         return now >= self._oauth_token_expiration_time
 
-    def _update_expiration_time(self, expiration_time: int):
+    def update_oauth_token(self, new_token: str):
+        self._oauth_token = new_token
+
+    def update_expiration_time(self, expiration_time: int):
         self._oauth_token_expiration_time = expiration_time
-
-    async def _generate_oauth_token(self) -> str:
-        try:
-            now: int = int(time.time())
-            headers: Dict[str, Any] = self.get_headers()
-            payload = f"{self.api_key}:{self.secret_key}".encode()
-            b64_payload = base64.b64encode(payload).decode()
-            headers.update({
-                "Authorization": f"Basic {b64_payload}"
-            })
-            body = ujson.dumps({
-                "grant_type": "client_credentials"
-            })
-            resp = await self._http_client.post(url=constants.TOKEN_URL,
-                                                headers=headers,
-                                                data=body)
-            token_resp = await resp.json()
-
-            if resp.status != 200:
-                raise ValueError(f"{__name__}: Error occurred retrieving new OAuth Token. Response: {token_resp}")
-
-            # POST /token endpoint returns both access_token and expires_in
-            # Updates _oauth_token_expiration_time
-
-            self._update_expiration_time(now + token_resp["expires_in"])
-            return token_resp["access_token"]
-        except Exception as e:
-            raise e
 
     async def get_oauth_token(self) -> str:
         if self._oauth_token is None or self._token_has_expired():
-            self._oauth_token = await self._generate_oauth_token()
+            self._oauth_token = await self.generate_oauth_token()
         return self._oauth_token
 
-    async def generate_auth_dict(self):
+    def generate_auth_dict(self):
         """
         Generates authentication signature and return it in a dictionary along with other inputs
         :return: a dictionary of request info including the request signature
@@ -70,9 +49,8 @@ class ProbitAuth():
 
         headers = self.get_headers()
 
-        access_token = await self.get_oauth_token()
         headers.update({
-            "Authorization": f"Bearer {access_token}"
+            "Authorization": f"Bearer {self._oauth_token}"
         })
 
         return headers
--- hummingbot/connector/exchange/probit/probit_constants.py
+++ hummingbot/connector/exchange/probit/probit_constants.py
@@ -8,6 +8,7 @@ WSS_URL = "wss://api.probit.com/api/exchange/v1/ws"
 REST_API_VERSON = "v1"
 
 # REST API Public Endpoints
+TIME_URL = f"{REST_URL+REST_API_VERSON}/time"
 TICKER_URL = f"{REST_URL+REST_API_VERSON}/ticker"
 MARKETS_URL = f"{REST_URL+REST_API_VERSON}/market"
 ORDER_BOOK_URL = f"{REST_URL+REST_API_VERSON}/order_book"
