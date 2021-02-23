1a65bd38ae23e2988c6ed746dfcc04964e9adcdc
Nicolas Baum <nicolas@hummingbot.io>
2021-02-23 19:21:41 -0300

    (fix) Fixed mapping of Kraken trading pairs

--- hummingbot/connector/exchange/kraken/kraken_utils.py
+++ hummingbot/connector/exchange/kraken/kraken_utils.py
@@ -18,52 +18,40 @@ def split_trading_pair(trading_pair: str) -> Tuple[str, str]:
     return tuple(convert_from_exchange_trading_pair(trading_pair).split("-"))
 
 
-def clean_symbol(symbol: str) -> str:
-    if len(symbol) == 4 and symbol[0] == "X" or symbol[0] == "Z":
-        symbol = symbol[1:]
-    if symbol == "XBT":
-        symbol = "BTC"
-    return symbol
-
-
 def convert_from_exchange_symbol(symbol: str) -> str:
-    if (len(symbol) == 4 or len(symbol) == 6) and (symbol[0] == "X" or symbol[0] == "Z"):
+    # Assuming if starts with Z or X and has 4 letters then Z/X is removable
+    if (symbol[0] == "X" or symbol[0] == "Z") and len(symbol) == 4:
         symbol = symbol[1:]
-    if symbol == "XBT":
-        symbol = "BTC"
-    return symbol
+    return constants.KRAKEN_TO_HB_MAP.get(symbol, symbol)
 
 
 def convert_to_exchange_symbol(symbol: str) -> str:
-    if symbol == "BTC":
-        symbol = "XBT"
-    return symbol
+    inverted_kraken_to_hb_map = {v: k for k, v in constants.KRAKEN_TO_HB_MAP.items()}
+    return inverted_kraken_to_hb_map.get(symbol, symbol)
 
 
 def split_to_base_quote(exchange_trading_pair: str) -> (Optional[str], Optional[str]):
-    base, quote = None, None
-    for quote_asset in constants.QUOTES:
-        if quote_asset == exchange_trading_pair[-len(quote_asset):]:
-            if len(exchange_trading_pair[:-len(quote_asset)]) > 2 or exchange_trading_pair[:-len(quote_asset)] == "SC":
-                base, quote = exchange_trading_pair[:-len(quote_asset)], exchange_trading_pair[-len(quote_asset):]
-                break
-    if not base:
-        quote_asset_d = [quote + ".d" for quote in constants.QUOTES]
-        for quote_asset in quote_asset_d:
-            if quote_asset == exchange_trading_pair[-len(quote_asset):]:
-                base, quote = exchange_trading_pair[:-len(quote_asset)], exchange_trading_pair[-len(quote_asset):]
-                break
+    base, quote = exchange_trading_pair.split("-")
     return base, quote
 
 
-def convert_from_exchange_trading_pair(exchange_trading_pair: str) -> Optional[str]:
+def convert_from_exchange_trading_pair(exchange_trading_pair: str, available_trading_pairs: Optional[Tuple] = None) -> Optional[str]:
     base, quote = "", ""
     if "-" in exchange_trading_pair:
-        return exchange_trading_pair
-    if "/" in exchange_trading_pair:
-        base, quote = exchange_trading_pair.split("/")
-    else:
         base, quote = split_to_base_quote(exchange_trading_pair)
+    elif "/" in exchange_trading_pair:
+        base, quote = exchange_trading_pair.split("/")
+    elif len(available_trading_pairs) > 0:
+        # If trading pair has no spaces (i.e. ETHUSDT). Then it will have to match with the existing pairs
+        # Option 1: Using traditional naming convention
+        connector_trading_pair = {''.join(convert_from_exchange_trading_pair(tp).split('-')): tp for tp in available_trading_pairs}.get(
+            exchange_trading_pair)
+        if not connector_trading_pair:
+            # Option 2: Using kraken naming convention ( XXBT for Bitcoin, XXDG for Doge, ZUSD for USD, etc)
+            connector_trading_pair = {''.join(tp.split('-')): tp for tp in available_trading_pairs}.get(
+                exchange_trading_pair)
+        return connector_trading_pair
+
     if not base or not quote:
         return None
     base = convert_from_exchange_symbol(base)
@@ -76,7 +64,6 @@ def convert_to_exchange_trading_pair(hb_trading_pair: str, delimiter: str = "") 
     Note: The result of this method can safely be used to submit/make queries.
     Result shouldn't be used to parse responses as Kraken add special formating to most pairs.
     """
-
     if "-" in hb_trading_pair:
         base, quote = hb_trading_pair.split("-")
     elif "/" in hb_trading_pair:
