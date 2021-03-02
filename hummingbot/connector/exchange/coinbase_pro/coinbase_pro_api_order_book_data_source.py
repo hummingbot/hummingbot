eb658c6f61942b77d9b64cd1cf41eca7c975d916
bmorenus <58372262+bmorenus@users.noreply.github.com>
2021-03-02 10:30:43 -0500

    removed Coinbase changes part ii

--- hummingbot/connector/exchange/coinbase_pro/coinbase_pro_api_order_book_data_source.py
+++ hummingbot/connector/exchange/coinbase_pro/coinbase_pro_api_order_book_data_source.py
@@ -113,8 +113,8 @@ class CoinbaseProAPIOrderBookDataSource(OrderBookTrackerDataSource):
     async def get_tracking_pairs(self) -> Dict[str, OrderBookTrackerEntry]:
         """
         *required
-        Initializes order books and order book trackers for the list of trading pairs 
-        returned by `self.fetch_trading_pairs`
+        Initializes order books and order book trackers for the list of trading pairs
+        returned by `self.get_trading_pairs`
         :returns: A dictionary of order book trackers for each trading pair
         """
         # Get the currently active markets
