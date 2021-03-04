d851d12f3f3da8ccd0d51c1ca36255af352517b4
Jay Bell <zappra@outlook.com>
2021-03-04 18:24:37 +0000

    (feat) catch syntax and module import errors when starting scripts

--- hummingbot/script/script_process.py
+++ hummingbot/script/script_process.py
@@ -5,20 +5,24 @@ import os
 
 from multiprocessing import Queue
 from hummingbot.script.script_base import ScriptBase
-from hummingbot.script.script_interface import set_child_queue
+from hummingbot.script.script_interface import set_child_queue, CallNotify
 
 
 def run_script(script_file_name: str, parent_queue: Queue, child_queue: Queue, queue_check_interval: float):
-    script_class = import_script_sub_class(script_file_name)
-    script = script_class()
-    script.assign_init(parent_queue, child_queue, queue_check_interval)
-    set_child_queue(child_queue)
-    policy = asyncio.get_event_loop_policy()
-    policy.set_event_loop(policy.new_event_loop())
-    ev_loop = asyncio.get_event_loop()
-    ev_loop.create_task(script.run())
-    ev_loop.run_forever()
-    ev_loop.close()
+    try:
+        script_class = import_script_sub_class(script_file_name)
+        script = script_class()
+        script.assign_init(parent_queue, child_queue, queue_check_interval)
+        set_child_queue(child_queue)
+        policy = asyncio.get_event_loop_policy()
+        policy.set_event_loop(policy.new_event_loop())
+        ev_loop = asyncio.get_event_loop()
+        ev_loop.create_task(script.run())
+        ev_loop.run_forever()
+        ev_loop.close()
+    except Exception as ex:
+        child_queue.put(CallNotify(f'Failed to start script {script_file_name}:'))
+        child_queue.put(CallNotify(f'{ex}'))
 
 
 def import_script_sub_class(script_file_name: str):
