6ee898e5580ebaab0c9b334746fa634d89f08aa1
SGoh <51857968+sdgoh@users.noreply.github.com>
2021-03-31 14:09:06 +0800

    Update hummingbot/strategy/amm_arb/data_types.py
    Co-authored-by: vic-en <31972210+vic-en@users.noreply.github.com>

--- hummingbot/strategy/amm_arb/data_types.py
+++ hummingbot/strategy/amm_arb/data_types.py
@@ -79,9 +79,7 @@ class ArbProposal:
 
         sell_gained_net = (sell.amount * sell.quote_price) - sell_fee_amount
         buy_spent_net = (buy.amount * buy.quote_price) + buy_fee_amount
-        profit_percentage = ((sell_gained_net - buy_spent_net) / buy_spent_net) if buy_spent_net != s_decimal_0 else s_decimal_0
-
-        return profit_percentage
+        return ((sell_gained_net - buy_spent_net) / buy_spent_net) if buy_spent_net != s_decimal_0 else s_decimal_0
 
     def __repr__(self):
         return f"First Side - {self.first_side}\nSecond Side - {self.second_side}"
