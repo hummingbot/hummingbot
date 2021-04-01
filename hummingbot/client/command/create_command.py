a87eaa01f557b8cd4b0685ee26d2a83a7a248895
Nullably <37262506+Nullably@users.noreply.github.com>
2021-04-01 16:15:08 +0800

    (feat) update oracle settings on trading pair configs

--- hummingbot/client/command/create_command.py
+++ hummingbot/client/command/create_command.py
@@ -108,9 +108,11 @@ class CreateCommand:
 
         if self.app.to_stop_config:
             return
+        config.value = parse_cvar_value(config, input_value)
         err_msg = await config.validate(input_value)
         if err_msg is not None:
             self._notify(err_msg)
+            config.value = None
             await self.prompt_a_config(config)
         else:
             config.value = parse_cvar_value(config, input_value)
