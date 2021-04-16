74785039ae90055bdeec29188818e9a14fc16bfb
Nicolas Baum <nicolas@hummingbot.io>
2021-04-16 01:12:10 -0300

    Added updates of spreads used to calculate parameters when volatility is high

--- hummingbot/strategy/avellaneda_market_making/avellaneda_market_making.pyx
+++ hummingbot/strategy/avellaneda_market_making/avellaneda_market_making.pyx
@@ -554,8 +554,8 @@ cdef class AvellanedaMarketMakingStrategy(StrategyBase):
         price=self.get_price()
 
         if q != 0:
-            min_spread = self._min_spread * price
-            max_spread = self._max_spread * price
+            min_spread = max(self._min_spread * price, self._vol_to_spread_multiplier * vol)
+            max_spread = min_spread + (self._max_spread - self._min_spread) * price
 
             # GAMMA
             # If q or vol are close to 0, gamma will -> Inf. Is this desirable?
