a4c5d17ac16f1c9f6591fcfc5c54f99a6da91da5
Nullably <37262506+Nullably@users.noreply.github.com>
2021-04-26 16:48:45 +0800

    (Feat) add basic starting files

---
+++ hummingbot/strategy/uniswap_v3_market_making/uniswap_v3_market_making.py
@@ -0,0 +1,23 @@
+from decimal import Decimal
+import asyncio
+from hummingbot.strategy.market_trading_pair_tuple import MarketTradingPairTuple
+from hummingbot.strategy.strategy_py_base import StrategyPyBase
+
+
+class UniswapV3MarketMakingStrategy(StrategyPyBase):
+
+    def __init__(self,
+                 market_info: MarketTradingPairTuple,
+                 range_order_quote_amount: Decimal,
+                 range_order_spread: Decimal,
+                 status_report_interval: float = 900):
+        super().__init__()
+        self._market_info = market_info
+        self._range_order_quote_amount = range_order_quote_amount
+        self._range_order_spread = range_order_spread
+
+        self._ev_loop = asyncio.get_event_loop()
+
+        self._last_timestamp = 0
+        self._status_report_interval = status_report_interval
+        self.add_markets([market_info.market])
