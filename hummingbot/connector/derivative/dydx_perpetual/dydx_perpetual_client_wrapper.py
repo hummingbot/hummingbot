993e563f44e99a42ccdcdfcacc5e86c53c90d434
Nicolas Baum <nicolas@hummingbot.io>
2021-05-02 19:17:55 -0300

    Added trigger_price for MARKET type orders in dydx perpetual

--- hummingbot/connector/derivative/dydx_perpetual/dydx_perpetual_client_wrapper.py
+++ hummingbot/connector/derivative/dydx_perpetual/dydx_perpetual_client_wrapper.py
@@ -42,17 +42,20 @@ class DydxPerpetualClientWrapper:
         if side == 'SELL':
             dydx_client_id += 1
 
+        trigger_price = price if order_type == "MARKET" else None
+
         f = self._loop.run_in_executor(None, partial(self.client.private.create_order,
-                                                     position_id = account['account']['positionId'],
-                                                     market = market,
-                                                     side = side,
-                                                     size = amount,
-                                                     price = price,
-                                                     order_type = order_type,
-                                                     post_only = postOnly,
-                                                     client_id = str(dydx_client_id),
-                                                     limit_fee = limit_fee,
-                                                     expiration_epoch_seconds = expiration))
+                                                     position_id=account['account']['positionId'],
+                                                     market=market,
+                                                     side=side,
+                                                     size=amount,
+                                                     price=price,
+                                                     order_type=order_type,
+                                                     post_only=postOnly,
+                                                     client_id=str(dydx_client_id),
+                                                     limit_fee=limit_fee,
+                                                     expiration_epoch_seconds=expiration,
+                                                     trigger_price=trigger_price))
         return await f
 
     async def cancel_order(self, exchange_order_id):
