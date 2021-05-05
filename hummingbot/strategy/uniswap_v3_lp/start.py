a26abfb367c5ba74d888a4ba8b78d44d1f87d164
vic-en <victoreni14@gmail.com>
2021-05-05 23:30:57 +0100

    (feat) add main logic for v3 strategy

--- hummingbot/strategy/uniswap_v3_lp/start.py
+++ hummingbot/strategy/uniswap_v3_lp/start.py
@@ -6,16 +6,23 @@ from hummingbot.strategy.uniswap_v3_lp.uniswap_v3_lp import UniswapV3LpStrategy
 
 def start(self):
     pair = c_map.get("market").value
-    upper_price_bound = c_map.get("upper_price_bound").value
-    lower_price_bound = c_map.get("lower_price_bound").value
-    boundaries_margin = c_map.get("boundaries_margin").value / Decimal("100")
-    token = c_map.get("token").value
+    fee_tier = c_map.get("fee_tier").value
+    buy_position_spread = c_map.get("buy_position_spread").value / Decimal("100")
+    sell_position_spread = c_map.get("sell_position_spread").value / Decimal("100")
+    buy_position_price_spread = c_map.get("buy_position_price_spread").value / Decimal("100")
+    sell_position_price_spread = c_map.get("sell_position_price_spread").value / Decimal("100")
     token_amount = c_map.get("token_amount").value
 
     self._initialize_markets([("uniswap_v3", [pair])])
     base, quote = pair.split("-")
-    self.assets = {base, quote}
+    self.assets = set([base, quote])
 
     market_info = MarketTradingPairTuple(self.markets["uniswap_v3"], pair, base, quote)
-    self.strategy = UniswapV3LpStrategy(market_info, upper_price_bound, lower_price_bound, boundaries_margin,
-                                        token, token_amount)
+    self.market_trading_pair_tuples = [market_info]
+    self.strategy = UniswapV3LpStrategy(market_info,
+                                        fee_tier,
+                                        buy_position_spread,
+                                        sell_position_spread,
+                                        buy_position_price_spread,
+                                        sell_position_price_spread,
+                                        token_amount)
