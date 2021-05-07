108d6641c88708b897a8347d278b5cd1277e8a64
abel <abarmoa+github@gmail.com>
2021-05-07 17:28:34 -0300

    Fix to bug causing pairs with 3 letters quotes being parsed as pairs with 4 letters quotes, taking one letter from the base

---
+++ test/connector/derivative/binance_perpetual/test_binance_perpetual_utils.py
@@ -0,0 +1,32 @@
+import unittest
+import hummingbot.connector.derivative.binance_perpetual.binance_perpetual_utils as utils
+
+
+class TradingPairUtilsTest(unittest.TestCase):
+    def testParseThreeLettersBaseAndThreeLettersQuote(self):
+        parsed_pair = utils.convert_from_exchange_trading_pair("BTCUSD")
+        self.assertEqual(parsed_pair, "BTC-USD")
+
+    def testParseThreeLettersBaseAndFourLettersQuote(self):
+        parsed_pair = utils.convert_from_exchange_trading_pair("BTCUSDT")
+        self.assertEqual(parsed_pair, "BTC-USDT")
+
+    def testParseThreeLettersBaseAndThreeLettersfQuoteMatchingWithAFourLettersQuoteCandidate(self):
+        parsed_pair = utils.convert_from_exchange_trading_pair("VETUSD")
+        self.assertEqual(parsed_pair, "VET-USD")
+
+    def testConvertToExchangeFormatThreeLettersBaseAndThreeLettersQuote(self):
+        converted_pair = utils.convert_to_exchange_trading_pair("BTC-USD")
+        self.assertEqual(converted_pair, "BTCUSD")
+
+    def testConvertToExchangeFormatThreeLettersBaseAndFourLettersfQuote(self):
+        converted_pair = utils.convert_to_exchange_trading_pair("BTC-USDT")
+        self.assertEqual(converted_pair, "BTCUSDT")
+
+    def testConvertToExchangeFormatThreeLettersBaseAndThreeLettersfQuoteMatchingWithAFourLettersQuoteCandidate(self):
+        converted_pair = utils.convert_to_exchange_trading_pair("VET-USD")
+        self.assertEqual(converted_pair, "VETUSD")
+
+
+if __name__ == '__main__':
+    unittest.main()
