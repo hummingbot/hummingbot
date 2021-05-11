74a875e9c476e97b55c326c437688ecd79e877a2
Daniel Tan <daniel@hummingbot.io>
2021-05-11 20:46:18 +0800

    (refactor) refactor dev_0_hello_world to inherit StrategyPyBase

---
+++ hummingbot/strategy/dev_0_hello_world/dev_0_hello_world.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+import logging
+
+from typing import (
+    Dict,
+)
+
+from hummingbot.connector.exchange_base import ExchangeBase
+from hummingbot.logger import HummingbotLogger
+from hummingbot.strategy.market_trading_pair_tuple import MarketTradingPairTuple
+from hummingbot.strategy.strategy_py_base import StrategyPyBase
+
+hws_logger = None
+
+
+class HelloWorldStrategy(StrategyPyBase):
+    """
+
+    """
+
+    @classmethod
+    def logger(cls) -> HummingbotLogger:
+        global hws_logger
+        if hws_logger is None:
+            hws_logger = logging.getLogger(__name__)
+        return hws_logger
+
+    def __init__(self,
+                 exchange: ExchangeBase,
+                 market_infos: Dict[str, MarketTradingPairTuple],
+                 trading_pair: str,
+                 asset: str):
+
+        super().__init__()
+        self._exchange = exchange
+        self._market_infos = market_infos
+        self._asset = asset
+        self._ready = False
+
+        self.add_markets([self._exchange])
+
+    def notify_hb_app(self, msg: str):
+        """
+        Method called to display message on the Output Panel(upper left)
+        """
+        from hummingbot.client.hummingbot_application import HummingbotApplication
+        HummingbotApplication.main_application()._notify(msg)
+
+    async def format_status(self) -> str:
+        """
+        Method called by the `status` command. Generates the status report for this strategy.
+        Simply outputs the balance of the specified asset on the exchange.
+        """
+        if not self._ready:
+            return "Exchange connector(s) are not ready."
+        lines = []
+
+        for market_info in self._market_infos.values():
+            lines.extend(["", "  Assets:"] + ["    " + str(self._asset) + "    " +
+                                              str(market_info.market.get_balance(self._asset))])
+
+        return "\n".join(lines)
+
+    def tick(self, timestamp: float):
+        """
+        Clock tick entry point, it runs every second (on normal tick settings)
+        : param timestamp: current tick timestamp
+        """
+        if self._ready:
+            return
+
+        if not self._ready and not self._exchange.ready:
+            # Message output using self.logger() will be displayed on Log panel(right) and saved on the strategy's log file.
+            self.logger().warning(f"{self._exchange.name} is not ready. Please wait...")
+        else:
+            self._ready = True
+            # Message output using self.notify_hb_app(...) will be displayed on the Output panel(upper left) and not saved on the strategy's log file.
+            self.notify_hb_app(f"{self._exchange.name} is ready!")
+
+        return
