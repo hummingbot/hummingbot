730e819b6dd1e40d323ef35817e145dbc1aa7a41
james-hummingbot <james@hummingbot.io>
2021-05-12 12:11:29 +0200

    Experiment with setup that reduces compile time by removing optimizations (should only be used for testing)

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if is_posix:
     if "Darwin" in os_name:
         os.environ["CFLAGS"] = "-stdlib=libc++ -std=c++11"
     else:
-        os.environ["CFLAGS"] = "-std=c++11"
+        os.environ["CFLAGS"] = "-std=c++11 -O0"
 
 
 # Avoid a gcc warning below:
@@ -131,7 +131,7 @@ def main():
         "attrs",
         "certifi",
         "chardet",
-        "cython==0.29.15",
+        "cython==0.29.23",
         "idna",
         "idna_ssl",
         "multidict",
@@ -148,8 +148,12 @@ def main():
     cython_kwargs = {
         "language": "c++",
         "language_level": 3,
-    }
 
+    }
+    compiler_directives = {
+        "optimize.use_switch": False,
+        "optimize.unpack_method_calls": False,
+    }
     if is_posix:
         cython_kwargs["nthreads"] = cpu_count
 
@@ -173,7 +177,7 @@ def main():
           packages=packages,
           package_data=package_data,
           install_requires=install_requires,
-          ext_modules=cythonize(["hummingbot/**/*.pyx"], **cython_kwargs),
+          ext_modules=cythonize(["hummingbot/**/*.pyx"], compiler_directives=compiler_directives, **cython_kwargs, annotate=False),
           include_dirs=[
               np.get_include()
           ],
