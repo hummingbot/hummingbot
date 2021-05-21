4b301f1b031bec92bce6ad72292b7e60825d23e7
Nicolas Baum <nicolas@hummingbot.io>
2021-05-21 11:14:06 -0300

    Thank you flake

--- test/hummingbot/core/utils/test_tracking_nonce.py
+++ test/hummingbot/core/utils/test_tracking_nonce.py
@@ -2,7 +2,9 @@ from unittest import TestCase
 import asyncio
 import hummingbot.core.utils.tracking_nonce as tracking_nonce
 
+
 class TrackingNonceTest(TestCase):
+
     global _last_tracking_nonce
     def test_get_tracking_nonce(self):
         nonce = tracking_nonce.get_tracking_nonce()
@@ -19,13 +21,6 @@ class TrackingNonceTest(TestCase):
     def test_get_concurrent_nonce_in_low_res(self):
         async def task():
             return tracking_nonce.get_tracking_nonce_low_res()
-        tasks = [task(), task()]
-        ret = asyncio.get_event_loop().run_until_complete(asyncio.gather(*tasks))
-        self.assertGreaterEqual(ret[1], ret[0])
-
-    def test_get_concurrent_nonce_in_high_res(self):
-        async def task():
-            return tracking_nonce.get_tracking_nonce()
         tasks = [task(), task()]
         ret = asyncio.get_event_loop().run_until_complete(asyncio.gather(*tasks))
         self.assertGreaterEqual(ret[1], ret[0])
