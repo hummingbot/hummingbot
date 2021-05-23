b0f073d8be1d6e1161fe880d27214e9ed2b36633
Sean Wolfe <sean@iheartsquares.com>
2021-05-23 03:11:39 -0700

    Change to use an Aroon Oscillator for indicator. Initital implementation.

--- hummingbot/strategy/aroon_oscillator/aroon_oscillator_config_map.py
+++ hummingbot/strategy/aroon_oscillator/aroon_oscillator_config_map.py
@@ -23,7 +23,7 @@ from typing import Optional
 
 
 def maker_trading_pair_prompt():
-    exchange = williams_pct_r_config_map.get("exchange").value
+    exchange = aroon_oscillator_config_map.get("exchange").value
     example = EXAMPLE_PAIRS.get(exchange)
     return "Enter the token trading pair you would like to trade on %s%s >>> " \
            % (exchange, f" (e.g. {example})" if example else "")
@@ -31,13 +31,13 @@ def maker_trading_pair_prompt():
 
 # strategy specific validators
 def validate_exchange_trading_pair(value: str) -> Optional[str]:
-    exchange = williams_pct_r_config_map.get("exchange").value
+    exchange = aroon_oscillator_config_map.get("exchange").value
     return validate_market_trading_pair(exchange, value)
 
 
 async def order_amount_prompt() -> str:
-    exchange = williams_pct_r_config_map["exchange"].value
-    trading_pair = williams_pct_r_config_map["market"].value
+    exchange = aroon_oscillator_config_map["exchange"].value
+    trading_pair = aroon_oscillator_config_map["market"].value
     base_asset, quote_asset = trading_pair.split("-")
     min_amount = await minimum_order_amount(exchange, trading_pair)
     return f"What is the amount of {base_asset} per order? (minimum {min_amount}) >>> "
@@ -45,8 +45,8 @@ async def order_amount_prompt() -> str:
 
 async def validate_order_amount(value: str) -> Optional[str]:
     try:
-        exchange = williams_pct_r_config_map["exchange"].value
-        trading_pair = williams_pct_r_config_map["market"].value
+        exchange = aroon_oscillator_config_map["exchange"].value
+        trading_pair = aroon_oscillator_config_map["market"].value
         min_amount = await minimum_order_amount(exchange, trading_pair)
         if Decimal(value) < min_amount:
             return f"Order amount must be at least {min_amount}."
@@ -61,34 +61,13 @@ def validate_price_source(value: str) -> Optional[str]:
 
 def on_validate_price_source(value: str):
     if value != "external_market":
-        williams_pct_r_config_map["price_source_exchange"].value = None
-        williams_pct_r_config_map["price_source_market"].value = None
-        williams_pct_r_config_map["take_if_crossed"].value = None
+        aroon_oscillator_config_map["price_source_exchange"].value = None
+        aroon_oscillator_config_map["price_source_market"].value = None
+        aroon_oscillator_config_map["take_if_crossed"].value = None
     if value != "custom_api":
-        williams_pct_r_config_map["price_source_custom_api"].value = None
+        aroon_oscillator_config_map["price_source_custom_api"].value = None
     else:
-        williams_pct_r_config_map["price_type"].value = None
-
-
-def price_source_market_prompt() -> str:
-    external_market = williams_pct_r_config_map.get("price_source_exchange").value
-    return f'Enter the token trading pair on {external_market} >>> '
-
-
-def validate_price_source_exchange(value: str) -> Optional[str]:
-    if value == williams_pct_r_config_map.get("exchange").value:
-        return "Price source exchange cannot be the same as maker exchange."
-    return validate_exchange(value)
-
-
-def on_validated_price_source_exchange(value: str):
-    if value is None:
-        williams_pct_r_config_map["price_source_market"].value = None
-
-
-def validate_price_source_market(value: str) -> Optional[str]:
-    market = williams_pct_r_config_map.get("price_source_exchange").value
-    return validate_market_trading_pair(market, value)
+        aroon_oscillator_config_map["price_type"].value = None
 
 
 def validate_price_floor_ceiling(value: str) -> Optional[str]:
@@ -102,18 +81,18 @@ def validate_price_floor_ceiling(value: str) -> Optional[str]:
 
 def on_validated_price_type(value: str):
     if value == 'inventory_cost':
-        williams_pct_r_config_map["inventory_price"].value = None
+        aroon_oscillator_config_map["inventory_price"].value = None
 
 
 def exchange_on_validated(value: str):
     required_exchanges.append(value)
 
 
-williams_pct_r_config_map = {
+aroon_oscillator_config_map = {
     "strategy":
         ConfigVar(key="strategy",
                   prompt=None,
-                  default="williams_pct_r"),
+                  default="aroon_oscillator"),
     "exchange":
         ConfigVar(key="exchange",
                   prompt="Enter your maker spot connector >>> ",
@@ -125,27 +104,37 @@ williams_pct_r_config_map = {
                   prompt=maker_trading_pair_prompt,
                   validator=validate_exchange_trading_pair,
                   prompt_on_new=True),
-    "bid_spread":
-        ConfigVar(key="bid_spread",
-                  prompt="How far away from the mid price do you want to place the "
-                         "first bid order? (Enter 1 to indicate 1%) >>> ",
-                  type_str="decimal",
-                  validator=lambda v: validate_decimal(v, 0, 100, inclusive=False),
-                  prompt_on_new=True),
-    "ask_spread":
-        ConfigVar(key="ask_spread",
-                  prompt="How far away from the mid price do you want to place the "
-                         "first ask order? (Enter 1 to indicate 1%) >>> ",
-                  type_str="decimal",
-                  validator=lambda v: validate_decimal(v, 0, 100, inclusive=False),
-                  prompt_on_new=True),
     "minimum_spread":
         ConfigVar(key="minimum_spread",
-                  prompt="At what minimum spread should the bot automatically cancel orders? (Enter 1 for 1%) >>> ",
+                  prompt="What is the closest to the mid price should the bot automatically create orders for? (Enter 1 for 1%) >>> ",
                   required_if=lambda: False,
                   type_str="decimal",
-                  default=Decimal(-100),
-                  validator=lambda v: validate_decimal(v, -100, 100, True)),
+                  validator=lambda v: validate_decimal(v, 0, 100, True),
+                  prompt_on_new=True),
+    "maximum_spread":
+        ConfigVar(key="maximum_spread",
+                  prompt="What is the farthest away from the mid price do you want the bot automatically create orders for? "
+                         "(Enter 1 to indicate 1%) >>> ",
+                  type_str="decimal",
+                  validator=lambda v: validate_decimal(v, 0, 100, inclusive=False),
+                  prompt_on_new=True),
+    "period_length":
+        ConfigVar(key="period_length",
+                  prompt="How many time periods will be used to calculate the Aroon Oscillator? "
+                         "This indicator typically uses a timeframe of 25 periods however "
+                         "the timeframe is subjective. Use more periods to get fewer waves and "
+                         "smoother trend indicator. Use fewer periods to generate more waves "
+                         "and quicker turnarounds in the trend indicator. >>> ",
+                  type_str="int",
+                  validator=lambda v: validate_int(v, 1, 100, inclusive=True),
+                  default=25,
+                  prompt_on_new=True),
+    "period_duration":
+        ConfigVar(key="period_duration",
+                  prompt="How long in seconds are the Periods in the Aroon Oscillator? >>> ",
+                  type_str="int",
+                  validator=lambda v: validate_int(v, min_value=1, inclusive=True),
+                  default=60),
     "order_refresh_time":
         ConfigVar(key="order_refresh_time",
                   prompt="How often do you want to cancel and replace bids and asks "
@@ -191,13 +180,6 @@ williams_pct_r_config_map = {
                   type_str="decimal",
                   default=Decimal("-1"),
                   validator=validate_price_floor_ceiling),
-    "ping_pong_enabled":
-        ConfigVar(key="ping_pong_enabled",
-                  prompt="Would you like to use the ping pong feature and alternate between buy and sell orders after fills? (Yes/No) >>> ",
-                  type_str="bool",
-                  default=False,
-                  prompt_on_new=True,
-                  validator=validate_bool),
     "order_levels":
         ConfigVar(key="order_levels",
                   prompt="How many orders do you want to place on both sides? >>> ",
@@ -208,7 +190,7 @@ williams_pct_r_config_map = {
         ConfigVar(key="order_level_amount",
                   prompt="How much do you want to increase or decrease the order size for each "
                          "additional order? (decrease < 0 > increase) >>> ",
-                  required_if=lambda: williams_pct_r_config_map.get("order_levels").value > 1,
+                  required_if=lambda: aroon_oscillator_config_map.get("order_levels").value > 1,
                   type_str="decimal",
                   validator=lambda v: validate_decimal(v),
                   default=0),
@@ -216,7 +198,7 @@ williams_pct_r_config_map = {
         ConfigVar(key="order_level_spread",
                   prompt="Enter the price increments (as percentage) for subsequent "
                          "orders? (Enter 1 to indicate 1%) >>> ",
-                  required_if=lambda: williams_pct_r_config_map.get("order_levels").value > 1,
+                  required_if=lambda: aroon_oscillator_config_map.get("order_levels").value > 1,
                   type_str="decimal",
                   validator=lambda v: validate_decimal(v, 0, 100, inclusive=False),
                   default=Decimal("1")),
@@ -229,7 +211,7 @@ williams_pct_r_config_map = {
     "inventory_target_base_pct":
         ConfigVar(key="inventory_target_base_pct",
                   prompt="What is your target base asset percentage? Enter 50 for 50% >>> ",
-                  required_if=lambda: williams_pct_r_config_map.get("inventory_skew_enabled").value,
+                  required_if=lambda: aroon_oscillator_config_map.get("inventory_skew_enabled").value,
                   type_str="decimal",
                   validator=lambda v: validate_decimal(v, 0, 100),
                   default=Decimal("50")),
@@ -237,7 +219,7 @@ williams_pct_r_config_map = {
         ConfigVar(key="inventory_range_multiplier",
                   prompt="What is your tolerable range of inventory around the target, "
                          "expressed in multiples of your total order size? ",
-                  required_if=lambda: williams_pct_r_config_map.get("inventory_skew_enabled").value,
+                  required_if=lambda: aroon_oscillator_config_map.get("inventory_skew_enabled").value,
                   type_str="decimal",
                   validator=lambda v: validate_decimal(v, min_value=0, inclusive=False),
                   default=Decimal("1")),
@@ -246,7 +228,7 @@ williams_pct_r_config_map = {
                   prompt="What is the price of your base asset inventory? ",
                   type_str="decimal",
                   validator=lambda v: validate_decimal(v, min_value=Decimal("0"), inclusive=True),
-                  required_if=lambda: williams_pct_r_config_map.get("price_type").value == "inventory_cost",
+                  required_if=lambda: aroon_oscillator_config_map.get("price_type").value == "inventory_cost",
                   default=Decimal("1"),
                   ),
     "filled_order_delay":
@@ -266,7 +248,7 @@ williams_pct_r_config_map = {
         ConfigVar(key="hanging_orders_cancel_pct",
                   prompt="At what spread percentage (from mid price) will hanging orders be canceled? "
                          "(Enter 1 to indicate 1%) >>> ",
-                  required_if=lambda: williams_pct_r_config_map.get("hanging_orders_enabled").value,
+                  required_if=lambda: aroon_oscillator_config_map.get("hanging_orders_enabled").value,
                   type_str="decimal",
                   default=Decimal("10"),
                   validator=lambda v: validate_decimal(v, 0, 100, inclusive=False)),
@@ -281,7 +263,7 @@ williams_pct_r_config_map = {
                   prompt="How deep do you want to go into the order book for calculating "
                          "the top ask, ignoring dust orders on the top "
                          "(expressed in base asset amount)? >>> ",
-                  required_if=lambda: williams_pct_r_config_map.get("order_optimization_enabled").value,
+                  required_if=lambda: aroon_oscillator_config_map.get("order_optimization_enabled").value,
                   type_str="decimal",
                   validator=lambda v: validate_decimal(v, min_value=0),
                   default=0),
@@ -290,7 +272,7 @@ williams_pct_r_config_map = {
                   prompt="How deep do you want to go into the order book for calculating "
                          "the top bid, ignoring dust orders on the top "
                          "(expressed in base asset amount)? >>> ",
-                  required_if=lambda: williams_pct_r_config_map.get("order_optimization_enabled").value,
+                  required_if=lambda: aroon_oscillator_config_map.get("order_optimization_enabled").value,
                   type_str="decimal",
                   validator=lambda v: validate_decimal(v, min_value=0),
                   default=0),
@@ -300,19 +282,12 @@ williams_pct_r_config_map = {
                   type_str="bool",
                   default=False,
                   validator=validate_bool),
-    "price_source":
-        ConfigVar(key="price_source",
-                  prompt="Which price source to use? (current_market/external_market/custom_api) >>> ",
-                  type_str="str",
-                  default="current_market",
-                  validator=validate_price_source,
-                  on_validated=on_validate_price_source),
     "price_type":
         ConfigVar(key="price_type",
                   prompt="Which price type to use? ("
                          "mid_price/last_price/last_own_trade_price/best_bid/best_ask/inventory_cost) >>> ",
                   type_str="str",
-                  required_if=lambda: williams_pct_r_config_map.get("price_source").value != "custom_api",
+                  required_if=lambda: aroon_oscillator_config_map.get("price_source").value != "custom_api",
                   default="mid_price",
                   on_validated=on_validated_price_type,
                   validator=lambda s: None if s in {"mid_price",
@@ -323,30 +298,17 @@ williams_pct_r_config_map = {
                                                     "inventory_cost",
                                                     } else
                   "Invalid price type."),
-    "price_source_exchange":
-        ConfigVar(key="price_source_exchange",
-                  prompt="Enter external price source exchange name >>> ",
-                  required_if=lambda: williams_pct_r_config_map.get("price_source").value == "external_market",
-                  type_str="str",
-                  validator=validate_price_source_exchange,
-                  on_validated=on_validated_price_source_exchange),
-    "price_source_market":
-        ConfigVar(key="price_source_market",
-                  prompt=price_source_market_prompt,
-                  required_if=lambda: williams_pct_r_config_map.get("price_source").value == "external_market",
-                  type_str="str",
-                  validator=validate_price_source_market),
     "take_if_crossed":
         ConfigVar(key="take_if_crossed",
                   prompt="Do you want to take the best order if orders cross the orderbook? ((Yes/No) >>> ",
-                  required_if=lambda: williams_pct_r_config_map.get(
+                  required_if=lambda: aroon_oscillator_config_map.get(
                       "price_source").value == "external_market",
                   type_str="bool",
                   validator=validate_bool),
     "price_source_custom_api":
         ConfigVar(key="price_source_custom_api",
                   prompt="Enter pricing API URL >>> ",
-                  required_if=lambda: williams_pct_r_config_map.get("price_source").value == "custom_api",
+                  required_if=lambda: aroon_oscillator_config_map.get("price_source").value == "custom_api",
                   type_str="str"),
     "order_override":
         ConfigVar(key="order_override",
