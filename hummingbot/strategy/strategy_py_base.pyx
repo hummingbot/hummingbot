4efd4b94c18b9f34193afb5a251e9d746a54e196
Daniel Tan <daniel@hummingbot.io>
2021-05-29 15:33:36 +0800

    (feat) add better type hinting to StrategyPyBase and StrategyBase

--- hummingbot/strategy/strategy_py_base.pyx
+++ hummingbot/strategy/strategy_py_base.pyx
@@ -1,11 +1,22 @@
 from typing import List
+
 from hummingbot.strategy.strategy_base cimport StrategyBase
 from hummingbot.connector.connector_base import ConnectorBase
 from hummingbot.core.clock import Clock
 from hummingbot.core.clock cimport Clock
+from hummingbot.core.event.events import (
+    BuyOrderCreatedEvent,
+    SellOrderCreatedEvent,
+    OrderFilledEvent,
+    MarketOrderFailureEvent,
+    OrderCancelledEvent,
+    OrderExpiredEvent,
+    BuyOrderCompletedEvent,
+    SellOrderCompletedEvent,
+    FundingPaymentCompletedEvent,
+)
 from hummingbot.strategy.market_trading_pair_tuple import MarketTradingPairTuple
-
-from .order_tracker import OrderTracker
+from hummingbot.strategy.order_tracker import OrderTracker
 
 cdef class StrategyPyBase(StrategyBase):
     def __init__(self):
@@ -35,53 +46,53 @@ cdef class StrategyPyBase(StrategyBase):
     cdef c_did_create_buy_order(self, object order_created_event):
         self.did_create_buy_order(order_created_event)
 
-    def did_create_buy_order(self, order_created_event):
+    def did_create_buy_order(self, order_created_event: BuyOrderCreatedEvent):
         pass
 
     cdef c_did_create_sell_order(self, object order_created_event):
         self.did_create_sell_order(order_created_event)
 
-    def did_create_sell_order(self, order_created_event):
+    def did_create_sell_order(self, order_created_event: SellOrderCreatedEvent):
         pass
 
     cdef c_did_fill_order(self, object order_filled_event):
         self.did_fill_order(order_filled_event)
 
-    def did_fill_order(self, order_filled_event):
+    def did_fill_order(self, order_filled_event: OrderFilledEvent):
         pass
 
     cdef c_did_fail_order(self, object order_failed_event):
         self.did_fail_order(order_failed_event)
 
-    def did_fail_order(self, order_failed_event):
+    def did_fail_order(self, order_failed_event: MarketOrderFailureEvent):
         pass
 
     cdef c_did_cancel_order(self, object cancelled_event):
         self.did_cancel_order(cancelled_event)
 
-    def did_cancel_order(self, cancelled_event):
+    def did_cancel_order(self, cancelled_event: OrderCancelledEvent):
         pass
 
     cdef c_did_expire_order(self, object expired_event):
         self.did_expire_order(expired_event)
 
-    def did_expire_order(self, expired_event):
+    def did_expire_order(self, expired_event: OrderExpiredEvent):
         pass
 
     cdef c_did_complete_buy_order(self, object order_completed_event):
         self.did_complete_buy_order(order_completed_event)
 
-    def did_complete_buy_order(self, order_completed_event):
+    def did_complete_buy_order(self, order_completed_event: BuyOrderCompletedEvent):
         pass
 
     cdef c_did_complete_sell_order(self, object order_completed_event):
         self.did_complete_sell_order(order_completed_event)
 
-    def did_complete_sell_order(self, order_completed_event):
+    def did_complete_sell_order(self, order_completed_event: SellOrderCompletedEvent):
         pass
 
     cdef c_did_complete_funding_payment(self, object funding_payment_completed_event):
         self.did_complete_funding_payment(funding_payment_completed_event)
 
-    def did_complete_funding_payment(self, funding_payment_completed_event):
+    def did_complete_funding_payment(self, funding_payment_completed_event: FundingPaymentCompletedEvent):
         pass
