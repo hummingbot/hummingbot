a693a532143362761e7dfee1303ebefb17395f1b
Daniel Tan <daniel@hummingbot.io>
2021-06-01 09:20:03 +0800

    (feat) add Avellaneda MM unit tests

---
+++ test/hummingbot/strategy/avellaneda_market_making/test_avellaneda_market_making.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+import unittest
+
+
+class AvellanedaMarketMakingUnitTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        return super().setUpClass()
+
+    def setUp(self):
+        return super().setUp()
+
+    def test_all_markets_ready(self):
+        pass
+
+    def test_market_info(self):
+        pass
+
+    def test_order_refresh_tolerance_pct(self):
+        pass
+
+    def test_order_amount(self):
+        pass
+
+    def test_inventory_target_base_oct(self):
+        pass
+
+    def test_order_optimization_enabled(self):
+        pass
+
+    def test_order_refresh_time(self):
+        pass
+
+    def test_filled_order_delay(self):
+        pass
+
+    def test_add_transaction_costs_to_orders(self):
+        pass
+
+    def test_base_asset(self):
+        pass
+
+    def test_quote_asset(self):
+        pass
+
+    def test_trading_pair(self):
+        pass
+
+    def test_get_price(self):
+        pass
+
+    def test_get_last_price(self):
+        pass
+
+    def test_get_mid_price(self):
+        pass
+
+    def test_market_info_to_active_orders(self):
+        pass
+
+    def test_active_orders(self):
+        pass
+
+    def test_active_buys(self):
+        pass
+
+    def test_active_sells(self):
+        pass
+
+    def test_logging_options(self):
+        pass
+
+    def test_order_tracker(self):
+        pass
+
+    def test_pure_mm_asset_df(self):
+        pass
+
+    def test_active_orders_df(self):
+        pass
+
+    def test_market_data_frame(self):
+        pass
+
+    def test_format_status(self):
+        pass
+
+    def test_execute_orders_proposal(self):
+        pass
+
+    def test_cancel_order(self):
+        pass
+
+    def test_volatility_diff_from_last_parameter_calculation(self):
+        pass
+
+    def test_get_spread(self):
+        pass
+
+    def test_get_volatility(self):
+        pass
+
+    def test_calculate_target_inventory(self):
+        pass
+
+    def test_get_min_and_max_spread(self):
+        pass
+
+    def test_recalculate_parameters(self):
+        pass
+
+    def test_is_algorithm_ready(self):
+        pass
