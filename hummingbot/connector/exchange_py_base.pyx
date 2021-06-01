5204ff9175ef437b105c744e237f87566c9dc0e0
Nullably <37262506+Nullably@users.noreply.github.com>
2021-06-01 14:39:13 +0800

    (fix) add unit testing on quantize_value function

--- hummingbot/connector/exchange_py_base.pyx
+++ hummingbot/connector/exchange_py_base.pyx
@@ -5,6 +5,9 @@ s_decimal_NaN = Decimal("NaN")
 
 
 cdef class ExchangePyBase(ExchangeBase):
+    @staticmethod
+    def quantize_value(value: Decimal, quantum: Decimal) -> Decimal:
+        return round(value / quantum) * quantum
 
     cdef object c_quantize_order_price(self, str trading_pair, object price):
         return self.quantize_order_price(trading_pair, price)
@@ -13,11 +16,11 @@ cdef class ExchangePyBase(ExchangeBase):
         if price.is_nan():
             return price
         price_quantum = self.c_get_order_price_quantum(trading_pair, price)
-        return round(price / price_quantum) * price_quantum
+        return ExchangePyBase.quantize_value(price, price_quantum)
 
     cdef object c_quantize_order_amount(self, str trading_pair, object amount, object price=s_decimal_NaN):
         return self.quantize_order_amount(trading_pair, amount)
 
     def quantize_order_amount(self, trading_pair: str, amount: Decimal) -> Decimal:
         order_size_quantum = self.c_get_order_size_quantum(trading_pair, amount)
-        return (amount // order_size_quantum) * order_size_quantum
+        return ExchangePyBase.quantize_value(amount, order_size_quantum)
