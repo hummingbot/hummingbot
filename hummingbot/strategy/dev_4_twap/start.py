a5c81e145ad41f3c94f80cd899185e6f42eace16
aarmoa <abarmoa+github@gmail.com>
2021-06-09 18:02:20 -0300

    Change TWAP strategy to work using a target amount and order size instead of target amount and number of orders

--- hummingbot/strategy/dev_4_twap/start.py
+++ hummingbot/strategy/dev_4_twap/start.py
@@ -12,19 +12,14 @@ from hummingbot.strategy.dev_4_twap.dev_4_twap_config_map import dev_4_twap_conf
 
 def start(self):
     try:
-        order_amount = dev_4_twap_config_map.get("order_amount").value
-        order_type = dev_4_twap_config_map.get("order_type").value
-        is_buy = dev_4_twap_config_map.get("is_buy").value
-        time_delay = dev_4_twap_config_map.get("time_delay").value
-        num_individual_orders = dev_4_twap_config_map.get("num_individual_orders").value
+        order_step_size = dev_4_twap_config_map.get("order_step_size").value
+        trade_side = dev_4_twap_config_map.get("trade_side").value
+        target_asset_amount = dev_4_twap_config_map.get("target_asset_amount").value
+        order_delay_time = dev_4_twap_config_map.get("order_delay_time").value
         exchange = dev_4_twap_config_map.get("exchange").value.lower()
-        raw_market_trading_pair = dev_4_twap_config_map.get("market").value
-        order_price = None
-        cancel_order_wait_time = None
-
-        if order_type == "limit":
-            order_price = dev_4_twap_config_map.get("order_price").value
-            cancel_order_wait_time = dev_4_twap_config_map.get("cancel_order_wait_time").value
+        raw_market_trading_pair = dev_4_twap_config_map.get("trading_pair").value
+        order_price = dev_4_twap_config_map.get("order_price").value
+        cancel_order_wait_time = dev_4_twap_config_map.get("cancel_order_wait_time").value
 
         try:
             assets: Tuple[str, str] = self._initialize_market_assets(exchange, [raw_market_trading_pair])[0]
@@ -41,14 +36,15 @@ def start(self):
         maker_data = [self.markets[exchange], raw_market_trading_pair] + list(assets)
         self.market_trading_pair_tuples = [MarketTradingPairTuple(*maker_data)]
 
+        is_buy = trade_side == "buy"
+
         self.strategy = Dev4TwapTradeStrategy(market_infos=[MarketTradingPairTuple(*maker_data)],
-                                              order_type=order_type,
-                                              order_price=order_price,
-                                              cancel_order_wait_time=cancel_order_wait_time,
                                               is_buy=is_buy,
-                                              time_delay=time_delay,
-                                              num_individual_orders=num_individual_orders,
-                                              order_amount=order_amount)
+                                              target_asset_amount=target_asset_amount,
+                                              order_step_size=order_step_size,
+                                              order_price=order_price,
+                                              order_delay_time=order_delay_time,
+                                              cancel_order_wait_time=cancel_order_wait_time)
     except Exception as e:
         self._notify(str(e))
         self.logger().error("Unknown error during initialization.", exc_info=True)
