edfef415a28bba5b17aef7f2276f67277e3256c0
Petio Petrov <petioptrv@icloud.com>
2021-06-09 09:45:14 -0400

    (cleanup) Removes unused event attribute.

--- test/hummingbot/core/test_pubsub.py
+++ test/hummingbot/core/test_pubsub.py
@@ -15,8 +15,7 @@ class PubSubTest(unittest.TestCase):
         self.listener_one = MockEventListener()
         self.event_tag_zero = MockEventType.EVENT_ZERO
         self.event_tag_one = MockEventType.EVENT_ONE
-        self.event_c = MockEvent(payload=100)
-        self.event_cc = MockEvent(payload=200)
+        self.event = MockEvent(payload=1)
 
     def test_get_listeners_no_listeners(self):
         listeners_count = len(self.pubsub.get_listeners(self.event_tag_zero))
@@ -64,9 +63,9 @@ class PubSubTest(unittest.TestCase):
     def test_trigger_event(self):
         self.pubsub.add_listener(self.event_tag_zero, self.listener_zero)
         self.pubsub.add_listener(self.event_tag_one, self.listener_one)
-        self.pubsub.trigger_event(self.event_tag_zero, self.event_c)
+        self.pubsub.trigger_event(self.event_tag_zero, self.event)
         self.assertEqual(1, self.listener_zero.events_count)
-        self.assertEqual(self.event_c, self.listener_zero.last_event)
+        self.assertEqual(self.event, self.listener_zero.last_event)
         self.assertEqual(0, self.listener_one.events_count)
 
     def test_lapsed_listener_remove_on_get_listeners(self):
