a3edd091025ebd2ead9d2864296fa011398d274d
aarmoa <abarmoa+github@gmail.com>
2021-06-09 18:02:20 -0300

    Change TWAP strategy to work using a target amount and order size instead of target amount and number of orders

--- hummingbot/strategy/dev_4_twap/dev_4_twap_config_map.py
+++ hummingbot/strategy/dev_4_twap/dev_4_twap_config_map.py
@@ -17,6 +17,15 @@ def trading_pair_prompt():
            % (exchange, f" (e.g. {example})" if example else "")
 
 
+def target_asset_amount_prompt():
+    trading_pair = dev_4_twap_config_map.get("trading_pair").value
+    is_buy = dev_4_twap_config_map.get("is_buy").value
+    base_token, quote_token = trading_pair.split("-")
+    target_asset = quote_token if is_buy else base_token
+
+    return f"What is the total amount of {target_asset} to be traded? >>> "
+
+
 def str2bool(value: str):
     return str(value).lower() in ("yes", "true", "t", "1")
 
@@ -38,45 +47,43 @@ dev_4_twap_config_map = {
                   validator=validate_exchange,
                   on_validated=lambda value: required_exchanges.append(value),
                   prompt_on_new=True),
-    "market":
-        ConfigVar(key="market",
+    "trading_pair":
+        ConfigVar(key="trading_pair",
                   prompt=trading_pair_prompt,
                   validator=validate_market_trading_pair_tuple,
                   prompt_on_new=True),
-    "order_type":
-        ConfigVar(key="order_type",
-                  prompt="Enter type of order (limit/market) default is market >>> ",
+    "trade_side":
+        ConfigVar(key="trade_side",
+                  prompt="What operation will be executed? (buy/sell) >>> ",
                   type_str="str",
-                  validator=lambda v: None if v in {"limit", "market", ""} else "Invalid order type.",
-                  default="market"),
-    "order_amount":
+                  validator=lambda v: None if v in {"buy", "sell", ""} else "Invalid operation type.",
+                  default="buy",
+                  prompt_on_new=True),
+    "target_asset_amount":
+        ConfigVar(key="target_asset_amount",
+                  prompt=target_asset_amount_prompt,
+                  default=1.0,
+                  type_str="decimal",
+                  prompt_on_new=True),
+    "order_step_size":
         ConfigVar(key="order_amount",
-                  prompt="What is your preferred quantity (denominated in the base asset, default is 1)? "
+                  prompt="What is the amount of each individual order (denominated in the base asset, default is 1)? "
                          ">>> ",
                   default=1.0,
-                  type_str="float"),
-    "is_buy":
-        ConfigVar(key="is_buy",
-                  prompt="Enter True for Buy order and False for Sell order (default is Buy Order) >>> ",
-                  type_str="bool",
-                  default=True),
-    "num_individual_orders":
-        ConfigVar(key="num_individual_orders",
-                  prompt="Into how many individual orders do you want to split this order? (Enter 10 to indicate 10 individual orders. "
-                         "Default is 1)? >>> ",
-                  type_str="float",
-                  default=1),
-    "time_delay":
-        ConfigVar(key="time_delay",
+                  type_str="decimal",
+                  prompt_on_new=True),
+    "order_price":
+        ConfigVar(key="order_price",
+                  prompt="What is the price for the limit orders? >>> ",
+                  type_str="decimal",
+                  prompt_on_new=True),
+    "order_delay_time":
+        ConfigVar(key="order_delay_time",
                   prompt="How many seconds do you want to wait between each individual order? (Enter 10 to indicate 10 seconds. "
                          "Default is 10)? >>> ",
                   type_str="float",
-                  default=10),
-    "order_price":
-        ConfigVar(key="order_price",
-                  prompt="What is the price of the limit order ? >>> ",
-                  required_if=lambda: dev_4_twap_config_map.get("order_type").value == "limit",
-                  type_str="float"),
+                  default=10,
+                  prompt_on_new=True),
     "cancel_order_wait_time":
         ConfigVar(key="cancel_order_wait_time",
                   prompt="How long do you want to wait before cancelling your limit order (in seconds). "
