a49768ffa7075228efff1e179341ba09306f29e7
TheHolyRoger <theholyroger@theholyroger.com>
2021-06-11 09:09:08 +0100

    Gate.io - Increase throttler again

--- hummingbot/connector/exchange/gate_io/gate_io_utils.py
+++ hummingbot/connector/exchange/gate_io/gate_io_utils.py
@@ -21,7 +21,7 @@ EXAMPLE_PAIR = "BTC-USDT"
 
 DEFAULT_FEES = [0.2, 0.2]
 
-REQUEST_THROTTLER = Throttler(rate_limit = (6.0, 4.0))
+REQUEST_THROTTLER = Throttler(rate_limit = (12.0, 10.0))
 
 
 class GateIoAPIError(IOError):
