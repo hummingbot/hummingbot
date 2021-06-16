96034cabf63a5c52b10f8dad118d19aa63a6f5c3
aarmoa <abarmoa+github@gmail.com>
2021-06-15 21:52:16 -0300

    Add prints to debug the issue

--- test/hummingbot/core/mock_api/test_mock_web_socket_server.py
+++ test/hummingbot/core/mock_api/test_mock_web_socket_server.py
@@ -10,11 +10,13 @@ class MockWebSocketServerFactoryTest(unittest.TestCase):
     def setUpClass(cls) -> None:
         cls.ev_loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
         cls.ws_server = MockWebSocketServerFactory.start_new_server("wss://www.google.com/ws/")
+        print(f"1-- {cls.ws_server}\n{cls.ws_server.websocket}")
         cls._patcher = unittest.mock.patch("websockets.connect", autospec=True)
         cls._mock = cls._patcher.start()
         cls._mock.side_effect = MockWebSocketServerFactory.reroute_ws_connect
         # need to wait a bit for the server to be available
         cls.ev_loop.run_until_complete(asyncio.wait_for(cls.ws_server.wait_til_started(), 1))
+        print(f"5-- {cls.ws_server}\n{cls.ws_server.websocket}")
 
     @classmethod
     def tearDownClass(cls) -> None:
@@ -23,11 +25,13 @@ class MockWebSocketServerFactoryTest(unittest.TestCase):
 
     async def _test_web_socket(self):
         uri = "wss://www.google.com/ws/"
-
+        print(f"6-- {self.ws_server}\n{self.ws_server.websocket}")
         # Retry up to 3 times if there is any error connecting to the mock server address and port
         for retry_attempt in range(1, 4):
             try:
                 async with websockets.connect(uri) as websocket:
+                    print(f"7-- {self.ws_server}\n{self.ws_server.websocket}")
+                    print(f"2-- {websocket}")
                     await MockWebSocketServerFactory.send_str(uri, "aaa")
                     answer = await websocket.recv()
                     print(answer)
