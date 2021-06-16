639ec24ae02d9a8765e4f76c2ceb170bfcf57b3f
Dennis Ocaña <50150287+dennisocana@users.noreply.github.com>
2021-06-16 16:48:41 +0800

    (release) version 0.40.0 release date

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ class BuildExt(build_ext):
 
 def main():
     cpu_count = os.cpu_count() or 8
-    version = "20210518"
+    version = "20210616"
     packages = [
         "hummingbot",
         "hummingbot.client",
