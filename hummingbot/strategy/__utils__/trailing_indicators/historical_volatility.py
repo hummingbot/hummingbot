dea6eeedb722c4f3c81ebe314ecd7951461eb82d
Nicolas Baum <nicolas@hummingbot.io>
2021-06-25 16:47:36 -0300

    Added Historical Volatility indicator. Renamed Average volatility to Instant Volatility Indicator (changed in avellaneda strategy)

--- hummingbot/strategy/__utils__/trailing_indicators/historical_volatility.py
+++ hummingbot/strategy/__utils__/trailing_indicators/historical_volatility.py
@@ -2,13 +2,15 @@ from .base_trailing_indicator import BaseTrailingIndicator
 import numpy as np
 
 
-class AverageVolatilityIndicator(BaseTrailingIndicator):
+class HistoricalVolatilityIndicator(BaseTrailingIndicator):
     def __init__(self, sampling_length: int = 30, processing_length: int = 15):
         super().__init__(sampling_length, processing_length)
 
     def _indicator_calculation(self) -> float:
-        return np.var(self._sampling_buffer.get_as_numpy_array())
+        prices = self._sampling_buffer.get_as_numpy_array()
+        log_returns = np.diff(np.log(prices))
+        return np.var(log_returns)
 
     def _processing_calculation(self) -> float:
         processing_array = self._processing_buffer.get_as_numpy_array()
-        return np.sqrt(np.mean(processing_array))
+        return np.sqrt(np.mean(np.nan_to_num(processing_array)))
