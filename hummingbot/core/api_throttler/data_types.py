26c402ebb8d689d0f3a8b56e77081f7bede3f3b3
Daniel Tan <daniel@hummingbot.io>
2021-07-09 17:22:49 +0800

    (refactor) update data_types for API Throttler

--- hummingbot/core/api_throttler/data_types.py
+++ hummingbot/core/api_throttler/data_types.py
@@ -1,23 +1,25 @@
 from dataclasses import dataclass
 
-
-@dataclass
-class RateLimit():
-    limit: int
-    time_interval: float
-    path_url: str = ""
-    weight: int = 1
-
-
-@dataclass
-class TaskLog():
-    timestamp: float
-    path_url: str = ""
-    weight: int = 1
+DEFAULT_PATH = ""
+DEFAULT_WEIGHT = 1
 
 
 Limit = int             # Integer representing the no. of requests be time interval
 RequestPath = str       # String representing the request path url
 RequestWeight = int     # Integer representing the request weight of the path url
-
 Seconds = float
+
+
+@dataclass
+class RateLimit():
+    limit: Limit
+    time_interval: Seconds
+    path_url: RequestPath = DEFAULT_PATH
+    weight: RequestWeight = DEFAULT_WEIGHT
+
+
+@dataclass
+class TaskLog():
+    timestamp: float
+    path_url: RequestPath = DEFAULT_PATH
+    weight: RequestWeight = DEFAULT_WEIGHT
