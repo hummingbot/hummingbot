f8deb845f4f22528f46f3d8c2d07c94c57ad7bb6
Daniel Tan <daniel@hummingbot.io>
2021-07-15 14:10:49 +0800

    (feat) add NDAX OrderBookTracker & Data Source [WIP]

--- hummingbot/connector/exchange/ndax/ndax_constants.py
+++ hummingbot/connector/exchange/ndax/ndax_constants.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python
+
+EXCHANGE_NAME = "ndax"
+
+REST_URL = "https://api.ndax.io:8443/AP"
+WSS_URL = "wss://api.ndax.io/WSGateway/"
+TEST_WSS_URL = "wss://ndaxmarginstaging.cdnhop.net/WSGateway"
+
+# REST API Public Endpoints
+MARKETS_URL = f"{REST_URL}/GetInstruments"
+ORDER_BOOK_URL = f"{REST_URL}/GetL2Snapshot"
