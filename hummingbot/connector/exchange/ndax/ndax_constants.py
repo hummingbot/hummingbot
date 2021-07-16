23125ea6475bc7e5b91499e9dd6ea2a2fe95c941
aarmoa <abarmoa+github@gmail.com>
2021-07-16 09:47:23 -0300

    Rebase with epic/ndax_connector

--- hummingbot/connector/exchange/ndax/ndax_constants.py
+++ hummingbot/connector/exchange/ndax/ndax_constants.py
@@ -1,1 +1,16 @@
-#!/usr/bin/env python
+# A single source of truth for constant variables related to the exchange
+
+EXCHANGE_NAME = "ndax"
+
+# Production URLs
+# REST_URL = "https://api.ndax.io:8443/AP/"
+# WSS_URL = "wss://api.ndax.io/WSGateway"
+
+# Testing URLs
+REST_URL = "https://ndaxmarginstaging.cdnhop.net:8443/AP/"
+WSS_URL = "wss://ndaxmarginstaging.cdnhop.net/WSGateway"
+
+REST_API_VERSON = "v3.3"
+
+AUTHENTICATE_USER_ENDPOINT_NAME = "AuthenticateUser"
+SUBSCRIBE_ACCOUNT_EVENTS_ENDPOINT_NAME = "SubscribeAccountEvents"
