5eaf3d2af18ab54964d5b734e49f2c02ac741b00
aarmoa <abarmoa+github@gmail.com>
2021-07-19 12:34:25 -0300

    Change in payload processing for NDAX websocket responses (payload is not received as a serialized dictionary, but as a normal dictionary inside the response json)

--- test/hummingbot/connector/exchange/ndax/test_ndax_user_stream_tracker.py
+++ test/hummingbot/connector/exchange/ndax/test_ndax_user_stream_tracker.py
@@ -43,7 +43,7 @@ class NdaxUserStreamTrackerTests(TestCase):
                 "Use2FA": True}
         payload = {"Authenticated": authenticated,
                    "SessionToken": "74e7c5b0-26b1-4ca5-b852-79b796b0e599",
-                   "User": json.dumps(user),
+                   "User": user,
                    "Locked": False,
                    "Requires2FA": False,
                    "EnforceEnable2FA": False,
