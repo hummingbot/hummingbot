8acc63fe5c9628924c68ba48de2498f2642e81be
Nullably <37262506+Nullably@users.noreply.github.com>
2021-07-20 17:21:48 +0800

    (fix) remove mock web server usages from these tests as it is not needed and caused test fails occasionally

--- test/hummingbot/core/api_throttler/test_weighted_api_throttler.py
+++ test/hummingbot/core/api_throttler/test_weighted_api_throttler.py
@@ -1,19 +1,10 @@
 import unittest
-import aiohttp
 import asyncio
-import requests
-
-from unittest.mock import patch
 
 from hummingbot.core.api_throttler.data_types import RateLimit
 from hummingbot.core.api_throttler.weighted_api_throttler import WeightedAPIThrottler
 
-from test.hummingbot.core.api_throttler.throttled_mock_server import (
-    ThrottledMockServer,
-    BASE_URL,
-    TEST_PATH_URL,
-)
-
+TEST_PATH_URL = "TEST_PATH_URL"
 WEIGHTED_RATE_LIMIT = [
     RateLimit(5, 5, TEST_PATH_URL, 2)
 ]
@@ -26,39 +17,16 @@ class WeightedAPIThrottlerUnitTests(unittest.TestCase):
         super().setUpClass()
         cls.ev_loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
 
-        cls.mock_server = ThrottledMockServer.get_instance()
-        cls.mock_server.add_host_to_mock(BASE_URL)
-        cls.mock_server.start()
-        cls.ev_loop.run_until_complete(cls.mock_server.wait_til_started())
-
-        cls._patcher = patch("aiohttp.client.URL")
-        cls._url_mock = cls._patcher.start()
-        cls._url_mock.side_effect = ThrottledMockServer.reroute_local
-
-        cls._req_patcher = unittest.mock.patch.object(requests.Session, "request", autospec=True)
-        cls._req_url_mock = cls._req_patcher.start()
-        cls._req_url_mock.side_effect = ThrottledMockServer.reroute_request
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.mock_server.stop()
-        cls._patcher.stop()
-        cls._req_patcher.stop()
-        super().tearDownClass()
-
     def setUp(self) -> None:
         super().setUp()
-
-        self.mock_server.reset_request_count()
-
         self.weighted_rate_throttler = WeightedAPIThrottler(rate_limit_list=WEIGHTED_RATE_LIMIT,
                                                             retry_interval=5.0)
+        self.request_count = 0
 
     async def execute_n_requests(self, n: int, throttler: WeightedAPIThrottler):
         for _ in range(n):
             async with throttler.execute_task(path_url=TEST_PATH_URL):
-                async with aiohttp.ClientSession() as client:
-                    await client.get(f"https://{BASE_URL + TEST_PATH_URL}")
+                self.request_count += 1
 
     def test_weighted_rate_throttler_above_limit(self):
         # Test Scenario: API requests sent > Rate Limit
@@ -74,7 +42,7 @@ class WeightedAPIThrottlerUnitTests(unittest.TestCase):
                 asyncio.wait_for(self.execute_n_requests(n, throttler=self.weighted_rate_throttler), timeout=1.0)
             )
 
-        self.assertEqual(expected_request_count, self.mock_server.request_count)
+        self.assertEqual(expected_request_count, self.request_count)
 
     def test_weighted_rate_throttler_below_limit(self):
         n: int = WEIGHTED_RATE_LIMIT[0].limit // WEIGHTED_RATE_LIMIT[0].weight
@@ -82,7 +50,7 @@ class WeightedAPIThrottlerUnitTests(unittest.TestCase):
         self.ev_loop.run_until_complete(
             self.execute_n_requests(n, throttler=self.weighted_rate_throttler))
 
-        self.assertEqual(n, self.mock_server.request_count)
+        self.assertEqual(n, self.request_count)
 
     def test_weighted_rate_throttler_equal_limit(self):
         rate_limit: RateLimit = WEIGHTED_RATE_LIMIT[0]
@@ -92,4 +60,4 @@ class WeightedAPIThrottlerUnitTests(unittest.TestCase):
         self.ev_loop.run_until_complete(
             self.execute_n_requests(n, throttler=self.weighted_rate_throttler))
 
-        self.assertEqual(n, self.mock_server.request_count)
+        self.assertEqual(n, self.request_count)
