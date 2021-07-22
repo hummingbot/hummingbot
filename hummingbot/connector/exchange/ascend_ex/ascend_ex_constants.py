85fa545c2798ab3e8cdc697511595f3005eed069
Nullably <37262506+Nullably@users.noreply.github.com>
2021-07-22 18:09:59 +0800

    (feat) fix rate limits

--- hummingbot/connector/exchange/ascend_ex/ascend_ex_constants.py
+++ hummingbot/connector/exchange/ascend_ex/ascend_ex_constants.py
@@ -10,7 +10,7 @@ PONG_PAYLOAD = {"op": "pong"}
 # AscendEx has multiple pools for API request limits
 # Any call increases call rate in ALL pool, so e.g. a cash/order call will contribute to both ALL and cash/order pools.
 REQUEST_CALL_LIMITS: Dict[str, CallRateLimit] = {
-    "all_endpoints": CallRateLimit(limit_id="all_endpoints", limit=2, time_interval=1),
-    "cash/order": CallRateLimit(limit_id="cash/order", limit=2, time_interval=1),
+    "all_endpoints": CallRateLimit(limit_id="all_endpoints", limit=100, time_interval=1),
+    "cash/order": CallRateLimit(limit_id="cash/order", limit=50, time_interval=1),
     "order/hist": CallRateLimit(limit_id="order/hist", limit=60, time_interval=60)
 }
