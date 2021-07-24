abf2a0aadaa42f49ebb0821738d7191e46eace59
M.Mortaz <Mortaz.Mehdi@gmail.com>
2021-07-24 17:39:11 +0430

    Add get_price_by_type method to resolve bug in custom_api use case in market making strategy

--- hummingbot/strategy/perpetual_market_making/api_asset_price_delegate.pyx
+++ hummingbot/strategy/perpetual_market_making/api_asset_price_delegate.pyx
@@ -1,14 +1,20 @@
+from decimal import Decimal
+
+from hummingbot.core.event.events import PriceType
 from .asset_price_delegate cimport AssetPriceDelegate
 from hummingbot.data_feed.custom_api_data_feed import CustomAPIDataFeed, NetworkStatus
 
 cdef class APIAssetPriceDelegate(AssetPriceDelegate):
-    def __init__(self, api_url: str):
+    def __init__(self, api_url: str, update_interval: float):
         super().__init__()
-        self._custom_api_feed = CustomAPIDataFeed(api_url=api_url)
+        self._custom_api_feed = CustomAPIDataFeed(api_url=api_url, update_interval=update_interval)
         self._custom_api_feed.start()
 
     cdef object c_get_mid_price(self):
         return self._custom_api_feed.get_price()
+
+    def get_price_by_type(self, _: PriceType) -> Decimal:
+        return self.c_get_mid_price()
 
     @property
     def ready(self) -> bool:
