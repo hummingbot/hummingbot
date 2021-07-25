897232923bd741e2662a027b7b345879a1245608
La Hoang <machinelearningguys@gmail.com>
2021-07-25 16:59:10 +0700

    Add reward sniping script

---
+++ scripts/my_scripts/reward_sniping.py
@@ -0,0 +1,9 @@
+# Coding plan:
+
+# Implement envelope and bolinger band to indicate fast/med/slow regions of trading paces + moving average
+# Implement trend indicator
+# Implement whale preventing methods:
+#   - Random order amount function based on trend and volatility, maybe use bolinger band + envelop band
+# Implement inventory control
+# Implement trend-oriented trade bias (which side of trade should I place more order amount)
+# Explore the idea 
