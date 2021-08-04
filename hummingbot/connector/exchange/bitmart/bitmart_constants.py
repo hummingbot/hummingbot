429fffb9ea460381d24ba763808cb6767d53b8d7
Sri Ram Bandi <srirambandi.654@gmail.com>
2021-08-05 04:22:39 +0530

    (feat) Add connector for BitMart

---
+++ hummingbot/connector/exchange/bitmart/bitmart_constants.py
@@ -0,0 +1,51 @@
+# A single source of truth for constant variables related to the exchange
+
+from hummingbot.core.api_throttler.data_types import RateLimit
+
+EXCHANGE_NAME = "bitmart"
+REST_URL = "https://api-cloud.bitmart.com"
+REST_URL_HK = "https://api-cloud.bitmart.news"
+WSS_URL = "wss://ws-manager-compress.bitmart.com?protocol=1.1"
+WSS_URL_HK = "wss://ws-manager-compress.bitmart.news?protocol=1.1"
+
+# REST API ENDPOINTS
+CHECK_NETWORK_PATH_URL = "system/service"
+GET_TRADING_PAIRS_PATH_URL = "spot/v1/symbols"
+GET_TRADING_RULES_PATH_URL = "spot/v1/symbols/details"
+GET_LAST_TRADING_PRICES_PATH_URL = "spot/v1/ticker"
+GET_ORDER_BOOK_PATH_URL = "spot/v1/symbols/book"
+CREATE_ORDER_PATH_URL = "spot/v1/submit_order"
+CANCEL_ORDER_PATH_URL = "spot/v2/cancel_order"
+GET_ACCOUNT_SUMMARY_PATH_URL = "spot/v1/wallet"
+GET_ORDER_DETAIL_PATH_URL = "spot/v1/order_detail"
+GET_TRADE_DETAIL_PATH_URL = "spot/v1/trades"
+GET_OPEN_ORDERS_PATH_URL = "spot/v1/orders"
+
+# BitMart has a per method API limit
+RATE_LIMITS = [
+    # RateLimit(Limit, TimeInterval(seconds), RequestPath)
+    RateLimit(10, 1, path_url=CHECK_NETWORK_PATH_URL),
+    RateLimit(5, 1, path_url=GET_TRADING_PAIRS_PATH_URL),
+    RateLimit(5, 1, path_url=GET_TRADING_RULES_PATH_URL),
+    RateLimit(5, 1, path_url=GET_LAST_TRADING_PRICES_PATH_URL),
+    RateLimit(5, 1, path_url=GET_ORDER_BOOK_PATH_URL),
+    RateLimit(50, 1, path_url=CREATE_ORDER_PATH_URL),
+    RateLimit(50, 1, path_url=CANCEL_ORDER_PATH_URL),
+    RateLimit(10, 1, path_url=GET_ACCOUNT_SUMMARY_PATH_URL),
+    RateLimit(50, 1, path_url=GET_ORDER_DETAIL_PATH_URL),
+    RateLimit(10, 1, path_url=GET_TRADE_DETAIL_PATH_URL),
+    RateLimit(10, 1, path_url=GET_OPEN_ORDERS_PATH_URL),
+]
+
+ORDER_STATUS = {
+    1: "FAILED",        # Order failure
+    2: "OPEN",          # Placing order
+    3: "REJECTED",      # Order failure, Freeze failure
+    4: "ACTIVE",        # Order success, Pending for fulfilment
+    5: "ACTIVE",        # Partially filled
+    6: "FILLED",        # Fully filled
+    7: "ACTIVE",        # Canceling
+    8: "CANCELED",      # Canceled
+    9: "ACTIVE",        # Outstanding (4 and 5)
+    10: "COMPLETED"     # 6 and 8
+}
--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ def main():
         "hummingbot.connector.exchange.huobi",
         "hummingbot.connector.exchange.radar_relay",
         "hummingbot.connector.exchange.kraken",
+        "hummingbot.connector.exchange.bitmart",
         "hummingbot.connector.exchange.crypto_com",
         "hummingbot.connector.exchange.kucoin",
         "hummingbot.connector.exchange.loopring",
