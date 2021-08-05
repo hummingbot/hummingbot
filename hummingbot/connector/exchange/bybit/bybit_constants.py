478cc454aeaa4aa56f86ace17cc472b6f499a522
aarmoa <abarmoa+github@gmail.com>
2021-08-05 16:14:06 -0300

    Logic to get last traded prices

---
+++ hummingbot/connector/exchange/bybit/bybit_constants.py
@@ -0,0 +1,21 @@
+# A single source of truth for constant variables related to the exchange
+
+EXCHANGE_NAME = "bybit"
+
+REST_URLS = {"bybit_main": "https://api.bybit.com/",
+             "bybit_testnet": "https://api-testnet.bybit.com/"}
+WSS_URLS = {"bybit_main": "wss://stream.bybit.com/realtime",
+            "bybit_testnet": "wss://stream-testnet.bybit.com/realtime"}
+
+REST_API_VERSION = "v2"
+
+# REST API Public Endpoints
+LATEST_SYMBOL_INFORMATION_ENDPOINT = "/public/tickers"
+
+# REST API Private Endpoints
+
+
+# WebSocket Public Endpoints
+
+
+# WebSocket Message Events
