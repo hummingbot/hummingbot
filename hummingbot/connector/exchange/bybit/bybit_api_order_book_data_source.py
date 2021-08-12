3a6019d72a99687d54703f3d69adddef4921c86a
aarmoa <abarmoa+github@gmail.com>
2021-08-12 12:18:18 -0300

    Bybit order book data source including functionality for listening to order book, trades and instruments updates

--- hummingbot/connector/exchange/bybit/bybit_api_order_book_data_source.py
+++ hummingbot/connector/exchange/bybit/bybit_api_order_book_data_source.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import aiohttp
 import asyncio
 import logging
@@ -5,11 +7,14 @@ import logging
 from typing import (
     Dict,
     List,
-    Optional,
+    Optional, Any,
 )
 
 from hummingbot.connector.exchange.bybit import bybit_constants as CONSTANTS, bybit_utils
-from hummingbot.core.data_type.order_book import OrderBook
+from hummingbot.core.data_type.order_book import OrderBook, OrderBookMessage
+
+from hummingbot.connector.exchange.bybit.bybit_order_book import BybitOrderBook
+from hummingbot.connector.exchange.bybit.bybit_websocket_adaptor import BybitWebSocketAdaptor
 from hummingbot.core.data_type.order_book_tracker_data_source import OrderBookTrackerDataSource
 from hummingbot.logger import HummingbotLogger
 
@@ -21,6 +26,8 @@ class BybitAPIOrderBookDataSource(OrderBookTrackerDataSource):
     # SNAPSHOT_TIMEOUT = 10.0
 
     _logger: Optional[HummingbotLogger] = None
+    _trading_pair_symbol_map: Dict[str, Dict[str, str]] = {}
+    _last_traded_prices: Dict[str, Dict[str, float]] = defaultdict(dict)
 
     @classmethod
     def logger(cls) -> HummingbotLogger:
@@ -28,39 +35,88 @@ class BybitAPIOrderBookDataSource(OrderBookTrackerDataSource):
             cls._logger = logging.getLogger(__name__)
         return cls._logger
 
-    def __init__(self, trading_pairs: List[str] = None, domain: Optional[str] = None):
+    def __init__(self, trading_pairs: List[str] = None,
+                 domain: Optional[str] = None,
+                 session: Optional[aiohttp.ClientSession] = None):
         super().__init__(trading_pairs)
         self._domain = domain
         self._trading_pairs: List[str] = trading_pairs
+        self._session = session or aiohttp.ClientSession()
+        self._messages_queues: Dict[str, asyncio.Queue] = defaultdict(asyncio.Queue)
+
+    async def _create_websocket_connection(self) -> BybitWebSocketAdaptor:
+        """
+        Initialize WebSocket client for UserStreamDataSource
+        """
+        try:
+            ws = await self._session.ws_connect(bybit_utils.wss_url(self._domain))
+            return BybitWebSocketAdaptor(websocket=ws)
+        except asyncio.CancelledError:
+            raise
+        except Exception as ex:
+            self.logger().network(f"Unexpected error occurred during {CONSTANTS.EXCHANGE_NAME} WebSocket Connection "
+                                  f"({ex})")
+            raise
+
+    @classmethod
+    async def init_trading_pair_symbols(cls, domain: Optional[str] = None):
+        """Initialize _trading_pair_symbol_map class variable
+        """
+        cls._trading_pair_symbol_map[domain] = {}
+
+        endpoint_url = bybit_utils.rest_api_url_for_endpoint(CONSTANTS.QUERY_SYMBOL_ENDPOINT, domain)
+
+        async with aiohttp.ClientSession() as client:
+            async with client.get(endpoint_url, params={}) as response:
+                if response.status == 200:
+                    resp_json: Dict[str, Any] = await response.json()
+
+                    cls._trading_pair_symbol_map[domain] = {
+                        instrument["name"]: f"{instrument['base_currency']}-{instrument['quote_currency']}"
+                        for instrument in resp_json["result"]
+                        if (instrument["status"] == "Trading"
+                            and instrument["name"] == f"{instrument['base_currency']}{instrument['quote_currency']}")
+                    }
+
+    @classmethod
+    async def trading_pair_symbol_map(cls, domain: Optional[str] = None):
+        if domain not in cls._trading_pair_symbol_map or not cls._trading_pair_symbol_map[domain]:
+            await cls.init_trading_pair_symbols(domain)
+
+        return cls._trading_pair_symbol_map[domain]
 
     @classmethod
     async def get_last_traded_prices(cls, trading_pairs: List[str], domain: Optional[str] = None) -> Dict[str, float]:
+        if (domain in cls._last_traded_prices
+                and all(trading_pair in cls._last_traded_prices[domain]
+                        for trading_pair
+                        in trading_pairs)):
+            result = {trading_pair: cls._last_traded_prices[domain][trading_pair] for trading_pair in trading_pairs}
+        else:
+            result = await cls._get_last_traded_prices_from_exchange(trading_pairs, domain)
+        return result
+
+    @classmethod
+    async def _get_last_traded_prices_from_exchange(cls, trading_pairs, domain):
         result = {}
+        trading_pair_symbol_map = await cls.trading_pair_symbol_map(domain=domain)
         endpoint_url = bybit_utils.rest_api_url_for_endpoint(CONSTANTS.LATEST_SYMBOL_INFORMATION_ENDPOINT, domain)
         async with aiohttp.ClientSession() as client:
-            async with client.get(f"{endpoint_url}") as response:
+            async with client.get(endpoint_url) as response:
                 if response.status == 200:
                     resp_json = await response.json()
                     if "result" in resp_json:
                         for token_pair_info in resp_json["result"]:
-                            pass
-                            # token_pair = f"{token_pair_info[:]}"
-                            # if token_pair_info["market_id"] in trading_pairs:
-                            #     result[market["market_id"]] = float(market["last"])
-
+                            token_pair = trading_pair_symbol_map[token_pair_info["symbol"]]
+                            if token_pair in trading_pairs:
+                                result[token_pair] = float(token_pair_info["last_price"])
         return result
 
     @staticmethod
-    async def fetch_trading_pairs(domain: str = "com") -> List[str]:
-        pass
+    async def fetch_trading_pairs(domain: Optional[str] = None) -> List[str]:
+        symbols_map = await BybitAPIOrderBookDataSource.trading_pair_symbol_map(domain=domain)
+        return list(symbols_map.values())
 
-    #     async with aiohttp.ClientSession() as client:
-    #         async with client.get(f"{CONSTANTS.MARKETS_URL.format(domain)}") as response:
-    #             if response.status == 200:
-    #                 resp_json: Dict[str, Any] = await response.json()
-    #                 return [market["id"] for market in resp_json["data"] if market["closed"] is False]
-    #             return []
-    #
     # @staticmethod
     # async def get_order_book_data(trading_pair: str, domain: str = "com") -> Dict[str, any]:
     #     """
@@ -107,101 +163,98 @@ class BybitAPIOrderBookDataSource(OrderBookTrackerDataSource):
     #         await ws.close()
     #
 
-    async def listen_for_trades(self, ev_loop: asyncio.BaseEventLoop, output: asyncio.Queue):
-        pass
+    async def listen_for_subscriptions(self):
+        """
+        Subscribe to all required events and start the listening cycle.
+        """
 
-    #     """
-    #     Listen for trades using websocket trade channel
-    #     """
-    #     while True:
-    #         try:
-    #             async with websockets.connect(uri=CONSTANTS.WSS_URL.format(self._domain)) as ws:
-    #                 for trading_pair in self._trading_pairs:
-    #                     params: Dict[str, Any] = {
-    #                         "channel": "marketdata",
-    #                         "filter": ["recent_trades"],
-    #                         "interval": 100,
-    #                         "market_id": trading_pair,
-    #                         "type": "subscribe"
-    #                     }
-    #                     await ws.send(ujson.dumps(params))
-    #                 async for raw_msg in self._inner_messages(ws):
-    #                     msg_timestamp: int = int(time.time() * 1e3)
-    #                     msg = ujson.loads(raw_msg)
-    #                     if "recent_trades" not in msg:
-    #                         # Unrecognized response from "recent_trades" channel
-    #                         continue
-    #
-    #                     if "reset" in msg and msg["reset"] is True:
-    #                         # Ignores first response from "recent_trades" channel. This response details the last 100 trades.
-    #                         continue
-    #                     for trade_entry in msg["recent_trades"]:
-    #                         trade_msg: OrderBookMessage = ProbitOrderBook.trade_message_from_exchange(
-    #                             msg=trade_entry,
-    #                             timestamp=msg_timestamp,
-    #                             metadata={"market_id": msg["market_id"]})
-    #                         output.put_nowait(trade_msg)
-    #         except asyncio.CancelledError:
-    #             raise
-    #         except Exception:
-    #             self.logger().error("Unexpected error.", exc_info=True)
-    #             await asyncio.sleep(5.0)
-    #         finally:
-    #             await ws.close()
-    #
+        while True:
+            try:
+                ws_adaptor: BybitWebSocketAdaptor = await self._create_websocket_connection()
+                symbols_and_pairs_map = await self.trading_pair_symbol_map(self._domain)
+                symbols = [symbol for symbol, pair in symbols_and_pairs_map.items() if pair in self._trading_pairs]
+
+                await ws_adaptor.subscribe_to_order_book(symbols)
+                await ws_adaptor.subscribe_to_trades(symbols)
+                await ws_adaptor.subscribe_to_instruments_info(symbols)
+
+                async for json_message in ws_adaptor.iter_messages():
+                    if "success" in json_message:
+                        if json_message["success"]:
+                            self.logger().info(
+                                f"Successful subscription to the topic {json_message['request']['args']}")
+                        else:
+                            self.logger().error("There was an error subscribing to the topic "
+                                                f"{json_message['request']['args']} ({json_message['ret_msg']})")
+                    else:
+                        topic = json_message["topic"]
+                        topic = ".".join(topic.split(".")[:-1])
+                        self._messages_queues.get(topic).put_nowait(json_message)
+
+            except asyncio.CancelledError:
+                raise
+            except Exception as ex:
+                self.logger().network(
+                    f"Unexpected error with WebSocket connection ({ex})",
+                    exc_info=True,
+                    app_warning_msg="Unexpected error with WebSocket connection. Retrying in 30 seconds. "
+                                    "Check network connection."
+                )
+                if ws_adaptor:
+                    await ws_adaptor.close()
+                await asyncio.sleep(30.0)
+
+    async def listen_for_trades(self, ev_loop: asyncio.BaseEventLoop, output: asyncio.Queue):
+        """
+        Process trade events received through the websocket channel
+        """
+        symbol_map = await self.trading_pair_symbol_map(self._domain)
+
+        while True:
+            try:
+                trade_message = await self._messages_queues[CONSTANTS.WS_TRADES_TOPIC].get()
+                for trade_entry in trade_message["data"]:
+                    trade_msg: OrderBookMessage = BybitOrderBook.trade_message_from_exchange(
+                        msg=trade_entry,
+                        timestamp=trade_entry["trade_time_ms"] * 1e-3,
+                        metadata={"trading_pair": symbol_map[trade_entry["symbol"]]})
+                    output.put_nowait(trade_msg)
+            except asyncio.CancelledError:
+                raise
+            except Exception as ex:
+                self.logger().error(f"Unexpected error ({ex})", exc_info=True)
 
     async def listen_for_order_book_diffs(self, ev_loop: asyncio.BaseEventLoop, output: asyncio.Queue):
-        pass
+        """
+        Listen for order book diffs events received through the websocket channel
+        """
+        symbol_map = await self.trading_pair_symbol_map(self._domain)
 
-    #     """
-    #     Listen for orderbook diffs using websocket book channel
-    #     """
-    #     while True:
-    #         try:
-    #             async with websockets.connect(uri=CONSTANTS.WSS_URL.format(self._domain)) as ws:
-    #                 ws: websockets.WebSocketClientProtocol = ws
-    #                 for trading_pair in self._trading_pairs:
-    #                     params: Dict[str, Any] = {
-    #                         "channel": "marketdata",
-    #                         "filter": ["order_books"],
-    #                         "interval": 100,
-    #                         "market_id": trading_pair,
-    #                         "type": "subscribe"
-    #                     }
-    #                     await ws.send(ujson.dumps(params))
-    #                 async for raw_msg in self._inner_messages(ws):
-    #                     msg_timestamp: int = int(time.time() * 1e3)
-    #                     msg: Dict[str, Any] = ujson.loads(raw_msg)
-    #                     if "order_books" not in msg:
-    #                         # Unrecognized response from "order_books" channel
-    #                         continue
-    #                     if "reset" in msg and msg["reset"] is True:
-    #                         # First response from websocket is a snapshot. This is only when reset = True
-    #                         snapshot_msg: OrderBookMessage = ProbitOrderBook.snapshot_message_from_exchange(
-    #                             msg=msg,
-    #                             timestamp=msg_timestamp,
-    #                         )
-    #                         output.put_nowait(snapshot_msg)
-    #                     else:
-    #                         diff_msg: OrderBookMessage = ProbitOrderBook.diff_message_from_exchange(
-    #                             msg=msg,
-    #                             timestamp=msg_timestamp,
-    #                             metadata={"market_id": msg["market_id"]}
-    #                         )
-    #                         output.put_nowait(diff_msg)
-    #         except asyncio.CancelledError:
-    #             raise
-    #         except Exception:
-    #             self.logger().network(
-    #                 "Unexpected error with WebSocket connection.",
-    #                 exc_info=True,
-    #                 app_warning_msg="Unexpected error with WebSocket connection. Retrying in 30 seconds. "
-    #                                 "Check network connection."
-    #             )
-    #             await asyncio.sleep(30.0)
-    #         finally:
-    #             await ws.close()
-    #
+        while True:
+            try:
+                order_book_message = await self._messages_queues[CONSTANTS.WS_ORDER_BOOK_EVENTS_TOPIC].get()
+
+                symbol = order_book_message["topic"].split(".")[-1]
+                trading_pair = symbol_map[symbol]
+                event_type = order_book_message["type"]
+
+                if event_type == "snapshot":
+                    snapshot_msg: OrderBookMessage = BybitOrderBook.snapshot_message_from_exchange(
+                        msg=order_book_message,
+                        timestamp=order_book_message["timestamp_e6"] * 1e-6,
+                        metadata={"trading_pair": trading_pair})
+                    output.put_nowait(snapshot_msg)
+
+                if event_type == "delta":
+                    diff_msg: OrderBookMessage = BybitOrderBook.diff_message_from_exchange(
+                        msg=order_book_message,
+                        timestamp=order_book_message["timestamp_e6"] * 1e-6,
+                        metadata={"trading_pair": trading_pair})
+                    output.put_nowait(diff_msg)
+            except asyncio.CancelledError:
+                raise
+            except Exception as ex:
+                self.logger().error(f"Unexpected error ({ex})", exc_info=True)
 
     async def listen_for_order_book_snapshots(self, ev_loop: asyncio.BaseEventLoop, output: asyncio.Queue):
         pass
@@ -242,3 +295,32 @@ class BybitAPIOrderBookDataSource(OrderBookTrackerDataSource):
     #         except Exception:
     #             self.logger().error("Unexpected error.", exc_info=True)
     #             await asyncio.sleep(5.0)
+
+    async def listen_for_instruments_info(self, ev_loop: asyncio.BaseEventLoop):
+        """
+        Listen for instruments information events received through the websocket channel to update last traded price
+        """
+        symbol_map = await self.trading_pair_symbol_map(self._domain)
+
+        while True:
+            try:
+                instrument_info_message = await self._messages_queues[CONSTANTS.WS_INSTRUMENTS_INFO_TOPIC].get()
+
+                symbol = instrument_info_message["topic"].split(".")[-1]
+                trading_pair = symbol_map[symbol]
+                event_type = instrument_info_message["type"]
+
+                entries = []
+                if event_type == "snapshot":
+                    entries.append(instrument_info_message["data"])
+                if event_type == "delta":
+                    entries.extend(instrument_info_message["data"]["update"])
+
+                for entry in entries:
+                    if "last_price_e4" in entry:
+                        self._last_traded_prices[self._domain][trading_pair] = entry["last_price_e4"] * 1e-4
+
+            except asyncio.CancelledError:
+                raise
+            except Exception as ex:
+                self.logger().error(f"Unexpected error ({ex})", exc_info=True)
--- hummingbot/connector/exchange/bybit/bybit_constants.py
+++ hummingbot/connector/exchange/bybit/bybit_constants.py
@@ -11,11 +11,15 @@ REST_API_VERSION = "v2"
 
 # REST API Public Endpoints
 LATEST_SYMBOL_INFORMATION_ENDPOINT = "/public/tickers"
+QUERY_SYMBOL_ENDPOINT = "/public/symbols"
 
 # REST API Private Endpoints
 
 
 # WebSocket Public Endpoints
-
+WS_PING_REQUEST = "ping"
+WS_ORDER_BOOK_EVENTS_TOPIC = "orderBook_200.100ms"
+WS_TRADES_TOPIC = "trade"
+WS_INSTRUMENTS_INFO_TOPIC = "instrument_info.100ms"
 
 # WebSocket Message Events
