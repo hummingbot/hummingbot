335b607ce98a730932a321b1ccf7800c3fef5404
Daniel Tan <daniel@hummingbot.io>
2021-08-12 21:00:04 +0800

    (refactor) refactor crypto_com connector to use new throttler

--- hummingbot/connector/exchange/crypto_com/crypto_com_constants.py
+++ hummingbot/connector/exchange/crypto_com/crypto_com_constants.py
@@ -1,6 +1,6 @@
 # A single source of truth for constant variables related to the exchange
 
-# from hummingbot.core.api_throttler.data_types import RateLimit
+from hummingbot.core.api_throttler.data_types import RateLimit
 
 EXCHANGE_NAME = "crypto_com"
 REST_URL = "https://api.crypto.com/v2"
@@ -29,6 +29,28 @@ GET_OPEN_ORDERS_PATH_URL = "private/get-open-orders"
 #     RateLimit(30, 0.1, path_url=GET_ORDER_DETAIL_PATH_URL),
 #     RateLimit(3, 0.1, path_url=GET_OPEN_ORDERS_PATH_URL),
 # ]
+
+# RATE_LIMITS = [
+#     RateLimit(limit_id="ALL", limit=100, time_interval=1),
+#     RateLimit(limit_id=CHECK_NETWORK_PATH_URL, limit=100, time_interval=1, linked_limits=["ALL"]),
+#     RateLimit(limit_id=GET_TRADING_RULES_PATH_URL, limit=100, time_interval=1, linked_limits=["ALL"]),
+#     RateLimit(limit_id=CREATE_ORDER_PATH_URL, limit=15, time_interval=0.1, linked_limits=["ALL"]),
+#     RateLimit(limit_id=CANCEL_ORDER_PATH_URL, limit=15, time_interval=0.1, linked_limits=["ALL"]),
+#     RateLimit(limit_id=GET_ACCOUNT_SUMMARY_PATH_URL, limit=3, time_interval=0.1, linked_limits=["ALL"]),
+#     RateLimit(limit_id=GET_ORDER_DETAIL_PATH_URL, limit=30, time_interval=0.1, linked_limits=["ALL"]),
+#     RateLimit(limit_id=GET_OPEN_ORDERS_PATH_URL, limit=3, time_interval=0.1, linked_limits=["ALL"]),
+# ]
+
+RATE_LIMITS = [
+    RateLimit(limit_id=CHECK_NETWORK_PATH_URL, limit=100, time_interval=1),
+    RateLimit(limit_id=GET_TRADING_RULES_PATH_URL, limit=100, time_interval=1),
+    RateLimit(limit_id=CREATE_ORDER_PATH_URL, limit=15, time_interval=0.1),
+    RateLimit(limit_id=CANCEL_ORDER_PATH_URL, limit=15, time_interval=0.1),
+    RateLimit(limit_id=GET_ACCOUNT_SUMMARY_PATH_URL, limit=3, time_interval=0.1),
+    RateLimit(limit_id=GET_ORDER_DETAIL_PATH_URL, limit=30, time_interval=0.1),
+    RateLimit(limit_id=GET_OPEN_ORDERS_PATH_URL, limit=3, time_interval=0.1),
+]
+
 
 API_REASONS = {
     0: "Success",
