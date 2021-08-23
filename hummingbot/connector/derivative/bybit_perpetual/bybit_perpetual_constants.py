f27c12d4f2675b50f8ca217e560e130681bba341
Daniel Tan <daniel@hummingbot.io>
2021-08-23 16:43:21 +0800

    (feat) add user_funding_fee_polling_loop

--- hummingbot/connector/derivative/bybit_perpetual/bybit_perpetual_constants.py
+++ hummingbot/connector/derivative/bybit_perpetual/bybit_perpetual_constants.py
@@ -17,6 +17,7 @@ QUERY_SYMBOL_ENDPOINT = "/public/symbols"
 ORDER_BOOK_ENDPOINT = "/public/orderBook/L2"
 GET_FUNDING_FEE_PATH_URL = "/private/linear/funding/prev-funding"
 SET_LEVERAGE_PATH_URL = "/private/position/leverage/save"
+GET_LAST_FUNDING_RATE = "/private/funding/prev-funding"
 
 # WebSocket Public Endpoints
 WS_PING_REQUEST = "ping"
