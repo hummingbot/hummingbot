3663ec51b51871baa69fb040c36fd06704f87a78
Daniel Tan <daniel@hummingbot.io>
2021-08-26 16:53:33 +0800

    (fix) resolve PR comments

--- hummingbot/connector/derivative/bybit_perpetual/bybit_perpetual_api_order_book_data_source.py
+++ hummingbot/connector/derivative/bybit_perpetual/bybit_perpetual_api_order_book_data_source.py
@@ -165,10 +165,17 @@ class BybitPerpetualAPIOrderBookDataSource(OrderBookTrackerDataSource):
 
         return order_book
 
-    async def _get_funding_info_from_exchange(self, trading_pair: str, domain: str) -> FundingInfo:
-        symbol_trading_pair_map = await self.trading_pair_symbol_map(domain)
+    async def _get_funding_info_from_exchange(self, trading_pair: str) -> FundingInfo:
+        symbol_map = await self.trading_pair_symbol_map(self._domain)
+        symbols = [symbol for symbol, pair in symbol_map.items() if trading_pair == pair]
+
+        if symbols:
+            symbol = symbols[0]
+        else:
+            raise ValueError(f"There is no symbol mapping for trading pair {trading_pair}")
+
         params = {
-            "symbol": symbol_trading_pair_map[bybit_perpetual_utils.convert_to_exchange_trading_pair(trading_pair)]
+            "symbol": symbol
         }
         funding_info = None
         async with self._session as client:
@@ -181,16 +188,16 @@ class BybitPerpetualAPIOrderBookDataSource(OrderBookTrackerDataSource):
                                                index_price=Decimal(str(symbol_info["index_price"])),
                                                mark_price=Decimal(str(symbol_info["mark_price"])),
                                                next_funding_utc_timestamp=int(pd.Timestamp(symbol_info["next_funding_time"]).timestamp()),
-                                               rate=Decimal(str(symbol_info["predicted_funding_rate"])))  # Note: Absence of _e6 suffix for REST API response
+                                               rate=Decimal(str(symbol_info["predicted_funding_rate"])))  # Note: Absence of _e6 suffix from REST API response
         return funding_info
 
     async def get_funding_info(self, trading_pair: str) -> FundingInfo:
-        async with self._funding_info_async_lock:
-            if trading_pair not in self._funding_info:
-                funding_info = await self._get_funding_info_from_exchange(trading_pair, self._domain)
-            else:
-                funding_info = self._funding_info[trading_pair]
-            return funding_info
+        """
+        Returns the FundingInfo of the specified trading pair. If it does not exist, it will query the REST API.
+        """
+        if trading_pair not in self._funding_info:
+            self._funding_info[trading_pair] = await self._get_funding_info_from_exchange(trading_pair)
+        return self._funding_info[trading_pair]
 
     async def listen_for_subscriptions(self):
         """
@@ -342,15 +349,17 @@ class BybitPerpetualAPIOrderBookDataSource(OrderBookTrackerDataSource):
 
                     # Updates funding info for the relevant domain and trading_pair
                     async with self._funding_info_async_lock:
-                        if trading_pair not in self._funding_info:
-                            current_funding_info = FundingInfo(trading_pair=trading_pair,
-                                                               index_price=Decimal(str(entry["index_price"])),
-                                                               mark_price=Decimal(str(entry["mark_price"])),
-                                                               next_funding_utc_timestamp=int(pd.Timestamp(str(entry["next_funding_time"]), tz="UTC").timestamp()),
-                                                               rate=Decimal(str(entry["predicted_funding_rate_e6"])) * Decimal(1e-6),
-                                                               )
+                        if event_type == "snapshot":
+                            # Snapshot messages have all the data fields required to construct a new FundingInfo.
+                            current_funding_info: FundingInfo = FundingInfo(trading_pair=trading_pair,
+                                                                            index_price=Decimal(str(entry["index_price"])),
+                                                                            mark_price=Decimal(str(entry["mark_price"])),
+                                                                            next_funding_utc_timestamp=int(pd.Timestamp(str(entry["next_funding_time"]), tz="UTC").timestamp()),
+                                                                            rate=Decimal(str(entry["predicted_funding_rate_e6"])) * Decimal(1e-6),
+                                                                            )
                         else:
-                            current_funding_info: FundingInfo = self._funding_info[trading_pair]
+                            # Delta messages do not necessarily have all the data required.
+                            current_funding_info: FundingInfo = await self.get_funding_info(trading_pair)
                             if "index_price" in entry:
                                 current_funding_info.index_price = Decimal(str(entry["index_price"]))
                             if "mark_price" in entry:
