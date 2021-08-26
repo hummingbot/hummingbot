576ea60bb65330c5f69ca27664d0dd800c565cae
cjg <carlos.goncalves@outsystems.com>
2021-08-26 14:33:12 +0100

    (test) previous_strategy: no previous strategy stored  scenario

---
+++ test/hummingbot/client/test_previous_command.py
@@ -0,0 +1,17 @@
+# from hummingbot.client.command.import_command import ImportCommand
+import unittest
+from hummingbot.core.utils.async_utils import safe_ensure_future
+from hummingbot.client.command.previous_strategy import PreviousCommand
+# from hummingbot.client.command.previous_strategy import HummingbotApplication
+# from hummingbot.client.config.global_config_map import global_config_map
+from hummingbot.client.hummingbot_application import HummingbotApplication
+from unittest.mock import patch
+
+
+class PreviousCommandUnitTest(unittest.TestCase):
+
+    @patch.object(HummingbotApplication, '_notify')
+    def test_message_prompt(self, hummingbotApplication):
+        safe_ensure_future(PreviousCommand.previous_statrategy(hummingbotApplication, option=""))
+        hummingbotApplication._notify.assert_called()
+        self.assertTrue(True)
