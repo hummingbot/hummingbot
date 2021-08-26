3112d38b5497498ab2fa05fda2510aa313a49a4c
aarmoa <abarmoa+github@gmail.com>
2021-08-26 18:34:23 -0300

    Fix issues in requests to set leverage and to create an order

--- hummingbot/connector/derivative/bybit_perpetual/bybit_perpetual_auth.py
+++ hummingbot/connector/derivative/bybit_perpetual/bybit_perpetual_auth.py
@@ -1,6 +1,8 @@
 import hashlib
 import hmac
+import json
 import time
+from decimal import Decimal
 from typing import Dict, Any
 
 
@@ -14,7 +16,7 @@ class BybitPerpetualAuth():
         self._secret_key: str = secret_key
 
     def get_expiration_timestamp(self):
-        return str(int((round(time.time()) + 1) * 1e3))
+        return str(int((time.time() + 1) * 1e3))
 
     def get_ws_auth_payload(self) -> Dict[str, Any]:
         """
@@ -40,7 +42,13 @@ class BybitPerpetualAuth():
     def extend_params_with_authentication_info(self, params: Dict[str, Any]):
         params["timestamp"] = self.get_expiration_timestamp()
         params["api_key"] = self._api_key
-        raw_signature = '&'.join([str(key) + "=" + str(value) for key, value in sorted(params.items())])
+        params["recv_window"] = 10000
+        key_value_elements = []
+        for key, value in sorted(params.items()):
+            converted_value = float(value) if type(value) is Decimal else value
+            converted_value = converted_value if type(value) is str else json.dumps(converted_value)
+            key_value_elements.append(str(key) + "=" + converted_value)
+        raw_signature = '&'.join(key_value_elements)
         signature = hmac.new(self._secret_key.encode('utf-8'), raw_signature.encode('utf-8'), hashlib.sha256).hexdigest()
         params["sign"] = signature
         return params
