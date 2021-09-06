6602f703557c26a13e5ff559e16f56c390684cfd
willaPa <willypaterson@gmail.com>
2021-09-06 11:54:20 -0300

    adds mock data and tests

--- gateway/manual-tests/curl.sh
+++ gateway/manual-tests/curl.sh
@@ -14,11 +14,11 @@ curl -X POST -H "Content-Type: application/json" -d "{\"privateKey\":\"$ETH_PRIV
 
 # approve Ethereum to spend your WETH
 
-curl -X POST -H "Content-Type: application/json" -d "{\"privateKey\":\"$ETH_PRIVATE_KEY\",\"spender\":\"0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D\",\"token\":\"DAI\"}" localhost:5000/eth/approve
+curl -X POST -H "Content-Type: application/json" -d "{\"privateKey\":\"0b8040a6719bc21e2fce075b843e33cc1fab503c105c76bd5375e3f784724bc6\",\"spender\":\"0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D\",\"token\":\"DAI\"}" localhost:5000/eth/approve
 
-# check status of transaction 0x6d068067a5e5a0f08c6395b31938893d1cdad81f54a54456221ecd8c1941294d
+# check status of transaction 0x2faeb1aa55f96c1db55f643a8cf19b0f76bf091d0b7d1b068d2e829414576362
 
-curl -X POST -H "Content-Type: application/json" -d "{\"txHash\":\"0x6d068067a5e5a0f08c6395b31938893d1cdad81f54a54456221ecd8c1941294d\"}" localhost:5000/eth/poll
+curl -X POST -H "Content-Type: application/json" -d "{\"txHash\":\"0x2faeb1aa55f96c1db55f643a8cf19b0f76bf091d0b7d1b068d2e829414576362\"}" localhost:5000/eth/poll
 
 # update config
 
