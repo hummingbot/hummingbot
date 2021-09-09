6f251f7b4d69857afe17893b7dc4b27635e9e0db
Daniel Tan <daniel@hummingbot.io>
2021-09-09 14:16:32 +0800

    (fix) add condition to let task without any RateLimits to be executed regardless

--- hummingbot/core/api_throttler/async_throttler.py
+++ hummingbot/core/api_throttler/async_throttler.py
@@ -19,22 +19,23 @@ class AsyncRequestContext(AsyncRequestContextBase):
         Note: A task can be associated to one or more RateLimit.
         :return: True if it is within capacity to add a new task
         """
-        now: float = time.time()
-        for rate_limit in self._rate_limits:
-            same_pool_tasks = [task
-                               for task in self._task_logs
-                               if rate_limit in task.rate_limits and
-                               now - task.timestamp - (rate_limit.time_interval * self._safety_margin_pct) <= rate_limit.time_interval
-                               ]
-            if (len(same_pool_tasks) + 1) * rate_limit.weight > rate_limit.limit:
-                if self._last_max_cap_warning_ts < now - MAX_CAPACITY_REACHED_WARNING_INTERVAL:
-                    msg = f"API rate limit on {rate_limit.limit_id} ({rate_limit.limit} calls per " \
-                          f"{rate_limit.time_interval}s) has almost reached. Number of calls " \
-                          f"is {len(same_pool_tasks) * rate_limit.weight} in the last " \
-                          f"{rate_limit.time_interval} seconds"
-                    self.logger().notify(msg)
-                    AsyncRequestContextBase._last_max_cap_warning_ts = now
-                return False
+        if len(self._rate_limits) > 0:
+            now: float = time.time()
+            for rate_limit in self._rate_limits:
+                same_pool_tasks = [task
+                                   for task in self._task_logs
+                                   if rate_limit in task.rate_limits and
+                                   now - task.timestamp - (rate_limit.time_interval * self._safety_margin_pct) <= rate_limit.time_interval
+                                   ]
+                if (len(same_pool_tasks) + 1) * rate_limit.weight > rate_limit.limit:
+                    if self._last_max_cap_warning_ts < now - MAX_CAPACITY_REACHED_WARNING_INTERVAL:
+                        msg = f"API rate limit on {rate_limit.limit_id} ({rate_limit.limit} calls per " \
+                              f"{rate_limit.time_interval}s) has almost reached. Number of calls " \
+                              f"is {len(same_pool_tasks) * rate_limit.weight} in the last " \
+                              f"{rate_limit.time_interval} seconds"
+                        self.logger().notify(msg)
+                        AsyncRequestContextBase._last_max_cap_warning_ts = now
+                    return False
         return True
 
 
