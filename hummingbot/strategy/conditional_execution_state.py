fed91d7d0c88b8569a681561346a6e0631185ed8
Matej Horvath <contact@mhrvth.net>
2021-09-09 14:11:05 +0200

    delayed start feature

--- hummingbot/strategy/conditional_execution_state.py
+++ hummingbot/strategy/conditional_execution_state.py
@@ -51,3 +51,27 @@ class RunInTimeSpanExecutionState(ConditionalExecutionState):
             strategy.logger().debug("Time span execution: tick will not be processed "
                                     f"(executing between {self._start_timestamp.isoformat(sep=' ')} "
                                     f"and {self._end_timestamp.isoformat(sep=' ')})")
+
+
+class RunInDelayedStartExecutionState(ConditionalExecutionState):
+    """
+    Execution configuration to always run the strategy only for the ticks that happen between the specified start
+    timestamp and stop timestamp
+    :param start_timestamp: Specifies the moment to start running the strategy (datetime)
+    :param end_timestamp: Specifies the moment to stop running the strategy (datetime)
+    """
+
+    def __init__(self, start_timestamp: datetime):
+        super().__init__()
+
+        self._start_timestamp: datetime = start_timestamp
+
+    def __str__(self):
+        return f"run from {self._start_timestamp}"
+
+    def process_tick(self, timestamp: float, strategy: StrategyBase):
+        if self._start_timestamp.timestamp() <= timestamp:
+            strategy.process_tick(timestamp)
+        else:
+            strategy.logger().debug("Delayed start execution: tick will not be processed "
+                                    f"(executing from {self._start_timestamp.isoformat(sep=' ')})")
