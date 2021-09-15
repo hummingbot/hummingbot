c6e332b5db5809353a22416d942d4d686c361de3
Daniel Tan <daniel@hummingbot.io>
2021-09-15 13:39:32 +0800

    (feat) add _sleep() wrapper function in OrderBookTrackerDataSource base class

--- hummingbot/core/data_type/order_book_tracker_data_source.py
+++ hummingbot/core/data_type/order_book_tracker_data_source.py
@@ -71,3 +71,9 @@ class OrderBookTrackerDataSource(metaclass=ABCMeta):
         Object type in the output queue must be OrderBookMessage
         """
         raise NotImplementedError
+
+    async def _sleep(self, delay):
+        """
+        Function added only to facilitate patching the sleep in unit tests without affecting the asyncio module
+        """
+        await asyncio.sleep(delay)
