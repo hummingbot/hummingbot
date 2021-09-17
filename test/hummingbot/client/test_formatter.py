d71162ae778d1d6b02e3623d73b0cc68b43cfc06
Petio Petrov <petioptrv@icloud.com>
2021-09-17 11:56:53 +0700

    (cleanup) Changed test case numbers to increase readability

--- test/hummingbot/client/test_formatter.py
+++ test/hummingbot/client/test_formatter.py
@@ -6,34 +6,34 @@ from hummingbot.client import format_decimal, FLOAT_PRINTOUT_PRECISION
 
 class FormatterTest(unittest.TestCase):
     def test_precision_assumption(self):
-        self.assertEqual(7, FLOAT_PRINTOUT_PRECISION)  # if failed, remaining test case must be adapted
+        self.assertEqual(7, FLOAT_PRINTOUT_PRECISION)  # test cases must be adapted on precision change
 
     def test_format_float_decimal_places_rounded(self):
-        n = 0.12345678
+        n = 0.87654321
         s = format_decimal(n)
-        self.assertEqual("0.1234568", s)
+        self.assertEqual("0.8765432", s)
 
     def test_format_float_places_no_rounding(self):
-        n = 0.12
+        n = 0.21
         s = format_decimal(n)
-        self.assertEqual("0.12", s)
+        self.assertEqual("0.21", s)
 
     def test_format_large_float(self):
-        n = 12345678.0
+        n = 87654321.0
         s = format_decimal(n)
-        self.assertEqual("12345678", s)
+        self.assertEqual("87654321", s)
 
     def test_format_decimal_obj_decimal_places_rounded(self):
-        n = Decimal("0.12345678")
+        n = Decimal("0.87654321")
         s = format_decimal(n)
-        self.assertEqual("0.1234568", s)
+        self.assertEqual("0.8765432", s)
 
     def test_format_decimal_obj_places_no_rounding(self):
-        n = Decimal("0.12")
+        n = Decimal("0.21")
         s = format_decimal(n)
-        self.assertEqual("0.12", s)
+        self.assertEqual("0.21", s)
 
     def test_format_large_decimal_obj(self):
-        n = Decimal("12345678.0")
+        n = Decimal("87654321.0")
         s = format_decimal(n)
-        self.assertEqual("12345678", s)
+        self.assertEqual("87654321", s)
