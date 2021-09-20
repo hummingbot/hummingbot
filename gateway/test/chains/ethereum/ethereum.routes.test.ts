201c4cf6687e0c9fd1113086795effa86f076b8f
james-hummingbot <james@hummingbot.io>
2021-09-20 15:59:43 +0200

    Simple mocking for /eth/approval

--- gateway/test/chains/ethereum/ethereum.routes.test.ts
+++ gateway/test/chains/ethereum/ethereum.routes.test.ts
@@ -32,3 +32,58 @@ describe('POST /eth/nonce', () => {
     expect(res.statusCode).toEqual(200);
   });
 });
+
+describe('POST /eth/approve', () => {
+  it('should return 200', async () => {
+    EthereumRoutes.ethereum.getWallet = jest.fn().mockReturnValue(() => {
+      address: '0x0000000000000000000';
+    });
+    EthereumRoutes.ethereum.getTokenBySymbol = jest.fn().mockReturnValue(() => {
+      chainId: 42;
+      name: 'WETH';
+      symbol: 'WETH';
+      address: '0xd0A1E359811322d97991E03f863a0C30C2cF029C';
+      decimals: 18;
+    });
+    EthereumRoutes.ethereum.nonceManager.getNonce = jest
+      .fn()
+      .mockReturnValue(() => Promise.resolve(2));
+    EthereumRoutes.ethereum.approveERC20 = jest.fn().mockReturnValue(() => {
+      chainId: 42;
+      name: 'WETH';
+      symbol: 'WETH';
+      address: '0xd0A1E359811322d97991E03f863a0C30C2cF029C';
+      decimals: 18;
+    });
+
+    EthereumRoutes.ethereum.getContract = jest.fn().mockReturnValue(() => {
+      getNonce: () =>
+        Promise.resolve({
+          hash: '',
+          to: '',
+          from: '',
+          nonce: '',
+          gasLimit: '',
+          gasPrice: '',
+          maxFeePerGas: '',
+          maxPriorityFeePerGas: '',
+          data: '',
+          value: '',
+          chainId: '',
+          r: '',
+          s: '',
+          v: '',
+        });
+    });
+    const res = await request(app)
+      .post(`/eth/nonce`)
+      .send({
+        privateKey: 'abc123',
+        spender: 'xyz098',
+        token: 'WETH',
+        nonce: '23',
+      })
+      .set('Accept', 'application/json');
+    expect(res.statusCode).toEqual(200);
+  });
+});
