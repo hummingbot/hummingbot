92d490382dec5b0d72692865423dbbd2b7f5940a
Petio Petrov <petioptrv@icloud.com>
2021-10-01 15:14:40 +0700

    (tests) Adds test cases for the Bitmart exchange.
    Introduces light refactoring of problematic sections encountered in the process.

--- hummingbot/connector/exchange/bitmart/bitmart_exchange.py
+++ hummingbot/connector/exchange/bitmart/bitmart_exchange.py
@@ -11,7 +11,6 @@ import asyncio
 import json
 import aiohttp
 import math
-import time
 
 from hummingbot.core.network_iterator import NetworkStatus
 from hummingbot.logger import HummingbotLogger
@@ -56,10 +55,9 @@ class BitmartExchange(ExchangeBase):
     trading functionality.
     """
     API_CALL_TIMEOUT = 10.0
-    SHORT_POLL_INTERVAL = 1.0
+    POLL_INTERVAL = 1.0
     UPDATE_ORDER_STATUS_MIN_INTERVAL = 10.0
     UPDATE_TRADE_STATUS_MIN_INTERVAL = 10.0
-    LONG_POLL_INTERVAL = 1.0
 
     @classmethod
     def logger(cls) -> HummingbotLogger:
@@ -319,7 +317,7 @@ class BitmartExchange(ExchangeBase):
     async def _api_request(self,
                            method: str,
                            path_url: str,
-                           params: Dict[str, Any] = {},
+                           params: Optional[Dict[str, Any]] = None,
                            auth_type: str = None) -> Dict[str, Any]:
         """
         Sends an aiohttp request and waits for a response.
@@ -329,6 +327,7 @@ class BitmartExchange(ExchangeBase):
         :param auth_type: Type of Authorization header to send in request, from {"SIGNED", "KEYED", None}
         :returns A response in json format.
         """
+        params = params or {}
         async with self._throttler.execute_task(path_url):
             url = f"{CONSTANTS.REST_URL}/{path_url}"
             client = await self._http_client()
@@ -538,7 +537,7 @@ class BitmartExchange(ExchangeBase):
                 "SIGNED"
             )
 
-            # result = True is a successful cancel, False indicates fcancel failed due to already cancelled or matched
+            # result = True is a successful cancel, False indicates cancel failed due to already cancelled or matched
             if "result" in response["data"] and not response["data"]["result"]:
                 raise ValueError(f"Failed to cancel order - {order_id}. Order was already matched or cancelled on the exchange.")
             return order_id
@@ -559,13 +558,13 @@ class BitmartExchange(ExchangeBase):
         """
         while True:
             try:
-                self._poll_notifier = asyncio.Event()
                 await self._poll_notifier.wait()
                 await safe_gather(
                     self._update_balances(),
                     self._update_order_status(),
                 )
                 self._last_poll_timestamp = self.current_timestamp
+                self._poll_notifier.clear()
             except asyncio.CancelledError:
                 raise
             except Exception as e:
@@ -814,12 +813,8 @@ class BitmartExchange(ExchangeBase):
         Is called automatically by the clock for each clock's tick (1 second by default).
         It checks if status polling task is due for execution.
         """
-        now = time.time()
-        poll_interval = (self.SHORT_POLL_INTERVAL
-                         if now - self._user_stream_tracker.last_recv_time > 60.0
-                         else self.LONG_POLL_INTERVAL)
-        last_tick = int(self._last_timestamp / poll_interval)
-        current_tick = int(timestamp / poll_interval)
+        last_tick = int(self._last_timestamp / self.POLL_INTERVAL)
+        current_tick = int(timestamp / self.POLL_INTERVAL)
         if current_tick > last_tick:
             if not self._poll_notifier.is_set():
                 self._poll_notifier.set()
@@ -873,24 +868,26 @@ class BitmartExchange(ExchangeBase):
                 await asyncio.sleep(5.0)
 
     async def get_open_orders(self) -> List[OpenOrder]:
-        """
-        Listens to message in _user_stream_tracker.user_stream queue. The messages are put in by
-        BitmartAPIUserStreamDataSource.
-        """
-
         if self._trading_pairs is None:
             raise Exception("get_open_orders can only be used when trading_pairs are specified.")
 
-        tasks = []
+        page_len = 100
+        responses = []
         for trading_pair in self._trading_pairs:
-            for page in range(1, 6):
-                tasks.append(self._api_request("get", CONSTANTS.GET_OPEN_ORDERS_PATH_URL,
-                                               {"symbol": bitmart_utils.convert_to_exchange_trading_pair(trading_pair),
-                                                "offset": page,
-                                                "limit": 100,
-                                                "status": "9"},
-                                               "KEYED"))
-        responses = await safe_gather(*tasks, return_exceptions=True)
+            page = 1
+            while True:
+                response = await self._api_request("get", CONSTANTS.GET_OPEN_ORDERS_PATH_URL,
+                                                   {"symbol": bitmart_utils.convert_to_exchange_trading_pair(trading_pair),
+                                                    "offset": page,
+                                                    "limit": page_len,
+                                                    "status": "9"},
+                                                   "KEYED")
+                responses.append(response)
+                count = len(response["data"]["orders"])
+                if count < page_len:
+                    break
+                else:
+                    page += 1
 
         for order in self._in_flight_orders.values():
             await order.get_exchange_order_id()
