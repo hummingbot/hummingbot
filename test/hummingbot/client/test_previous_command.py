350e8608040291d51e7c93dab2798e2e01e5f3a1
cjg <carlos.goncalves@outsystems.com>
2021-10-02 22:54:52 +0100

    missing tests

--- test/hummingbot/client/test_previous_command.py
+++ test/hummingbot/client/test_previous_command.py
@@ -1,14 +1,9 @@
-# from hummingbot.client.command.import_command import ImportCommand
 import unittest
 import asyncio
-# from hummingbot.core.utils.async_utils import safe_ensure_future
-# from hummingbot.client.command.previous_strategy import PreviousCommand
-# from hummingbot.client.command.previous_strategy import HummingbotApplication
 from hummingbot.client.config.global_config_map import global_config_map
 from hummingbot.client.hummingbot_application import HummingbotApplication
-# from unittest.mock import patch, MagicMock, AsyncMock
+from unittest.mock import patch, MagicMock
 from typing import Awaitable
-# from unittest import IsolatedAsyncioTestCase
 from test.mock.mock_cli import CLIMockingAssistant
 
 
@@ -38,23 +33,22 @@ class PreviousCommandUnitTest(unittest.TestCase):
         self.assertTrue(
             self.cli_mock_assistant.check_log_called_with("No previous strategy found."))
 
-    # @patch.object(HummingbotApplication, '_notify')
-    # @patch('HummingbotApplication')
-    # def test_strategy_found_and_user_declines(self):
-    #     # hummingbotApplication = Mock(spec=HummingbotApplication)
-    #     strategy_name = "conf_1.yml"
-    #     global_config_map["previous_strategy"].value = strategy_name
-    #     self.async_run_with_timeout(self.app.previous_statrategy(option=""))
-    #     self.assertEqual(strategy_name, self.app.strategy_name)
-        # hummingbotApplication.prompt_answer.side_effects = self.mock_user_response
-        # loop = asyncio.get_event_loop()
-        # loop.run_until_complete(safe_ensure_future(PreviousCommand.previous_statrategy(hummingbotApplication, option="")))
-        # task = PreviousCommand.previous_statrategy(app, option="")
-        # await task
-        # # task._result()
-        # hummingbotApplication._notify.assert_called()
-        # hummingbotApplication._notify.assert_called_with('No previous strategy found.')
-        # with patch.object(HummingbotApplication, "_notify") as submethod_mocked:
-        #     submethod_mocked
-        # submethod_mocked.return_value = 13
-        # MyClassUnderTest().main_method()
+    @patch("hummingbot.client.command.import_command.ImportCommand.import_command")
+    def test_strategy_found_and_user_declines(self, import_command: MagicMock):
+        strategy_name = "conf_1.yml"
+        self.cli_mock_assistant.queue_prompt_reply("No")
+        self.async_run_with_timeout(
+            self.app.prompt_for_previous_strategy(strategy_name)
+        )
+        import_command.assert_not_called()
+
+    @patch("hummingbot.client.command.import_command.ImportCommand.import_command")
+    def test_strategy_found_and_user_accepts(self, import_command: MagicMock):
+        strategy_name = "conf_1.yml"
+        global_config_map["previous_strategy"].value = strategy_name
+        self.cli_mock_assistant.queue_prompt_reply("Yes")
+        self.async_run_with_timeout(
+            self.app.prompt_for_previous_strategy(strategy_name)
+        )
+        import_command.assert_called()
+        self.assertTrue(import_command.call_args[0][1] == strategy_name)
