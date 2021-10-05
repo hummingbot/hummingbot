821489890ffd55a25bc1bd55b10d1b7ba0d7d5df
james-hummingbot <james@hummingbot.io>
2021-10-05 15:55:38 +0200

    Move majority of controller code from ethereum.routes to ethereum.controllers

--- gateway/src/chains/ethereum/ethereum.routes.ts
+++ gateway/src/chains/ethereum/ethereum.routes.ts
@@ -1,18 +1,15 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import { Transaction, Wallet } from 'ethers';
 import { NextFunction, Router, Request, Response } from 'express';
 import { Ethereum } from './ethereum';
 import { EthereumConfig } from './ethereum.config';
 import { ConfigManager } from '../../services/config-manager';
-import { Token } from '../../services/ethereum-base';
 import { verifyEthereumIsAvailable } from './ethereum-middlewares';
-import { HttpException, asyncHandler } from '../../services/error-handler';
-import { latency } from '../../services/base';
-import { tokenValueToString } from '../../services/base';
-import { UniswapConfig } from './uniswap/uniswap.config';
+import { asyncHandler } from '../../services/error-handler';
 import {
-  EthereumTransactionReceipt,
   approve,
+  allowances,
+  balances,
+  nonce,
   poll,
 } from './ethereum.controllers';
 import {
@@ -21,14 +18,12 @@ import {
   EthereumBalanceRequest,
   EthereumApproveRequest,
   EthereumPollRequest,
+  EthereumNonceResponse,
+  EthereumAllowancesResponse,
+  EthereumBalanceResponse,
+  EthereumApproveResponse,
+  EthereumPollResponse,
 } from './ethereum.requests';
-import {
-  validateEthereumNonceRequest,
-  validateEthereumAllowancesRequest,
-  validateEthereumBalanceRequest,
-  validateEthereumApproveRequest,
-  validateEthereumPollRequest,
-} from './ethereum.validators';
 
 export namespace EthereumRoutes {
   export const router = Router();
@@ -58,10 +53,6 @@ export namespace EthereumRoutes {
     })
   );
 
-  interface EthereumNonceResponse {
-    nonce: number; // the user's nonce
-  }
-
   router.post(
     '/nonce',
     asyncHandler(
@@ -69,57 +60,11 @@ export namespace EthereumRoutes {
         req: Request<{}, {}, EthereumNonceRequest>,
         res: Response<EthereumNonceResponse | string, {}>
       ) => {
-        validateEthereumNonceRequest(req.body);
-
-        // get the address via the private key since we generally use the private
-        // key to interact with gateway and the address is not part of the user config
-        const wallet = ethereum.getWallet(req.body.privateKey);
-        const nonce = await ethereum.nonceManager.getNonce(wallet.address);
-        res.status(200).json({ nonce: nonce });
+        const response = await nonce(req.body);
+        res.status(200).json(response);
       }
     )
   );
-
-  interface EthereumAllowancesResponse {
-    network: string;
-    timestamp: number;
-    latency: number;
-    spender: string;
-    approvals: Record<string, string>;
-  }
-
-  const getSpender = (reqSpender: string): string => {
-    let spender: string;
-    if (reqSpender === 'uniswap') {
-      if (ConfigManager.config.ETHEREUM_CHAIN === 'mainnet') {
-        spender = UniswapConfig.config.mainnet.uniswapV2RouterAddress;
-      } else {
-        spender = UniswapConfig.config.kovan.uniswapV2RouterAddress;
-      }
-    } else {
-      spender = reqSpender;
-    }
-
-    return spender;
-  };
-
-  const getTokenSymbolsToTokens = (
-    tokenSymbols: Array<string>
-  ): Record<string, Token> => {
-    const tokens: Record<string, Token> = {};
-
-    for (let i = 0; i < tokenSymbols.length; i++) {
-      const symbol = tokenSymbols[i];
-      const token = ethereum.getTokenBySymbol(symbol);
-      if (!token) {
-        continue;
-      }
-
-      tokens[symbol] = token;
-    }
-
-    return tokens;
-  };
 
   router.post(
     '/allowances',
@@ -128,46 +73,11 @@ export namespace EthereumRoutes {
         req: Request<{}, {}, EthereumAllowancesRequest>,
         res: Response<EthereumAllowancesResponse | string, {}>
       ) => {
-        validateEthereumAllowancesRequest(req.body);
-
-        const initTime = Date.now();
-        const wallet = ethereum.getWallet(req.body.privateKey);
-
-        const tokens = getTokenSymbolsToTokens(req.body.tokenSymbols);
-
-        const spender = getSpender(req.body.spender);
-
-        const approvals: Record<string, string> = {};
-        await Promise.all(
-          Object.keys(tokens).map(async (symbol) => {
-            approvals[symbol] = tokenValueToString(
-              await ethereum.getERC20Allowance(
-                wallet,
-                spender,
-                tokens[symbol].address,
-                tokens[symbol].decimals
-              )
-            );
-          })
-        );
-
-        res.status(200).json({
-          network: ConfigManager.config.ETHEREUM_CHAIN,
-          timestamp: initTime,
-          latency: latency(initTime, Date.now()),
-          spender: spender,
-          approvals: approvals,
-        });
+        const response = await allowances(req.body);
+        res.status(200).json(response);
       }
     )
   );
-
-  interface EthereumBalanceResponse {
-    network: string;
-    timestamp: number;
-    latency: number;
-    balances: Record<string, string>; // the balance should be a string encoded number
-  }
 
   router.post(
     '/balances',
@@ -177,56 +87,10 @@ export namespace EthereumRoutes {
         res: Response<EthereumBalanceResponse | string, {}>,
         _next: NextFunction
       ) => {
-        validateEthereumBalanceRequest(req.body);
-
-        const initTime = Date.now();
-
-        let wallet: Wallet;
-        try {
-          wallet = ethereum.getWallet(req.body.privateKey);
-        } catch (err) {
-          throw new HttpException(500, 'Error getting wallet ' + err);
-        }
-
-        const tokens = getTokenSymbolsToTokens(req.body.tokenSymbols);
-
-        const balances: Record<string, string> = {};
-        balances.ETH = tokenValueToString(await ethereum.getEthBalance(wallet));
-        await Promise.all(
-          Object.keys(tokens).map(async (symbol) => {
-            if (tokens[symbol] !== undefined) {
-              const address = tokens[symbol].address;
-              const decimals = tokens[symbol].decimals;
-              const balance = await ethereum.getERC20Balance(
-                wallet,
-                address,
-                decimals
-              );
-              balances[symbol] = tokenValueToString(balance);
-            }
-          })
-        );
-
-        res.status(200).json({
-          network: ConfigManager.config.ETHEREUM_CHAIN,
-          timestamp: initTime,
-          latency: latency(initTime, Date.now()),
-          balances: balances,
-        });
+        res.status(200).json(await balances(req.body));
       }
     )
   );
-
-  export interface EthereumApproveResponse {
-    network: string;
-    timestamp: number;
-    latency: number;
-    tokenAddress: string;
-    spender: string;
-    amount: string;
-    nonce: number;
-    approval: Transaction;
-  }
 
   router.post(
     '/approve',
@@ -235,24 +99,11 @@ export namespace EthereumRoutes {
         req: Request<{}, {}, EthereumApproveRequest>,
         res: Response<EthereumApproveResponse | string, {}>
       ) => {
-        validateEthereumApproveRequest(req.body);
-
-        const { nonce, privateKey, token, amount } = req.body;
-        const spender = getSpender(req.body.spender);
-        const result = await approve(spender, privateKey, token, amount, nonce);
+        const result = await approve(req.body);
         return res.status(200).json(result);
       }
     )
   );
-
-  interface EthereumPollResponse {
-    network: string;
-    timestamp: number;
-    latency: number;
-    txHash: string;
-    confirmed: boolean;
-    receipt: EthereumTransactionReceipt | null;
-  }
 
   router.post(
     '/poll',
@@ -261,9 +112,7 @@ export namespace EthereumRoutes {
         req: Request<{}, {}, EthereumPollRequest>,
         res: Response<EthereumPollResponse, {}>
       ) => {
-        validateEthereumPollRequest(req.body);
-
-        const result = await poll(req.body.txHash);
+        const result = await poll(req.body);
         res.status(200).json(result);
       }
     )
