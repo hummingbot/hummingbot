d82a4e972d00c5b2ba81519d1f8cd68bf40681bd
Daniel Tan <daniel@hummingbot.io>
2021-10-06 18:32:13 +0800

    (refactor) enforce usage of safe_ensure_future() over asyncio.ensure_future()

--- hummingbot/connector/exchange/bittrex/bittrex_user_stream_tracker.py
+++ hummingbot/connector/exchange/bittrex/bittrex_user_stream_tracker.py
@@ -2,12 +2,15 @@
 
 import asyncio
 import logging
-from typing import Optional, List
-from hummingbot.core.data_type.user_stream_tracker_data_source import UserStreamTrackerDataSource
-from hummingbot.logger import HummingbotLogger
-from hummingbot.core.data_type.user_stream_tracker import UserStreamTracker
+
+from typing import List, Optional
+
 from hummingbot.connector.exchange.bittrex.bittrex_api_user_stream_data_source import BittrexAPIUserStreamDataSource
 from hummingbot.connector.exchange.bittrex.bittrex_auth import BittrexAuth
+from hummingbot.core.data_type.user_stream_tracker import UserStreamTracker
+from hummingbot.core.data_type.user_stream_tracker_data_source import UserStreamTrackerDataSource
+from hummingbot.core.utils.async_utils import safe_ensure_future
+from hummingbot.logger import HummingbotLogger
 
 
 class BittrexUserStreamTracker(UserStreamTracker):
@@ -43,7 +46,7 @@ class BittrexUserStreamTracker(UserStreamTracker):
         return "bittrex"
 
     async def start(self):
-        self._user_stream_tracking_task = asyncio.ensure_future(
+        self._user_stream_tracking_task = safe_ensure_future(
             self.data_source.listen_for_user_stream(self._ev_loop, self._user_stream)
         )
         await asyncio.gather(self._user_stream_tracking_task)
