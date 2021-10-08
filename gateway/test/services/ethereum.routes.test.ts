546ee3766e3315a8a11a7a99006c5ae268dece07
willaPa <willypaterson@gmail.com>
2021-10-08 11:36:25 -0300

    adds testing scenario

--- gateway/test/services/ethereum.routes.test.ts
+++ gateway/test/services/ethereum.routes.test.ts
@@ -4,7 +4,7 @@ import { app } from '../../src/app';
 import { Ethereum } from '../../src/chains/ethereum/ethereum';
 import * as transactionOutOfGas from './fixtures/transaction-out-of-gas.json';
 import * as transactionOutOfGasReceipt from './fixtures/transaction-out-of-gas-receipt.json';
-// import * as transactionUnconfirmedReceipt from './fixtures/transaction-unconfirmed-receipt.json';
+import * as transactionUnconfirmedReceipt from './fixtures/transaction-unconfirmed-receipt.json';
 
 const OUT_OF_GAS_ERROR_CODE = 1003;
 
@@ -40,9 +40,21 @@ describe('Eth endpoints', () => {
     expect(res.body.txData).toBeDefined();
   });
 
-  it('should get a null in txReceipt and txData for Tx that didnt reach the mempool', async () => {
+  it('should get a null in txReceipt and txData for Tx that didnt reach the mempool and TxReceipt is null', async () => {
     patch(eth, 'getTransaction', () => null);
     patch(eth, 'getTransactionReceipt', () => null);
+    const res = await request(app).post('/eth/poll').send({
+      txHash:
+        '0x2faeb1aa55f96c1db55f643a8cf19b0f76bf091d0b7d1b068d2e829414576362',
+    });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.txReceipt).toEqual(null);
+    expect(res.body.txData).toEqual(null);
+  });
+
+  it('should get a null in txReceipt and txData for Tx that didnt reach the mempool and TxReceipt is an object', async () => {
+    patch(eth, 'getTransaction', () => null);
+    patch(eth, 'getTransactionReceipt', () => transactionUnconfirmedReceipt);
     const res = await request(app).post('/eth/poll').send({
       txHash:
         '0x2faeb1aa55f96c1db55f643a8cf19b0f76bf091d0b7d1b068d2e829414576362',
