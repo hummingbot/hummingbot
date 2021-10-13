a285ab803a7b732d76e605318322a016b4d8bd24
vic-en <victoreni14@gmail.com>
2021-10-13 19:08:54 +0100

    update test to confirm ethers errors are handled by gateway

--- gateway/test/services/ethereum.routes.test.ts
+++ gateway/test/services/ethereum.routes.test.ts
@@ -1,4 +1,5 @@
 import request from 'supertest';
+import { logger, errors } from 'ethers';
 import { app } from '../../src/app';
 import { Ethereum } from '../../src/chains/ethereum/ethereum';
 import * as transactionOutOfGas from './fixtures/transaction-out-of-gas.json';
@@ -23,5 +24,42 @@ describe('Eth endpoints', () => {
     });
     expect(res.statusCode).toEqual(503);
     expect(res.body.errorCode).toEqual(OUT_OF_GAS_ERROR_CODE);
+  });
+  it('should get timeout error', async () => {
+    eth.getTransaction = jest.fn().mockImplementation(() => {
+      return logger.throwError(errors.TIMEOUT, errors.TIMEOUT);
+    });
+    const res = await request(app).post('/eth/poll').send({
+      txHash:
+        '0x2faeb1aa55f96c1db55f643a8cf19b0f76bf091d0b7d1b068d2e829414576362',
+    });
+    expect(res.statusCode).toEqual(500);
+    expect(res.body.message).toEqual(expect.stringContaining(errors.TIMEOUT));
+  });
+  it('should get network error', async () => {
+    eth.getTransaction = jest.fn().mockImplementation(() => {
+      return logger.throwError(errors.NETWORK_ERROR, errors.NETWORK_ERROR);
+    });
+    const res = await request(app).post('/eth/poll').send({
+      txHash:
+        '0x2faeb1aa55f96c1db55f643a8cf19b0f76bf091d0b7d1b068d2e829414576362',
+    });
+    expect(res.statusCode).toEqual(500);
+    expect(res.body.message).toEqual(
+      expect.stringContaining(errors.NETWORK_ERROR)
+    );
+  });
+  it('should get server error', async () => {
+    eth.getTransaction = jest.fn().mockImplementation(() => {
+      return logger.throwError(errors.SERVER_ERROR, errors.SERVER_ERROR);
+    });
+    const res = await request(app).post('/eth/poll').send({
+      txHash:
+        '0x2faeb1aa55f96c1db55f643a8cf19b0f76bf091d0b7d1b068d2e829414576362',
+    });
+    expect(res.statusCode).toEqual(500);
+    expect(res.body.message).toEqual(
+      expect.stringContaining(errors.SERVER_ERROR)
+    );
   });
 });
