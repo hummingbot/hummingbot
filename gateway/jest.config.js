e5eca56278e90c1229e5fb288eb3286a1b1d4d0c
james-hummingbot <james@hummingbot.io>
2021-10-28 21:45:03 +0200

    Ignore test files in dist, ignore services for coverage

--- gateway/jest.config.js
+++ gateway/jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  detectOpenHandles: true,
+  coveragePathIgnorePatterns: [
+    'src/app.ts',
+    'src/services/ethereum-base.ts',
+    'src/services/chains/ethereum/ethereum.ts',
+    'src/services/chains/ethereum/uniswap/uniswap.ts',
+  ],
 };
