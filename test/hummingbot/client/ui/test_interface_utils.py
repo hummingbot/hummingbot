cadee7e9012591c1b59244f1fc8e2cd4c5addfd3
Nullably <37262506+Nullably@users.noreply.github.com>
2021-11-03 10:41:51 +0800

    fix failing tests

--- test/hummingbot/client/ui/test_interface_utils.py
+++ test/hummingbot/client/ui/test_interface_utils.py
@@ -5,6 +5,10 @@ from unittest.mock import patch, MagicMock, AsyncMock
 from hummingbot.client.ui.interface_utils import start_trade_monitor, format_bytes, start_timer, start_process_monitor
 
 
+class ExpectedException(Exception):
+    pass
+
+
 class InterfaceUtilsTest(unittest.TestCase):
 
     def test_format_bytes(self):
@@ -15,10 +19,9 @@ class InterfaceUtilsTest(unittest.TestCase):
     @patch("hummingbot.client.ui.interface_utils._sleep", new_callable=AsyncMock)
     def test_start_timer(self, mock_sleep):
         mock_timer = MagicMock()
-        mock_sleep.side_effect = [None, Exception("returns")]
-        with self.assertRaises(Exception) as context:
+        mock_sleep.side_effect = [None, ExpectedException()]
+        with self.assertRaises(ExpectedException):
             asyncio.get_event_loop().run_until_complete(start_timer(mock_timer))
-        self.assertEqual('returns', str(context.exception))
         self.assertEqual('Duration: 0:00:02', mock_timer.log.call_args_list[0].args[0])
         self.assertEqual('Duration: 0:00:03', mock_timer.log.call_args_list[1].args[0])
 
@@ -29,10 +32,9 @@ class InterfaceUtilsTest(unittest.TestCase):
         mock_process.return_value.cpu_percent.return_value = 30
         mock_process.return_value.memory_info.return_value = [0, 1024.]
         mock_monitor = MagicMock()
-        mock_sleep.side_effect = Exception("returns")
-        with self.assertRaises(Exception) as context:
+        mock_sleep.side_effect = ExpectedException()
+        with self.assertRaises(ExpectedException):
             asyncio.get_event_loop().run_until_complete(start_process_monitor(mock_monitor))
-        self.assertEqual('returns', str(context.exception))
         self.assertEqual("CPU:    30%, Mem:   512.00 B, Threads:   2, ", mock_monitor.log.call_args_list[0].args[0])
 
     @patch("hummingbot.client.ui.interface_utils._sleep", new_callable=AsyncMock)
@@ -47,10 +49,9 @@ class InterfaceUtilsTest(unittest.TestCase):
         mock_app.get_current_balances = AsyncMock()
         mock_perf.side_effect = [MagicMock(return_pct=Decimal("0.01"), total_pnl=Decimal("2")),
                                  MagicMock(return_pct=Decimal("0.02"), total_pnl=Decimal("2"))]
-        mock_sleep.side_effect = [None, Exception("returns")]
-        with self.assertRaises(Exception) as context:
+        mock_sleep.side_effect = [None, ExpectedException()]
+        with self.assertRaises(ExpectedException):
             asyncio.get_event_loop().run_until_complete(start_trade_monitor(mock_result))
-        self.assertEqual('returns', str(context.exception))
         self.assertEqual(3, mock_result.log.call_count)
         self.assertEqual('Trades: 0, Total P&L: 0.00, Return %: 0.00%', mock_result.log.call_args_list[0].args[0])
         self.assertEqual('Trades: 1, Total P&L: 2.00 USDT, Return %: 1.00%', mock_result.log.call_args_list[1].args[0])
@@ -59,7 +60,7 @@ class InterfaceUtilsTest(unittest.TestCase):
     @patch("hummingbot.client.ui.interface_utils._sleep", new_callable=AsyncMock)
     @patch("hummingbot.client.ui.interface_utils.PerformanceMetrics.create", new_callable=AsyncMock)
     @patch("hummingbot.client.hummingbot_application.HummingbotApplication")
-    def test_sstart_trade_monitor_multi_pairs_diff_quotes(self, mock_hb_app, mock_perf, mock_sleep):
+    def test_start_trade_monitor_multi_pairs_diff_quotes(self, mock_hb_app, mock_perf, mock_sleep):
         mock_result = MagicMock()
         mock_app = mock_hb_app.main_application()
         mock_app.strategy_task.done.return_value = False
@@ -71,10 +72,9 @@ class InterfaceUtilsTest(unittest.TestCase):
         mock_app.get_current_balances = AsyncMock()
         mock_perf.side_effect = [MagicMock(return_pct=Decimal("0.01"), total_pnl=Decimal("2")),
                                  MagicMock(return_pct=Decimal("0.02"), total_pnl=Decimal("3"))]
-        mock_sleep.side_effect = Exception("returns")
-        with self.assertRaises(Exception) as context:
+        mock_sleep.side_effect = ExpectedException()
+        with self.assertRaises(ExpectedException):
             asyncio.get_event_loop().run_until_complete(start_trade_monitor(mock_result))
-        self.assertEqual('returns', str(context.exception))
         self.assertEqual(2, mock_result.log.call_count)
         self.assertEqual('Trades: 0, Total P&L: 0.00, Return %: 0.00%', mock_result.log.call_args_list[0].args[0])
         self.assertEqual('Trades: 2, Total P&L: N/A, Return %: 1.50%', mock_result.log.call_args_list[1].args[0])
@@ -94,10 +94,9 @@ class InterfaceUtilsTest(unittest.TestCase):
         mock_app.get_current_balances = AsyncMock()
         mock_perf.side_effect = [MagicMock(return_pct=Decimal("0.01"), total_pnl=Decimal("2")),
                                  MagicMock(return_pct=Decimal("0.02"), total_pnl=Decimal("3"))]
-        mock_sleep.side_effect = Exception("returns")
-        with self.assertRaises(Exception) as context:
+        mock_sleep.side_effect = ExpectedException()
+        with self.assertRaises(ExpectedException):
             asyncio.get_event_loop().run_until_complete(start_trade_monitor(mock_result))
-        self.assertEqual('returns', str(context.exception))
         self.assertEqual(2, mock_result.log.call_count)
         self.assertEqual('Trades: 0, Total P&L: 0.00, Return %: 0.00%', mock_result.log.call_args_list[0].args[0])
         self.assertEqual('Trades: 2, Total P&L: 5.00 USDT, Return %: 1.50%', mock_result.log.call_args_list[1].args[0])
@@ -109,10 +108,9 @@ class InterfaceUtilsTest(unittest.TestCase):
         mock_app = mock_hb_app.main_application()
         mock_app.strategy_task.done.return_value = False
         mock_app.markets.return_values = {"a": MagicMock(ready=False)}
-        mock_sleep.side_effect = Exception("returns")
-        with self.assertRaises(Exception) as context:
+        mock_sleep.side_effect = ExpectedException()
+        with self.assertRaises(ExpectedException):
             asyncio.get_event_loop().run_until_complete(start_trade_monitor(mock_result))
-        self.assertEqual('returns', str(context.exception))
         self.assertEqual(1, mock_result.log.call_count)
         self.assertEqual('Trades: 0, Total P&L: 0.00, Return %: 0.00%', mock_result.log.call_args_list[0].args[0])
 
@@ -124,9 +122,8 @@ class InterfaceUtilsTest(unittest.TestCase):
         mock_app.strategy_task.done.return_value = False
         mock_app.markets.return_values = {"a": MagicMock(ready=True)}
         mock_app._get_trades_from_session.return_value = []
-        mock_sleep.side_effect = Exception("returns")
-        with self.assertRaises(Exception) as context:
+        mock_sleep.side_effect = ExpectedException()
+        with self.assertRaises(ExpectedException):
             asyncio.get_event_loop().run_until_complete(start_trade_monitor(mock_result))
-        self.assertEqual('returns', str(context.exception))
         self.assertEqual(1, mock_result.log.call_count)
         self.assertEqual('Trades: 0, Total P&L: 0.00, Return %: 0.00%', mock_result.log.call_args_list[0].args[0])
