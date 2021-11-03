55ffeebbf3b9b7a3452cfc5d7a703e9329ab9748
Nullably <37262506+Nullably@users.noreply.github.com>
2021-11-03 16:21:37 +0800

    (feat) fix dynamic imports

--- hummingbot/client/tab/__init__.py
+++ hummingbot/client/tab/__init__.py
@@ -5,5 +5,5 @@ __globals = globals()
 
 for file in os.listdir(os.path.dirname(__file__)):
     mod_name = file[:-3]
-    if not file.startswith("_"):
+    if not file.startswith("_") and file[-2:] == "py":
         __globals[mod_name] = importlib.import_module('.' + mod_name, package=__name__)
