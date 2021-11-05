f0a6c8ec611f6979219c5dec52c7fc067fae8f13
james-hummingbot <james@hummingbot.io>
2021-11-05 11:21:49 +0100

    Increment coverage

--- gateway/jest.config.js
+++ gateway/jest.config.js
@@ -8,6 +8,8 @@ module.exports = {
     'src/services/ethereum-base.ts',
     'src/chains/ethereum/ethereum.ts',
     'src/chains/ethereum/uniswap/uniswap.ts',
+    'src/chains/ethereum/avalanche.ts',
+    'src/chains/ethereum/pangolin/pangolin.ts',
   ],
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
 };
