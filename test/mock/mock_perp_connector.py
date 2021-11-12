a7be4267ac01579c70b5086f4173d5376c117af5
abel <abel@hummingbot.io>
2021-11-12 18:07:30 -0300

    (feat) Removed unnecessary logic form perpetual market making. Traslated the strategy to pure Python

--- test/mock/mock_perp_connector.py
+++ test/mock/mock_perp_connector.py
@@ -1,11 +1,14 @@
+from decimal import Decimal
+
 from hummingbot.connector.perpetual_trading import PerpetualTrading
 from hummingbot.core.event.events import PositionMode
 from test.mock.mock_paper_exchange import MockPaperExchange
 
 
 class MockPerpConnector(MockPaperExchange, PerpetualTrading):
-    def __init__(self):
-        MockPaperExchange.__init__(self)
+
+    def __init__(self, fee_percent: Decimal = Decimal("0")):
+        MockPaperExchange.__init__(self, fee_percent=fee_percent)
         PerpetualTrading.__init__(self)
         self._funding_payment_span = [0, 10]
 
