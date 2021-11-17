7ef8af6abd642ca76fb7e4b78296a2d5273d5b93
Matej Horvath <contact@mhrvth.net>
2021-11-17 14:15:50 +0100

    (refactor) min_spread, volatility_buffer_size

--- hummingbot/strategy/avellaneda_market_making/avellaneda_market_making.pyx
+++ hummingbot/strategy/avellaneda_market_making/avellaneda_market_making.pyx
@@ -122,7 +122,7 @@ cdef class AvellanedaMarketMakingStrategy(StrategyBase):
 
         self.c_add_markets([market_info.market])
         self._ticks_to_be_ready = max(volatility_buffer_size, trading_intensity_buffer_size)
-        self._avg_vol = InstantVolatilityIndicator()
+        self._avg_vol = InstantVolatilityIndicator(sampling_length=volatility_buffer_size)
         self._trading_intensity = TradingIntensityIndicator(trading_intensity_buffer_size)
         self._last_sampling_timestamp = 0
         self._alpha = None
@@ -709,8 +709,10 @@ cdef class AvellanedaMarketMakingStrategy(StrategyBase):
             self._optimal_spread = self._gamma * mid_price_variance * time_left_fraction
             self._optimal_spread += 2 * Decimal(1 + self._gamma / self._kappa).ln() / self._gamma
 
-            max_limit_bid = price - Decimal(str(self._min_spread / 2))
-            min_limit_ask = price + Decimal(str(self._min_spread / 2))
+            min_spread = price / 100 * self._min_spread
+
+            max_limit_bid = price - Decimal(str(min_spread / 2))
+            min_limit_ask = price + Decimal(str(min_spread / 2))
 
             self._optimal_ask = max(self._reserved_price + self._optimal_spread / 2, min_limit_ask)
             self._optimal_bid = min(self._reserved_price - self._optimal_spread / 2, max_limit_bid)
