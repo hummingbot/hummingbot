cb678e4ad14775e2536495913844cb2d330104a8
james-hummingbot <james@hummingbot.io>
2021-11-26 16:18:04 +0100

    complete generate config2 file

--- gateway/setup/config2.sh
+++ gateway/setup/config2.sh
@@ -3,7 +3,7 @@
 
 echo
 echo
-echo "===============  GENERATE GATEWAY CONFIGURATION FILE(S) ==============="
+echo "===============  GENERATE GATEWAY CONFIGURATION FILES ==============="
 echo
 echo
 
@@ -25,7 +25,7 @@ echo "Infura API Key is $INFURA_KEY."
 prompt_infura_api_key
 
 prompt_eth_gas_station_api_key () {
-read -p "Enter Eth Gas Station API Key (required for Ethereum, if you do not have one, one at https://ethgasstation.info/): " ETH_GAS_STATION_KEY
+read -p "Enter Eth Gas Station API Key (required for Ethereum, if you do not have one, one at https://ethgasstation.info): " ETH_GAS_STATION_KEY
 echo "Infura API Key is $ETH_GAS_STATION_KEY."
 }
 prompt_eth_gas_station_api_key
@@ -49,25 +49,52 @@ prompt_to_allow_telemetry
 
 CONFIG_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/../conf"
 
-# generate the telemetry file
-echo "enabled: $TELEMETRY" > "$CONFIG_PATH/telemetry.yml"
+
+# generate ethereum file
+echo "networks:" > "$CONFIG_PATH/ethereum.yml"
+echo "  mainnet:" >> "$CONFIG_PATH/ethereum.yml"
+echo "    chainID: 1" >> "$CONFIG_PATH/ethereum.yml"
+echo "    nodeURL: https://kovan.infura.io/v3/" >> "$CONFIG_PATH/ethereum.yml"
+echo "    nodeApiKey: $INFURA_KEY" >> "$CONFIG_PATH/ethereum.yml"
+echo "  kovan:" >> "$CONFIG_PATH/ethereum.yml"
+echo "    chainID: 42" >> "$CONFIG_PATH/ethereum.yml"
+echo "    nodeURL: https://kovan.infura.io/v3/" >> "$CONFIG_PATH/ethereum.yml"
+echo "    nodeApiKey: $INFURA_KEY" >> "$CONFIG_PATH/ethereum.yml"
+echo "created $CONFIG_PATH/ethereum.yml"
 
 # generate ssl file
 echo "caCertificatePath: $CERT_PATH/ca_cert.pem"   > "$CONFIG_PATH/ssl.yml"
 echo "certificatePath: $CERT_PATH/server_cert.pem" >> "$CONFIG_PATH/ssl.yml"
 echo "keyPath: $CERT_PATH/server_key.pem"          >> "$CONFIG_PATH/ssl.yml"
 echo "passPhrasePath: $CONFIG_PATH/gateway-passphrase.yml" >> "$CONFIG_PATH/ssl.yml"
+echo "created $CONFIG_PATH/ssl.yml"
 
-# update apiKey in the ethereum file
+# update apiKey in the ethereum-gas-station file
+cp "$CONFIG_PATH/samples/ethereum-gas-station.yml" "$CONFIG_PATH/ethereum-gas-station.yml"
 sed -i '/apiKey: .*/d;/^ *$/d' "$CONFIG_PATH/ethereum-gas-station.yml"
 echo "apiKey: '$ETH_GAS_STATION_KEY'" >> "$CONFIG_PATH/ethereum-gas-station.yml"
+echo "created $CONFIG_PATH/ethereum-gas-station.yml"
 
 # copy the following files
 
 cp "$CONFIG_PATH/samples/avalanche.yml" "$CONFIG_PATH/avalanche.yml"
-cp "$CONFIG_PATH/samples/ethereum.yml" "$CONFIG_PATH/ethereum.yml"
+echo "created $CONFIG_PATH/avalanche.yml"
+
 cp "$CONFIG_PATH/samples/logging.yml" "$CONFIG_PATH/logging.yml"
+echo "created $CONFIG_PATH/logging.yml"
+
 cp "$CONFIG_PATH/samples/pangolin.yml" "$CONFIG_PATH/pangolin.yml"
+echo "created $CONFIG_PATH/pangolin.yml"
+
 cp "$CONFIG_PATH/samples/root.yml" "$CONFIG_PATH/root.yml"
+echo "created $CONFIG_PATH/root.yml"
+
 cp "$CONFIG_PATH/samples/server.yml" "$CONFIG_PATH/server.yml"
+echo "created $CONFIG_PATH/server.yml"
+
 cp "$CONFIG_PATH/samples/uniswap.yml" "$CONFIG_PATH/uniswap.yml"
+echo "created $CONFIG_PATH/uniswap.yml"
+
+# generate the telemetry file
+echo "enabled: $TELEMETRY" > "$CONFIG_PATH/telemetry.yml"
+echo "created $CONFIG_PATH/telemetry.yml"
