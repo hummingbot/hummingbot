9324680098fe4d667e99ca6f03fe8f9fee217b27
james-hummingbot <james@hummingbot.io>
2021-11-30 19:28:07 +0100

    Refactor local-storage to allow for cleaner testing

--- gateway/src/services/local-storage.ts
+++ gateway/src/services/local-storage.ts
@@ -1,52 +1,64 @@
 import { LevelDB } from 'level';
 const level = require('level-party');
 
-const db: LevelDB = level('gateway.level', { createIfMissing: true });
+export class LocalStorage {
+  readonly #dbPath: string;
+  #db: LevelDB;
 
-export async function dbSaveNonce(
-  chain: string,
-  chainId: number,
-  address: string,
-  nonce: number
-): Promise<void> {
-  return db.put(chain + '/' + String(chainId) + '/' + address, nonce);
-}
+  constructor(dbPath: string = 'gateway.level') {
+    this.#dbPath = dbPath;
+    this.#db = level(dbPath, { createIfMissing: true });
+  }
 
-export async function dbDeleteNonce(
-  chain: string,
-  chainId: number,
-  address: string
-): Promise<void> {
-  return db.del(chain + '/' + String(chainId) + '/' + address);
-}
+  get dbPath(): string {
+    return this.#dbPath;
+  }
 
-export async function dbGetChainNonces(
-  chain: string,
-  chainId: number
-): Promise<Record<string, number>> {
-  const stream = db.createReadStream();
-  const result = await new Promise<Record<string, number>>(
-    (resolve, reject) => {
-      const results: Record<string, number> = {};
-      stream
-        .on('data', ({ key, value }) => {
-          const splitKey = key.split('/');
-          if (
-            splitKey.length === 3 &&
-            splitKey[0] === chain &&
-            splitKey[1] === String(chainId)
-          ) {
-            results[splitKey[2]] = parseInt(value);
-          }
-        })
-        .on('error', (err) => {
-          reject(err);
-        })
-        .on('end', () => {
-          resolve(results);
-        });
-    }
-  );
+  async saveNonce(
+    chain: string,
+    chainId: number,
+    address: string,
+    nonce: number
+  ): Promise<void> {
+    return this.#db.put(chain + '/' + String(chainId) + '/' + address, nonce);
+  }
 
-  return result;
+  async deleteNonce(
+    chain: string,
+    chainId: number,
+    address: string
+  ): Promise<void> {
+    return this.#db.del(chain + '/' + String(chainId) + '/' + address);
+  }
+
+  async getChainNonces(
+    chain: string,
+    chainId: number
+  ): Promise<Record<string, number>> {
+    const stream = this.#db.createReadStream();
+    const result = await new Promise<Record<string, number>>(
+      (resolve, reject) => {
+        const results: Record<string, number> = {};
+        stream
+          .on('data', ({ key, value }) => {
+            const splitKey = key.split('/');
+            if (
+              splitKey.length === 3 &&
+              splitKey[0] === chain &&
+              splitKey[1] === String(chainId)
+            ) {
+              results[splitKey[2]] = parseInt(value);
+            }
+          })
+          .on('error', (err) => {
+            reject(err);
+          })
+          .on('end', () => {
+            resolve(results);
+          });
+      }
+    );
+
+    return result;
+  }
 }
--- gateway/test/services/local-storage.test.ts
+++ gateway/test/services/local-storage.test.ts
@@ -1,24 +1,22 @@
-import {
-  dbDeleteNonce,
-  dbSaveNonce,
-  dbGetChainNonces,
-} from '../../src/services/local-storage';
+import { LocalStorage } from '../../src/services/local-storage';
 import 'jest-extended';
 
 describe('Test local-storage', () => {
-  it('store with dbSaveNonce and retrieve with dbGetChainNonces', async () => {
+  it('store with saveNonce and retrieve with getChainNonces', async () => {
     const testChain = 'testchain';
     const testChainId = 123;
     const testAddress = 'testaddress';
     const testValue = 541;
 
+    const db = new LocalStorage('/tmp/local-storage.test.level');
+
     // clean up any previous db runs
-    await dbDeleteNonce(testChain, testChainId, testAddress);
+    await db.deleteNonce(testChain, testChainId, testAddress);
 
     // saves with a combination of chain/id/address
-    await dbSaveNonce(testChain, testChainId, testAddress, testValue);
+    await db.saveNonce(testChain, testChainId, testAddress, testValue);
 
-    const results = await dbGetChainNonces(testChain, testChainId);
+    const results = await db.getChainNonces(testChain, testChainId);
     // returns with an address as key, the chain/id is known by the parameters you provide
     expect(results).toEqual({
       [testAddress]: testValue,
