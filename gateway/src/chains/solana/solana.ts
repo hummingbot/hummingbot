cf380ab55b5c79497d779b1abe7b5b2b1006e06f
Gott50 <xxGott50xx@gmail.com>
2021-12-12 16:49:12 +0100

    fix linter errors

--- gateway/src/chains/solana/solana.ts
+++ gateway/src/chains/solana/solana.ts
@@ -1,7 +1,7 @@
 import { logger } from '../../services/logger';
 import { ConfigManager } from '../../services/config-manager';
-import { TokenValue, countDecimals} from '../../services/base';
-import NodeCache from "node-cache";
+import { TokenValue, countDecimals } from '../../services/base';
+import NodeCache from 'node-cache';
 import bs58 from 'bs58';
 import { BigNumber } from 'ethers';
 import {
@@ -12,13 +12,14 @@ import {
   TokenAmount,
   LogsFilter,
   LogsCallback,
-  SlotUpdateCallback, TransactionResponse
-} from "@solana/web3.js";
+  SlotUpdateCallback,
+  TransactionResponse,
+} from '@solana/web3.js';
 import {
   Token as TokenProgram,
-  AccountInfo as TokenAccount
-} from "@solana/spl-token";
-import {TokenListProvider, TokenInfo} from "@solana/spl-token-registry";
+  AccountInfo as TokenAccount,
+} from '@solana/spl-token';
+import { TokenListProvider, TokenInfo } from '@solana/spl-token-registry';
 
 export class Solana {
   public rpcUrl;
@@ -44,96 +45,96 @@ export class Solana {
   private _initPromise: Promise<void> = Promise.resolve();
 
   constructor() {
-    this._cluster = ConfigManager.config.SOLANA_CLUSTER
+    this._cluster = ConfigManager.config.SOLANA_CLUSTER;
 
-    if(ConfigManager.config.SOLANA_CUSTOM_RPC == null) {
+    if (ConfigManager.config.SOLANA_CUSTOM_RPC == null) {
       switch (this._cluster) {
         case 'mainnet-beta':
-          this.rpcUrl = 'https://mainnet.infura.io/v3/'
+          this.rpcUrl = 'https://mainnet.infura.io/v3/';
           break;
         case 'devnet':
-          this.rpcUrl = 'https://api.devnet.solana.com'
+          this.rpcUrl = 'https://api.devnet.solana.com';
           break;
         case 'testnet':
-          this.rpcUrl = 'https://api.testnet.solana.com'
+          this.rpcUrl = 'https://api.testnet.solana.com';
           break;
         default:
-          throw new Error('SOLANA_CHAIN not valid')
+          throw new Error('SOLANA_CHAIN not valid');
       }
-    }
-    else
-    {
-      this.rpcUrl = ConfigManager.config.SOLANA_CUSTOM_RPC
+    } else {
+      this.rpcUrl = ConfigManager.config.SOLANA_CUSTOM_RPC;
     }
 
-    this._connection = new Connection(this.rpcUrl, "processed" as Commitment)
-    this.cache = new NodeCache({ stdTTL: 3600 }) // set default cache ttl to 1hr
+    this._connection = new Connection(this.rpcUrl, 'processed' as Commitment);
+    this.cache = new NodeCache({ stdTTL: 3600 }); // set default cache ttl to 1hr
 
-    this._nativeTokenSymbol = 'SOL'
-    this._tokenProgramAddress = new PublicKey(ConfigManager.config.SOLANA_TOKEN_PROGRAM)
+    this._nativeTokenSymbol = 'SOL';
+    this._tokenProgramAddress = new PublicKey(
+      ConfigManager.config.SOLANA_TOKEN_PROGRAM
+    );
 
-    this.transactionLamports = ConfigManager.config.SOLANA_TRANSACTION_LAMPORTS
-    this._lamportPrice = ConfigManager.config.SOLANA_LAMPORTS_TO_SOL
-    this._lamportDecimals = countDecimals(this._lamportPrice)
+    this.transactionLamports = ConfigManager.config.SOLANA_TRANSACTION_LAMPORTS;
+    this._lamportPrice = ConfigManager.config.SOLANA_LAMPORTS_TO_SOL;
+    this._lamportDecimals = countDecimals(this._lamportPrice);
 
-    this._requestCount = 0
-    this._metricsLogInterval = 300000 // 5 minutes
+    this._requestCount = 0;
+    this._metricsLogInterval = 300000; // 5 minutes
 
-    this.onDebugMessage('all', this.requestCounter.bind(this))
-    setInterval(this.metricLogger.bind(this), this.metricsLogInterval)
+    this.onDebugMessage('all', this.requestCounter.bind(this));
+    setInterval(this.metricLogger.bind(this), this.metricsLogInterval);
   }
 
   public static getInstance(): Solana {
     if (!Solana._instance) {
-      Solana._instance = new Solana()
+      Solana._instance = new Solana();
     }
 
-    return Solana._instance
+    return Solana._instance;
   }
 
   public static reload(): Solana {
-    Solana._instance = new Solana()
-    return Solana._instance
+    Solana._instance = new Solana();
+    return Solana._instance;
   }
 
   ready(): boolean {
-    return this._ready
+    return this._ready;
   }
 
   public get connection() {
-    return this._connection
+    return this._connection;
   }
 
   public onNewSlot(func: SlotUpdateCallback) {
-    this._connection.onSlotUpdate(func)
+    this._connection.onSlotUpdate(func);
   }
 
   public onDebugMessage(filter: LogsFilter, func: LogsCallback) {
-    this._connection.onLogs(filter, func)
+    this._connection.onLogs(filter, func);
   }
 
   async init(): Promise<void> {
     if (!this.ready() && !this._initializing) {
-      this._initializing = true
+      this._initializing = true;
       this._initPromise = this.loadTokens().then(() => {
-        this._ready = true
-        this._initializing = false
-      })
+        this._ready = true;
+        this._initializing = false;
+      });
     }
-    return this._initPromise
+    return this._initPromise;
   }
 
   async loadTokens(): Promise<void> {
     this.tokenList = await this.getTokenList();
     this.tokenList.forEach(
       (token: TokenInfo) => (this._tokenMap[token.symbol] = token)
-    )
+    );
   }
 
   // returns a Tokens for a given list source and list type
   async getTokenList(): Promise<TokenInfo[]> {
-    const tokens = await new TokenListProvider().resolve()
-    return tokens.filterByClusterSlug(this._cluster).getList()
+    const tokens = await new TokenListProvider().resolve();
+    return tokens.filterByClusterSlug(this._cluster).getList();
   }
 
   // returns the price of 1 lamport in SOL
@@ -159,15 +160,18 @@ export class Solana {
     return Keypair.fromSecretKey(decoded);
   }
 
-  async getBalance(wallet: Keypair): Promise<string, TokenValue>
+  async getBalance(wallet: Keypair): Promise<string, TokenValue>;
   // returns the SOL balance, convert BigNumber to string
   async getSolBalance(wallet: Keypair): Promise<TokenValue> {
     const lamports = await this.connection.getBalance(wallet.publicKey);
-    return {value: BigNumber.from(lamports), decimals: this._lamportDecimals}
+    return { value: BigNumber.from(lamports), decimals: this._lamportDecimals };
   }
 
   tokenResponseToTokenValue(account: TokenAmount): TokenValue {
-    return { value: BigNumber.from(account.amount), decimals: account.decimals };
+    return {
+      value: BigNumber.from(account.amount),
+      decimals: account.decimals,
+    };
   }
 
   // returns the balance for an SPL token
@@ -175,11 +179,16 @@ export class Solana {
     wallet: Keypair,
     mintAddress: PublicKey
   ): Promise<TokenValue> {
-    const response = await this.connection.getParsedTokenAccountsByOwner(wallet.publicKey, {mint: mintAddress});
-    if(response['value'].length == 0) {
+    const response = await this.connection.getParsedTokenAccountsByOwner(
+      wallet.publicKey,
+      { mint: mintAddress }
+    );
+    if (response['value'].length == 0) {
       throw new Error(`Token account not initialized`);
     }
-    return this.tokenResponseToTokenValue(response.value[0].account.data.parsed['info']['tokenAmount']);
+    return this.tokenResponseToTokenValue(
+      response.value[0].account.data.parsed['info']['tokenAmount']
+    );
   }
 
   // returns whether the token account is initialized, given its mint address
@@ -188,11 +197,14 @@ export class Solana {
     mintAddress: PublicKey
   ): Promise<boolean> {
     const response = await this.connection.getParsedTokenAccountsByOwner(
-        wallet.publicKey,
-        {programId: this._tokenProgramAddress}
-    )
-    for(let accountInfo of response.value) {
-      if(accountInfo.account.data.parsed['info']['mint'] == mintAddress.toBase58())
+      wallet.publicKey,
+      { programId: this._tokenProgramAddress }
+    );
+    for (const accountInfo of response.value) {
+      if (
+        accountInfo.account.data.parsed['info']['mint'] ==
+        mintAddress.toBase58()
+      )
         return true;
     }
     return false;
@@ -200,13 +212,25 @@ export class Solana {
 
   // Gets token account information, or creates a new token account for given token mint address
   // if needed, which costs 0.035 SOL
-  async getOrCreateAssociatedTokenAccount(wallet: Keypair, tokenAddress: PublicKey): Promise<TokenAccount> {
-    const tokenProgram = new TokenProgram(this._connection, tokenAddress, this._tokenProgramAddress, wallet)
-    return await tokenProgram.getOrCreateAssociatedAccountInfo(wallet.publicKey)
+  async getOrCreateAssociatedTokenAccount(
+    wallet: Keypair,
+    tokenAddress: PublicKey
+  ): Promise<TokenAccount> {
+    const tokenProgram = new TokenProgram(
+      this._connection,
+      tokenAddress,
+      this._tokenProgramAddress,
+      wallet
+    );
+    return await tokenProgram.getOrCreateAssociatedAccountInfo(
+      wallet.publicKey
+    );
   }
 
   // returns an ethereum TransactionResponse for a txHash.
-  async getTransaction(payerSignature: string): Promise<TransactionResponse | null> {
+  async getTransaction(
+    payerSignature: string
+  ): Promise<TransactionResponse | null> {
     return this._connection.getTransaction(payerSignature);
   }
 
@@ -218,7 +242,8 @@ export class Solana {
 
   public getTokenBySymbol(tokenSymbol: string): TokenInfo | undefined {
     return this.tokenList.find(
-      (token: TokenInfo) => token.symbol.toUpperCase() === tokenSymbol.toUpperCase()
+      (token: TokenInfo) =>
+        token.symbol.toUpperCase() === tokenSymbol.toUpperCase()
     );
   }
 
