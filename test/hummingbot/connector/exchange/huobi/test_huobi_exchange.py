c3819969441b8034895bb8950d31aa42c085af40
abel <abel@hummingbot.io>
2021-12-14 18:25:37 -0300

    (fix) Added logic for complete event to check all fills have been processed before raising the complete event

--- test/hummingbot/connector/exchange/huobi/test_huobi_exchange.py
+++ test/hummingbot/connector/exchange/huobi/test_huobi_exchange.py
@@ -1,7 +1,6 @@
 import asyncio
-import functools
 from decimal import Decimal
-from typing import Awaitable, Callable, Optional
+from typing import Awaitable, Optional
 from unittest import TestCase
 from unittest.mock import AsyncMock
 
@@ -10,6 +9,7 @@ import hummingbot.connector.exchange.huobi.huobi_constants as CONSTANTS
 from hummingbot.connector.exchange.huobi.huobi_exchange import HuobiExchange
 from hummingbot.core.event.event_logger import EventLogger
 from hummingbot.core.event.events import (
+    BuyOrderCompletedEvent,
     MarketEvent,
     OrderFilledEvent,
     OrderType,
@@ -35,7 +35,6 @@ class HuobiExchangeTests(TestCase):
 
         self.log_records = []
         self.test_task: Optional[asyncio.Task] = None
-        self.resume_test_event = asyncio.Event()
 
         self.exchange = HuobiExchange(
             huobi_api_key="testAPIKey",
@@ -74,12 +73,6 @@ class HuobiExchangeTests(TestCase):
     def async_run_with_timeout(self, coroutine: Awaitable, timeout: float = 1):
         ret = asyncio.get_event_loop().run_until_complete(asyncio.wait_for(coroutine, timeout))
         return ret
-
-    def _return_calculation_and_set_done_event(self, calculation: Callable, *args, **kwargs):
-        if self.resume_test_event.is_set():
-            raise asyncio.CancelledError
-        self.resume_test_event.set()
-        return calculation(*args, **kwargs)
 
     def test_order_fill_event_takes_fee_from_update_event(self):
         self.exchange.start_tracking_order(
@@ -123,13 +116,15 @@ class HuobiExchangeTests(TestCase):
         }
 
         mock_user_stream = AsyncMock()
-        mock_user_stream.get.side_effect = functools.partial(self._return_calculation_and_set_done_event,
-                                                             lambda: message)
+        mock_user_stream.get.side_effect = [message, asyncio.CancelledError()]
 
         self.exchange.user_stream_tracker._user_stream = mock_user_stream
 
         self.test_task = asyncio.get_event_loop().create_task(self.exchange._user_stream_event_listener())
-        self.async_run_with_timeout(self.resume_test_event.wait())
+        try:
+            self.async_run_with_timeout(self.test_task)
+        except asyncio.CancelledError:
+            pass
 
         self.assertEqual(Decimal("10"), order.fee_paid)
         self.assertEqual(1, len(self.order_filled_logger.event_log))
@@ -170,15 +165,16 @@ class HuobiExchangeTests(TestCase):
 
         message["data"] = complete_fill
 
-        self.resume_test_event = asyncio.Event()
         mock_user_stream = AsyncMock()
-        mock_user_stream.get.side_effect = functools.partial(self._return_calculation_and_set_done_event,
-                                                             lambda: message)
+        mock_user_stream.get.side_effect = [message, asyncio.CancelledError()]
 
         self.exchange.user_stream_tracker._user_stream = mock_user_stream
 
         self.test_task = asyncio.get_event_loop().create_task(self.exchange._user_stream_event_listener())
-        self.async_run_with_timeout(self.resume_test_event.wait())
+        try:
+            self.async_run_with_timeout(self.test_task)
+        except asyncio.CancelledError:
+            pass
 
         self.assertEqual(Decimal("40"), order.fee_paid)
 
@@ -195,3 +191,106 @@ class HuobiExchangeTests(TestCase):
         ))
 
         self.assertEqual(0, len(self.buy_order_completed_logger.event_log))
+
+    def test_order_fill_event_processed_before_order_complete_event(self):
+        self.exchange.start_tracking_order(
+            order_id="OID1",
+            exchange_order_id="99998888",
+            trading_pair=self.trading_pair,
+            order_type=OrderType.LIMIT,
+            trade_type=TradeType.BUY,
+            price=Decimal("10000"),
+            amount=Decimal("1"),
+        )
+
+        order = self.exchange.in_flight_orders.get("OID1")
+
+        complete_fill = {
+            "eventType": "trade",
+            "symbol": "choinalphahbot",
+            "orderId": 99998888,
+            "tradePrice": "10060.0",
+            "tradeVolume": "1",
+            "orderSide": "buy",
+            "aggressor": True,
+            "tradeId": 1,
+            "tradeTime": 998787897878,
+            "transactFee": "30.0",
+            "feeDeduct ": "0",
+            "feeDeductType": "",
+            "feeCurrency": "usdt",
+            "accountId": 9912791,
+            "source": "spot-api",
+            "orderPrice": "10000",
+            "orderSize": "1",
+            "clientOrderId": "OID1",
+            "orderCreateTime": 998787897878,
+            "orderStatus": "partial-filled"
+        }
+
+        fill_message = {
+            "ch": CONSTANTS.HUOBI_TRADE_DETAILS_TOPIC,
+            "data": complete_fill
+        }
+
+        update_data = {
+            "tradePrice": "10060.0",
+            "tradeVolume": "1",
+            "tradeId": 1,
+            "tradeTime": 1583854188883,
+            "aggressor": True,
+            "remainAmt": "0.0",
+            "execAmt": "1",
+            "orderId": 99998888,
+            "type": "buy-limit",
+            "clientOrderId": "OID1",
+            "orderSource": "spot-api",
+            "orderPrice": "10000",
+            "orderSize": "1",
+            "orderStatus": "filled",
+            "symbol": "btcusdt",
+            "eventType": "trade"
+        }
+
+        update_message = {
+            "action": "push",
+            "ch": CONSTANTS.HUOBI_ORDER_UPDATE_TOPIC,
+            "data": update_data,
+        }
+
+        mock_user_stream = AsyncMock()
+        # We simulate the case when the order update arrives before the order fill
+        mock_user_stream.get.side_effect = [update_message, fill_message, asyncio.CancelledError()]
+
+        self.exchange.user_stream_tracker._user_stream = mock_user_stream
+
+        self.test_task = asyncio.get_event_loop().create_task(self.exchange._user_stream_event_listener())
+        try:
+            self.async_run_with_timeout(self.test_task)
+        except asyncio.CancelledError:
+            pass
+
+        self.async_run_with_timeout(order.wait_until_completely_filled())
+
+        self.assertEqual(Decimal("30"), order.fee_paid)
+        self.assertEqual(1, len(self.order_filled_logger.event_log))
+        fill_event: OrderFilledEvent = self.order_filled_logger.event_log[0]
+        self.assertEqual(0.0, fill_event.trade_fee.percent)
+        self.assertEqual([(complete_fill["feeCurrency"].upper(), Decimal(complete_fill["transactFee"]))],
+                         fill_event.trade_fee.flat_fees)
+        self.assertTrue(self._is_logged(
+            "INFO",
+            f"Filled {Decimal(complete_fill['tradeVolume'])} out of {order.amount} of order "
+            f"{order.order_type.name}-{order.client_order_id}"
+        ))
+
+        self.assertTrue(self._is_logged(
+            "INFO",
+            f"The {order.trade_type.name} order {order.client_order_id} "
+            f"has completed according to order delta websocket API."
+        ))
+
+        self.assertEqual(1, len(self.buy_order_completed_logger.event_log))
+        buy_event: BuyOrderCompletedEvent = self.buy_order_completed_logger.event_log[0]
+        self.assertEqual(complete_fill["feeCurrency"].upper(), buy_event.fee_asset)
+        self.assertEqual(Decimal(complete_fill["transactFee"]), buy_event.fee_amount)
