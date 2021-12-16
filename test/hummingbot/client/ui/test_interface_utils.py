cf76b827ea7a057308113951d86c0ae69dd9e79f
Petio Petrov <petioptrv@icloud.com>
2021-12-16 15:58:10 +0700

    (tests) Adds test cases for the fix.

--- test/hummingbot/client/ui/test_interface_utils.py
+++ test/hummingbot/client/ui/test_interface_utils.py
@@ -1,6 +1,7 @@
 import unittest
 from decimal import Decimal
 import asyncio
+from typing import Awaitable
 from unittest.mock import patch, MagicMock, AsyncMock, PropertyMock
 from hummingbot.client.ui.interface_utils import start_trade_monitor, format_bytes, start_timer, start_process_monitor
 
@@ -10,6 +11,23 @@ class ExpectedException(Exception):
 
 
 class InterfaceUtilsTest(unittest.TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.ev_loop = asyncio.get_event_loop()
+        self.return_queue = asyncio.Queue()
+        self.done_event = asyncio.Event()
+
+    def async_run_with_timeout(self, coroutine: Awaitable, timeout: float = 1):
+        ret = self.ev_loop.run_until_complete(asyncio.wait_for(coroutine, timeout))
+        return ret
+
+    async def return_until_empty_then_signal_done_and_release_loop(self, *args, **kwargs):
+        if not self.return_queue.empty():
+            ret = self.return_queue.get_nowait()
+        else:
+            self.done_event.set()
+            await asyncio.sleep(0.3)  # simply to release loop — the 0.3 seconds are not actually awaited
+        return ret
 
     def test_format_bytes(self):
         size = 1024.
@@ -19,9 +37,11 @@ class InterfaceUtilsTest(unittest.TestCase):
     @patch("hummingbot.client.ui.interface_utils._sleep", new_callable=AsyncMock)
     def test_start_timer(self, mock_sleep):
         mock_timer = MagicMock()
-        mock_sleep.side_effect = [None, ExpectedException()]
-        with self.assertRaises(ExpectedException):
-            asyncio.get_event_loop().run_until_complete(start_timer(mock_timer))
+        self.return_queue.put_nowait(None)
+        mock_sleep.side_effect = self.return_until_empty_then_signal_done_and_release_loop
+        task = self.ev_loop.create_task(start_timer(mock_timer))
+        self.async_run_with_timeout(self.done_event.wait())
+        task.cancel()
         self.assertEqual('Duration: 0:00:02', mock_timer.log.call_args_list[0].args[0])
         self.assertEqual('Duration: 0:00:03', mock_timer.log.call_args_list[1].args[0])
 
@@ -37,9 +57,10 @@ class InterfaceUtilsTest(unittest.TestCase):
 
         mock_process.return_value.memory_info.return_value = memory_info
         mock_monitor = MagicMock()
-        mock_sleep.side_effect = ExpectedException()
-        with self.assertRaises(ExpectedException):
-            asyncio.get_event_loop().run_until_complete(start_process_monitor(mock_monitor))
+        mock_sleep.side_effect = self.return_until_empty_then_signal_done_and_release_loop
+        task = self.ev_loop.create_task(start_process_monitor(mock_monitor))
+        self.async_run_with_timeout(self.done_event.wait())
+        task.cancel()
         self.assertEqual(
             "CPU:    30%, Mem:   512.00 B (1.00 KB), Threads:   2, ",
             mock_monitor.log.call_args_list[0].args[0])
@@ -56,9 +77,11 @@ class InterfaceUtilsTest(unittest.TestCase):
         mock_app.get_current_balances = AsyncMock()
         mock_perf.side_effect = [MagicMock(return_pct=Decimal("0.01"), total_pnl=Decimal("2")),
                                  MagicMock(return_pct=Decimal("0.02"), total_pnl=Decimal("2"))]
-        mock_sleep.side_effect = [None, ExpectedException()]
-        with self.assertRaises(ExpectedException):
-            asyncio.get_event_loop().run_until_complete(start_trade_monitor(mock_result))
+        self.return_queue.put_nowait(None)
+        mock_sleep.side_effect = self.return_until_empty_then_signal_done_and_release_loop
+        task = self.ev_loop.create_task(start_trade_monitor(mock_result))
+        self.async_run_with_timeout(self.done_event.wait())
+        task.cancel()
         self.assertEqual(3, mock_result.log.call_count)
         self.assertEqual('Trades: 0, Total P&L: 0.00, Return %: 0.00%', mock_result.log.call_args_list[0].args[0])
         self.assertEqual('Trades: 1, Total P&L: 2.00 USDT, Return %: 1.00%', mock_result.log.call_args_list[1].args[0])
@@ -79,9 +102,10 @@ class InterfaceUtilsTest(unittest.TestCase):
         mock_app.get_current_balances = AsyncMock()
         mock_perf.side_effect = [MagicMock(return_pct=Decimal("0.01"), total_pnl=Decimal("2")),
                                  MagicMock(return_pct=Decimal("0.02"), total_pnl=Decimal("3"))]
-        mock_sleep.side_effect = ExpectedException()
-        with self.assertRaises(ExpectedException):
-            asyncio.get_event_loop().run_until_complete(start_trade_monitor(mock_result))
+        mock_sleep.side_effect = self.return_until_empty_then_signal_done_and_release_loop
+        task = self.ev_loop.create_task(start_trade_monitor(mock_result))
+        self.async_run_with_timeout(self.done_event.wait())
+        task.cancel()
         self.assertEqual(2, mock_result.log.call_count)
         self.assertEqual('Trades: 0, Total P&L: 0.00, Return %: 0.00%', mock_result.log.call_args_list[0].args[0])
         self.assertEqual('Trades: 2, Total P&L: N/A, Return %: 1.50%', mock_result.log.call_args_list[1].args[0])
@@ -101,9 +125,10 @@ class InterfaceUtilsTest(unittest.TestCase):
         mock_app.get_current_balances = AsyncMock()
         mock_perf.side_effect = [MagicMock(return_pct=Decimal("0.01"), total_pnl=Decimal("2")),
                                  MagicMock(return_pct=Decimal("0.02"), total_pnl=Decimal("3"))]
-        mock_sleep.side_effect = ExpectedException()
-        with self.assertRaises(ExpectedException):
-            asyncio.get_event_loop().run_until_complete(start_trade_monitor(mock_result))
+        mock_sleep.side_effect = self.return_until_empty_then_signal_done_and_release_loop
+        task = self.ev_loop.create_task(start_trade_monitor(mock_result))
+        self.async_run_with_timeout(self.done_event.wait())
+        task.cancel()
         self.assertEqual(2, mock_result.log.call_count)
         self.assertEqual('Trades: 0, Total P&L: 0.00, Return %: 0.00%', mock_result.log.call_args_list[0].args[0])
         self.assertEqual('Trades: 2, Total P&L: 5.00 USDT, Return %: 1.50%', mock_result.log.call_args_list[1].args[0])
@@ -115,9 +140,10 @@ class InterfaceUtilsTest(unittest.TestCase):
         mock_app = mock_hb_app.main_application()
         mock_app.strategy_task.done.return_value = False
         mock_app.markets.return_values = {"a": MagicMock(ready=False)}
-        mock_sleep.side_effect = ExpectedException()
-        with self.assertRaises(ExpectedException):
-            asyncio.get_event_loop().run_until_complete(start_trade_monitor(mock_result))
+        mock_sleep.side_effect = self.return_until_empty_then_signal_done_and_release_loop
+        task = self.ev_loop.create_task(start_trade_monitor(mock_result))
+        self.async_run_with_timeout(self.done_event.wait())
+        task.cancel()
         self.assertEqual(1, mock_result.log.call_count)
         self.assertEqual('Trades: 0, Total P&L: 0.00, Return %: 0.00%', mock_result.log.call_args_list[0].args[0])
 
@@ -129,8 +155,21 @@ class InterfaceUtilsTest(unittest.TestCase):
         mock_app.strategy_task.done.return_value = False
         mock_app.markets.return_values = {"a": MagicMock(ready=True)}
         mock_app._get_trades_from_session.return_value = []
-        mock_sleep.side_effect = ExpectedException()
-        with self.assertRaises(ExpectedException):
-            asyncio.get_event_loop().run_until_complete(start_trade_monitor(mock_result))
+        mock_sleep.side_effect = self.return_until_empty_then_signal_done_and_release_loop
+        task = self.ev_loop.create_task(start_trade_monitor(mock_result))
+        self.async_run_with_timeout(self.done_event.wait())
+        task.cancel()
         self.assertEqual(1, mock_result.log.call_count)
         self.assertEqual('Trades: 0, Total P&L: 0.00, Return %: 0.00%', mock_result.log.call_args_list[0].args[0])
+
+    @patch("hummingbot.client.ui.interface_utils._sleep", new_callable=AsyncMock)
+    @patch("hummingbot.client.hummingbot_application.HummingbotApplication")
+    def test_start_trade_monitor_loop_continues_on_failure(self, mock_hb_app, mock_sleep):
+        mock_result = MagicMock()
+        mock_app = mock_hb_app.main_application()
+        mock_app.strategy_task.done.side_effect = [RuntimeError(), True]
+        mock_sleep.side_effect = self.return_until_empty_then_signal_done_and_release_loop
+        task = self.ev_loop.create_task(start_trade_monitor(mock_result))
+        self.async_run_with_timeout(self.done_event.wait())
+        task.cancel()
+        self.assertEqual(2, mock_app.strategy_task.done.call_count)  # was called again after exception
