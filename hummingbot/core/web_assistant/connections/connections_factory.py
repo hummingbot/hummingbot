966e4f6efa287cb3e8cdebfc49a3f474f57ce3bb
Petio Petrov <petioptrv@icloud.com>
2021-12-16 12:24:00 +0700

    (fix) Fixes the "Should be created in an async function" warning.

--- hummingbot/core/web_assistant/connections/connections_factory.py
+++ hummingbot/core/web_assistant/connections/connections_factory.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import aiohttp
 from hummingbot.core.web_assistant.connections.rest_connection import RESTConnection
 from hummingbot.core.web_assistant.connections.ws_connection import WSConnection
@@ -5,12 +7,18 @@ from hummingbot.core.web_assistant.connections.ws_connection import WSConnection
 
 class ConnectionsFactory:
     def __init__(self):
-        self._shared_client = aiohttp.ClientSession()
+        self._shared_client: Optional[aiohttp.ClientSession] = None
 
     async def get_rest_connection(self) -> RESTConnection:
-        connection = RESTConnection(aiohttp_client_session=self._shared_client)
+        shared_client = await self._get_shared_client()
+        connection = RESTConnection(aiohttp_client_session=shared_client)
         return connection
 
     async def get_ws_connection(self) -> WSConnection:
-        connection = WSConnection(aiohttp_client_session=self._shared_client)
+        shared_client = await self._get_shared_client()
+        connection = WSConnection(aiohttp_client_session=shared_client)
         return connection
+
+    async def _get_shared_client(self) -> aiohttp.ClientSession:
+        self._shared_client = self._shared_client or aiohttp.ClientSession()
+        return self._shared_client
