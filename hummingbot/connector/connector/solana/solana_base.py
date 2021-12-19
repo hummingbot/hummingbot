19ebc226c6a496674bf046998543eaac3d04cf24
Mike Hukiewitz <m.h.hukiewitz@students.uu.nl>
2021-12-19 17:54:37 +0100

    add SolanaBase classes

---
+++ hummingbot/connector/connector/solana/solana_base.py
@@ -0,0 +1,243 @@
+import logging
+from decimal import Decimal
+import asyncio
+import aiohttp
+from typing import Dict, Any, List
+import json
+import time
+import ssl
+import copy
+
+from hummingbot.connector.connector.solana.solana_in_flight_order import SolanaInFlightOrder
+from hummingbot.logger.struct_logger import METRICS_LOG_LEVEL
+from hummingbot.core.network_iterator import NetworkStatus
+from hummingbot.core.utils.async_utils import safe_ensure_future, safe_gather
+from hummingbot.logger import HummingbotLogger
+from hummingbot.core.data_type.limit_order import LimitOrder
+from hummingbot.core.data_type.cancellation_result import CancellationResult
+from hummingbot.core.event.events import (
+    OrderType,
+    TradeType
+)
+from hummingbot.connector.connector_base import ConnectorBase
+from hummingbot.client.settings import GATEAWAY_CA_CERT_PATH, GATEAWAY_CLIENT_CERT_PATH, GATEAWAY_CLIENT_KEY_PATH
+from hummingbot.client.config.global_config_map import global_config_map
+
+s_logger = None
+s_decimal_0 = Decimal("0")
+s_decimal_NaN = Decimal("nan")
+logging.basicConfig(level=METRICS_LOG_LEVEL)
+
+
+class SolanaBase(ConnectorBase):
+    """
+    SolanaInFlightOrder connects with solana gateway APIs and provides user account and transactions tracking.
+    """
+    API_CALL_TIMEOUT = 10.0
+    POLL_INTERVAL = 60.0
+
+    @classmethod
+    def logger(cls) -> HummingbotLogger:
+        global s_logger
+        if s_logger is None:
+            s_logger = logging.getLogger(__name__)
+        return s_logger
+
+    def __init__(self,
+                 solana_wallet_address: str,
+                 solana_wallet_private_key: str,
+                 trading_required: bool = True
+                 ):
+        """
+        """
+        super().__init__()
+        self._solana_wallet_address = solana_wallet_address
+        self._solana_wallet_private_key = solana_wallet_private_key
+        self._trading_required = trading_required
+        self._ev_loop = asyncio.get_event_loop()
+        self._shared_client = None
+        self._last_poll_timestamp = 0.0
+        self._in_flight_orders = {}
+        self._status_polling_task = None
+        self._real_time_balance_update = False
+        self._poll_notifier = None
+
+    @property
+    def name(self):
+        return "solana"
+
+    @property
+    def limit_orders(self) -> List[LimitOrder]:
+        return [
+            in_flight_order.to_limit_order()
+            for in_flight_order in self._in_flight_orders.values()
+        ]
+
+    def start_tracking_order(self,
+                             order_id: str,
+                             exchange_order_id: str,
+                             trading_pair: str,
+                             trade_type: TradeType,
+                             price: Decimal,
+                             amount: Decimal):
+        """
+        Starts tracking an order by simply adding it into _in_flight_orders dictionary.
+        """
+        self._in_flight_orders[order_id] = SolanaInFlightOrder(
+            client_order_id=order_id,
+            exchange_order_id=exchange_order_id,
+            trading_pair=trading_pair,
+            order_type=OrderType.LIMIT,
+            trade_type=trade_type,
+            price=price,
+            amount=amount
+        )
+
+    def stop_tracking_order(self, order_id: str):
+        """
+        Stops tracking an order by simply removing it from _in_flight_orders dictionary.
+        """
+        if order_id in self._in_flight_orders:
+            del self._in_flight_orders[order_id]
+
+    def get_taker_order_type(self):
+        return OrderType.LIMIT
+
+    def get_order_price_quantum(self, trading_pair: str, price: Decimal) -> Decimal:
+        # TODO: Find out smallest price increments
+        return super().get_order_price_quantum()
+
+    def get_order_size_quantum(self, trading_pair: str, order_size: Decimal) -> Decimal:
+        return super().get_order_size_quantum()
+
+    @property
+    def ready(self):
+        return all(self.status_dict.values())
+
+    @property
+    def status_dict(self) -> Dict[str, bool]:
+        return {
+            "account_balance": len(self._account_balances) > 0 if self._trading_required else True
+        }
+
+    async def start_network(self):
+        if self._trading_required:
+            self._status_polling_task = safe_ensure_future(self._status_polling_loop())
+
+    async def stop_network(self):
+        if self._status_polling_task is not None:
+            self._status_polling_task.cancel()
+            self._status_polling_task = None
+
+    async def check_network(self) -> NetworkStatus:
+        try:
+            response = await self._api_request("get", "api")
+            if response["status"] != "ok":
+                raise Exception(f"Error connecting to Gateway API. HTTP status is {response.status}.")
+        except asyncio.CancelledError:
+            raise
+        except Exception:
+            return NetworkStatus.NOT_CONNECTED
+        return NetworkStatus.CONNECTED
+
+    def tick(self, timestamp: float):
+        """
+        Is called automatically by the clock for each clock's tick (1 second by default).
+        It checks if status polling task is due for execution.
+        """
+        if time.time() - self._last_poll_timestamp > self.POLL_INTERVAL:
+            if self._poll_notifier is not None and not self._poll_notifier.is_set():
+                self._poll_notifier.set()
+
+    async def _status_polling_loop(self):
+        while True:
+            try:
+                self._poll_notifier = asyncio.Event()
+                await self._poll_notifier.wait()
+                await safe_gather(
+                    self._update_balances(),
+                )
+                self._last_poll_timestamp = self.current_timestamp
+            except asyncio.CancelledError:
+                raise
+            except Exception as e:
+                self.logger().error(str(e), exc_info=True)
+                self.logger().network("Unexpected error while fetching account updates.",
+                                      exc_info=True,
+                                      app_warning_msg="Could not fetch balances from Gateway API.")
+                await asyncio.sleep(0.5)
+
+    async def _update_balances(self):
+        """
+        Calls Solana API to update total and available balances.
+        """
+        local_asset_names = set(self._account_balances.keys())
+        remote_asset_names = set()
+        resp_json = await self._api_request("post",
+                                            f"{self.name}/balances",
+                                            {"address": self._solana_wallet_address})
+        # TODO: Abstract to GatewayBase, rename current GatewayBase to EthereumBase
+        for token, bal in resp_json["balances"].items():
+            self._account_available_balances[token] = Decimal(str(bal))
+            self._account_balances[token] = Decimal(str(bal))
+            remote_asset_names.add(token)
+
+        asset_names_to_remove = local_asset_names.difference(remote_asset_names)
+        for asset_name in asset_names_to_remove:
+            del self._account_available_balances[asset_name]
+            del self._account_balances[asset_name]
+
+        self._in_flight_orders_snapshot = {k: copy.copy(v) for k, v in self._in_flight_orders.items()}
+        self._in_flight_orders_snapshot_timestamp = self.current_timestamp
+
+    async def _http_client(self) -> aiohttp.ClientSession:
+        """
+        :returns Shared client session instance
+        """
+        if self._shared_client is None:
+            ssl_ctx = ssl.create_default_context(cafile=GATEAWAY_CA_CERT_PATH)
+            ssl_ctx.load_cert_chain(GATEAWAY_CLIENT_CERT_PATH, GATEAWAY_CLIENT_KEY_PATH)
+            conn = aiohttp.TCPConnector(ssl_context=ssl_ctx)
+            self._shared_client = aiohttp.ClientSession(connector=conn)
+        return self._shared_client
+
+    async def _api_request(self,
+                           method: str,
+                           path_url: str,
+                           params: Dict[str, Any] = {}) -> Dict[str, Any]:
+        """
+        Sends an aiohttp request and waits for a response.
+        :param method: The HTTP method, e.g. get or post
+        :param path_url: The path url or the API end point
+        :param params: A dictionary of required params for the end point
+        :returns A response in json format.
+        """
+        base_url = f"https://{global_config_map['gateway_api_host'].value}:" \
+                   f"{global_config_map['gateway_api_port'].value}"
+        url = f"{base_url}/{path_url}"
+        client = await self._http_client()
+        if method == "get":
+            if len(params) > 0:
+                response = await client.get(url, params=params)
+            else:
+                response = await client.get(url)
+        elif method == "post":
+            response = await client.post(url, data=params)
+
+        parsed_response = json.loads(await response.text())
+        if response.status != 200:
+            err_msg = ""
+            if "error" in parsed_response:
+                err_msg = f" Message: {parsed_response['error']}"
+            raise IOError(f"Error fetching data from {url}. HTTP status is {response.status}.{err_msg}")
+        if "error" in parsed_response:
+            raise Exception(f"Error: {parsed_response['error']}")
+
+        return parsed_response
+
+    async def cancel_all(self, timeout_seconds: float) -> List[CancellationResult]:
+        return []
+
+    @property
+    def in_flight_orders(self) -> Dict[str, SolanaInFlightOrder]:
+        return self._in_flight_orders
