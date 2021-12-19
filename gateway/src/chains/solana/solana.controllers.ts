d6d9b4a73c9c511d0858baa3098cdb9b4e668641
Gott50 <xxGott50xx@gmail.com>
2021-12-19 20:47:47 +0100

    implement getOrCreateTokenAccount() in solana.controllers.ts

--- gateway/src/chains/solana/solana.controllers.ts
+++ gateway/src/chains/solana/solana.controllers.ts
@@ -76,10 +76,7 @@ export async function token(
 
   const walletAddress = new PublicKey(req.publicKey);
   const mintAddress = new PublicKey(tokenInfo.address);
-  const accountInfo = await solanaish.getTokenAccount(
-    walletAddress,
-    mintAddress
-  );
+  const account = await solanaish.getTokenAccount(walletAddress, mintAddress);
 
   let amount;
   try {
@@ -95,7 +92,7 @@ export async function token(
     timestamp: initTime,
     token: req.token,
     mintAddress: mintAddress.toString(),
-    accountAddress: accountInfo?.owner?.toString(),
+    accountAddress: account?.owner?.toString(),
     amount,
   };
 }
@@ -105,17 +102,33 @@ export async function getOrCreateTokenAccount(
   req: PostSolanaTokenRequest
 ): Promise<SolanaTokenResponse> {
   const initTime = Date.now();
-  const token = '',
-    mintAddress = '',
-    accountAddress = '',
-    amount = undefined; // TODO: Implement
+  const tokenInfo = await solanaish.getTokenForSymbol(req.token);
+  if (!tokenInfo) {
+    throw new HttpException(501, 'Token not found');
+  }
+
+  const wallet = solanaish.getWallet(req.privateKey);
+  const mintAddress = new PublicKey(tokenInfo.address);
+  const account = await solanaish.getOrCreateAssociatedTokenAccount(
+    wallet,
+    mintAddress
+  );
+
+  let amount;
+  try {
+    amount = tokenValueToString(
+      await solanaish.getSplBalance(wallet.publicKey, mintAddress)
+    );
+  } catch (err) {
+    amount = undefined;
+  }
 
   return {
     network: solanaish.cluster,
     timestamp: initTime,
-    token,
-    mintAddress,
-    accountAddress,
+    token: req.token,
+    mintAddress: mintAddress.toString(),
+    accountAddress: account?.owner?.toString(),
     amount,
   };
 }
