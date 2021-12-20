9c1e0e718d6b9c6dabe9314ec4fbf3ffcdbad374
Mike Hukiewitz <m.h.hukiewitz@students.uu.nl>
2021-12-20 17:16:14 +0100

    add key validation for Solana and refactor validators

--- gateway/src/chains/solana/solana.validators.ts
+++ gateway/src/chains/solana/solana.validators.ts
@@ -1,19 +1,23 @@
 import {
   isNaturalNumberString,
-  missingParameter,
+  validateTokenSymbols,
   mkValidator,
   mkRequestValidator,
   RequestValidator,
   Validator,
+  isBase58,
 } from '../../services/validators';
+import bs58 from 'bs58';
+
+import {} from '../ethereum/ethereum.validators';
 
 // invalid parameter errors
 
 export const invalidPrivateKeyError: string =
-  'The privateKey param is not a valid Solana private key (64 hexidecimal characters).';
+  'The privateKey param is not a valid Solana private key (base58 string worth 32 bytes).';
 
 export const invalidSpenderError: string =
-  'The spender param is not a valid Solana public key (0x followed by 40 hexidecimal characters).';
+  'The spender param is not a valid Solana public key (base58 string worth 32 bytes).';
 
 export const invalidTokenSymbolsError: string =
   'The tokenSymbols param should be an array of strings.';
@@ -23,25 +27,16 @@ export const invalidTokenError: string = 'The token param should be a string.';
 export const invalidAmountError: string =
   'If amount is included it must be a string of a non-negative integer.';
 
-export const invalidNonceError: string =
-  'If nonce is included it must be a non-negative integer.';
-
-export const invalidMaxFeePerGasError: string =
-  'If maxFeePerGas is included it must be a string of a non-negative integer.';
-
-export const invalidMaxPriorityFeePerGasError: string =
-  'If maxPriorityFeePerGas is included it must be a string of a non-negative integer.';
-
 export const invalidTxHashError: string = 'The txHash param must be a string.';
 
 // test if a string matches the shape of an Solana public key
 export const isPublicKey = (str: string): boolean => {
-  return /^[a-fA-F0-9]{44}$/.test(str);
+  return isBase58(str) && bs58.decode(str).length == 32;
 };
 
 // test if a string matches the shape of an Solana private key
 export const isPrivateKey = (str: string): boolean => {
-  return /^[a-fA-F0-9]{44}$/.test(str);
+  return isBase58(str) && bs58.decode(str).length == 32;
 };
 
 // given a request, look for a key called privateKey that is an Solana private key
@@ -57,25 +52,6 @@ export const validatePublicKey: Validator = mkValidator(
   invalidSpenderError,
   (val) => typeof val === 'string' && isPublicKey(val)
 );
-
-// confirm that tokenSymbols is an array of strings
-export const validateTokenSymbols: Validator = (req: any) => {
-  const errors: Array<string> = [];
-  if (req.tokenSymbols) {
-    if (Array.isArray(req.tokenSymbols)) {
-      req.tokenSymbols.forEach((symbol: any) => {
-        if (typeof symbol !== 'string') {
-          errors.push(invalidTokenSymbolsError);
-        }
-      });
-    } else {
-      errors.push(invalidTokenSymbolsError);
-    }
-  } else {
-    errors.push(missingParameter('tokenSymbols'));
-  }
-  return errors;
-};
 
 // confirm that token is a string
 export const validateToken: Validator = mkValidator(
