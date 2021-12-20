b2eeb9d0fcba994fd368be60a738154b9b42056d
Gott50 <xxGott50xx@gmail.com>
2021-12-20 21:27:19 +0100

    refactor: move validateToken, validateAmount, validateTxHash to validators.ts

--- gateway/test/services/validators.test.ts
+++ gateway/test/services/validators.test.ts
@@ -4,10 +4,17 @@ import {
   isFloatString,
   missingParameter,
   validateTokenSymbols,
+  isBase58,
+  validateToken,
+  validateAmount,
+  validateTxHash,
+  invalidTokenSymbolsError,
+  invalidTokenError,
+  invalidAmountError,
+  invalidTxHashError,
 } from '../../src/services/validators';
 
 import 'jest-extended';
-import { invalidTokenSymbolsError } from '../../src/chains/ethereum/ethereum.validators';
 
 describe('isNaturalNumberString', () => {
   it('pass against a well formed natural number in a string', () => {
@@ -86,5 +93,109 @@ describe('validateTokenSymbols', () => {
         tokenSymbols: 'WETH',
       })
     ).toEqual([invalidTokenSymbolsError]);
+  });
+});
+
+describe('isBase58', () => {
+  it('pass against a well formed Base58', () => {
+    expect(isBase58('HAE1oNnc3XBmPudphRcHhyCvGShtgDYtZVzx2MocKEr1')).toEqual(
+      true
+    );
+  });
+
+  it('fail against a string that has non-Base58 characters', () => {
+    expect(isBase58('HAE1oNnc3XBmPudphRcHhyCvGShtgDYtZVzx2MocKErI')).toEqual(
+      false
+    );
+  });
+});
+
+describe('validateToken', () => {
+  it('valid when req.token is a string', () => {
+    expect(
+      validateToken({
+        token: 'DAI',
+      })
+    ).toEqual([]);
+
+    expect(
+      validateToken({
+        token: 'WETH',
+      })
+    ).toEqual([]);
+  });
+
+  it('return error when req.token does not exist', () => {
+    expect(
+      validateToken({
+        hello: 'world',
+      })
+    ).toEqual([missingParameter('token')]);
+  });
+
+  it('return error when req.token is invalid', () => {
+    expect(
+      validateToken({
+        token: 123,
+      })
+    ).toEqual([invalidTokenError]);
+  });
+});
+
+describe('validateAmount', () => {
+  it('valid when req.amount is a string of an integer', () => {
+    expect(
+      validateAmount({
+        amount: '0',
+      })
+    ).toEqual([]);
+    expect(
+      validateAmount({
+        amount: '9999999999999999999999',
+      })
+    ).toEqual([]);
+  });
+
+  it('valid when req.amount does not exist', () => {
+    expect(
+      validateAmount({
+        hello: 'world',
+      })
+    ).toEqual([]);
+  });
+
+  it('return error when req.amount is invalid', () => {
+    expect(
+      validateAmount({
+        amount: 'WETH',
+      })
+    ).toEqual([invalidAmountError]);
+  });
+});
+
+describe('validateTxHash', () => {
+  it('valid when req.txHash is a string', () => {
+    expect(
+      validateTxHash({
+        txHash:
+          '0x6d068067a5e5a0f08c6395b31938893d1cdad81f54a54456221ecd8c1941294d',
+      })
+    ).toEqual([]);
+  });
+
+  it('invalid when req.txHash does not exist', () => {
+    expect(
+      validateTxHash({
+        hello: 'world',
+      })
+    ).toEqual([missingParameter('txHash')]);
+  });
+
+  it('return error when req.txHash is invalid', () => {
+    expect(
+      validateTxHash({
+        txHash: 123,
+      })
+    ).toEqual([invalidTxHashError]);
   });
 });
