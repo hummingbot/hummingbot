06ba18676519bc90f63f4d89fdb99e7d53baabc8
Gott50 <xxGott50xx@gmail.com>
2021-12-21 16:25:04 +0100

    simplify toSolanaBalances() in solana.controllers.ts

--- gateway/test/chains/solana/solana.routes.test.ts
+++ gateway/test/chains/solana/solana.routes.test.ts
@@ -1,7 +1,10 @@
 import request from 'supertest';
-import { unpatch } from '../../services/patch';
+import { patch, unpatch } from '../../services/patch';
 import { app } from '../../../src/app';
 import { Solana } from '../../../src/chains/solana/solana';
+import { privateKey, publicKey } from './solana.validators.test';
+import { tokenSymbols } from '../../services/validators.test';
+import bs58 from 'bs58';
 
 let solana: Solana;
 beforeAll(async () => {
@@ -22,7 +25,46 @@ describe('GET /solana', () => {
   });
 });
 
+const patchGetWallet = () => {
+  patch(solana, 'getWallet', () => {
+    return {
+      publicKey: bs58.decode(publicKey),
+      secretKey: bs58.decode(privateKey),
+    };
+  });
+};
+
+const patchGetBalances = () => {
+  patch(solana, 'getBalances', () => {
+    return {
+      [tokenSymbols[0]]: { value: 1, decimals: 1 },
+      [tokenSymbols[1]]: { value: 2, decimals: 2 },
+      OTH: { value: 3, decimals: 3 },
+    };
+  });
+};
+
 describe('POST /solana/balance', () => {
+  it('should return 200', async () => {
+    patchGetWallet();
+    patchGetBalances();
+
+    await request(app)
+      .post(`/solana/balance`)
+      .send({ privateKey, tokenSymbols })
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .expect((res) => expect(res.body.network).toBe(solana.cluster))
+      .expect((res) => expect(res.body.timestamp).toBeNumber())
+      .expect((res) => expect(res.body.latency).toBeNumber())
+      .expect((res) =>
+        expect(res.body.balances).toEqual({
+          [tokenSymbols[0]]: '0.1',
+          [tokenSymbols[1]]: '0.02',
+        })
+      );
+  });
+
   it('should return 404 when parameters are invalid', async () => {
     await request(app).post(`/solana/balance`).send({}).expect(404);
   });
