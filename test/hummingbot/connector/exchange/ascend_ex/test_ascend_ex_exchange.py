42accf8794793b3f4a85567d8e4db72c9dd6d1ce
abel <abel@hummingbot.io>
2021-12-21 18:11:02 -0300

    (fix) Add unit tests for cancel_all method to comply with coverage requirements

--- test/hummingbot/connector/exchange/ascend_ex/test_ascend_ex_exchange.py
+++ test/hummingbot/connector/exchange/ascend_ex/test_ascend_ex_exchange.py
@@ -1,14 +1,20 @@
 import asyncio
+import json
+import re
 import unittest
 
 from decimal import Decimal
-from typing import Awaitable
+from typing import Awaitable, Optional, List
 
+from aioresponses import aioresponses
+
+from hummingbot.connector.exchange.ascend_ex import ascend_ex_constants as CONSTANTS, ascend_ex_utils
 from hummingbot.connector.exchange.ascend_ex.ascend_ex_exchange import (
     AscendExExchange,
     AscendExTradingRule,
     AscendExCommissionType,
 )
+from hummingbot.core.data_type.cancellation_result import CancellationResult
 from hummingbot.core.event.events import OrderType, TradeType
 from test.hummingbot.connector.network_mocking_assistant import NetworkMockingAssistant
 
@@ -31,12 +37,14 @@ class TestAscendExExchange(unittest.TestCase):
     def setUp(self) -> None:
         super().setUp()
         self.log_records = []
+        self.async_task: Optional[asyncio.Task] = None
 
         self.exchange = AscendExExchange(self.api_key, self.api_secret_key, trading_pairs=[self.trading_pair])
         self.mocking_assistant = NetworkMockingAssistant()
 
     def tearDown(self) -> None:
         self.exchange._shared_client and self.exchange._shared_client.close()
+        self.async_task and self.async_task.cancel()
         super().tearDown()
 
     def handle(self, record):
@@ -102,134 +110,57 @@ class TestAscendExExchange(unittest.TestCase):
         self.assertEqual(Decimal("0"), buy_fee.percent)
         self.assertEqual(Decimal("0"), sell_fee.percent)
 
-    # def test_execute_cancel_order_not_found_exceed_limit(self):
-    #     client_order_id = "ORDER_NOT_FOUND_ID"
-    #     self.async_run_with_timeout(self.exchange._execute_cancel(self.trading_pair, client_order_id))
-    #     self.assertIn(client_order_id, self.exchange._order_not_found_records)
+    @aioresponses()
+    def test_cancel_all_does_not_cancel_orders_without_exchange_id(self, mock_api):
+        self.exchange._account_group = 0
 
-    #     self.async_run_with_timeout(self.exchange._execute_cancel(self.trading_pair, client_order_id))
-    #     self.assertNotIn(client_order_id, self.exchange._order_not_found_records)
+        url = f"{ascend_ex_utils.get_rest_url_private(0)}/{CONSTANTS.ORDER_BATCH_PATH_URL}"
+        regex_url = re.compile(f"^{url}".replace(".", r"\.").replace("?", r"\?"))
 
-    #     self._is_logged(
-    #         "NETWORK",
-    #         f"Failed to cancel the order {client_order_id} on AscendEx. Check API key and network connection.",
-    #     )
+        mock_response = {
+            "code": 0,
+            "data": {
+                "ac": "CASH",
+                "accountId": "cshQtyfq8XLAA9kcf19h8bXHbAwwoqDo",
+                "action": "batch-cancel-order",
+                "status": "Ack",
+                "info": [
+                    {
+                        "id": "0a8bXHbAwwoqDo3b485d7ea0b09c2cd8",
+                        "orderId": "16e61d5ff43s8bXHbAwwoqDo9d817339",
+                        "orderType": "NULL_VAL",
+                        "symbol": f"{self.base_asset}/{self.quote_asset}",
+                        "timestamp": 1573619097746
+                    },
+                ]
+            }
+        }
 
-    # def test_websocket_process_order_message_no_open_for_cancel(self):
+        mock_api.delete(regex_url, body=json.dumps(mock_response))
 
-    #     client_order_id = "someClientOrderId"
-    #     ex_order_id = "HMBotExchangeOrderId1234"
-    #     self.exchange._in_flight_orders[client_order_id] = AscendExInFlightOrder(
-    #         client_order_id=client_order_id,
-    #         exchange_order_id=ex_order_id,
-    #         trading_pair=self.trading_pair,
-    #         order_type=OrderType.LIMIT,
-    #         trade_type=TradeType.BUY,
-    #         price=Decimal("1"),
-    #         amount=Decimal("10"),
-    #     )
+        self.exchange.start_tracking_order(
+            order_id="testOrderId1",
+            trading_pair=self.trading_pair,
+            trade_type=TradeType.BUY,
+            price=Decimal("10000"),
+            amount=Decimal("1"),
+            order_type=OrderType.LIMIT,
+            exchange_order_id="16e61d5ff43s8bXHbAwwoqDo9d817339"
+        )
+        self.exchange.start_tracking_order(
+            order_id="testOrderId2",
+            trading_pair=self.trading_pair,
+            trade_type=TradeType.BUY,
+            price=Decimal("20000"),
+            amount=Decimal("2"),
+            order_type=OrderType.LIMIT
+        )
 
-    #     ws_resp = {
-    #         "m": "order",
-    #         "accountId": "cshSomeAccountId",
-    #         "ac": "CASH",
-    #         "data": {
-    #             "sn": 28361787015,
-    #             "orderId": ex_order_id,
-    #             "s": self.ex_trading_pair,
-    #             "ot": "NULL_VAL",
-    #             "t": 1638440160232,
-    #             "p": "0",
-    #             "q": "0",
-    #             "sd": "NULL_VAL",
-    #             "st": "Rejected",
-    #             "ap": "0",
-    #             "cfq": "0",
-    #             "sp": "",
-    #             "err": "NoOpenForCancel",
-    #             "btb": "23",
-    #             "bab": "23",
-    #             "qtb": "51.99954056",
-    #             "qab": "31.33329056",
-    #             "cf": "0",
-    #             "fa": "USDT",
-    #             "ei": "NULL_VAL",
-    #         },
-    #     }
+        self.async_task = asyncio.get_event_loop().create_task(self.exchange.cancel_all(10))
+        result: List[CancellationResult] = self.async_run_with_timeout(self.async_task)
 
-    #     parsed_order_message: AscendExOrder = AscendExOrder(
-    #         self.ex_trading_pair,
-    #         ws_resp["data"]["p"],
-    #         ws_resp["data"]["q"],
-    #         ws_resp["data"]["ot"],
-    #         ws_resp["data"]["ap"],
-    #         ws_resp["data"]["cf"],
-    #         ws_resp["data"]["cfq"],
-    #         ws_resp["data"]["err"],
-    #         ws_resp["data"]["fa"],
-    #         ws_resp["data"]["t"],
-    #         ws_resp["data"]["orderId"],
-    #         ws_resp["data"]["sn"],
-    #         ws_resp["data"]["sd"],
-    #         ws_resp["data"]["st"],
-    #         ws_resp["data"]["sp"],
-    #         ws_resp["data"]["ei"],
-    #     )
-
-    #     self.exchange._process_order_message(parsed_order_message)
-
-    #     self.assertIn(client_order_id, self.exchange.in_flight_orders)
-    #     self._is_logged(
-    #         "INFO",
-    #         f"Order {client_order_id} has failed according to order status API. API order response: {parsed_order_message}",
-    #     )
-
-    #     # Stops tracking order after a Failure message is received for more than STOP_TRACKING_ORDER_FAILURE_LIMIT
-    #     self.exchange._process_order_message(parsed_order_message)
-    #     self.assertNotIn(client_order_id, self.exchange.in_flight_orders)
-
-    # def test_update_order_status_successful(self):
-
-    #     mock_response = {
-    #         "code": 0,
-    #         "accountCategory": "CASH",
-    #         "accountId": "cshQtyfq8XLAA9kcf19h8bXHbAwwoqDo",
-    #         "data": [
-    #             {
-    #                 "symbol": "BTC/USDT",
-    #                 "price": "8130.24",
-    #                 "orderQty": "0.00082",
-    #                 "orderType": "Limit",
-    #                 "avgPx": "7391.13",
-    #                 "cumFee": "0.005151618",
-    #                 "cumFilledQty": "0.00082",
-    #                 "errorCode": "",
-    #                 "feeAsset": "USDT",
-    #                 "lastExecTime": 1575953134011,
-    #                 "orderId": "a16eee206d610866943712rPNknIyhH",
-    #                 "seqNum": 2622058,
-    #                 "side": "Buy",
-    #                 "status": "Filled",
-    #                 "stopPrice": "",
-    #                 "execInst": "NULL_VAL",
-    #             },
-    #             {
-    #                 "symbol": "BTC/USDT",
-    #                 "price": "8131.22",
-    #                 "orderQty": "0.00082",
-    #                 "orderType": "Market",
-    #                 "avgPx": "7392.02",
-    #                 "cumFee": "0.005152238",
-    #                 "cumFilledQty": "0.00082",
-    #                 "errorCode": "",
-    #                 "feeAsset": "USDT",
-    #                 "lastExecTime": 1575953151764,
-    #                 "orderId": "a16eee20b6750866943712zWEDdAjt3",
-    #                 "seqNum": 2623469,
-    #                 "side": "Buy",
-    #                 "status": "Filled",
-    #                 "stopPrice": "",
-    #                 "execInst": "NULL_VAL",
-    #             },
-    #         ],
-    #     }
+        self.assertEqual(2, len(result))
+        self.assertEqual("testOrderId1", result[0].order_id)
+        self.assertTrue(result[0].success)
+        self.assertEqual("testOrderId2", result[1].order_id)
+        self.assertFalse(result[1].success)
