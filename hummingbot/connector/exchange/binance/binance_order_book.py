907858bbba7ca95e9740761e9c4f06eef01df82c
abel <abel@hummingbot.io>
2022-01-02 00:32:09 -0300

    (refactor) Convert BinanceOrderBook class to pure Python and add unit tests

--- hummingbot/connector/exchange/binance/binance_order_book.py
+++ hummingbot/connector/exchange/binance/binance_order_book.py
@@ -1,30 +1,14 @@
-import logging
-from typing import (
-    Dict,
-    Optional
-)
-
-import ujson
-from aiokafka import ConsumerRecord
+from typing import Dict, Optional
 
 from hummingbot.core.event.events import TradeType
-from hummingbot.core.data_type.order_book cimport OrderBook
+from hummingbot.core.data_type.order_book import OrderBook
 from hummingbot.core.data_type.order_book_message import (
     OrderBookMessage,
     OrderBookMessageType
 )
-from hummingbot.logger import HummingbotLogger
-
-_bob_logger = None
 
 
-cdef class BinanceOrderBook(OrderBook):
-    @classmethod
-    def logger(cls) -> HummingbotLogger:
-        global _bob_logger
-        if _bob_logger is None:
-            _bob_logger = logging.getLogger(__name__)
-        return _bob_logger
+class BinanceOrderBook(OrderBook):
 
     @classmethod
     def snapshot_message_from_exchange(cls,
@@ -56,31 +40,6 @@ cdef class BinanceOrderBook(OrderBook):
         }, timestamp=timestamp)
 
     @classmethod
-    def snapshot_message_from_kafka(cls, record: ConsumerRecord, metadata: Optional[Dict] = None) -> OrderBookMessage:
-        msg = ujson.loads(record.value.decode("utf-8"))
-        if metadata:
-            msg.update(metadata)
-        return OrderBookMessage(OrderBookMessageType.SNAPSHOT, {
-            "trading_pair": msg["trading_pair"],
-            "update_id": msg["lastUpdateId"],
-            "bids": msg["bids"],
-            "asks": msg["asks"]
-        }, timestamp=record.timestamp * 1e-3)
-
-    @classmethod
-    def diff_message_from_kafka(cls, record: ConsumerRecord, metadata: Optional[Dict] = None) -> OrderBookMessage:
-        msg = ujson.loads(record.value.decode("utf-8"))
-        if metadata:
-            msg.update(metadata)
-        return OrderBookMessage(OrderBookMessageType.DIFF, {
-            "trading_pair": msg["trading_pair"],
-            "update_id": msg["u"],
-            "bids": msg["b"],
-            "asks": msg["a"],
-
-        }, timestamp=record.timestamp * 1e-3)
-
-    @classmethod
     def trade_message_from_exchange(cls, msg: Dict[str, any], metadata: Optional[Dict] = None):
         if metadata:
             msg.update(metadata)
@@ -93,9 +52,3 @@ cdef class BinanceOrderBook(OrderBook):
             "price": msg["p"],
             "amount": msg["q"]
         }, timestamp=ts * 1e-3)
-
-    @classmethod
-    def from_snapshot(cls, msg: OrderBookMessage) -> "OrderBook":
-        retval = BinanceOrderBook()
-        retval.apply_snapshot(msg.bids, msg.asks, msg.update_id)
-        return retval
