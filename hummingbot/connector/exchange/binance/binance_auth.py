935c788042968975527f2b947a8acc20f1c8a907
abel <abel@hummingbot.io>
2022-01-06 17:26:44 -0300

    (refactor) Move use of request auth object to the web assistant components

--- hummingbot/connector/exchange/binance/binance_auth.py
+++ hummingbot/connector/exchange/binance/binance_auth.py
@@ -9,35 +9,37 @@ from typing import (
 )
 from urllib.parse import urlencode
 
+from hummingbot.connector.time_synchronizer import TimeSynchronizer
+from hummingbot.core.web_assistant.auth import AuthBase
+from hummingbot.core.web_assistant.connections.data_types import RESTRequest, RESTMethod, WSRequest
 
-class BinanceAuth:
-    def __init__(self, api_key: str, secret_key: str):
-        self.api_key: str = api_key
-        self.secret_key: str = secret_key
 
-    # @staticmethod
-    # def keysort(dictionary: Dict[str, str]) -> Dict[str, str]:
-    #    return OrderedDict(sorted(dictionary.items(), key=lambda t: t[0]))
+class BinanceAuth(AuthBase):
 
-    def get_headers(self, request_type: str) -> Dict[str, Any]:
-        """
-        Generates authentication headers required by ProBit
-        :return: a dictionary of auth headers
-        """
-        content_type = "application/json" if request_type == "post" else "application/x-www-form-urlencoded"
-        return {
-            "Content-Type": content_type,
-        }
+    def __init__(self, api_key: str, secret_key: str, time_provider: TimeSynchronizer):
+        self.api_key = api_key
+        self.secret_key = secret_key
+        self.time_provider = time_provider
 
-    def get_auth_headers(self, request_type: str) -> Dict[str, Any]:
-        headers = self.get_headers(request_type=request_type)
-        headers["X-MBX-APIKEY"] = self.api_key
-        return headers
+    async def rest_authenticate(self, request: RESTRequest) -> RESTRequest:
+        if request.method == RESTMethod.POST:
+            request.data = self.add_auth_to_params(params=request.data)
+        else:
+            request.params = self.add_auth_to_params(params=request.params)
+
+        headers = {}
+        headers.update(request.headers)
+        headers.update(self.header_for_authentication())
+        request.headers = headers
+
+        return request
+
+    async def ws_authenticate(self, request: WSRequest) -> WSRequest:
+        return request  # pass-through
 
     def add_auth_to_params(self,
-                           params: Dict[str, Any],
-                           current_time: float):
-        timestamp = int(current_time * 1e3)
+                           params: Dict[str, Any]):
+        timestamp = int(self.time_provider.time() * 1e3)
 
         request_params = OrderedDict(params or {})
         request_params["timestamp"] = timestamp
@@ -46,6 +48,9 @@ class BinanceAuth:
         request_params["signature"] = signature
 
         return request_params
+
+    def header_for_authentication(self) -> Dict[str, str]:
+        return {"X-MBX-APIKEY": self.api_key}
 
     def _generate_signature(self, params: Dict[str, Any]) -> str:
 
