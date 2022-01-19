19b543026cc31b14d6225075127e5110fda4f131
willaPa <willypaterson@gmail.com>
2022-01-19 18:23:50 -0300

    adds uniswap test

---
+++ gateway/test/chains/ethereum/uniswap/uniswap.test.ts
@@ -0,0 +1,121 @@
+jest.useFakeTimers();
+import { NewUniswap } from '../../../../src/connectors/uniswap/new_uniswap';
+import { patch, unpatch } from '../../../services/patch';
+import {
+  Fetcher,
+  Pair,
+  Route,
+  Token,
+  TokenAmount,
+  Trade,
+  TradeType,
+} from '@uniswap/sdk';
+import { BigNumber } from 'ethers';
+import { NewEthereum } from '../../../../src/chains/ethereum/new_ethereum';
+
+let ethereum: NewEthereum;
+let uniswap: NewUniswap;
+
+const WETH = new Token(
+  3,
+  '0xd0A1E359811322d97991E03f863a0C30C2cF029C',
+  18,
+  'WETH'
+);
+const DAI = new Token(
+  3,
+  '0x4f96fe3b7a6cf9725f59d353f723c1bdb64ca6aa',
+  18,
+  'DAI'
+);
+
+beforeAll(async () => {
+  ethereum = NewEthereum.getInstance('kovan');
+  await ethereum.init();
+  uniswap = NewUniswap.getInstance('ethereum', 'kovan');
+  await uniswap.init();
+});
+
+afterEach(() => {
+  unpatch();
+});
+
+const patchFetchPairData = () => {
+  patch(Fetcher, 'fetchPairData', () => {
+    return new Pair(
+      new TokenAmount(WETH, '2000000000000000000'),
+      new TokenAmount(DAI, '1000000000000000000')
+    );
+  });
+};
+
+const patchTrade = (key: string, error?: Error) => {
+  patch(Trade, key, () => {
+    if (error) return [];
+    const WETH_DAI = new Pair(
+      new TokenAmount(WETH, '2000000000000000000'),
+      new TokenAmount(DAI, '1000000000000000000')
+    );
+    const DAI_TO_WETH = new Route([WETH_DAI], DAI);
+    return [
+      new Trade(
+        DAI_TO_WETH,
+        new TokenAmount(DAI, '1000000000000000'),
+        TradeType.EXACT_INPUT
+      ),
+    ];
+  });
+};
+describe('verify Uniswap priceSwapIn', () => {
+  it('Should return an ExpectedTrade when available', async () => {
+    patchFetchPairData();
+    patchTrade('bestTradeExactIn');
+
+    const expectedTrade = await uniswap.priceSwapIn(
+      WETH,
+      DAI,
+      BigNumber.from(1)
+    );
+    expect(expectedTrade).toHaveProperty('trade');
+    expect(expectedTrade).toHaveProperty('expectedAmount');
+  });
+
+  it('Should return an error if no pair is available', async () => {
+    patchFetchPairData();
+    patchTrade('bestTradeExactIn', new Error('error getting trade'));
+
+    const expectedTrade = await uniswap.priceSwapIn(
+      WETH,
+      DAI,
+      BigNumber.from(1)
+    );
+    expect(typeof expectedTrade).toBe('string');
+  });
+});
+
+describe('verify Uniswap priceSwapOut', () => {
+  it('Should return an ExpectedTrade when available', async () => {
+    patchFetchPairData();
+    patchTrade('bestTradeExactOut');
+
+    const expectedTrade = await uniswap.priceSwapOut(
+      WETH,
+      DAI,
+      BigNumber.from(1)
+    );
+    expect(expectedTrade).toHaveProperty('trade');
+    expect(expectedTrade).toHaveProperty('expectedAmount');
+  });
+
+  it('Should return an error if no pair is available', async () => {
+    patchFetchPairData();
+    patchTrade('bestTradeExactOut', new Error('error getting trade'));
+
+    const expectedTrade = await uniswap.priceSwapOut(
+      WETH,
+      DAI,
+      BigNumber.from(1)
+    );
+    expect(typeof expectedTrade).toBe('string');
+  });
+});
