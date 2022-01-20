9e802ed2d46b0f2e0d55e6f0567441b54764de22
vic-en <victoreni14@gmail.com>
2022-01-20 12:58:23 +0100

    change owner on cert dir

--- bin/hummingbot_quickstart.py
+++ bin/hummingbot_quickstart.py
@@ -64,7 +64,7 @@ class CmdlineParser(argparse.ArgumentParser):
 def autofix_permissions(user_group_spec: str):
     project_home: str = os.path.realpath(os.path.join(__file__, "../../"))
     subprocess.run(f"cd '{project_home}' && "
-                   f"sudo chown -R {user_group_spec} conf/ data/ logs/", capture_output=True, shell=True)
+                   f"sudo chown -R {user_group_spec} conf/ data/ logs/ certs/", capture_output=True, shell=True)
     uid, gid = user_group_spec.split(':')
     os.setgid(int(gid))
     os.setuid(int(uid))
