b268184d75cacd4434e2ad0c6a94557a9965b49c
abel <abel@hummingbot.io>
2022-01-25 17:29:33 -0300

    Fix when registering orders from restores states in strategies with hanging orders enabled. Added validation in logic that registers orders from restored states using the Client Order Tracker

--- test/hummingbot/connector/derivative/binance_perpetual/test_binance_perpetual_derivative.py
+++ test/hummingbot/connector/derivative/binance_perpetual/test_binance_perpetual_derivative.py
@@ -1,25 +1,23 @@
 import asyncio
 import functools
 import json
-import pandas as pd
 import re
 import unittest
-
 from decimal import Decimal
 from typing import Any, Awaitable, Callable, Dict, List, Optional
 from unittest.mock import patch, AsyncMock
 
+import pandas as pd
 from aioresponses.core import aioresponses
 from bidict import bidict
 
-import hummingbot.connector.derivative.binance_perpetual.constants as CONSTANTS
 import hummingbot.connector.derivative.binance_perpetual.binance_perpetual_utils as utils
-
+import hummingbot.connector.derivative.binance_perpetual.constants as CONSTANTS
 from hummingbot.connector.derivative.binance_perpetual.binance_perpetual_api_order_book_data_source import \
     BinancePerpetualAPIOrderBookDataSource
 from hummingbot.connector.derivative.binance_perpetual.binance_perpetual_derivative import \
     BinancePerpetualDerivative
-from hummingbot.core.data_type.in_flight_order import OrderState
+from hummingbot.core.data_type.in_flight_order import OrderState, InFlightOrder
 from hummingbot.core.data_type.trade_fee import TokenAmount
 from hummingbot.core.event.event_logger import EventLogger
 from hummingbot.core.event.events import (
@@ -1570,3 +1568,54 @@ class BinancePerpetualDerivativeUnitTest(unittest.TestCase):
             f"Error submitting order to Binance Perpetuals for 9999 {self.trading_pair} "
             f"1010."
         ))
+
+    def test_restore_tracking_states_only_registers_open_orders(self):
+        orders = []
+        orders.append(InFlightOrder(
+            client_order_id="OID1",
+            exchange_order_id="EOID1",
+            trading_pair=self.trading_pair,
+            order_type=OrderType.LIMIT,
+            trade_type=TradeType.BUY,
+            amount=Decimal("1000.0"),
+            price=Decimal("1.0"),
+        ))
+        orders.append(InFlightOrder(
+            client_order_id="OID2",
+            exchange_order_id="EOID2",
+            trading_pair=self.trading_pair,
+            order_type=OrderType.LIMIT,
+            trade_type=TradeType.BUY,
+            amount=Decimal("1000.0"),
+            price=Decimal("1.0"),
+            initial_state=OrderState.CANCELLED
+        ))
+        orders.append(InFlightOrder(
+            client_order_id="OID3",
+            exchange_order_id="EOID3",
+            trading_pair=self.trading_pair,
+            order_type=OrderType.LIMIT,
+            trade_type=TradeType.BUY,
+            amount=Decimal("1000.0"),
+            price=Decimal("1.0"),
+            initial_state=OrderState.FILLED
+        ))
+        orders.append(InFlightOrder(
+            client_order_id="OID4",
+            exchange_order_id="EOID4",
+            trading_pair=self.trading_pair,
+            order_type=OrderType.LIMIT,
+            trade_type=TradeType.BUY,
+            amount=Decimal("1000.0"),
+            price=Decimal("1.0"),
+            initial_state=OrderState.FAILED
+        ))
+
+        tracking_states = {order.client_order_id: order.to_json() for order in orders}
+
+        self.exchange.restore_tracking_states(tracking_states)
+
+        self.assertIn("OID1", self.exchange.in_flight_orders)
+        self.assertNotIn("OID2", self.exchange.in_flight_orders)
+        self.assertNotIn("OID3", self.exchange.in_flight_orders)
+        self.assertNotIn("OID4", self.exchange.in_flight_orders)
