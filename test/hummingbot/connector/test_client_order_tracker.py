19e5fff3c02dd69fca83d575e594712023abfb2e
abel <abel@hummingbot.io>
2022-01-26 14:36:17 -0300

    (fix) Fix broken unit tests

--- test/hummingbot/connector/test_client_order_tracker.py
+++ test/hummingbot/connector/test_client_order_tracker.py
@@ -798,6 +798,7 @@ class ClientOrderTrackerUnitTest(unittest.TestCase):
             trade_type=TradeType.BUY,
             amount=Decimal("1000.0"),
             price=Decimal("1.0"),
+            creation_timestamp=1640001112.223,
         ))
         orders.append(InFlightOrder(
             client_order_id="OID2",
@@ -807,6 +808,7 @@ class ClientOrderTrackerUnitTest(unittest.TestCase):
             trade_type=TradeType.BUY,
             amount=Decimal("1000.0"),
             price=Decimal("1.0"),
+            creation_timestamp=1640001112.223,
             initial_state=OrderState.CANCELLED
         ))
         orders.append(InFlightOrder(
@@ -817,6 +819,7 @@ class ClientOrderTrackerUnitTest(unittest.TestCase):
             trade_type=TradeType.BUY,
             amount=Decimal("1000.0"),
             price=Decimal("1.0"),
+            creation_timestamp=1640001112.223,
             initial_state=OrderState.FILLED
         ))
         orders.append(InFlightOrder(
@@ -827,6 +830,7 @@ class ClientOrderTrackerUnitTest(unittest.TestCase):
             trade_type=TradeType.BUY,
             amount=Decimal("1000.0"),
             price=Decimal("1.0"),
+            creation_timestamp=1640001112.223,
             initial_state=OrderState.FAILED
         ))
 
