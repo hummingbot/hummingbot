84ce5d7038ccef2069c6a74a164940d01876119d
mhrvth <contact@mhrvth.net>
2022-01-27 13:22:58 +0100

    (refactor) binance exchange name

--- hummingbot/connector/exchange/binance/binance_constants.py
+++ hummingbot/connector/exchange/binance/binance_constants.py
@@ -1,6 +1,7 @@
 from hummingbot.core.api_throttler.data_types import LinkedLimitWeightPair, RateLimit
 from hummingbot.core.data_type.in_flight_order import OrderState
 
+EXCHANGE_NAME = "binance"
 HBOT_ORDER_ID_PREFIX = "x-XEKWYICX"
 
 # Base URL
