04ff1bb0d39340dcbfb16ace30aa7bbab54d6191
Daniel Tan <daniel@hummingbot.io>
2022-01-27 13:56:14 +0800

    (fix) fix duplicate test in test_gate_io_exchange.py

--- test/hummingbot/connector/exchange/gate_io/test_gate_io_exchange.py
+++ test/hummingbot/connector/exchange/gate_io/test_gate_io_exchange.py
@@ -307,36 +307,6 @@ class TestGateIoExchange(unittest.TestCase):
         self.assertTrue(order_id in self.exchange.in_flight_orders)
 
     @aioresponses()
-    def test_create_order_when_order_is_instantly_closed(self, mock_api):
-        trading_rules = self.get_trading_rules_mock()
-        self.exchange._trading_rules = self.exchange._format_trading_rules(trading_rules)
-
-        url = f"{CONSTANTS.REST_URL}/{CONSTANTS.ORDER_CREATE_PATH_URL}"
-        regex_url = re.compile(f"^{url}".replace(".", r"\.").replace("?", r"\?"))
-        resp = self.get_order_create_response_mock()
-        resp["status"] = "closed"
-        mock_api.post(regex_url, body=json.dumps(resp))
-
-        event_logger = EventLogger()
-        self.exchange.add_listener(MarketEvent.BuyOrderCreated, event_logger)
-
-        order_id = "someId"
-        self.async_run_with_timeout(
-            coroutine=self.exchange._create_order(
-                trade_type=TradeType.BUY,
-                order_id=order_id,
-                trading_pair=self.trading_pair,
-                amount=Decimal("1"),
-                order_type=OrderType.LIMIT,
-                price=Decimal("5.1"),
-            )
-        )
-
-        self.assertEqual(1, len(event_logger.event_log))
-        self.assertEqual(order_id, event_logger.event_log[0].order_id)
-        self.assertTrue(order_id in self.exchange.in_flight_orders)
-
-    @aioresponses()
     def test_order_with_less_amount_than_allowed_is_not_created(self, mock_api):
         trading_rules = self.get_trading_rules_mock()
         self.exchange._trading_rules = self.exchange._format_trading_rules(trading_rules)
