5d87a4d2e4de5d03adcba1b600a5c975c858f939
willaPa <willypaterson@gmail.com>
2022-01-28 09:16:36 -0300

    fix swagger tests

--- gateway/test/app.test.ts
+++ gateway/test/app.test.ts
@@ -12,6 +12,8 @@ describe('verify swagger docs', () => {
         './docs/swagger/main-routes.yml',
         './docs/swagger/trading-routes.yml',
         './docs/swagger/wallet-routes.yml',
+        './docs/swagger/evm-routes.yml',
+        './docs/swagger/network-routes.yml',
         './docs/swagger/solana-routes.yml',
       ]
     );
