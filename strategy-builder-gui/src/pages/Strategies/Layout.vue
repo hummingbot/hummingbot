21cde67e3a63b07a980b287b5c476f1923a97c17
Mike <33065483+Th0rd@users.noreply.github.com>
2022-01-28 18:57:00 +0300

    (feat) add header to Strategies-page layout

--- strategy-builder-gui/src/pages/Strategies/Layout.vue
+++ strategy-builder-gui/src/pages/Strategies/Layout.vue
@@ -1,16 +1,18 @@
 <template>
-  <BaseLayout>
+  <div class="q-py-lg q-px-xl">
+    <Header />
     <Strategies />
-  </BaseLayout>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import Header from '../../components/Header.vue';
 import Strategies from './components/Index.vue';
 
 export default defineComponent({
-  components: { Strategies },
+  components: { Strategies, Header },
 });
 </script>
 
