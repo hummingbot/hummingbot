922ff74a40f854d59cc494e94a34b1a93b1cf042
Mike <33065483+Th0rd@users.noreply.github.com>
2022-01-28 18:56:33 +0300

    (feat) Header component

---
+++ strategy-builder-gui/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row flex items-center q-mb-lg" style="height: 56px">
+    <div class="col"> <img src="~assets/header-logo.svg" style="cursor: pointer" /> </div>
+    <div class="col flex justify-end">
+      <q-btn color="mono-grey-1" class="q-py-md q-px-lg text-h6" @click="onClickUpload()"
+        >UPLOAD STRATEGY</q-btn
+      >
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  setup() {
+    function onClickUpload() {
+      // eslint-disable-next-line no-console
+      console.log('upload click');
+    }
+
+    return { onClickUpload };
+  },
+};
+</script>
+
+<style></style>
