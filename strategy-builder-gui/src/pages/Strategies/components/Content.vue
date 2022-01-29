798c5412b8c7ba59667566715a6ac93192a9e852
Mike <33065483+Th0rd@users.noreply.github.com>
2022-01-29 16:33:17 +0300

    (feat) Add Box component to Content

--- strategy-builder-gui/src/pages/Strategies/components/Content.vue
+++ strategy-builder-gui/src/pages/Strategies/components/Content.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="text-h1"> Strategies page </div>
+  <div style="gap: 20px" class="flex row">
+    <Box class="col" v-bind="{ type: 'strategy' }" />
+    <Box class="col" v-bind="{ type: 'exchange' }" />
+  </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import Box from './Box.vue';
+
+export default {
+  components: { Box },
+};
 </script>
 
 <style></style>
