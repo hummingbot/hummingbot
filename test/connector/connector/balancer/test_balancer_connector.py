679e3b45951080ec2b01f89ffc50ac51d65ab2f3
vic-en <victoreni14@gmail.com>
2022-02-01 09:15:56 +0100

    remove noqa tags in uniswap and balancer connector

--- test/connector/connector/balancer/test_balancer_connector.py
+++ test/connector/connector/balancer/test_balancer_connector.py
@@ -60,7 +60,7 @@ class BalancerConnectorUnitTest(unittest.TestCase):
         cls.clock: Clock = Clock(ClockMode.REALTIME)
         cls.connector: BalancerConnector = BalancerConnector(
             [trading_pair],
-            "0xdc393a78a366ac53ffbd5283e71785fd2097807fef1bc5b73b8ec84da47fb8de",  # noqa: mock
+            "0xdc393a78a366ac53ffbd5283e71785fd2097807fef1bc5b73b8ec84da47fb8de",
             "")
         print("Initializing CryptoCom market... this will take about a minute.")
         cls.clock.add_iterator(cls.connector)
