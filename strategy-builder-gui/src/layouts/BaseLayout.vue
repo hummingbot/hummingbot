b9f2d9e728757e7514df91d535625a1942a21bf7
Mike <unclethord@gmail.com>
2022-02-01 16:34:13 +0300

    (fix) Create BaseLayout

---
+++ strategy-builder-gui/src/layouts/BaseLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-layout view="lHh Lpr lFf" class="flex column">
+    <q-page-container class="q-py-lg q-px-xl flex self-center full-width" style="max-width: 1440px">
+      <Header />
+      <slot />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import Header from '../components/Header/Header.vue';
+
+export default defineComponent({
+  name: 'BaseLayout',
+  components: { Header },
+});
+</script>
