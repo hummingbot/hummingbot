7a44a1323299ba36650a657a83773795edab7586
Willy Paterson <willypaterson+1@gmail.com>
2022-02-03 15:59:15 -0300

    adds connectors endpoint

--- gateway/src/app.ts
+++ gateway/src/app.ts
@@ -15,6 +15,7 @@ import {
 import { ConfigManagerV2 } from './services/config-manager-v2';
 import { SwaggerManager } from './services/swagger-manager';
 import { NetworkRoutes } from './network/network.routes';
+import { ConnectorsRoutes } from './connectors/connectors.routes';
 import { EVMRoutes } from './evm/evm.routes';
 import { AmmRoutes } from './amm/amm.routes';
 
@@ -33,6 +34,7 @@ gatewayApp.use(express.urlencoded({ extended: true }));
 // mount sub routers
 gatewayApp.use('/network', NetworkRoutes.router);
 gatewayApp.use('/evm', EVMRoutes.router);
+gatewayApp.use('/connectors', ConnectorsRoutes.router);
 
 gatewayApp.use('/amm', AmmRoutes.router);
 gatewayApp.use('/wallet', WalletRoutes.router);
