63ae64872c5e9a287aea71a5d62ba9d094b78e11
Petio Petrov <petioptrv@icloud.com>
2022-02-03 16:02:25 +0700

    (test) Adds a test case for binance spot changes.

--- test/hummingbot/connector/exchange/binance/test_binance_user_stream_data_source.py
+++ test/hummingbot/connector/exchange/binance/test_binance_user_stream_data_source.py
@@ -226,6 +226,7 @@ class BinanceUserStreamDataSourceUnitTests(unittest.TestCase):
 
         msg = self.async_run_with_timeout(msg_queue.get())
         self.assertTrue(msg, self._user_update_event)
+        mock_ws.return_value.ping.assert_called()
 
     @aioresponses()
     @patch("aiohttp.ClientSession.ws_connect", new_callable=AsyncMock)
