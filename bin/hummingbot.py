b624580f36e67e1e3b0e24d4ca138e8549d7ab00
Martin Kou <martin@hummingbot.io>
2022-02-05 05:30:16 -0800

    Fixed crash-at-start issue when using hummingbot_quickstart.py with password.

--- bin/hummingbot.py
+++ bin/hummingbot.py
@@ -16,13 +16,13 @@ from hummingbot.client.config.config_helpers import (
     create_yml_files,
     read_system_configs_from_yml
 )
+from hummingbot.core.event.event_listener import EventListener
 from hummingbot import (
     init_logging,
     check_dev_mode,
     chdir_to_data_directory
 )
 from hummingbot.client.ui import login_prompt
-from hummingbot.client.ui.stdout_redirection import patch_stdout
 from hummingbot.client.settings import AllConnectorSettings
 from hummingbot.core.utils.async_utils import safe_gather
 
@@ -53,23 +53,28 @@ async def main():
 
     hb = HummingbotApplication.main_application()
 
-    with patch_stdout(log_field=hb.app.log_field):
-        dev_mode = check_dev_mode()
-        if dev_mode:
-            hb.app.log("Running from dev branches. Full remote logging will be enabled.")
-        init_logging("hummingbot_logs.yml",
-                     override_log_level=global_config_map.get("log_level").value,
-                     dev_mode=dev_mode)
-        tasks: List[Coroutine] = [hb.run()]
-        if global_config_map.get("debug_console").value:
-            if not hasattr(__builtins__, "help"):
-                import _sitebuiltins
-                __builtins__.help = _sitebuiltins._Helper()
+    class UIStartListener(EventListener):
+        def __call__(self, _):
+            asyncio.create_task(self.ui_start_handler())
 
-            from hummingbot.core.management.console import start_management_console
-            management_port: int = detect_available_port(8211)
-            tasks.append(start_management_console(locals(), host="localhost", port=management_port))
-        await safe_gather(*tasks)
+        @staticmethod
+        async def ui_start_handler():
+            dev_mode = check_dev_mode()
+            if dev_mode:
+                hb.app.log("Running from dev branches. Full remote logging will be enabled.")
+
+    hb.app.add_listener(hb.app.Event.START, UIStartListener())
+
+    tasks: List[Coroutine] = [hb.run()]
+    if global_config_map.get("debug_console").value:
+        if not hasattr(__builtins__, "help"):
+            import _sitebuiltins
+            __builtins__.help = _sitebuiltins._Helper()
+
+        from hummingbot.core.management.console import start_management_console
+        management_port: int = detect_available_port(8211)
+        tasks.append(start_management_console(locals(), host="localhost", port=management_port))
+    await safe_gather(*tasks)
 
 
 if __name__ == "__main__":
