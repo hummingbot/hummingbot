30a41f48241a4865fae3ebdb4e6821ab1ef37c93
Mike <unclethord@gmail.com>
2022-02-06 03:45:30 +0300

    (feat) Strategy box layout

---
+++ strategy-builder-gui/src/pages/Strategies/components/StrategyBox.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-card class="bg-mono-grey-1 overflow-hidden rounded-borders full-height q-px-md q-py-md">
+    <q-card-section horizontal class="flex row full-height">
+      <q-card-section class="q-py-none q-px-none q-pr-md">
+        <div class="text-white text-h6 q-mb-sm">Pure Market Making</div>
+        <div class="text-mono-grey-4 text-body2 q-mb-lg">
+          This strategy allows Hummingbot users to run a market making strategy on a single trading
+          pair on a spot exchanges.
+        </div>
+        <div class="row q-gutter-sm">
+          <q-btn class="bg-mono-grey-2 rounded-borders" padding="sm">
+            <q-icon :name="'img:' + require('../assets/file.svg')" size="10px" />
+          </q-btn>
+          <q-btn class="bg-mono-green-2 rounded-borders" padding="sm">
+            <q-icon :name="'img:' + require('../assets/play.svg')" size="10px" />
+          </q-btn>
+        </div>
+      </q-card-section>
+      <q-card-section class="fit q-py-none q-px-none place">
+        <div class="row bg-mono-grey-2 rounded-borders q-py-none q-px-sm">
+          <div class="col q-mr-xs text-h6 text-white">1</div>
+          <div class="col">
+            <q-icon :name="'img:' + require('../assets/place-stay.svg')" size="8px" />
+          </div>
+        </div>
+      </q-card-section>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
+
+<style lang="scss" scoped>
+@use 'sass:map';
+
+.place {
+  max-width: 36px;
+}
+</style>
