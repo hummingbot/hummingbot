1501925a0a0bb4829ff83f09fedc7d0773f6bdfa
Mike <unclethord@gmail.com>
2022-02-07 17:47:30 +0300

    (feat) Move StrategyBox to component folder

--- strategy-builder-gui/src/pages/Strategies/components/StrategyBox/StrategyBox.vue
+++ strategy-builder-gui/src/pages/Strategies/components/StrategyBox/StrategyBox.vue
@@ -1,25 +1,25 @@
 <template>
-  <q-card class="bg-mono-grey-1 overflow-hidden rounded-borders full-height q-px-md q-py-md">
+  <q-card class="bg-mono-grey-1 overflow-hidden rounded-borders full-height q-pl-md q-py-md">
     <q-card-section horizontal class="flex row full-height">
-      <q-card-section class="q-py-none q-px-none q-pr-md">
+      <q-card-section class="q-py-none q-px-none q-pr-md col-10 column">
         <div class="text-white text-h6 q-mb-sm">{{ title }}</div>
         <div class="text-mono-grey-4 text-body2 q-mb-lg">
           {{ desc }}
         </div>
-        <div class="row q-gutter-sm">
+        <div class="row q-gutter-sm q-mt-auto">
           <q-btn class="bg-mono-grey-2 rounded-borders" padding="sm" :href="fileHref">
-            <q-icon :name="'img:' + require('../assets/file.svg')" size="10px" />
+            <q-icon :name="'img:' + require('../../assets/file.svg')" size="10px" />
           </q-btn>
           <q-btn class="bg-mono-green-2 rounded-borders" padding="sm" :href="startHref">
-            <q-icon :name="'img:' + require('../assets/play.svg')" size="10px" />
+            <q-icon :name="'img:' + require('../../assets/play.svg')" size="10px" />
           </q-btn>
         </div>
       </q-card-section>
-      <q-card-section class="fit q-py-none q-px-none place">
-        <div class="row bg-mono-grey-2 rounded-borders q-py-none q-px-sm">
-          <div class="col q-mr-xs text-h6 text-white">{{ place }}</div>
-          <div class="col">
-            <q-icon :name="`img:${require(`../assets/place-${placeType}.svg`)}`" size="8px" />
+      <q-card-section class="q-py-none q-px-none flex column justify-start items-end">
+        <div class="row bg-mono-grey-2 rounded-borders q-py-none q-px-sm col-auto">
+          <div class="col q-mr-xs text-h6 text-white q-mr-xs">{{ place }}</div>
+          <div class="col flex items-center justify-center">
+            <q-icon :name="`img:${require(`../../assets/place-${placeType}.svg`)}`" size="8px" />
           </div>
         </div>
       </q-card-section>
@@ -30,13 +30,11 @@
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
 
-export type StrategyBoxPlaceType = keyof { equal: string; lt: string; gt: string };
-
 export default defineComponent({
   props: {
     place: { type: Number, requaried: true, default: () => 0 },
     placeType: {
-      type: String as PropType<StrategyBoxPlaceType>,
+      type: String as PropType<'equal' | 'lt' | 'gt'>,
       requaried: true,
       default: () => 'equal',
     },
@@ -47,11 +45,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style lang="scss" scoped>
-@use 'sass:map';
-
-.place {
-  max-width: 36px;
-}
-</style>
