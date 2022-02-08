843365ca126f9ec214bd851ce4525e5c66329ba2
Mike <unclethord@gmail.com>
2022-02-08 19:00:11 +0300

    (fix) Repair husky and lint-staged

--- strategy-builder-gui/src/pages/Strategies/components/StrategyBox/composables/useStrategiesFilter.ts
+++ strategy-builder-gui/src/pages/Strategies/components/StrategyBox/composables/useStrategiesFilter.ts
@@ -1,14 +1,13 @@
-import { $strategies, StrategyCategory } from 'src/pages/Strategies/stores/strategies'
-import { computed, Ref } from 'vue'
-
+import { $strategies, StrategyCategory } from 'src/pages/Strategies/stores/strategies';
+import { computed, Ref } from 'vue';
 
 export const useStrategiesFilter = (category: Ref<StrategyCategory>) => {
-    const strategies = computed(() => $strategies.value
+  const strategies = computed(() =>
+    $strategies.value
       .sort((a, b) => a.place - b.place)
-      .filter((val) => category.value === StrategyCategory.All || category.value === val.category )
-    );
-    const categories: string[] = Object.values(StrategyCategory);
-    
-    return { strategies, categories };
-  };
-  
+      .filter((val) => category.value === StrategyCategory.All || category.value === val.category),
+  );
+  const categories: string[] = Object.values(StrategyCategory);
+
+  return { strategies, categories };
+};
