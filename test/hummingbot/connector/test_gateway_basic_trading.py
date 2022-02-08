e04b2b84a52c52da82151dafe0986a755040be70
vic-en <victoreni14@gmail.com>
2022-02-08 21:55:26 +0100

    refactor GatewayBase class

--- test/hummingbot/connector/test_gateway_basic_trading.py
+++ test/hummingbot/connector/test_gateway_basic_trading.py
@@ -8,13 +8,11 @@ from unittest.mock import patch, AsyncMock
 import aiohttp
 from aioresponses import aioresponses
 
-from hummingbot.client.config.fee_overrides_config_map import fee_overrides_config_map
 from hummingbot.client.config.global_config_map import global_config_map
-from hummingbot.connector.connector.balancer.balancer_connector import BalancerConnector
-from hummingbot.core.data_type.trade_fee import TokenAmount
+from hummingbot.connector.gateway_basic_trading import GatewayBasicTrading
 
 
-class BalancerConnectorTest(unittest.TestCase):
+class GatewayBasicTradingTest(unittest.TestCase):
     def setUp(self) -> None:
         super().setUp()
         self.base = "COINALHPA"
@@ -25,10 +23,12 @@ class BalancerConnectorTest(unittest.TestCase):
         self.gateway_port = 123
         global_config_map["gateway_api_host"].value = self.gateway_host
         global_config_map["gateway_api_port"].value = self.gateway_port
-        self.connector = BalancerConnector(
+        self.connector = GatewayBasicTrading(
+            connector_name = "uniswap",
+            chain = "ethereum",
+            network = "mainnet",
+            wallet_public_key = "0xABCD....1234",
             trading_pairs=[self.trading_pair],
-            wallet_private_key=self.wallet_key,
-            ethereum_rpc_url="https://<network>.infura.io/v3/YOUR-PROJECT-ID",
         )
         self.ev_loop = asyncio.get_event_loop()
 
@@ -38,12 +38,12 @@ class BalancerConnectorTest(unittest.TestCase):
 
     @aioresponses()
     @patch(
-        "hummingbot.connector.connector.balancer.balancer_connector.BalancerConnector._http_client",
+        "hummingbot.connector.gateway_basic_trading.GatewayBasicTrading._http_client",
         new_callable=AsyncMock
     )
     def test_get_quote_price_updates_fee_overrides_config_map(self, mocked_api, mocked_http_client):
         mocked_http_client.return_value = aiohttp.ClientSession()
-        url = f"https://{self.gateway_host}:{self.gateway_port}/eth/balancer/price"
+        url = f"https://{self.gateway_host}:{self.gateway_port}/amm/price"
         mock_response = {
             "price": 10,
             "gasLimit": 30000,
@@ -51,18 +51,11 @@ class BalancerConnectorTest(unittest.TestCase):
             "gasCost": 2,
             "swaps": [],
         }
-        mocked_api.post(url, body=json.dumps(mock_response))
+        mocked_api.get(url, body=json.dumps(mock_response))
 
         self.connector._account_balances = {"ETH": Decimal("10000")}
         self.connector._allowances = {self.quote: Decimal("10000")}
 
         self.async_run_with_timeout(
             self.connector.get_quote_price(self.trading_pair, is_buy=True, amount=Decimal("2"))
-        )
-
-        self.assertEqual(
-            fee_overrides_config_map["balancer_maker_fixed_fees"].value, [TokenAmount("ETH", Decimal(str("2")))]
-        )
-        self.assertEqual(
-            fee_overrides_config_map["balancer_taker_fixed_fees"].value, [TokenAmount("ETH", Decimal(str("2")))]
         )
