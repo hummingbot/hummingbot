d9d85befe83114e74027cd8ddce84beb36bbefc8
Mike <unclethord@gmail.com>
2022-02-09 15:15:00 +0300

    (feat) Step layout

--- strategy-builder-gui/src/pages/Settings/components/Stepper/Step.vue
+++ strategy-builder-gui/src/pages/Settings/components/Stepper/Step.vue
@@ -1,8 +1,11 @@
 <template>
-  <q-card class="q-pt-sm q-pb-md q-px-lg bg-mono-grey-1">
-    <div class=""> status </div>
-    <div> title </div>
-    <div> desc </div>
+  <q-card class="q-pt-md q-pb-lg q-px-lg bg-mono-grey-1">
+    <div class="row justify-between items-center q-mb-md">
+      <div class="text-mono-green-2 text-h6"> Completed </div>
+      <q-icon :name="`img:${require('../../assets/status-completed.svg')}`" size="20px" />
+    </div>
+    <div class="text-white text-h5 q-mb-xs">1. Choose strategy </div>
+    <div class="text-mono-grey-3 text-body1"> Pure market making </div>
   </q-card>
 </template>
 
