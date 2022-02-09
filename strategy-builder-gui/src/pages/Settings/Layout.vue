d1ae016cd93cd15cf1c2c01401b45fdfc01ee9e1
Mike <unclethord@gmail.com>
2022-02-09 16:56:02 +0300

    (fix) Get rid of unnecessary Index.vue

--- strategy-builder-gui/src/pages/Settings/Layout.vue
+++ strategy-builder-gui/src/pages/Settings/Layout.vue
@@ -8,7 +8,7 @@
 import BaseLayout from 'layouts/BaseLayout.vue';
 import { defineComponent } from 'vue';
 
-import Settings from './components/Index.vue';
+import Settings from './components/Content.vue';
 
 export default defineComponent({
   components: { Settings, BaseLayout },
