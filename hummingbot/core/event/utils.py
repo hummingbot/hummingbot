84a8efe61843f8d4513854cb560239f3b68a62e5
abel <abel@hummingbot.io>
2022-02-09 12:19:27 -0300

    (fix) Change the convertion of fees to quote asset in amb_arb to use the rate source configured in the strategy

--- hummingbot/core/event/utils.py
+++
@@ -1,3 +0,0 @@
-def interchangeable(token_a: str, token_b: str) -> bool:
-    interchangeable_tokens = {"WETH", "ETH", "WBTC", "BTC"}
-    return token_a == token_b or ({token_a, token_b} <= interchangeable_tokens)
