77cb2e0db164cbdc7d21c2eede229cbffbbf3958
Petio Petrov <petioptrv@icloud.com>
2022-02-10 16:39:28 +0700

    (refactor) Adds max length for KuCoin and ProBit.

--- hummingbot/connector/exchange/kucoin/kucoin_exchange.pyx
+++ hummingbot/connector/exchange/kucoin/kucoin_exchange.pyx
@@ -784,7 +784,9 @@ cdef class KucoinExchange(ExchangeBase):
                    object price = s_decimal_0,
                    dict kwargs = {}):
         cdef:
-            str order_id = get_new_client_order_id(is_buy=True, trading_pair=trading_pair)
+            str order_id = get_new_client_order_id(
+                is_buy=True, trading_pair=trading_pair, max_id_len=CONSTANTS.MAX_ORDER_ID_LEN
+            )
 
         safe_ensure_future(self.execute_buy(order_id, trading_pair, amount, order_type, price))
         return order_id
@@ -858,7 +860,9 @@ cdef class KucoinExchange(ExchangeBase):
                     object price = s_decimal_0,
                     dict kwargs = {}):
         cdef:
-            str order_id = get_new_client_order_id(is_buy=False, trading_pair=trading_pair)
+            str order_id = get_new_client_order_id(
+                is_buy=False, trading_pair=trading_pair, max_id_len=CONSTANTS.MAX_ORDER_ID_LEN
+            )
 
         safe_ensure_future(self.execute_sell(order_id, trading_pair, amount, order_type, price))
         return order_id
