e70ecca7345576d0ed7a6b7c0e31dcf0d41789c2
Mike <unclethord@gmail.com>
2022-02-14 08:03:13 +0300

    (feat) useSettingsForm

---
+++ strategy-builder-gui/src/pages/Settings/composables/useSettingsForm.ts
@@ -0,0 +1,17 @@
+import { computed } from 'vue';
+
+import { $settingsForm } from '../stores/settingsForm';
+
+export const useSettingsForm = () => {
+  const { inputs, counters, selects, toggles } = $settingsForm;
+  const formObject = { ...inputs, ...selects, ...counters, ...toggles };
+
+  const submitValue = computed(() =>
+    Object.keys(formObject).reduce(
+      (acc, key) => ({ ...acc, [key]: formObject[key].value.modelValue }),
+      {},
+    ),
+  );
+
+  return { settingsForm: $settingsForm, submitValue };
+};
