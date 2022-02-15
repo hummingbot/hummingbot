5e773af54135f3575385edca6ff42690f226c95c
Mike <unclethord@gmail.com>
2022-02-15 18:50:10 +0300

    (feat) Move Field to component folder

---
+++ strategy-builder-gui/src/pages/Settings/components/Field/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="row justify-between items-center q-pb-md border">
+    <div v-if="title" class="row text-body1 text-white items-center">
+      {{ title }}
+      <q-icon v-if="hint" :name="`img:${require('./hint.svg')}`" class="q-ml-xs">
+        <q-tooltip
+          class="bg-mono-grey-2 text-white text-body2"
+          anchor="top middle"
+          self="center right"
+          max-width="300px"
+        >
+          {{ hint }}
+        </q-tooltip>
+      </q-icon>
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    title: { type: String, required: false, default: () => '' },
+    hint: { type: String, required: false, default: () => '' },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@use 'sass:map';
+.border {
+  border-bottom: 2px dashed map.get($colors, 'mono-grey-2') !important;
+}
+</style>
