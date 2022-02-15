54005252093d9dc377c8dcd36e95ab1a0b6b8d0a
Mike <unclethord@gmail.com>
2022-02-15 09:23:27 +0300

    (feat) Implement advanced form

--- strategy-builder-gui/src/pages/Settings/components/Form.vue
+++ strategy-builder-gui/src/pages/Settings/components/Form.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="bg-mono-grey-1 q-px-xl q-py-lg rounded-borders q-mt-md full-width">
     <div class="text-white text-h4 q-mb-xl">Settings</div>
-    <q-form class="q-gutter-md" @submit="handleSubmit">
+    <div class="row q-gutter-md q-mb-lg">
+      <q-btn
+        v-model="formType"
+        flat
+        rounded
+        :ripple="false"
+        class="text-capitalize"
+        :class="formType === FormType.Basic ? 'text-white bg-mono-grey-2' : 'bg-none'"
+        @click="formType = FormType.Basic"
+      >
+        Basic
+      </q-btn>
+      <q-btn
+        v-model="formType"
+        rounded
+        flat
+        :ripple="false"
+        class="text-capitalize"
+        :class="formType === FormType.Advanced ? 'text-white bg-mono-grey-2' : 'bg-none'"
+        @click="formType = FormType.Advanced"
+      >
+        Advanced
+      </q-btn>
+    </div>
+    <q-form v-if="formType === FormType.Basic" class="q-gutter-md" @submit="handleSubmit">
       <Field title="Exchange">
         <Select v-model="exchange.value.value" v-bind="{ ...exchange.properties }" />
       </Field>
@@ -41,11 +65,155 @@
       </Field>
       <q-btn label="Submit" type="submit" color="main-green-1" />
     </q-form>
+    <q-form v-if="formType === FormType.Advanced" class="q-gutter-md" @submit="handleSubmit">
+      <Field title="Order levels">
+        <Counter
+          v-model="orderLevels.value.value"
+          :type="CounterType.CountInt"
+          v-bind="{ ...orderLevels.properties }"
+        />
+      </Field>
+      <Field title="Order level amount">
+        <Counter
+          v-model="orderLevelAmount.value.value"
+          :type="CounterType.CountInt"
+          v-bind="{ ...orderLevelAmount.properties }"
+        />
+      </Field>
+      <Field title="Order level spread">
+        <Counter
+          v-model="orderLevelSpread.value.value"
+          :type="CounterType.Percentage"
+          v-bind="{ ...orderLevelSpread.properties }"
+        />
+      </Field>
+      <Field title="Inventory skew">
+        <q-toggle v-model="inventorySkew.value.value" color="main-green-1" />
+      </Field>
+      <Field title="Inventory target base">
+        <div class="row q-col-gutter-md">
+          <Counter
+            v-model="inventoryTargetBaseCounter.value.value"
+            :type="CounterType.Percentage"
+            v-bind="{ ...inventoryTargetBaseCounter.properties }"
+          />
+          <q-toggle v-model="inventoryTargetBaseToggle.value.value" color="main-green-1" />
+        </div>
+      </Field>
+      <Field title="Inventory range multiplier">
+        <Counter
+          v-model="inventoryRangeMultiplier.value.value"
+          :type="CounterType.FloatCount"
+          v-bind="{ ...inventoryRangeMultiplier.properties }"
+        />
+      </Field>
+      <Field title="Inventory price">
+        <Input
+          v-model="inventoryPrice.value.value"
+          :type="InputType.Number"
+          v-bind="{ ...inventoryPrice.properties }"
+        />
+      </Field>
+      <Field title="Filled order delay">
+        <Counter
+          v-model="filledOrderDelay.value.value"
+          :type="CounterType.Seconds"
+          v-bind="{ ...filledOrderDelay.properties }"
+        />
+      </Field>
+      <Field title="Hanging orders">
+        <q-toggle v-model="hangingOrders.value.value" color="main-green-1" />
+      </Field>
+      <Field title="Hanging order cancel percentage">
+        <Counter
+          v-model="hangingOrderCancel.value.value"
+          :type="CounterType.Percentage"
+          v-bind="{ ...hangingOrderCancel.properties }"
+        />
+      </Field>
+      <Field title="Minimum spread">
+        <Counter
+          v-model="minimumSpread.value.value"
+          :type="CounterType.Percentage"
+          v-bind="{ ...minimumSpread.properties }"
+        />
+      </Field>
+      <Field title="Order refresh tollerance">
+        <Counter
+          v-model="orderRefreshTollerance.value.value"
+          :type="CounterType.Percentage"
+          v-bind="{ ...orderRefreshTollerance.properties }"
+        />
+      </Field>
+      <Field title="Price ceiling">
+        <Input
+          v-model="priceCeiling.value.value"
+          :type="InputType.Number"
+          v-bind="{ ...priceCeiling.properties }"
+        />
+      </Field>
+      <Field title="Price floor">
+        <Input
+          v-model="priceFloor.value.value"
+          :type="InputType.Number"
+          v-bind="{ ...priceFloor.properties }"
+        />
+      </Field>
+      <Field title="Order optimisation">
+        <q-toggle v-model="orderOptimisation.value.value" color="main-green-1" />
+      </Field>
+      <Field title="Ask order optimization depth">
+        <Input
+          v-model="askOrderOptimizationDepth.value.value"
+          :type="InputType.Number"
+          v-bind="{ ...askOrderOptimizationDepth.properties }"
+        />
+      </Field>
+      <Field title="Bid order optimization depth">
+        <Input
+          v-model="bidOrderOptimizationDepth.value.value"
+          :type="InputType.Number"
+          v-bind="{ ...bidOrderOptimizationDepth.properties }"
+        />
+      </Field>
+      <Field title="Add transaction costs">
+        <q-toggle v-model="addTransactionCosts.value.value" color="main-green-1" />
+      </Field>
+      <Field title="Price source">
+        <Select v-model="priceSource.value.value" v-bind="{ ...priceSource.properties }" />
+      </Field>
+      <Field title="Price type">
+        <Select v-model="priceType.value.value" v-bind="{ ...priceType.properties }" />
+      </Field>
+      <Field title="Price source exchange">
+        <Select
+          v-model="priceSourceExchange.value.value"
+          v-bind="{ ...priceSourceExchange.properties }"
+        />
+      </Field>
+      <Field title="Price source market">
+        <Select
+          v-model="priceSourceMarket.value.value"
+          v-bind="{ ...priceSourceMarket.properties }"
+        />
+      </Field>
+      <Field title="Take if crossed">
+        <q-toggle v-model="takeIfCrossed.value.value" color="main-green-1" />
+      </Field>
+      <Field title="Price source custom API">
+        <Input
+          v-model="priceSourceCustomAPI.value.value"
+          :type="InputType.Number"
+          v-bind="{ ...priceSourceCustomAPI.properties }"
+          class="col-6"
+        />
+      </Field>
+    </q-form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 import { useSettingsForm } from '../composables/useSettingsForm';
 import Counter, { CounterType } from './Counter.vue';
@@ -53,11 +221,18 @@ import Field from './Field.vue';
 import Input, { InputType } from './Input.vue';
 import Select from './Select/Index.vue';
 
+enum FormType {
+  Basic,
+  Advanced,
+}
+
 export default defineComponent({
   components: { Field, Select, Counter, Input },
+  emits: ['update:formType'],
 
   setup() {
     const { settingsForm, values } = useSettingsForm();
+    const formType = ref(FormType.Basic);
 
     const handleSubmit = () => {
       // eslint-disable-next-line no-console
@@ -69,6 +244,8 @@ export default defineComponent({
       ...settingsForm,
       CounterType,
       InputType,
+      formType,
+      FormType,
     };
   },
 });
--- strategy-builder-gui/src/pages/Settings/stores/settingsForm.ts
+++ strategy-builder-gui/src/pages/Settings/stores/settingsForm.ts
@@ -82,4 +82,164 @@ export const $settingsForm: $SettingsForm = {
   pingPong: {
     value: ref(false),
   },
+  orderLevels: {
+    value: ref(0),
+    properties: {
+      min: 0,
+      max: 10,
+      step: 1,
+    },
+  },
+  orderLevelAmount: {
+    value: ref(0),
+    properties: {
+      min: 0,
+      max: 10,
+      step: 1,
+    },
+  },
+  orderLevelSpread: {
+    value: ref(0),
+    properties: {
+      min: 0,
+      max: 1,
+      step: 0.1,
+    },
+  },
+  inventorySkew: {
+    value: ref(false),
+  },
+  inventoryTargetBaseCounter: {
+    value: ref(0),
+    properties: {
+      min: 0,
+      max: 1,
+      step: 0.1,
+    },
+  },
+  inventoryTargetBaseToggle: {
+    value: ref(false),
+  },
+  inventoryRangeMultiplier: {
+    value: ref(0),
+    properties: {
+      min: 0,
+      max: 1,
+      step: 0.1,
+    },
+  },
+  inventoryPrice: {
+    value: ref(''),
+    properties: {
+      placeholder: 'Input Price',
+      rightText: '',
+    },
+  },
+  filledOrderDelay: {
+    value: ref(0),
+    properties: {
+      min: 1,
+      max: 10,
+      step: 1,
+    },
+  },
+  hangingOrders: {
+    value: ref(false),
+  },
+  hangingOrderCancel: {
+    value: ref(0),
+    properties: {
+      min: 0,
+      max: 1,
+      step: 0.1,
+    },
+  },
+  minimumSpread: {
+    value: ref(0),
+    properties: {
+      min: 0,
+      max: 1,
+      step: 0.1,
+    },
+  },
+  orderRefreshTollerance: {
+    value: ref(0),
+    properties: {
+      min: 0,
+      max: 1,
+      step: 0.1,
+    },
+  },
+  priceCeiling: {
+    value: ref(''),
+    properties: {
+      placeholder: 'Price ceiling',
+      rightText: '',
+    },
+  },
+  priceFloor: {
+    value: ref(''),
+    properties: {
+      placeholder: 'Price floor',
+      rightText: '',
+    },
+  },
+  orderOptimisation: {
+    value: ref(false),
+  },
+  askOrderOptimizationDepth: {
+    value: ref(''),
+    properties: {
+      placeholder: 'Ask order',
+      rightText: '',
+    },
+  },
+  bidOrderOptimizationDepth: {
+    value: ref(''),
+    properties: {
+      placeholder: 'Bid order',
+      rightText: '',
+    },
+  },
+  addTransactionCosts: {
+    value: ref(false),
+  },
+  priceSource: {
+    value: ref(''),
+    properties: {
+      options: ['1', '2', '3', '4', '5'],
+      labelText: 'Choose source',
+    },
+  },
+  priceType: {
+    value: ref(''),
+    properties: {
+      options: ['1', '2', '3', '4', '5'],
+      labelText: 'Choose type',
+    },
+  },
+  priceSourceExchange: {
+    value: ref(''),
+    properties: {
+      options: ['1', '2', '3', '4', '5'],
+      labelText: 'Choose exchange',
+    },
+  },
+  priceSourceMarket: {
+    value: ref(''),
+    properties: {
+      options: ['1', '2', '3', '4', '5'],
+      labelText: 'Choose pair',
+    },
+  },
+  takeIfCrossed: {
+    value: ref(false),
+  },
+  priceSourceCustomAPI: {
+    value: ref(''),
+    properties: {
+      placeholder: 'Pricing API url',
+      rightText: '',
+    },
+  },
 };
