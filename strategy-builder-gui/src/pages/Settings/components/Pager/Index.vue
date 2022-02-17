b16dde0f4d2069bdc0f89dffbda7740bf2afb4cd
Mike <unclethord@gmail.com>
2022-02-17 05:45:07 +0300

    (feat) Pager layout

--- strategy-builder-gui/src/pages/Settings/components/Pager/Index.vue
+++ strategy-builder-gui/src/pages/Settings/components/Pager/Index.vue
@@ -1,11 +1,21 @@
 <template>
-  <div>Pager</div>
+  <div
+    class="flex justify-between items-center q-px-xl q-py-md q-mt-md bg-mono-grey-1 rounded-borders"
+  >
+    <q-btn class="bg-mono-grey-2 text-uppercase text-white q-py-sm" flat>back</q-btn>
+    <Steps />
+    <q-btn class="bg-main-green-1 text-uppercase text-white q-py-sm" flat>next</q-btn>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from 'vue';
 
-export default defineComponent({});
+import Steps from './Steps.vue';
+
+export default defineComponent({
+  components: { Steps },
+});
 </script>
 
 <style></style>
