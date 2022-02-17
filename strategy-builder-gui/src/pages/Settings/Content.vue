28b8343035d98ef7fb36a63850fc05feef92816b
Mike <unclethord@gmail.com>
2022-02-17 05:27:48 +0300

    (feat) Implement Pager

--- strategy-builder-gui/src/pages/Settings/Content.vue
+++ strategy-builder-gui/src/pages/Settings/Content.vue
@@ -2,16 +2,18 @@
   <div class="column">
     <Steps :in-progress-step="2" />
     <Form />
+    <Pager />
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
+import Pager from './components/Pager/Index.vue';
 import Steps from './components/Stepper/Steps.vue';
 import Form from './Form.vue';
 
 export default defineComponent({
-  components: { Steps, Form },
+  components: { Steps, Pager, Form },
 });
 </script>
---
+++ strategy-builder-gui/src/pages/Settings/components/Pager/Index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Pager</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
+
+<style></style>
