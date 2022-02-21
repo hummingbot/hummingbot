ab09946e2a3767314e8b282170ee2381e5a6ac17
Mike <unclethord@gmail.com>
2022-02-21 13:29:20 +0300

    (fix) Display form title

--- strategy-builder-gui/src/pages/Settings/Forms/Form.vue
+++ strategy-builder-gui/src/pages/Settings/Forms/Form.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="bg-mono-grey-1 q-px-xl q-py-lg rounded-borders q-mt-md full-width">
-    <div class="text-white text-h4 q-mb-lg">{{ displayTitle }}</div>
+    <div class="text-white text-h4 q-mb-lg">{{
+      currentStep === stepCount ? strategyName : title
+    }}</div>
     <q-form>
       <slot />
     </q-form>
@@ -8,7 +10,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   props: {
@@ -16,13 +18,6 @@ export default defineComponent({
     currentStep: { type: Number, required: true, default: () => 2 },
     stepCount: { type: Number, required: true, default: () => 3 },
     strategyName: { type: String, required: false, default: () => '' },
-  },
-  setup(props) {
-    const displayTitle = computed(() =>
-      props.currentStep === props.stepCount ? props.strategyName : props.title,
-    );
-
-    return { displayTitle };
   },
 });
 </script>
