0f53349136145c02e435faa8632483c50731e227
Petio Petrov <petioptrv@icloud.com>
2022-02-22 11:32:53 +0700

    Update hummingbot/client/config/config_data_types.py
    Co-authored-by: Abel Armoa <30988000+aarmoa@users.noreply.github.com>

--- hummingbot/client/config/config_data_types.py
+++ hummingbot/client/config/config_data_types.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
-from typing import Any, Optional, Callable
+from typing import Any, Callable, Optional
 
 from pydantic import BaseModel
 from pydantic.schema import default_ref_template
