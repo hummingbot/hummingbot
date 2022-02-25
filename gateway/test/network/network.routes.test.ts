61407d1bf9dbe992e03fd4ef7088e3d75a4fb228
James <james@hummingbot.io>
2022-02-25 15:44:14 +0100

    Use mocks in testing

--- gateway/test/network/network.routes.test.ts
+++ gateway/test/network/network.routes.test.ts
@@ -2,17 +2,36 @@ import request from 'supertest';
 import { gatewayApp } from '../../src/app';
 import { patch, unpatch } from '../services/patch';
 import { Ethereum } from '../../src/chains/ethereum/ethereum';
+import { Harmony } from '../../src/chains/harmony/harmony';
+import { Avalanche } from '../../src/chains/avalanche/avalanche';
 
 let eth: Ethereum;
+let avalanche: Avalanche;
+let harmony: Harmony;
 beforeAll(async () => {
   eth = Ethereum.getInstance('kovan');
   await eth.init();
+
+  avalanche = Avalanche.getInstance('fuji');
+  await avalanche.init();
+
+  harmony = Harmony.getInstance('testnet');
+  await harmony.init();
 });
 
 afterEach(() => unpatch());
 
 describe('GET /network/status', () => {
   it('should return 200 when asking for harmony network status', async () => {
+    patch(harmony, 'chain', () => {
+      return 'testnet';
+    });
+    patch(harmony, 'rpcUrl', 'http://...');
+    patch(harmony, 'chainId', 88);
+    patch(harmony, 'getCurrentBlockNumber', () => {
+      return 3;
+    });
+
     await request(gatewayApp)
       .get(`/network/status`)
       .query({
@@ -52,6 +71,15 @@ describe('GET /network/status', () => {
   });
 
   it('should return 200 when asking for avalance network status', async () => {
+    patch(avalanche, 'chain', () => {
+      return 'fuji';
+    });
+    patch(avalanche, 'rpcUrl', 'http://...');
+    patch(avalanche, 'chainId', 20);
+    patch(avalanche, 'getCurrentBlockNumber', () => {
+      return 2;
+    });
+
     await request(gatewayApp)
       .get(`/network/status`)
       .query({
@@ -67,6 +95,17 @@ describe('GET /network/status', () => {
   });
 
   it('should return 200 when requesting network status without specifying', async () => {
+    patch(eth, 'getCurrentBlockNumber', () => {
+      return 212;
+    });
+
+    patch(avalanche, 'getCurrentBlockNumber', () => {
+      return 204;
+    });
+    patch(harmony, 'getCurrentBlockNumber', () => {
+      return 100;
+    });
+
     await request(gatewayApp)
       .get(`/network/status`)
       .expect('Content-Type', /json/)
