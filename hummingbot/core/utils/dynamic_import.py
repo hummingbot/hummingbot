aa927cbed69589a43ae8d9385e6d332be438fc67
Nullably <37262506+Nullably@users.noreply.github.com>
2022-02-25 13:15:49 +0800

    (feat) add test for dynamic import

--- hummingbot/core/utils/dynamic_import.py
+++ hummingbot/core/utils/dynamic_import.py
@@ -5,7 +5,10 @@ import os
 from hummingbot.strategy.lite_strategy_base import LiteStrategyBase
 
 
-def import_lite_strategy_sub_class(lite_file_name: str):
+def import_lite_strategy_sub_class(lite_file_name: str) -> LiteStrategyBase:
+    """
+    Imports a Lite strategy class (any classes that derive from LiteStrategyBase) at run-time.
+    """
     name = os.path.basename(lite_file_name).split(".")[0]
     spec = importlib.util.spec_from_file_location(name, lite_file_name)
     module = importlib.util.module_from_spec(spec)
@@ -14,3 +17,4 @@ def import_lite_strategy_sub_class(lite_file_name: str):
         obj = getattr(module, x)
         if inspect.isclass(obj) and issubclass(obj, LiteStrategyBase) and obj.__name__ != "LiteStrategyBase":
             return obj
+    raise Exception("The file does not contain any LiteStrategyBase derived class.")
