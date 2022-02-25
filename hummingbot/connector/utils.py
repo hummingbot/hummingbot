2089e426f0722da07f13e9bbfaf1e1c28b0b781d
abel <abel@hummingbot.io>
2022-02-25 19:00:53 -0300

    Added docstring in TimeSynchronizerRESTPreProcessor

--- hummingbot/connector/utils.py
+++ hummingbot/connector/utils.py
@@ -88,6 +88,10 @@ def get_new_client_order_id(
 
 
 class TimeSynchronizerRESTPreProcessor(RESTPreProcessorBase):
+    """
+    This pre processor is intended to be used in those connectors that require synchronization with the server time
+    to accept API requests. It ensures the synchronizer has at least one server time sample before being used.
+    """
 
     def __init__(self, synchronizer: TimeSynchronizer, time_provider: Callable):
         super().__init__()
