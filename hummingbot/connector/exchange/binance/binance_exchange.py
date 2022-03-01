579d2f982e34676e62baa89e98ed5a235a390ac5
abel <abel@hummingbot.io>
2022-03-01 15:06:34 -0300

    Refactoring in Binance spot connector to use the TimeSynchronizer for all requests. Created a unique function to requests

--- hummingbot/connector/exchange/binance/binance_exchange.py
+++ hummingbot/connector/exchange/binance/binance_exchange.py
@@ -966,7 +966,10 @@ class BinanceExchange(ExchangeBase):
         remote_asset_names = set()
 
         await self._binance_time_synchronizer.update_server_time_if_not_initialized(
-            time_provider=self._get_current_server_time())
+            time_provider=web_utils.get_current_server_time(
+                throttler=self._throttler,
+                domain=self._domain,
+            ))
 
         try:
             account_info = await self._api_request(
