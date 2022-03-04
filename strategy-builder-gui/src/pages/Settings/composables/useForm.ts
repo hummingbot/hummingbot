97c1e1c1e42874ae6580bf613af602f5bcefb952
Mike <33065483+Th0rd@users.noreply.github.com>
2022-03-04 12:40:31 +0300

    (fix) Float form values

--- strategy-builder-gui/src/pages/Settings/composables/useForm.ts
+++ strategy-builder-gui/src/pages/Settings/composables/useForm.ts
@@ -5,10 +5,18 @@ import { $form } from '../stores/form';
 
 export { BtnToggleType } from '../stores/form.types';
 
+const fixValue = (value: number | string | boolean) => {
+  if (typeof value === 'number') {
+    return Number(value.toFixed(2));
+  }
+
+  return value;
+};
+
 export const useForm = (strategyName: Ref<StrategyName>) => {
   const values = computed(() =>
     Object.keys($form[strategyName.value]).reduce(
-      (acc, key) => ({ ...acc, [key]: $form[strategyName.value][key].value.value }),
+      (acc, key) => ({ ...acc, [key]: fixValue($form[strategyName.value][key].value.value) }),
       {},
     ),
   );
