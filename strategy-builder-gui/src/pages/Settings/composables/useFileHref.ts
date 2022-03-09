a32046d89e29dd256d0a9b70f91aa3d7758cefc1
Mike <33065483+Th0rd@users.noreply.github.com>
2022-03-09 17:27:04 +0300

    (fix) File data format

--- strategy-builder-gui/src/pages/Settings/composables/useFileHref.ts
+++ strategy-builder-gui/src/pages/Settings/composables/useFileHref.ts
@@ -1,4 +1,7 @@
 import { ComputedRef } from 'vue';
 
 export const useFileHref = (values: ComputedRef) =>
-  `data:application/octet-stream,${JSON.stringify(values.value).replace(/[,{}]/g, '\n')}`;
+  `data:application/octet-stream,${JSON.stringify(values.value)
+    .replace(/,/g, '\n')
+    .replace(/[{}]/g, '')
+    .replace(/"/g, ' ')}`;
