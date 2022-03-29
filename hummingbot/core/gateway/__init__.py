55355714d1dd09eceb41b83f1ecd3fe78424d442
Martin Kou <martin@hummingbot.io>
2022-03-29 00:22:31 -0700

    Updated gateway docker image.

--- hummingbot/core/gateway/__init__.py
+++ hummingbot/core/gateway/__init__.py
@@ -13,7 +13,7 @@ _default_paths: Optional["GatewayPaths"] = None
 _hummingbot_pipe: Optional[aioprocessing.AioConnection] = None
 
 GATEWAY_DOCKER_REPO: str = "coinalpha/gateway-v2-dev"
-GATEWAY_DOCKER_TAG: str = "20220328-1"
+GATEWAY_DOCKER_TAG: str = "20220329"
 S_DECIMAL_0: Decimal = Decimal(0)
 
 
