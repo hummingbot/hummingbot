5f7cab829bb4e86fd35606ed75d72b13a4c3198e
Daniel Tan <daniel@hummingbot.io>
2022-04-01 16:52:27 +0800

    (fix) use pwd and grp to obtain uid and gid

--- bin/hummingbot_quickstart.py
+++ bin/hummingbot_quickstart.py
@@ -3,15 +3,17 @@
 import path_util        # noqa: F401
 import argparse
 import asyncio
+import grp
 import logging
 import os
-from pathlib import Path
 import pwd
+import subprocess
+
+from pathlib import Path
 from typing import (
     Coroutine,
     List,
 )
-import subprocess
 
 from bin.hummingbot import (
     detect_available_port,
@@ -56,7 +58,11 @@ class CmdlineParser(argparse.ArgumentParser):
 
 
 def autofix_permissions(user_group_spec: str):
-    uid, gid = [int(i) for i in user_group_spec.split(':')]
+    uid, gid = [sub_str for sub_str in user_group_spec.split(':')]
+
+    uid = int(uid) if uid.isnumeric() else pwd.getpwnam(uid).pw_uid
+    gid = int(gid) if gid.isnumeric() else grp.getgrnam(gid).gr_gid
+
     os.environ["HOME"] = pwd.getpwuid(uid).pw_dir
     project_home: str = os.path.realpath(os.path.join(__file__, "../../"))
 
