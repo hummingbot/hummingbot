0f43d9504f6509cff31a7a78cd6d1e622d066dc5
James <james@hummingbot.io>
2022-04-25 11:24:05 +0200

    Improve validator

--- gateway/src/services/config-validators.ts
+++ gateway/src/services/config-validators.ts
@@ -1,12 +1,57 @@
-import { Validator, mkValidator, isFractionString } from './validators';
+import { Validator, isFloatString, isFractionString } from './validators';
 
 export const invalidAllowedSlippage: string =
   'allowedSlippage should be a number or a string of a fraction.';
 
-export const validateAllowedSlippage: Validator = mkValidator(
+export const missingParameter = (key: string): string => {
+  return `The request is missing a key that ends with: ${key}`;
+};
+
+// This is a specialized version of mkValidator. Since config parameters are a
+// chain of keys, sometimes we want to generalize the validator behavior for
+// certain groups of keys (for example: uniswap.versions.v2.allowedSlippage and
+// avalanche.allowedSlippage). These keys live in different files but are
+// expected to have the same type of permissible values.
+export const mkConfigValidator = (
+  key: string,
+  errorMsg: string,
+  condition: (x: any) => boolean,
+  optional: boolean = false
+): Validator => {
+  return (req: any) => {
+    let matchingKey: string | null = null;
+    for (const reqKey in req) {
+      if (reqKey.endsWith(key)) {
+        matchingKey = reqKey;
+        break;
+      }
+    }
+
+    const errors: Array<string> = [];
+    if (typeof matchingKey === 'string') {
+      if (req[matchingKey]) {
+        if (!condition(req[matchingKey])) {
+          errors.push(errorMsg);
+        }
+      } else {
+        if (!optional) {
+          errors.push(missingParameter(key));
+        }
+      }
+    } else {
+      if (!optional) {
+        errors.push(missingParameter(key));
+      }
+    }
+
+    return errors;
+  };
+};
+
+export const validateAllowedSlippage: Validator = mkConfigValidator(
   'allowedSlippage',
   invalidAllowedSlippage,
   (val) =>
     typeof val === 'number' ||
-    (typeof val === 'string' && isFractionString(val))
+    (typeof val === 'string' && (isFractionString(val) || isFloatString(val)))
 );
---
+++ gateway/test/services/config-validators.test.ts
@@ -0,0 +1,58 @@
+import {
+  invalidAllowedSlippage,
+  validateAllowedSlippage,
+  missingParameter,
+} from '../../src/services/config-validators';
+import 'jest-extended';
+
+describe('validateAllowedSlippage', () => {
+  it('valid when req.uniswap.versions.v2.allowedSlippage is a fraction string', () => {
+    expect(
+      validateAllowedSlippage({
+        'req.uniswap.versions.v2.allowedSlippage': '1/100',
+      })
+    ).toEqual([]);
+  });
+
+  it('valid when req.uniswap.versions.v2.allowedSlippage is a number', () => {
+    expect(
+      validateAllowedSlippage({ 'avalanche.allowedSlippage': 0.1 })
+    ).toEqual([]);
+  });
+
+  it('valid when req.avalanche.allowedSlippage is a fraction string', () => {
+    expect(
+      validateAllowedSlippage({ 'req.avalanche.allowedSlippage': '3/10' })
+    ).toEqual([]);
+  });
+
+  it('valid when req.avalanche.allowedSlippage is a float string', () => {
+    expect(
+      validateAllowedSlippage({ 'req.avalanche.allowedSlippage': '0.005' })
+    ).toEqual([]);
+  });
+
+  it('valid when req.avalanche.allowedSlippage is a float string', () => {
+    expect(
+      validateAllowedSlippage({ 'req.avalanche.allowedSlippage': '0.005' })
+    ).toEqual([]);
+  });
+
+  it('valid when req.avalanche.allowedSlippage is a number', () => {
+    expect(
+      validateAllowedSlippage({ 'avalanche.allowedSlippage': 0.005 })
+    ).toEqual([]);
+  });
+
+  it('invalid when req.allowedSlippage does not exist', () => {
+    expect(validateAllowedSlippage({ hello: 'world' })).toEqual([
+      missingParameter('allowedSlippage'),
+    ]);
+  });
+
+  it('invalid when req.avalanche.allowedSlippage is a non-numerical string', () => {
+    expect(
+      validateAllowedSlippage({ 'avalanche.allowedSlippage': 'hello' })
+    ).toEqual([invalidAllowedSlippage]);
+  });
+});
