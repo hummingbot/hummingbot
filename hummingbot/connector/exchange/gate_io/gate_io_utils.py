a053eeebefcf9e91818663a5bef584af75effe9f
Michele <michele.frettoli@hummingbot.io>
2022-04-26 18:45:30 +0200

    restore some of gate_io_utils

--- hummingbot/connector/exchange/gate_io/gate_io_utils.py
+++ hummingbot/connector/exchange/gate_io/gate_io_utils.py
@@ -1,3 +1,22 @@
-# Compatibility for hummingbot/client/settings.py
+import hummingbot.connector.exchange.gate_io.gate_io_constants as CONSTANTS
+from hummingbot.client.config.config_methods import using_exchange
+from hummingbot.client.config.config_var import ConfigVar
 
-from .gate_io_constants import *  # noqa: F403 F401
+CENTRALIZED = True
+EXAMPLE_PAIR = "BTC-USDT"
+DEFAULT_FEES = [0.2, 0.2]
+
+KEYS = {
+    "gate_io_api_key":
+        ConfigVar(key="gate_io_api_key",
+                  prompt=f"Enter your {CONSTANTS.EXCHANGE_NAME} API key >>> ",
+                  required_if=using_exchange("gate_io"),
+                  is_secure=True,
+                  is_connect_key=True),
+    "gate_io_secret_key":
+        ConfigVar(key="gate_io_secret_key",
+                  prompt=f"Enter your {CONSTANTS.EXCHANGE_NAME} secret key >>> ",
+                  required_if=using_exchange("gate_io"),
+                  is_secure=True,
+                  is_connect_key=True),
+}
