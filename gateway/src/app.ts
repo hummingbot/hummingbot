4dbf30986099652f674ea563d49a62eca61850e3
kanghoulin <kanghoulin@ops>
2022-04-29 15:44:10 +0800

    (feat) add connector openocean and test files

--- gateway/src/app.ts
+++ gateway/src/app.ts
@@ -19,6 +19,7 @@ import { EVMRoutes } from './evm/evm.routes';
 import { AmmRoutes } from './amm/amm.routes';
 import { PangolinConfig } from './connectors/pangolin/pangolin.config';
 import { UniswapConfig } from './connectors/uniswap/uniswap.config';
+import { OpenoceanConfig } from './connectors/openocean/openocean.config';
 import { AvailableNetworks } from './services/config-manager-types';
 import morgan from 'morgan';
 
@@ -54,6 +55,7 @@ gatewayApp.get('/', (_req: Request, res: Response) => {
 interface ConnectorsResponse {
   uniswap: Array<AvailableNetworks>;
   pangolin: Array<AvailableNetworks>;
+  openocean: Array<AvailableNetworks>;
 }
 
 gatewayApp.get(
@@ -62,6 +64,7 @@ gatewayApp.get(
     res.status(200).json({
       uniswap: UniswapConfig.config.availableNetworks,
       pangolin: PangolinConfig.config.availableNetworks,
+      openocean: OpenoceanConfig.config.availableNetworks,
     });
   })
 );
