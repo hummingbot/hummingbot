315e00a1c386d2ac815ed9c019db21e5b8d9e5ca
Navneeth Jayendran <navneejay@gmail.com>
2022-05-13 20:49:33 -0700

    Fix gateway build

--- gateway/src/connectors/defira/defira.controllers.ts
+++ gateway/src/connectors/defira/defira.controllers.ts
@@ -42,7 +42,7 @@ export interface TradeInfo {
 
 export async function getTradeInfo(
   ethereumish: Ethereumish,
-  uniswapish: uniswapish,
+  uniswapish: Uniswapish,
   baseAsset: string,
   quoteAsset: string,
   baseAmount: Decimal,
@@ -351,7 +351,7 @@ export function getFullTokenFromSymbol(
 
 export async function estimateGas(
   ethereumish: Ethereumish,
-  uniswapish: uniswapish
+  uniswapish: Uniswapish
 ): Promise<EstimateGasResponse> {
   const gasPrice: number = ethereumish.gasPrice;
   const gasLimit: number = uniswapish.gasLimit;
