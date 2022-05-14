16815b8b5db3c41fb5a9082fe8afb1f17cd39bc0
zuzu_cat <zuyichen@gmail.com>
2022-05-14 00:05:25 -0500

    feat remove v3 files and edit defira config

--- gateway/src/connectors/defira/defira.config.ts
+++ gateway/src/connectors/defira/defira.config.ts
@@ -2,50 +2,28 @@ import { ConfigManagerV2 } from '../../services/config-manager-v2';
 import { AvailableNetworks } from '../../services/config-manager-types';
 export namespace DefiraConfig {
   export interface NetworkConfig {
-    allowedSlippage: (version: number) => string;
-    gasLimit: (version: number) => number;
-    ttl: (version: number) => number;
+    allowedSlippage: () => string;
+    gasLimit: () => number;
+    ttl: () => number;
     uniswapV2RouterAddress: (network: string) => string;
-    // TODO: probably will not need this
-    uniswapV3RouterAddress: (network: string) => string;
-    // TODO: probably will not need this
-    uniswapV3NftManagerAddress: (network: string) => string;
-    tradingTypes: (network: string) => Array<string>;
+    tradingTypes: Array<string>;
     availableNetworks: Array<AvailableNetworks>;
   }
 
   export const config: NetworkConfig = {
-    allowedSlippage: (version: number) =>
-      ConfigManagerV2.getInstance().get(
-        `defira.versions.v${version}.allowedSlippage`
-      ),
-    gasLimit: (version: number) =>
-      ConfigManagerV2.getInstance().get(`defira.versions.v${version}.gasLimit`),
-    ttl: (version: number) =>
-      ConfigManagerV2.getInstance().get(`defira.versions.v${version}.ttl`),
+    allowedSlippage: () =>
+      ConfigManagerV2.getInstance().get(`defira.versions.v2.allowedSlippage`),
+    gasLimit: () => ConfigManagerV2.getInstance().get(`defira.gasLimit`),
+    ttl: () => ConfigManagerV2.getInstance().get(`defira.ttl`),
     uniswapV2RouterAddress: (network: string) =>
       ConfigManagerV2.getInstance().get(
-        `defira.contractAddresses.${network}.uniswapV2RouterAddress`
+        `defira.contractAddresses.${network}.routerAddress`
       ),
-
-    // TODO: probably will not need this
-    uniswapV3RouterAddress: (network: string) =>
-      ConfigManagerV2.getInstance().get(
-        `defira.contractAddresses.${network}.uniswapV3RouterAddress`
-      ),
-    // TODO: probably will not need this
-    uniswapV3NftManagerAddress: (network: string) =>
-      ConfigManagerV2.getInstance().get(
-        `defira.contractAddresses.${network}.uniswapV3NftManagerAddress`
-      ),
-    tradingTypes: (network: string) =>
-      network === 'v2' ? ['EVM_AMM'] : ['EVM_Range_AMM'],
+    tradingTypes: ['EVM_AMM'],
     availableNetworks: [
       {
-        chain: 'ethereum',
-        networks: Object.keys(
-          ConfigManagerV2.getInstance().get('defira.contractAddresses')
-        ),
+        chain: 'harmony',
+        networks: ['mainnet', 'testnet'],
       },
     ],
   };
