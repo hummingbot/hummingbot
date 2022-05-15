e7bed31cece5be74e25e8ddd2e56a5679e3a4d47
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2022-05-15 20:00:48 -0300

    Updating serum fixtures configurations.

--- gateway/test/connectors/serum/fixtures/serumConfig.ts
+++ gateway/test/connectors/serum/fixtures/serumConfig.ts
@@ -7,9 +7,7 @@ export default {
           '3mFmUEqdf86r7VEs8gCAHaEZ2gBmXzpgVPPjQ71fbMDYejso3Qqd18AgnkUbyqFyHrCDC7BUJLSXRqZuLK7Bd9yP',
       },
       payer: {
-        publicKey: 'Frao7w1tXtB9i2PAj6bbLXLVUypLrDCm92MNpBWNg6VW',
-        privateKey:
-          '5xxXkxGHPx77yuKJJeCfGbSERvxSuiUm9zSS7PHVQijsUQZd7BLek8ER5vZ8RVfojNQ3hx5QiZLgpHMxPstjLeRN',
+        publicKey: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
       },
     },
   },
