94b3429781f3eee0b57c43c7777981352888c5fd
vic-en <victoreni14@gmail.com>
2022-05-18 01:13:08 -0500

    delay logging when market isn't ready in amm strategy

--- hummingbot/strategy/amm_arb/amm_arb.py
+++ hummingbot/strategy/amm_arb/amm_arb.py
@@ -1,9 +1,10 @@
-import logging
 import asyncio
+import logging
 from decimal import Decimal
 from functools import lru_cache
+from typing import Callable, Dict, List, Optional, Tuple, cast
+
 import pandas as pd
-from typing import List, Dict, Tuple, Optional, Callable, cast
 
 from hummingbot.client.performance import PerformanceMetrics
 from hummingbot.client.settings import AllConnectorSettings
@@ -14,12 +15,19 @@ from hummingbot.core.clock import Clock
 from hummingbot.core.data_type.limit_order import LimitOrder
 from hummingbot.core.data_type.market_order import MarketOrder
 from hummingbot.core.data_type.trade_fee import TokenAmount
-from hummingbot.core.event.events import BuyOrderCompletedEvent, MarketOrderFailureEvent, OrderCancelledEvent, OrderExpiredEvent, OrderType, SellOrderCompletedEvent
+from hummingbot.core.event.events import (
+    BuyOrderCompletedEvent,
+    MarketOrderFailureEvent,
+    OrderCancelledEvent,
+    OrderExpiredEvent,
+    OrderType,
+    SellOrderCompletedEvent,
+)
 from hummingbot.core.rate_oracle.rate_oracle import RateOracle
 from hummingbot.core.utils.async_utils import safe_ensure_future
 from hummingbot.logger import HummingbotLogger
 from hummingbot.strategy.amm_arb.data_types import ArbProposalSide
-from hummingbot.strategy.amm_arb.utils import create_arb_proposals, ArbProposal
+from hummingbot.strategy.amm_arb.utils import ArbProposal, create_arb_proposals
 from hummingbot.strategy.market_trading_pair_tuple import MarketTradingPairTuple
 from hummingbot.strategy.strategy_py_base import StrategyPyBase
 
@@ -155,7 +163,7 @@ class AmmArbStrategy(StrategyPyBase):
         """
         if not self._all_markets_ready:
             self._all_markets_ready = all([market.ready for market in self.active_markets])
-            if not self._all_markets_ready:
+            if not self._all_markets_ready and int(timestamp) % 10 == 0:
                 unready_markets = ', '.join([market.name for market in self.active_markets if market.ready is False])
                 self.logger().warning(f"Markets are not ready ({unready_markets}). Please wait...")
                 return
