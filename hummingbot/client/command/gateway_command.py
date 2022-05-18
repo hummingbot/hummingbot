075a5579e9de5c46290116317b8d641feaefc1a1
James <james@hummingbot.io>
2022-05-18 17:49:49 +0200

    get started

--- hummingbot/client/command/gateway_command.py
+++ hummingbot/client/command/gateway_command.py
@@ -1,55 +1,47 @@
 #!/usr/bin/env python
-import aiohttp
 import asyncio
-from contextlib import contextmanager
-import docker
 import itertools
 import json
-import pandas as pd
-from typing import (
-    Dict,
-    Any,
-    TYPE_CHECKING,
-    List,
-    Generator,
-)
+from contextlib import contextmanager
+from typing import TYPE_CHECKING, Any, Array, Dict, Generator, List, Optional
 
+import aiohttp
+import pandas as pd
+
+import docker
+from hummingbot.client.config.config_helpers import refresh_trade_fees_config, save_to_yml
+from hummingbot.client.config.global_config_map import global_config_map
+from hummingbot.client.config.security import Security
 from hummingbot.client.settings import (
     GATEWAY_CONNECTORS,
     GLOBAL_CONFIG_PATH,
-    GatewayConnectionSetting
+    AllConnectorSettings,
+    GatewayConnectionSetting,
 )
+from hummingbot.client.ui.completer import load_completer
 from hummingbot.core.gateway import (
-    docker_ipc,
-    docker_ipc_with_generator,
-    get_gateway_container_name,
-    get_gateway_paths,
     GATEWAY_DOCKER_REPO,
     GATEWAY_DOCKER_TAG,
     GatewayPaths,
+    docker_ipc,
+    docker_ipc_with_generator,
     get_default_gateway_port,
+    get_gateway_container_name,
+    get_gateway_paths,
     start_gateway,
-    stop_gateway
+    stop_gateway,
 )
+from hummingbot.core.gateway.gateway_http_client import GatewayHttpClient
 from hummingbot.core.gateway.status_monitor import Status
 from hummingbot.core.utils.async_utils import safe_ensure_future
 from hummingbot.core.utils.gateway_config_utils import (
-    search_configs,
     build_config_dict_display,
     build_connector_display,
     build_wallet_display,
     native_tokens,
+    search_configs,
 )
-from hummingbot.core.gateway.gateway_http_client import GatewayHttpClient
 from hummingbot.core.utils.ssl_cert import certs_files_exist, create_self_sign_certs
-from hummingbot.client.config.config_helpers import (
-    save_to_yml,
-    refresh_trade_fees_config,
-)
-from hummingbot.client.config.global_config_map import global_config_map
-from hummingbot.client.config.security import Security
-from hummingbot.client.settings import AllConnectorSettings
-from hummingbot.client.ui.completer import load_completer
 
 if TYPE_CHECKING:
     from hummingbot.client.hummingbot_application import HummingbotApplication
@@ -84,6 +76,9 @@ class GatewayCommand:
 
     def gateway_stop(self):
         safe_ensure_future(stop_gateway(), loop=self.ev_loop)
+
+    def gateway_tokens(self, connector_chain_network: str):
+        safe_ensure_future(self._gateway_tokens(connector_chain_network), loop=self.ev_loop)
 
     def generate_certs(self):
         safe_ensure_future(self._generate_certs(), loop=self.ev_loop)
@@ -496,3 +491,45 @@ class GatewayCommand:
 
                 # Reload completer here to include newly added gateway connectors
                 self.app.input_field.completer = load_completer(self)
+
+    async def _gateway_tokens(
+            self,           # type: HummingbotApplication
+            connector_chain_network: str
+    ):
+        with begin_placeholder_mode(self):
+            gateway_connections_conf: List[Dict[str, str]] = GatewayConnectionSetting.load()
+            vals = connector_chain_network.split('_')  # uniswap_ethereum_kovan
+            if vals.length == 3:
+                connector: str = vals[0]
+                chain: str = vals[1]
+                network: str = vals[2]
+
+                selected_connection: Optional[Dict[str, str]] = None
+
+                for connection in gateway_connections_conf:
+                    if connection['connector'] == connector \
+                       and connection['chain'] == chain \
+                       and connection['network'] == network:
+                        selected_connection = connection
+                        break
+
+                if selected_connection is None:
+                    self.notify(f"No available blockchain networks available for the connector '{connector_chain_network}'.")
+                else:
+
+                    current_tokens: Array[str] = selected_connection.get('tokens', [])
+
+                    print(current_tokens)
+                    print(selected_connection)
+
+                    self.app.clear_input()
+                    self.placeholder_mode = True
+                    new_tokens = await self.app.prompt(
+                        prompt=f"Enter {connector}_{chain}_{network} tokens whose balances you want to monitor in hummingbot for wallet {selected_connection['wallet_address']} >>> "
+                    )
+                    self.app.clear_input()
+
+                    print(new_tokens)
+
+            else:
+                self.notify(f"No available blockchain networks available for the connector '{connector_chain_network}'.")
