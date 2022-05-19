ed8a8f55cfaa076b9491a24efe994041acb6e3b6
vic-en <victoreni14@gmail.com>
2022-05-18 22:21:47 -0500

    fix backward compatibility issue

--- hummingbot/connector/gateway_EVM_AMM.py
+++ hummingbot/connector/gateway_EVM_AMM.py
@@ -79,6 +79,7 @@ class GatewayEVMAMM(ConnectorBase):
                  network: str,
                  wallet_address: str,
                  trading_pairs: List[str] = [],
+                 additional_spenders: List[str] = [],  # not implemented
                  trading_required: bool = True
                  ):
         """
