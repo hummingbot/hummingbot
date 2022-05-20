60b089ac0977779b237cefb41056720ecdc62b9d
mhrvth <contact@mhrvth.net>
2022-05-20 19:28:37 +0200

    (feat) config map pydantic tests

--- test/hummingbot/strategy/cross_exchange_market_making/test_cross_exchange_market_making_config_map_pydantic.py
+++ test/hummingbot/strategy/cross_exchange_market_making/test_cross_exchange_market_making_config_map_pydantic.py
@@ -1,10 +1,18 @@
 import unittest
+from decimal import Decimal
+from pathlib import Path
 from typing import Dict
+from unittest.mock import patch
 
-from hummingbot.client.config.config_helpers import ClientConfigAdapter
-from hummingbot.client.settings import AllConnectorSettings
+import yaml
+
+from hummingbot.client.config.config_helpers import ClientConfigAdapter, ConfigValidationError
 from hummingbot.strategy.cross_exchange_market_making.cross_exchange_market_making_config_map_pydantic import (
+    ActiveOrderRefreshMode,
     CrossExchangeMarketMakingConfigMap,
+    OracleConversionRateMode,
+    PassiveOrderRefreshMode,
+    TakerToMakerConversionRateMode,
 )
 
 
@@ -35,6 +43,13 @@ class CrossExchangeMarketMakingConfigMapPydanticTest(unittest.TestCase):
         }
         return config_settings
 
+    def test_top_depth_tolerance_prompt(self):
+        self.config_map.maker_market_trading_pair = self.trading_pair
+        prompt = self.config_map.top_depth_tolerance_prompt(self.config_map)
+        expected = f"What is your top depth tolerance? (in {self.base_asset})"
+
+        self.assertEqual(expected, prompt)
+
     def test_order_amount_prompt(self):
         self.config_map.maker_market_trading_pair = self.trading_pair
         prompt = self.config_map.order_amount_prompt(self.config_map)
@@ -42,22 +57,49 @@ class CrossExchangeMarketMakingConfigMapPydanticTest(unittest.TestCase):
 
         self.assertEqual(expected, prompt)
 
-    def test_maker_trading_pair_prompt(self):
-        exchange = self.config_map.maker_market = self.maker_exchange
-        example = AllConnectorSettings.get_example_pairs().get(exchange)
+    @patch(
+        "hummingbot.client.config.config_data_types.validate_market_trading_pair"
+    )
+    def test_validators(self, _):
+        self.config_map.order_refresh_mode = "active_order_refresh"
+        self.assertIsInstance(self.config_map.order_refresh_mode.hb_config, ActiveOrderRefreshMode)
 
-        prompt = self.config_map.trading_pair_prompt(self.config_map, True)
-        expected = f"Enter the token trading pair you would like to trade on maker market: {exchange} " \
-                   f"(e.g. {example})"
+        self.config_map.order_refresh_mode = "passive_order_refresh"
+        self.config_map.order_refresh_mode.cancel_order_threshold = Decimal("1.0")
+        self.config_map.order_refresh_mode.cancel_order_threshold = Decimal("2.0")
+        self.assertIsInstance(self.config_map.order_refresh_mode.hb_config, PassiveOrderRefreshMode)
 
-        self.assertEqual(expected, prompt)
+        with self.assertRaises(ConfigValidationError) as e:
+            self.config_map.order_refresh_mode = "XXX"
 
-    def test_taker_trading_pair_prompt(self):
-        exchange = self.config_map.maker_market = self.taker_exchange
-        example = AllConnectorSettings.get_example_pairs().get(exchange)
+        error_msg = (
+            "Invalid order refresh mode, please choose value from ['passive_order_refresh', 'active_order_refresh']."
+        )
+        self.assertEqual(error_msg, str(e.exception))
 
-        prompt = self.config_map.trading_pair_prompt(self.config_map, False)
-        expected = f"Enter the token trading pair you would like to trade on taker market: {exchange} " \
-                   f"(e.g. {example})"
+        self.config_map.conversion_rate_mode = "rate_oracle_conversion_rate"
+        self.assertIsInstance(self.config_map.conversion_rate_mode.hb_config, OracleConversionRateMode)
 
-        self.assertEqual(expected, prompt)
+        self.config_map.conversion_rate_mode = "fixed_conversion_rate"
+        self.config_map.conversion_rate_mode.taker_to_maker_base_conversion_rate = Decimal("1.0")
+        self.config_map.conversion_rate_mode.taker_to_maker_quote_conversion_rate = Decimal("2.0")
+        self.assertIsInstance(self.config_map.conversion_rate_mode.hb_config, TakerToMakerConversionRateMode)
+
+        with self.assertRaises(ConfigValidationError) as e:
+            self.config_map.conversion_rate_mode = "XXX"
+
+        error_msg = (
+            "Invalid conversion rate mode, please choose value from ['rate_oracle_conversion_rate', 'fixed_conversion_rate']."
+        )
+        self.assertEqual(error_msg, str(e.exception))
+
+    def test_load_configs_from_yaml(self):
+        cur_dir = Path(__file__).parent
+        f_path = cur_dir / "test_config.yml"
+
+        with open(f_path, "r") as file:
+            data = yaml.safe_load(file)
+
+        loaded_config_map = ClientConfigAdapter(CrossExchangeMarketMakingConfigMap(**data))
+
+        self.assertEqual(self.config_map, loaded_config_map)
