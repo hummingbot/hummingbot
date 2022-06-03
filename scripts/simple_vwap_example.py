782a0799af8328be2c07ba385067329d7d60a83b
cardosofede <federico.cardoso.e@gmail.com>
2022-06-03 17:45:48 -0300

    (feat) mvp vwap strategy

--- scripts/simple_vwap_example.py
+++ scripts/simple_vwap_example.py
@@ -1,4 +1,5 @@
 import logging
+import math
 from decimal import Decimal
 from typing import Dict
 
@@ -9,7 +10,7 @@ from hummingbot.core.rate_oracle.rate_oracle import RateOracle
 from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
 
 
-class AVWAPExample(ScriptStrategyBase):
+class VWAPExample(ScriptStrategyBase):
     """
     This example lets you create one VWAP in a market using a percentage of the sum volume of the order book
     until a spread from the mid price.
@@ -19,8 +20,8 @@ class AVWAPExample(ScriptStrategyBase):
       - How to code a "utility" strategy
     """
     last_ordered_ts = 0
-    vwap: Dict = dict(connector_name="binance_paper_trade", trading_pair="ETH-USDT", is_buy=True,
-                      total_volume_usd=100000, price_spread=0.002, volume_perc=0.001, order_delay_time=10)
+    vwap: Dict = {'connector_name': "binance_paper_trade", 'trading_pair': "ETH-USDT", 'is_buy': True,
+                  'total_volume_usd': 100000, 'price_spread': 0.001, 'volume_perc': 0.001, 'order_delay_time': 10}
     markets = {vwap["connector_name"]: {vwap["trading_pair"]}}
 
     def on_tick(self):
@@ -38,7 +39,8 @@ class AVWAPExample(ScriptStrategyBase):
                 self.init_vwap_stats()
             elif self.vwap.get("status") == "ACTIVE":
                 vwap_order: OrderCandidate = self.create_order()
-                vwap_order_adjusted = self.vwap["connector"].budget_checker.adjust_candidate(vwap_order, all_or_none=False)
+                vwap_order_adjusted = self.vwap["connector"].budget_checker.adjust_candidate(vwap_order,
+                                                                                             all_or_none=False)
                 if vwap_order_adjusted.amount > Decimal("0"):
                     self.place_order(
                         connector_name=self.vwap["connector_name"],
@@ -53,7 +55,9 @@ class AVWAPExample(ScriptStrategyBase):
         vwap = self.vwap.copy()
         vwap["connector"] = self.connectors[vwap["connector_name"]]
         vwap["delta"] = 0
+        vwap["trades"] = []
         vwap["status"] = "ACTIVE"
+        vwap["trade_type"] = TradeType.BUY if self.vwap["is_buy"] else TradeType.SELL
         base_asset, quote_asset = split_hb_trading_pair(vwap["trading_pair"])
 
         # USD conversion to quote and base asset
@@ -61,15 +65,17 @@ class AVWAPExample(ScriptStrategyBase):
         conversion_quote_asset = f"{quote_asset}-USD"
         base_conversion_rate = RateOracle.get_instance().rate(conversion_base_asset)
         quote_conversion_rate = RateOracle.get_instance().rate(conversion_quote_asset)
-        vwap["target_base_volume"] = vwap["total_volume_usd"] / base_conversion_rate
-        vwap["volume_remaining"] = vwap["target_base_volume"]
-        vwap["ideal_quote_volume"] = vwap["total_volume_usd"] / quote_conversion_rate
         vwap["start_price"] = vwap["connector"].get_price(vwap["trading_pair"], vwap["is_buy"])
+        vwap["target_base_volume"] = vwap["total_volume_usd"] / base_conversion_rate
+        vwap["ideal_quote_volume"] = vwap["total_volume_usd"] / quote_conversion_rate
 
         # Compute market order scenario
-        orderbook_query = vwap["connector"].get_quote_volume_for_base_amount(vwap["trading_pair"], vwap["is_buy"], vwap["target_base_volume"])
+        orderbook_query = vwap["connector"].get_quote_volume_for_base_amount(vwap["trading_pair"], vwap["is_buy"],
+                                                                             vwap["target_base_volume"])
         vwap["market_order_base_volume"] = orderbook_query.query_volume
         vwap["market_order_quote_volume"] = orderbook_query.result_volume
+        vwap["volume_remaining"] = vwap["target_base_volume"]
+        vwap["real_quote_volume"] = Decimal(0)
         self.vwap = vwap
 
     def create_order(self) -> OrderCandidate:
@@ -89,14 +95,21 @@ class AVWAPExample(ScriptStrategyBase):
             price=price_affected_by_spread)
         volume_for_price = orderbook_query.result_volume
 
+        # Check if the volume available is higher than the remaining
+        amount = min(volume_for_price * Decimal(self.vwap["volume_perc"]), Decimal(self.vwap["volume_remaining"]))
+
+        # Quantize the order amount and price
+        amount = self.vwap["connector"].quantize_order_amount(self.vwap["trading_pair"], amount)
+        price = self.vwap["connector"].quantize_order_price(self.vwap["trading_pair"],
+                                                            Decimal(price_affected_by_spread))
         # Create the Order Candidate
         vwap_order = OrderCandidate(
             trading_pair=self.vwap["trading_pair"],
             is_maker=False,
             order_type=OrderType.MARKET,
-            order_side=TradeType.BUY if self.vwap["is_buy"] else TradeType.SELL,
-            amount=volume_for_price * Decimal(self.vwap["volume_perc"]),
-            price=Decimal(price_affected_by_spread))
+            order_side=self.vwap["trade_type"],
+            amount=amount,
+            price=price)
         return vwap_order
 
     def place_order(self,
@@ -117,7 +130,49 @@ class AVWAPExample(ScriptStrategyBase):
         Listens to fill order event to log it and notify the hummingbot application.
         If you set up Telegram bot, you will get notification there as well.
         """
-        msg = (f"({event.trading_pair}) {event.trade_type.name} order (price: {event.price}) of {event.amount} "
+        if event.trading_pair == self.vwap["trading_pair"] and event.trade_type == self.vwap["trade_type"]:
+            self.vwap["volume_remaining"] -= event.amount
+            self.vwap["delta"] = (self.vwap["target_base_volume"] - self.vwap["volume_remaining"]) / self.vwap[
+                "target_base_volume"]
+            self.vwap["real_quote_volume"] += event.price * event.amount
+            self.vwap["trades"].append(event)
+            if math.isclose(self.vwap["delta"], 1, rel_tol=1e-5):
+                self.vwap["status"] = "COMPLETE"
+        msg = (f"({event.trading_pair}) {event.trade_type.name} order (price: {round(event.price, 2)}) of "
+               f"{round(event.amount, 2)} "
                f"{split_hb_trading_pair(event.trading_pair)[0]} is filled.")
+
         self.log_with_clock(logging.INFO, msg)
         self.notify_hb_app_with_timestamp(msg)
+
+    def format_status(self) -> str:
+        """
+                Returns status of the current strategy on user balances and current active orders. This function is called
+                when status command is issued. Override this function to create custom status display output.
+                """
+        if not self.ready_to_trade:
+            return "Market connectors are not ready."
+        lines = []
+        warning_lines = []
+        warning_lines.extend(self.network_warning(self.get_market_trading_pair_tuples()))
+
+        balance_df = self.get_balance_df()
+        lines.extend(["", "  Balances:"] + ["    " + line for line in balance_df.to_string(index=False).split("\n")])
+
+        try:
+            df = self.active_orders_df()
+            lines.extend(["", "  Orders:"] + ["    " + line for line in df.to_string(index=False).split("\n")])
+        except ValueError:
+            lines.extend(["", "  No active maker orders."])
+        lines.extend(["", "VWAP Info:"] + ["   " + key + ": " + value
+                                           for key, value in self.vwap.items()
+                                           if type(value) == str])
+
+        lines.extend(["", "VWAP Stats:"] + ["   " + key + ": " + str(round(value, 4))
+                                            for key, value in self.vwap.items()
+                                            if type(value) in [int, float, Decimal]])
+
+        warning_lines.extend(self.balance_warning(self.get_market_trading_pair_tuples()))
+        if len(warning_lines) > 0:
+            lines.extend(["", "*** WARNINGS ***"] + warning_lines)
+        return "\n".join(lines)
