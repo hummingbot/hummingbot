482bc2fb13075d335d7e2f614dec8431b917b2a0
abel <abel@hummingbot.io>
2022-06-07 00:37:04 -0300

    (fix) Update of Bitmart exchange class to the latest standards

--- hummingbot/connector/exchange/binance/binance_exchange.py
+++ hummingbot/connector/exchange/binance/binance_exchange.py
@@ -106,6 +106,10 @@ class BinanceExchange(ExchangePyBase):
     def is_cancel_request_in_exchange_synchronous(self) -> bool:
         return True
 
+    @property
+    def is_trading_required(self) -> bool:
+        return self._trading_required
+
     def supported_order_types(self):
         return [OrderType.LIMIT, OrderType.LIMIT_MAKER]
 
