020bc7c165450d9d2b08cca74d993a480475ea69
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2022-06-07 11:21:07 -0300

    Adding serum swagger definitions.

--- gateway/src/app.ts
+++ gateway/src/app.ts
@@ -127,6 +127,7 @@ export const swaggerDocument = SwaggerManager.generateSwaggerJson(
     './docs/swagger/evm-routes.yml',
     './docs/swagger/network-routes.yml',
     './docs/swagger/solana-routes.yml',
+    './docs/swagger/serum-routes.yml',
   ]
 );
 
