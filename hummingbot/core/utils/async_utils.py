ad32f2dea6ba0d67f924190a73ba186ffa5febe5
memento <claude.rc@gmail.com>
2022-06-16 11:16:42 -0500

    (fix) Add Thread-Safe wrapping of asyncio.Event class
    When setting the AsyncIO debug env. variable PYTHONASYNCIODEBUG=1, hummingbot fails to start, reporting a call to Event.set() from a different thread in security.py

--- hummingbot/core/utils/async_utils.py
+++ hummingbot/core/utils/async_utils.py
@@ -1,7 +1,20 @@
 import asyncio
+import inspect
 import logging
 import time
-import inspect
+
+
+class EventThreadSafe(asyncio.Event):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self._loop is None:
+            self._loop = asyncio.get_event_loop()
+
+    def set(self):
+        self._loop.call_soon_threadsafe(super().set)
+
+    def clear(self):
+        self._loop.call_soon_threadsafe(super().clear)
 
 
 async def safe_wrapper(c):
