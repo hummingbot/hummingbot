58b76dbf28f4fc9aae4f31dba370c5d8c163faa7
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2022-06-29 18:11:16 -0300

    Solving todo issue.

--- gateway/src/@types/buffer-layout.d.ts
+++ gateway/src/@types/buffer-layout.d.ts
@@ -6,10 +6,3 @@ declare module 'buffer-layout' {
   export class UInt {}
   export class Union {}
 }
-
-// TODO similar to this problem, yarn build fails with a conflicting declaration of web3.js in solana and serum!!!
-
-// TODO this not is not related to this file, but more to do not forget!!!
-// It is needed to create a documention in the hummingbot-site github repository:
-//   https://hummingbot.org/developers/gateway/building-gateway-connectors/#10-create-connector-documentation-page
-//   https://hummingbot.org/developers/gateway/building-gateway-connectors/#11-add-documentation-page-to-index
