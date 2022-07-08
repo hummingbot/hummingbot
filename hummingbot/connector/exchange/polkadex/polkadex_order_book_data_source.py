bc62768dae9e52017012b7381e31428c059e425f
gautham <gauthamastro@gmail.com>
2022-07-08 09:02:47 +0530

    able to get orderbook

--- hummingbot/connector/exchange/polkadex/polkadex_order_book_data_source.py
+++ hummingbot/connector/exchange/polkadex/polkadex_order_book_data_source.py
@@ -49,7 +49,7 @@ class PolkadexOrderbookDataSource(OrderBookTrackerDataSource):
 
     async def _order_book_snapshot(self, trading_pair: str) -> OrderBookMessage:
         print("Getting orderbook snapshot for: ", trading_pair)
-        result: List[Dict[str, Any]] = await get_orderbook(trading_pair, None, None, self._connector.wss_url,
+        result: List[Dict[str, Any]] = await get_orderbook(trading_pair, None, None, self._connector.endpoint,
                                                            self._api_key)
         snapshot_timestamp: float = time.time()
         snapshot_msg: OrderBookMessage = PolkadexOrderbook.snapshot_message_from_exchange(
