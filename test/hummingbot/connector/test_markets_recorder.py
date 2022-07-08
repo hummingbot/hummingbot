e344c577b83b261446bb18d40bf63ac4e79f0295
memento <claude.rc@gmail.com>
2022-07-08 18:14:13 -0500

    (feat) Adding tests for the get/save market states methods

--- test/hummingbot/connector/test_markets_recorder.py
+++ test/hummingbot/connector/test_markets_recorder.py
@@ -7,8 +7,10 @@ from sqlalchemy import create_engine
 
 from hummingbot.client.config.client_config_map import ClientConfigMap
 from hummingbot.client.config.config_helpers import ClientConfigAdapter
+from hummingbot.connector.exchange_base import ExchangeBase
 from hummingbot.connector.markets_recorder import MarketsRecorder
 from hummingbot.core.data_type.common import OrderType, PositionAction, TradeType
+from hummingbot.core.data_type.in_flight_order import InFlightOrder
 from hummingbot.core.data_type.trade_fee import AddedToCostTradeFee
 from hummingbot.core.event.events import (
     BuyOrderCompletedEvent,
@@ -17,9 +19,14 @@ from hummingbot.core.event.events import (
     OrderFilledEvent,
     SellOrderCreatedEvent,
 )
+from hummingbot.model.market_state import MarketState
 from hummingbot.model.order import Order
 from hummingbot.model.sql_connection_manager import SQLConnectionManager, SQLConnectionType
 from hummingbot.model.trade_fill import TradeFill
+
+
+class MockExchange(ExchangeBase):
+    tracking_states = dict()
 
 
 class MarketsRecorderTests(TestCase):
@@ -36,12 +43,14 @@ class MarketsRecorderTests(TestCase):
         self.quote = "HBOT"
         self.trading_pair = f"{self.base}-{self.quote}"
 
+        self.market = MockExchange(client_config_map=ClientConfigAdapter(ClientConfigMap()))
+        self.market._set_current_timestamp(1640000000.0)
+        self.market.tracking_states = {}
+
         engine_mock.return_value = create_engine("sqlite:///:memory:")
         self.manager = SQLConnectionManager(
             ClientConfigAdapter(ClientConfigMap()), SQLConnectionType.TRADE_FILLS, db_name="test_DB"
         )
-
-        self.tracking_states = dict()
 
     def add_trade_fills_from_market_recorder(self, current_trade_fills):
         pass
@@ -115,7 +124,7 @@ class MarketsRecorderTests(TestCase):
             exchange_order_id="EOID1",
         )
 
-        recorder._did_create_order(MarketEvent.BuyOrderCreated.value, self, event)
+        recorder._did_create_order(MarketEvent.BuyOrderCreated.value, self.market, event)
 
         with self.manager.get_new_session() as session:
             query = session.query(Order)
@@ -151,7 +160,7 @@ class MarketsRecorderTests(TestCase):
             exchange_order_id="EOID1",
         )
 
-        recorder._did_create_order(MarketEvent.SellOrderCreated.value, self, event)
+        recorder._did_create_order(MarketEvent.SellOrderCreated.value, self.market, event)
 
         with self.manager.get_new_session() as session:
             query = session.query(Order)
@@ -187,7 +196,7 @@ class MarketsRecorderTests(TestCase):
             exchange_order_id="EOID1",
         )
 
-        recorder._did_create_order(MarketEvent.BuyOrderCreated.value, self, create_event)
+        recorder._did_create_order(MarketEvent.BuyOrderCreated.value, self.market, create_event)
 
         fill_event = OrderFilledEvent(
             timestamp=1642020000,
@@ -201,7 +210,7 @@ class MarketsRecorderTests(TestCase):
             exchange_trade_id="TradeId1"
         )
 
-        recorder._did_fill_order(MarketEvent.OrderFilled.value, self, fill_event)
+        recorder._did_fill_order(MarketEvent.OrderFilled.value, self.market, fill_event)
 
         with self.manager.get_new_session() as session:
             query = session.query(Order)
@@ -239,7 +248,7 @@ class MarketsRecorderTests(TestCase):
             exchange_order_id="EOID1",
         )
 
-        recorder._did_create_order(MarketEvent.BuyOrderCreated.value, self, create_event)
+        recorder._did_create_order(MarketEvent.BuyOrderCreated.value, self.market, create_event)
 
         complete_event = BuyOrderCompletedEvent(
             timestamp=1642020000,
@@ -250,7 +259,7 @@ class MarketsRecorderTests(TestCase):
             quote_asset_amount=create_event.amount * create_event.price,
             order_type=create_event.type)
 
-        recorder._did_complete_order(MarketEvent.BuyOrderCompleted.value, self, complete_event)
+        recorder._did_complete_order(MarketEvent.BuyOrderCompleted.value, self.market, complete_event)
 
         with self.manager.get_new_session() as session:
             query = session.query(Order)
@@ -266,3 +275,99 @@ class MarketsRecorderTests(TestCase):
         self.assertEqual(MarketEvent.BuyOrderCreated.name, order_status[0].status)
         self.assertEqual(MarketEvent.BuyOrderCompleted.name, order_status[1].status)
         self.assertEqual(0, len(trade_fills))
+
+    def test_save_market_states_w_market_states(self):
+        recorder = MarketsRecorder(
+            sql=self.manager,
+            markets=[self.market],
+            config_file_path=self.config_file_path,
+            strategy_name=self.strategy_name
+        )
+
+        order = InFlightOrder(
+            client_order_id="OID1",
+            exchange_order_id="EOID1",
+            trading_pair=self.trading_pair,
+            order_type=OrderType.LIMIT,
+            trade_type=TradeType.BUY,
+            amount=Decimal("1000.0"),
+            creation_timestamp=1640001112.223,
+            price=Decimal("1.0"),
+        )
+
+        self.market.tracking_states = {order.client_order_id: order.to_json()}
+
+        with patch.object(MarketsRecorder, 'get_market_states') as get_market_states:
+            get_market_states.return_value = None
+            with self.manager.get_new_session() as session:
+                recorder.save_market_states(self.config_file_path, self.market, session)
+                query = session.query(MarketState)
+                states = query.all()
+
+        self.assertEqual(self.config_file_path, states[0].config_file_path)
+        self.assertEqual(True, 'OID1' in states[0].saved_state.keys())
+        self.assertEqual(order.creation_timestamp, states[0].saved_state['OID1']['creation_timestamp'])
+
+    def test_save_market_states_wo_market_states(self):
+        recorder = MarketsRecorder(
+            sql=self.manager,
+            markets=[self.market],
+            config_file_path=self.config_file_path,
+            strategy_name=self.strategy_name
+        )
+
+        order = InFlightOrder(
+            client_order_id="OID1",
+            exchange_order_id="EOID1",
+            trading_pair=self.trading_pair,
+            order_type=OrderType.LIMIT,
+            trade_type=TradeType.BUY,
+            amount=Decimal("1000.0"),
+            creation_timestamp=1640001112.223,
+            price=Decimal("1.0"),
+        )
+
+        self.market.tracking_states = {order.client_order_id: order.to_json()}
+
+        market_states = MarketState(id='1', config_file_path='test_config', market='MockExchange',
+                                    timestamp=1657227811659, saved_state={})
+
+        with patch.object(MarketsRecorder, 'get_market_states') as get_market_states:
+            get_market_states.return_value = market_states
+            with self.manager.get_new_session() as session:
+                recorder.save_market_states(self.config_file_path, self.market, session)
+
+        get_market_states.assert_called_with(self.config_file_path, self.market, session=session)
+        self.assertNotEqual(1657227811659, market_states.timestamp)
+        self.assertEqual(True, 'OID1' in market_states.saved_state.keys())
+        self.assertEqual(order.creation_timestamp, market_states.saved_state['OID1']['creation_timestamp'])
+
+    def test_get_market_states(self):
+        recorder = MarketsRecorder(
+            sql=self.manager,
+            markets=[self.market],
+            config_file_path=self.config_file_path,
+            strategy_name=self.strategy_name
+        )
+
+        with self.manager.get_new_session() as session:
+            with session.begin():
+                ms = MarketState(id='1',
+                                 config_file_path='test_config',
+                                 market='MockExchange',
+                                 timestamp=1657227811659,
+                                 saved_state={
+                                     'OID1': {'client_order_id': 'OID1', 'exchange_order_id': 'EOID1',
+                                              'trading_pair': 'COINALPHA-HBOT', 'order_type': 'LIMIT',
+                                              'trade_type': 'BUY',
+                                              'price': '1.0', 'amount': '1000.0', 'executed_amount_base': '0',
+                                              'executed_amount_quote': '0', 'last_state': '0', 'leverage': '1',
+                                              'position': 'NIL',
+                                              'creation_timestamp': 1640001112.223, 'order_fills': {}}})
+                session.add(ms)
+
+            market_state = recorder.get_market_states(self.config_file_path, self.market, session)
+
+        self.assertEqual(self.config_file_path, market_state.config_file_path)
+        self.assertEqual(True, 'OID1' in market_state.saved_state.keys())
+        self.assertEqual(1640001112.223, market_state.saved_state['OID1']['creation_timestamp'])
