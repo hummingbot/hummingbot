a44ec2df8827360eb60f56aba7f1f486b57e5bfe
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2022-07-14 00:19:23 -0300

    Replacing test file with the one from the development branch and fixing the test.

--- gateway/test/services/config-manager-v2.test.ts
+++ gateway/test/services/config-manager-v2.test.ts
@@ -220,9 +220,7 @@ describe('Configuration manager v2 tests', () => {
 
 describe('Sample configurations', () => {
   it('Read sample schemas', (done) => {
-    const sampleConfigManager = new ConfigManagerV2(
-      '../src/templates/root.yml'
-    );
+    const sampleConfigManager = new ConfigManagerV2('./src/templates/root.yml');
     expect(sampleConfigManager.get('ssl.caCertificatePath')).toBeDefined();
     done();
   });
