a579d77d05757c66fbd73e45a39822e314618b19
abel <abel@hummingbot.io>
2022-07-15 14:50:48 -0300

    (fix) Added unit tests for OKX and Bitmart for the time synchronizer fix

--- test/hummingbot/connector/exchange/bitmart/test_bitmar_exchange.py
+++ test/hummingbot/connector/exchange/bitmart/test_bitmar_exchange.py
@@ -594,6 +594,27 @@ class BitmartExchangeTests(AbstractExchangeConnectorTests.ExchangeConnectorTests
     def trade_event_for_full_fill_websocket_update(self, order: InFlightOrder):
         pass
 
+    def test_time_synchronizer_related_reqeust_error_detection(self):
+        exception = IOError("Error executing request POST https://api.binance.com/api/v3/order. HTTP status is 400. "
+                            'Error: {"code":30007,"msg":"Header X-BM-TIMESTAMP range. Within a minute"}')
+        self.assertTrue(self.exchange._is_request_exception_related_to_time_synchronizer(exception))
+
+        exception = IOError("Error executing request POST https://api.binance.com/api/v3/order. HTTP status is 400. "
+                            'Error: {"code":30008,"msg":"Header X-BM-TIMESTAMP invalid format"}')
+        self.assertTrue(self.exchange._is_request_exception_related_to_time_synchronizer(exception))
+
+        exception = IOError("Error executing request POST https://api.binance.com/api/v3/order. HTTP status is 400. "
+                            'Error: {"code":30000,"msg":"Header X-BM-TIMESTAMP range. Within a minute"}')
+        self.assertFalse(self.exchange._is_request_exception_related_to_time_synchronizer(exception))
+
+        exception = IOError("Error executing request POST https://api.binance.com/api/v3/order. HTTP status is 400. "
+                            'Error: {"code":30007,"msg":"Other message"}')
+        self.assertFalse(self.exchange._is_request_exception_related_to_time_synchronizer(exception))
+
+        exception = IOError("Error executing request POST https://api.binance.com/api/v3/order. HTTP status is 400. "
+                            'Error: {"code":30008,"msg":"Other message"}')
+        self.assertFalse(self.exchange._is_request_exception_related_to_time_synchronizer(exception))
+
     def _order_cancelation_request_successful_mock_response(self, order: InFlightOrder) -> Any:
         return {
             "code": 1000,
