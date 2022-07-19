f9a7347d802d3a05c7f63b694582bd23a26f80f7
Navneeth Jayendran <navneeth.jayendran@affirm.com>
2022-07-18 17:18:21 -0700

    Fix things post merge

--- gateway/test/chains/harmony/defikingdoms/defikingdoms.routes.test.ts
+++ gateway/test/chains/harmony/defikingdoms/defikingdoms.routes.test.ts
@@ -647,7 +647,7 @@ describe('POST /amm/estimateGas', () => {
         expect(res.body.network).toEqual('mainnet');
         expect(res.body.gasPrice).toEqual(100);
         expect(res.body.gasCost).toEqual(
-          gasCostInEthString(100, harmony.gasLimit)
+          gasCostInEthString(100, defikingdoms.gasLimitEstimate)
         );
       });
   });
