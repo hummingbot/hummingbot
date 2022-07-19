f764d3715272e5118987c2d65317ff0f6d312da4
baron737 <sambaran@hummingbot.io>
2022-07-19 14:26:04 +0530

    Added util functions and exchange constants

--- hummingbot/connector/exchange/bittrex/bittrex_constants.py
+++ hummingbot/connector/exchange/bittrex/bittrex_constants.py
@@ -1,5 +1,4 @@
 # A SINGLE POINT OF TRUTH FOR BITTREX
-
 from hummingbot.core.api_throttler.data_types import RateLimit
 
 EXCHANGE_NAME = "Bittrex"
@@ -21,15 +20,21 @@ SERVER_TIME_URL = "/ping"
 
 # SERVER UTILITY URLS
 ORDERBOOK_SNAPSHOT_URL = "/markets/{}/orderbook"
-
-BITTREX_EXCHANGE_INFO_PATH = "/markets"
+EXCHANGE_INFO_PATH_URL = "/markets"
+ORDER_CREATION_URL = "/orders"
+ORDER_DELETION_URL = "/orders/{}"
 BITTREX_MARKET_SUMMARY_PATH = "/markets/summaries"
 BITTREX_TICKER_PATH = "/markets/tickers"
 
-
+# RATE-LIMIT ID
 ORDERBOOK_SNAPSHOT_LIMIT_ID = "orderBook"
+ORDER_DELETE_LIMIT_ID = "orderDel"
+
 RATE_LIMITS = [
     RateLimit(limit_id=ORDERBOOK_SNAPSHOT_LIMIT_ID, limit=60, time_interval=60),
     RateLimit(limit_id=SERVER_TIME_URL, limit=60, time_interval=60),
+    RateLimit(limit_id=EXCHANGE_INFO_PATH_URL, limit=60, time_interval=60),
+    RateLimit(limit_id=ORDER_CREATION_URL, limit=60, time_interval=60),
+    RateLimit(limit_id=ORDER_DELETE_LIMIT_ID, limit=60, time_interval=60),
 
 ]
