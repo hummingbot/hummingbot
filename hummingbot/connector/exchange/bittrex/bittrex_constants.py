c73aead5821c25522cfa9a54f151bb30b281e404
baron737 <sambaran@hummingbot.io>
2022-07-21 17:56:32 +0530

    Removed unnecessary files

--- hummingbot/connector/exchange/bittrex/bittrex_constants.py
+++ hummingbot/connector/exchange/bittrex/bittrex_constants.py
@@ -1,5 +1,6 @@
 # A SINGLE POINT OF TRUTH FOR BITTREX
 from hummingbot.core.api_throttler.data_types import RateLimit
+from hummingbot.core.data_type.in_flight_order import OrderState
 
 EXCHANGE_NAME = "Bittrex"
 PING_TIMEOUT = 10.0
@@ -15,16 +16,24 @@ BITTREX_WS_URL = "https://socket-v3.bittrex.com/signalr"
 TRADE_EVENT_KEY = "trade"
 DIFF_EVENT_KEY = "orderbook_25"
 
+# Order States
+ORDER_STATE = {
+    "OPEN": OrderState.OPEN,
+    "CLOSED": OrderState.CANCELED,
+}
+
 # SERVER REFERENCE DATA URLS
 SERVER_TIME_URL = "/ping"
 
 # SERVER UTILITY URLS
+BALANCES_URL = "/balances"
 ORDERBOOK_SNAPSHOT_URL = "/markets/{}/orderbook"
 EXCHANGE_INFO_PATH_URL = "/markets"
 ORDER_CREATION_URL = "/orders"
 ORDER_DELETION_URL = "/orders/{}"
-BITTREX_MARKET_SUMMARY_PATH = "/markets/summaries"
-BITTREX_TICKER_PATH = "/markets/tickers"
+ALL_OPEN_ORDERS_URL = "/orders/open"
+SYMBOL_TICKER_PATH = "/markets/{}/ticker"
+FEES_URL = "/account/fees/trading"
 
 # RATE-LIMIT ID
 ORDERBOOK_SNAPSHOT_LIMIT_ID = "orderBook"
@@ -36,5 +45,8 @@ RATE_LIMITS = [
     RateLimit(limit_id=EXCHANGE_INFO_PATH_URL, limit=60, time_interval=60),
     RateLimit(limit_id=ORDER_CREATION_URL, limit=60, time_interval=60),
     RateLimit(limit_id=ORDER_DELETE_LIMIT_ID, limit=60, time_interval=60),
+    RateLimit(limit_id=ALL_OPEN_ORDERS_URL, limit=60, time_interval=60),
+    RateLimit(limit_id=SYMBOL_TICKER_PATH, limit=60, time_interval=60),
+    RateLimit(limit_id=FEES_URL, limit=60, time_interval=60),
 
 ]
