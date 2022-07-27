b16424486d6673dd40232ec230b3e7e73365f524
abduramann <abduraman@m20040.contaboserver.net>
2022-07-27 06:41:09 -0400

    binance rate limits fixed

--- hummingbot/connector/exchange/binance/binance_constants.py
+++ hummingbot/connector/exchange/binance/binance_constants.py
@@ -70,9 +70,9 @@ RATE_LIMITS = [
     # Pools
     RateLimit(limit_id=REQUEST_WEIGHT, limit=1200, time_interval=ONE_MINUTE),
     RateLimit(limit_id=ORDERS, limit=50, time_interval=10 * ONE_SECOND),
-    RateLimit(limit_id=ORDERS_24HR, limit=100000, time_interval=ONE_DAY),
-    RateLimit(limit_id=RAW_REQUESTS, limit=6000, time_interval= 5 * ONE_MINUTE),    
-    
+    RateLimit(limit_id=ORDERS_24HR, limit=160000, time_interval=ONE_DAY),
+    RateLimit(limit_id=RAW_REQUESTS, limit=6100, time_interval= 5 * ONE_MINUTE),
+
     # Weighted Limits
     RateLimit(limit_id=TICKER_PRICE_CHANGE_PATH_URL, limit=MAX_REQUEST, time_interval=ONE_MINUTE,
               linked_limits=[LinkedLimitWeightPair(REQUEST_WEIGHT, 40),
@@ -98,8 +98,10 @@ RATE_LIMITS = [
     RateLimit(limit_id=MY_TRADES_PATH_URL, limit=MAX_REQUEST, time_interval=ONE_MINUTE,
               linked_limits=[LinkedLimitWeightPair(REQUEST_WEIGHT, 10),
                              LinkedLimitWeightPair(RAW_REQUESTS, 1)]),
+
+    # checking order weight: 2, placing order weight: 1
     RateLimit(limit_id=ORDER_PATH_URL, limit=MAX_REQUEST, time_interval=ONE_MINUTE,
-              linked_limits=[LinkedLimitWeightPair(REQUEST_WEIGHT, 2), #checking: 2, placing: 1
+              linked_limits=[LinkedLimitWeightPair(REQUEST_WEIGHT, 2),
                              LinkedLimitWeightPair(ORDERS, 1),
                              LinkedLimitWeightPair(ORDERS_24HR, 1),
                              LinkedLimitWeightPair(RAW_REQUESTS, 1)])
