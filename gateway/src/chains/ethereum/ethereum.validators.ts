6409acc089023bb0be2693bd427149505683e027
Navneeth Jayendran <navneejay@gmail.com>
2022-08-02 10:03:17 -0500

    Fix actual `getSpender` validation

--- gateway/src/chains/ethereum/ethereum.validators.ts
+++ gateway/src/chains/ethereum/ethereum.validators.ts
@@ -57,6 +57,7 @@ export const validateSpender: Validator = mkValidator(
       val === 'viperswap' ||
       val === 'openocean' ||
       val === 'quickswap' ||
+      val === 'defira' ||
       isAddress(val))
 );
 
