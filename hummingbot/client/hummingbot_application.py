21ae1185b21997e26f96166854dd67817e4b78af
Konstantinos Panayiotou <klpanagi@gmail.com>
2022-08-10 23:29:59 +0300

    Load bot uuid from env variable

--- hummingbot/client/hummingbot_application.py
+++ hummingbot/client/hummingbot_application.py
@@ -4,6 +4,7 @@ import asyncio
 import logging
 import os
 import time
+import uuid
 from collections import deque
 from typing import Deque, Dict, List, Optional, Tuple, Union
 
@@ -50,6 +51,12 @@ from hummingbot.strategy.strategy_base import StrategyBase
 
 s_logger = None
 
+UID = os.getenv('HBOT_UID')
+
+if UID is None:
+    UID = uuid.uuid4()
+    os.environ['HBOT_UID'] = str(UID)
+
 
 class HummingbotApplication(*commands):
     KILL_TIMEOUT = 10.0
@@ -63,6 +70,7 @@ class HummingbotApplication(*commands):
         global s_logger
         if s_logger is None:
             s_logger = logging.getLogger(__name__)
+            s_logger = logging.getLogger(str(cls.uid()))
         return s_logger
 
     @classmethod
@@ -70,6 +78,11 @@ class HummingbotApplication(*commands):
         if cls._main_app is None:
             cls._main_app = HummingbotApplication(client_config_map)
         return cls._main_app
+
+    @classmethod
+    def uid(cls) -> str:
+        global UID
+        return str(UID)
 
     def __init__(self, client_config_map: Optional[ClientConfigAdapter] = None):
         self.client_config_map: Union[ClientConfigMap, ClientConfigAdapter] = (  # type-hint enables IDE auto-complete
