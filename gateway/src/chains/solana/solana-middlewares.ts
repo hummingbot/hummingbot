4dbd77fa6a96eb54c4f74fd4914a39671ff5099e
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2022-08-19 18:32:01 -0300

    Added validation to solana-middleware.ts file.

--- gateway/src/chains/solana/solana-middlewares.ts
+++ gateway/src/chains/solana/solana-middlewares.ts
@@ -1,3 +1,4 @@
+import { HttpException } from '../../services/error-handler';
 import { Solana } from './solana';
 import { NextFunction, Request, Response } from 'express';
 
@@ -6,6 +7,10 @@ export const verifySolanaIsAvailable = async (
   _res: Response,
   next: NextFunction
 ) => {
+  if (!req || !req.body || !req.body.network) {
+    throw new HttpException(404, 'No Solana network informed.');
+  }
+
   const solana = await Solana.getInstance(req.body.network);
   if (!solana.ready) {
     await solana.init();
