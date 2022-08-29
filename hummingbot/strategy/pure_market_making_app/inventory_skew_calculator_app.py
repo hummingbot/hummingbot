b861ed0a7bd2d7d3b8a8194d427102dcedfba535
memento <claude.rc@gmail.com>
2022-08-29 16:52:13 -0500

    (feat) Augmented Pure Python

---
+++ hummingbot/strategy/pure_market_making_app/inventory_skew_calculator_app.py
@@ -0,0 +1,68 @@
+from decimal import Decimal
+
+import cython
+import numpy as np
+
+from ..data_types import InventorySkewBidAskRatios
+
+decimal_0 = Decimal(0)
+decimal_1 = Decimal(1)
+decimal_2 = Decimal(2)
+
+
+def calculate_total_order_size(order_start_size: Decimal, order_step_size: Decimal = decimal_0,
+                               order_levels: cython.int = 1) -> Decimal:
+    order_levels_decimal: cython.int = order_levels
+    return (decimal_2 *
+            (order_levels_decimal * order_start_size +
+             order_levels_decimal * (order_levels_decimal - decimal_1) / decimal_2 * order_step_size
+             )
+            )
+
+
+def calculate_bid_ask_ratios_from_base_asset_ratio(
+        base_asset_amount: float, quote_asset_amount: float, price: float,
+        target_base_asset_ratio: float, base_asset_range: float) -> InventorySkewBidAskRatios:
+    return _c_calculate_bid_ask_ratios_from_base_asset_ratio(base_asset_amount,
+                                                             quote_asset_amount,
+                                                             price,
+                                                             target_base_asset_ratio,
+                                                             base_asset_range)
+
+
+@cython.ccall
+@cython.inline
+def _c_calculate_bid_ask_ratios_from_base_asset_ratio(
+        base_asset_amount: cython.double,
+        quote_asset_amount: cython.double,
+        price: cython.double,
+        target_base_asset_ratio: cython.double,
+        base_asset_range: cython.double) -> object:
+    total_portfolio_value: cython.double = base_asset_amount * price + quote_asset_amount
+
+    if total_portfolio_value <= 0.0 or base_asset_range <= 0.0:
+        return InventorySkewBidAskRatios(0.0, 0.0)
+
+    base_asset_value: cython.double = base_asset_amount * price
+    base_asset_range_value: cython.double = min(base_asset_range * price, total_portfolio_value * 0.5)
+    target_base_asset_value: cython.double = total_portfolio_value * target_base_asset_ratio
+    left_base_asset_value_limit: cython.double = max(target_base_asset_value - base_asset_range_value, 0.0)
+    right_base_asset_value_limit: cython.double = target_base_asset_value + base_asset_range_value
+
+    left_inventory_ratio: cython.double = np.interp(base_asset_value,
+                                                    (left_base_asset_value_limit, target_base_asset_value),
+                                                    (0, 0.5))
+
+    right_inventory_ratio: cython.double = np.interp(base_asset_value,
+                                                     (target_base_asset_value, right_base_asset_value_limit),
+                                                     (0.5, 1.0))
+    bid_adjustment: cython.double = (np.interp(left_inventory_ratio,
+                                               (0, 0.5),
+                                               (2.0, 1.0))
+                                     if base_asset_value < target_base_asset_value
+                                     else np.interp(right_inventory_ratio,
+                                                    (0.5, 1),
+                                                    (1.0, 0.0)))
+    ask_adjustment: cython.double = 2.0 - bid_adjustment
+
+    return InventorySkewBidAskRatios(bid_adjustment, ask_adjustment)
---
+++ hummingbot/strategy/pure_market_making_app/pure_market_making_app.pxd
@@ -0,0 +1,85 @@
+# distutils: language=c++
+
+from libc.stdint cimport int64_t
+
+from hummingbot.strategy.strategy_py_base cimport StrategyPyBase
+
+cdef class PureMarketMakingStrategyAugmentedPurePython(StrategyPyBase):
+    cdef:
+        object _market_info
+
+        object _bid_spread
+        object _ask_spread
+        object _minimum_spread
+        object _order_amount
+        int _order_levels
+        int _buy_levels
+        int _sell_levels
+        object _split_order_levels_enabled
+        object _bid_order_level_spreads
+        object _ask_order_level_spreads
+        object _order_level_spread
+        object _order_level_amount
+        double _order_refresh_time
+        double _max_order_age
+        object _order_refresh_tolerance_pct
+        double _filled_order_delay
+        bint _inventory_skew_enabled
+        object _inventory_target_base_pct
+        object _inventory_range_multiplier
+        bint _hanging_orders_enabled
+        object _hanging_orders_tracker
+        bint _order_optimization_enabled
+        object _ask_order_optimization_depth
+        object _bid_order_optimization_depth
+        bint _add_transaction_costs_to_orders
+        object _asset_price_delegate
+        object _inventory_cost_price_delegate
+        object _price_type
+        bint _take_if_crossed
+        object _price_ceiling
+        object _price_floor
+        bint _ping_pong_enabled
+        list _ping_pong_warning_lines
+        bint _hb_app_notification
+        object _order_override
+
+        double _cancel_timestamp
+        double _create_timestamp
+        object _limit_order_type
+        bint _all_markets_ready
+        int _filled_buys_balance
+        int _filled_sells_balance
+        double _last_timestamp
+        double _status_report_interval
+        int64_t _logging_options
+        object _last_own_trade_price
+        bint _should_wait_order_cancel_confirmation
+
+        object _moving_price_band
+
+    cpdef tick(self, double timestamp)
+    cpdef start(self, object clock, double timestamp)
+    cpdef stop(self, object clock)
+    cpdef all_markets_ready(self)
+    cdef apply_inventory_skew(self, object proposal)
+    cdef apply_order_optimization(self, object proposal)
+    cdef apply_add_transaction_costs(self, object proposal)
+    cdef apply_order_levels_modifiers(self, object proposal)
+    cdef apply_order_size_modifiers(self, object proposal)
+    cdef apply_order_price_modifiers(self, object proposal)
+    cdef apply_budget_constraint(self, object proposal)
+    cdef apply_ping_pong(self, object proposal)
+    cdef apply_price_band(self, object proposal)
+    cdef apply_moving_price_band(self, object proposal)
+    cdef filter_out_takers(self, object proposal)
+    cdef tuple get_adjusted_available_balance(self, list orders)
+    cdef object create_base_proposal(self)
+    cdef object get_mid_price(self)
+    cdef execute_orders_proposal(self, object proposal)
+    cdef to_create_orders(self, object proposal)
+    cdef cancel_active_orders(self, object proposal)
+    cdef cancel_orders_below_min_spread(self)
+    cdef cancel_active_orders_on_max_age_limit(self)
+
+    #cpdef object create_base_proposal(self)
