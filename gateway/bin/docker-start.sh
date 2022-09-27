89ff32b428de4b864c3fbc620ec60e0fbbac636e
memento <claude.rc@gmail.com>
2022-09-27 17:09:09 -0500

    (fix) Gateway fails to start in development version of HB

--- gateway/bin/docker-start.sh
+++ gateway/bin/docker-start.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-mkdir -p /usr/src/app/gateway.level /usr/src/app/transactions.level \
-    /usr/src/app/db
-chown -R hummingbot:hummingbot /usr/src/app/logs /usr/src/app/conf \
-    /usr/src/app/gateway.level /usr/src/app/transactions.level \
-    /usr/src/app/db
+mkdir -p /usr/src/app/gateway.level \
+         /usr/src/app/transactions.level \
+         /usr/src/app/db \
+         /usr/src/app/certs  \
+         /usr/src/app/logs \
+         /usr/src/app/conf
+chown -R hummingbot:hummingbot \
+         /usr/src/app/gateway.level \
+         /usr/src/app/transactions.level \
+         /usr/src/app/db \
+         /usr/src/app/certs \
+         /usr/src/app/logs \
+         /usr/src/app/conf
 
 gosu hummingbot:hummingbot yarn run start
