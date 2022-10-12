397f9ba873a6259bbece9025bd9d092da466fd5a
cardosofede <federico.cardoso.e@gmail.com>
2022-10-12 19:36:00 -0300

    (feat) add auth module and test

--- hummingbot/connector/derivative/ftx_perpetual/ftx_perpetual_auth.py
+++ hummingbot/connector/derivative/ftx_perpetual/ftx_perpetual_auth.py
@@ -1,41 +1,37 @@
 import hashlib
 import hmac
-import json
 import time
-from decimal import Decimal
-from typing import Any, Dict, List
+from collections import OrderedDict
+from typing import Any, Dict, Optional
+from urllib.parse import quote, urlencode, urlsplit
 
 from hummingbot.core.web_assistant.auth import AuthBase
-from hummingbot.core.web_assistant.connections.data_types import RESTMethod, RESTRequest, WSRequest
+from hummingbot.core.web_assistant.connections.data_types import RESTRequest, WSRequest
 
 
 class FtxPerpetualAuth(AuthBase):
-    """
-    Auth class required by Ftx Perpetual API
-    """
-    def __init__(self, api_key: str, secret_key: str):
-        self._api_key: str = api_key
-        self._secret_key: str = secret_key
+
+    def __init__(self, api_key: str, secret_key: str, subaccount_name: Optional[str] = None):
+        self.api_key = api_key
+        self.secret_key = secret_key
+        self.subaccount = subaccount_name
 
     async def rest_authenticate(self, request: RESTRequest) -> RESTRequest:
-        if request.method == RESTMethod.GET:
-            request = await self._authenticate_get(request)
-        elif request.method == RESTMethod.POST:
-            request = await self._authenticate_post(request)
-        else:
-            raise NotImplementedError
-        return request
+        """
+        Adds the server time and the signature to the request, required for authenticated interactions. It also adds
+        the required parameter in the request header.
 
-    async def _authenticate_get(self, request: RESTRequest) -> RESTRequest:
-        params = request.params or {}
-        request.params = self._extend_params_with_authentication_info(params)
-        return request
+        :param request: the request to be configured for authenticated interaction
 
-    async def _authenticate_post(self, request: RESTRequest) -> RESTRequest:
-        data = json.loads(request.data) if request.data is not None else {}
-        data = self._extend_params_with_authentication_info(data)
-        data = {key: value for key, value in sorted(data.items())}
-        request.data = json.dumps(data)
+        :return: The RESTRequest with auth information included
+        """
+
+        headers = {}
+        if request.headers is not None:
+            headers.update(request.headers)
+        headers.update(self.authentication_headers(request=request))
+        request.headers = headers
+
         return request
 
     async def ws_authenticate(self, request: WSRequest) -> WSRequest:
@@ -45,37 +41,57 @@ class FtxPerpetualAuth(AuthBase):
         """
         return request  # pass-through
 
-    def get_ws_auth_payload(self) -> List[str]:
-        """
-        Generates a dictionary with all required information for the authentication process
-        :return: a dictionary of authentication info including the request signature
-        """
-        expires = self._get_expiration_timestamp()
-        raw_signature = "GET/realtime" + expires
+    @staticmethod
+    def keysort(dictionary: Dict[str, str]) -> Dict[str, str]:
+        return OrderedDict(sorted(dictionary.items(), key=lambda t: t[0]))
+
+    def authentication_headers(self, request: RESTRequest) -> Dict[str, Any]:
+        timestamp = int(self._time() * 1e3)
+
+        path_url = urlsplit(request.url).path
+        if request.params:
+            query_string_components = urlencode(request.params, safe="/")
+            path_url = f"{path_url}?{query_string_components}"
+
+        header = {
+            "FTX-KEY": self.api_key,
+            "FTX-TS": str(timestamp),
+            "FTX-SIGN": self._generate_signature(timestamp, request.method.value.upper(), path_url, request.data),
+        }
+
+        if self.subaccount:
+            header["FTX-SUBACCOUNT"] = quote(self.subaccount)
+
+        return header
+
+    def websocket_login_parameters(self) -> Dict[str, Any]:
+        timestamp = int(self._time() * 1e3)
+        signature = self._sign(payload=f"{timestamp}websocket_login")
+
+        payload = {
+            "key": self.api_key,
+            "sign": signature,
+            "time": timestamp,
+        }
+
+        if self.subaccount:
+            payload["subaccount"] = quote(self.subaccount)
+
+        return payload
+
+    def _sign(self, payload: str):
         signature = hmac.new(
-            self._secret_key.encode("utf-8"), raw_signature.encode("utf-8"), hashlib.sha256
-        ).hexdigest()
-        auth_info = [self._api_key, expires, signature]
+            self.secret_key.encode(),
+            payload.encode(),
+            hashlib.sha256).hexdigest()
+        return signature
 
-        return auth_info
+    def _generate_signature(self, timestamp: int, method: str, path_url: str, body: Optional[str] = None) -> str:
+        unsigned_signature = str(timestamp) + method + path_url
+        if body is not None:
+            unsigned_signature += body
 
-    def _extend_params_with_authentication_info(self, params: Dict[str, Any]) -> Dict[str, Any]:
-        params["timestamp"] = self._get_timestamp()
-        params["api_key"] = self._api_key
-        key_value_elements = []
-        for key, value in sorted(params.items()):
-            converted_value = float(value) if type(value) is Decimal else value
-            converted_value = converted_value if type(value) is str else json.dumps(converted_value)
-            key_value_elements.append(str(key) + "=" + converted_value)
-        raw_signature = "&".join(key_value_elements)
-        signature = hmac.new(self._secret_key.encode("utf-8"), raw_signature.encode("utf-8"), hashlib.sha256).hexdigest()
-        params["sign"] = signature
-        return params
+        return self._sign(payload=unsigned_signature)
 
-    @staticmethod
-    def _get_timestamp():
-        return str(int(time.time() * 1e3))
-
-    @staticmethod
-    def _get_expiration_timestamp():
-        return str(int((round(time.time()) + 5) * 1e3))
+    def _time(self):
+        return time.time()
