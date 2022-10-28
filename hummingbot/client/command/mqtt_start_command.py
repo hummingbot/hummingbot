62ac866e6d168f9dd8d42213cfc5a6710f4c1164
Konstantinos Panayiotou <klpanagi@gmail.com>
2022-10-28 16:31:13 +0300

    Command to start MQTT connections

---
+++ hummingbot/client/command/mqtt_start_command.py
@@ -0,0 +1,25 @@
+import threading
+from typing import TYPE_CHECKING
+
+from hummingbot.core.utils.async_utils import safe_ensure_future
+from hummingbot.remote_iface.mqtt import MQTTGateway
+
+if TYPE_CHECKING:
+    from hummingbot.client.hummingbot_application import HummingbotApplication
+
+
+class MQTTStartCommand:
+    def start_mqtt(self,  # type: HummingbotApplication
+                   ):
+        if threading.current_thread() != threading.main_thread():
+            self.ev_loop.call_soon_threadsafe(self.start_mqtt,)
+            return
+        safe_ensure_future(self.start_mqtt_async(), loop=self.ev_loop)
+
+    async def start_mqtt_async(self,  # type: HummingbotApplication
+                               ):
+        self.logger().info("MQTT.Start command initiated.")
+        self._mqtt = MQTTGateway(self)
+        self._mqtt.start_notifier()
+        self._mqtt.start_commands()
+        self._mqtt.run()
