64f636949674d22890395be7a621e46067dc109b
Petio Petrov <petioptrv@icloud.com>
2022-10-31 18:19:31 +0700

    (refactor) Revert ByBit perpetual changes

--- hummingbot/connector/derivative/bybit_perpetual/bybit_perpetual_derivative.py
+++ hummingbot/connector/derivative/bybit_perpetual/bybit_perpetual_derivative.py
@@ -27,7 +27,7 @@ from hummingbot.core.data_type.order_book_tracker_data_source import OrderBookTr
 from hummingbot.core.data_type.trade_fee import TokenAmount, TradeFeeBase
 from hummingbot.core.data_type.user_stream_tracker_data_source import UserStreamTrackerDataSource
 from hummingbot.core.utils.async_utils import safe_ensure_future, safe_gather
-from hummingbot.core.utils.estimate_fee import build_perpetual_trade_fee
+from hummingbot.core.utils.estimate_fee import build_trade_fee
 from hummingbot.core.web_assistant.connections.data_types import RESTMethod
 from hummingbot.core.web_assistant.web_assistants_factory import WebAssistantsFactory
 
@@ -249,7 +249,7 @@ class BybitPerpetualDerivative(PerpetualDerivativePyBase):
                  price: Decimal = s_decimal_NaN,
                  is_maker: Optional[bool] = None) -> TradeFeeBase:
         is_maker = is_maker or False
-        fee = build_perpetual_trade_fee(
+        fee = build_trade_fee(
             self.name,
             is_maker,
             base_currency=base_currency,
