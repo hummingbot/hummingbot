41eeb59bc33a4b65a50716a43457fb3bc20e4b67
rapcmia <ralph@hummingbot.org>
2022-11-25 13:36:42 +0800

    import formattedtext

--- hummingbot/connector/connector_certified.py
+++ hummingbot/connector/connector_certified.py
@@ -1,17 +1,20 @@
-certified_connector = {
-    'binance': 'green',
-    'binance_perpetual': 'yellow',
-    'ascendex': 'green',
-    'gate_io': 'green',
-    'kucoin': 'green',
-    'kucoin_testnet': 'green',
-    'hitbtc': 'green',
-    'okx': 'green',
-    'ftx': 'green',
-    'bybit': 'green',
-    'bybit_perpetual': 'green',
-    'huobi': 'green'
-}
+from prompt_toolkit import print_formatted_text as print
+from prompt_toolkit.formatted_text import FormattedText, to_formatted_text
+
+certified_connector = (
+    'binance',
+    'binance_perpetual',
+    'ascendex',
+    'gate_io',
+    'kucoin',
+    'kucoin_testnet',
+    'hitbtc',
+    'okx',
+    'ftx',
+    'bybit',
+    'bybit_perpetual',
+    'huobi'
+)
 
 
 def get_connector_certified(connector_name):
@@ -19,6 +22,9 @@ def get_connector_certified(connector_name):
     Filters connectors that are certified by adding background of GREEN color
     """
     if connector_name in certified_connector:
-        return f"{connector_name}"
+        text = FormattedText([
+            ('#000000', connector_name)
+        ])
+        return print(to_formatted_text(text, style='bg:#1CD085'))
     else:
         return connector_name
