05604a2e135ddf7d763f7a259b39549472ae42bf
TestDataGuy <37490771+TestDataGuy@users.noreply.github.com>
2022-12-27 13:42:36 -0800

    Fix Linter Issues

--- scripts/steamrolling.py
+++ scripts/steamrolling.py
@@ -2,34 +2,23 @@ from hummingbot.core.data_type.common import OrderType, TradeType
 from hummingbot.core.data_type.order_candidate import OrderCandidate
 from hummingbot.core.event.events import OrderFilledEvent
 from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
-
-from hummingbot.core.rate_oracle.rate_oracle import RateOracle
-
 from decimal import Decimal
-import pandas as pd
-
 
 
 class Steamroller(ScriptStrategyBase):
     """
-    This is a simple script that places bids or asks depending on the difference on exchanges based on liquidity.
-
+    This is a simple script that places bids or asks depending on
+    the difference on exchanges based on liquidity.
     """
-    liquid_exchange = "kucoin_paper_trade" 
+    liquid_exchange = "kucoin_paper_trade"
     less_liquid_exchange = "gate_io_paper_trade"
     reference_exchange = "binance_us_paper_trade"
-    security = "BTC-USDT"                   
-
-
-                                                        #securities = {"BTC-USDT","ETH-USDT",""}
-                                                        #markets = {liquid_exchange: securities}
-    max_order_age = 10                                  #What's the conversion rate for actual seconds? Any suggestions on 
-
-    order_amount_usd = Decimal("100")                            # amount for each order Is there 
-    spread_threshold_percent = Decimal("0.5")                    #difference in bid - bid or ask - ask (Make this a %?)
-
+    security = "BTC-USDT"
+    max_order_age = 10
+    order_amount_usd = Decimal("100")
+    spread_threshold_percent = Decimal("0.5")
     markets = {
-        liquid_exchange: {security},                    
+        liquid_exchange: {security},
         less_liquid_exchange: {security},
         reference_exchange: {security}
         }
@@ -40,123 +29,67 @@ class Steamroller(ScriptStrategyBase):
     short_position = False
 
     def on_tick(self):
-        best_bid_liquid = self.connectors[self.liquid_exchange].get_price(self.security, False)   #self.security replace for trading pair loop
+        best_bid_liquid = self.connectors[self.liquid_exchange].get_price(self.security, False)
         best_bid_less_liquid = self.connectors[self.less_liquid_exchange].get_price(self.security, False)
         best_ask_liquid = self.connectors[self.liquid_exchange].get_price(self.security, True)
         best_ask_less_liquid = self.connectors[self.less_liquid_exchange].get_price(self.security, True)
-
         increment = self.connectors[self.less_liquid_exchange].get_order_price_quantum(self.security, Decimal("1"))
-        conversion_rate = self.connectors[self.reference_exchange].get_price(self.security,False)
-        
-        maker_buy_price = best_bid_less_liquid + increment    #place a buy one increment above the best bid
-        maker_sell_price = best_ask_less_liquid - increment   #place a sell one increment below the best ask
-
+        conversion_rate = self.connectors[self.reference_exchange].get_price(self.security, False)
+        maker_buy_price = best_bid_less_liquid + increment    # Place a buy one increment above the best bid
+        maker_sell_price = best_ask_less_liquid - increment   # Place a sell one increment below the best ask
         sell_order_amount = self.order_amount_usd / conversion_rate
         buy_order_amount = self.order_amount_usd / conversion_rate
 
-        if not self.buy_order_placed and (((best_bid_liquid - best_bid_less_liquid) / best_bid_less_liquid ) * 100 ) >= self.spread_threshold_percent:
-            
-            self.logger().info(f"{buy_order_amount}")  
+        if not self.buy_order_placed and (((best_bid_liquid - best_bid_less_liquid) / best_bid_less_liquid) * 100) >= self.spread_threshold_percent:
+            self.logger().info(f"{buy_order_amount}")
             self.logger().info(f"{maker_buy_price}")
-
-            buy_order = OrderCandidate(trading_pair=self.security, 
-                                                    is_maker=True, 
-                                                    order_type=OrderType.LIMIT,
-                                                    order_side=TradeType.BUY,
-                                                    amount=buy_order_amount,
-                                                    price=maker_buy_price)
-
+            buy_order = OrderCandidate(trading_pair=self.security, is_maker=True, order_type=OrderType.LIMIT, order_side=TradeType.BUY, amount=buy_order_amount, price=maker_buy_price)
             buy_order_adjusted = self.connectors[self.less_liquid_exchange].budget_checker.adjust_candidate(buy_order, all_or_none=True)
-
-            self.buy(self.less_liquid_exchange, 
-                        self.security, 
-                        buy_order_adjusted.amount, 
-                        buy_order_adjusted.order_type, 
-                        buy_order_adjusted.price)
-
+            self.buy(self.less_liquid_exchange, self.security, buy_order_adjusted.amount, buy_order_adjusted.order_type, buy_order_adjusted.price)
             self.buy_order_placed = True
-            self.logger().info("Placed a buy")       
+            self.logger().info("Placed a buy")
 
-        if not self.sell_order_placed and (((best_ask_less_liquid - best_ask_liquid) / best_ask_liquid) *100) >= self.spread_threshold_percent:
-            
-            sell_order = OrderCandidate(trading_pair=self.security, 
-                                                    is_maker=True, 
-                                                    order_type=OrderType.LIMIT,
-                                                    order_side=TradeType.SELL,
-                                                    amount=sell_order_amount,
-                                                    price=maker_sell_price)
-
+        if not self.sell_order_placed and (((best_ask_less_liquid - best_ask_liquid) / best_ask_liquid) * 100) >= self.spread_threshold_percent:
+            sell_order = OrderCandidate(trading_pair=self.security, is_maker=True, order_type=OrderType.LIMIT, order_side=TradeType.SELL, amount=sell_order_amount, price=maker_sell_price)
             sell_order_adjusted = self.connectors[self.less_liquid_exchange].budget_checker.adjust_candidate(sell_order, all_or_none=True)
-
-            self.sell(self.less_liquid_exchange, 
-                                self.security, 
-                                sell_order_adjusted.amount, 
-                                sell_order_adjusted.order_type, 
-                                sell_order_adjusted.price)
-
+            self.sell(self.less_liquid_exchange, self.security, sell_order_adjusted.amount, sell_order_adjusted.order_type, sell_order_adjusted.price)
             self.sell_order_placed = True
             self.logger().info("Placed a sell")
 
-
-        #Canceling unfilled orders
-
+        # Canceling unfilled orders
         for order in self.get_active_orders(connector_name=self.less_liquid_exchange):
             cancel_timestamp = order.creation_timestamp / 1000000 + self.max_order_age
             self.logger().info(f"{cancel_timestamp}")
             self.logger().info(f"{order.creation_timestamp / 1000000 + self.max_order_age}")
 
             if order.is_buy:
-                if cancel_timestamp < self.current_timestamp or (((best_bid_liquid - best_bid_less_liquid) / best_bid_less_liquid ) * 100 )< self.spread_threshold_percent:
+                if cancel_timestamp < self.current_timestamp or (((best_bid_liquid - best_bid_less_liquid) / best_bid_less_liquid) * 100) < self.spread_threshold_percent:
                     self.cancel(self.less_liquid_exchange, order.trading_pair, order.client_order_id)
                     self.buy_order_placed = False
                     self.logger().info("Canceled a buy")
-            
+
             else:
-                if cancel_timestamp < self.current_timestamp or (((best_ask_less_liquid - best_ask_liquid) / best_ask_liquid) *100) < self.spread_threshold_percent:
+                if cancel_timestamp < self.current_timestamp or (((best_ask_less_liquid - best_ask_liquid) / best_ask_liquid) * 100) < self.spread_threshold_percent:
                     self.cancel(self.less_liquid_exchange, order.trading_pair, order.client_order_id)
                     self.sell_order_placed = False
                     self.logger().info("Canceled a sell")
 
         #
-        if self.long_position and (((best_bid_liquid - best_bid_less_liquid) / best_bid_less_liquid ) * 100 )< self.spread_threshold_percent:
-            sell_order = OrderCandidate(trading_pair=self.security, 
-                                        is_maker=True, 
-                                        order_type=OrderType.LIMIT,
-                                        order_side=TradeType.SELL,
-                                        amount=sell_order_amount,
-                                        price=maker_sell_price)
-
+        if self.long_position and (((best_bid_liquid - best_bid_less_liquid) / best_bid_less_liquid) * 100) < self.spread_threshold_percent:
+            sell_order = OrderCandidate(trading_pair=self.security, is_maker=True, order_type=OrderType.LIMIT, order_side=TradeType.SELL, amount=sell_order_amount, price=maker_sell_price)
             sell_order_adjusted = self.connectors[self.less_liquid_exchange].budget_checker.adjust_candidate(sell_order, all_or_none=True)
-
-            self.sell(self.less_liquid_exchange, 
-                                self.security, 
-                                sell_order_adjusted.amount, 
-                                sell_order_adjusted.order_type, 
-                                sell_order_adjusted.price)
-
+            self.sell(self.less_liquid_exchange, self.security, sell_order_adjusted.amount, sell_order_adjusted.order_type, sell_order_adjusted.price)
             self.logger().info("Closed the long position")
             self.long_position = False
 
-        if self.short_position and (((best_ask_less_liquid - best_ask_liquid) / best_ask_liquid) *100) < self.spread_threshold_percent:
+        if self.short_position and (((best_ask_less_liquid - best_ask_liquid) / best_ask_liquid) * 100) < self.spread_threshold_percent:
 
-            buy_order = OrderCandidate(trading_pair=self.security, 
-                                                    is_maker=True, 
-                                                    order_type=OrderType.LIMIT,
-                                                    order_side=TradeType.BUY,
-                                                    amount=buy_order_amount,
-                                                    price=maker_buy_price)
-
+            buy_order = OrderCandidate(trading_pair=self.security, is_maker=True, order_type=OrderType.LIMIT, order_side=TradeType.BUY, amount=buy_order_amount, price=maker_buy_price)
             buy_order_adjusted = self.connectors[self.less_liquid_exchange].budget_checker.adjust_candidate(buy_order, all_or_none=True)
-
-            self.buy(self.less_liquid_exchange, 
-                        self.security, 
-                        buy_order_adjusted.amount, 
-                        buy_order_adjusted.order_type, 
-                        buy_order_adjusted.price)
-
+            self.buy(self.less_liquid_exchange, self.security, buy_order_adjusted.amount, buy_order_adjusted.order_type, buy_order_adjusted.price)
             self.logger().info("Closed the short position")
             self.short_position = False
-            
+
         return
 
     def is_active_maker_order(self, event: OrderFilledEvent):
@@ -168,20 +101,15 @@ class Steamroller(ScriptStrategyBase):
                 return True
         return False
 
-
-
-        
     def did_fill_order(self, event: OrderFilledEvent):
 
-            if event.trade_type == TradeType.BUY and self.is_active_maker_order(event):
-                self.logger().info("Buy is filled")
-                self.long_position = True
-            else:
-                if event.trade_type == TradeType.SELL and self.is_active_maker_order(event):
-                    self.logger().info("Sell is filled")
-                    self.short_position = True
-    
-
+        if event.trade_type == TradeType.BUY and self.is_active_maker_order(event):
+            self.logger().info("Buy is filled")
+            self.long_position = True
+        else:
+            if event.trade_type == TradeType.SELL and self.is_active_maker_order(event):
+                self.logger().info("Sell is filled")
+                self.short_position = True
 
     def format_status(self) -> str:
         """
@@ -216,5 +144,3 @@ class Steamroller(ScriptStrategyBase):
         is_buy = percentage > 0
         result = self.connectors[row["Exchange"]].get_volume_for_price(row["Market"], is_buy, price)
         return result.result_volume
-
-
