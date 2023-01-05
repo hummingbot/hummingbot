7e27d6e15edc44db1e1e60a0c7119caca2f426ee
bczhang <zhangbocheng@zhangboengdembp>
2023-01-05 20:54:39 +0800

    update to the latest standards

--- test/hummingbot/connector/derivative/binance_perpetual/test_binance_perpetual_utils.py
+++ test/hummingbot/connector/derivative/binance_perpetual/test_binance_perpetual_utils.py
@@ -1,11 +1,4 @@
-import os
-import socket
 import unittest
-
-from mock import patch
-
-import hummingbot.connector.derivative.binance_perpetual.binance_perpetual_utils as utils
-from hummingbot.connector.derivative.binance_perpetual.binance_perpetual_utils import BROKER_ID
 
 
 class BinancePerpetualUtilsUnitTests(unittest.TestCase):
