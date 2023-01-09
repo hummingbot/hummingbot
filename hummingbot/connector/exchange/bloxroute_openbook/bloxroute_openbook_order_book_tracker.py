b18337530255aad21b2ddfbaaac13e4d79dc546a
Kaushik Donthi <kaushikdr@gmail.com>
2023-01-09 12:49:40 -0800

    Override  for the OrderBookTracker

--- hummingbot/connector/exchange/bloxroute_openbook/bloxroute_openbook_order_book_tracker.py
+++ hummingbot/connector/exchange/bloxroute_openbook/bloxroute_openbook_order_book_tracker.py
@@ -4,9 +4,12 @@ from typing import List, Optional
 from hummingbot.connector.exchange.bloxroute_openbook.bloxroute_openbook_api_order_book_data_source import (
     BloxrouteOpenbookAPIOrderBookDataSource,
 )
+from hummingbot.core.data_type.order_book_message import OrderBookMessage, OrderBookMessageType
+from hummingbot.core.data_type.order_book_row import OrderBookRow
 from hummingbot.core.data_type.order_book_tracker import OrderBookTracker
 from hummingbot.core.data_type.order_book_tracker_data_source import OrderBookTrackerDataSource
 
+from hummingbot.core.data_type.order_book import OrderBook
 
 class BloxrouteOpenbookOrderBookTracker(OrderBookTracker):
 
@@ -16,9 +19,29 @@ class BloxrouteOpenbookOrderBookTracker(OrderBookTracker):
         super().__init__(data_source, trading_pairs, domain)
         self.process_order_book_events_task = data_source.process_order_book_events_task
 
+    async def _track_single_book(self, trading_pair: str):
+        order_book: OrderBook = self._order_books[trading_pair]
+        tracking_message_queue = self._tracking_message_queues[trading_pair]
+
+        while True:
+            try:
+                message: OrderBookMessage = await tracking_message_queue.get()
+                if message.type is OrderBookMessageType.SNAPSHOT:
+                    order_book.apply_snapshot(bids=message.bids, asks=message.asks, update_id=message.update_id)
+            except asyncio.CancelledError:
+                raise
+            except Exception:
+                self.logger().network(
+                    f"Unexpected error tracking order book for {trading_pair}.",
+                    exc_info=True,
+                    app_warning_msg="Unexpected error tracking order book. Retrying after 5 seconds."
+                )
+                await asyncio.sleep(5.0)
+
     def stop(self):
         super().stop()
 
         if self.process_order_book_events_task is not None:
             self.process_order_book_events_task.close()
             self.process_order_book_events_task = None
+
