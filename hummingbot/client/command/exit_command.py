117b3100c0ea120147bece685e1bde82378ca92f
Konstantinos Panayiotou <klpanagi@gmail.com>
2023-01-13 20:17:33 +0200

    Revert changes to the exit command - restore force exit flag

--- hummingbot/client/command/exit_command.py
+++ hummingbot/client/command/exit_command.py
@@ -11,22 +11,22 @@ if TYPE_CHECKING:
 
 class ExitCommand:
     def exit(self,  # type: HummingbotApplication
-             ):
-        safe_ensure_future(self.exit_loop(), loop=self.ev_loop)
+             force: bool = False):
+        safe_ensure_future(self.exit_loop(force), loop=self.ev_loop)
 
     async def exit_loop(self,  # type: HummingbotApplication
-                        ):
+                        force: bool = False):
         if self.strategy_task is not None and not self.strategy_task.cancelled():
             self.strategy_task.cancel()
-        success = await self._cancel_outstanding_orders()
-        if not success:
-            self.notify(
-                'Wind down process terminated: Failed to cancel all outstanding orders. '
-                '\nYou may need to manually cancel remaining orders by logging into your chosen exchanges'
-            )
-            return
-        # Freeze screen 1 second for better UI
-        await asyncio.sleep(1)
+        if force is False and self._trading_required:
+            success = await self._cancel_outstanding_orders()
+            if not success:
+                self.notify('Wind down process terminated: Failed to cancel all outstanding orders. '
+                            '\nYou may need to manually cancel remaining orders by logging into your chosen exchanges'
+                            '\n\nTo force exit the app, enter "exit -f"')
+                return
+            # Freeze screen 1 second for better UI
+            await asyncio.sleep(1)
 
         if self._gateway_monitor is not None:
             self._gateway_monitor.stop()
