5566a047ed5b2e19e85b6368bd13a8a9bfd13b9a
cardosofede <federico.cardoso.e@gmail.com>
2023-01-18 21:07:15 -0300

    (feat) add noqa to avoid test errors

--- hummingbot/client/command/connect_command.py
+++ hummingbot/client/command/connect_command.py
@@ -12,7 +12,7 @@ from hummingbot.core.utils.async_utils import safe_ensure_future
 from hummingbot.user.user_balances import UserBalances
 
 if TYPE_CHECKING:
-    from hummingbot.client.hummingbot_application import HummingbotApplication
+    from hummingbot.client.hummingbot_application import HummingbotApplication  # noqa: F401
 
 OPTIONS = {cs.name for cs in AllConnectorSettings.get_connector_settings().values()
            if not cs.use_ethereum_wallet and not cs.uses_gateway_generic_connector()}
