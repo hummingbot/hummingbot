8ea1db80fdfcb5eba0b1e813abb81900811d4506
George Burry <gburry@protonmail.ch>
2023-01-24 20:02:54 +0100

    (fix) orders created if no active orders on exchange
    - This replaces bool flags that would stay set to True if taker order
      didn't get placed on maker order fill

--- scripts/xemm_perpetual_hedge.py
+++ scripts/xemm_perpetual_hedge.py
@@ -36,9 +36,6 @@ class XEMMPerpetualHedge(ScriptStrategyBase):
     initialized = False
     hedging_in_progress = False
 
-    buy_order_placed = False
-    sell_order_placed = False
-
     taker_order_id = ''
 
     def on_tick(self):
@@ -46,14 +43,122 @@ class XEMMPerpetualHedge(ScriptStrategyBase):
             self.logger().info('Initializing hedge position')
             self.adjust_perpetual_hedge_if_required()
             self.initialized = True
-
         taker_buy_result = self.connectors[self.taker_exchange].get_price_for_volume(self.taker_pair, True,
                                                                                      self.order_amount)
         taker_sell_result = self.connectors[self.taker_exchange].get_price_for_volume(self.taker_pair, False,
                                                                                       self.order_amount)
-        self.place_buy_order_if_none(taker_sell_result)
-        self.place_sell_order_if_none(taker_buy_result)
+        active_orders = self.get_active_orders(connector_name=self.maker_exchange)
+        self.place_buy_order_if_none(active_orders, taker_sell_result)
+        self.place_sell_order_if_none(active_orders, taker_buy_result)
         self.cancel_orders_if_invalidated(taker_buy_result, taker_sell_result)
+
+    def place_buy_order_if_none(self, active_orders: list, taker_sell_result: ClientOrderBookQueryResult):
+        active_buy_orders = [o for o in active_orders if o.is_buy and o.trading_pair == self.maker_pair]
+        if not active_buy_orders:
+            maker_buy_price = taker_sell_result.result_price * Decimal(1 - self.spread_bps / 10000)
+            buy_order_amount = min(self.order_amount, self.get_maker_buy_budget())
+            buy_order = OrderCandidate(trading_pair=self.maker_pair, is_maker=True, order_type=OrderType.LIMIT,
+                                       order_side=TradeType.BUY, amount=Decimal(buy_order_amount),
+                                       price=maker_buy_price)
+            buy_order_adjusted = self.connectors[self.maker_exchange].budget_checker.adjust_candidate(buy_order,
+                                                                                                      all_or_none=False)
+            self.buy(self.maker_exchange, self.maker_pair, buy_order_adjusted.amount, buy_order_adjusted.order_type,
+                     buy_order_adjusted.price)
+
+    def place_sell_order_if_none(self, active_orders: list, taker_buy_result: ClientOrderBookQueryResult):
+        active_sell_orders = [o for o in active_orders if not o.is_buy and o.trading_pair == self.maker_pair]
+        if not active_sell_orders:
+            maker_sell_price = taker_buy_result.result_price * Decimal(1 + self.spread_bps / 10000)
+            sell_order_amount = min(self.order_amount, self.get_maker_sell_budget())
+            sell_order = OrderCandidate(trading_pair=self.maker_pair, is_maker=True, order_type=OrderType.LIMIT,
+                                        order_side=TradeType.SELL, amount=Decimal(sell_order_amount),
+                                        price=maker_sell_price)
+            sell_order_adjusted = self.connectors[self.maker_exchange].budget_checker.adjust_candidate(sell_order,
+                                                                                                       all_or_none=False
+                                                                                                       )
+            self.sell(self.maker_exchange, self.maker_pair, sell_order_adjusted.amount, sell_order_adjusted.order_type,
+                      sell_order_adjusted.price)
+
+    def cancel_orders_if_invalidated(
+        self, taker_buy_result: ClientOrderBookQueryResult, taker_sell_result: ClientOrderBookQueryResult
+    ):
+        cancel_timestamps = []
+        for order in self.get_active_orders(connector_name=self.maker_exchange):
+            cancel_timestamp = order.creation_timestamp / 1000000 + self.max_order_age
+            cancel_timestamps.append(cancel_timestamp)
+            if order.is_buy:
+                buy_cancel_threshold = taker_sell_result.result_price * Decimal(1 - self.min_spread_bps / 10000)
+                if order.price > buy_cancel_threshold or cancel_timestamp < self.current_timestamp:
+                    self.logger().info(f"Cancelling buy order: {order.client_order_id}")
+                    self.cancel(self.maker_exchange, order.trading_pair, order.client_order_id)
+            else:
+                sell_cancel_threshold = taker_buy_result.result_price * Decimal(1 + self.min_spread_bps / 10000)
+                if order.price < sell_cancel_threshold or cancel_timestamp < self.current_timestamp:
+                    self.logger().info(f"Cancelling sell order: {order.client_order_id}")
+                    self.cancel(self.maker_exchange, order.trading_pair, order.client_order_id)
+        if max(cancel_timestamps) < self.current_timestamp and not self.hedging_in_progress:
+            self.adjust_perpetual_hedge_if_required()
+
+    def get_maker_buy_budget(self) -> float:
+        balance = self.connectors[self.maker_exchange].get_available_balance(self.maker_pair.split('-')[0])
+        return float(balance)
+
+    def get_maker_sell_budget(self) -> float:
+        balance = self.connectors[self.maker_exchange].get_available_balance(self.maker_pair.split('-')[1])
+        maker_buy_result = self.connectors[self.maker_exchange].get_price_for_volume(
+            self.maker_pair, True, self.order_amount)
+        return float(balance / maker_buy_result.result_price)
+
+    def get_taker_buy_budget(self) -> float:
+        balance = self.connectors[self.taker_exchange].get_available_balance(self.taker_pair.split('-')[0])
+        return float(balance)
+
+    def get_taker_sell_budget(self) -> float:
+        balance = self.connectors[self.taker_exchange].get_available_balance(self.taker_pair.split('-')[1])
+        taker_buy_result = self.connectors[self.taker_exchange].get_price_for_volume(
+            self.taker_pair, True, self.order_amount)
+        return float(balance / taker_buy_result.result_price)
+
+    def is_active_maker_order(self, event: OrderFilledEvent):
+        """
+        Helper function that checks if order is an active order on the maker exchange
+        """
+        return event.order_id in [
+            order.client_order_id for order in self.get_active_orders(connector_name=self.maker_exchange)
+        ]
+
+    def place_taker_buy_order(self, event: OrderFilledEvent):
+        mid_price = self.connectors[self.maker_exchange].get_mid_price(self.maker_pair)
+        taker_buy_result = self.connectors[self.taker_exchange].get_price_for_volume(self.taker_pair, True,
+                                                                                     self.order_amount)
+        buy_price_with_slippage = taker_buy_result.result_price * Decimal(1 + self.slippage_buffer_spread_bps / 10000)
+        buy_spread_bps = (event.price - taker_buy_result.result_price) / mid_price * 10000
+        self.logger().info(f"Filled maker sell order at price: {event.price}")
+        self.logger().info(f"Sending taker buy order: {taker_buy_result.result_price} spread: {int(buy_spread_bps)}")
+        buy_order = OrderCandidate(trading_pair=self.taker_pair, is_maker=False, order_type=OrderType.LIMIT,
+                                   order_side=TradeType.BUY, amount=Decimal(event.amount),
+                                   price=buy_price_with_slippage)
+        buy_order_adjusted = self.connectors[self.taker_exchange].budget_checker.adjust_candidate(buy_order,
+                                                                                                  all_or_none=False)
+        self.taker_order_id = self.buy(self.taker_exchange, self.taker_pair, buy_order_adjusted.amount,
+                                       buy_order_adjusted.order_type, buy_order_adjusted.price)
+
+    def place_taker_sell_order(self, event: OrderFilledEvent):
+        mid_price = self.connectors[self.maker_exchange].get_mid_price(self.maker_pair)
+        taker_sell_result = self.connectors[self.taker_exchange].get_price_for_volume(self.taker_pair, False,
+                                                                                      self.order_amount)
+        sell_price_with_slippage = taker_sell_result.result_price * Decimal(1 - self.slippage_buffer_spread_bps / 10000)
+        sell_spread_bps = (taker_sell_result.result_price - event.price) / mid_price * 10000
+        self.logger().info(f"Filled maker buy order with price: {event.price}")
+        self.logger().info(
+            f"Sending taker sell order at price: {taker_sell_result.result_price} spread: {int(sell_spread_bps)} bps")
+        sell_order = OrderCandidate(trading_pair=self.taker_pair, is_maker=False, order_type=OrderType.LIMIT,
+                                    order_side=TradeType.SELL, amount=Decimal(event.amount),
+                                    price=sell_price_with_slippage)
+        sell_order_adjusted = self.connectors[self.taker_exchange].budget_checker.adjust_candidate(sell_order,
+                                                                                                   all_or_none=False)
+        self.taker_order_id = self.sell(self.taker_exchange, self.taker_pair, sell_order_adjusted.amount,
+                                        sell_order_adjusted.order_type, sell_order_adjusted.price)
 
     def did_fill_order(self, event: OrderFilledEvent):
         if event.trade_type == TradeType.BUY and self.is_active_maker_order(event):
@@ -89,108 +194,6 @@ class XEMMPerpetualHedge(ScriptStrategyBase):
         lines.extend(["", f"  Hedge Discrepancy (base asset): {self._calculate_perpetual_discrepancy()}"])
 
         return "\n".join(lines)
-
-    def place_buy_order_if_none(self, taker_sell_result: ClientOrderBookQueryResult):
-        if not self.buy_order_placed:
-            maker_buy_price = taker_sell_result.result_price * Decimal(1 - self.spread_bps / 10000)
-            buy_order_amount = min(self.order_amount, self.get_taker_buy_budget())
-            buy_order = OrderCandidate(trading_pair=self.maker_pair, is_maker=True, order_type=OrderType.LIMIT,
-                                       order_side=TradeType.BUY, amount=Decimal(buy_order_amount),
-                                       price=maker_buy_price)
-            buy_order_adjusted = self.connectors[self.maker_exchange].budget_checker.adjust_candidate(buy_order,
-                                                                                                      all_or_none=False)
-            self.buy(self.maker_exchange, self.maker_pair, buy_order_adjusted.amount, buy_order_adjusted.order_type,
-                     buy_order_adjusted.price)
-            self.buy_order_placed = True
-
-    def place_sell_order_if_none(self, taker_buy_result: ClientOrderBookQueryResult):
-        if not self.sell_order_placed:
-            maker_sell_price = taker_buy_result.result_price * Decimal(1 + self.spread_bps / 10000)
-            sell_order_amount = min(self.order_amount, self.get_taker_sell_budget())
-            sell_order = OrderCandidate(trading_pair=self.maker_pair, is_maker=True, order_type=OrderType.LIMIT,
-                                        order_side=TradeType.SELL, amount=Decimal(sell_order_amount),
-                                        price=maker_sell_price)
-            sell_order_adjusted = self.connectors[self.maker_exchange].budget_checker.adjust_candidate(sell_order,
-                                                                                                       all_or_none=False
-                                                                                                       )
-            self.sell(self.maker_exchange, self.maker_pair, sell_order_adjusted.amount, sell_order_adjusted.order_type,
-                      sell_order_adjusted.price)
-            self.sell_order_placed = True
-
-    def cancel_orders_if_invalidated(
-        self, taker_buy_result: ClientOrderBookQueryResult, taker_sell_result: ClientOrderBookQueryResult
-    ):
-        cancel_timestamps = []
-        for order in self.get_active_orders(connector_name=self.maker_exchange):
-            cancel_timestamp = order.creation_timestamp / 1000000 + self.max_order_age
-            cancel_timestamps.append(cancel_timestamp)
-            if order.is_buy:
-                buy_cancel_threshold = taker_sell_result.result_price * Decimal(1 - self.min_spread_bps / 10000)
-                if order.price > buy_cancel_threshold or cancel_timestamp < self.current_timestamp:
-                    self.logger().info(f"Cancelling buy order: {order.client_order_id}")
-                    self.cancel(self.maker_exchange, order.trading_pair, order.client_order_id)
-                    self.buy_order_placed = False
-            else:
-                sell_cancel_threshold = taker_buy_result.result_price * Decimal(1 + self.min_spread_bps / 10000)
-                if order.price < sell_cancel_threshold or cancel_timestamp < self.current_timestamp:
-                    self.logger().info(f"Cancelling sell order: {order.client_order_id}")
-                    self.cancel(self.maker_exchange, order.trading_pair, order.client_order_id)
-                    self.sell_order_placed = False
-        if max(cancel_timestamps) < self.current_timestamp and not self.hedging_in_progress:
-            self.adjust_perpetual_hedge_if_required()
-
-    def get_taker_buy_budget(self) -> float:
-        balance = self.connectors[self.taker_exchange].get_available_balance(self.taker_pair.split('-')[0])
-        return float(balance)
-
-    def get_taker_sell_budget(self) -> float:
-        balance = self.connectors[self.taker_exchange].get_available_balance(self.taker_pair.split('-')[1])
-        taker_buy_result = self.connectors[self.taker_exchange].get_price_for_volume(
-            self.taker_pair, True, self.order_amount)
-        return float(balance / taker_buy_result.result_price)
-
-    def is_active_maker_order(self, event: OrderFilledEvent):
-        """
-        Helper function that checks if order is an active order on the maker exchange
-        """
-        return event.order_id in [
-            order.client_order_id for order in self.get_active_orders(connector_name=self.maker_exchange)
-        ]
-
-    def place_taker_buy_order(self, event: OrderFilledEvent):
-        mid_price = self.connectors[self.maker_exchange].get_mid_price(self.maker_pair)
-        taker_buy_result = self.connectors[self.taker_exchange].get_price_for_volume(self.taker_pair, True,
-                                                                                     self.order_amount)
-        buy_price_with_slippage = taker_buy_result.result_price * Decimal(1 + self.slippage_buffer_spread_bps / 10000)
-        buy_spread_bps = (event.price - taker_buy_result.result_price) / mid_price * 10000
-        self.logger().info(f"Filled maker sell order at price: {event.price}")
-        self.logger().info(f"Sending taker buy order: {taker_buy_result.result_price} spread: {int(buy_spread_bps)}")
-        buy_order = OrderCandidate(trading_pair=self.taker_pair, is_maker=False, order_type=OrderType.LIMIT,
-                                   order_side=TradeType.BUY, amount=Decimal(event.amount),
-                                   price=buy_price_with_slippage)
-        buy_order_adjusted = self.connectors[self.taker_exchange].budget_checker.adjust_candidate(buy_order,
-                                                                                                  all_or_none=False)
-        self.taker_order_id = self.buy(self.taker_exchange, self.taker_pair, buy_order_adjusted.amount,
-                                       buy_order_adjusted.order_type, buy_order_adjusted.price)
-        self.sell_order_placed = False
-
-    def place_taker_sell_order(self, event: OrderFilledEvent):
-        mid_price = self.connectors[self.maker_exchange].get_mid_price(self.maker_pair)
-        taker_sell_result = self.connectors[self.taker_exchange].get_price_for_volume(self.taker_pair, False,
-                                                                                      self.order_amount)
-        sell_price_with_slippage = taker_sell_result.result_price * Decimal(1 - self.slippage_buffer_spread_bps / 10000)
-        self.logger().info(f"Filled maker buy order with price: {event.price}")
-        sell_spread_bps = (taker_sell_result.result_price - event.price) / mid_price * 10000
-        self.logger().info(
-            f"Sending taker sell order at price: {taker_sell_result.result_price} spread: {int(sell_spread_bps)} bps")
-        sell_order = OrderCandidate(trading_pair=self.taker_pair, is_maker=False, order_type=OrderType.LIMIT,
-                                    order_side=TradeType.SELL, amount=Decimal(event.amount),
-                                    price=sell_price_with_slippage)
-        sell_order_adjusted = self.connectors[self.taker_exchange].budget_checker.adjust_candidate(sell_order,
-                                                                                                   all_or_none=False)
-        self.taker_order_id = self.sell(self.taker_exchange, self.taker_pair, sell_order_adjusted.amount,
-                                        sell_order_adjusted.order_type, sell_order_adjusted.price)
-        self.buy_order_placed = False
 
     def adjust_perpetual_hedge_if_required(self):
         try:
