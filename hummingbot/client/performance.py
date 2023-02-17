e1ce5704147473c0eda93a1bd669326b869d0a25
Nicolas Baum <nicolasbaum@gmail.com>
2023-02-17 16:48:00 -0300

    Thank you isort

--- hummingbot/client/performance.py
+++ hummingbot/client/performance.py
@@ -6,7 +6,7 @@ from typing import Any, Dict, List, Optional, Tuple
 
 from hummingbot.connector.utils import combine_to_hb_trading_pair, split_hb_trading_pair
 from hummingbot.core.data_type.common import PositionAction, TradeType
-from hummingbot.core.data_type.trade_fee import TokenAmount, DeductedFromReturnsTradeFee
+from hummingbot.core.data_type.trade_fee import DeductedFromReturnsTradeFee, TokenAmount
 from hummingbot.core.rate_oracle.rate_oracle import RateOracle
 from hummingbot.logger import HummingbotLogger
 from hummingbot.model.trade_fill import TradeFill
