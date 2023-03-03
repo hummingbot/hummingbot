6a0d06018b7338118524e7cd13c5273d89ca3467
memento <claude.rc@gmail.com>
2023-03-03 10:43:41 -0600

    (feat) Proposing a weakref Singleton Metaclass

---
+++ hummingbot/core/utils/weak_singleton_metaclass.py
@@ -0,0 +1,152 @@
+import abc
+import asyncio
+import functools
+import threading
+import weakref
+from typing import Dict, TypeVar, Union
+
+from typing_extensions import Type
+
+
+class ClassNotYetInstantiatedError(Exception):
+    """Exception raised when the WeakSingletonMetaclass object is not instantiated."""
+    pass
+
+
+class NoLockForInstanceError(Exception):
+    """Exception raised when the WeakSingletonMetaclass object has no lock when it has an instance."""
+    pass
+
+
+class ClassAlreadyInstantiatedError(Exception):
+    """Exception raised when the WeakSingletonMetaclass object is not instantiated."""
+    pass
+
+
+T = TypeVar("T", bound="WeakSingletonMetaclass")
+
+
+class WeakSingletonMetaclass(abc.ABCMeta):
+    """Metaclass for implementing a weak reference to singleton class.
+
+    The WeakSingletonMetaclass metaclass ensures that only one instance of a class
+    can exist at any time. It uses a dictionary to store the instances of each class.
+    """
+
+    _instances: Dict[Type[T], Union[weakref.ref, None]] = {}
+
+    _strict_locks: Dict[Type[T], type(threading.Lock())] = {}
+    _master_lock: type(threading.Lock()) = threading.Lock()
+
+    def __call__(cls: Type[T], *args, **kwargs) -> T:
+        if cls not in cls._instances:
+            with cls._master_lock:
+                if cls not in cls._instances:
+                    cls._instances[cls] = None
+                    cls._strict_locks[cls] = threading.Lock()
+
+        assert cls in cls._strict_locks, f"Class {cls} has no strict lock"
+        assert cls in cls._instances, f"Class {cls} has no instance"
+
+        if cls._has_no_instance_or_dead(cls):
+            with cls._strict_locks[cls]:
+                if cls._has_no_instance_or_dead(cls):
+                    instance = super(WeakSingletonMetaclass, cls).__call__(*args, **kwargs)
+                    cls._instances[cls] = weakref.ref(instance, functools.partial(cls._finalize, cls=cls))
+                    return instance
+
+        assert cls._has_hard_reference(cls), f"Class {cls} has no instance"
+        return cls._instances[cls]()
+
+    def is_instantiated(cls) -> bool:
+        return cls in cls._instances and cls._instances[cls] is not None and cls._instances[cls]() is not None
+
+    @classmethod
+    def _has_instance_only(mcs, cls: Type[T]):
+        """Check if the class has been initialized but has no instance"""
+        instances = getattr(mcs, "_instances", None)
+        return cls in instances and instances[cls] is None
+
+    @classmethod
+    def _has_no_instance(mcs, cls: Type[T]):
+        """Check if the class has been initialized but has no instance"""
+        return mcs._has_instance_only(cls)
+
+    @classmethod
+    def _has_no_instance_or_dead(mcs, cls: Type[T]):
+        """Check if the class has been initialized but has no instance"""
+        return mcs._has_instance_only(cls) or mcs._has_dead_reference(cls)
+
+    @classmethod
+    def _has_instance(mcs, cls: Type[T]):
+        """Check if the class has an instance"""
+        instances = getattr(mcs, "_instances", None)
+        return cls in instances and instances[cls] is not None
+
+    @classmethod
+    def _has_hard_reference(mcs, cls: Type[T]):
+        """Check if the class has a hard reference"""
+        instances = getattr(mcs, "_instances", None)
+        return mcs._has_instance(cls) and instances[cls]() is not None
+
+    @classmethod
+    def _has_dead_reference(mcs, cls: Type[T]):
+        """Check if the class has a dead reference"""
+        instances = getattr(mcs, "_instances", None)
+        return mcs._has_instance(cls) and instances[cls]() is None
+
+    @classmethod
+    def _cleanup_when_no_reference(mcs, cls: Type[T]):
+        """Clean the references to the class if its weak reference is dead (last instance deleted)"""
+        assert mcs._has_dead_reference(cls), "_cleanup_when_no_reference called with no dead reference"
+        assert cls in mcs._strict_locks, f"{cls} not in _strict_locks"
+        assert mcs._strict_locks[cls].locked(), "_cleanup_when_no_reference should be called when locked"
+
+        try:
+            with mcs._strict_locks[cls]:
+                mcs._instances[cls] = None
+                if mcs._strict_locks[cls].locked():
+                    mcs._strict_locks[cls] = threading.Lock()
+        except KeyError:
+            raise NoLockForInstanceError("Failed to lock access to instance")
+
+        assert mcs._has_no_instance(cls), "Instance should be None"
+
+    @staticmethod
+    def _finalize(wr_obj: weakref, cls: Type[T]):
+        """The reference count dropped to 0, simply set the weakref to None"""
+        assert wr_obj() is None, "Weakref should be dead"
+        print(f"Finalizing {cls}")
+        cls._instances[cls] = None
+        if cls._strict_locks[cls].locked():
+            cls._strict_locks[cls] = threading.Lock()
+        assert cls._has_no_instance(cls), "Instance should be None"
+
+    # --- Class testing methods ---
+    def __clear(cls: Type[T]):
+        """Clear the class attributes (registration of the class as Singleton)
+        This method was added for testing purposes only. It should not be used in production code.
+        This only simulates the class not being referenced as we cannot delete the instance directly."""
+        if cls in cls._instances:
+            with cls._strict_locks[cls]:
+                if cls in cls._instances:
+                    cls._instances[cls] = None
+                    cls._strict_locks[cls] = threading.Lock()
+
+    async def __aclear(cls: Type[T]):
+        """Clear the class attributes (registration of the class as Singleton)
+        This method was added for testing purposes only. It should not be used in production code."""
+        if cls in cls._instances and cls._instances[cls]() is not None:
+            with cls._strict_locks[cls]:
+                if cls in cls._instances and cls._instances[cls]() is not None:
+                    cls._instances[cls] = None
+                    cls._strict_locks[cls] = threading.Lock()
+        await asyncio.sleep(0)
+
+    def __lock(cls: Type[T]):
+        """Returns the lock for the class.
+        This method was added for testing purposes only. It should not be used in production code."""
+        if cls in cls._strict_locks:
+            return cls._strict_locks[cls]
+        else:
+            raise ClassNotYetInstantiatedError(f"Class {cls.__name__} not yet instantiated. Nothing to lock")
