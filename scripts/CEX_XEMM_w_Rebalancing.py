66839a176bc2896d63893bd43781edb28997abde
Tobi <63905328+TR111@users.noreply.github.com>
2023-03-04 14:14:57 +0100

    Update CEX_XEMM_w_Rebalancing.py
    Removed a comment

--- scripts/CEX_XEMM_w_Rebalancing.py
+++ scripts/CEX_XEMM_w_Rebalancing.py
@@ -146,7 +146,7 @@ class CEX_XEMM_w_Rebalancing(ScriptStrategyBase):
         base_asset_taker_exchange, quote_asset_taker_exchange, ratio_taker_exchange = self.get_base_quote_and_ratio_taker_exchange()
         base_asset_maker_exchange, quote_asset_maker_exchange, ratio_maker_exchange = self.get_base_quote_and_ratio_maker_exchange()
 
-        amount_to_rebalance = Decimal(self.order_amount) # for now just buy the same amount, but this is a thing that you should improve
+        amount_to_rebalance = Decimal(self.order_amount)
 
         if ratio_maker_exchange < self.rebalancing_pct:
             self.logger().info(f"Rebalancing since the ratio of base asset {ratio_maker_exchange} in the maker exchange is lower than {self.rebalancing_pct}")
