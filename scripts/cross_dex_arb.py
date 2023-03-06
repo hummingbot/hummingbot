d21f2aa53f10ca48a6f1ce304e9ccea38ea4d462
Alex Lau <alexlauwh@gmail.com>
2023-03-06 22:04:45 +0800

    feat: add method for execute trade and query tx status

--- scripts/cross_dex_arb.py
+++ scripts/cross_dex_arb.py
@@ -1,9 +1,10 @@
+import asyncio
 from typing import Dict, Optional
 
-from hummingbot.connector.connector_base import ConnectorBase
+from hummingbot.connector.connector_base import ConnectorBase  # type: ignore
 from hummingbot.core.data_type.common import TradeType
 from hummingbot.core.gateway.gateway_http_client import GatewayHttpClient
-from hummingbot.core.gateway.types import Transaction
+from hummingbot.core.gateway.types import Transaction, TransactionStatus
 from hummingbot.core.utils.async_utils import safe_ensure_future
 from hummingbot.data_feed.amm_data_feed import AmmDataFeed
 from hummingbot.data_feed.balance_data_feed import BalanceDataFeed
@@ -11,6 +12,7 @@ from hummingbot.data_feed.utils import split_base_quote
 from hummingbot.strategy.script_strategy_base import Decimal, ScriptStrategyBase
 
 UNKNONW = -1
+TRANSACTION_COMPLETED = 1
 
 
 class SlippagePriceError(Exception):
@@ -56,6 +58,7 @@ class CrossDexArb(ScriptStrategyBase):
     gas_token = "BNB"
     min_gas_fee_reserve = Decimal("0.001")
     min_profit_bp = 10
+    slippage_tolerance = 0.05
     is_submit_tx_incomplete = False
     is_poll_tx_incomplete = False
     transaction_a: Optional[Transaction] = None
@@ -63,7 +66,6 @@ class CrossDexArb(ScriptStrategyBase):
     last_trade_timestamp: int = UNKNONW
 
     def __init__(self, connectors: Dict[str, ConnectorBase]) -> None:
-        # Is necessary to start the Candles Feed.
         super().__init__(connectors)
         self.dex_data_feed_a.start()
         self.dex_data_feed_b.start()
@@ -79,14 +81,17 @@ class CrossDexArb(ScriptStrategyBase):
         self.dex_data_feed_b.stop()
         self.logger().info(f"{self.dex_data_feed_a.name} and {self.dex_data_feed_b.name} terminated...")
 
+    def format_status(self) -> str:
+        raise NotImplementedError
+
     def on_tick(self) -> None:
         if not self.is_precheck():
             return
 
         if self.is_transactions_in_progress():
-            safe_ensure_future(self.poll_transaction_status())
+            safe_ensure_future(self.reset_state_if_transactions_complete())  # type: ignore
         elif self.transaction_a is None and self.transaction_b is None:
-            safe_ensure_future(self.decide_trade())
+            safe_ensure_future(self.decide_and_trade())  # type: ignore
         else:
             self.logger().warning(
                 f"Either transaction_a ({self.transaction_a}) "
@@ -126,22 +131,37 @@ class CrossDexArb(ScriptStrategyBase):
             _is_precheck = True
         return _is_precheck
 
-    async def poll_transaction_status(self) -> None:
-        self.transaction_a = None
-        self.transaction_b = None
-        raise NotImplementedError
+    # TODO: how to get executed price and amounted received?
+    async def reset_state_if_transactions_complete(self) -> None:
+        assert self.transaction_a and self.transaction_b, "transaction_a and transaction_b should not be None."
+        self.is_poll_tx_incomplete = True
+        tx_hash_a = self.transaction_a.txHash
+        tx_hash_b = self.transaction_b.txHash
+        tx_status_a_task = asyncio.create_task(self._poll_transaction_status(tx_hash_a))
+        tx_status_b_task = asyncio.create_task(self._poll_transaction_status(tx_hash_b))
+        tx_status_a = await tx_status_a_task
+        tx_status_b = await tx_status_b_task
+        if tx_status_a.txStatus == TRANSACTION_COMPLETED and tx_status_b.txStatus == TRANSACTION_COMPLETED:
+            self.logger().info(
+                f"Both transactions ({tx_hash_a} and {tx_hash_b}) are completed. "
+                "Reset self.transaction_a and self.transaction_b to be None."
+            )
+            self.transaction_a = None
+            self.transaction_b = None
+        self.is_poll_tx_incomplete = False
+        return
 
-    async def decide_trade(self) -> None:
+    async def decide_and_trade(self) -> None:
         # TODO: handle the case when one-leg fail
         # e.g. insufficient balance on 1 leg/ slippage limit succeeded
         if self.should_base_buy_a_sell_b():
             if self.is_enough_balance_for_trade(buy_exchange=self.connector_a, sell_exchange=self.connector_b):
-                self.base_buy_a_sell_b()
+                await self.base_buy_a_sell_b()
             else:
                 self.logger().warning(f"Insufficient balance for trade. Skip this on_tick.")
         elif self.should_base_buy_b_sell_a():
             if self.is_enough_balance_for_trade(buy_exchange=self.connector_b, sell_exchange=self.connector_a):
-                self.base_buy_b_sell_a()
+                await self.base_buy_b_sell_a()
             else:
                 self.logger().warning(f"Insufficient balance for trade. Skip this on_tick.")
         return
@@ -165,19 +185,86 @@ class CrossDexArb(ScriptStrategyBase):
         profit_pb = (quote_amount_in - quote_amount_out - b_gas_fee - a_gas_fee) / quote_amount_in * 10000
         return profit_pb >= self.min_profit_bp
 
-    def base_buy_a_sell_b(self) -> None:
+    async def base_buy_a_sell_b(self) -> None:
+        assert self.balance_data_feed.wallet_address is not None, "wallet_address should not be None."
         self.is_submit_tx_incomplete = True
-        trading_pair = self.trading_pair
-        pass
+        await self._trade_two_legs(
+            self.trading_pair,
+            buy_data_feed=self.dex_data_feed_a,
+            sell_data_feed=self.dex_data_feed_b,
+            slippage_tolerance=self.slippage_tolerance,
+        )
         self.is_submit_tx_incomplete = False
-        raise NotImplementedError
+        return
 
-    def base_buy_b_sell_a(self) -> None:
+    async def base_buy_b_sell_a(self) -> None:
         self.is_submit_tx_incomplete = True
-        trading_pair = self.trading_pair
-        pass
+        await self._trade_two_legs(
+            self.trading_pair,
+            buy_data_feed=self.dex_data_feed_b,
+            sell_data_feed=self.dex_data_feed_a,
+            slippage_tolerance=self.slippage_tolerance,
+        )
         self.is_submit_tx_incomplete = False
-        raise NotImplementedError
+        return
+
+    async def _poll_transaction_status(self, transaction_hash: str) -> TransactionStatus:
+        transaction_status = await self.gateway_client.get_transaction_status(
+            self.chain, self.network, transaction_hash
+        )
+        return TransactionStatus(**transaction_status)
+
+    async def _trade_two_legs(
+        self,
+        trading_pair: str,
+        buy_data_feed: AmmDataFeed,
+        sell_data_feed: AmmDataFeed,
+        slippage_tolerance: float,
+    ) -> None:
+        assert self.balance_data_feed.wallet_address is not None, "wallet_address should not be None."
+        base, quote = split_base_quote(trading_pair)
+        buy_price_slippage_limit = float(buy_data_feed.price_dict[trading_pair].buy.price) * (1 + slippage_tolerance)
+        buy_base_in_b_task = asyncio.create_task(
+            self.gateway_client.amm_trade(
+                self.chain,
+                self.network,
+                buy_data_feed.connector,
+                self.balance_data_feed.wallet_address,
+                base,
+                quote,
+                TradeType.BUY,
+                self.order_amount,
+                Decimal(buy_price_slippage_limit),
+            )
+        )
+        sell_price_slippage_limit = float(sell_data_feed.price_dict[trading_pair].sell.price) * (1 - slippage_tolerance)
+        sell_base_in_a_task = asyncio.create_task(
+            self.gateway_client.amm_trade(
+                self.chain,
+                self.network,
+                sell_data_feed.connector,
+                self.balance_data_feed.wallet_address,
+                base,
+                quote,
+                TradeType.SELL,
+                self.order_amount,
+                Decimal(sell_price_slippage_limit),
+            )
+        )
+        transaction_a = await sell_base_in_a_task
+        transaction_b = await buy_base_in_b_task
+        self.transaction_a = Transaction(**transaction_a)
+        self.transaction_b = Transaction(**transaction_b)
+
+        expected_buy_quote_amount = float(buy_data_feed.price_dict[trading_pair].buy.expectedAmount)
+        expected_sell_quote_amount = float(sell_data_feed.price_dict[trading_pair].sell.expectedAmount)
+        self.logger().info(
+            f"Submitted sell order for {self.order_amount_in_base} of {base} token in {sell_data_feed.connector} at slippage limit of {sell_price_slippage_limit:.5f}. "
+            f"Expected receive {expected_buy_quote_amount:.5f} of {quote} token.\n"
+            f"Submitted buy order for {self.order_amount_in_base} of {base} token in {buy_data_feed.connector} at slippage limit of {buy_price_slippage_limit:.5f}. "
+            f"Expected spend {expected_sell_quote_amount:.5f} of {quote} token."
+        )
+        return
 
     def is_enough_balance_for_trade(self, buy_exchange: str, sell_exchange: str) -> bool:
         buy_side_data_feed = self.dex_data_feed_a if buy_exchange == self.connector_a else self.dex_data_feed_b
@@ -187,16 +274,13 @@ class CrossDexArb(ScriptStrategyBase):
         ), "buy_side_data_feed should not be the same as sell_side_data_feed."
         base, quote = split_base_quote(self.trading_pair)
         base_required = float(sell_side_data_feed.price_dict[self.trading_pair].sell.amount)
-        is_base_enough = float(self.balance_data_feed.balances[self.chain][base]) >= base_required
+        is_base_enough = float(self.balance_data_feed.balances[self.chain].balances[base]) >= base_required
         quote_required = float(buy_side_data_feed.price_dict[self.trading_pair].buy.expectedAmount)
-        is_quote_enough = float(self.balance_data_feed.balances[self.chain][quote]) >= quote_required
+        is_quote_enough = float(self.balance_data_feed.balances[self.chain].balances[quote]) >= quote_required
         return is_quote_enough and is_base_enough
 
     def is_balance_updated_after_last_trade(self) -> bool:
         return self.balance_data_feed.balances[self.chain].timestamp > self.last_trade_timestamp
-
-    def is_out_of_balance(self) -> bool:
-        raise NotImplementedError
 
     def is_out_of_gas_fee_reserve(self) -> bool:
         return float(self.balance_data_feed.balances[self.chain].balances[self.gas_token]) > self.min_gas_fee_reserve
