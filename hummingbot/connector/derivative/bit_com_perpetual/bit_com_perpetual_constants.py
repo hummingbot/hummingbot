6ccb68c609ac9d1d004b3ef104483988a8b10e35
bczhang <zhangbocheng@zhangboengdembp>
2023-03-23 15:07:51 +0800

    update broker_id

--- hummingbot/connector/derivative/bit_com_perpetual/bit_com_perpetual_constants.py
+++ hummingbot/connector/derivative/bit_com_perpetual/bit_com_perpetual_constants.py
@@ -2,7 +2,7 @@ from hummingbot.core.api_throttler.data_types import LinkedLimitWeightPair, Rate
 from hummingbot.core.data_type.in_flight_order import OrderState
 
 EXCHANGE_NAME = "bit_com_perpetual"
-BROKER_ID = "x-3QreWesy"
+BROKER_ID = "HBOT"
 MAX_ORDER_ID_LEN = 32
 
 DOMAIN = EXCHANGE_NAME
