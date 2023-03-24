74cf97c719d9f5718edf50777189a9362fb9e944
vic-en <31972210+vic-en@users.noreply.github.com>
2023-03-24 14:49:55 -0500

    update orderbook to hedged contract

--- hummingbot/connector/derivative/phemex_perpetual/phemex_perpetual_api_order_book_data_source.py
+++ hummingbot/connector/derivative/phemex_perpetual/phemex_perpetual_api_order_book_data_source.py
@@ -21,7 +21,7 @@ from hummingbot.logger import HummingbotLogger
 
 
 TradeStructure = namedtuple("Trade", "timestamp side price amount")
-PhemexPerpetualDerivative = ''  # To-do: cleanup. Just added so preliminaty commit is possible.
+PhemexPerpetualDerivative = ""  # To-do: cleanup. Just added so preliminaty commit is possible.
 
 
 class PhemexPerpetualAPIOrderBookDataSource(PerpetualAPIOrderBookDataSource):
@@ -30,11 +30,11 @@ class PhemexPerpetualAPIOrderBookDataSource(PerpetualAPIOrderBookDataSource):
     _mapping_initialization_lock = asyncio.Lock()
 
     def __init__(
-            self,
-            trading_pairs: List[str],
-            connector: 'PhemexPerpetualDerivative',
-            api_factory: WebAssistantsFactory,
-            domain: str = CONSTANTS.DOMAIN
+        self,
+        trading_pairs: List[str],
+        connector: "PhemexPerpetualDerivative",
+        api_factory: WebAssistantsFactory,
+        domain: str = CONSTANTS.DOMAIN,
     ):
         super().__init__(trading_pairs)
         self._connector = connector
@@ -42,47 +42,45 @@ class PhemexPerpetualAPIOrderBookDataSource(PerpetualAPIOrderBookDataSource):
         self._domain = domain
         self._trading_pairs: List[str] = trading_pairs
         self._message_queue: Dict[str, asyncio.Queue] = defaultdict(asyncio.Queue)
-        self._trade_messages_queue_key = "trades"
-        self._diff_messages_queue_key = "book"
-        self._funding_info_messages_queue_key = "market24h"
+        self._trade_messages_queue_key = "trades_p"
+        self._diff_messages_queue_key = "orderbook_p"
+        self._funding_info_messages_queue_key = "perp_market24h_pack_p.update"
         self._snapshot_messages_queue_key = "snapshot"
 
-    async def get_last_traded_prices(self,
-                                     trading_pairs: List[str],
-                                     domain: Optional[str] = None) -> Dict[str, float]:
+    async def get_last_traded_prices(self, trading_pairs: List[str], domain: Optional[str] = None) -> Dict[str, float]:
         return await self._connector.get_last_traded_prices(trading_pairs=trading_pairs)
 
     async def get_funding_info(self, trading_pair: str) -> FundingInfo:
         symbol_info: Dict[str, Any] = (await self._request_complete_funding_info(trading_pair))["result"]
         funding_info = FundingInfo(
             trading_pair=trading_pair,
-            index_price=Decimal(symbol_info["indexPrice"]),
-            mark_price=Decimal(symbol_info["markPrice"]),
+            index_price=Decimal(symbol_info["indexPriceRp"]),
+            mark_price=Decimal(symbol_info["markPriceRp"]),
             next_funding_utc_timestamp=self._next_funding_time(),
-            rate=Decimal(symbol_info["fundingRate"]),
+            rate=Decimal(symbol_info["fundingRateRr"]),
         )
         return funding_info
 
     async def _request_order_book_snapshot(self, trading_pair: str) -> Dict[str, Any]:
         ex_trading_pair = await self._connector.exchange_symbol_associated_to_pair(trading_pair=trading_pair)
 
-        params = {
-            "symbol": ex_trading_pair
-        }
+        params = {"symbol": ex_trading_pair}
 
-        data = await self._connector._api_get(
-            path_url=CONSTANTS.SNAPSHOT_REST_URL,
-            params=params)
+        data = await self._connector._api_get(path_url=CONSTANTS.SNAPSHOT_REST_URL, params=params)
         return data
 
     async def _order_book_snapshot(self, trading_pair: str) -> OrderBookMessage:
         snapshot_response: Dict[str, Any] = (await self._request_order_book_snapshot(trading_pair))["result"]
-        snapshot_msg: OrderBookMessage = OrderBookMessage(OrderBookMessageType.SNAPSHOT, {
-            "trading_pair": trading_pair,
-            "update_id": snapshot_response["sequence"],
-            "bids": snapshot_response["book"]["bids"],
-            "asks": snapshot_response["book"]["asks"]
-        }, timestamp=snapshot_response["timestamp"] // 1e9)
+        snapshot_msg: OrderBookMessage = OrderBookMessage(
+            OrderBookMessageType.SNAPSHOT,
+            {
+                "trading_pair": trading_pair,
+                "update_id": snapshot_response["sequence"],
+                "bids": snapshot_response[self._diff_messages_queue_key]["bids"],
+                "asks": snapshot_response[self._diff_messages_queue_key]["asks"],
+            },
+            timestamp=snapshot_response["timestamp"] // 1e9,
+        )
         return snapshot_msg
 
     async def _connected_websocket_assistant(self) -> WSAssistant:
@@ -126,37 +124,43 @@ class PhemexPerpetualAPIOrderBookDataSource(PerpetualAPIOrderBookDataSource):
             channel = self._diff_messages_queue_key
         elif self._trade_messages_queue_key in event_message:
             channel = self._trade_messages_queue_key
-        elif self._funding_info_messages_queue_key in event_message:
+        elif event_message["method"] == self._funding_info_messages_queue_key:
             channel = self._funding_info_messages_queue_key
         return channel
 
     async def _parse_order_book_diff_message(self, raw_message: Dict[str, Any], message_queue: asyncio.Queue):
         timestamp: float = time.time()
-        raw_message["symbol"] = await self._connector.trading_pair_associated_to_exchange_symbol(
-            raw_message["symbol"])
+        raw_message["symbol"] = await self._connector.trading_pair_associated_to_exchange_symbol(raw_message["symbol"])
         if raw_message["type"] == "incremental":
-            book = raw_message["book"]
-            order_book_message: OrderBookMessage = OrderBookMessage(OrderBookMessageType.DIFF, {
-                "trading_pair": raw_message["symbol"],
-                "update_id": raw_message["sequence"],
-                "bids": book["bids"],
-                "asks": book["asks"]
-            }, timestamp=timestamp)
+            book = raw_message[self._diff_messages_queue_key]
+            order_book_message: OrderBookMessage = OrderBookMessage(
+                OrderBookMessageType.DIFF,
+                {
+                    "trading_pair": raw_message["symbol"],
+                    "update_id": raw_message["sequence"],
+                    "bids": book["bids"],
+                    "asks": book["asks"],
+                },
+                timestamp=timestamp,
+            )
             message_queue.put_nowait(order_book_message)
 
     async def _parse_trade_message(self, raw_message: Dict[str, Any], message_queue: asyncio.Queue):
-        raw_message["symbol"] = await self._connector.trading_pair_associated_to_exchange_symbol(
-            raw_message["symbol"])
-        for trade in raw_message["trades"]:
+        raw_message["symbol"] = await self._connector.trading_pair_associated_to_exchange_symbol(raw_message["symbol"])
+        for trade in raw_message[self._trade_messages_queue_key]:
             mapped_trade = TradeStructure(*trade)
-            trade_message: OrderBookMessage = OrderBookMessage(OrderBookMessageType.TRADE, {
-                "trading_pair": raw_message["symbol"],
-                "trade_type": mapped_trade.side.upper(),
-                "trade_id": mapped_trade.timestamp,
-                "update_id": raw_message["sequence"],
-                "price": mapped_trade.price,
-                "amount": mapped_trade.amount
-            }, timestamp=mapped_trade.timestamp // 1e9)
+            trade_message: OrderBookMessage = OrderBookMessage(
+                OrderBookMessageType.TRADE,
+                {
+                    "trading_pair": raw_message["symbol"],
+                    "trade_type": mapped_trade.side.upper(),
+                    "trade_id": mapped_trade.timestamp,
+                    "update_id": raw_message["sequence"],
+                    "price": mapped_trade.price,
+                    "amount": mapped_trade.amount,
+                },
+                timestamp=mapped_trade.timestamp // 1e9,
+            )
 
             message_queue.put_nowait(trade_message)
 
@@ -178,28 +182,32 @@ class PhemexPerpetualAPIOrderBookDataSource(PerpetualAPIOrderBookDataSource):
                 await self._sleep(5.0)
 
     async def _parse_funding_info_message(self, raw_message: Dict[str, Any], message_queue: asyncio.Queue):
+        fields = raw_message.get("fields", [])
+        index_price_index = fields.index("indexRp")
+        mark_price_index = fields.index("markRp")
+        symbol_index = fields.index("symbol")
+        funding_rate_index = fields.index("fundingRateRr")
 
-        data: Dict[str, Any] = raw_message["market24h"]
-        trading_pair = await self._connector.trading_pair_associated_to_exchange_symbol(data["symbol"])
+        for data in raw_message.get("data", []):
+            trading_pair = await self._connector.trading_pair_associated_to_exchange_symbol(data[symbol_index])
 
-        if trading_pair not in self._trading_pairs:
-            return
-        funding_info = FundingInfoUpdate(
-            trading_pair=trading_pair,
-            index_price=Decimal(data["indexPrice"]),
-            mark_price=Decimal(data["markPrice"]),
-            next_funding_utc_timestamp=self._next_funding_time(),
-            rate=Decimal(data["predFundingRate"]),
-        )
+            if trading_pair not in self._trading_pairs:
+                return
+            funding_info = FundingInfoUpdate(
+                trading_pair=trading_pair,
+                index_price=Decimal(data[index_price_index]),
+                mark_price=Decimal(data[mark_price_index]),
+                next_funding_utc_timestamp=self._next_funding_time(),
+                rate=Decimal(data[funding_rate_index]),
+            )
 
         message_queue.put_nowait(funding_info)
 
     async def _request_complete_funding_info(self, trading_pair: str):
         ex_trading_pair = await self._connector.exchange_symbol_associated_to_pair(trading_pair=trading_pair)
         data = await self._connector._api_get(
-            path_url=CONSTANTS.MARK_PRICE_URL,
-            params={"symbol": ex_trading_pair},
-            is_auth_required=False)
+            path_url=CONSTANTS.MARK_PRICE_URL, params={"symbol": ex_trading_pair}, is_auth_required=False
+        )
         return data
 
     def _next_funding_time(self) -> int:
