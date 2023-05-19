69a6e37061ee1dcd6063ed2422e768b5e8547779
cardosofede <federico.cardoso.e@gmail.com>
2023-05-19 16:13:20 -0300

    (feat) add market making example

---
+++ scripts/pmm_with_position_executor.py
@@ -0,0 +1,108 @@
+import logging
+from decimal import Decimal
+from typing import List
+
+from hummingbot.core.data_type.common import OrderType, PriceType, TradeType
+from hummingbot.core.data_type.order_candidate import OrderCandidate
+from hummingbot.core.event.events import OrderFilledEvent
+from hummingbot.smart_components.position_executor.data_types import PositionConfig
+from hummingbot.smart_components.position_executor_v2.position_executor_v2 import PositionExecutorV2
+from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
+
+
+class SimplePMM(ScriptStrategyBase):
+    """
+    BotCamp Cohort: Sept 2022
+    Design Template: https://hummingbot-foundation.notion.site/Simple-PMM-63cc765486dd42228d3da0b32537fc92
+    Video: -
+    Description:
+    The bot will place two orders around the price_source (mid price or last traded price) in a trading_pair on
+    exchange, with a distance defined by the ask_spread and bid_spread. Every order_refresh_time in seconds,
+    the bot will cancel and replace the orders.
+    """
+    bid_spread = 0.08
+    ask_spread = 0.08
+    order_refresh_time = 15
+    order_amount = 0.01
+    create_timestamp = 0
+    trading_pair = "ETH-USDT"
+    exchange = "kucoin"
+    executors = []
+    # Here you can use for example the LastTrade price to use in your strategy
+    price_source = PriceType.MidPrice
+
+    markets = {exchange: {trading_pair}}
+
+    def on_tick(self):
+        if self.create_timestamp <= self.current_timestamp:
+            self.cancel_all_orders()
+            proposal: List[OrderCandidate] = self.create_proposal()
+            proposal_adjusted: List[OrderCandidate] = self.adjust_proposal_to_budget(proposal)
+            self.place_orders(proposal_adjusted)
+            self.create_timestamp = self.order_refresh_time + self.current_timestamp
+
+    def create_proposal(self) -> List[OrderCandidate]:
+        ref_price = self.connectors[self.exchange].get_price_by_type(self.trading_pair, self.price_source)
+        buy_price = ref_price * Decimal(1 - self.bid_spread)
+        sell_price = ref_price * Decimal(1 + self.ask_spread)
+
+        buy_order = OrderCandidate(trading_pair=self.trading_pair, is_maker=True, order_type=OrderType.LIMIT,
+                                   order_side=TradeType.BUY, amount=Decimal(self.order_amount), price=buy_price)
+
+        sell_order = OrderCandidate(trading_pair=self.trading_pair, is_maker=True, order_type=OrderType.LIMIT,
+                                    order_side=TradeType.SELL, amount=Decimal(self.order_amount), price=sell_price)
+
+        return [buy_order, sell_order]
+
+    def adjust_proposal_to_budget(self, proposal: List[OrderCandidate]) -> List[OrderCandidate]:
+        proposal_adjusted = self.connectors[self.exchange].budget_checker.adjust_candidates(proposal, all_or_none=True)
+        return proposal_adjusted
+
+    def place_orders(self, proposal: List[OrderCandidate]) -> None:
+        for order in proposal:
+            executor = PositionExecutorV2(self, position_config=PositionConfig(
+                trading_pair=order.trading_pair,
+                exchange=self.exchange,
+                price=order.price,
+                amount=order.amount,
+                side=order.order_side,
+                open_order_type=order.order_type,
+                take_profit=0.02,
+                stop_loss=0.04,
+                time_limit=120,
+            ))
+            self.executors.append(executor)
+
+    def place_order(self, connector_name: str, order: OrderCandidate):
+        if order.order_side == TradeType.SELL:
+            self.sell(connector_name=connector_name, trading_pair=order.trading_pair, amount=order.amount,
+                      order_type=order.order_type, price=order.price)
+        elif order.order_side == TradeType.BUY:
+            self.buy(connector_name=connector_name, trading_pair=order.trading_pair, amount=order.amount,
+                     order_type=order.order_type, price=order.price)
+
+    def cancel_all_orders(self):
+        for order in self.get_active_orders(connector_name=self.exchange):
+            self.cancel(self.exchange, order.trading_pair, order.client_order_id)
+
+    def did_fill_order(self, event: OrderFilledEvent):
+        msg = (f"{event.trade_type.name} {round(event.amount, 2)} {event.trading_pair} {self.exchange} at {round(event.price, 2)}")
+        self.log_with_clock(logging.INFO, msg)
+        self.notify_hb_app_with_timestamp(msg)
+
+    def format_status(self) -> str:
+        """
+        Displays the three candlesticks involved in the script with RSI, BBANDS and EMA.
+        """
+        if not self.ready_to_trade:
+            return "Market connectors are not ready."
+        lines = []
+
+        if len(self.executors) > 0:
+            lines.extend(["\n########################################## Executors ##########################################"])
+        for executor in self.executors:
+            lines.extend([f"|Signal id: {executor.position_config.timestamp}"])
+            lines.extend(executor.to_format_status())
+            lines.extend(["-----------------------------------------------------------------------------------------------------------"])
+
+        return "\n".join(lines)
