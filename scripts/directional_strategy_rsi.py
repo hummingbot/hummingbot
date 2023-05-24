c8425be8207644a0e4633e1fc1f2f238d99b6c91
cardosofede <federico.cardoso.e@gmail.com>
2023-05-24 20:48:46 -0300

    (feat) add rsi strategy

---
+++ scripts/directional_strategy_rsi.py
@@ -0,0 +1,48 @@
+from decimal import Decimal
+
+from hummingbot.data_feed.candles_feed.candles_factory import CandlesFactory
+from hummingbot.strategy.directional_strategy_base import DirectionalStrategyBase
+
+
+class RSI(DirectionalStrategyBase):
+    directional_strategy_name: str = "RSI"
+    # Define the trading pair and exchange that we want to use and the csv where we are going to store the entries
+    trading_pair: str = "FOOTBALL-USDT"
+    exchange: str = "binance_perpetual"
+    order_amount_usd = Decimal("15")
+    leverage = 10
+
+    # Configure the parameters for the position
+    stop_loss: float = 0.0075
+    take_profit: float = 0.015
+    time_limit: int = 60 * 55
+    trailing_stop_activation_delta = 0.004
+    trailing_stop_trailing_delta = 0.001
+
+    candles = [CandlesFactory.get_candle(connector=exchange,
+                                         trading_pair=trading_pair,
+                                         interval="1m", max_records=150)]
+    markets = {exchange: {trading_pair}}
+
+    def get_signal(self):
+        candles_df = self.get_processed_df()
+        rsi_value = candles_df.iat[-1, -1]
+        if rsi_value > 70:
+            return -1
+        elif rsi_value < 30:
+            return 1
+        else:
+            return 0
+
+    def get_processed_df(self):
+        candles_df = self.candles[0].candles_df
+        candles_df.ta.rsi(length=7, append=True)
+        return candles_df
+
+    def market_data_extra_info(self):
+        lines = []
+        columns_to_show = ["timestamp", "open", "low", "high", "close", "volume", "RSI_21"]
+        candles_df = self.get_processed_df()
+        lines.extend([f"Candles: {self.candles[0].name} | Interval: {self.candles[0].interval}\n"])
+        lines.extend(self.candles_formatted_list(candles_df, columns_to_show))
+        return lines
