68e57014cb4c96e1e4b0105ce30d739234521b91
cardosofede <federico.cardoso.e@gmail.com>
2023-05-26 15:22:09 -0300

    (feat) revert changes of trade fills pr

--- hummingbot/model/db_migration/transformations.py
+++ hummingbot/model/db_migration/transformations.py
@@ -130,21 +130,3 @@ class ConvertPriceAndAmountColumnsToBigint(DatabaseTransformation):
     @property
     def to_version(self):
         return 20220130
-
-
-class AddTradeFeeInQuote(DatabaseTransformation):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def apply(self, db_handle: SQLConnectionManager) -> SQLConnectionManager:
-        trade_fee = Column("trade_fee_in_quote", Integer, nullable=True)
-        self.add_column(db_handle.engine, "TradeFill", trade_fee, dry_run=False)
-        return db_handle
-
-    @property
-    def name(self):
-        return "AddTradeFeeInQuote"
-
-    @property
-    def to_version(self):
-        return 20230516
