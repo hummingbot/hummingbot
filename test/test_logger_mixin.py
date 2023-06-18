d2ffe2c584372c15afa843166f3e93771d998814
memento <claude.rc@gmail.com>
2023-06-18 10:13:45 -0500

    Forgot the main part ;p, adding the handler

--- test/test_logger_mixin.py
+++ test/test_logger_mixin.py
@@ -1,6 +1,8 @@
 import unittest
-from logging import LogRecord
+from logging import Logger, LogRecord
 from test.logger_mxin import LogLevel, TestLoggerMixin
+
+from hummingbot.logger import HummingbotLogger
 
 
 class TestTestLoggerMixin(unittest.TestCase):
@@ -29,6 +31,48 @@ class TestTestLoggerMixin(unittest.TestCase):
         self.assertTrue(self.logger.is_partially_logged("test", LogLevel.INFO))
         self.assertFalse(self.logger.is_partially_logged("test", LogLevel.ERROR))
         self.assertFalse(self.logger.is_partially_logged("other", LogLevel.INFO))
+
+    def test_set_loggers_single_logger(self):
+        logger = HummingbotLogger("TestLogger")
+        logger.level = LogLevel.INFO
+        self.assertNotEqual(logger.level, self.logger.level)
+        self.assertEqual(0, len(logger.handlers))
+
+        self.logger.set_loggers([logger])
+        self.assertEqual(logger.level, self.logger.level)
+        self.assertEqual(1, len(logger.handlers))
+        self.assertEqual(self.logger, logger.handlers[0])
+
+    def test_set_loggers_multiple_logger(self):
+        loggers = []
+        for i in range(5):
+            logger = HummingbotLogger(f"TestLogger{i}")
+            logger.level = LogLevel.INFO
+            loggers.append(logger)
+            self.assertNotEqual(logger.level, self.logger.level)
+            self.assertEqual(0, len(logger.handlers))
+
+        self.logger.set_loggers(loggers)
+
+        for logger in loggers:
+            self.assertEqual(logger.level, self.logger.level)
+            self.assertEqual(1, len(logger.handlers))
+            self.assertEqual(self.logger, logger.handlers[0])
+
+    def test_set_loggers_other_logger(self):
+        logger = Logger("TestLogger")
+        logger.level = LogLevel.INFO
+        self.assertNotEqual(logger.level, self.logger.level)
+        self.assertEqual(0, len(logger.handlers))
+
+        self.logger.set_loggers([logger])
+        self.assertEqual(logger.level, self.logger.level)
+        self.assertEqual(1, len(logger.handlers))
+        self.assertEqual(self.logger, logger.handlers[0])
+
+    def test_set_loggers_some_none(self):
+        loggers = [HummingbotLogger("Test"), None]
+        self.logger.set_loggers(loggers)
 
 
 if __name__ == "__main__":
