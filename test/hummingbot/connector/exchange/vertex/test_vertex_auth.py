4aa5b5015d19453b2f5280a704736d3dd5244820
Kollan House <kollanh@gmail.com>
2023-06-22 08:55:54 -0400

    (fix) for mock, updated for flake8

--- test/hummingbot/connector/exchange/vertex/test_vertex_auth.py
+++ test/hummingbot/connector/exchange/vertex/test_vertex_auth.py
@@ -12,7 +12,7 @@ class VertexAuthTests(TestCase):
     def setUp(self) -> None:
         super().setUp()
         # NOTE: RANDOM KEYS GENERATED JUST FOR UNIT TESTS
-        self.sender_address = "0x2162Db26939B9EAF0C5404217774d166056d31B5"
+        self.sender_address = "0x2162Db26939B9EAF0C5404217774d166056d31B5"  # noqa: mock
         self.private_key = "5500eb16bf3692840e04fb6a63547b9a80b75d9cbb36b43ca5662127d4c19c83"  # noqa: mock
 
         self.auth = VertexAuth(
@@ -47,22 +47,22 @@ class VertexAuthTests(TestCase):
 
     def test_sign_payload(self):
         order = Order(
-            sender="0x2162Db26939B9EAF0C5404217774d166056d31B5",
+            sender="0x2162Db26939B9EAF0C5404217774d166056d31B5",  # noqa: mock
             priceX18=26383000000000000000000,
             amount=2292000000000000000,
             expiration=1685989016166771694,
             nonce=1767924162661187978,
         )
-        contract = "0xbf16e41fb4ac9922545bfc1500f67064dc2dcc3b"
+        contract = "0xbf16e41fb4ac9922545bfc1500f67064dc2dcc3b"  # noqa: mock
         chain_id = "421613"
-        expected_signature = "0x458cb49f9c20f3f2c8f57d229ca9f33fd23556b3d5c87dbe9366e9e09ef00c43632ef996f67434f55350a9241f4bff62da7055aaa889237d33e403b482e8abab1b"
-        expected_digest = "0xaa4dadc6a1ed641eb46a22b1b58fd702e60392b8593e3fb29a5218f7f4010e69"
+        expected_signature = "0x458cb49f9c20f3f2c8f57d229ca9f33fd23556b3d5c87dbe9366e9e09ef00c43632ef996f67434f55350a9241f4bff62da7055aaa889237d33e403b482e8abab1b"  # noqa: mock
+        expected_digest = "0xaa4dadc6a1ed641eb46a22b1b58fd702e60392b8593e3fb29a5218f7f4010e69"  # noqa: mock
         signature, digest = self.auth.sign_payload(order, contract, chain_id)
         self.assertEqual(expected_signature, signature)
         self.assertEqual(expected_digest, digest)
 
     def test_generate_digest(self):
-        signable_bytes = b"\x19\x01\xb0_\xd0\xc1Co\xf9K\xb2C$*S\x8f\xd78\xac\xc3\xdcdu\xf0\xfcY\x9d9\xac\xe7\xff/\xa6)\x1fp-\xfcL\x9d\xdf\xe8\xbb\xffe\x0bJIl\x14\x94\x89\xc9{\x9af\x97\xad2\x13\x8a1\xca\x89\xfa\xd3"
-        expected_digest = "0xaa4dadc6a1ed641eb46a22b1b58fd702e60392b8593e3fb29a5218f7f4010e69"
+        signable_bytes = b"\x19\x01\xb0_\xd0\xc1Co\xf9K\xb2C$*S\x8f\xd78\xac\xc3\xdcdu\xf0\xfcY\x9d9\xac\xe7\xff/\xa6)\x1fp-\xfcL\x9d\xdf\xe8\xbb\xffe\x0bJIl\x14\x94\x89\xc9{\x9af\x97\xad2\x13\x8a1\xca\x89\xfa\xd3"  # noqa: mock
+        expected_digest = "0xaa4dadc6a1ed641eb46a22b1b58fd702e60392b8593e3fb29a5218f7f4010e69"  # noqa: mock
         digest = self.auth.generate_digest(signable_bytes)
         self.assertEqual(expected_digest, digest)
