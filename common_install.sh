49d7dfea887973365e8891969915303e44bc3122
memento <claude.rc@gmail.com>
2023-06-26 19:06:31 -0500

    (fix) Updating framework to python 3.10 with relaxed constraints to ease conda resolution

--- common_install.sh
+++ common_install.sh
@@ -5,17 +5,16 @@ find_conda_exe() {
     local -n _conda_exe=$1
     if [ "${_conda_exe} " == " " ]; then
       _conda_exe=$(( \
-        find /opt/conda/bin/conda || \
-        find ~/anaconda3/bin/conda || \
-        find /usr/local/anaconda3/bin/conda || \
-        find ~/miniconda3/bin/conda  || \
-        find /root/miniconda/bin/conda || \
-        find ~/Anaconda3/Scripts/conda) 2>/dev/null \
+        find ~ -name conda || \
+        find /opt/conda/bin -name conda || \
+        find /usr/local -name conda || \
+        find /root/*conda -name conda
+        ) 2>/dev/null \
       )
     fi
 
     if [ "${_conda_exe}_" == "_" ]; then
-        echo "Please install Anaconda w/ Python 3.7+ first"
+        echo "Please install Anaconda w/ Python 3.8.2+ first"
         echo "See: https://www.anaconda.com/distribution/"
         exit 1
     fi
@@ -26,7 +25,7 @@ get_env_file() {
     local env_dir=$(dirname ${env_file})
     local env_ext="${env_file##*.}"
 
-    local files=( $(find ${env_dir} -type f -name "*.${env_ext}") )
+    local files=( $(find ${env_dir} -type f -name "*.${env_ext}" | tac) )
     local i=1
     for file in "${files[@]}"; do
         echo "   ${i}: ${file}" >&2
@@ -85,3 +84,45 @@ check_env_name() {
   fi
 }
 
+verify_pip_packages() {
+  echo "  '-> Verifying conda alternative to pip packages" >&2
+  local install_dir="/tmp/hb_install"
+  rm -rf $install_dir
+  mkdir -p $install_dir
+  while read package; do
+    package_name=$(echo $package | cut -d'=' -f1)
+    package_version=$(echo $package | cut -d'=' -f3)
+    echo "      Searching for $package_name:$package_version" >&2
+    conda search --json $package_name | jq -r --arg pkg "$package_name" --arg ver "$package_version" '
+      if .[$pkg] == null or (.[$pkg] | map(has("version")) | all(false)) then
+        | empty
+      else
+        .[$pkg][] | select(.version) | .version  as $version
+        | ($version | split(".") | map(tonumber)) as $arrayed_version
+        | ($ver | split(".") | map(tonumber)) as $arrayed_ver
+        |  (if $arrayed_version >= $arrayed_ver then
+            $pkg + "==" + $version
+          else
+            empty
+          end) as $selected_version
+        | $selected_version
+        | halt_error
+        | .name + "=" + .version
+      end
+    ' 2>> $install_dir/conda_package_list.txt
+    echo | cat >> $install_dir/conda_package_list.txt
+  done < setup/pip_packages.txt
+  grep -v -f <(cut -d '=' -f1 $install_dir/conda_package_list.txt) setup/pip_packages.txt 2> $install_dir/updated_pip_packages.txt
+
+  #rm -rf $install_dir
+}
+#      | .[$pkg][] | select(.version) | .version as $matching_versions
+#      | "      -> Packages found for: " + $pkg + "==" + $ver + " -> " + ($matching_versions | join(", "))
+#      | (stderr | .)
+#      | map(. | tostring) as $versions
+#      | closestGreaterVersion($versions) as $closest_greater_version
+#      | "\n      -> Version selected: " + $closest_greater_version
+#      | (stderr | .)
+#      | .name + "==" + .version
+  # Update pip packages
+#    conda search --json $package_name | jq -r --arg pkg "$package_name" --arg ver "$package_version" '.[$pkg][].version | select(startswith($ver))' | head -n 1 | sed 's/^/'$package_name'==/'
