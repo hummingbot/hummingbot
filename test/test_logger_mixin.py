39c722a9e8822c09d3af994b8dffde85c7428a4e
memento <claude.rc@gmail.com>
2023-07-03 10:39:40 -0500

    (feat) Adding idea from klpanagi for a wait_for logged

--- test/test_logger_mixin.py
+++ test/test_logger_mixin.py
@@ -1,8 +1,9 @@
+import asyncio
 import unittest
 from logging import Logger, LogRecord
 from test.logger_mxin import LogLevel, TestLoggerMixin
 
-from hummingbot.logger import HummingbotLogger
+from hummingbot.logger.logger import HummingbotLogger
 
 
 class TestTestLoggerMixin(unittest.TestCase):
@@ -42,11 +43,11 @@ class TestTestLoggerMixin(unittest.TestCase):
     def test_set_loggers_single_logger(self):
         logger = HummingbotLogger("TestLogger")
         logger.level = LogLevel.INFO
-        self.assertNotEqual(logger.level, self.logger.level)
+        self.assertNotEqual(1, logger.level)
         self.assertEqual(0, len(logger.handlers))
 
         self.logger.set_loggers([logger])
-        self.assertEqual(logger.level, self.logger.level)
+        self.assertEqual(1, logger.level)
         self.assertEqual(1, len(logger.handlers))
         self.assertEqual(self.logger, logger.handlers[0])
 
@@ -56,13 +57,13 @@ class TestTestLoggerMixin(unittest.TestCase):
             logger = HummingbotLogger(f"TestLogger{i}")
             logger.level = LogLevel.INFO
             loggers.append(logger)
-            self.assertNotEqual(logger.level, self.logger.level)
+            self.assertNotEqual(1, logger.level)
             self.assertEqual(0, len(logger.handlers))
 
         self.logger.set_loggers(loggers)
 
         for logger in loggers:
-            self.assertEqual(logger.level, self.logger.level)
+            self.assertEqual(1, logger.level)
             self.assertEqual(1, len(logger.handlers))
             self.assertEqual(self.logger, logger.handlers[0])
 
@@ -73,13 +74,41 @@ class TestTestLoggerMixin(unittest.TestCase):
         self.assertEqual(0, len(logger.handlers))
 
         self.logger.set_loggers([logger])
-        self.assertEqual(logger.level, self.logger.level)
+        self.assertEqual(1, logger.level)
         self.assertEqual(1, len(logger.handlers))
         self.assertEqual(self.logger, logger.handlers[0])
 
     def test_set_loggers_some_none(self):
         loggers = [HummingbotLogger("Test"), None]
         self.logger.set_loggers(loggers)
+
+    async def test_wait_for_logged(self):
+        self.logger.log_records = []
+        record = LogRecord(name="test", level=LogLevel.INFO, pathname="", lineno=0, msg="test message", args=None,
+                           exc_info=None)
+        self.logger.handle(record)
+
+        # Test a message that has been logged
+        logged = await self.logger.wait_for_logged(LogLevel.INFO, "test message", False, wait_s=0.1)
+        self.assertIsNone(logged)
+
+        # Test a message that has not been logged
+        with self.assertRaises(asyncio.TimeoutError):
+            await self.logger.wait_for_logged(LogLevel.INFO, "other message", False, wait_s=0.1)
+
+    async def test_wait_for_logged_partial(self):
+        self.logger.log_records = []
+        record = LogRecord(name="test", level=LogLevel.INFO, pathname="", lineno=0, msg="test message", args=None,
+                           exc_info=None)
+        self.logger.handle(record)
+
+        # Test a message that has been logged
+        logged = await self.logger.wait_for_logged(LogLevel.INFO, "test", True, wait_s=0.1)
+        self.assertIsNone(logged)
+
+        # Test a message that has not been logged
+        with self.assertRaises(asyncio.TimeoutError):
+            await self.logger.wait_for_logged(LogLevel.INFO, "other", True, wait_s=0.1)
 
 
 if __name__ == "__main__":
