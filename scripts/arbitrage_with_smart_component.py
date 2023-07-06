ba3d4044c74b3246c1bdcff07c8341e829372e46
cardosofede <federico.cardoso.e@gmail.com>
2023-07-06 15:06:32 +0200

    (feat) remove buy order

--- scripts/arbitrage_with_smart_component.py
+++ scripts/arbitrage_with_smart_component.py
@@ -1,6 +1,5 @@
 from decimal import Decimal
 
-from hummingbot.core.data_type.common import OrderType
 from hummingbot.smart_components.arbitrage_executor.arbitrage_executor import ArbitrageExecutor
 from hummingbot.smart_components.arbitrage_executor.data_types import ArbitrageConfig, ExchangePair
 from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
@@ -14,18 +13,10 @@ class ArbitrageWithSmartComponent(ScriptStrategyBase):
                exchange_B: {trading_pair}}
     order_amount_usd = Decimal("12")
     active_arbitrages = []
-    buy_order_id = None
 
     def on_tick(self):
         if len(self.active_arbitrages) < 1:
             self.create_arbitrage_executor()
-        order_id = self.connectors[self.exchange_B].sell(
-            trading_pair=self.trading_pair,
-            amount=self.order_amount_usd,
-            order_type=OrderType.MARKET,
-            price=Decimal("0.99")
-        )
-        self.buy_order_id = order_id
 
     def get_arbitrage_config(self):
         price = self.connectors[self.exchange_A].get_mid_price(self.trading_pair)
