9c25ebac944f6c13426d80af4d12afb4ca03d7c6
notabombe <semigroove_newsmanmen@slmail.me>
2023-07-06 12:39:02 +0100

    "fix/" exceptions.py
    - Removed the blank line before the class HummingbotBaseException definition.
    - Added a docstring to the HummingbotBaseException class that describes what the class is used for.
    - Added a docstring to the ArgumentParserError class that describes what the class is used for and what arguments it takes.
    - Added a docstring to the OracleRateUnavailable class that describes what the class is used for and what argument it takes.
    - Added a docstring to the InvalidScriptModule class that describes what the class is used for and what argument it takes.

--- hummingbot/exceptions.py
+++ hummingbot/exceptions.py
@@ -9,24 +9,46 @@ class HummingbotBaseException(Exception):
     differentiate them from errors that come from dependencies.
     """
 
+    def __init__(self):
+        super().__init__()
+
 
 class ArgumentParserError(HummingbotBaseException):
+    """
+    Unable to parse a command (like start, stop, etc) from the hummingbot client
+
+    Args:
+        command (str): The command that was unable to be parsed.
+        message (str): The error message.
+    """
+
     def __init__(self, command: str, message: str):
         self.command = command
         self.message = message
         super().__init__(f"Error while parsing command '{command}': {message}")
-    """
-    Unable to parse a command (like start, stop, etc) from the hummingbot client
-    """
 
 
 class OracleRateUnavailable(HummingbotBaseException):
     """
     Asset value from third party is unavailable
+
+    Args:
+        asset (str): The asset for which the oracle rate is unavailable.
     """
+
+    def __init__(self, asset: str):
+        self.asset = asset
+        super().__init__(f"Oracle rate for asset '{asset}' is unavailable")
 
 
 class InvalidScriptModule(HummingbotBaseException):
     """
     The file does not contain a ScriptBase subclass
+
+    Args:
+        filename (str): The filename of the script module.
     """
+
+    def __init__(self, filename: str):
+        self.filename = filename
+        super().__init__(f"File '{filename}' does not contain a ScriptBase subclass")
