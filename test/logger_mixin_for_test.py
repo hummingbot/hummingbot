3fd5b84df00004d391b3df7052c25f7e0a801fa4
memento <claude.rc@gmail.com>
2023-07-07 16:52:41 -0500

    (fix) Setting the level and prevent erasing existing loggers. Python 3.10

--- test/logger_mixin_for_test.py
+++ test/logger_mixin_for_test.py
@@ -1,7 +1,8 @@
 import asyncio
 import logging
 from logging import Handler, LogRecord
-from typing import Callable, List, Protocol, Union
+from types import UnionType
+from typing import Callable, List, Protocol
 
 from async_timeout import timeout
 
@@ -17,20 +18,23 @@ class LogLevel:
     CRITICAL = 50
 
 
+_IntOrStr: UnionType = int | str
+
+
 class TestLoggerMixinProtocol(Protocol):
-    level: Union[int, str]
+    level: _IntOrStr
     log_records: List[LogRecord]
 
 
 class _LoggerProtocol(TestLoggerMixinProtocol, Protocol):
-    def setLevel(self, level: Union[str, LogLevel]):
+    def setLevel(self, level: _IntOrStr):
         ...
 
     def addHandler(self, handler: Handler):
         ...
 
 
-class TestLoggerMixin(Handler):
+class LoggerMixinForTest(Handler):
     """
     Test logger mixin class that can be used to capture log records during testing.
 
@@ -38,7 +42,7 @@ class TestLoggerMixin(Handler):
 
     Example usage:
     ```python
-    class MyTestCase(unittest.TestCase, TestLoggerMixin):
+    class MyTestCase(unittest.TestCase, LoggerMixinForTest):
         def test_something(self):
             self.logger.info("Testing...")
             self.assertTrue(self.is_logged("Testing...", logging.INFO))
@@ -47,57 +51,80 @@ class TestLoggerMixin(Handler):
     Attributes:
     - `level`: The default log level for the logger.
     """
-    level: Union[int, str] = LogLevel.NOTSET
+    level: _IntOrStr = LogLevel.NOTSET
 
     def _initialize(self: _LoggerProtocol):
+        """
+        Initialize the test logger mixin by setting the default log level and initializing the log records list.
+        """
+        self.level = 1
         self.log_records: List[LogRecord] = []
 
     @staticmethod
-    def _to_loglevel(log_level: Union[str, int]) -> str:
+    def _to_loglevel(log_level: _IntOrStr) -> str:
+        """
+        Convert a log level to a string.
+        :params int | str log_level: The log level to convert.
+        """
         if isinstance(log_level, int):
             log_level = logging.getLevelName(log_level)
         return log_level
 
-    def set_loggers(self, loggers: List[HummingbotLogger]):
+    def set_loggers(self, loggers: List[HummingbotLogger] | HummingbotLogger):
         """
         Set up the test logger mixin by adding the test logger to the provided loggers list.
+        :params List[HummingbotLogger] | HummingbotLogger loggers: The loggers to add to the LoggerMixinForTest.
         """
         # __init__() may not be called if the class is used as a mixin
-        # We forcefully create or initialize the log_records attribute here
-        self._initialize()
+        if not hasattr(self, "log_records"):
+            self._initialize()
+
+        if isinstance(loggers, HummingbotLogger):
+            loggers = [loggers]
+
         for logger in loggers:
             if logger is not None:
-                logger.setLevel(1)
+                logger.setLevel(self.level)
                 logger.addHandler(self)
 
     def handle(self, record: LogRecord):
         """
         Handle a log record by appending it to the log records list.
+        :params LogRecord record: The log record to handle.
         """
         self.log_records.append(record)
 
-    def is_logged(self, log_level: Union[str, int], message: str):
+    def is_logged(self, log_level: _IntOrStr, message: str):
         """
         Check if a certain message has been logged at a certain level.
+        :params int | str log_level: The log level to check.
+        :params str message: The message to check.
         """
         log_level = self._to_loglevel(log_level)
         return any([record.getMessage() == message and record.levelname == log_level for record in self.log_records])
 
-    def is_partially_logged(self, log_level: Union[str, int], message: str):
+    def is_partially_logged(self, log_level: _IntOrStr, message: str):
         """
-        Check if a part of a certain message has been logged at a certain level.
+        Check if a certain message has been 'partially' logged at a certain level.
+        This is useful for checking if a message has been logged with a dynamic value.
+        :params int | str log_level: The log level to check.
+        :params str message: The message to check.
         """
         log_level = self._to_loglevel(log_level)
         return any([message in record.getMessage() and record.levelname == log_level for record in self.log_records])
 
     async def wait_for_logged(self,
-                              log_level: str,
+                              log_level: _IntOrStr,
                               message: str,
                               partial: bool = False,
                               wait_s: float = 3) -> None:
         """
         Wait for a certain message to be logged at a certain level.
-        """
+        :params int | str log_level: The log level to check.
+        :params str message: The message to check.
+        :params bool partial: Whether to check if the message is partially logged.
+        :params float wait_s: The number of seconds to wait before timing out.
+                """
         log_level = self._to_loglevel(log_level)
         log_method: Callable[[str | int, str], bool] = self.is_partially_logged if partial else self.is_logged
         try:
--- test/test_logger_mixin_for_test.py
+++ test/test_logger_mixin_for_test.py
@@ -1,14 +1,14 @@
 import asyncio
 import unittest
 from logging import Logger, LogRecord
-from test.logger_mxin import LogLevel, TestLoggerMixin
+from test.logger_mixin_for_test import LoggerMixinForTest, LogLevel
 
 from hummingbot.logger import HummingbotLogger
 
 
 class TestTestLoggerMixin(unittest.TestCase):
     def setUp(self):
-        self.logger = TestLoggerMixin()
+        self.logger = LoggerMixinForTest()
 
     def test_handle(self):
         self.logger.log_records = []
@@ -82,33 +82,39 @@ class TestTestLoggerMixin(unittest.TestCase):
         loggers = [HummingbotLogger("Test"), None]
         self.logger.set_loggers(loggers)
 
-    async def test_wait_for_logged(self):
-        self.logger.log_records = []
-        record = LogRecord(name="test", level=LogLevel.INFO, pathname="", lineno=0, msg="test message", args=None,
-                           exc_info=None)
-        self.logger.handle(record)
+    def test_wait_for_logged(self):
+        async def async_test():
+            self.logger.log_records = []
+            record = LogRecord(name="test", level=LogLevel.INFO, pathname="", lineno=0, msg="test message", args=None,
+                               exc_info=None)
+            self.logger.handle(record)
 
-        # Test a message that has been logged
-        logged = await self.logger.wait_for_logged(LogLevel.INFO, "test message", False, wait_s=0.1)
-        self.assertIsNone(logged)
+            # Test a message that has been logged
+            logged = await self.logger.wait_for_logged(LogLevel.INFO, "test message", False, wait_s=0.1)
+            self.assertIsNone(logged)
 
-        # Test a message that has not been logged
-        with self.assertRaises(asyncio.TimeoutError):
-            await self.logger.wait_for_logged(LogLevel.INFO, "other message", False, wait_s=0.1)
+            # Test a message that has not been logged
+            with self.assertRaises(asyncio.TimeoutError):
+                await self.logger.wait_for_logged(LogLevel.INFO, "other message", False, wait_s=0.1)
 
-    async def test_wait_for_logged_partial(self):
-        self.logger.log_records = []
-        record = LogRecord(name="test", level=LogLevel.INFO, pathname="", lineno=0, msg="test message", args=None,
-                           exc_info=None)
-        self.logger.handle(record)
+        asyncio.get_event_loop().run_until_complete(async_test())
 
-        # Test a message that has been logged
-        logged = await self.logger.wait_for_logged(LogLevel.INFO, "test", True, wait_s=0.1)
-        self.assertIsNone(logged)
+    def test_wait_for_logged_partial(self):
+        async def async_test():
+            self.logger.log_records = []
+            record = LogRecord(name="test", level=LogLevel.INFO, pathname="", lineno=0, msg="test message", args=None,
+                               exc_info=None)
+            self.logger.handle(record)
 
-        # Test a message that has not been logged
-        with self.assertRaises(asyncio.TimeoutError):
-            await self.logger.wait_for_logged(LogLevel.INFO, "other", True, wait_s=0.1)
+            # Test a message that has been logged
+            logged = await self.logger.wait_for_logged(LogLevel.INFO, "test", True, wait_s=0.1)
+            self.assertIsNone(logged)
+
+            # Test a message that has not been logged
+            with self.assertRaises(asyncio.TimeoutError):
+                await self.logger.wait_for_logged(LogLevel.INFO, "other", True, wait_s=0.1)
+
+        asyncio.get_event_loop().run_until_complete(async_test())
 
 
 if __name__ == "__main__":
