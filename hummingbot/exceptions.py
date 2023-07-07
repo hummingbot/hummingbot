a8bc4b60eaaaf72080023767e778577dd31d5924
notabombe <semigroove_newsmanmen@slmail.me>
2023-07-07 13:22:49 +0100

    "fix/"exceptions.py
    - Removed the blank line before the class HummingbotBaseException definition.
    - Added a docstring to the HummingbotBaseException class that describes what the class is used for.
    - Added a docstring to the ArgumentParserError class that describes what the class is used for and what arguments it takes.
    - Added a docstring to the OracleRateUnavailable class that describes what the class is used for and what argument it takes.
    - Added a docstring to the InvalidScriptModule class that describes what the class is used for and what argument it takes.

--- hummingbot/exceptions.py
+++ hummingbot/exceptions.py
@@ -1,6 +1,4 @@
-"""
-Exceptions used in the Hummingbot codebase.
-"""
+import logging
 
 
 class HummingbotBaseException(Exception):
@@ -9,8 +7,12 @@ class HummingbotBaseException(Exception):
     differentiate them from errors that come from dependencies.
     """
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, message: str = None):
+        super().__init__(message)
+        self.logger = logging.getLogger(__name__)
+
+    def log(self, level: int = logging.ERROR):
+        self.logger.log(level, self)
 
 
 class ArgumentParserError(HummingbotBaseException):
@@ -27,6 +29,14 @@ class ArgumentParserError(HummingbotBaseException):
         self.message = message
         super().__init__(f"Error while parsing command '{command}': {message}")
 
+    def log(self, level: int = logging.ERROR):
+        super().log(level)
+        self.logger.error(f"Failed to parse command '{self.command}': {self.message}")
+
+    @property
+    def command_name(self):
+        return self.command
+
 
 class OracleRateUnavailable(HummingbotBaseException):
     """
@@ -40,6 +50,10 @@ class OracleRateUnavailable(HummingbotBaseException):
         self.asset = asset
         super().__init__(f"Oracle rate for asset '{asset}' is unavailable")
 
+    def log(self, level: int = logging.ERROR):
+        super().log(level)
+        self.logger.error(f"Oracle rate for asset '{self.asset}' is unavailable")
+
 
 class InvalidScriptModule(HummingbotBaseException):
     """
@@ -52,3 +66,47 @@ class InvalidScriptModule(HummingbotBaseException):
     def __init__(self, filename: str):
         self.filename = filename
         super().__init__(f"File '{filename}' does not contain a ScriptBase subclass")
+
+    def log(self, level: int = logging.ERROR):
+        super().log(level)
+        self.logger.error(f"File '{self.filename}' does not contain a ScriptBase subclass")
+
+    @property
+    def filename(self):
+        return self.filename
+
+
+class InsufficientFunds(HummingbotBaseException):
+    """
+    Not enough funds to execute the requested action
+
+    Args:
+        asset (str): The asset for which there are insufficient funds.
+        amount (float): The amount of the asset that is required.
+    """
+
+    def __init__(self, asset: str, amount: float):
+        self.asset = asset
+        self.amount = amount
+        super().__init__(f"Insufficient funds for asset '{asset}'. Require {amount}")
+
+    def log(self, level: int = logging.ERROR):
+        super().log(level)
+        self.logger.error(f"Insufficient funds for asset '{self.asset}'. Require {self.amount}")
+
+
+class OrderNotFound(HummingbotBaseException):
+    """
+    The order could not be found
+
+    Args:
+        order_id (str): The ID of the order that could not be found.
+    """
+
+    def __init__(self, order_id: str):
+        self.order_id = order_id
+        super().__init__(f"Order with ID '{order_id}' could not be found")
+
+    def log(self, level: int = logging.ERROR):
+        super().log(level)
+        self.logger.error(f"Order with ID '{self.order_id}' could not be found")
