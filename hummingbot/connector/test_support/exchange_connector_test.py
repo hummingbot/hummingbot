dcf1a0c9a87bad1309733caf75dec926eabcb12b
cardosofede <federico.cardoso.e@gmail.com>
2023-07-10 20:24:21 +0200

    (feat) adapt exchange connector test

--- hummingbot/connector/test_support/exchange_connector_test.py
+++ hummingbot/connector/test_support/exchange_connector_test.py
@@ -801,7 +801,9 @@ class AbstractExchangeConnectorTests:
             self.assertTrue(
                 self.is_logged(
                     "WARNING",
-                    "Buy order amount 0 is lower than the minimum order size 0.01. The order will not be created."
+                    "Buy order amount 0.0001 is lower than the minimum order "
+                    "size 0.01. The order will not be created, increase the "
+                    "amount to be higher than the minimum order size."
                 )
             )
             self.assertTrue(
