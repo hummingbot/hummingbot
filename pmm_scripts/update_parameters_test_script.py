a9c6c52de5fa15f9ab3db4eddf129e220395651e
notabombe <semigroove_newsmanmen@slmail.me>
2023-07-15 12:57:40 +0100

    "Refactor/" update_parameters_test_script.py
    Although the code appears to be correct and complete, here are a few suggestions to improve its readability and maintainability:
    
    Add type hints to the method signatures and variables to improve code understanding and help with type checking.
    
    Replace the explicit import of Decimal with a wildcard import (*) from the decimal module to avoid cluttering the code with repetitive imports.
    
    Consider using a dictionary or configuration file instead of explicitly setting each attribute value in the on_tick method. This would make it easier to modify the parameters and add new ones without modifying the code.
    
    Consider wrapping the code inside a try-except block to handle any potential exceptions and provide more robust error handling.
    
    Consider adding docstrings to document the purpose and functionality of the class and methods. This would make it easier for other developers to understand and use the code.
    
    Here's an updated version of the code with some of these suggestions implemented:

--- pmm_scripts/update_parameters_test_script.py
+++ pmm_scripts/update_parameters_test_script.py
@@ -1,4 +1,4 @@
-from decimal import Decimal
+from decimal import *
 from hummingbot.pmm_script.pmm_script_base import PMMScriptBase
 
 
@@ -9,26 +9,34 @@ class UpdateParametersTestPMMScript(PMMScriptBase):
 
     def __init__(self):
         super().__init__()
-        self._has_updated = False
+        self._has_updated: bool = False
 
-    def on_tick(self):
+    def on_tick(self) -> None:
+        """
+        Called periodically to update the pmm_parameters.
+        """
         if len(self.mid_prices) >= 5 and not self._has_updated:
-            self.pmm_parameters.buy_levels = 1
-            self.pmm_parameters.sell_levels = 2
-            self.pmm_parameters.order_levels = 3
-            self.pmm_parameters.bid_spread = Decimal("0.1")
-            self.pmm_parameters.ask_spread = Decimal("0.2")
-            self.pmm_parameters.hanging_orders_cancel_pct = Decimal("0.3")
-            self.pmm_parameters.hanging_orders_enabled = True
-            self.pmm_parameters.filled_order_delay = 50.0
-            self.pmm_parameters.order_refresh_tolerance_pct = Decimal("0.01")
-            self.pmm_parameters.order_refresh_time = 10.0
-            self.pmm_parameters.order_level_amount = Decimal("4")
-            self.pmm_parameters.order_level_spread = Decimal("0.05")
-            self.pmm_parameters.order_amount = Decimal("20")
-            self.pmm_parameters.inventory_skew_enabled = True
-            self.pmm_parameters.inventory_range_multiplier = Decimal("2")
-            self.pmm_parameters.inventory_target_base_pct = Decimal("0.6")
-            self.pmm_parameters.order_override = {"order_1": ["buy", Decimal("0.5"), Decimal("100")],
-                                                  "order_2": ["sell", Decimal("0.55"), Decimal("101")], }
+            pmm_parameters = {
+                "buy_levels": 1,
+                "sell_levels": 2,
+                "order_levels": 3,
+                "bid_spread": Decimal("0.1"),
+                "ask_spread": Decimal("0.2"),
+                "hanging_orders_cancel_pct": Decimal("0.3"),
+                "hanging_orders_enabled": True,
+                "filled_order_delay": 50.0,
+                "order_refresh_tolerance_pct": Decimal("0.01"),
+                "order_refresh_time": 10.0,
+                "order_level_amount": Decimal("4"),
+                "order_level_spread": Decimal("0.05"),
+                "order_amount": Decimal("20"),
+                "inventory_skew_enabled": True,
+                "inventory_range_multiplier": Decimal("2"),
+                "inventory_target_base_pct": Decimal("0.6"),
+                "order_override": {
+                    "order_1": ["buy", Decimal("0.5"), Decimal("100")],
+                    "order_2": ["sell", Decimal("0.55"), Decimal("101")],
+                },
+            }
+            self.pmm_parameters.update(pmm_parameters)
             self._has_updated = True
