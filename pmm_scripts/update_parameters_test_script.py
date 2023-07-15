3801682b6c43c1982c2a17400c27a1da5e2424fe
notabombe <semigroove_newsmanmen@slmail.me>
2023-07-15 16:30:04 +0100

    "Refactor/" update_parameters_test_script.py
    Suggestions
    Although the code appears to be correct and complete, here are a few suggestions to improve its readability and maintainability:
    
    Add type hints to the method signatures and variables to improve code understanding and help with type checking.
    
    Replace the explicit import of Decimal with a wildcard import (*) from the decimal module to avoid cluttering the code with repetitive imports.
    
    Consider using a dictionary or configuration file instead of explicitly setting each attribute value in the on_tick method. This would make it easier to modify the parameters and add new ones without modifying the code.
    
    Consider wrapping the code inside a try-except block to handle any potential exceptions and provide more robust error handling.
    
    Consider adding docstrings to document the purpose and functionality of the class and methods. This would make it easier for other developers to understand and use the code.
    
    Here's an updated version of the code with some of these suggestions implemented:

--- pmm_scripts/update_parameters_test_script.py
+++ pmm_scripts/update_parameters_test_script.py
@@ -1,10 +1,11 @@
-from decimal import Decimal
-from hummingbot.pmm_scripts.pmm_script_base import PMMScriptBase
+import decimal
+
+from hummingbot.pmm_script.pmm_script_base import PMMScriptBase
 
 
 class UpdateParametersTestPMMScript(PMMScriptBase):
     """
-    This PMM script is intended for unit testing purposes only.
+    This PMM script is intended for unit testing purpose only.
     """
 
     def __init__(self):
@@ -20,22 +21,22 @@ class UpdateParametersTestPMMScript(PMMScriptBase):
                 "buy_levels": 1,
                 "sell_levels": 2,
                 "order_levels": 3,
-                "bid_spread": Decimal("0.1"),
-                "ask_spread": Decimal("0.2"),
-                "hanging_orders_cancel_pct": Decimal("0.3"),
+                "bid_spread": decimal.Decimal("0.1"),
+                "ask_spread": decimal.Decimal("0.2"),
+                "hanging_orders_cancel_pct": decimal.Decimal("0.3"),
                 "hanging_orders_enabled": True,
                 "filled_order_delay": 50.0,
-                "order_refresh_tolerance_pct": Decimal("0.01"),
+                "order_refresh_tolerance_pct": decimal.Decimal("0.01"),
                 "order_refresh_time": 10.0,
-                "order_level_amount": Decimal("4"),
-                "order_level_spread": Decimal("0.05"),
-                "order_amount": Decimal("20"),
+                "order_level_amount": decimal.Decimal("4"),
+                "order_level_spread": decimal.Decimal("0.05"),
+                "order_amount": decimal.Decimal("20"),
                 "inventory_skew_enabled": True,
-                "inventory_range_multiplier": Decimal("2"),
-                "inventory_target_base_pct": Decimal("0.6"),
+                "inventory_range_multiplier": decimal.Decimal("2"),
+                "inventory_target_base_pct": decimal.Decimal("0.6"),
                 "order_override": {
-                    "order_1": ["buy", Decimal("0.5"), Decimal("100")],
-                    "order_2": ["sell", Decimal("0.55"), Decimal("101")],
+                    "order_1": ["buy", decimal.Decimal("0.5"), decimal.Decimal("100")],
+                    "order_2": ["sell", decimal.Decimal("0.55"), decimal.Decimal("101")],
                 },
             }
             self.pmm_parameters.update(pmm_parameters)
