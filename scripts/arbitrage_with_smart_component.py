e10da8f8bdd2a09b6ec48f10246051a4d1ae40f6
Michael Feng <mifeng@gmail.com>
2023-07-24 15:26:39 -0400

    (feat) removed rate oracle and changed order amount

--- scripts/arbitrage_with_smart_component.py
+++ scripts/arbitrage_with_smart_component.py
@@ -1,18 +1,19 @@
 from decimal import Decimal
 
-from hummingbot.core.rate_oracle.rate_oracle import RateOracle
 from hummingbot.smart_components.arbitrage_executor.arbitrage_executor import ArbitrageExecutor
 from hummingbot.smart_components.arbitrage_executor.data_types import ArbitrageConfig, ExchangePair
 from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
 
 
 class ArbitrageWithSmartComponent(ScriptStrategyBase):
-    exchange_pair_1 = ExchangePair(exchange="binance", trading_pair="MATIC-USDT")
+    # Parameters
+    exchange_pair_1 = ExchangePair(exchange="kucoin", trading_pair="MATIC-USDT")
     exchange_pair_2 = ExchangePair(exchange="uniswap_polygon_mainnet", trading_pair="WMATIC-USDT")
+    order_amount = Decimal("10")  # in base asset
+    min_profitability = Decimal("0.004")
+
     markets = {exchange_pair_1.exchange: {exchange_pair_1.trading_pair},
                exchange_pair_2.exchange: {exchange_pair_2.trading_pair}}
-    order_amount_usd = Decimal("35")
-    min_profitability = Decimal("0.004")
     active_buy_arbitrages = []
     active_sell_arbitrages = []
 
@@ -39,24 +40,17 @@ class ArbitrageWithSmartComponent(ScriptStrategyBase):
             arbitrage.terminate_control_loop()
 
     def create_arbitrage_executor(self, buying_exchange_pair: ExchangePair, selling_exchange_pair: ExchangePair):
-        base_usd_pair = buying_exchange_pair.trading_pair.split("-")[0] + "-USDT"
         try:
-            # Harcoded for now since we don't have a price oracle for WMATIC
-            # TODO: Add a good rate oracle source
-            base_usd_pair_temp = base_usd_pair.replace("W", "")
-            price = RateOracle.get_instance().get_pair_rate(base_usd_pair_temp)
-            order_amount = self.order_amount_usd / price
-
             base_asset_for_selling_exchange = self.connectors[selling_exchange_pair.exchange].get_available_balance(
                 selling_exchange_pair.trading_pair.split("-")[0])
-            if order_amount > base_asset_for_selling_exchange:
+            if self.order_amount > base_asset_for_selling_exchange:
                 self.logger().info(f"Insufficient balance in exchange {selling_exchange_pair.exchange}"
                                    f"for sell {selling_exchange_pair.trading_pair.split('-')[0]} Actual: {base_asset_for_selling_exchange}")
                 return
 
             quote_asset_for_buying_exchange = self.connectors[buying_exchange_pair.exchange].get_available_balance(
                 buying_exchange_pair.trading_pair.split("-")[1])
-            if self.order_amount_usd > quote_asset_for_buying_exchange:
+            if self.order_amount > quote_asset_for_buying_exchange:
                 self.logger().info(f"Insufficient balance in exchange {buying_exchange_pair.exchange} "
                                    f"for buy {buying_exchange_pair.trading_pair.split('-')[1]} Actual: {quote_asset_for_buying_exchange}")
                 return
@@ -64,14 +58,14 @@ class ArbitrageWithSmartComponent(ScriptStrategyBase):
             arbitrage_config = ArbitrageConfig(
                 buying_market=buying_exchange_pair,
                 selling_market=selling_exchange_pair,
-                order_amount=order_amount,
+                order_amount=self.order_amount,
                 min_profitability=self.min_profitability,
             )
             arbitrage_executor = ArbitrageExecutor(strategy=self,
                                                    arbitrage_config=arbitrage_config)
             return arbitrage_executor
         except Exception:
-            self.logger().error(f"Error getting price for {base_usd_pair}")
+            self.logger().error(f"Error creating executor to buy on {buying_exchange_pair.exchange} and sell on {selling_exchange_pair.exchange}")
 
     def format_status(self) -> str:
         status = []
