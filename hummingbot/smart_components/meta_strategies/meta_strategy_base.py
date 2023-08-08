6940749db0378ce42216b5e76547aa4772d19e77
cardosofede <federico.cardoso.e@gmail.com>
2023-08-08 17:52:37 +0200

    (feat) add meta strategy base

---
+++ hummingbot/smart_components/meta_strategies/meta_strategy_base.py
@@ -0,0 +1,105 @@
+import asyncio
+import datetime
+import os
+from typing import Generic, TypeVar
+
+import pandas as pd
+from pydantic import BaseModel
+
+from hummingbot import data_path
+from hummingbot.core.data_type.common import OrderType, PositionAction, PositionSide
+from hummingbot.core.utils.async_utils import safe_ensure_future
+from hummingbot.smart_components.executors.position_executor.data_types import PositionConfig
+from hummingbot.smart_components.executors.position_executor.position_executor import PositionExecutor
+from hummingbot.smart_components.meta_strategies.data_types import MetaStrategyMode, MetaStrategyStatus
+from hummingbot.smart_components.meta_strategies.market_making.market_making_strategy_base import OrderLevel
+from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
+
+ConfigType = TypeVar("ConfigType", bound=BaseModel)
+
+
+class MetaStrategyBase(Generic[ConfigType]):
+    def __init__(self, config: ConfigType, mode: MetaStrategyMode = MetaStrategyMode.LIVE):
+        self.config = config
+        self.mode = mode
+
+    def get_csv_prefix(self) -> str:
+        raise f"{self.config.strategy_name}"
+
+    def get_order_levels(self) -> list[OrderLevel]:
+        raise NotImplementedError
+
+
+class MetaExecutorBase:
+    def __init__(self, strategy: ScriptStrategyBase, meta_strategy: MetaStrategyBase, update_interval: float = 1.0):
+        self.strategy = strategy
+        self.ms = meta_strategy
+        self.update_interval = update_interval
+        self.terminated = asyncio.Event()
+        self.level_executors = {level.level_id: None for level in self.ms.get_order_levels()}
+        self.status = MetaStrategyStatus.NOT_STARTED
+
+    def start(self):
+        safe_ensure_future(self.control_loop())
+
+    def on_stop(self):
+        pass
+
+    def on_start(self):
+        pass
+
+    async def control_task(self):
+        raise NotImplementedError
+
+    def get_csv_path(self) -> str:
+        today = datetime.datetime.today()
+        csv_path = data_path() + f"{self.ms.get_csv_prefix()}/{today.day:02d}-{today.month:02d}-{today.year}.csv"
+        return csv_path
+
+    def store_executor(self, executor: PositionExecutor, order_level: str):
+        if executor:
+            csv_path = self.get_csv_path()
+            executor_data = executor.to_json()
+            if not os.path.exists(csv_path):
+                headers = executor_data.keys()
+                df_header = pd.DataFrame(columns=headers)
+                df_header.to_csv(csv_path, mode='a', header=True, index=False)
+            df = pd.DataFrame([executor_data])
+            df.to_csv(csv_path, mode='a', header=False, index=False)
+            self.level_executors[order_level] = None
+
+    def create_executor(self, position_config: PositionConfig, order_level: str):
+        executor = PositionExecutor(self.strategy, position_config)
+        self.level_executors[order_level] = executor
+
+    async def control_loop(self):
+        self.on_start()
+        self.status = MetaStrategyStatus.ACTIVE
+        while not self.terminated.is_set():
+            await self.control_task()
+            await asyncio.sleep(self.update_interval)
+        self.status = MetaStrategyStatus.TERMINATED
+        self.on_stop()
+
+    def terminate_control_loop(self):
+        self.terminated.set()
+
+    def close_open_positions(self, connector_name: str = None, trading_pair: str = None):
+        # we are going to close all the open positions when the bot stops
+        connector = self.strategy.connectors[connector_name]
+        for pos_key, position in connector.account_positions.items():
+            if position.trading_pair == trading_pair:
+                if position.position_side == PositionSide.LONG:
+                    self.sell(connector_name=connector_name,
+                              trading_pair=position.trading_pair,
+                              amount=abs(position.amount),
+                              order_type=OrderType.MARKET,
+                              price=connector.get_mid_price(position.trading_pair),
+                              position_action=PositionAction.CLOSE)
+                elif position.position_side == PositionSide.SHORT:
+                    self.buy(connector_name=connector_name,
+                             trading_pair=position.trading_pair,
+                             amount=abs(position.amount),
+                             order_type=OrderType.MARKET,
+                             price=connector.get_mid_price(position.trading_pair),
+                             position_action=PositionAction.CLOSE)
