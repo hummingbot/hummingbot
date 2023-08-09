3ed6dac3d5be4a53be15b6aab749c589cf9a17a8
cardosofede <federico.cardoso.e@gmail.com>
2023-08-09 16:16:12 +0200

    (feat) improve dman strategy

--- hummingbot/smart_components/meta_strategies/market_making/strategies/dman_v1.py
+++ hummingbot/smart_components/meta_strategies/market_making/strategies/dman_v1.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 
 import pandas_ta as ta  # noqa: F401
 
+from hummingbot.core.data_type.common import TradeType
 from hummingbot.smart_components.executors.position_executor.data_types import PositionConfig, TrailingStop
 from hummingbot.smart_components.executors.position_executor.position_executor import PositionExecutor
 from hummingbot.smart_components.meta_strategies.data_types import MetaStrategyMode, OrderLevel
@@ -31,7 +32,7 @@ class DMan(MarketMakingStrategyBase):
         self.config = config
 
     def early_stop_condition(self, executor: PositionExecutor) -> bool:
-        raise False
+        return False
 
     def get_close_price(self, connector: str, trading_pair: str):
         """
@@ -39,7 +40,7 @@ class DMan(MarketMakingStrategyBase):
         """
         candles = self.get_candles_by_connector_trading_pair(connector, trading_pair)
         first_candle = list(candles.values())[0]
-        return first_candle.candles_df["close"].iloc[-1]
+        return Decimal(first_candle.candles_df["close"].iloc[-1])
 
     def get_candles_with_price_and_spread_multipier(self):
         """
@@ -53,8 +54,8 @@ class DMan(MarketMakingStrategyBase):
         # natr = ta.natr(candles_df["high"], candles_df["low"], candles_df["close"], length=self.config.natr_length)
 
         # candles_df["spread_multiplier"] = natr.iloc[-1]
-        candles_df["spread_multiplier"] = 1
-        candles_df["price_multiplier"] = 1
+        candles_df["spread_multiplier"] = 0.01
+        candles_df["price_multiplier"] = 0.002
         return candles_df
 
     def get_price_and_spread_multiplier(self):
@@ -62,7 +63,7 @@ class DMan(MarketMakingStrategyBase):
         Gets the price and spread multiplier from the last candlestick.
         """
         candles_df = self.get_candles_with_price_and_spread_multipier()
-        return candles_df["price_multiplier"].iloc[-1], candles_df["spread_multiplier"].iloc[-1]
+        return Decimal(candles_df["price_multiplier"].iloc[-1]), Decimal(candles_df["spread_multiplier"].iloc[-1])
 
     def get_position_config(self, order_level: OrderLevel) -> PositionConfig:
         """
@@ -74,7 +75,7 @@ class DMan(MarketMakingStrategyBase):
         price_multiplier, spread_multiplier = self.get_price_and_spread_multiplier()
 
         price_adjusted = close_price * (1 + price_multiplier)
-        side_multiplier = -1 if order_level.side == "buy" else 1
+        side_multiplier = -1 if order_level.side == TradeType.BUY else 1
         order_price = price_adjusted * (1 + order_level.spread_factor * spread_multiplier * side_multiplier)
 
         position_config = PositionConfig(
@@ -92,6 +93,7 @@ class DMan(MarketMakingStrategyBase):
             trailing_stop=TrailingStop(
                 activation_price_delta=order_level.trailing_stop_activation_price_delta,
                 trailing_delta=order_level.trailing_stop_trailing_delta,
-            )
+            ),
+            leverage=self.config.leverage
         )
         return position_config
