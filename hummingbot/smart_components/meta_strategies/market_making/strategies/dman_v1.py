1b872b42b3e893c87284d3b0ee3feeba0d554461
cardosofede <federico.cardoso.e@gmail.com>
2023-08-09 17:59:49 +0200

    (feat) remove methods abstracted and add base ones

--- hummingbot/smart_components/meta_strategies/market_making/strategies/dman_v1.py
+++ hummingbot/smart_components/meta_strategies/market_making/strategies/dman_v1.py
@@ -27,22 +27,37 @@ class DMan(MarketMakingStrategyBase):
     """
     Directional Market Making Strategy
     """
+
     def __init__(self, config: DManConfig, mode: MetaStrategyMode = MetaStrategyMode.LIVE):
         super().__init__(config, mode)
         self.config = config
 
+    def refresh_order_condition(self, executor: PositionExecutor) -> bool:
+        """
+        Checks if the order needs to be refreshed.
+        You can reimplement this method to add more conditions.
+        """
+        if executor.position_config.timestamp + self.config.order_refresh_time > time.time():
+            return False
+        return True
+
     def early_stop_condition(self, executor: PositionExecutor) -> bool:
+        """
+        If an executor has an active position, should we close it based on a condition.
+        """
         return False
 
-    def get_close_price(self, connector: str, trading_pair: str):
+    def cooldown_condition(self, executor: PositionExecutor) -> bool:
         """
-        Gets the close price of the last candlestick.
+        After finishing an order, the executor will be in cooldown for a certain amount of time.
+        This prevents the executor from creating a new order immediately after finishing one and execute a lot
+        of orders in a short period of time from the same side.
         """
-        candles = self.get_candles_by_connector_trading_pair(connector, trading_pair)
-        first_candle = list(candles.values())[0]
-        return Decimal(first_candle.candles_df["close"].iloc[-1])
+        if executor.position_config.timestamp + self.config.cooldown_time > time.time():
+            return True
+        return False
 
-    def get_candles_with_price_and_spread_multipier(self):
+    def get_candles_with_price_and_spread_multipiers(self):
         """
         Gets the price and spread multiplier from the last candlestick.
         """
@@ -57,13 +72,6 @@ class DMan(MarketMakingStrategyBase):
         candles_df["spread_multiplier"] = 0.01
         candles_df["price_multiplier"] = 0.002
         return candles_df
-
-    def get_price_and_spread_multiplier(self):
-        """
-        Gets the price and spread multiplier from the last candlestick.
-        """
-        candles_df = self.get_candles_with_price_and_spread_multipier()
-        return Decimal(candles_df["price_multiplier"].iloc[-1]), Decimal(candles_df["spread_multiplier"].iloc[-1])
 
     def get_position_config(self, order_level: OrderLevel) -> PositionConfig:
         """
@@ -84,15 +92,15 @@ class DMan(MarketMakingStrategyBase):
             exchange=self.config.exchange,
             side=order_level.side,
             amount=amount,
-            take_profit=order_level.take_profit,
-            stop_loss=order_level.stop_loss,
-            time_limit=order_level.time_limit,
+            take_profit=order_level.triple_barrier_conf.take_profit,
+            stop_loss=order_level.triple_barrier_conf.stop_loss,
+            time_limit=order_level.triple_barrier_conf.time_limit,
             entry_price=Decimal(order_price),
-            open_order_type=order_level.open_order_type,
-            take_profit_order_type=order_level.take_profit_order_type,
+            open_order_type=order_level.triple_barrier_conf.open_order_type,
+            take_profit_order_type=order_level.triple_barrier_conf.take_profit_order_type,
             trailing_stop=TrailingStop(
-                activation_price_delta=order_level.trailing_stop_activation_price_delta,
-                trailing_delta=order_level.trailing_stop_trailing_delta,
+                activation_price_delta=order_level.triple_barrier_conf.trailing_stop_activation_price_delta,
+                trailing_delta=order_level.triple_barrier_conf.trailing_stop_trailing_delta,
             ),
             leverage=self.config.leverage
         )
