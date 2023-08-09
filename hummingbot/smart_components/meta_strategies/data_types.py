1278342997ca6a76eb8eded8bd2a33e301e71325
cardosofede <federico.cardoso.e@gmail.com>
2023-08-09 12:49:35 +0200

    (feat) add level id property

--- hummingbot/smart_components/meta_strategies/data_types.py
+++ hummingbot/smart_components/meta_strategies/data_types.py
@@ -1,9 +1,10 @@
+from decimal import Decimal
 from enum import Enum
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
-from hummingbot.core.data_type.common import OrderType
+from hummingbot.core.data_type.common import OrderType, TradeType
 
 
 class MetaExecutorStatus(Enum):
@@ -19,20 +20,26 @@ class MetaStrategyMode(Enum):
 
 class OrderLevel(BaseModel):
     level: int
-    side: str
-    order_amount_usd: float
-    spread_factor: float
+    side: TradeType
+    order_amount_usd: Decimal
+    spread_factor: Decimal
     # Configure the parameters for the position
-    stop_loss: Optional[float]
-    take_profit: Optional[float]
+    stop_loss: Optional[Decimal]
+    take_profit: Optional[Decimal]
     time_limit: Optional[int]
-    trailing_stop_activation_delta: Optional[float]
-    trailing_stop_trailing_delta: Optional[float]
+    trailing_stop_activation_price_delta: Optional[Decimal]
+    trailing_stop_trailing_delta: Optional[Decimal]
     # Configure the parameters for the order
     open_order_type: OrderType = OrderType.LIMIT
     take_profit_order_type: OrderType = OrderType.MARKET
     stop_loss_order_type: OrderType = OrderType.MARKET
     time_limit_order_type: OrderType = OrderType.MARKET
 
-    def __post_init__(self):
-        self.level_id = f"{self.side}_{self.level}"
+    @property
+    def level_id(self):
+        return f"{self.side.name}_{self.level}"
+
+    @validator("order_amount_usd", "spread_factor", "stop_loss", "take_profit",
+               "trailing_stop_activation_price_delta", "trailing_stop_trailing_delta", pre=True)
+    def float_to_decimal(cls, v):
+        return Decimal(v)
