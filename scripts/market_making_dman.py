ac188a2d2a8f945dda74cbd2d345a563da2f5c95
cardosofede <federico.cardoso.e@gmail.com>
2023-08-10 19:35:37 +0200

    (feat) run two strategies in the same file

--- scripts/market_making_dman.py
+++ scripts/market_making_dman.py
@@ -5,7 +5,8 @@ from hummingbot.connector.connector_base import ConnectorBase, TradeType
 from hummingbot.data_feed.candles_feed.candles_factory import CandlesConfig
 from hummingbot.smart_components.meta_strategies.data_types import MetaExecutorStatus, OrderLevel, TripleBarrierConf
 from hummingbot.smart_components.meta_strategies.market_making.market_making_executor import MarketMakingExecutor
-from hummingbot.smart_components.meta_strategies.market_making.strategies.dman_v1 import DMan, DManConfig
+from hummingbot.smart_components.meta_strategies.market_making.strategies.dman_v1 import DManV1, DManV1Config
+from hummingbot.smart_components.meta_strategies.market_making.strategies.dman_v2 import DManV2, DManV2Config
 from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
 
 
@@ -17,46 +18,77 @@ class MarketMakingDman(ScriptStrategyBase):
         trailing_stop_trailing_delta=Decimal("0.02")
     )
 
-    config = DManConfig(
+    config_v1 = DManV1Config(
         exchange="binance_perpetual",
-        trading_pair="BTC-USDT",
-        order_refresh_time=60,
+        trading_pair="LPT-USDT",
+        order_refresh_time=60 * 5,
         cooldown_time=15,
         order_levels=[
             OrderLevel(level=0, side=TradeType.BUY, order_amount_usd=Decimal(50),
-                       spread_factor=Decimal(1), triple_barrier_conf=triple_barrier_conf),
+                       spread_factor=Decimal(1.0), triple_barrier_conf=triple_barrier_conf),
             OrderLevel(level=1, side=TradeType.BUY, order_amount_usd=Decimal(50),
-                       spread_factor=Decimal(1.5), triple_barrier_conf=triple_barrier_conf),
+                       spread_factor=Decimal(2.0), triple_barrier_conf=triple_barrier_conf),
             OrderLevel(level=0, side=TradeType.SELL, order_amount_usd=Decimal(50),
-                       spread_factor=Decimal(1), triple_barrier_conf=triple_barrier_conf),
+                       spread_factor=Decimal(1.0), triple_barrier_conf=triple_barrier_conf),
             OrderLevel(level=1, side=TradeType.SELL, order_amount_usd=Decimal(50),
-                       spread_factor=Decimal(1.5), triple_barrier_conf=triple_barrier_conf),
+                       spread_factor=Decimal(2.0), triple_barrier_conf=triple_barrier_conf),
         ],
         candles_config=[
-            CandlesConfig(connector="binance_perpetual", trading_pair="BTC-USDT", interval="3m", max_records=1000),
+            CandlesConfig(connector="binance_perpetual", trading_pair="LPT-USDT", interval="3m", max_records=1000),
         ],
-        leverage=20
+        leverage=10,
+        natr_length=21
     )
-    meta_strategy = DMan(config=config)
+    config_v2 = DManV2Config(
+        exchange="binance_perpetual",
+        trading_pair="LPT-USDT",
+        order_refresh_time=60 * 5,
+        cooldown_time=15,
+        order_levels=[
+            OrderLevel(level=0, side=TradeType.BUY, order_amount_usd=Decimal(50),
+                       spread_factor=Decimal(1.0), triple_barrier_conf=triple_barrier_conf),
+            OrderLevel(level=1, side=TradeType.BUY, order_amount_usd=Decimal(50),
+                       spread_factor=Decimal(2.0), triple_barrier_conf=triple_barrier_conf),
+            OrderLevel(level=0, side=TradeType.SELL, order_amount_usd=Decimal(50),
+                       spread_factor=Decimal(1.0), triple_barrier_conf=triple_barrier_conf),
+            OrderLevel(level=1, side=TradeType.SELL, order_amount_usd=Decimal(50),
+                       spread_factor=Decimal(2.0), triple_barrier_conf=triple_barrier_conf),
+        ],
+        candles_config=[
+            CandlesConfig(connector="binance_perpetual", trading_pair="LPT-USDT", interval="3m", max_records=1000),
+        ],
+        leverage=10,
+        natr_length=21, macd_fast=12, macd_slow=26, macd_signal=9
+    )
+    dman_v1 = DManV1(config=config_v1)
+    dman_v2 = DManV2(config=config_v2)
+
     empty_markets = {}
-    markets = meta_strategy.update_strategy_markets_dict(empty_markets)
+    markets = dman_v1.update_strategy_markets_dict(empty_markets)
 
     def __init__(self, connectors: Dict[str, ConnectorBase]):
         super().__init__(connectors)
-        self.mm_executor = MarketMakingExecutor(strategy=self, meta_strategy=self.meta_strategy)
+        self.dman_v1_executor = MarketMakingExecutor(strategy=self, meta_strategy=self.dman_v1)
+        self.dman_v2_executor = MarketMakingExecutor(strategy=self, meta_strategy=self.dman_v2)
 
     def on_stop(self):
-        self.mm_executor.terminate_control_loop()
+        self.dman_v1_executor.terminate_control_loop()
+        self.dman_v2_executor.terminate_control_loop()
 
     def on_tick(self):
         """
         This shows you how you can start meta strategies. You can run more than one at the same time and based on the
         market conditions, you can orchestrate from this script when to stop or start them.
         """
-        if self.mm_executor.status == MetaExecutorStatus.NOT_STARTED:
-            self.mm_executor.start()
+        if self.dman_v1_executor.status == MetaExecutorStatus.NOT_STARTED:
+            self.dman_v1_executor.start()
+        if self.dman_v2_executor.status == MetaExecutorStatus.NOT_STARTED:
+            self.dman_v2_executor.start()
 
     def format_status(self) -> str:
         if not self.ready_to_trade:
             return "Market connectors are not ready."
-        return self.mm_executor.to_format_status()
+        lines = []
+        lines.extend(["DMAN V1", self.dman_v1_executor.to_format_status()])
+        lines.extend(["DMAN V2", self.dman_v2_executor.to_format_status()])
+        return "\n".join(lines)
