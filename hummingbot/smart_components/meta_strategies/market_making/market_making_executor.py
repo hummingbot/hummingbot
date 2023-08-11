fe865ac334958128a8e766b9f02c62d707a9afca
cardosofede <federico.cardoso.e@gmail.com>
2023-08-11 18:28:41 +0200

    (feat) improve order handling

--- hummingbot/smart_components/meta_strategies/market_making/market_making_executor.py
+++ hummingbot/smart_components/meta_strategies/market_making/market_making_executor.py
@@ -1,3 +1,4 @@
+from hummingbot.smart_components.executors.position_executor.data_types import PositionExecutorStatus
 from hummingbot.smart_components.meta_strategies.market_making.market_making_strategy_base import (
     MarketMakingStrategyBase,
 )
@@ -6,7 +7,8 @@ from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
 
 
 class MarketMakingExecutor(MetaExecutorBase):
-    def __init__(self, strategy: ScriptStrategyBase, meta_strategy: MarketMakingStrategyBase, update_interval: float = 1.0):
+    def __init__(self, strategy: ScriptStrategyBase, meta_strategy: MarketMakingStrategyBase,
+                 update_interval: float = 1.0):
         super().__init__(strategy, meta_strategy, update_interval)
         self.ms = meta_strategy
 
@@ -29,13 +31,16 @@ class MarketMakingExecutor(MetaExecutorBase):
             for order_level in self.ms.config.order_levels:
                 current_executor = self.level_executors[order_level.level_id]
                 if current_executor:
-                    if current_executor.is_closed:
-                        if not self.ms.cooldown_condition(current_executor, order_level):
-                            self.store_executor(current_executor, order_level)
-                    else:
-                        if self.ms.refresh_order_condition(current_executor, order_level) \
-                                or self.ms.early_stop_condition(current_executor, order_level):
-                            current_executor.early_stop()
+                    closed_and_not_in_cooldown = current_executor.is_closed and not self.ms.cooldown_condition(
+                        current_executor, order_level)
+                    active_and_early_stop_condition = current_executor.executor_status == PositionExecutorStatus.ACTIVE_POSITION and self.ms.early_stop_condition(
+                        current_executor, order_level)
+                    order_placed_and_refresh_condition = current_executor.executor_status == PositionExecutorStatus.NOT_STARTED and self.ms.refresh_order_condition(
+                        current_executor, order_level)
+                    if closed_and_not_in_cooldown:
+                        self.store_executor(current_executor, order_level)
+                    elif active_and_early_stop_condition or order_placed_and_refresh_condition:
+                        current_executor.early_stop()
                 else:
                     position_config = self.ms.get_position_config(order_level)
                     self.create_executor(position_config, order_level)
