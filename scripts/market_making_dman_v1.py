86e62155212938c0a034c1885e3015b41975d0cf
cardosofede <federico.cardoso.e@gmail.com>
2023-08-11 19:21:34 +0200

    (feat) add scripts for how-to-call

---
+++ scripts/market_making_dman_v1.py
@@ -0,0 +1,70 @@
+from decimal import Decimal
+from typing import Dict
+
+from hummingbot.connector.connector_base import ConnectorBase, TradeType
+from hummingbot.data_feed.candles_feed.candles_factory import CandlesConfig
+from hummingbot.smart_components.meta_strategies.data_types import MetaExecutorStatus, OrderLevel, TripleBarrierConf
+from hummingbot.smart_components.meta_strategies.market_making.market_making_executor import MarketMakingExecutor
+from hummingbot.smart_components.meta_strategies.market_making.strategies.dman_v1 import DManV1, DManV1Config
+from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
+
+
+class MarketMakingDman(ScriptStrategyBase):
+    triple_barrier_conf = TripleBarrierConf(
+        stop_loss=Decimal("0.03"), take_profit=Decimal("0.02"),
+        time_limit=60 * 60 * 24,
+        trailing_stop_activation_price_delta=Decimal("0.002"),
+        trailing_stop_trailing_delta=Decimal("0.0005")
+    )
+
+    config_v1 = DManV1Config(
+        exchange="binance_perpetual",
+        trading_pair="ETH-USDT",
+        order_levels=[
+            OrderLevel(level=0, side=TradeType.BUY, order_amount_usd=Decimal(20),
+                       spread_factor=Decimal(1.5), order_refresh_time=60 * 5,
+                       cooldown_time=15, triple_barrier_conf=triple_barrier_conf),
+            OrderLevel(level=1, side=TradeType.BUY, order_amount_usd=Decimal(50),
+                       spread_factor=Decimal(2.5), order_refresh_time=60 * 5,
+                       cooldown_time=15, triple_barrier_conf=triple_barrier_conf),
+            OrderLevel(level=0, side=TradeType.SELL, order_amount_usd=Decimal(20),
+                       spread_factor=Decimal(1.5), order_refresh_time=60 * 5,
+                       cooldown_time=15, triple_barrier_conf=triple_barrier_conf),
+            OrderLevel(level=1, side=TradeType.SELL, order_amount_usd=Decimal(50),
+                       spread_factor=Decimal(2.5), order_refresh_time=60 * 5,
+                       cooldown_time=15, triple_barrier_conf=triple_barrier_conf),
+        ],
+        candles_config=[
+            CandlesConfig(connector="binance_perpetual", trading_pair="ETH-USDT", interval="3m", max_records=1000),
+        ],
+        leverage=10,
+        natr_length=21
+    )
+
+    dman_v1 = DManV1(config=config_v1)
+
+    empty_markets = {}
+    markets = dman_v1.update_strategy_markets_dict(empty_markets)
+
+    def __init__(self, connectors: Dict[str, ConnectorBase]):
+        super().__init__(connectors)
+        self.dman_v1_executor = MarketMakingExecutor(strategy=self, meta_strategy=self.dman_v1)
+
+    def on_stop(self):
+        self.dman_v1_executor.terminate_control_loop()
+
+    def on_tick(self):
+        """
+        This shows you how you can start meta strategies. You can run more than one at the same time and based on the
+        market conditions, you can orchestrate from this script when to stop or start them.
+        """
+        if self.dman_v1_executor.status == MetaExecutorStatus.NOT_STARTED:
+            self.dman_v1_executor.start()
+
+    def format_status(self) -> str:
+        if not self.ready_to_trade:
+            return "Market connectors are not ready."
+        lines = []
+        lines.extend(["DMAN V1", self.dman_v1_executor.to_format_status()])
+        lines.extend(["\n-----------------------------------------\n"])
+        return "\n".join(lines)
