aedb619321f972834daee959079e65dfb40724c1
cardosofede <federico.cardoso.e@gmail.com>
2023-08-22 02:51:55 +0200

    (feat) add test coverage for executor handler

--- test/hummingbot/smart_components/test_executor_handler_base.py
+++ test/hummingbot/smart_components/test_executor_handler_base.py
@@ -1,12 +1,13 @@
-import unittest
-from unittest.mock import MagicMock, patch
+from test.isolated_asyncio_wrapper_test_case import IsolatedAsyncioWrapperTestCase
+from unittest.mock import AsyncMock, MagicMock, patch
 
 from hummingbot.smart_components.controller_base import ControllerBase
 from hummingbot.smart_components.executor_handler_base import ExecutorHandlerBase
 
 
-class TestExecutorHandlerBase(unittest.TestCase):
+class TestExecutorHandlerBase(IsolatedAsyncioWrapperTestCase):
     def setUp(self):
+        super().setUp()
         self.mock_strategy = MagicMock()
         self.mock_controller = MagicMock(spec=ControllerBase)
         self.mock_controller.config = MagicMock()
@@ -41,3 +42,19 @@ class TestExecutorHandlerBase(unittest.TestCase):
         path = self.executor_handler.get_csv_path()
         self.assertTrue(path.endswith(".csv"))
         self.assertIn("test_strategy", path)
+
+    @patch("pandas.DataFrame.to_csv", new_callable=MagicMock)
+    def test_store_executor(self, _):
+        mock_executor = MagicMock()
+        mock_executor.to_json = MagicMock(return_value={"test": "test"})
+        mock_order_level = MagicMock()
+        self.executor_handler.store_executor(mock_executor, mock_order_level)
+        self.assertIsNone(self.executor_handler.level_executors[mock_order_level.level_id])
+
+    @patch.object(ExecutorHandlerBase, "_sleep", new_callable=AsyncMock)
+    @patch.object(ExecutorHandlerBase, "control_task", new_callable=AsyncMock)
+    async def test_control_loop(self, mock_control_task, mock_sleep):
+        mock_sleep.side_effect = [None, Exception]
+        with self.assertRaises(Exception):
+            await self.executor_handler.control_loop()
+        mock_control_task.assert_called()
