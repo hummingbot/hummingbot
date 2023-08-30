79b3ad5f1f45b60626e5f2cc0594d36abdac9794
cardosofede <federico.cardoso.e@gmail.com>
2023-08-30 15:04:08 +0200

    (feat) refactored time logic

--- hummingbot/smart_components/strategy_frameworks/directional_trading/directional_trading_backtesting_engine.py
+++ hummingbot/smart_components/strategy_frameworks/directional_trading/directional_trading_backtesting_engine.py
@@ -15,12 +15,12 @@ class DirectionalTradingBacktestingEngine(BacktestingEngineBase):
                                                   trade_cost=trade_cost)
             for index, row in df[(df["side"] == order_level.side.name)].iterrows():
                 last_close_time = self.level_executors[order_level.level_id]
-                if index + pd.Timedelta(seconds=order_level.cooldown_time) > last_close_time:
+                if index >= last_close_time + pd.Timedelta(seconds=order_level.cooldown_time):
                     row["order_level"] = order_level.level_id
                     row["amount"] = float(order_level.order_amount_usd)
                     row["net_pnl_quote"] = row["net_pnl"] * row["amount"]
                     executors.append(row)
                     self.level_executors[order_level.level_id] = row["close_time"]
-        executors_df = pd.DataFrame(executors)
+        executors_df = pd.DataFrame(executors).sort_index()
         executors_df["inventory"] = initial_portfolio_usd + executors_df["net_pnl_quote"].cumsum().shift().fillna(0)
         return executors_df
