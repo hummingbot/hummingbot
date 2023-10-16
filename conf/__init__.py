145d848335b7317f756907f8b6f43b79d9f979a4
memento <claude.rc@gmail.com>
2023-10-16 10:58:25 -0500

    One more __init__.py

--- conf/__init__.py
+++ conf/__init__.py
@@ -49,8 +49,8 @@ binance_perpetuals_api_key = os.getenv("BINANCE_PERPETUALS_API_KEY")
 binance_perpetuals_api_secret = os.getenv("BINANCE_PERPETUALS_API_SECRET")
 
 # Coinbase Advanced Trade Tests
-coinbase_advanced_trade_v2_api_key = os.getenv("COINBASE_ADVANCED_TRADE_V2_API_KEY")
-coinbase_advanced_trade_v2_secret_key = os.getenv("COINBASE_ADVANCED_TRADE_V2_SECRET_KEY")
+coinbase_advanced_trade_api_key = os.getenv("COINBASE_ADVANCED_TRADE_API_KEY")
+coinbase_advanced_trade_secret_key = os.getenv("COINBASE_ADVANCED_TRADE_SECRET_KEY")
 
 # Coinbase Pro Tests
 coinbase_pro_api_key = os.getenv("COINBASE_PRO_API_KEY")
