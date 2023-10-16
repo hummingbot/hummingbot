d777173d53f58077b0587b42b82beb64c4ed9618
memento <claude.rc@gmail.com>
2023-10-16 10:57:24 -0500

    Misplaced __init__.py

---
+++ hummingbot/data_feed/candles_feed/coinbase_advanced_trade_spot_candles/__init__.py
@@ -0,0 +1,5 @@
+from hummingbot.data_feed.candles_feed.coinbase_advanced_trade_spot_candles.coinbase_advanced_trade_spot_candles import (
+    CoinbaseAdvancedTradeSpotCandles,
+)
+
+__all__ = ["CoinbaseAdvancedTradeSpotCandles"]
