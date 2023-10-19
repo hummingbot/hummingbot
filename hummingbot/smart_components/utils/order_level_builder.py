95e6e2a02fc4967c8345de90c941e3cbea48beb1
cardosofede <federico.cardoso.e@gmail.com>
2023-10-19 19:42:32 -0300

    (feat) minor fixes in types

--- hummingbot/smart_components/utils/order_level_builder.py
+++ hummingbot/smart_components/utils/order_level_builder.py
@@ -3,7 +3,7 @@ from typing import Any, Dict, List, Optional, Union
 
 from hummingbot.core.data_type.common import TradeType
 from hummingbot.smart_components.strategy_frameworks.data_types import OrderLevel, TripleBarrierConf
-from hummingbot.smart_components.utils.distributions import Distribution
+from hummingbot.smart_components.utils.distributions import Distributions
 
 
 class OrderLevelBuilder:
@@ -16,17 +16,17 @@ class OrderLevelBuilder:
         """
         self.n_levels = n_levels
 
-    def resolve_input(self, input_data: Union[Decimal, List[Decimal], Dict[str, Any]]) -> Union[Decimal, List[Decimal], List[float], Any]:
+    def resolve_input(self, input_data: Union[Decimal, List[Decimal], Dict[str, Any]]) -> List[Decimal | float | int]:
         """
-        Resolve the provided input data into a list of values.
+        Resolve the provided input data into a list of Decimal values.
 
         Args:
             input_data: The input data to resolve. Can be a single value, list, or dictionary.
 
         Returns:
-            List[Decimal]: List of resolved values.
+            List[Decimal | float | int]: List of resolved Decimal values.
         """
-        if isinstance(input_data, Decimal):
+        if isinstance(input_data, Decimal) or isinstance(input_data, float) or isinstance(input_data, int):
             return [input_data] * self.n_levels
         elif isinstance(input_data, list):
             if len(input_data) != self.n_levels:
@@ -34,7 +34,7 @@ class OrderLevelBuilder:
             return input_data
         elif isinstance(input_data, dict):
             distribution_method = input_data["method"]
-            distribution_func = getattr(Distribution, distribution_method, None)
+            distribution_func = getattr(Distributions, distribution_method, None)
             if not distribution_func:
                 raise ValueError(f"Unsupported distribution method: {distribution_method}")
             return distribution_func(self.n_levels, **input_data["params"])
@@ -45,43 +45,45 @@ class OrderLevelBuilder:
                            amounts: Union[Decimal, List[Decimal], Dict[str, Any]],
                            spreads: Union[Decimal, List[Decimal], Dict[str, Any]],
                            triple_barrier_confs: Union[TripleBarrierConf, List[TripleBarrierConf]],
+                           order_refresh_time: Union[int, List[int], Dict[str, Any]] = 60 * 5,
+                           cooldown_time: Union[int, List[int], Dict[str, Any]] = 0,
                            sides: Optional[List[TradeType]] = None) -> List[OrderLevel]:
         """
-        Build a list of OrderLevels based on given parameters.
+        Build a list of OrderLevels based on the given parameters.
 
         Args:
-            sides: Trading sides, either BUY or SELL.
             amounts: Amounts to be used for each order level.
             spreads: Spread factors for each order level.
             triple_barrier_confs: Triple barrier configurations.
+            order_refresh_time: Time in seconds to wait before refreshing orders.
+            cooldown_time: Time in seconds to wait after an order fills before placing a new one.
+            sides: Trading sides, either BUY or SELL. Default is both.
 
         Returns:
             List[OrderLevel]: List of constructed OrderLevel objects.
         """
-
-        # Default to both BUY and SELL if sides not provided
         if sides is None:
             sides = [TradeType.BUY, TradeType.SELL]
 
-        # Resolve input data into a consistent format
         resolved_amounts = self.resolve_input(amounts)
         resolved_spreads = self.resolve_input(spreads)
+        resolved_order_refresh_time = self.resolve_input(order_refresh_time)
+        resolved_cooldown_time = self.resolve_input(cooldown_time)
 
-        # If only one TripleBarrierConf is provided, replicate it for all levels
-        if isinstance(triple_barrier_confs, TripleBarrierConf):
+        if not isinstance(triple_barrier_confs, list):
             triple_barrier_confs = [triple_barrier_confs] * self.n_levels
 
         order_levels = []
-        for i, spread in enumerate(resolved_spreads):
-            barrier_conf = triple_barrier_confs[i]
-
+        for i in range(self.n_levels):
             for side in sides:
                 order_level = OrderLevel(
                     level=i + 1,
                     side=side,
-                    order_amount_usd=Decimal(resolved_amounts[i]),
-                    spread_factor=Decimal(spread),
-                    triple_barrier_conf=barrier_conf
+                    order_amount_usd=resolved_amounts[i],
+                    spread_factor=resolved_spreads[i],
+                    triple_barrier_conf=triple_barrier_confs[i],
+                    order_refresh_time=resolved_order_refresh_time[i],
+                    cooldown_time=resolved_cooldown_time[i]
                 )
                 order_levels.append(order_level)
 
