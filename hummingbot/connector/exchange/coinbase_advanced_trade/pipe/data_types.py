83bca69ba580655ab8eddb06d9fd66467c202d78
memento <claude.rc@gmail.com>
2023-10-24 16:28:48 -0500

    Cosmetic changes.Prepare for the Splitter (orderBook with multi)

--- hummingbot/connector/exchange/coinbase_advanced_trade/pipe/data_types.py
+++ hummingbot/connector/exchange/coinbase_advanced_trade/pipe/data_types.py
@@ -13,5 +13,10 @@ FromTupleDataT = Tuple[FromDataT | Sentinel, ...]
 _SyncTransformerT: Type = Callable[[FromDataT], ToDataT]
 _AsyncTransformerT: Type = Callable[[FromDataT], Awaitable[ToDataT]]
 _SyncComposerT: Type = Callable[[FromDataT], Generator[ToDataT, None, None]]
-_AsyncDecomposerT: Type = Callable[[FromDataT], AsyncGenerator[ToDataT, None]]
-HandlerT: UnionType = _SyncTransformerT | _AsyncTransformerT | _SyncComposerT | _AsyncDecomposerT
+_AsyncComposerT: Type = Callable[[FromDataT], AsyncGenerator[ToDataT, None]]
+HandlerT: UnionType = _SyncTransformerT | _AsyncTransformerT | _SyncComposerT | _AsyncComposerT
+
+_SyncSplitterT: Type = Callable[[FromDataT], Tuple[ToDataT, ...]]
+_AsyncSplitterT: Type = Callable[[FromDataT], Awaitable[ToDataT]]
+_SyncComposerT: Type = Callable[[FromDataT], Generator[ToDataT, None, None]]
+_AsyncComposerT: Type = Callable[[FromDataT], AsyncGenerator[ToDataT, None]]
