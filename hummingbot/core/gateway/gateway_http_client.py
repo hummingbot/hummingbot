01d6d1ada77c0c154edaa36ade1294d2b22ae2cb
H_S <81474463+HarjobandeepSingh@users.noreply.github.com>
2023-12-16 13:47:48 +0530

    fix/ amm_trade function do not work correctly with small prices.

--- hummingbot/core/gateway/gateway_http_client.py
+++ hummingbot/core/gateway/gateway_http_client.py
@@ -492,7 +492,7 @@ class GatewayHttpClient:
             "quote": quote_asset,
             "side": side.name,
             "amount": f"{amount:.18f}",
-            "limitPrice": str(price),
+            "limitPrice": '{:f}'.format(price),
             "allowedSlippage": "0/1",  # hummingbot applies slippage itself
         }
         if nonce is not None:
