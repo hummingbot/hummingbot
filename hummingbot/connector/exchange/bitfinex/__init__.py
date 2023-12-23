e007dcfea7d984b85949d03ed51230395721341b
Ferit <ferit@cryptolab.net>
2023-12-23 17:53:11 +0000

    (feat) add Bitfinex heartbeat interval constant

--- hummingbot/connector/exchange/bitfinex/__init__.py
+++ hummingbot/connector/exchange/bitfinex/__init__.py
@@ -6,6 +6,8 @@ BITFINEX_REST_AUTH_URL = "https://api.bitfinex.com/v2"
 BITFINEX_WS_URI = "wss://api-pub.bitfinex.com/ws/2"
 BITFINEX_WS_AUTH_URI = "wss://api.bitfinex.com/ws/2"
 
+BITFINEX_HEARTBEAT_INTERVAL = 15  # https://docs.bitfinex.com/docs/ws-general#heartbeating
+
 # this values ​​set by empirically way, because the bitfinex-market does not have
 # these values. maybe later it will be in market-api.
 TAKER_FEE = Decimal("0.002")
