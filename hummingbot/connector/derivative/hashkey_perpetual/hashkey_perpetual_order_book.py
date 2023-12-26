331b9774be21b676df456bafcfefd2a94bc0dedf
恒 <daniel@hengdeMacBook-Air.local>
2023-12-27 03:53:43 +0800

    feat: add hashkey global spot and perpetual connector

---
+++ hummingbot/connector/derivative/hashkey_perpetual/hashkey_perpetual_order_book.py
@@ -0,0 +1,71 @@
+from typing import Dict, Optional
+
+from hummingbot.core.data_type.common import TradeType
+from hummingbot.core.data_type.order_book import OrderBook
+from hummingbot.core.data_type.order_book_message import OrderBookMessage, OrderBookMessageType
+
+
+class HashkeyPerpetualsOrderBook(OrderBook):
+    @classmethod
+    def snapshot_message_from_exchange_websocket(cls,
+                                                 msg: Dict[str, any],
+                                                 timestamp: float,
+                                                 metadata: Optional[Dict] = None) -> OrderBookMessage:
+        """
+        Creates a snapshot message with the order book snapshot message
+        :param msg: the response from the exchange when requesting the order book snapshot
+        :param timestamp: the snapshot timestamp
+        :param metadata: a dictionary with extra information to add to the snapshot data
+        :return: a snapshot message with the snapshot information received from the exchange
+        """
+        if metadata:
+            msg.update(metadata)
+        ts = msg["t"]
+        return OrderBookMessage(OrderBookMessageType.SNAPSHOT, {
+            "trading_pair": msg["trading_pair"],
+            "update_id": ts,
+            "bids": msg["b"],
+            "asks": msg["a"]
+        }, timestamp=timestamp)
+
+    @classmethod
+    def snapshot_message_from_exchange_rest(cls,
+                                            msg: Dict[str, any],
+                                            timestamp: float,
+                                            metadata: Optional[Dict] = None) -> OrderBookMessage:
+        """
+        Creates a snapshot message with the order book snapshot message
+        :param msg: the response from the exchange when requesting the order book snapshot
+        :param timestamp: the snapshot timestamp
+        :param metadata: a dictionary with extra information to add to the snapshot data
+        :return: a snapshot message with the snapshot information received from the exchange
+        """
+        if metadata:
+            msg.update(metadata)
+        ts = msg["t"]
+        return OrderBookMessage(OrderBookMessageType.SNAPSHOT, {
+            "trading_pair": msg["trading_pair"],
+            "update_id": ts,
+            "bids": msg["b"],
+            "asks": msg["a"]
+        }, timestamp=timestamp)
+
+    @classmethod
+    def trade_message_from_exchange(cls, msg: Dict[str, any], metadata: Optional[Dict] = None):
+        """
+        Creates a trade message with the information from the trade event sent by the exchange
+        :param msg: the trade event details sent by the exchange
+        :param metadata: a dictionary with extra information to add to trade message
+        :return: a trade message with the details of the trade as provided by the exchange
+        """
+        if metadata:
+            msg.update(metadata)
+        ts = msg["t"]
+        return OrderBookMessage(OrderBookMessageType.TRADE, {
+            "trading_pair": msg["trading_pair"],
+            "trade_type": float(TradeType.BUY.value) if msg["m"] else float(TradeType.SELL.value),
+            "trade_id": ts,
+            "update_id": ts,
+            "price": msg["p"],
+            "amount": msg["q"]
+        }, timestamp=ts * 1e-3)
