9b9efb3d336bfc44930d4c585311a1cda309f1fe
cardosofede <federico.cardoso.e@gmail.com>
2023-12-27 18:53:03 -0300

    (feat) remove advanced market making

--- hummingbot/smart_components/strategy_frameworks/advanced_market_making/__init__.py
+++
@@ -1,8 +0,0 @@
-from .advanced_market_making_controller_base import MarketMakingControllerBase, MarketMakingControllerConfigBase
-from .advanced_market_making_executor_handler import MarketMakingExecutorHandler
-
-__all__ = [
-    "MarketMakingControllerConfigBase",
-    "MarketMakingControllerBase",
-    "MarketMakingExecutorHandler"
-]
