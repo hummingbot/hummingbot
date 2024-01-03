db3676bd511a07f19d5a845e9699c2cf42fe51a3
cardosofede <federico.cardoso.e@gmail.com>
2024-01-03 13:45:16 -0300

    (feat) refactor config

--- scripts/v2_market-making_dman_v3_with_config.py
+++ scripts/v2_market-making_dman_v3_with_config.py
@@ -3,6 +3,7 @@ from typing import Dict
 
 from pydantic import Field
 
+from hummingbot.client.config.config_data_types import BaseClientModel, ClientFieldData
 from hummingbot.connector.connector_base import ConnectorBase
 from hummingbot.core.data_type.common import OrderType, PositionAction, PositionSide
 from hummingbot.data_feed.candles_feed.candles_factory import CandlesConfig
@@ -13,32 +14,48 @@ from hummingbot.smart_components.strategy_frameworks.market_making.market_making
 )
 from hummingbot.smart_components.utils.distributions import Distributions
 from hummingbot.smart_components.utils.order_level_builder import OrderLevelBuilder
-from hummingbot.strategy.script_strategy_base import ScriptConfigBase, ScriptStrategyBase
+from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
 
 
-class DManV3ScriptConfig(ScriptConfigBase):
-    exchange: str = Field("binance_perpetual", description="The exchange to trade on.", required=True)
-    trading_pairs: str = Field("DOGE-USDT,INJ-USDT", description="The trading pairs to trade on separated by comma.", required=True)
-    leverage: int = Field(1, description="The leverage to use for the perpetual market.")
-    candles_exchange: str = Field("binance_perpetual", description="The exchange to get candles from.")
-    candles_interval: str = Field("30m", description="The interval of the candles.")
-    candles_max_records: int = Field(100, description="The maximum number of candles to get.")
-    bollinger_band_length: int = Field(20, description="The length of the Bollinger Bands.")
-    bollinger_band_std: float = Field(2.0, description="The standard deviation of the Bollinger Bands.")
-    order_amount: Decimal = Field(20, description="The amount of the orders.")
-    n_levels: int = Field(5, description="The number of levels to place orders.")
-    start_spread: float = Field(0.01, description="The start spread.")
-    step_between_orders: float = Field(0.01, description="The step between orders.")
-    stop_loss: Decimal = Field(0.01, description="The stop loss.")
-    take_profit: Decimal = Field(0.01, description="The take profit.")
-    time_limit: int = Field(60, description="The time limit.")
-    trailing_stop_activation_price_delta: Decimal = Field(0.01, description="The trailing stop activation price delta.")
-    trailing_stop_trailing_delta: Decimal = Field(0.01, description="The trailing stop trailing delta.")
-    side_filter: bool = Field(True, description="The side filter.")
-    dynamic_spread_factor: bool = Field(True, description="The dynamic spread factor.")
-    dynamic_target_spread: bool = Field(True, description="The dynamic target spread.")
-    smart_activation: bool = Field(True, description="The smart activation.")
-    activation_threshold: Decimal = Field(0.01, description="The activation threshold.")
+class DManV3ScriptConfig(BaseClientModel):
+    exchange: str = Field("binance_perpetual",
+                          client_data=ClientFieldData(prompt_on_new=True,
+                                                      prompt=lambda mi: "The exchange to trade on"))
+    trading_pairs: str = Field("DOGE-USDT,INJ-USDT",
+                               client_data=ClientFieldData(prompt_on_new=True,
+                                                           prompt=lambda mi: "The trading pairs to trade on separated by comma"))
+    leverage: int = Field(1, client_data=ClientFieldData(prompt_on_new=True,
+                                                         prompt=lambda mi: "The leverage to use for the perpetual market"))
+    candles_exchange: str = Field("binance_perpetual",
+                                  client_data=ClientFieldData(prompt_on_new=True,
+                                                              prompt=lambda mi: "The exchange to get the candles from"))
+    candles_interval: str = Field("30m",
+                                  client_data=ClientFieldData(prompt_on_new=True,
+                                                              prompt=lambda mi: "The interval of the candles"))
+    candles_max_records: int = Field(500, client_data=ClientFieldData(prompt_on_new=False,
+                                                                      prompt=lambda mi: "The max records of the candles"))
+    bollinger_band_length: int = Field(200, client_data=ClientFieldData(prompt_on_new=True,
+                                                                        prompt=lambda mi: "The length of the Bollinger Bands"))
+    bollinger_band_std: float = Field(20, client_data=ClientFieldData(prompt_on_new=False,
+                                                                      prompt=lambda mi: "The standard deviation of the Bollinger Bands"))
+    order_amount: Decimal = Field(20, client_data=ClientFieldData(prompt_on_new=True, prompt=lambda mi: "The order amount in quote asset"))
+    n_levels: int = Field(5, client_data=ClientFieldData(prompt_on_new=True, prompt=lambda mi: "The number of order levels"))
+    start_spread: float = Field(10, client_data=ClientFieldData(
+        prompt_on_new=True,
+        prompt=lambda mi: "The spread of the first order based on the value of the bollinger band, 1.0 == upper/lower band"))
+    step_between_orders: float = Field(0.1, client_data=ClientFieldData(
+        prompt_on_new=True,
+        prompt=lambda mi: "The step between orders based on the value of the bollinger band"))
+    stop_loss: Decimal = Field(0.2, client_data=ClientFieldData(prompt_on_new=True, prompt=lambda mi: "The stop loss"))
+    take_profit: Decimal = Field(0.06, client_data=ClientFieldData(prompt_on_new=True, prompt=lambda mi: "The take profit"))
+    time_limit: int = Field(60 * 60 * 24 * 3, client_data=ClientFieldData(prompt_on_new=True, prompt=lambda mi: "The time limit"))
+    trailing_stop_activation_price_delta: Decimal = Field(0.01, client_data=ClientFieldData(prompt_on_new=True, prompt=lambda mi: "The trailing stop activation price delta"))
+    trailing_stop_trailing_delta: Decimal = Field(0.01, client_data=ClientFieldData(prompt_on_new=True, prompt=lambda mi: "The trailing stop activation price delta"))
+    side_filter: bool = Field(True, client_data=ClientFieldData(prompt_on_new=False, prompt=lambda mi: "The side filter"))
+    dynamic_spread_factor: bool = Field(True, client_data=ClientFieldData(prompt_on_new=False, prompt=lambda mi: "The dynamic spread factor"))
+    dynamic_target_spread: bool = Field(True, client_data=ClientFieldData(prompt_on_new=False, prompt=lambda mi: "The dynamic target spread"))
+    smart_activation: bool = Field(True, client_data=ClientFieldData(prompt_on_new=False, prompt=lambda mi: "The smart activation"))
+    activation_threshold: Decimal = Field(0.01, client_data=ClientFieldData(prompt_on_new=False, prompt=lambda mi: "The activation threshold"))
 
 
 class DManV3MultiplePairs(ScriptStrategyBase):
@@ -52,7 +69,8 @@ class DManV3MultiplePairs(ScriptStrategyBase):
         order_level_builder = OrderLevelBuilder(n_levels=config.n_levels)
         order_levels = order_level_builder.build_order_levels(
             amounts=config.order_amount,
-            spreads=Distributions.arithmetic(n_levels=config.n_levels, start=config.start_spread, step=config.step_between_orders),
+            spreads=Distributions.arithmetic(n_levels=config.n_levels, start=config.start_spread,
+                                             step=config.step_between_orders),
             triple_barrier_confs=TripleBarrierConf(
                 stop_loss=config.stop_loss, take_profit=config.take_profit, time_limit=config.time_limit,
                 trailing_stop_activation_price_delta=config.trailing_stop_activation_price_delta,
