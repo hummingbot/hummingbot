30bcdf0637e1b65b04ac5d0991a3fdd4374e1c19
bczhang <zhangbocheng@zhangboengdembp>
2024-01-15 16:25:50 +0800

    (refactor) Create&updates kraken connector to the latest standards

---
+++ hummingbot/connector/exchange/kraken_v1/kraken_api_user_stream_data_source.py
@@ -0,0 +1,126 @@
+import asyncio
+import logging
+from typing import Any, Dict, Optional
+
+from hummingbot.connector.exchange.kraken import kraken_constants as CONSTANTS
+from hummingbot.connector.exchange.kraken.kraken_auth import KrakenAuth
+from hummingbot.connector.exchange.kraken.kraken_order_book import KrakenOrderBook
+from hummingbot.connector.exchange.kraken.kraken_utils import build_api_factory
+from hummingbot.core.api_throttler.async_throttler import AsyncThrottler
+from hummingbot.core.data_type.user_stream_tracker_data_source import UserStreamTrackerDataSource
+from hummingbot.core.web_assistant.connections.data_types import RESTMethod, RESTRequest, WSJSONRequest
+from hummingbot.core.web_assistant.rest_assistant import RESTAssistant
+from hummingbot.core.web_assistant.web_assistants_factory import WebAssistantsFactory
+from hummingbot.core.web_assistant.ws_assistant import WSAssistant
+from hummingbot.logger import HummingbotLogger
+
+MESSAGE_TIMEOUT = 3.0
+PING_TIMEOUT = 5.0
+
+
+class KrakenAPIUserStreamDataSource(UserStreamTrackerDataSource):
+
+    _krausds_logger: Optional[HummingbotLogger] = None
+
+    @classmethod
+    def logger(cls) -> HummingbotLogger:
+        if cls._krausds_logger is None:
+            cls._krausds_logger = logging.getLogger(__name__)
+        return cls._krausds_logger
+
+    def __init__(self,
+                 throttler: AsyncThrottler,
+                 kraken_auth: KrakenAuth,
+                 api_factory: Optional[WebAssistantsFactory] = None):
+        self._throttler = throttler
+        self._api_factory = api_factory or build_api_factory(throttler=throttler)
+        self._rest_assistant = None
+        self._ws_assistant = None
+        self._kraken_auth: KrakenAuth = kraken_auth
+        self._current_auth_token: Optional[str] = None
+        super().__init__()
+
+    @property
+    def order_book_class(self):
+        return KrakenOrderBook
+
+    @property
+    def last_recv_time(self):
+        if self._ws_assistant is None:
+            return 0
+        else:
+            return self._ws_assistant.last_recv_time
+
+    async def _get_rest_assistant(self) -> RESTAssistant:
+        if self._rest_assistant is None:
+            self._rest_assistant = await self._api_factory.get_rest_assistant()
+        return self._rest_assistant
+
+    async def get_auth_token(self) -> str:
+        api_auth: Dict[str, Any] = self._kraken_auth.generate_auth_dict(uri=CONSTANTS.GET_TOKEN_PATH_URL)
+
+        url = f"{CONSTANTS.BASE_URL}{CONSTANTS.GET_TOKEN_PATH_URL}"
+
+        request = RESTRequest(
+            method=RESTMethod.POST,
+            url=url,
+            headers=api_auth["headers"],
+            data=api_auth["postDict"]
+        )
+        rest_assistant = await self._get_rest_assistant()
+
+        async with self._throttler.execute_task(CONSTANTS.GET_TOKEN_PATH_URL):
+            response = await rest_assistant.call(request=request, timeout=100)
+            if response.status != 200:
+                raise IOError(f"Error fetching Kraken user stream listen key. HTTP status is {response.status}.")
+
+            try:
+                response_json: Dict[str, Any] = await response.json()
+            except Exception:
+                raise IOError(f"Error parsing data from {url}.")
+
+            err = response_json["error"]
+            if "EAPI:Invalid nonce" in err:
+                self.logger().error(f"Invalid nonce error from {url}. " +
+                                    "Please ensure your Kraken API key nonce window is at least 10, " +
+                                    "and if needed reset your API key.")
+                raise IOError({"error": response_json})
+
+            return response_json["result"]["token"]
+
+    async def listen_for_user_stream(self, output: asyncio.Queue):
+        ws = None
+        while True:
+            try:
+                async with self._throttler.execute_task(CONSTANTS.WS_CONNECTION_LIMIT_ID):
+                    ws: WSAssistant = await self._api_factory.get_ws_assistant()
+                    await ws.connect(ws_url=CONSTANTS.WS_AUTH_URL, ping_timeout=PING_TIMEOUT)
+
+                    if self._current_auth_token is None:
+                        self._current_auth_token = await self.get_auth_token()
+
+                    for subscription_type in ["openOrders", "ownTrades"]:
+                        subscribe_request: WSJSONRequest = WSJSONRequest({
+                            "event": "subscribe",
+                            "subscription": {
+                                "name": subscription_type,
+                                "token": self._current_auth_token
+                            }
+                        })
+                        await ws.send(subscribe_request)
+
+                    async for ws_response in ws.iter_messages():
+                        msg = ws_response.data
+                        if not (type(msg) is dict and "event" in msg.keys() and
+                                msg["event"] in ["heartbeat", "systemStatus", "subscriptionStatus"]):
+                            output.put_nowait(msg)
+            except asyncio.CancelledError:
+                raise
+            except Exception:
+                self.logger().error("Unexpected error with Kraken WebSocket connection. "
+                                    "Retrying after 30 seconds...", exc_info=True)
+                self._current_auth_token = None
+                await asyncio.sleep(30.0)
+            finally:
+                if ws is not None:
+                    await ws.disconnect()
---
+++ hummingbot/connector/exchange/kraken_v1/kraken_utils.py
@@ -0,0 +1,217 @@
+from typing import Any, Dict, List, Optional, Tuple
+
+from pydantic import Field, SecretStr
+
+import hummingbot.connector.exchange.kraken.kraken_constants as CONSTANTS
+from hummingbot.client.config.config_data_types import BaseConnectorConfigMap, ClientFieldData
+from hummingbot.connector.exchange.kraken.kraken_constants import KrakenAPITier
+from hummingbot.core.api_throttler.async_throttler import AsyncThrottler
+from hummingbot.core.api_throttler.data_types import LinkedLimitWeightPair, RateLimit
+from hummingbot.core.web_assistant.web_assistants_factory import WebAssistantsFactory
+
+CENTRALIZED = True
+
+EXAMPLE_PAIR = "ETH-USDC"
+
+DEFAULT_FEES = [0.16, 0.26]
+
+
+def split_trading_pair(trading_pair: str) -> Tuple[str, str]:
+    return tuple(convert_from_exchange_trading_pair(trading_pair).split("-"))
+
+
+def convert_from_exchange_symbol(symbol: str) -> str:
+    # Assuming if starts with Z or X and has 4 letters then Z/X is removable
+    if (symbol[0] == "X" or symbol[0] == "Z") and len(symbol) == 4:
+        symbol = symbol[1:]
+    return CONSTANTS.KRAKEN_TO_HB_MAP.get(symbol, symbol)
+
+
+def convert_to_exchange_symbol(symbol: str) -> str:
+    inverted_kraken_to_hb_map = {v: k for k, v in CONSTANTS.KRAKEN_TO_HB_MAP.items()}
+    return inverted_kraken_to_hb_map.get(symbol, symbol)
+
+
+def split_to_base_quote(exchange_trading_pair: str) -> Tuple[Optional[str], Optional[str]]:
+    base, quote = exchange_trading_pair.split("-")
+    return base, quote
+
+
+def convert_from_exchange_trading_pair(exchange_trading_pair: str, available_trading_pairs: Optional[Tuple] = None) -> Optional[str]:
+    base, quote = "", ""
+    if "-" in exchange_trading_pair:
+        base, quote = split_to_base_quote(exchange_trading_pair)
+    elif "/" in exchange_trading_pair:
+        base, quote = exchange_trading_pair.split("/")
+    elif len(available_trading_pairs) > 0:
+        # If trading pair has no spaces (i.e. ETHUSDT). Then it will have to match with the existing pairs
+        # Option 1: Using traditional naming convention
+        connector_trading_pair = {''.join(convert_from_exchange_trading_pair(tp).split('-')): tp for tp in available_trading_pairs}.get(
+            exchange_trading_pair)
+        if not connector_trading_pair:
+            # Option 2: Using kraken naming convention ( XXBT for Bitcoin, XXDG for Doge, ZUSD for USD, etc)
+            connector_trading_pair = {''.join(tp.split('-')): tp for tp in available_trading_pairs}.get(
+                exchange_trading_pair)
+            if not connector_trading_pair:
+                # Option 3: Kraken naming convention but without the initial X and Z
+                connector_trading_pair = {''.join(convert_to_exchange_symbol(convert_from_exchange_symbol(s))
+                                                  for s in tp.split('-')): tp
+                                          for tp in available_trading_pairs}.get(exchange_trading_pair)
+        return connector_trading_pair
+
+    if not base or not quote:
+        return None
+    base = convert_from_exchange_symbol(base)
+    quote = convert_from_exchange_symbol(quote)
+    return f"{base}-{quote}"
+
+
+def convert_to_exchange_trading_pair(hb_trading_pair: str, delimiter: str = "") -> str:
+    """
+    Note: The result of this method can safely be used to submit/make queries.
+    Result shouldn't be used to parse responses as Kraken add special formating to most pairs.
+    """
+    if "-" in hb_trading_pair:
+        base, quote = hb_trading_pair.split("-")
+    elif "/" in hb_trading_pair:
+        base, quote = hb_trading_pair.split("/")
+    else:
+        return hb_trading_pair
+    base = convert_to_exchange_symbol(base)
+    quote = convert_to_exchange_symbol(quote)
+
+    exchange_trading_pair = f"{base}{delimiter}{quote}"
+    return exchange_trading_pair
+
+
+def is_dark_pool(trading_pair_details: Dict[str, Any]):
+    '''
+    Want to filter out dark pool trading pairs from the list of trading pairs
+    For more info, please check
+    https://support.kraken.com/hc/en-us/articles/360001391906-Introducing-the-Kraken-Dark-Pool
+    '''
+    if trading_pair_details.get('altname'):
+        return trading_pair_details.get('altname').endswith('.d')
+    return False
+
+
+def _build_private_rate_limits(tier: KrakenAPITier = KrakenAPITier.STARTER) -> List[RateLimit]:
+    private_rate_limits = []
+
+    PRIVATE_ENDPOINT_LIMIT, MATCHING_ENGINE_LIMIT = CONSTANTS.KRAKEN_TIER_LIMITS[tier]
+
+    # Private REST endpoints
+    private_rate_limits.extend([
+        # Private API Pool
+        RateLimit(
+            limit_id=CONSTANTS.PRIVATE_ENDPOINT_LIMIT_ID,
+            limit=PRIVATE_ENDPOINT_LIMIT,
+            time_interval=CONSTANTS.PRIVATE_ENDPOINT_LIMIT_INTERVAL,
+        ),
+        # Private endpoints
+        RateLimit(
+            limit_id=CONSTANTS.GET_TOKEN_PATH_URL,
+            limit=PRIVATE_ENDPOINT_LIMIT,
+            time_interval=CONSTANTS.PRIVATE_ENDPOINT_LIMIT_INTERVAL,
+            linked_limits=[LinkedLimitWeightPair(CONSTANTS.PRIVATE_ENDPOINT_LIMIT_ID)],
+        ),
+        RateLimit(
+            limit_id=CONSTANTS.BALANCE_PATH_URL,
+            limit=PRIVATE_ENDPOINT_LIMIT,
+            time_interval=CONSTANTS.PRIVATE_ENDPOINT_LIMIT_INTERVAL,
+            weight=2,
+            linked_limits=[LinkedLimitWeightPair(CONSTANTS.PRIVATE_ENDPOINT_LIMIT_ID)],
+        ),
+        RateLimit(
+            limit_id=CONSTANTS.OPEN_ORDERS_PATH_URL,
+            limit=PRIVATE_ENDPOINT_LIMIT,
+            time_interval=CONSTANTS.PRIVATE_ENDPOINT_LIMIT_INTERVAL,
+            weight=2,
+            linked_limits=[LinkedLimitWeightPair(CONSTANTS.PRIVATE_ENDPOINT_LIMIT_ID)],
+        ),
+        RateLimit(
+            limit_id=CONSTANTS.QUERY_ORDERS_PATH_URL,
+            limit=PRIVATE_ENDPOINT_LIMIT,
+            time_interval=CONSTANTS.PRIVATE_ENDPOINT_LIMIT_INTERVAL,
+            weight=2,
+            linked_limits=[LinkedLimitWeightPair(CONSTANTS.PRIVATE_ENDPOINT_LIMIT_ID)],
+        ),
+    ])
+
+    # Matching Engine Limits
+    private_rate_limits.extend([
+        RateLimit(
+            limit_id=CONSTANTS.ADD_ORDER_PATH_URL,
+            limit=MATCHING_ENGINE_LIMIT,
+            time_interval=CONSTANTS.MATCHING_ENGINE_LIMIT_INTERVAL,
+            linked_limits=[LinkedLimitWeightPair(CONSTANTS.MATCHING_ENGINE_LIMIT_ID)],
+        ),
+        RateLimit(
+            limit_id=CONSTANTS.CANCEL_ORDER_PATH_URL,
+            limit=MATCHING_ENGINE_LIMIT,
+            time_interval=CONSTANTS.MATCHING_ENGINE_LIMIT_INTERVAL,
+            linked_limits=[LinkedLimitWeightPair(CONSTANTS.MATCHING_ENGINE_LIMIT_ID)],
+        ),
+    ])
+
+    return private_rate_limits
+
+
+def build_rate_limits_by_tier(tier: KrakenAPITier = KrakenAPITier.STARTER) -> List[RateLimit]:
+    rate_limits = []
+
+    rate_limits.extend(CONSTANTS.PUBLIC_API_LIMITS)
+    rate_limits.extend(_build_private_rate_limits(tier=tier))
+
+    return rate_limits
+
+
+def _api_tier_validator(value: str) -> Optional[str]:
+    """
+    Determines if input value is a valid API tier
+    """
+    try:
+        KrakenAPITier(value.upper())
+    except ValueError:
+        return "No such Kraken API Tier."
+
+
+class KrakenConfigMap(BaseConnectorConfigMap):
+    connector: str = Field(default="kraken", client_data=None)
+    kraken_api_key: SecretStr = Field(
+        default=...,
+        client_data=ClientFieldData(
+            prompt=lambda cm: "Enter your Kraken API key",
+            is_secure=True,
+            is_connect_key=True,
+            prompt_on_new=True,
+        )
+    )
+    kraken_secret_key: SecretStr = Field(
+        default=...,
+        client_data=ClientFieldData(
+            prompt=lambda cm: "Enter your Kraken secret key",
+            is_secure=True,
+            is_connect_key=True,
+            prompt_on_new=True,
+        )
+    )
+    kraken_api_tier: str = Field(
+        default="Starter",
+        client_data=ClientFieldData(
+            prompt=lambda cm: "Enter your Kraken API Tier (Starter/Intermediate/Pro)",
+            is_connect_key=True,
+            prompt_on_new=True,
+        )
+    )
+
+    class Config:
+        title = "kraken"
+
+
+KEYS = KrakenConfigMap.construct()
+
+
+def build_api_factory(throttler: AsyncThrottler) -> WebAssistantsFactory:
+    api_factory = WebAssistantsFactory(throttler=throttler)
+    return api_factory
