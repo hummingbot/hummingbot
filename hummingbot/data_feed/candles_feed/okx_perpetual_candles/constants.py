d34728b1b4be26b632de2de0ed2affa2d826dc78
drupman <tomasgaudino@hotmail.com>
2024-01-17 15:12:18 -0300

    (refactor) finish constants.py

--- hummingbot/data_feed/candles_feed/okx_perpetual_candles/constants.py
+++ hummingbot/data_feed/candles_feed/okx_perpetual_candles/constants.py
@@ -2,13 +2,14 @@ from bidict import bidict
 
 from hummingbot.core.api_throttler.data_types import LinkedLimitWeightPair, RateLimit
 
-# Update REST, WS and HEALTH endpoints with https://www.okx.com/docs-v5/en/?shell#overview-account-mode
 REST_URL = "https://www.okx.com"
-HEALTH_CHECK_ENDPOINT = "/api/v5/system/status"
-CANDLES_ENDPOINT = "/api/v5/market/history-candles"
 WSS_URL = "wss://ws.okx.com:8443/ws/v5/business"
 
-# Update bidict intervals with https://www.okx.com/docs-v5/en/?shell#order-book-trading-market-data-get-candlesticks-history
+# GET / Candlesticks history (https://www.okx.com/docs-v5/en/?shell#order-book-trading-market-data-get-candlesticks-history)
+# Retrieve history candlestick charts from recent years(It is last 3 months supported for 1s candlestick).
+# Rate Limit: 20 requests per 2 seconds
+# Rate limit rule: IP
+CANDLES_ENDPOINT = "/api/v5/market/history-candles"
 INTERVALS = bidict({
     "1s": "1s",
     "1m": "1m",
@@ -28,13 +29,14 @@ INTERVALS = bidict({
     "1M": "1Mutc",
     "3M": "3Mutc"
 })
-
-# TODO: What is request_weight
-REQUEST_WEIGHT = "REQUEST_WEIGHT"
-
 MAX_RESULTS_PER_CANDLESTICK_REST_REQUEST = 100
-# Update rate limits according to every endpoint
+
+# Get system time (https://www.okx.com/docs-v5/en/?shell#public-data-rest-api-get-system-time)
+# Retrieve API server time.
+# Rate Limit: 10 requests per 2 seconds
+# Rate limit rule: IP
+HEALTH_CHECK_ENDPOINT = "/api/v5/public/time"
+
 RATE_LIMITS = [
-    RateLimit(REQUEST_WEIGHT, limit=6000, time_interval=60),
-    RateLimit(CANDLES_ENDPOINT, limit=40, time_interval=2, linked_limits=[LinkedLimitWeightPair("raw", 1)]),
-    RateLimit(HEALTH_CHECK_ENDPOINT, limit=1200, time_interval=60, linked_limits=[LinkedLimitWeightPair("raw", 1)])]
+    RateLimit(CANDLES_ENDPOINT, limit=20, time_interval=2, linked_limits=[LinkedLimitWeightPair("raw", 1)]),
+    RateLimit(HEALTH_CHECK_ENDPOINT, limit=10, time_interval=2, linked_limits=[LinkedLimitWeightPair("raw", 1)])]
