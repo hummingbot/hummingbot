ec86488f94594ff565625984d7bd12876c647688
drupman <tomasgaudino@hotmail.com>
2024-01-19 00:26:28 -0300

    (doc) add time sync justification

--- hummingbot/connector/derivative/okx_perpetual/okx_perpetual_web_utils.py
+++ hummingbot/connector/derivative/okx_perpetual/okx_perpetual_web_utils.py
@@ -45,6 +45,18 @@ def create_throttler(trading_pairs: List[str] = None) -> AsyncThrottler:
 
 async def get_current_server_time(throttler: Optional[AsyncThrottler] = None,
                                   domain: str = CONSTANTS.DEFAULT_DOMAIN) -> float:
+    """
+    Transaction Timeouts (https://www.okx.com/docs-v5/en/?shell#overview-general-info)
+    Orders may not be processed in time due to network delay or busy OKX servers.
+
+    You can configure the expiry time of the request using expTime if you want the order request to be
+    discarded after a specific time.
+
+    If expTime is specified in the requests for Place (multiple) orders or Amend (multiple) orders,
+    the request will not be processed if the current system time of the server is after the expTime.
+
+    You should synchronize with our system time. Use Get system time to obtain the current system time.
+    """
     throttler = throttler or create_throttler()
     api_factory = build_api_factory_without_time_synchronizer_pre_processor(throttler=throttler)
     rest_assistant = await api_factory.get_rest_assistant()
