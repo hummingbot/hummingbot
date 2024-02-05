edb552f3df403f5945422ee436c36e5b48d64192
bczhang <zhangbocheng@zhangboengdembp>
2024-02-05 15:36:40 +0800

    fix bug

--- test/hummingbot/connector/derivative/hyperliquid_perpetual/test_hyperliquid_perpetual_utils.py
+++ test/hummingbot/connector/derivative/hyperliquid_perpetual/test_hyperliquid_perpetual_utils.py
@@ -29,25 +29,25 @@ class HyperliquidPerpetualUtilsTests(TestCase):
 
         validations = [HyperliquidPerpetualConfigMap.validate_bool(value) for value in valid_values]
         for validation in validations:
-            self.assertIsNone(validation)
+            self.assertTrue(validation)
 
     def test_cls_validate_bool_fails(self):
         wrong_value = "ye"
         valid_values = ('true', 'yes', 'y', 'false', 'no', 'n')
-
-        validation_error = HyperliquidPerpetualConfigMap.validate_bool(wrong_value)
-        self.assertEqual(validation_error, f"Invalid value, please choose value from {valid_values}")
+        with self.assertRaises(ValueError) as exception_context:
+            validation_error = HyperliquidPerpetualConfigMap.validate_bool(wrong_value)
+        self.assertEqual(str(exception_context.exception), f"Invalid value, please choose value from {valid_values}")
 
     def test_cls_testnet_validate_bool_succeed(self):
         valid_values = ['true', 'yes', 'y', 'false', 'no', 'n']
 
         validations = [HyperliquidPerpetualTestnetConfigMap.validate_bool(value) for value in valid_values]
         for validation in validations:
-            self.assertIsNone(validation)
+            self.assertTrue(validation)
 
     def test_cls_testnet_validate_bool_fails(self):
         wrong_value = "ye"
         valid_values = ('true', 'yes', 'y', 'false', 'no', 'n')
-
-        validation_error = HyperliquidPerpetualTestnetConfigMap.validate_bool(wrong_value)
-        self.assertEqual(validation_error, f"Invalid value, please choose value from {valid_values}")
+        with self.assertRaises(ValueError) as exception_context:
+            validation_error = HyperliquidPerpetualTestnetConfigMap.validate_bool(wrong_value)
+        self.assertEqual(str(exception_context.exception), f"Invalid value, please choose value from {valid_values}")
