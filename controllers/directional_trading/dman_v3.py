6d1decccebb5fe0e5152ae1665fca103b0e7cd99
cardosofede <federico.cardoso.e@gmail.com>
2024-02-28 23:37:06 -0300

    (feat) refactor directional trading controllers

--- controllers/directional_trading/dman_v3.py
+++ controllers/directional_trading/dman_v3.py
@@ -1,115 +1,197 @@
 import time
 from decimal import Decimal
-from typing import Optional
+from typing import List, Optional, Tuple
 
+import pandas as pd
 import pandas_ta as ta  # noqa: F401
+from pydantic import Field, validator
 
+from hummingbot.client.config.config_data_types import ClientFieldData
 from hummingbot.core.data_type.common import TradeType
-from hummingbot.smart_components.executors.position_executor.data_types import PositionExecutorConfig
-from hummingbot.smart_components.executors.position_executor.position_executor import PositionExecutor
-from hummingbot.smart_components.order_level_distributions.order_level_builder import OrderLevel
-from hummingbot.smart_components.strategy_frameworks.market_making.market_making_controller_base import (
-    MarketMakingControllerBase,
-    MarketMakingControllerConfigBase,
+from hummingbot.data_feed.candles_feed.candles_factory import CandlesConfig
+from hummingbot.smart_components.controllers.directional_trading_controller_base import (
+    DirectionalTradingControllerBase,
+    DirectionalTradingControllerConfigBase,
 )
+from hummingbot.smart_components.executors.dca_executor.data_types import DCAExecutorConfig, DCAMode
+from hummingbot.smart_components.executors.position_executor.data_types import TrailingStop
 
 
-class DManV3Config(MarketMakingControllerConfigBase):
-    strategy_name: str = "dman_v3"
-    bb_length: int = 100
-    bb_std: float = 2.0
-    side_filter: bool = False
-    smart_activation: bool = False
-    activation_threshold: Decimal = Decimal("0.001")
-    dynamic_spread_factor: bool = True
-    dynamic_target_spread: bool = False
+class DManV3ControllerConfig(DirectionalTradingControllerConfigBase):
+    controller_name: str = "dman_v3"
+    candles_config: List[CandlesConfig] = []
+    interval: str = Field(
+        default="3m",
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the candle interval (e.g., 1m, 5m, 1h, 1d): ",
+            prompt_on_new=False))
+    bb_length: int = Field(
+        default=100,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the Bollinger Bands length: ",
+            prompt_on_new=True))
+    bb_std: float = Field(
+        default=2.0,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the Bollinger Bands standard deviation: ",
+            prompt_on_new=False))
+    bb_long_threshold: float = Field(
+        default=0.0,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the Bollinger Bands long threshold: ",
+            prompt_on_new=True))
+    bb_short_threshold: float = Field(
+        default=1.0,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the Bollinger Bands short threshold: ",
+            prompt_on_new=True))
+    dca_spreads: List[Decimal] = Field(
+        default="0.002,0.006,0.01,0.015,0.02",
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the spreads for each DCA level (comma-separated) if dynamic_spread=True this value "
+                              "will multiply the Bollinger Bands width, e.g. if the Bollinger Bands width is 0.1 (10%)"
+                              "and the spread is 0.2, the distance of the order to the current price will be 0.02 (2%) ",
+            prompt_on_new=True))
+    dca_amounts_pct: List[Decimal] = Field(
+        default=None,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the amounts for each DCA level (as a percentage of the total balance, "
+                              "comma-separated). Don't worry about the final sum, it will be normalized. ",
+            prompt_on_new=True))
+    dynamic_order_spread: bool = Field(
+        default=None,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Do you want to make the spread dynamic? (Yes/No) ",
+            prompt_on_new=True))
+    dynamic_target: bool = Field(
+        default=None,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Do you want to make the target dynamic? (Yes/No) ",
+            prompt_on_new=True))
+
+    activation_bounds: Optional[List[Decimal]] = Field(
+        default=None,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the activation bounds for the orders "
+                              "(e.g., 0.01 activates the next order when the price is closer than 1%): ",
+            prompt_on_new=True))
+
+    @validator("activation_bounds", pre=True, always=True)
+    def parse_activation_bounds(cls, v):
+        if isinstance(v, str):
+            if v == "":
+                return None
+            return [Decimal(val) for val in v.split(",")]
+        if isinstance(v, list):
+            return [Decimal(val) for val in v]
+        return v
+
+    @validator('dca_spreads', pre=True, always=True)
+    def validate_spreads(cls, v):
+        if isinstance(v, str):
+            return [Decimal(val) for val in v.split(",")]
+        return v
+
+    @validator('dca_amounts_pct', pre=True, always=True)
+    def validate_amounts(cls, v, values):
+        spreads = values.get("dca_spreads")
+        if isinstance(v, str):
+            if v == "":
+                return [Decimal('1.0') / len(spreads) for _ in spreads]
+            amounts = [Decimal(val) for val in v.split(",")]
+            if len(amounts) != len(spreads):
+                raise ValueError("Amounts and spreads must have the same length")
+            return amounts
+        if v is None:
+            return [Decimal('1.0') / len(spreads) for _ in spreads]
+        return v
+
+    def get_spreads_and_amounts_in_quote(self, trade_type: TradeType, total_amount_quote: Decimal) -> Tuple[List[Decimal], List[Decimal]]:
+        amounts_pct = self.dca_amounts_pct
+        if amounts_pct is None:
+            # Equally distribute if amounts_pct is not set
+            spreads = self.dca_spreads
+            normalized_amounts_pct = [Decimal('1.0') / len(spreads) for _ in spreads]
+        else:
+            if trade_type == TradeType.BUY:
+                normalized_amounts_pct = [amt_pct / sum(amounts_pct) for amt_pct in amounts_pct]
+            else:  # TradeType.SELL
+                normalized_amounts_pct = [amt_pct / sum(amounts_pct) for amt_pct in amounts_pct]
+
+        return self.dca_spreads, [amt_pct * total_amount_quote for amt_pct in normalized_amounts_pct]
 
 
-class DManV3(MarketMakingControllerBase):
+class DManV3Controller(DirectionalTradingControllerBase):
     """
     Mean reversion strategy with Grid execution making use of Bollinger Bands indicator to make spreads dynamic
     and shift the mid price.
     """
-
-    def __init__(self, config: DManV3Config):
-        super().__init__(config)
+    def __init__(self, config: DManV3ControllerConfig, *args, **kwargs):
         self.config = config
+        self.max_records = config.bb_length
+        if len(self.config.candles_config) == 0:
+            self.config.candles_config = [CandlesConfig(
+                connector=config.connector_name,
+                trading_pair=config.trading_pair,
+                interval=config.interval,
+                max_records=self.max_records
+            )]
+        super().__init__(config, *args, **kwargs)
 
-    def refresh_order_condition(self, executor: PositionExecutor, order_level: OrderLevel) -> bool:
-        """
-        Checks if the order needs to be refreshed.
-        You can reimplement this method to add more conditions.
-        """
-        if executor.position_config.timestamp + order_level.order_refresh_time > time.time():
-            return False
-        return True
+    def get_signal(self) -> int:
+        return self.get_processed_data()["signal"].iloc[-1]
 
-    def early_stop_condition(self, executor: PositionExecutor, order_level: OrderLevel) -> bool:
-        """
-        If an executor has an active position, should we close it based on a condition.
-        """
-        return False
+    def get_processed_data(self) -> pd.DataFrame:
+        df = self.market_data_provider.get_candles_df(connector_name=self.config.connector_name,
+                                                      trading_pair=self.config.trading_pair,
+                                                      interval=self.config.interval,
+                                                      max_records=self.max_records)
+        # Add indicators
+        df.ta.bbands(length=self.config.bb_length, std=self.config.bb_std, append=True)
 
-    def cooldown_condition(self, executor: PositionExecutor, order_level: OrderLevel) -> bool:
-        """
-        After finishing an order, the executor will be in cooldown for a certain amount of time.
-        This prevents the executor from creating a new order immediately after finishing one and execute a lot
-        of orders in a short period of time from the same side.
-        """
-        if executor.close_timestamp and executor.close_timestamp + order_level.cooldown_time > time.time():
-            return True
-        return False
+        # Generate signal
+        long_condition = df[f"BBP_{self.config.bb_length}_{self.config.bb_std}"] < self.config.bb_long_threshold
+        short_condition = df[f"BBP_{self.config.bb_length}_{self.config.bb_std}"] > self.config.bb_short_threshold
 
-    def get_processed_data(self):
-        """
-        Gets the price and spread multiplier from the last candlestick.
-        """
-        candles_df = self.candles[0].candles_df
-        bbp = ta.bbands(candles_df["close"], length=self.config.bb_length, std=self.config.bb_std)
+        # Generate signal
+        df["signal"] = 0
+        df.loc[long_condition, "signal"] = 1
+        df.loc[short_condition, "signal"] = -1
+        return df
 
-        candles_df["price_multiplier"] = bbp[f"BBM_{self.config.bb_length}_{self.config.bb_std}"]
-        candles_df["spread_multiplier"] = bbp[f"BBB_{self.config.bb_length}_{self.config.bb_std}"] / 200
-        return candles_df
+    def get_spread_multiplier(self) -> Decimal:
+        if self.config.dynamic_order_spread:
+            df = self.get_processed_data()
+            bb_width = df[f"BBB_{self.config.bb_length}_{self.config.bb_std}"].iloc[-1]
+            return Decimal(bb_width / 200)
+        else:
+            return Decimal("1.0")
 
-    def get_position_config(self, order_level: OrderLevel) -> Optional[PositionExecutorConfig]:
-        """
-        Creates a PositionConfig object from an OrderLevel object.
-        Here you can use technical indicators to determine the parameters of the position config.
-        """
-        close_price = self.get_close_price(self.close_price_trading_pair)
-
-        bollinger_mid_price, spread_multiplier = self.get_price_and_spread_multiplier()
-        if not self.config.dynamic_spread_factor:
-            spread_multiplier = 1
-        side_multiplier = -1 if order_level.side == TradeType.BUY else 1
-        order_spread_multiplier = order_level.spread_factor * spread_multiplier * side_multiplier
-        order_price = bollinger_mid_price * (1 + order_spread_multiplier)
-        amount = order_level.order_amount_usd / order_price
-
-        # Avoid placing the order from the opposite side
-        side_filter_condition = self.config.side_filter and (
-            (bollinger_mid_price > close_price and side_multiplier == 1) or
-            (bollinger_mid_price < close_price and side_multiplier == -1))
-        if side_filter_condition:
-            return
-
-        # Smart activation of orders
-        smart_activation_condition = self.config.smart_activation and (
-            side_multiplier == 1 and (close_price < order_price * (1 + self.config.activation_threshold)) or
-            (side_multiplier == -1 and (close_price > order_price * (1 - self.config.activation_threshold))))
-        if smart_activation_condition:
-            return
-
-        target_spread = spread_multiplier if self.config.dynamic_target_spread else 1
-        triple_barrier_conf = order_level.triple_barrier_conf.new_instance_with_adjusted_volatility(target_spread)
-        position_config = PositionExecutorConfig(
+    def get_executor_config(self, trade_type: TradeType, price: Decimal, amount: Decimal) -> DCAExecutorConfig:
+        spread, amounts_quote = self.config.get_spreads_and_amounts_in_quote(trade_type, amount * price)
+        spread_multiplier = self.get_spread_multiplier()
+        if trade_type == TradeType.BUY:
+            prices = [price * (1 - spread * spread_multiplier) for spread in spread]
+        else:
+            prices = [price * (1 + spread * spread_multiplier) for spread in spread]
+        if self.config.dynamic_target:
+            stop_loss = self.config.stop_loss * spread_multiplier
+            trailing_stop = TrailingStop(activation_price=self.config.trailing_stop.activation_price * spread_multiplier,
+                                         trailing_delta=self.config.trailing_stop.trailing_delta * spread_multiplier)
+        else:
+            stop_loss = self.config.stop_loss
+            trailing_stop = self.config.trailing_stop
+        return DCAExecutorConfig(
             timestamp=time.time(),
+            exchange=self.config.connector_name,
             trading_pair=self.config.trading_pair,
-            exchange=self.config.exchange,
-            side=order_level.side,
-            amount=amount,
-            entry_price=Decimal(order_price),
-            triple_barrier_config=triple_barrier_conf,
-            leverage=self.config.leverage
+            side=trade_type,
+            mode=DCAMode.MAKER,
+            prices=prices,
+            amounts_quote=amounts_quote,
+            time_limit=self.config.time_limit,
+            stop_loss=stop_loss,
+            trailing_stop=trailing_stop,
+            leverage=self.config.leverage,
+            activation_bounds=self.config.activation_bounds,
         )
-        return position_config
--- controllers/directional_trading/macd_bb_v1.py
+++ controllers/directional_trading/macd_bb_v1.py
@@ -1,60 +1,89 @@
-import time
-from typing import Optional
+from typing import List
 
 import pandas as pd
+import pandas_ta as ta  # noqa: F401
 from pydantic import Field
 
-from hummingbot.smart_components.executors.position_executor.position_executor import PositionExecutor
-from hummingbot.smart_components.order_level_distributions.order_level_builder import OrderLevel
-from hummingbot.smart_components.strategy_frameworks.directional_trading.directional_trading_controller_base import (
+from hummingbot.client.config.config_data_types import ClientFieldData
+from hummingbot.client.ui.interface_utils import format_df_for_printout
+from hummingbot.data_feed.candles_feed.candles_factory import CandlesConfig
+from hummingbot.smart_components.controllers.directional_trading_controller_base import (
     DirectionalTradingControllerBase,
     DirectionalTradingControllerConfigBase,
 )
 
 
-class MACDBBV1Config(DirectionalTradingControllerConfigBase):
-    strategy_name: str = "macd_bb_v1"
-    bb_length: int = Field(default=100, ge=20, le=1000)
-    bb_std: float = Field(default=2.0, ge=0.5, le=4.0)
-    bb_long_threshold: float = Field(default=0.0, ge=-3.0, le=0.5)
-    bb_short_threshold: float = Field(default=1.0, ge=0.5, le=3.0)
-    macd_fast: int = Field(default=21, ge=2, le=100)
-    macd_slow: int = Field(default=42, ge=30, le=1000)
-    macd_signal: int = Field(default=9, ge=2, le=100)
-    std_span: Optional[int] = None
+class MACDBBV1ControllerConfig(DirectionalTradingControllerConfigBase):
+    controller_name = "macd_bb_v1"
+    candles_config: List[CandlesConfig] = []
+    interval: str = Field(
+        default="3m",
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the candle interval (e.g., 1m, 5m, 1h, 1d): ",
+            prompt_on_new=False))
+    bb_length: int = Field(
+        default=100,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the Bollinger Bands length: ",
+            prompt_on_new=True))
+    bb_std: float = Field(
+        default=2.0,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the Bollinger Bands standard deviation: ",
+            prompt_on_new=False))
+    bb_long_threshold: float = Field(
+        default=0.0,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the Bollinger Bands long threshold: ",
+            prompt_on_new=True))
+    bb_short_threshold: float = Field(
+        default=1.0,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the Bollinger Bands short threshold: ",
+            prompt_on_new=True))
+    macd_fast: int = Field(
+        default=21,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the MACD fast period: ",
+            prompt_on_new=True))
+    macd_slow: int = Field(
+        default=42,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the MACD slow period: ",
+            prompt_on_new=True))
+    macd_signal: int = Field(
+        default=9,
+        client_data=ClientFieldData(
+            prompt=lambda mi: "Enter the MACD signal period: ",
+            prompt_on_new=True))
 
 
-class MACDBBV1(DirectionalTradingControllerBase):
-    """
-    Directional Market Making Strategy making use of NATR indicator to make spreads dynamic.
-    """
+class MACDBBV1Controller(DirectionalTradingControllerBase):
 
-    def __init__(self, config: MACDBBV1Config):
-        super().__init__(config)
+    def __init__(self, config: MACDBBV1ControllerConfig, *args, **kwargs):
         self.config = config
+        self.max_records = max(config.macd_slow, config.macd_fast, config.macd_signal, config.bb_length)
+        if len(self.config.candles_config) == 0:
+            self.config.candles_config = [CandlesConfig(
+                connector=config.connector_name,
+                trading_pair=config.trading_pair,
+                interval=config.interval,
+                max_records=self.max_records
+            )]
+        super().__init__(config, *args, **kwargs)
 
-    def early_stop_condition(self, executor: PositionExecutor, order_level: OrderLevel) -> bool:
-        """
-        If an executor has an active position, should we close it based on a condition.
-        """
-        return False
-
-    def cooldown_condition(self, executor: PositionExecutor, order_level: OrderLevel) -> bool:
-        """
-        After finishing an order, the executor will be in cooldown for a certain amount of time.
-        This prevents the executor from creating a new order immediately after finishing one and execute a lot
-        of orders in a short period of time from the same side.
-        """
-        if executor.close_timestamp and executor.close_timestamp + order_level.cooldown_time > time.time():
-            return True
-        return False
+    def get_signal(self) -> int:
+        return self.get_processed_data()["signal"].iloc[-1]
 
     def get_processed_data(self) -> pd.DataFrame:
-        df = self.candles[0].candles_df
-
+        df = self.market_data_provider.get_candles_df(connector_name=self.config.connector_name,
+                                                      trading_pair=self.config.trading_pair,
+                                                      interval=self.config.interval,
+                                                      max_records=self.max_records)
         # Add indicators
         df.ta.bbands(length=self.config.bb_length, std=self.config.bb_std, append=True)
         df.ta.macd(fast=self.config.macd_fast, slow=self.config.macd_slow, signal=self.config.macd_signal, append=True)
+
         bbp = df[f"BBP_{self.config.bb_length}_{self.config.bb_std}"]
         macdh = df[f"MACDh_{self.config.macd_fast}_{self.config.macd_slow}_{self.config.macd_signal}"]
         macd = df[f"MACD_{self.config.macd_fast}_{self.config.macd_slow}_{self.config.macd_signal}"]
@@ -62,16 +91,13 @@ class MACDBBV1(DirectionalTradingControllerBase):
         # Generate signal
         long_condition = (bbp < self.config.bb_long_threshold) & (macdh > 0) & (macd < 0)
         short_condition = (bbp > self.config.bb_short_threshold) & (macdh < 0) & (macd > 0)
+
         df["signal"] = 0
         df.loc[long_condition, "signal"] = 1
         df.loc[short_condition, "signal"] = -1
 
-        # Optional: Generate spread multiplier
-        if self.config.std_span:
-            df["target"] = df["close"].rolling(self.config.std_span).std() / df["close"]
         return df
 
-    def extra_columns_to_show(self):
-        return [f"BBP_{self.config.bb_length}_{self.config.bb_std}",
-                f"MACDh_{self.config.macd_fast}_{self.config.macd_slow}_{self.config.macd_signal}",
-                f"MACD_{self.config.macd_fast}_{self.config.macd_slow}_{self.config.macd_signal}"]
+    def to_format_status(self) -> List[str]:
+        df = self.get_processed_data()
+        return [format_df_for_printout(df.tail(5), table_format="psql",)]
