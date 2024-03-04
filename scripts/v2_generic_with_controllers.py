3af900962846a22f854010b01fa053afe29d3441
cardosofede <federico.cardoso.e@gmail.com>
2024-03-04 13:55:51 -0300

    (feat) replace list for dict

--- scripts/v2_generic_with_controllers.py
+++ scripts/v2_generic_with_controllers.py
@@ -16,7 +16,7 @@ from hummingbot.strategy.strategy_v2_base import StrategyV2Base, StrategyV2Confi
 class GenericV2StrategyWithControllersConfig(StrategyV2ConfigBase):
     script_file_name: str = Field(default_factory=lambda: os.path.basename(__file__))
     candles_config: List[CandlesConfig] = []
-    markets: Dict[str, Set[str]] = []
+    markets: Dict[str, Set[str]] = {}
 
 
 class GenericV2StrategyWithControllers(StrategyV2Base):
