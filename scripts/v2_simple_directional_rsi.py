72192909b81c6c8bf569f8216158279d06d2e183
cardosofede <federico.cardoso.e@gmail.com>
2024-03-08 11:51:59 -0300

    (feat) improve base scripts

--- scripts/v2_simple_directional_rsi.py
+++ scripts/v2_simple_directional_rsi.py
@@ -7,6 +7,7 @@ from pydantic import Field, validator
 
 from hummingbot.client.config.config_data_types import ClientFieldData
 from hummingbot.connector.connector_base import ConnectorBase
+from hummingbot.core.clock import Clock
 from hummingbot.core.data_type.common import OrderType, PositionMode, PriceType, TradeType
 from hummingbot.data_feed.candles_feed.candles_factory import CandlesConfig
 from hummingbot.smart_components.executors.position_executor.data_types import (
@@ -26,18 +27,18 @@ class SimpleDirectionalRSIConfig(StrategyV2ConfigBase):
         client_data=ClientFieldData(
             prompt=lambda mi: "Enter the RSI period (e.g. 14): ",
             prompt_on_new=True))
-    rsi_low: Decimal = Field(
-        default=Decimal("30"), gt=0,
+    rsi_low: float = Field(
+        default=30, gt=0,
         client_data=ClientFieldData(
             prompt=lambda mi: "Enter the RSI low (e.g. 30): ",
             prompt_on_new=True))
-    rsi_high: Decimal = Field(
-        default=Decimal("70"), gt=0,
+    rsi_high: float = Field(
+        default=70, gt=0,
         client_data=ClientFieldData(
             prompt=lambda mi: "Enter the RSI high (e.g. 70): ",
             prompt_on_new=True))
     interval: str = Field(
-        default="1m",
+        default="3m",
         client_data=ClientFieldData(
             prompt=lambda mi: "Enter the interval (e.g. 1m): ",
             prompt_on_new=True))
@@ -99,7 +100,7 @@ class SimpleDirectionalRSI(StrategyV2Base):
 
     def __init__(self, connectors: Dict[str, ConnectorBase], config: SimpleDirectionalRSIConfig):
         if len(config.candles_config) == 0:
-            self.max_records = config.rsi_period
+            self.max_records = config.rsi_period + 10
             for connector_name, trading_pairs in config.markets.items():
                 for trading_pair in trading_pairs:
                     config.candles_config.append(CandlesConfig(
@@ -111,19 +112,26 @@ class SimpleDirectionalRSI(StrategyV2Base):
         super().__init__(connectors, config)
         self.config = config
 
-    def on_tick(self):
-        self.set_position_mode_and_leverage()
-        super().on_tick()
+    def start(self, clock: Clock, timestamp: float) -> None:
+        """
+        Start the strategy.
+        :param clock: Clock to use.
+        :param timestamp: Current time.
+        """
+        self._last_timestamp = timestamp
+        self.apply_initial_setting()
 
     def create_actions_proposal(self) -> List[CreateExecutorAction]:
+        create_actions = []
+
         for connector_name, trading_pairs in self.config.markets.items():
             for trading_pair in trading_pairs:
                 signal = self.get_signal(connector_name, trading_pair)
                 active_longs, active_shorts = self.get_active_executors_by_side(connector_name, trading_pair)
                 if signal is not None:
                     mid_price = self.market_data_provider.get_price_by_type(connector_name, trading_pair, PriceType.MidPrice)
-                    if signal["signal"] == 1 and len(active_longs) == 0:
-                        return [CreateExecutorAction(
+                    if signal == 1 and len(active_longs) == 0:
+                        create_actions.append(CreateExecutorAction(
                             executor_config=PositionExecutorConfig(
                                 timestamp=self.current_timestamp,
                                 connector_name=connector_name,
@@ -133,9 +141,9 @@ class SimpleDirectionalRSI(StrategyV2Base):
                                 amount=self.config.order_amount_quote / mid_price,
                                 triple_barrier_config=self.config.triple_barrier_config,
                                 leverage=self.config.leverage
-                            ))]
-                    elif signal["signal"] == -1 and len(active_shorts) == 0:
-                        return [CreateExecutorAction(
+                            )))
+                    elif signal == -1 and len(active_shorts) == 0:
+                        create_actions.append(CreateExecutorAction(
                             executor_config=PositionExecutorConfig(
                                 timestamp=self.current_timestamp,
                                 connector_name=connector_name,
@@ -145,37 +153,40 @@ class SimpleDirectionalRSI(StrategyV2Base):
                                 amount=self.config.order_amount_quote / mid_price,
                                 triple_barrier_config=self.config.triple_barrier_config,
                                 leverage=self.config.leverage
-                            ))]
+                            )))
+        return create_actions
 
     def stop_actions_proposal(self) -> List[StopExecutorAction]:
+        stop_actions = []
         for connector_name, trading_pairs in self.config.markets.items():
             for trading_pair in trading_pairs:
                 signal = self.get_signal(connector_name, trading_pair)
                 active_longs, active_shorts = self.get_active_executors_by_side(connector_name, trading_pair)
                 if signal is not None:
-                    if signal["signal"] == -1 and len(active_longs) > 0:
-                        return [StopExecutorAction(executor_id=e.id) for e in active_longs]
-                    elif signal["signal"] == 1 and len(active_shorts) > 0:
-                        return [StopExecutorAction(executor_id=e.id) for e in active_shorts]
+                    if signal == -1 and len(active_longs) > 0:
+                        stop_actions.extend([StopExecutorAction(executor_id=e.id) for e in active_longs])
+                    elif signal == 1 and len(active_shorts) > 0:
+                        stop_actions.extend([StopExecutorAction(executor_id=e.id) for e in active_shorts])
+        return stop_actions
 
     def get_active_executors_by_side(self, connector_name: str, trading_pair: str):
         active_executors_by_connector_pair = self.filter_executors(
             executors=self.get_all_executors(),
-            filter_func=lambda e: e.connector_name == connector_name and e.connector_name == trading_pair and e.is_active
+            filter_func=lambda e: e.connector_name == connector_name and e.trading_pair == trading_pair and e.is_active
         )
         active_longs = [e for e in active_executors_by_connector_pair if e.side == TradeType.BUY]
         active_shorts = [e for e in active_executors_by_connector_pair if e.side == TradeType.SELL]
         return active_longs, active_shorts
 
-    def get_signal(self, connector_name: str, trading_pair: str) -> Optional[Decimal]:
-        candles = self.market_data_provider.get_candles_df(connector_name, trading_pair, self.config.interval)
-        candles.ta.rsi(candles["close"], length=self.config.rsi_period, append=True)
+    def get_signal(self, connector_name: str, trading_pair: str) -> Optional[float]:
+        candles = self.market_data_provider.get_candles_df(connector_name, trading_pair, self.config.interval, self.max_records)
+        candles.ta.rsi(length=self.config.rsi_period, append=True)
         candles["signal"] = 0
-        candles.loc[candles["RSI_14"] < self.config.rsi_low, "signal"] = 1
-        candles.loc[candles["RSI_14"] > self.config.rsi_high, "signal"] = -1
-        return candles.iloc[-1] if not candles.empty else None
+        candles.loc[candles[f"RSI_{self.config.rsi_period}"] < self.config.rsi_low, "signal"] = 1
+        candles.loc[candles[f"RSI_{self.config.rsi_period}"] > self.config.rsi_high, "signal"] = -1
+        return candles.iloc[-1]["signal"] if not candles.empty else None
 
-    def set_position_mode_and_leverage(self):
+    def apply_initial_setting(self):
         if not self.account_config_set:
             for connector_name, connector in self.connectors.items():
                 if self.is_perpetual(connector_name):
