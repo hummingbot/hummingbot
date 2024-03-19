59be0e8fedfaddd1b5407fca67f2ed018fcc4412
Gulli <ghreidarsson@ripple.com>
2024-03-19 15:36:44 +0000

    small update on script

--- scripts/automated_cash_n_carry.py
+++ scripts/automated_cash_n_carry.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from decimal import Decimal
 from enum import Enum
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Union
 
 import numpy as np
 import pandas as pd
@@ -9,6 +9,12 @@ import pandas as pd
 from hummingbot.connector.connector_base import ConnectorBase
 from hummingbot.core.data_type.common import OrderType, PositionAction, PositionMode, PositionSide
 from hummingbot.core.data_type.funding_info import FundingInfo
+from hummingbot.core.event.events import (
+    BuyOrderCompletedEvent,
+    MarketOrderFailureEvent,
+    RangePositionClosedEvent,
+    SellOrderCompletedEvent,
+)
 from hummingbot.core.rate_oracle.rate_oracle import RateOracle
 from hummingbot.strategy.market_trading_pair_tuple import MarketTradingPairTuple
 from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
@@ -35,6 +41,17 @@ class SpotOrder:
     side: bool
     amount: Decimal
     price: Decimal
+    exchange_id: str
+
+
+@dataclass
+class Position:
+    trading_pair: str
+    exchange: str
+    position_side: PositionSide
+    amount: Decimal
+    price: Decimal
+    exchange_id: str
 
 
 '''
@@ -50,39 +67,46 @@ class SpotOrder:
 class AutomatedCashNCarry(ScriptStrategyBase):
     CURRENT_STRATEGY_SIDE: PositionSide = None
     STRATEGY_INFLIGHT = False
-    inflight_spot_orders: List[SpotOrder] = []
-
+    dry_run: bool = False
+    inflight_strategy_position_and_order: Dict[str, Union[Position, SpotOrder]] = {}
+    inflight_trading_pair = ""
     perp_connector = "binance_perpetual_testnet"
     spot_connector = "kucoin_paper_trade"
     # spot_connector = "kucoin"
-    trading_pair = "ETH-USDT"
-    trading_pairs_set = {"ETH-USDT", "XRP-USDT", "CRV-USDT", "DOGE-USDT", "UNI-USDT"}
-    # trading_pairs_set = {"ETH-USDT"}
+    trading_pairs_set = {"ETH-USDT", "XRP-USDT", "CRV-USDT",
+                         "DOGE-USDT", "UNI-USDT", "BCH-USDT",
+                         "EOS-USDT", "LTC-USDT", "LINK-USDT",
+                         "ALGO-USDT", "BNB-USDT", "ZRX-USDT"}
+
     funding_data = {}
 
     long_opportunities = {}
     short_opportunities = {}
 
     # trading_pairs_list = ["ETH-USDT", "XRP-USDT", "CRV-USDT", "DOGE-USDT", "UNI-USDT", "PEPE-USDT"]
-    leverage = 10
+    perp_trading_pairs: List
+    leverage = 5
     markets = {
         spot_connector: trading_pairs_set,
         perp_connector: trading_pairs_set
     }
 
-    perp_trading_pairs: List
-
     def __init__(self, connectors: Dict[str, ConnectorBase]):
         super().__init__(connectors)
+        self.perp_trading_pairs = self.get_perpetual_market_trading_pair_tuples()
         self.set_leverage()
-        # self.get_funding_rate_for_pair()
 
     '''
         Implement events from async clocks
     '''
 
     def set_leverage(self):
-        self.perp_trading_pairs = self.get_perpetual_market_trading_pair_tuples(True)
+        for trading_pair in self.perp_trading_pairs:
+            self.connectors[self.perp_connector].set_position_mode(PositionMode.HEDGE)
+            self.connectors[self.perp_connector].set_leverage(trading_pair=trading_pair, leverage=self.leverage)
+            self.logger().info(
+                f"Setting leverage to {self.leverage}x for {self.perp_connector} on {trading_pair}"
+            )
 
     def on_tick(self):
         if self.ready_to_trade:
@@ -119,68 +143,109 @@ class AutomatedCashNCarry(ScriptStrategyBase):
                     self.notify_hb_app_with_timestamp(
                         f"going to open short {op.trading_pair} @ {op.exchange} - rate {op.info.rate}")
                     self.notify_hb_app_with_timestamp(f"Heding BUY @ spot for {op.trading_pair}")
-
-                if op.action == StrategyAction.BUY_SPOT_SHORT_PERP:
-                    conversion_rate = RateOracle.get_instance().get_pair_rate(op.trading_pair)
-                    self.logger().info(f"got the conversion rate {conversion_rate}")
-                    amount = Decimal(100) / conversion_rate
-                    amount_perp = Decimal(100) / conversion_rate
-                    self.logger().info(f"perp amount {amount_perp}")
-                    # buy spot
-                    print("buying spot")
-                    spot_order_id = self.buy(
-                        self.spot_connector,
-                        op.trading_pair,
-                        amount,
-                        OrderType.MARKET,
-                        self.connectors[self.spot_connector].get_mid_price(op.trading_pair)
-                    )
-                    self.logger().info(f"order id -> {spot_order_id}")
-                    self.inflight_spot_orders.append(SpotOrder(op.trading_pair, self.spot_connector, True, amount,
-                                                               self.connectors[self.spot_connector].get_mid_price(
-                                                                   op.trading_pair)))
-
-                    self.place_perp_order(op.trading_pair, amount_perp)
+                    self.action(op)
 
             self.STRATEGY_INFLIGHT = True
+            self.monitor_funding_rates()
+
+    def monitor_funding_rates(self):
+        op = self.get_best_opportunity()
+        if op.trading_pair != self.inflight_trading_pair:
+            self.logger().info(f"Recieved a new top contender {op.trading_pair} - Evaluating...")
+            rate_diff = op.info.rate - self.get_funding_rate_for_pair(self.perp_connector,
+                                                                      self.inflight_trading_pair).rate
+
+            rate_diff_percent = np.abs(rate_diff / op.info.rate) * Decimal('100')
+
+            # Check if the rate difference is greater than 2%
+            if rate_diff_percent > Decimal('2'):
+                # Perform your action here
+                # For example:
+                print("Rate difference is greater than 2%")
+            else:
+                print("Rate difference is not greater than 2%")
+
+            self.notify_hb_app_with_timestamp(
+                f"NEW: going to open short {op.trading_pair} @ {op.exchange} - rate {op.info.rate}")
+            self.close_open_positions()
+            self.revert_spot_positions()
+            # Reset inflight map
+            self.inflight_strategy_position_and_order = {}
+            self.action(op)
+
+    def action(self, op: Opportunity):
+        if op.action == StrategyAction.BUY_SPOT_SHORT_PERP:
+            conversion_rate = RateOracle.get_instance().get_pair_rate(op.trading_pair)
+            amount = Decimal(100) / conversion_rate
+            amount_perp = Decimal(100) / conversion_rate
+            price = self.connectors[self.spot_connector].get_mid_price(op.trading_pair)
+            # buy spot
+
+            spot_order_id = self.buy(
+                self.spot_connector,
+                op.trading_pair,
+                amount,
+                OrderType.MARKET,
+                self.connectors[self.spot_connector].get_mid_price(op.trading_pair)
+            )
+            self.logger().info(f"order id -> {spot_order_id}")
+            perp_id = self.place_perp_order(op.trading_pair, amount_perp)
+
+            self.inflight_strategy_position_and_order[f"perpetual-{op.trading_pair}"] = Position(
+                op.trading_pair,
+                self.perp_connector,
+                PositionSide.SHORT,
+                amount_perp,
+                price,
+                perp_id
+            )
+            self.inflight_strategy_position_and_order[f"spot-{op.trading_pair}"] = SpotOrder(
+                op.trading_pair,
+                self.spot_connector,
+                True,
+                amount,
+                price,
+                spot_order_id
+            )
+            self.inflight_trading_pair = op.trading_pair
+
+        # elif op.action == StrategyAction.SELL_SPOT_LONG_PERP:
+        #     self.logger().info("Check if I have asset")
+        #     pass
 
     def on_stop(self):
-        self.logger().info("IM STOPPING MUST CLOSE OPEN POSITIONS")
         self.close_open_positions()
-        if len(self.inflight_spot_orders) > 0:
-            self.logger().info("reverting spot position")
-            order = self.inflight_spot_orders[0]
-            sell_order_id = self.sell(
-                order.exchange,
-                order.trading_pair,
-                order.amount,
-                OrderType.MARKET,
-                order.price
-            )
-            self.logger().info(f"reverting spot position {sell_order_id}")
-            self.inflight_spot_orders.pop()
+        self.revert_spot_positions()
+        self.inflight_trading_pair = ""
+        self.inflight_strategy_position_and_order = {}
+        self.STRATEGY_INFLIGHT = False
 
-    def place_perp_order(self, trading_pair: str, amount: Decimal):
+    def place_perp_order(self, trading_pair: str, amount: Decimal) -> str:
         # What is the difference between PositionMode oneway and hedge
-        perp_order_id = self.sell(
-            self.perp_connector,
-            trading_pair,
-            amount,
-            OrderType.MARKET,
-            self.connectors[self.spot_connector].get_mid_price(trading_pair),
-            PositionAction.OPEN
-        )
-        self.logger().info(f"open short perpetual -> {perp_order_id}")
+        if not self.dry_run:
+            perp_order_id = self.sell(
+                self.perp_connector,
+                trading_pair,
+                amount,
+                OrderType.MARKET,
+                self.connectors[self.spot_connector].get_mid_price(trading_pair),
+                PositionAction.OPEN
+            )
+            self.logger().info(f"open short perpetual -> {perp_order_id}")
+        else:
+            perp_order_id = 13337
+            self.logger().info(f"DRY-RUN    open short perpetual -> {perp_order_id}")
+
+        return perp_order_id
 
     '''
         Various getters
     '''
+
     def get_best_opportunity(self) -> Opportunity | None:
-        # Retrieve the first item from the sorted dictionary
         first_item_short: Opportunity = next(iter(self.short_opportunities.values()), Decimal('0'))
         first_item_long: Opportunity = next(iter(self.long_opportunities.values()), Decimal('0'))
 
-        # Check if there is any item in the dictionary
         if (first_item_short is not None or
                 first_item_long is not None):
             long_op_rate = abs(first_item_long.info.rate) if first_item_long != Decimal('0') else Decimal('0')
@@ -189,9 +254,7 @@ class AutomatedCashNCarry(ScriptStrategyBase):
                 return first_item_long
             else:
                 return first_item_short
-
         else:
-            # Handle the case when the dictionary is empty
             return None
 
     def rank_funding_info(self):
@@ -200,12 +263,10 @@ class AutomatedCashNCarry(ScriptStrategyBase):
                 key = f"{trading_pair}_{exchange}"
                 if info.rate > Decimal('0'):
                     if key not in self.short_opportunities or self.short_opportunities[key].info.rate != info.rate:
-                        print(f"S: updating rates for {trading_pair} info.rate={info.rate}")
                         self.short_opportunities[key] = Opportunity(
                             trading_pair, exchange, info, StrategyAction.BUY_SPOT_SHORT_PERP)
                 else:
                     if key not in self.long_opportunities or self.long_opportunities[key].info.rate != info.rate:
-                        print(f"L: updating rates for {trading_pair} info.rate={info.rate}")
                         self.long_opportunities[key] = Opportunity(
                             trading_pair, exchange, info, StrategyAction.SELL_SPOT_LONG_PERP)
 
@@ -225,6 +286,18 @@ class AutomatedCashNCarry(ScriptStrategyBase):
         for connector_name, connector in self.connectors.items():
             if "perpetual" not in connector_name:
                 pass
+
+    def revert_spot_positions(self):
+        if f"spot-{self.inflight_trading_pair}" in self.inflight_strategy_position_and_order:
+            order = self.inflight_strategy_position_and_order[f"spot-{self.inflight_trading_pair}"]
+            sell_order_id = self.sell(
+                order.exchange,
+                order.trading_pair,
+                order.amount,
+                OrderType.MARKET,
+                order.price
+            )
+            self.logger().info(f"reverting spot position {sell_order_id}")
 
     def close_open_positions(self):
         # Closing positions
@@ -306,7 +379,6 @@ class AutomatedCashNCarry(ScriptStrategyBase):
                             float(position.unrealized_pnl)
                         ])
         if not data:
-            # self.logger().info("There is no active open position")
             data.append(["NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"])
             df = pd.DataFrame(data=data, columns=columns)
             return df
@@ -336,7 +408,7 @@ class AutomatedCashNCarry(ScriptStrategyBase):
             ])
         return pd.DataFrame(data=data, columns=columns).replace(np.nan, '', regex=True)
 
-    def get_perpetual_market_trading_pair_tuples(self, set_leverage: bool) -> List[MarketTradingPairTuple]:
+    def get_perpetual_market_trading_pair_tuples(self) -> List[MarketTradingPairTuple]:
         """
         Returns a list of MarketTradingPairTuple for all perpetual connectors and trading pairs combination.
         """
@@ -344,11 +416,32 @@ class AutomatedCashNCarry(ScriptStrategyBase):
         for name, connector in self.connectors.items():
             if "perpetual" in name:
                 for trading_pair in self.markets[name]:
-                    if set_leverage:
-                        connector.set_position_mode(PositionMode.HEDGE)
-                        connector.set_leverage(trading_pair=trading_pair, leverage=self.leverage)
-                        self.logger().info(
-                            f"Setting leverage to {self.leverage}x for {self.perp_connector} on {trading_pair}"
-                        )
                     result.append(self._market_trading_pair_tuple(name, trading_pair))
         return result
+
+    '''
+        Events handling
+    '''
+
+    def did_complete_buy_order(self, event: BuyOrderCompletedEvent):
+        """
+        Method called when the connector notifies a buy order has been completed (fully filled)
+        """
+        self.logger().info(f"The buy order {event.order_id} has been completed")
+
+    def did_complete_sell_order(self, event: SellOrderCompletedEvent):
+        """
+        Method called when the connector notifies a sell order has been completed (fully filled)
+        """
+        self.logger().info(f"The sell order {event.order_id} has been completed")
+
+    def did_fail_order(self, event: MarketOrderFailureEvent):
+        """
+        Method called when the connector notifies an order has failed
+        """
+        self.logger().info(f"The order {event.order_id} failed")
+        # self.close_open_positions()
+
+    def did_close_position(self, closed_position_event: RangePositionClosedEvent):
+        self.logger().info(
+            f"Closing position {closed_position_event.token_0}-{closed_position_event.token_1}. {closed_position_event.token_id} --- fee {closed_position_event.claimed_fee_0} or {closed_position_event.claimed_fee_1}")
