906f43a51f33ad66ec6046b20f202d5efae59722
Konstantinos Panayiotou <klpanagi@gmail.com>
2024-03-31 16:08:58 +0300

    Cleaup web_utils

--- hummingbot/connector/derivative/bybit_perpetual/bybit_perpetual_web_utils.py
+++ hummingbot/connector/derivative/bybit_perpetual/bybit_perpetual_web_utils.py
@@ -1,11 +1,9 @@
 from typing import Any, Callable, Dict, List, Optional
 
 from hummingbot.connector.derivative.bybit_perpetual import bybit_perpetual_constants as CONSTANTS
-from hummingbot.connector.derivative.bybit_perpetual.bybit_perpetual_utils import is_linear_perpetual
 from hummingbot.connector.time_synchronizer import TimeSynchronizer
 from hummingbot.connector.utils import TimeSynchronizerRESTPreProcessor
 from hummingbot.core.api_throttler.async_throttler import AsyncThrottler
-from hummingbot.core.api_throttler.data_types import LinkedLimitWeightPair, RateLimit
 from hummingbot.core.web_assistant.auth import AuthBase
 from hummingbot.core.web_assistant.connections.data_types import RESTMethod, RESTRequest
 from hummingbot.core.web_assistant.rest_pre_processors import RESTPreProcessorBase
@@ -122,7 +120,8 @@ async def get_current_server_time(
         api_factory=api_factory,
         throttler=throttler,
         domain=domain,
-        method=RESTMethod.GET)
+        method=RESTMethod.GET
+    )
 
     # Use nanoseconds for higher resolution
     server_time = float(response["result"]["timeNano"]) / 10**9
@@ -151,75 +150,3 @@ def payload_from_message(message: Dict[str, Any]) -> List[Dict[str, Any]]:
 def build_api_factory_without_time_synchronizer_pre_processor(throttler: AsyncThrottler) -> WebAssistantsFactory:
     api_factory = WebAssistantsFactory(throttler=throttler)
     return api_factory
-
-
-def get_rest_url_for_endpoint(
-    endpoint: Dict[str, str], trading_pair: Optional[str] = None, domain: str = CONSTANTS.DEFAULT_DOMAIN
-):
-    market = _get_rest_api_market_for_endpoint(trading_pair)
-    variant = domain if domain else CONSTANTS.DEFAULT_DOMAIN
-    return CONSTANTS.REST_URLS.get(variant) + endpoint[market]
-
-
-def get_pair_specific_limit_id(base_limit_id: str, trading_pair: str) -> str:
-    limit_id = f"{base_limit_id}-{trading_pair}"
-    return limit_id
-
-
-def get_rest_api_limit_id_for_endpoint(endpoint: Dict[str, str], trading_pair: Optional[str] = None) -> str:
-    market = _get_rest_api_market_for_endpoint(trading_pair)
-    limit_id = endpoint[market]
-    if trading_pair is not None:
-        limit_id = get_pair_specific_limit_id(limit_id, trading_pair)
-    return limit_id
-
-
-def _wss_url(endpoint: Dict[str, str], connector_variant_label: Optional[str]) -> str:
-    variant = connector_variant_label if connector_variant_label else CONSTANTS.DEFAULT_DOMAIN
-    return endpoint.get(variant)
-
-
-def wss_linear_public_url(connector_variant_label: Optional[str]) -> str:
-    return _wss_url(CONSTANTS.WSS_LINEAR_PUBLIC_URLS, connector_variant_label)
-
-
-def wss_linear_private_url(connector_variant_label: Optional[str]) -> str:
-    return _wss_url(CONSTANTS.WSS_LINEAR_PRIVATE_URLS, connector_variant_label)
-
-
-def wss_non_linear_public_url(connector_variant_label: Optional[str]) -> str:
-    return _wss_url(CONSTANTS.WSS_NON_LINEAR_PUBLIC_URLS, connector_variant_label)
-
-
-def wss_non_linear_private_url(connector_variant_label: Optional[str]) -> str:
-    return _wss_url(CONSTANTS.WSS_NON_LINEAR_PRIVATE_URLS, connector_variant_label)
-
-
-def _build_private_general_rate_limits() -> List[RateLimit]:
-    rate_limits = [
-        RateLimit(  # same for linear and non-linear
-            limit_id=CONSTANTS.GET_WALLET_BALANCE_PATH_URL[CONSTANTS.NON_LINEAR_MARKET],
-            limit=120,
-            time_interval=60,
-            linked_limits=[LinkedLimitWeightPair(CONSTANTS.GET_LIMIT_ID),
-                           LinkedLimitWeightPair(CONSTANTS.NON_LINEAR_PRIVATE_BUCKET_120_B_LIMIT_ID)],
-        ),
-        RateLimit(  # same for linear and non-linear
-            limit_id=CONSTANTS.SET_POSITION_MODE_URL[CONSTANTS.LINEAR_MARKET],
-            limit=120,
-            time_interval=60,
-            linked_limits=[LinkedLimitWeightPair(CONSTANTS.GET_LIMIT_ID),
-                           LinkedLimitWeightPair(CONSTANTS.NON_LINEAR_PRIVATE_BUCKET_120_B_LIMIT_ID)],
-        ),
-    ]
-    return rate_limits
-
-
-def _get_rest_api_market_for_endpoint(trading_pair: Optional[str] = None) -> str:
-    # The default selection should be linear because general requests such as setting position mode
-    # exists only for linear market and is without a trading pair
-    if trading_pair is None or is_linear_perpetual(trading_pair):
-        market = CONSTANTS.LINEAR_MARKET
-    else:
-        market = CONSTANTS.NON_LINEAR_MARKET
-    return market
