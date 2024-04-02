411d77f830fc57425400a47da09789fbeb936151
cardosofede <federico.cardoso.e@gmail.com>
2024-04-02 17:17:54 -0300

    (feat) add xemm executor class

---
+++ hummingbot/smart_components/executors/xemm_executor/xemm_executor.py
@@ -0,0 +1,95 @@
+import logging
+from decimal import Decimal
+
+from hummingbot.connector.utils import split_hb_trading_pair
+from hummingbot.core.data_type.common import OrderType, PriceType, TradeType
+from hummingbot.core.data_type.order_candidate import OrderCandidate
+from hummingbot.logger import HummingbotLogger
+from hummingbot.smart_components.executors.executor_base import ExecutorBase
+from hummingbot.smart_components.executors.xemm_executor.data_types import XEMMExecutorConfig
+from hummingbot.smart_components.models.executors import CloseType
+from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
+
+
+class XEMMExecutor(ExecutorBase):
+    _logger = None
+
+    @classmethod
+    def logger(cls) -> HummingbotLogger:
+        if cls._logger is None:
+            cls._logger = logging.getLogger(__name__)
+        return cls._logger
+
+    @staticmethod
+    def _are_tokens_interchangeable(first_token: str, second_token: str):
+        interchangeable_tokens = [
+            {"WETH", "ETH"},
+            {"WBTC", "BTC"},
+            {"WBNB", "BNB"},
+            {"WMATIC", "MATIC"},
+            {"WAVAX", "AVAX"},
+            {"WONE", "ONE"},
+        ]
+        same_token_condition = first_token == second_token
+        tokens_interchangeable_condition = any(({first_token, second_token} <= interchangeable_pair
+                                                for interchangeable_pair
+                                                in interchangeable_tokens))
+        # for now, we will consider all the stablecoins interchangeable
+        stable_coins_condition = "USD" in first_token and "USD" in second_token
+        return same_token_condition or tokens_interchangeable_condition or stable_coins_condition
+
+    def is_arbitrage_valid(self, pair1, pair2):
+        base_asset1, quote_asset1 = split_hb_trading_pair(pair1)
+        base_asset2, quote_asset2 = split_hb_trading_pair(pair2)
+        return self._are_tokens_interchangeable(base_asset1, base_asset2) and \
+            self._are_tokens_interchangeable(quote_asset1, quote_asset2)
+
+    def __init__(self, strategy: ScriptStrategyBase, config: XEMMExecutorConfig, update_interval: float = 1.0):
+        if not self.is_arbitrage_valid(pair1=config.buying_market.trading_pair,
+                                       pair2=config.selling_market.trading_pair):
+            raise Exception("XEMM is not valid since the trading pairs are not interchangeable.")
+        self.config = config
+        if config.maker_side == TradeType.BUY:
+            self.maker_connector = config.buying_market.connector_name
+            self.maker_trading_pair = config.buying_market.trading_pair
+            self.maker_order_side = TradeType.BUY
+            self.taker_connector = config.selling_market.connector_name
+            self.taker_trading_pair = config.selling_market.trading_pair
+            self.taker_order_side = TradeType.SELL
+        else:
+            self.maker_connector = config.selling_market.connector_name
+            self.maker_trading_pair = config.selling_market.trading_pair
+            self.maker_order_side = TradeType.SELL
+            self.taker_connector = config.buying_market.connector_name
+            self.taker_trading_pair = config.buying_market.trading_pair
+            self.taker_order_side = TradeType.BUY
+        self._last_buy_price = Decimal("1")
+        self._last_sell_price = Decimal("1")
+        self._last_tx_cost = Decimal("1")
+        super().__init__(strategy=strategy,
+                         connectors=[config.buying_market.connector_name, config.selling_market.connector_name],
+                         config=config, update_interval=update_interval)
+
+    def validate_sufficient_balance(self):
+        mid_price = self.get_price(self.maker_connector, self.maker_trading_pair,
+                                   price_type=PriceType.MidPrice)
+        maker_order_candidate = OrderCandidate(
+            trading_pair=self.maker_trading_pair,
+            is_maker=True,
+            order_type=OrderType.LIMIT,
+            order_side=self.maker_order_side,
+            amount=self.config.order_amount,
+            price=mid_price,)
+        taker_order_candidate = OrderCandidate(
+            trading_pair=self.taker_trading_pair,
+            is_maker=False,
+            order_type=OrderType.LIMIT,
+            order_side=self.taker_order_side,
+            amount=self.config.order_amount,
+            price=mid_price,)
+        maker_adjusted_candidate = self.adjust_order_candidates(self.maker_connector, [maker_order_candidate])[0]
+        taker_adjusted_candidate = self.adjust_order_candidates(self.taker_connector, [taker_order_candidate])[0]
+        if maker_adjusted_candidate.amount == Decimal("0") or taker_adjusted_candidate.amount == Decimal("0"):
+            self.close_type = CloseType.INSUFFICIENT_BALANCE
+            self.logger().error("Not enough budget to open position.")
+            self.stop()
