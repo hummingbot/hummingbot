ef7ceef349264141dd206f82a2a31b998c4fa578
cardosofede <federico.cardoso.e@gmail.com>
2024-04-02 17:17:36 -0300

    (feat) initialize xemm data types

---
+++ hummingbot/smart_components/executors/xemm_executor/data_types.py
@@ -0,0 +1,13 @@
+from decimal import Decimal
+
+from hummingbot.core.data_type.common import TradeType
+from hummingbot.smart_components.executors.data_types import ConnectorPair, ExecutorConfigBase
+
+
+class XEMMExecutorConfig(ExecutorConfigBase):
+    type = "xemm_executor"
+    buying_market: ConnectorPair
+    selling_market: ConnectorPair
+    maker_side: TradeType
+    order_amount: Decimal
+    min_profitability: Decimal
