0c59b2790c6c47afd589af4da68afbcbfc90fedd
cardosofede <federico.cardoso.e@gmail.com>
2024-04-19 17:25:45 -0300

    (feat) increase executors buffer

--- scripts/v2_generic_with_cash_out.py
+++ scripts/v2_generic_with_cash_out.py
@@ -35,6 +35,7 @@ class GenericV2StrategyWithCashOut(StrategyV2Base):
         super().__init__(connectors, config)
         self.config = config
         self.cashing_out = False
+        self.closed_executors_buffer: int = 20
         if self.config.time_to_cash_out:
             self.cash_out_time = self.config.time_to_cash_out + time.time()
         else:
