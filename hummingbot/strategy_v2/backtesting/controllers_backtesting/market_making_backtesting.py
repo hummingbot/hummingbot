e84e82d790471bf6136880ad55ca6342ac5f9eb9
cardosofede <federico.cardoso.e@gmail.com>
2024-05-06 16:44:36 -0300

    (feat) clean up backtesting folder

--- hummingbot/strategy_v2/backtesting/controllers_backtesting/market_making_backtesting.py
+++ hummingbot/strategy_v2/backtesting/controllers_backtesting/market_making_backtesting.py
@@ -5,7 +5,7 @@ import pandas as pd
 from hummingbot.strategy_v2.backtesting.backtesting_engine_base import BacktestingEngineBase
 
 
-class MarketMakingBacktesting2(BacktestingEngineBase):
+class MarketMakingBacktesting(BacktestingEngineBase):
     def update_processed_data(self, row: pd.Series):
         self.controller.processed_data["reference_price"] = Decimal(row["reference_price"])
         self.controller.processed_data["spread_multiplier"] = Decimal(row["spread_multiplier"])
