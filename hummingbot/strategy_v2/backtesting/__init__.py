4dfd54569cb4a2f2c2cdcb07f5f7cc8546c64fee
cardosofede <federico.cardoso.e@gmail.com>
2024-05-15 15:45:04 -0400

    (feat) improve library modules imports

---
+++ hummingbot/strategy_v2/backtesting/__init__.py
@@ -0,0 +1,9 @@
+from hummingbot.strategy_v2.backtesting.controllers_backtesting.directional_trading_backtesting import (
+    DirectionalTradingBacktesting,
+)
+from hummingbot.strategy_v2.backtesting.controllers_backtesting.market_making_backtesting import MarketMakingBacktesting
+
+__all__ = [
+    "DirectionalTradingBacktesting",
+    "MarketMakingBacktesting",
+]
