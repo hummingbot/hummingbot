7a93d2e52170354dce8812e1b03b2828ca60f93d
drupman <tomasgaudino@hotmail.com>
2024-06-11 18:29:09 -0300

    (feat) refactor binance spot candles

--- hummingbot/data_feed/candles_feed/binance_spot_candles/binance_spot_candles.py
+++ hummingbot/data_feed/candles_feed/binance_spot_candles/binance_spot_candles.py
@@ -1,12 +1,9 @@
-import asyncio
 import logging
-from typing import Any, Dict, Optional
+from typing import Optional
 
 import numpy as np
 
-from hummingbot.core.network_iterator import NetworkStatus, safe_ensure_future
-from hummingbot.core.web_assistant.connections.data_types import WSJSONRequest
-from hummingbot.core.web_assistant.ws_assistant import WSAssistant
+from hummingbot.core.network_iterator import NetworkStatus
 from hummingbot.data_feed.candles_feed.binance_spot_candles import constants as CONSTANTS
 from hummingbot.data_feed.candles_feed.candles_base import CandlesBase
 from hummingbot.logger import HummingbotLogger
@@ -64,71 +61,46 @@ class BinanceSpotCandles(CandlesBase):
     async def fetch_candles(self,
                             start_time: Optional[int] = None,
                             end_time: Optional[int] = None,
-                            limit: Optional[int] = 500):
+                            limit: Optional[int] = CONSTANTS.MAX_RESULTS_PER_CANDLESTICK_REST_REQUEST):
         rest_assistant = await self._api_factory.get_rest_assistant()
-        params = {"symbol": self._ex_trading_pair, "interval": self.interval, "limit": limit}
+        params = {
+            "symbol": self._ex_trading_pair,
+            "interval": self.interval,
+            "limit": limit
+        }
         if start_time:
-            params["startTime"] = start_time
+            params["startTime"] = start_time * 1000
         if end_time:
-            params["endTime"] = end_time
-        candles = await rest_assistant.execute_request(url=self.candles_url,
-                                                       throttler_limit_id=CONSTANTS.CANDLES_ENDPOINT,
-                                                       params=params)
+            params["endTime"] = end_time * 1000
+        response = await rest_assistant.execute_request(url=self.candles_url,
+                                                        throttler_limit_id=CONSTANTS.CANDLES_ENDPOINT,
+                                                        params=params)
+        candles = np.array([[self.ensure_timestamp_in_seconds(row[0]), row[1], row[2], row[3], row[4], row[5],
+                             0., 0., 0., 0.]
+                            for row in response]
+                           ).astype(float)
+        return candles
 
-        return np.array(candles)[:, [0, 1, 2, 3, 4, 5, 7, 8, 9, 10]].astype(float)
+    def ws_subscription_payload(self):
+        candle_params = [f"{self._ex_trading_pair.lower()}@kline_{self.interval}"]
+        payload = {
+            "method": "SUBSCRIBE",
+            "params": candle_params,
+            "id": 1
+        }
+        return payload
 
-    async def _subscribe_channels(self, ws: WSAssistant):
-        """
-        Subscribes to the candles events through the provided websocket connection.
-        :param ws: the websocket assistant used to connect to the exchange
-        """
-        try:
-            candle_params = []
-            candle_params.append(f"{self._ex_trading_pair.lower()}@kline_{self.interval}")
-            payload = {
-                "method": "SUBSCRIBE",
-                "params": candle_params,
-                "id": 1
-            }
-            subscribe_candles_request: WSJSONRequest = WSJSONRequest(payload=payload)
-
-            await ws.send(subscribe_candles_request)
-            self.logger().info("Subscribed to public klines...")
-        except asyncio.CancelledError:
-            raise
-        except Exception:
-            self.logger().error(
-                "Unexpected error occurred subscribing to public klines...",
-                exc_info=True
-            )
-            raise
-
-    async def _process_websocket_messages(self, websocket_assistant: WSAssistant):
-        async for ws_response in websocket_assistant.iter_messages():
-            data: Dict[str, Any] = ws_response.data
-            if data is not None and data.get("e") == "kline":  # data will be None when the websocket is disconnected
-                timestamp = data["k"]["t"]
-                open = data["k"]["o"]
-                high = data["k"]["h"]
-                low = data["k"]["l"]
-                close = data["k"]["c"]
-                volume = data["k"]["v"]
-                quote_asset_volume = data["k"]["q"]
-                n_trades = data["k"]["n"]
-                taker_buy_base_volume = data["k"]["V"]
-                taker_buy_quote_volume = data["k"]["Q"]
-                if len(self._candles) == 0:
-                    self._candles.append(np.array([timestamp, open, high, low, close, volume,
-                                                   quote_asset_volume, n_trades, taker_buy_base_volume,
-                                                   taker_buy_quote_volume]))
-                    safe_ensure_future(self.fill_historical_candles())
-                elif timestamp > int(self._candles[-1][0]):
-                    # TODO: validate also that the diff of timestamp == interval (issue with 1M interval).
-                    self._candles.append(np.array([timestamp, open, high, low, close, volume,
-                                                   quote_asset_volume, n_trades, taker_buy_base_volume,
-                                                   taker_buy_quote_volume]))
-                elif timestamp == int(self._candles[-1][0]):
-                    self._candles.pop()
-                    self._candles.append(np.array([timestamp, open, high, low, close, volume,
-                                                   quote_asset_volume, n_trades, taker_buy_base_volume,
-                                                   taker_buy_quote_volume]))
+    def _parse_websocket_message(self, data: dict):
+        candles_row_dict = {}
+        if data is not None and data.get("e") == "kline":  # data will be None when the websocket is disconnected
+            candles_row_dict["timestamp"] = data["k"]["t"]
+            candles_row_dict["open"] = data["k"]["o"]
+            candles_row_dict["low"] = data["k"]["h"]
+            candles_row_dict["high"] = data["k"]["l"]
+            candles_row_dict["close"] = data["k"]["c"]
+            candles_row_dict["volume"] = data["k"]["v"]
+            candles_row_dict["quote_asset_volume"] = data["k"]["q"]
+            candles_row_dict["n_trades"] = data["k"]["n"]
+            candles_row_dict["taker_buy_base_volume"] = data["k"]["V"]
+            candles_row_dict["taker_buy_quote_volume"] = data["k"]["Q"]
+            return candles_row_dict
