0b33dc04b0d46e2df1f5c95462d630670f953de6
Konstantinos Panayiotou <klpanagi@gmail.com>
2024-06-12 22:54:06 +0300

    Fixes bybit derivative connector utils

--- test/hummingbot/connector/derivative/bybit_perpetual/test_bybit_perpetual_utils.py
+++ test/hummingbot/connector/derivative/bybit_perpetual/test_bybit_perpetual_utils.py
@@ -8,30 +8,26 @@ from hummingbot.connector.derivative.bybit_perpetual import bybit_perpetual_util
 class BybitPerpetualUtilsTests(TestCase):
     def test_is_exchange_information_valid(self):
         exchange_info = {
-            "name": "BTCUSD",
-            "alias": "BTCUSD",
+            "symbol": "BTCUSDT",
+            "baseCoin": "BTC",
+            "quoteCoin": "USDT",
+            "innovation": "0",
             "status": "Trading",
-            "base_currency": "BTC",
-            "quote_currency": "USD",
-            "price_scale": 2,
-            "taker_fee": "0.00075",
-            "maker_fee": "-0.00025",
-            "funding_interval": 480,
-            "leverage_filter": {
-                "min_leverage": 1,
-                "max_leverage": 100,
-                "leverage_step": "0.01"
+            "marginTrading": "both",
+            "lotSizeFilter": {
+                "basePrecision": "0.000001",
+                "quotePrecision": "0.00000001",
+                "minOrderQty": "0.000048",
+                "maxOrderQty": "71.73956243",
+                "minOrderAmt": "1",
+                "maxOrderAmt": "2000000"
             },
-            "price_filter": {
-                "min_price": "0.5",
-                "max_price": "999999.5",
-                "tick_size": "0.5"
+            "priceFilter": {
+                "tickSize": "0.01"
             },
-            "lot_size_filter": {
-                "max_trading_qty": 1000000,
-                "min_trading_qty": 1,
-                "qty_step": 1,
-                "post_only_max_trading_qty": "5000000"
+            "riskParameters": {
+                "limitParameter": "0.05",
+                "marketParameter": "0.05"
             }
         }
 
