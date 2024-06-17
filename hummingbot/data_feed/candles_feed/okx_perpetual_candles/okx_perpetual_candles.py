a670d97b796c38cbe235862c0f1a3b4735699e7a
drupman <tomasgaudino@hotmail.com>
2024-06-17 12:41:48 -0300

    (feat) refactor okx perpetual candles

--- hummingbot/data_feed/candles_feed/okx_perpetual_candles/okx_perpetual_candles.py
+++ hummingbot/data_feed/candles_feed/okx_perpetual_candles/okx_perpetual_candles.py
@@ -1,15 +1,8 @@
-import asyncio
 import logging
-from typing import Any, Dict, Optional
+from typing import List, Optional
 
-import numpy as np
-import pandas as pd
-
-from hummingbot.core.network_iterator import NetworkStatus, safe_ensure_future
-from hummingbot.core.web_assistant.connections.data_types import WSJSONRequest
-from hummingbot.core.web_assistant.ws_assistant import WSAssistant
+from hummingbot.core.network_iterator import NetworkStatus
 from hummingbot.data_feed.candles_feed.candles_base import CandlesBase
-from hummingbot.data_feed.candles_feed.data_types import HistoricalCandlesConfig
 from hummingbot.data_feed.candles_feed.okx_perpetual_candles import constants as CONSTANTS
 from hummingbot.logger import HummingbotLogger
 
@@ -67,6 +60,10 @@ class OKXPerpetualCandles(CandlesBase):
         return self.rest_url + CONSTANTS.CANDLES_ENDPOINT
 
     @property
+    def candles_endpoint(self):
+        return CONSTANTS.CANDLES_ENDPOINT
+
+    @property
     def rate_limits(self):
         return CONSTANTS.RATE_LIMITS
 
@@ -83,91 +80,45 @@ class OKXPerpetualCandles(CandlesBase):
     def get_exchange_trading_pair(self, trading_pair):
         return f"{trading_pair}-SWAP"
 
-    async def fetch_candles(self, start_time: Optional[int] = None, end_time: Optional[int] = None,
-                            limit: Optional[int] = CONSTANTS.MAX_RESULTS_PER_CANDLESTICK_REST_REQUEST):
-        rest_assistant = await self._api_factory.get_rest_assistant()
-        params = {"instId": self._ex_trading_pair, "bar": CONSTANTS.INTERVALS[self.interval], "limit": limit}
+    def _get_rest_candles_params(self, start_time: Optional[int] = None, end_time: Optional[int] = None,
+                                 limit: Optional[int] = CONSTANTS.MAX_RESULTS_PER_CANDLESTICK_REST_REQUEST) -> dict:
+        params = {
+            "instId": self._ex_trading_pair,
+            "bar": CONSTANTS.INTERVALS[self.interval]
+        }
         if end_time:
-            params["after"] = end_time
+            params["after"] = end_time * 1000
         if start_time:
-            params["before"] = start_time
-        candles = await rest_assistant.execute_request(url=self.candles_url,
-                                                       throttler_limit_id=CONSTANTS.CANDLES_ENDPOINT,
-                                                       params=params)
+            params["before"] = start_time * 1000
+        return params
 
-        arr = [[row[0], row[1], row[2], row[3], row[4], row[6], row[7], 0., 0., 0.] for row in candles["data"]]
-        return np.array(arr).astype(float)
+    def _parse_rest_candles(self, data: dict, end_time: Optional[int] = None) -> List[List[float]]:
+        return [
+            [
+                self.ensure_timestamp_in_seconds(row[0]), row[1], row[2], row[3], row[4], row[6], row[7], 0., 0., 0.
+            ]
+            for row in data["data"] if self.ensure_timestamp_in_seconds(row[0]) < end_time
+        ][::-1]
 
-    async def get_historical_candles(self, config: HistoricalCandlesConfig):
-        try:
-            all_candles = []
-            current_start_time = config.start_time
-            while current_start_time <= config.end_time:
-                current_end_time = current_start_time + self.interval_to_milliseconds_dict[config.interval] * CONSTANTS.MAX_RESULTS_PER_CANDLESTICK_REST_REQUEST
-                fetched_candles = await self.fetch_candles(end_time=current_end_time)
-                if fetched_candles.size == 0:
-                    break
+    def ws_subscription_payload(self):
+        candle_args = [{"channel": f"candle{CONSTANTS.INTERVALS[self.interval]}", "instId": self._ex_trading_pair}]
+        return {
+            "op": "subscribe",
+            "args": candle_args
+        }
 
-                all_candles.append(fetched_candles[::-1])
-                last_timestamp = fetched_candles[0][0]  # Assuming the first column is the timestamp
-                current_start_time = int(last_timestamp)
-
-            final_candles = np.concatenate(all_candles, axis=0) if all_candles else np.array([])
-            candles_df = pd.DataFrame(final_candles, columns=self.columns)
-            candles_df.drop_duplicates(subset=["timestamp"], inplace=True)
-            return candles_df
-        except Exception as e:
-            self.logger().exception(f"Error fetching historical candles: {str(e)}")
-
-    async def _subscribe_channels(self, ws: WSAssistant):
-        """
-        Subscribes to the candles events through the provided websocket connection.
-        :param ws: the websocket assistant used to connect to the exchange
-        """
-        try:
-            candle_args = []
-            candle_args.append({"channel": f"candle{CONSTANTS.INTERVALS[self.interval]}", "instId": self._ex_trading_pair})
-            payload = {
-                "op": "subscribe",
-                "args": candle_args
-            }
-            subscribe_candles_request: WSJSONRequest = WSJSONRequest(payload=payload)
-
-            await ws.send(subscribe_candles_request)
-            self.logger().info("Subscribed to public klines...")
-        except asyncio.CancelledError:
-            raise
-        except Exception:
-            self.logger().error(
-                "Unexpected error occurred subscribing to public klines...",
-                exc_info=True
-            )
-            raise
-
-    async def _process_websocket_messages(self, websocket_assistant: WSAssistant):
-        async for ws_response in websocket_assistant.iter_messages():
-            data: Dict[str, Any] = ws_response.data
-            if data is not None and "data" in data:  # data will be None when the websocket is disconnected
-                candles = data["data"][0]
-                timestamp = candles[0]
-                open = candles[1]
-                high = candles[2]
-                low = candles[3]
-                close = candles[4]
-                volume = candles[6]
-                quote_asset_volume = candles[7]
-                n_trades = 0.
-                taker_buy_base_volume = 0.
-                taker_buy_quote_volume = 0.
-
-                candles_row = np.array([timestamp, open, high, low, close, volume,
-                                        quote_asset_volume, n_trades, taker_buy_base_volume,
-                                        taker_buy_quote_volume]).astype(float)
-                if len(self._candles) == 0:
-                    self._candles.append(candles_row)
-                    safe_ensure_future(self.fill_historical_candles())
-                elif int(timestamp) > int(self._candles[-1][0]):
-                    self._candles.append(candles_row)
-                elif int(timestamp) == int(self._candles[-1][0]):
-                    self._candles.pop()
-                    self._candles.append(candles_row)
+    def _parse_websocket_message(self, data: dict):
+        candles_row_dict = {}
+        if data is not None and "data" in data:  # data will be None when the websocket is disconnected
+            candles = data["data"][0]
+            candles_row_dict["timestamp"] = self.ensure_timestamp_in_seconds(candles[0])
+            candles_row_dict["open"] = candles[1]
+            candles_row_dict["high"] = candles[2]
+            candles_row_dict["low"] = candles[3]
+            candles_row_dict["close"] = candles[4]
+            candles_row_dict["volume"] = candles[6]
+            candles_row_dict["quote_asset_volume"] = candles[7]
+            candles_row_dict["n_trades"] = 0.
+            candles_row_dict["taker_buy_base_volume"] = 0.
+            candles_row_dict["taker_buy_quote_volume"] = 0.
+            return candles_row_dict
