4e31bec7284aba0ac59775a3e34fd630e1775db2
cardosofede <federico.cardoso.e@gmail.com>
2024-07-12 15:30:39 +0300

    (feat) simplify test

--- test/hummingbot/core/test_network_base.py
+++ test/hummingbot/core/test_network_base.py
@@ -1,16 +1,17 @@
 import asyncio
+from test.isolated_asyncio_wrapper_test_case import IsolatedAsyncioWrapperTestCase
+
 from hummingbot.core.network_base import NetworkBase
 from hummingbot.core.network_iterator import NetworkStatus
-import unittest
 
 
 class SampleNetwork(NetworkBase):
     async def check_network(self) -> NetworkStatus:
-        "Override check_network to always return connected"
+        """Override check_network to always return connected"""
         return NetworkStatus.CONNECTED
 
 
-class NetworkBaseTest(unittest.TestCase):
+class NetworkBaseTest(IsolatedAsyncioWrapperTestCase):
     def test_init(self):
         """
         This lets us know if the initial values have changed and extends
@@ -35,7 +36,7 @@ class NetworkBaseTest(unittest.TestCase):
         nb.check_network_timeout = 45.0
         self.assertEqual(nb.check_network_timeout, 45.0)
 
-    def test_network(self):
+    async def test_network(self):
         """
         NetworkBase has a couple of method sketches that do not do anything
         but are used by child classes.
@@ -43,17 +44,14 @@ class NetworkBaseTest(unittest.TestCase):
 
         nb = NetworkBase()
 
-        self.assertEqual(asyncio.get_event_loop().run_until_complete(nb.start_network()), None)
-
-        self.assertEqual(asyncio.get_event_loop().run_until_complete(nb.stop_network()), None)
-
-        self.assertEqual(asyncio.get_event_loop().run_until_complete(nb.check_network()), NetworkStatus.NOT_CONNECTED)
+        await nb.start_network()
+        await nb.stop_network()
+        self.assertEqual(await nb.check_network(), NetworkStatus.NOT_CONNECTED)
 
     def test_start_and_stop_network(self):
         """
         Assert that start and stop update the started property.
         """
-
         nb = NetworkBase()
 
         nb.start()
