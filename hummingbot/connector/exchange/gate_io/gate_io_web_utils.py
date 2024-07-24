dfb3874874db429b0e4fa598202718a841c8759e
= <=>
2024-07-24 08:08:58 +0000

    (fix) parameter name for public_rest_url in get_current_server_time

--- hummingbot/connector/exchange/gate_io/gate_io_web_utils.py
+++ hummingbot/connector/exchange/gate_io/gate_io_web_utils.py
@@ -65,7 +65,7 @@ async def get_current_server_time(
     api_factory = build_api_factory_without_time_synchronizer_pre_processor(throttler=throttler)
     rest_assistant = await api_factory.get_rest_assistant()
     response = await rest_assistant.execute_request(
-        url=public_rest_url(path_url=CONSTANTS.SERVER_TIME_URL, domain=domain),
+        url=public_rest_url(endpoint=CONSTANTS.SERVER_TIME_URL, domain=domain),
         method=RESTMethod.GET,
         throttler_limit_id=CONSTANTS.SERVER_TIME_URL,
     )
