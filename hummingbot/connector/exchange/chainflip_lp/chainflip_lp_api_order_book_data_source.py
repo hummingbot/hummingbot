bf61e0ae02f4baea70f73063a791375c9cd7415c
jaykayudo <jaykayudo@gmail.com>
2024-07-27 01:00:58 +0100

    (feat) add chainflip lp connector

---
+++ hummingbot/connector/exchange/chainflip_lp/chainflip_lp_api_order_book_data_source.py
@@ -0,0 +1,90 @@
+import asyncio
+from typing import TYPE_CHECKING, Any, Dict, List, Optional
+
+from hummingbot.connector.exchange.chainflip_lp import chainflip_lp_constants as CONSTANTS
+from hummingbot.connector.exchange.chainflip_lp.chainflip_lp_data_source import ChainflipLPDataSource
+from hummingbot.connector.exchange.chainflip_lp.chainflip_lp_data_formatter import DataFormatter
+from hummingbot.core.data_type.order_book_message import OrderBookMessage
+from hummingbot.core.data_type.order_book_tracker_data_source import OrderBookTrackerDataSource
+from hummingbot.core.event.event_forwarder import EventForwarder
+from hummingbot.core.web_assistant.connections.data_types import WSJSONRequest
+from hummingbot.core.event.events import OrderBookEvent
+from hummingbot.core.web_assistant.web_assistants_factory import WebAssistantsFactory
+from hummingbot.core.web_assistant.ws_assistant import WSAssistant
+
+if TYPE_CHECKING:
+    from hummingbot.connector.exchange.chainflip_lp.chainflip_lp_exchange import ChainflipLPExchange
+
+
+class ChainflipLPAPIOrderBookDataSource(OrderBookTrackerDataSource):
+    def __init__(
+        self,
+        trading_pairs: List[str],
+        connector: "ChainflipLPExchange",
+        data_source: "ChainflipLPDataSource",
+        api_factory: "WebAssistantsFactory",
+        domain: str = CONSTANTS.DEFAULT_DOMAIN,
+
+    ):
+        super().__init__(trading_pairs=trading_pairs)
+        self._ev_loop = asyncio.get_event_loop()
+        self._connector = connector
+        self._data_source = data_source
+        self._domain = domain
+        self._api_factory = api_factory
+        # self._forwarders = []
+        # self._configure_event_forwarders()
+
+    async def get_last_traded_prices(self,
+                                     trading_pairs: List[str],
+                                     domain: Optional[str] = None) -> Dict[str, float]:
+        return await self._connector.get_last_traded_prices(trading_pairs=trading_pairs)
+    async def _order_book_snapshot(self, trading_pair: str) -> OrderBookMessage:
+        snapshot = await self._data_source.order_book_snapshot(trading_pair=trading_pair)
+        return snapshot
+    async def listen_for_subscriptions(self):
+        pass
+
+    async def _parse_order_book_snapshot_message(self, raw_message: Dict[str, Any], message_queue: asyncio.Queue):
+        raise NotImplementedError
+
+    async def _connected_websocket_assistant(self) -> WSAssistant:
+        ws: WSAssistant = await self._api_factory.get_ws_assistant()
+        await ws.connect(ws_url=CONSTANTS.WS_RPC_URLS[self._domain],
+                         ping_timeout=CONSTANTS.WS_HEARTBEAT_TIME_INTERVAL)
+        return ws
+
+    async def _subscribe_channels(self, ws: WSAssistant):
+        """
+        Subscribes to the scheduled swaps
+        """
+        try:
+            all_assets = self._data_source.assets_list()
+            for trading_pair in self._trading_pairs:
+                symbol = await self._connector.exchange_symbol_associated_to_pair(trading_pair=trading_pair)
+                asset = DataFormatter.format_trading_pair(symbol, all_assets)
+                payload = {
+                    "id":1,
+                    "jsonrpc":"2.0",
+                    "method": CONSTANTS.SCHEDULED_SWAPS,
+                    "params": {
+                        'base_asset': asset["base_asset"],
+                        'quote_asset': asset["quote_asset"]
+                    }
+                }
+                subscribe_scheduled_swaps: WSJSONRequest = WSJSONRequest(payload=payload)
+                await ws.send(subscribe_scheduled_swaps)
+
+                self.logger().info(f"Subscribed to scheduled for {trading_pair}")
+        except asyncio.CancelledError:
+            raise
+        except Exception:
+            self.logger().error(
+                "Unexpected error occurred subscribing to order book trading and delta streams...",
+                exc_info=True
+            )
+            raise
+
+    def _channel_originating_message(self, event_message: Dict[str, Any]) -> str:
+        # 
+        return ""
---
+++ hummingbot/connector/exchange/chainflip_lp/chainflip_lp_data_source.py
@@ -0,0 +1,177 @@
+import asyncio
+import json
+import logging
+import time
+from decimal import Decimal
+from typing import TYPE_CHECKING, Any, Dict, List, Mapping, Optional, Tuple
+from urllib.parse import urlparse
+
+
+from bidict import bidict
+from substrateinterface import Keypair, KeypairType
+
+from hummingbot.connector.exchange.chainflip_lp import chainflip_lp_constants as CONSTANTS
+from hummingbot.connector.exchange.chainflip_lp.chainflip_lp_rpc_executor import RPCQueryExecutor
+from hummingbot.connector.exchange.chainflip_lp.chainflip_lp_data_formatter import DataFormatter
+from hummingbot.connector.utils import combine_to_hb_trading_pair
+from hummingbot.core.api_throttler.async_throttler import AsyncThrottler
+from hummingbot.core.api_throttler.async_throttler_base import AsyncThrottlerBase
+from hummingbot.core.data_type.common import OrderType, TradeType
+from hummingbot.core.data_type.in_flight_order import InFlightOrder
+from hummingbot.core.data_type.order_book_message import OrderBookMessage, OrderBookMessageType
+from hummingbot.core.event.event_listener import EventListener
+from hummingbot.core.network_iterator import NetworkStatus
+from hummingbot.core.pubsub import Enum, PubSub
+from hummingbot.logger import HummingbotLogger
+
+if TYPE_CHECKING:
+    from hummingbot.connector.exchange_py_base import ExchangePyBase
+
+
+
+class ChainflipLPDataSource:
+    _logger: Optional[HummingbotLogger] = None
+
+    @classmethod
+    def logger(cls) -> HummingbotLogger:
+        if cls._logger is None:
+            cls._logger = logging.getLogger(HummingbotLogger.logger_name_for_class(cls))
+        return cls._logger
+
+    def __init__(
+        self,
+        connector: "ExchangePyBase",
+        seed_phrase: str,
+        rpc_api_url: str,
+        domain: Optional[str] = CONSTANTS.DEFAULT_DOMAIN,
+        trading_required: bool = True,
+        trading_pairs: list = [],
+    ):
+        self._connector = connector
+        self._domain = domain
+        self._trading_required = trading_required
+        self._seed_phrase = seed_phrase
+        self._trading_pairs = trading_pairs
+        self._lp_api_url = rpc_api_url
+        self._keypair = Keypair.create_from_mnemonic(
+            seed_phrase
+        )
+        self._publisher = PubSub()
+        self._last_received_message_time = 0
+        # We create a throttler instance here just to have a fully valid instance from the first moment.
+        # The connector using this data source should replace the throttler with the one used by the connector.
+        self._throttler = AsyncThrottler(rate_limits=CONSTANTS.RATE_LIMITS)
+        self._events_listening_tasks = []
+        self._assets_list: List[Dict[str, str]] = []
+
+        self._rpc_executor = RPCQueryExecutor(
+            throttler=self._throttler,
+            chainflip_lp_api_url=self._lp_api_url,
+            lp_account_address=self._keypair.ss58_address,
+            domain=self._domain
+        )
+    async def start(self):
+        await self.assets_list()
+    def configure_throttler(self, throttler: AsyncThrottlerBase):
+        self._throttler = throttler
+    async def assets_list(self) -> Dict[str, str]:
+        all_assets = await self._rpc_executor.all_assets()
+        self._assets_list = all_assets
+        return self._assets_list
+
+    async def order_book_snapshot(self, trading_pair: str) -> OrderBookMessage:
+        all_assets = await self.assets_list()
+        symbol_dict = DataFormatter.format_trading_pair(trading_pair, all_assets)
+        orderbook_items = await self._rpc_executor.get_orderbook(
+            symbol_dict["base_asset"],
+            symbol_dict["quote_asset"]
+        )
+        timestamp = self._time()
+        bids = []
+        asks = []
+        for orderbook_item in orderbook_items:
+            for bid in orderbook_item["bids"]:
+                price = Decimal(bid["sqrt_price"])
+                amount = Decimal(bid["amount"])
+                bids.append((price, amount))
+            for ask in orderbook_item["asks"]:
+                price = Decimal(ask["sqrt_price"])
+                amount = Decimal(ask["amount"])
+                asks.append((price, amount))
+
+        order_book_message_content = {
+            "trading_pair": trading_pair,
+            "bids": bids,
+            "asks": asks,
+        }
+        snapshot_msg: OrderBookMessage = OrderBookMessage(
+            message_type=OrderBookMessageType.SNAPSHOT,
+            content=order_book_message_content,
+            timestamp=timestamp,
+        )
+        return snapshot_msg
+    async def exchange_status(self):
+        status = await self._rpc_executor.check_connection_status()
+
+        if status:
+            result = NetworkStatus.CONNECTED
+        else:
+            result = NetworkStatus.NOT_CONNECTED
+        return result
+    async def symbols_map(self) -> Mapping[str, str]:
+        symbols_map = bidict()
+        all_markets = await self._rpc_executor.all_markets()
+        for market_info in all_markets:
+            try:
+                base, quote = market_info["symbol"].split("-")
+                symbols_map[market_info["symbol"]] = combine_to_hb_trading_pair(base=base, quote=quote)
+            except KeyError:
+                continue
+        return symbols_map
+    async def all_balances(self) -> List[Dict[str, Any]]:
+        balances = await self._rpc_executor.get_all_balances()
+        return balances
+    async def place_order(
+            self,
+            order_id: str,
+            trading_pair: str,
+            amount: Decimal,
+            trade_type: TradeType,
+            order_type: OrderType,
+            price: Decimal,
+            *kwargs
+        ) -> Tuple[str, float]:
+        asset_list = await self.assets_list()
+        asset = DataFormatter.format_trading_pair(trading_pair,asset_list)
+        place_order_response = await self._rpc_executor.place_limit_order(
+            base_asset=asset["base_asset"],
+            quote_asset=asset["quote_asset"],
+            order_id=order_id,
+            order_price=price,
+            side=CONSTANTS.SIDE_BUY if trade_type == TradeType.BUY else CONSTANTS.SIDE_SELL,
+            sell_amount=amount
+        )
+        timestamp = self._time()
+        return order_id, timestamp
+    async def place_cancel(self,order_id:str, trading_pair:str,tracked_order:InFlightOrder):
+        asset_list = await self.assets_list()
+        asset = DataFormatter.format_trading_pair(trading_pair,asset_list)
+        status = self._rpc_executor.cancel_order(
+            base_asset=asset["base_asset"],
+            quote_asset=asset["quote_asset"],
+            order_id=order_id,
+            side = CONSTANTS.SIDE_BUY if tracked_order.trade_type == TradeType.BUY else CONSTANTS.SIDE_SELL
+        )
+        return status
+    async def get_last_traded_price(self, trading_pair):
+        asset = DataFormatter.format_trading_pair(trading_pair,self._assets_list)
+        price = await self._rpc_executor.get_market_price(
+            base_asset=asset["base_asset"],
+            quote_asset= asset["quote_asset"]
+        )["price"]
+        return {trading_pair: price}
+    def add_listener(self, event_tag: Enum, listener: EventListener):
+        self._publisher.add_listener(event_tag=event_tag, listener=listener)
+    def _time(self):
+        return time.time()
+    
