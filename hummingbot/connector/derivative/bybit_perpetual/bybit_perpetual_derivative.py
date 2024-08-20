519e1c979e74da49161fdf3d00efc155a5e42b20
drupman <tomasgaudino@hotmail.com>
2024-08-20 14:52:59 -0300

    (feat) update set position mode endpoint and parsing

--- hummingbot/connector/derivative/bybit_perpetual/bybit_perpetual_derivative.py
+++ hummingbot/connector/derivative/bybit_perpetual/bybit_perpetual_derivative.py
@@ -800,7 +800,10 @@ class BybitPerpetualDerivative(PerpetualDerivativePyBase):
 
         if is_linear:
             exchange_symbol = await self.exchange_symbol_associated_to_pair(trading_pair)
-            data = {"symbol": exchange_symbol, "mode": api_mode}
+            data = {
+                "category": "linear",
+                "symbol": exchange_symbol,
+                "mode": api_mode}
 
             response = await self._api_post(
                 path_url=CONSTANTS.SET_POSITION_MODE_URL,
@@ -808,15 +811,15 @@ class BybitPerpetualDerivative(PerpetualDerivativePyBase):
                 is_auth_required=True,
             )
 
-            response_code = response["ret_code"]
+            response_code = response["retCode"]
 
             if response_code not in [CONSTANTS.RET_CODE_OK, CONSTANTS.RET_CODE_MODE_NOT_MODIFIED]:
                 formatted_ret_code = self._format_ret_code_for_print(response_code)
-                msg = f"{formatted_ret_code} - {response['ret_msg']}"
+                msg = f"{formatted_ret_code} - {response['retMsg']}"
                 success = False
         else:
             #  Inverse Perpetuals don't have set_position_mode()
-            msg = "Inverse Perpetuals don't allow for a position mode change."
+            msg = "USDC / Inverse Perpetuals don't allow for a position mode change."
             success = False
 
         return success, msg
