7e6dfe627fdcdc4353647e39b0881e35f4f90cd2
cardosofede <federico.cardoso.e@gmail.com>
2024-09-10 09:37:25 +0800

    (feat) improve data collection on backtesting provider

--- hummingbot/strategy_v2/backtesting/backtesting_data_provider.py
+++ hummingbot/strategy_v2/backtesting/backtesting_data_provider.py
@@ -1,33 +1,83 @@
+import logging
 from decimal import Decimal
 from typing import Dict
 
 import pandas as pd
 
+from hummingbot.client.config.client_config_map import ClientConfigMap
+from hummingbot.client.config.config_helpers import ClientConfigAdapter, get_connector_class
+from hummingbot.client.settings import AllConnectorSettings, ConnectorType
 from hummingbot.connector.connector_base import ConnectorBase
 from hummingbot.core.data_type.common import PriceType
 from hummingbot.data_feed.candles_feed.candles_factory import CandlesFactory
 from hummingbot.data_feed.candles_feed.data_types import CandlesConfig, HistoricalCandlesConfig
 from hummingbot.data_feed.market_data_provider import MarketDataProvider
 
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 class BacktestingDataProvider(MarketDataProvider):
+    CONNECTOR_TYPES = [ConnectorType.CLOB_SPOT, ConnectorType.CLOB_PERP, ConnectorType.Exchange,
+                       ConnectorType.Derivative]
+    EXCLUDED_CONNECTORS = ["vega_perpetual", "hyperliquid_perpetual", "dydx_perpetual", "cube",
+                           "polkadex", "coinbase_advanced_trade", "kraken", "dydx_v4_perpetual", "hitbtc"]
+
     def __init__(self, connectors: Dict[str, ConnectorBase]):
         super().__init__(connectors)
         self.start_time = None
         self.end_time = None
         self.prices = {}
         self._time = None
+        self.trading_rules = {}
+        self.conn_settings = AllConnectorSettings.get_connector_settings()
+        self.connectors = {name: self.get_connector(name) for name, settings in self.conn_settings.items()
+                           if settings.type in self.CONNECTOR_TYPES and name not in self.EXCLUDED_CONNECTORS and
+                           "testnet" not in name}
+
+    def get_connector(self, connector_name: str):
+        conn_setting = self.conn_settings.get(connector_name)
+        if conn_setting is None:
+            logger.error(f"Connector {connector_name} not found")
+            raise ValueError(f"Connector {connector_name} not found")
+
+        client_config_map = ClientConfigAdapter(ClientConfigMap())
+        init_params = conn_setting.conn_init_parameters(
+            trading_pairs=[],
+            trading_required=False,
+            api_keys=self.get_connector_config_map(connector_name),
+            client_config_map=client_config_map,
+        )
+        connector_class = get_connector_class(connector_name)
+        connector = connector_class(**init_params)
+        return connector
+
+    @staticmethod
+    def get_connector_config_map(connector_name: str):
+        connector_config = AllConnectorSettings.get_connector_config_keys(connector_name)
+        return {key: "" for key in connector_config.__fields__.keys() if key != "connector"}
+
+    def get_trading_rules(self, connector_name: str, trading_pair: str):
+        """
+        Retrieves the trading rules from the specified connector.
+        :param connector_name: str
+        :return: Trading rules.
+        """
+        return self.trading_rules[trading_pair]
 
     def time(self):
         return self._time
+
+    async def initialize_trading_rules(self, connector_name: str):
+        connector = self.connectors.get(connector_name)
+        await connector._update_trading_rules()
+        self.trading_rules = connector.trading_rules
 
     async def initialize_candles_feed(self, config: CandlesConfig):
         await self.get_candles_feed(config)
 
     def update_backtesting_time(self, start_time: int, end_time: int):
-        if (self.start_time is None or self.end_time is None) or \
-                (start_time < self.start_time or end_time > self.end_time):
-            self.candles_feeds = {}
         self.start_time = start_time
         self.end_time = end_time
         self._time = start_time
@@ -43,20 +93,21 @@ class BacktestingDataProvider(MarketDataProvider):
         existing_feed = self.candles_feeds.get(key, pd.DataFrame())
 
         if not existing_feed.empty:
-            # Existing feed is sufficient, return it
-            return existing_feed
-        else:
-            # Create a new feed or restart the existing one with updated max_records
-            candle_feed = CandlesFactory.get_candle(config)
-            candles_df = await candle_feed.get_historical_candles(config=HistoricalCandlesConfig(
-                connector_name=config.connector,
-                trading_pair=config.trading_pair,
-                interval=config.interval,
-                start_time=self.start_time,
-                end_time=self.end_time,
-            ))
-            self.candles_feeds[key] = candles_df
-            return candles_df
+            existing_feed_start_time = existing_feed["timestamp"].min()
+            existing_feed_end_time = existing_feed["timestamp"].max()
+            if existing_feed_start_time <= self.start_time and existing_feed_end_time >= self.end_time:
+                return existing_feed
+        # Create a new feed or restart the existing one with updated max_records
+        candle_feed = CandlesFactory.get_candle(config)
+        candles_df = await candle_feed.get_historical_candles(config=HistoricalCandlesConfig(
+            connector_name=config.connector,
+            trading_pair=config.trading_pair,
+            interval=config.interval,
+            start_time=self.start_time,
+            end_time=self.end_time,
+        ))
+        self.candles_feeds[key] = candles_df
+        return candles_df
 
     def get_candles_df(self, connector_name: str, trading_pair: str, interval: str, max_records: int = 500):
         """
