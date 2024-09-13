330acce62acf528c93cdcfdc0212a65f8dd943e2
ishaan-ghosh <ishaanghosh01@gmail.com>
2024-09-13 18:35:57 +0400

    Update url util functions

--- hummingbot/connector/exchange/ourbit/ourbit_web_utils.py
+++ hummingbot/connector/exchange/ourbit/ourbit_web_utils.py
@@ -17,7 +17,7 @@ def public_rest_url(path_url: str, domain: str = CONSTANTS.DEFAULT_DOMAIN) -> st
     :param domain: the Mexc domain to connect to ("com" or "us"). The default value is "com"
     :return: the full URL to the endpoint
     """
-    return CONSTANTS.REST_URL.format(domain) + CONSTANTS.PUBLIC_API_VERSION + path_url
+    return CONSTANTS.REST_URL + CONSTANTS.PUBLIC_API_VERSION + path_url
 
 
 def private_rest_url(path_url: str, domain: str = CONSTANTS.DEFAULT_DOMAIN) -> str:
@@ -27,7 +27,7 @@ def private_rest_url(path_url: str, domain: str = CONSTANTS.DEFAULT_DOMAIN) -> s
     :param domain: the Mexc domain to connect to ("com" or "us"). The default value is "com"
     :return: the full URL to the endpoint
     """
-    return CONSTANTS.REST_URL.format(domain) + CONSTANTS.PRIVATE_API_VERSION + path_url
+    return CONSTANTS.REST_URL + CONSTANTS.PRIVATE_API_VERSION + path_url
 
 
 def build_api_factory(
