59f109c3897e06dab96fe5e5f14a8fba027769c4
memento <claude.rc@gmail.com>
2024-10-31 15:16:57 -0500

    (test) add executor example

--- hummingbot/strategy_v2/models/executors_info.py
+++ hummingbot/strategy_v2/models/executors_info.py
@@ -7,6 +7,7 @@ from hummingbot.core.data_type.common import TradeType
 from hummingbot.strategy_v2.executors.arbitrage_executor.data_types import ArbitrageExecutorConfig
 from hummingbot.strategy_v2.executors.data_types import ExecutorConfigBase
 from hummingbot.strategy_v2.executors.dca_executor.data_types import DCAExecutorConfig
+from hummingbot.strategy_v2.executors.position_exchange_executor.data_types import PositionExchangeExecutorConfig
 from hummingbot.strategy_v2.executors.position_executor.data_types import PositionExecutorConfig
 from hummingbot.strategy_v2.executors.twap_executor.data_types import TWAPExecutorConfig
 from hummingbot.strategy_v2.executors.xemm_executor.data_types import XEMMExecutorConfig
@@ -21,7 +22,7 @@ class ExecutorInfo(BaseModel):
     close_timestamp: Optional[float]
     close_type: Optional[CloseType]
     status: RunnableStatus
-    config: Union[PositionExecutorConfig, XEMMExecutorConfig, ArbitrageExecutorConfig, DCAExecutorConfig, TWAPExecutorConfig, ExecutorConfigBase]
+    config: Union[PositionExecutorConfig, PositionExchangeExecutorConfig, XEMMExecutorConfig, ArbitrageExecutorConfig, DCAExecutorConfig, TWAPExecutorConfig, ExecutorConfigBase]
     net_pnl_pct: Decimal
     net_pnl_quote: Decimal
     cum_fees_quote: Decimal
@@ -59,6 +60,8 @@ class ExecutorHandlerInfo(BaseModel):
     status: RunnableStatus
     active_position_executors: list[ExecutorInfo]
     closed_position_executors: list[ExecutorInfo]
+    active_position_exchange_executors: list[ExecutorInfo]
+    closed_position_exchange_executors: list[ExecutorInfo]
     active_dca_executors: list[ExecutorInfo]
     closed_dca_executors: list[ExecutorInfo]
     active_arbitrage_executors: list[ExecutorInfo]
