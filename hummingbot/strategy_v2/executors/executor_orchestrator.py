a0e131df5770e1ba4fbd1e1072690c02e0c09fd1
cardosofede <federico.cardoso.e@gmail.com>
2024-11-29 18:42:20 -0300

    (feat) add grid executor to orchestrator

--- hummingbot/strategy_v2/executors/executor_orchestrator.py
+++ hummingbot/strategy_v2/executors/executor_orchestrator.py
@@ -11,6 +11,8 @@ from hummingbot.strategy_v2.executors.arbitrage_executor.arbitrage_executor impo
 from hummingbot.strategy_v2.executors.arbitrage_executor.data_types import ArbitrageExecutorConfig
 from hummingbot.strategy_v2.executors.dca_executor.data_types import DCAExecutorConfig
 from hummingbot.strategy_v2.executors.dca_executor.dca_executor import DCAExecutor
+from hummingbot.strategy_v2.executors.grid_executor.data_types import GridExecutorConfig
+from hummingbot.strategy_v2.executors.grid_executor.grid_executor import GridExecutor
 from hummingbot.strategy_v2.executors.position_executor.data_types import PositionExecutorConfig
 from hummingbot.strategy_v2.executors.position_executor.position_executor import PositionExecutor
 from hummingbot.strategy_v2.executors.twap_executor.data_types import TWAPExecutorConfig
@@ -121,6 +123,8 @@ class ExecutorOrchestrator:
 
         if isinstance(executor_config, PositionExecutorConfig):
             executor = PositionExecutor(self.strategy, executor_config, self.executors_update_interval)
+        elif isinstance(executor_config, GridExecutorConfig):
+            executor = GridExecutor(self.strategy, executor_config, self.executors_update_interval)
         elif isinstance(executor_config, DCAExecutorConfig):
             executor = DCAExecutor(self.strategy, executor_config, self.executors_update_interval)
         elif isinstance(executor_config, ArbitrageExecutorConfig):
