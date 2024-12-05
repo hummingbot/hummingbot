8ac484faae341598de12a4d5c22293a3cc78aac3
drupman <tomasgaudino@hotmail.com>
2024-12-05 00:39:42 -0300

    (wip) update utils

--- hummingbot/connector/derivative/bitmart_perpetual/bitmart_perpetual_utils.py
+++ hummingbot/connector/derivative/bitmart_perpetual/bitmart_perpetual_utils.py
@@ -7,70 +7,46 @@ from hummingbot.core.data_type.trade_fee import TradeFeeSchema
 
 DEFAULT_FEES = TradeFeeSchema(
     maker_percent_fee_decimal=Decimal("0.0002"),
-    taker_percent_fee_decimal=Decimal("0.0004"),
+    taker_percent_fee_decimal=Decimal("0.0006"),
     buy_percent_fee_deducted_from_returns=True
 )
 
 CENTRALIZED = True
 
-EXAMPLE_PAIR = "BTC-USDT"
+EXAMPLE_PAIR = "BTCUSDT"
 
 BROKER_ID = "x-3QreWesy"
 
 
-class BinancePerpetualConfigMap(BaseConnectorConfigMap):
-    connector: str = Field(default="binance_perpetual", client_data=None)
-    binance_perpetual_api_key: SecretStr = Field(
+class BitmartPerpetualConfigMap(BaseConnectorConfigMap):
+    connector: str = Field(default="bitmart_perpetual", client_data=None)
+    bitmart_perpetual_api_key: SecretStr = Field(
         default=...,
         client_data=ClientFieldData(
-            prompt=lambda cm: "Enter your Binance Perpetual API key",
+            prompt=lambda cm: "Enter your Bitmart Perpetual API key",
             is_secure=True,
             is_connect_key=True,
             prompt_on_new=True,
         )
     )
-    binance_perpetual_api_secret: SecretStr = Field(
+    bitmart_perpetual_api_secret: SecretStr = Field(
         default=...,
         client_data=ClientFieldData(
-            prompt=lambda cm: "Enter your Binance Perpetual API secret",
+            prompt=lambda cm: "Enter your Bitmart Perpetual API secret",
             is_secure=True,
             is_connect_key=True,
             prompt_on_new=True,
         )
     )
-
-
-KEYS = BinancePerpetualConfigMap.construct()
-
-OTHER_DOMAINS = ["binance_perpetual_testnet"]
-OTHER_DOMAINS_PARAMETER = {"binance_perpetual_testnet": "binance_perpetual_testnet"}
-OTHER_DOMAINS_EXAMPLE_PAIR = {"binance_perpetual_testnet": "BTC-USDT"}
-OTHER_DOMAINS_DEFAULT_FEES = {"binance_perpetual_testnet": [0.02, 0.04]}
-
-
-class BinancePerpetualTestnetConfigMap(BaseConnectorConfigMap):
-    connector: str = Field(default="binance_perpetual_testnet", client_data=None)
-    binance_perpetual_testnet_api_key: SecretStr = Field(
+    bitmart_perpetual_memo: SecretStr = Field(
         default=...,
         client_data=ClientFieldData(
-            prompt=lambda cm: "Enter your Binance Perpetual testnet API key",
-            is_secure=True,
-            is_connect_key=True,
-            prompt_on_new=True,
-        )
-    )
-    binance_perpetual_testnet_api_secret: SecretStr = Field(
-        default=...,
-        client_data=ClientFieldData(
-            prompt=lambda cm: "Enter your Binance Perpetual testnet API secret",
+            prompt=lambda cm: "Enter your Bitmart Perpetual Memo",
             is_secure=True,
             is_connect_key=True,
             prompt_on_new=True,
         )
     )
 
-    class Config:
-        title = "binance_perpetual"
 
-
-OTHER_DOMAINS_KEYS = {"binance_perpetual_testnet": BinancePerpetualTestnetConfigMap.construct()}
+KEYS = BitmartPerpetualConfigMap.construct()
