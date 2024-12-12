9d8b59e4a90478061e0e4ef46cfec7eabc6458d4
drupman <tomasgaudino@hotmail.com>
2024-12-12 15:58:01 -0300

    (feat) update order book snapshot and diff

--- hummingbot/connector/derivative/bitmart_perpetual/bitmart_perpetual_api_order_book_data_source.py
+++ hummingbot/connector/derivative/bitmart_perpetual/bitmart_perpetual_api_order_book_data_source.py
@@ -1,5 +1,4 @@
 import asyncio
-import time
 from collections import defaultdict
 from decimal import Decimal
 from typing import TYPE_CHECKING, Any, Dict, List, Mapping, Optional
@@ -42,10 +41,10 @@ class BitmartPerpetualAPIOrderBookDataSource(PerpetualAPIOrderBookDataSource):
         self._trading_pairs: List[str] = trading_pairs
         self._message_queue: Dict[str, asyncio.Queue] = defaultdict(asyncio.Queue)
         self._trade_messages_queue_key = CONSTANTS.TRADE_STREAM_CHANNEL
-        self._diff_messages_queue_key = CONSTANTS.DIFF_STREAM_CHANNEL
+        self._snapshot_messages_queue_key = CONSTANTS.ORDER_BOOK_CHANNEL + "_SNAPSHOT"
+        self._diff_messages_queue_key = CONSTANTS.ORDER_BOOK_CHANNEL + "_DIFF"
         self._funding_info_messages_queue_key = CONSTANTS.FUNDING_INFO_CHANNEL
         self._tickers_messages_queue_key = CONSTANTS.TICKERS_CHANNEL
-        self._snapshot_messages_queue_key = CONSTANTS.SNAPSHOT_CHANNEL
         self._last_index_price = None
         self._last_mark_price = None
         self._last_next_funding_utc_timestamp = None
@@ -76,32 +75,6 @@ class BitmartPerpetualAPIOrderBookDataSource(PerpetualAPIOrderBookDataSource):
             )
             return funding_info
 
-    async def _request_order_book_snapshot(self, trading_pair: str) -> Dict[str, Any]:
-        ex_trading_pair = await self._connector.exchange_symbol_associated_to_pair(trading_pair=trading_pair)
-
-        params = {
-            "symbol": ex_trading_pair,
-            "limit": "1000"
-        }
-
-        data = await self._connector._api_get(
-            path_url=CONSTANTS.SNAPSHOT_REST_URL,
-            params=params)
-        return data
-
-    async def _order_book_snapshot(self, trading_pair: str) -> OrderBookMessage:
-        snapshot_response: Dict[str, Any] = await self._request_order_book_snapshot(trading_pair)
-        snapshot_data: Dict[str, Any] = snapshot_response.get("data")
-        snapshot_timestamp: float = snapshot_data["timestamp"] / 1e3
-        snapshot_data.update({"trading_pair": trading_pair})
-        snapshot_msg: OrderBookMessage = OrderBookMessage(OrderBookMessageType.SNAPSHOT, {
-            "trading_pair": snapshot_data["trading_pair"],
-            "update_id": int(time.time()),  # TODO: check for what is this
-            "bids": snapshot_data["bids"],
-            "asks": snapshot_data["asks"]
-        }, timestamp=snapshot_timestamp)
-        return snapshot_msg
-
     async def _connected_websocket_assistant(self) -> WSAssistant:
         url = f"{web_utils.wss_url(CONSTANTS.PUBLIC_WS_ENDPOINT, domain=self._domain)}"
         ws: WSAssistant = await self._api_factory.get_ws_assistant()
@@ -115,7 +88,7 @@ class BitmartPerpetualAPIOrderBookDataSource(PerpetualAPIOrderBookDataSource):
         """
         try:
             stream_id_channel_pairs = [
-                CONSTANTS.DIFF_STREAM_CHANNEL,
+                CONSTANTS.ORDER_BOOK_CHANNEL,
                 CONSTANTS.TRADE_STREAM_CHANNEL,
                 CONSTANTS.FUNDING_INFO_CHANNEL,
                 CONSTANTS.TICKERS_CHANNEL,
@@ -142,8 +115,11 @@ class BitmartPerpetualAPIOrderBookDataSource(PerpetualAPIOrderBookDataSource):
         channel = ""
         if "result" not in event_message and event_message.get("data") is not None:
             stream_name = event_message.get("group")
-            if CONSTANTS.DIFF_STREAM_CHANNEL in stream_name:
-                channel = self._diff_messages_queue_key
+            if CONSTANTS.ORDER_BOOK_CHANNEL in stream_name:
+                if event_message["data"]["type"] == "update":
+                    channel = self._diff_messages_queue_key
+                else:
+                    channel = self._snapshot_messages_queue_key
             elif CONSTANTS.TRADE_STREAM_CHANNEL in stream_name:
                 channel = self._trade_messages_queue_key
             elif CONSTANTS.FUNDING_INFO_CHANNEL in stream_name:
@@ -152,42 +128,14 @@ class BitmartPerpetualAPIOrderBookDataSource(PerpetualAPIOrderBookDataSource):
                 channel = self._tickers_messages_queue_key
         return channel
 
-    async def _parse_order_book_diff_message(self, raw_message: Dict[str, Any], message_queue: asyncio.Queue):
-        raw_message["data"]["symbol"] = await self._connector.trading_pair_associated_to_exchange_symbol(
-            raw_message["data"]["symbol"])
-        data = raw_message["data"]
-        # TODO: Check what's going on with order book differences
-        order_book_message: OrderBookMessage = OrderBookMessage(OrderBookMessageType.DIFF, {
-            "trading_pair": data["symbol"],
-            "update_id": int(data["ms_t"]),
-            "bids": [(depth["price"], depth["vol"]) for depth in data["depths"] if data["way"] == 1],
-            "asks": [(depth["price"], depth["vol"]) for depth in data["depths"] if data["way"] == 2]
-        }, timestamp=data["ms_t"] / 1e3)
-        message_queue.put_nowait(order_book_message)
-
-    @staticmethod
-    def _parse_trade_way(way: int) -> str:
-        """
-        Parse the trade 'way' to determine if it's a buy or sell action.
-
-        Args:
-            way (int): The integer value representing the trade way.
-
-        Returns:
-            str: "buy" or "sell" based on the way value.
-        """
-        way_to_trade_type = {
-            1: TradeType.BUY,  # buy_open_long or sell_open_short (treated as buy)
-            2: TradeType.BUY,  # buy_open_long or sell_close_long (treated as buy)
-            3: TradeType.SELL,  # buy_close_short or sell_open_short (treated as sell)
-            4: TradeType.SELL,  # buy_close_short or sell_close_long (treated as sell)
-            5: TradeType.BUY,  # sell_open_short or buy_open_long (treated as buy)
-            6: TradeType.SELL,  # sell_open_short or buy_close_short (treated as sell)
-            7: TradeType.SELL,  # sell_close_long or buy_open_long (treated as sell)
-            8: TradeType.BUY,  # sell_close_long or buy_close_short (treated as buy)
-        }
-
-        return way_to_trade_type.get(way)  # Default to "unknown" if way is invalid
+    def _get_messages_queue_keys(self) -> List[str]:
+        return [
+            self._snapshot_messages_queue_key,
+            self._diff_messages_queue_key,
+            self._trade_messages_queue_key,
+            self._funding_info_messages_queue_key,
+            self._tickers_messages_queue_key
+        ]
 
     async def _parse_trade_message(self, raw_message: Dict[str, Any], message_queue: asyncio.Queue):
         if len(raw_message["data"]) > 0:
@@ -206,31 +154,42 @@ class BitmartPerpetualAPIOrderBookDataSource(PerpetualAPIOrderBookDataSource):
                 timestamp=trade_data["created_at"])
             message_queue.put_nowait(trade_message)
 
-    async def listen_for_order_book_snapshots(self, ev_loop: asyncio.BaseEventLoop, output: asyncio.Queue):
-        while True:
-            try:
-                for trading_pair in self._trading_pairs:
-                    snapshot_msg: OrderBookMessage = await self._order_book_snapshot(trading_pair)
-                    output.put_nowait(snapshot_msg)
-                    self.logger().debug(f"Saved order book snapshot for {trading_pair}")
-                delta = CONSTANTS.ONE_HOUR - time.time() % CONSTANTS.ONE_HOUR
-                await self._sleep(delta)
-            except asyncio.CancelledError:
-                raise
-            except Exception:
-                self.logger().error(
-                    "Unexpected error occurred fetching orderbook snapshots. Retrying in 5 seconds...", exc_info=True
-                )
-                await self._sleep(5.0)
+    async def _order_book_snapshot(self, trading_pair: str) -> OrderBookMessage:
+        snapshot_response: Dict[str, Any] = await self._request_order_book_snapshot(trading_pair)
+        snapshot_data: Dict[str, Any] = snapshot_response.get("data")
+        snapshot_timestamp: float = snapshot_data["timestamp"] / 1e3
+        snapshot_data.update({"trading_pair": trading_pair})
+        snapshot_msg: OrderBookMessage = OrderBookMessage(OrderBookMessageType.SNAPSHOT, {
+            "trading_pair": snapshot_data["trading_pair"],
+            "update_id": 1,
+            "bids": [(bid[0], bid[1]) for bid in snapshot_data["bids"]],
+            "asks": [(ask[0], ask[1]) for ask in snapshot_data["asks"]]
+        }, timestamp=snapshot_timestamp)
+        return snapshot_msg
 
-    def _get_messages_queue_keys(self) -> List[str]:
-        return [
-            self._snapshot_messages_queue_key,
-            self._diff_messages_queue_key,
-            self._trade_messages_queue_key,
-            self._funding_info_messages_queue_key,
-            self._tickers_messages_queue_key
-        ]
+    async def _parse_order_book_diff_message(self, raw_message: Dict[str, Any], message_queue: asyncio.Queue):
+        raw_message["data"]["symbol"] = await self._connector.trading_pair_associated_to_exchange_symbol(
+            raw_message["data"]["symbol"])
+        data = raw_message["data"]
+        order_book_message: OrderBookMessage = OrderBookMessage(OrderBookMessageType.DIFF, {
+            "trading_pair": data["symbol"],
+            "update_id": int(data["version"]),
+            "bids": [(depth["price"], depth["vol"]) for depth in data["bids"]],
+            "asks": [(depth["price"], depth["vol"]) for depth in data["asks"]],
+        }, timestamp=data["ms_t"] / 1e3)
+        message_queue.put_nowait(order_book_message)
+
+    async def _parse_order_book_snapshot_message(self, raw_message: Dict[str, Any], message_queue: asyncio.Queue):
+        raw_message["data"]["symbol"] = await self._connector.trading_pair_associated_to_exchange_symbol(
+            raw_message["data"]["symbol"])
+        data = raw_message["data"]
+        order_book_message: OrderBookMessage = OrderBookMessage(OrderBookMessageType.SNAPSHOT, {
+            "trading_pair": data["symbol"],
+            "update_id": int(data["version"]),
+            "bids": [(depth["price"], depth["vol"]) for depth in data["bids"]],
+            "asks": [(depth["price"], depth["vol"]) for depth in data["asks"]],
+        }, timestamp=data["ms_t"] / 1e3)
+        message_queue.put_nowait(order_book_message)
 
     async def _parse_funding_info_message(self, raw_message: Dict[str, Any], message_queue: asyncio.Queue):
         data: Dict[str, Any] = raw_message["data"]
@@ -273,3 +232,27 @@ class BitmartPerpetualAPIOrderBookDataSource(PerpetualAPIOrderBookDataSource):
             path_url=CONSTANTS.EXCHANGE_INFO_URL,
             params={"symbol": ex_trading_pair})
         return data
+
+    async def _request_order_book_snapshot(self, trading_pair: str) -> Dict[str, Any]:
+        ex_trading_pair = await self._connector.exchange_symbol_associated_to_pair(trading_pair=trading_pair)
+        params = {
+            "symbol": ex_trading_pair,
+        }
+        data = await self._connector._api_get(
+            path_url=CONSTANTS.SNAPSHOT_REST_URL,
+            params=params)
+        return data
+
+    @staticmethod
+    def _parse_trade_way(way: int) -> str:
+        way_to_trade_type = {
+            1: TradeType.BUY,  # buy_open_long or sell_open_short (treated as buy)
+            2: TradeType.BUY,  # buy_open_long or sell_close_long (treated as buy)
+            3: TradeType.SELL,  # buy_close_short or sell_open_short (treated as sell)
+            4: TradeType.SELL,  # buy_close_short or sell_close_long (treated as sell)
+            5: TradeType.BUY,  # sell_open_short or buy_open_long (treated as buy)
+            6: TradeType.SELL,  # sell_open_short or buy_close_short (treated as sell)
+            7: TradeType.SELL,  # sell_close_long or buy_open_long (treated as sell)
+            8: TradeType.BUY,  # sell_close_long or buy_close_short (treated as buy)
+        }
+        return way_to_trade_type.get(way)  # Default to "unknown" if way is invalid
