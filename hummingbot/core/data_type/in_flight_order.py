e91065239c8f2c5dc1b3124ea939e056841c2e36
cardosofede <federico.cardoso.e@gmail.com>
2024-12-18 12:19:27 -0300

    (feat) remove error handling from order since the rates should be available now

--- hummingbot/core/data_type/in_flight_order.py
+++ hummingbot/core/data_type/in_flight_order.py
@@ -304,17 +304,14 @@ class InFlightOrder:
         :return: the cumulative fee paid for all partial fills in the specified token
         """
         total_fee_in_token = Decimal("0")
-        try:
-            for trade_update in self.order_fills.values():
-                total_fee_in_token += trade_update.fee.fee_amount_in_token(
-                    trading_pair=self.trading_pair,
-                    price=trade_update.fill_price,
-                    order_amount=trade_update.fill_base_amount,
-                    token=token,
-                    exchange=exchange
-                )
-        except Exception:
-            pass
+        for trade_update in self.order_fills.values():
+            total_fee_in_token += trade_update.fee.fee_amount_in_token(
+                trading_pair=self.trading_pair,
+                price=trade_update.fill_price,
+                order_amount=trade_update.fill_base_amount,
+                token=token,
+                exchange=exchange
+            )
         return total_fee_in_token
 
     def update_with_order_update(self, order_update: OrderUpdate) -> bool:
