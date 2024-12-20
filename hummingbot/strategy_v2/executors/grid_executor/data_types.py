c6d4388ddb4ac5652a6831c033b6c819658b6898
cardosofede <federico.cardoso.e@gmail.com>
2024-12-20 16:46:36 -0300

    (feat) make default deduct base fee false

--- hummingbot/strategy_v2/executors/grid_executor/data_types.py
+++ hummingbot/strategy_v2/executors/grid_executor/data_types.py
@@ -33,7 +33,7 @@ class GridExecutorConfig(ExecutorConfigBase):
     triple_barrier_config: TripleBarrierConfig
     leverage: int = 20
     level_id: Optional[str] = None
-    deduct_base_fees: bool = True
+    deduct_base_fees: bool = False
 
 
 class GridLevelStates(Enum):
