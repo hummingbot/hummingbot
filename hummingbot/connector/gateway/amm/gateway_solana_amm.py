c607fde8077bcbb5d7cfc831b6e983d8e88d7d6d
Michael Feng <mifeng@gmail.com>
2024-12-24 16:45:02 -0800

    (fix) fix solana fees

--- hummingbot/connector/gateway/amm/gateway_solana_amm.py
+++ hummingbot/connector/gateway/amm/gateway_solana_amm.py
@@ -6,7 +6,6 @@ from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union, cast
 from hummingbot.connector.gateway.amm.gateway_evm_amm import GatewayEVMAMM
 from hummingbot.connector.gateway.gateway_in_flight_order import GatewayInFlightOrder
 from hummingbot.core.data_type.in_flight_order import OrderState, OrderUpdate
-from hummingbot.core.data_type.trade_fee import TokenAmount
 from hummingbot.core.utils.async_utils import safe_ensure_future, safe_gather
 from hummingbot.logger import HummingbotLogger
 
@@ -51,8 +50,6 @@ class GatewaySolanaAMM(GatewayEVMAMM):
                          additional_spenders=additional_spenders,
                          trading_required=trading_required)
         self._native_currency = "SOL"
-        self._default_fee = Decimal("0")
-        self._network_transaction_fee: Optional[TokenAmount] = TokenAmount(token=self._native_currency, amount=self._default_fee)  # No Solana fees
 
     @classmethod
     def logger(cls) -> HummingbotLogger:
@@ -83,6 +80,7 @@ class GatewaySolanaAMM(GatewayEVMAMM):
     async def start_network(self):
         if self._trading_required:
             self._status_polling_task = safe_ensure_future(self._status_polling_loop())
+            self._get_gas_estimate_task = safe_ensure_future(self.get_gas_estimate())
         self._get_chain_info_task = safe_ensure_future(self.get_chain_info())
 
     async def stop_network(self):
@@ -91,6 +89,9 @@ class GatewaySolanaAMM(GatewayEVMAMM):
             self._status_polling_task = None
         if self._get_chain_info_task is not None:
             self._get_chain_info_task.cancel()
+            self._get_chain_info_task = None
+        if self._get_gas_estimate_task is not None:
+            self._get_gas_estimate_task.cancel()
             self._get_chain_info_task = None
 
     async def update_order_status(self, tracked_orders: List[GatewayInFlightOrder]):
