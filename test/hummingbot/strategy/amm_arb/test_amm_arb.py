231d0e01eb9b5e0080744e479792713a9fe5a094
cardosofede <federico.cardoso.e@gmail.com>
2025-02-04 11:21:44 -0300

    (feat) update amm arb tests

--- test/hummingbot/strategy/amm_arb/test_amm_arb.py
+++ test/hummingbot/strategy/amm_arb/test_amm_arb.py
@@ -2,10 +2,9 @@ import asyncio
 import contextlib
 import unittest
 from decimal import Decimal
+from test.isolated_asyncio_wrapper_test_case import IsolatedAsyncioWrapperTestCase
 from typing import List
 from unittest.mock import patch
-
-from aiounittest import async_test
 
 from hummingbot.client.config.client_config_map import ClientConfigMap
 from hummingbot.client.config.config_helpers import ClientConfigAdapter
@@ -32,7 +31,6 @@ TRADING_PAIR: str = "HBOT-USDT"
 BASE_ASSET: str = TRADING_PAIR.split("-")[0]
 QUOTE_ASSET: str = TRADING_PAIR.split("-")[1]
 
-ev_loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
 s_decimal_0 = Decimal(0)
 
 
@@ -127,7 +125,7 @@ class MockAMM(ConnectorBase):
         return []
 
 
-class AmmArbUnitTest(unittest.TestCase):
+class AmmArbUnitTest(IsolatedAsyncioWrapperTestCase):
     def setUp(self):
         self.clock: Clock = Clock(ClockMode.REALTIME)
         self.stack: contextlib.ExitStack = contextlib.ExitStack()
@@ -180,28 +178,20 @@ class AmmArbUnitTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.stack.close()
         self.clock_task.cancel()
-        try:
-            ev_loop.run_until_complete(self.clock_task)
-        except asyncio.CancelledError:
-            pass
 
-    @async_test(loop=ev_loop)
     async def test_arbitrage_not_profitable(self):
         self.amm_1.set_prices(TRADING_PAIR, True, 101)
         self.amm_1.set_prices(TRADING_PAIR, False, 100)
         self.amm_2.set_prices(TRADING_PAIR, True, 101)
         self.amm_2.set_prices(TRADING_PAIR, False, 100)
-        await asyncio.sleep(2)
         taker_orders = self.strategy.tracked_limit_orders + self.strategy.tracked_market_orders
         self.assertTrue(len(taker_orders) == 0)
 
-    @async_test(loop=ev_loop)
     async def test_arb_buy_amm_1_sell_amm_2(self):
         self.amm_1.set_prices(TRADING_PAIR, True, 101)
         self.amm_1.set_prices(TRADING_PAIR, False, 100)
         self.amm_2.set_prices(TRADING_PAIR, True, 105)
         self.amm_2.set_prices(TRADING_PAIR, False, 104)
-        await asyncio.sleep(1.5)
         placed_orders = self.strategy.tracked_limit_orders
         amm_1_order = [order for market, order in placed_orders if market == self.amm_1][0]
         amm_2_order = [order for market, order in placed_orders if market == self.amm_2][0]
@@ -223,7 +213,6 @@ class AmmArbUnitTest(unittest.TestCase):
 
         # There are outstanding orders, the strategy is not ready to take on new arb
         self.assertFalse(self.strategy.ready_for_new_arb_trades())
-        await asyncio.sleep(2)
         placed_orders = self.strategy.tracked_limit_orders
         new_amm_1_order = [order for market, order in placed_orders if market == self.amm_1][0]
         new_amm_2_order = [order for market, order in placed_orders if market == self.amm_2][0]
@@ -231,13 +220,11 @@ class AmmArbUnitTest(unittest.TestCase):
         self.assertEqual(amm_1_order.client_order_id, new_amm_1_order.client_order_id)
         self.assertEqual(amm_2_order.client_order_id, new_amm_2_order.client_order_id)
 
-    @async_test(loop=ev_loop)
     async def test_arb_buy_amm_2_sell_amm_1(self):
         self.amm_1.set_prices(TRADING_PAIR, True, 105)
         self.amm_1.set_prices(TRADING_PAIR, False, 104)
         self.amm_2.set_prices(TRADING_PAIR, True, 101)
         self.amm_2.set_prices(TRADING_PAIR, False, 100)
-        await asyncio.sleep(1.5)
         placed_orders = self.strategy.tracked_limit_orders
         amm_1_order = [order for market, order in placed_orders if market == self.amm_1][0]
         amm_2_order = [order for market, order in placed_orders if market == self.amm_2][0]
@@ -254,7 +241,6 @@ class AmmArbUnitTest(unittest.TestCase):
         self.assertEqual(exp_price, amm_2_order.price)
         self.assertEqual(TRADING_PAIR, amm_2_order.trading_pair)
 
-    @async_test(loop=ev_loop)
     async def test_insufficient_balance(self):
         self.amm_1.set_prices(TRADING_PAIR, True, 105)
         self.amm_1.set_prices(TRADING_PAIR, False, 104)
@@ -262,13 +248,11 @@ class AmmArbUnitTest(unittest.TestCase):
         self.amm_2.set_prices(TRADING_PAIR, False, 100)
         # set base_asset to below order_amount, so not enough to sell on amm_1
         self.amm_1.set_balance(BASE_ASSET, 0.5)
-        await asyncio.sleep(1.5)
         placed_orders = self.strategy.tracked_limit_orders
         self.assertTrue(len(placed_orders) == 0)
         self.amm_1.set_balance(BASE_ASSET, 10)
         # set quote balance to 0 on amm_2, so not enough to buy
         self.amm_2.set_balance(QUOTE_ASSET, 0)
-        await asyncio.sleep(1.5)
         placed_orders = self.strategy.tracked_limit_orders
         self.assertTrue(len(placed_orders) == 0)
 
@@ -283,7 +267,6 @@ class AmmArbUnitTest(unittest.TestCase):
                                 event_class(connector.current_timestamp, order_id, BASE_ASSET, QUOTE_ASSET,
                                             amount, amount * price, OrderType.LIMIT))
 
-    @async_test(loop=ev_loop)
     async def test_non_concurrent_orders_submission(self):
         # On non concurrent orders submission, the second leg of the arb trade has to wait for the first leg order gets
         # filled.
@@ -297,7 +280,6 @@ class AmmArbUnitTest(unittest.TestCase):
         )
         self.strategy.rate_source = self.rate_source
         self.clock.add_iterator(self.strategy)
-        await asyncio.sleep(1.5)
         placed_orders = self.strategy.tracked_limit_orders
         self.assertEqual(1, len(placed_orders))
         # Only one order submitted at this point, the one from amm_1
@@ -308,7 +290,6 @@ class AmmArbUnitTest(unittest.TestCase):
         self.trigger_order_complete(True, self.amm_1, amm_1_order.quantity, amm_1_order.price,
                                     amm_1_order.client_order_id)
         # After the first leg order completed, the second one is now submitted.
-        await asyncio.sleep(1.5)
         placed_orders = self.strategy.tracked_limit_orders
         amm_2_orders = [order for market, order in placed_orders if market == self.amm_2]
         self.assertEqual(1, len(amm_2_orders))
@@ -316,41 +297,33 @@ class AmmArbUnitTest(unittest.TestCase):
         self.assertEqual(False, amm_2_order.is_buy)
         self.trigger_order_complete(False, self.amm_2, amm_2_order.quantity, amm_2_order.price,
                                     amm_2_order.client_order_id)
-        await asyncio.sleep(1.5)
         placed_orders = self.strategy.tracked_limit_orders
         new_amm_1_order = [order for market, order in placed_orders if market == self.amm_1][0]
         # Check if new order is submitted when arb opportunity still presents
         self.assertNotEqual(amm_1_order.client_order_id, new_amm_1_order.client_order_id)
 
-    @async_test(loop=ev_loop)
     async def test_arb_not_profitable_from_gas_prices(self):
         self.amm_1.set_prices(TRADING_PAIR, True, 101)
         self.amm_1.set_prices(TRADING_PAIR, False, 100)
         self.amm_2.set_prices(TRADING_PAIR, True, 110)
         self.amm_2.set_prices(TRADING_PAIR, False, 109)
         self.amm_1.network_transaction_fee = TokenAmount("ETH", Decimal("0.01"))
-        await asyncio.sleep(2)
         taker_orders = self.strategy.tracked_limit_orders + self.strategy.tracked_market_orders
         self.assertTrue(len(taker_orders) == 0)
 
-    @async_test(loop=ev_loop)
     async def test_arb_profitable_after_gas_prices(self):
         self.amm_1.set_prices(TRADING_PAIR, True, 101)
         self.amm_1.set_prices(TRADING_PAIR, False, 100)
         self.amm_2.set_prices(TRADING_PAIR, True, 105)
         self.amm_2.set_prices(TRADING_PAIR, False, 104)
         self.amm_1.network_transaction_fee = TokenAmount("ETH", Decimal("0.0002"))
-        await asyncio.sleep(2)
         placed_orders = self.strategy.tracked_limit_orders + self.strategy.tracked_market_orders
         self.assertEqual(2, len(placed_orders))
 
-    @async_test(loop=ev_loop)
     @unittest.mock.patch("hummingbot.strategy.amm_arb.amm_arb.AmmArbStrategy.apply_gateway_transaction_cancel_interval")
     async def test_apply_cancel_interval(self, patched_func: unittest.mock.AsyncMock):
-        await asyncio.sleep(2)
         patched_func.assert_awaited()
 
-    @async_test(loop=ev_loop)
     @unittest.mock.patch("hummingbot.strategy.amm_arb.amm_arb.AmmArbStrategy.is_gateway_market", return_value=True)
     @unittest.mock.patch("hummingbot.client.settings.GatewayConnectionSetting.get_connector_spec_from_market_name")
     @unittest.mock.patch.object(MockAMM, "cancel_outdated_orders")
@@ -370,21 +343,17 @@ class AmmArbUnitTest(unittest.TestCase):
         await asyncio.sleep(2)
         cancel_outdated_orders_func.assert_awaited()
 
-    @async_test(loop=ev_loop)
     async def test_set_order_failed(self):
         self.amm_1.set_prices(TRADING_PAIR, True, 101)
         self.amm_1.set_prices(TRADING_PAIR, False, 100)
         self.amm_2.set_prices(TRADING_PAIR, True, 105)
         self.amm_2.set_prices(TRADING_PAIR, False, 104)
         self.amm_1.network_transaction_fee = TokenAmount("ETH", Decimal("0.0002"))
-        await asyncio.sleep(2)
         new_amm_1_order = [order for market, order in self.strategy.tracked_limit_orders if market == self.amm_1][0]
         self.assertEqual(2, len(self.strategy.tracked_limit_orders))
         self.strategy.set_order_failed(new_amm_1_order.client_order_id)
         self.assertEqual(2, len(self.strategy.tracked_limit_orders))
 
-    @async_test(loop=ev_loop)
     async def test_market_ready(self):
         self.amm_1.ready = False
-        await asyncio.sleep(10)
         self.assertFalse(self.strategy._all_markets_ready)
