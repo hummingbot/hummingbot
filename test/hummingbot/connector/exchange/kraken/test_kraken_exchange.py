bd23eee782c565bf59e2fd18f3131c98030a068d
memento <claude.rc@gmail.com>
2025-02-05 16:43:02 -0600

    Companion kraken update to new order types

--- test/hummingbot/connector/exchange/kraken/test_kraken_exchange.py
+++ test/hummingbot/connector/exchange/kraken/test_kraken_exchange.py
@@ -422,7 +422,17 @@ class KrakenExchangeTests(AbstractExchangeConnectorTests.ExchangeConnectorTests)
 
     @property
     def expected_supported_order_types(self):
-        return [OrderType.LIMIT, OrderType.LIMIT_MAKER, OrderType.MARKET]
+        return [
+            OrderType.LIMIT,
+            OrderType.LIMIT_MAKER,
+            OrderType.MARKET,
+            OrderType.STOP_LOSS,
+            OrderType.TAKE_PROFIT,
+            OrderType.TRAILING_STOP,
+            # OrderType.STOP_LOSS_LIMIT,
+            # OrderType.TAKE_PROFIT_LIMIT,
+            # OrderType.TRAILING_STOP_LIMIT,
+        ]
 
     @property
     def expected_trading_rule(self):
