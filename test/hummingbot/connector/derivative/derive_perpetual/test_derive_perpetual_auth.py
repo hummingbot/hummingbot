46cacb4cd39f52db3b6d49e05705a0f28ab21512
memento <claude.rc@gmail.com>
2025-03-20 14:09:27 -0500

    (dev) proposal for derive. fix test with correct key/address

--- test/hummingbot/connector/derivative/derive_perpetual/test_derive_perpetual_auth.py
+++ test/hummingbot/connector/derivative/derive_perpetual/test_derive_perpetual_auth.py
@@ -12,7 +12,7 @@ from hummingbot.core.web_assistant.connections.data_types import RESTMethod, RES
 class DerivePerpetualAuthTests(TestCase):
     def setUp(self) -> None:
         super().setUp()
-        self.api_key = "testApiKey"
+        self.api_key = "0x1234567890abcdef1234567890abcdef12345678"
         self.api_secret = "13e56ca9cceebf1f33065c2c5376ab38570a114bc1b003b60d838f92be9d7930"  # noqa: mock
         self.sub_id = "45686"  # noqa: mock
         self.auth = DerivePerpetualAuth(api_key=self.api_key,
@@ -68,9 +68,13 @@ class DerivePerpetualAuthTests(TestCase):
         self.assertEqual(authenticated_request.data, json.dumps({"key": "value"}))
 
     def test_add_auth_to_params_post(self):
+        import eth_utils
+        address = "0x1234567890abcdef1234567890abcdef12345678"
+        self.assertTrue(eth_utils.is_hex_address(address))
         params = {
             "type": "order",
-            "asset_address": "abc",
+            # This needs to be 0x40-long
+            "asset_address": address,
             "sub_id": 1,
             "limit_price": "100",
             "amount": "10",
