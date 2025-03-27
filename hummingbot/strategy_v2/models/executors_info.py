98ac9e38a7e0f594f55316ce45688d89c0fd559f
cardosofede <federico.cardoso.e@gmail.com>
2025-03-27 16:18:12 -0300

    (feat) improve type hints

--- hummingbot/strategy_v2/models/executors_info.py
+++ hummingbot/strategy_v2/models/executors_info.py
@@ -1,18 +1,14 @@
 from decimal import Decimal
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Optional, Union, TypeVar
 
 from pydantic.v1 import BaseModel
 
 from hummingbot.core.data_type.common import TradeType
-from hummingbot.strategy_v2.executors.arbitrage_executor.data_types import ArbitrageExecutorConfig
 from hummingbot.strategy_v2.executors.data_types import ExecutorConfigBase
-from hummingbot.strategy_v2.executors.dca_executor.data_types import DCAExecutorConfig
-from hummingbot.strategy_v2.executors.position_executor.data_types import PositionExecutorConfig
-from hummingbot.strategy_v2.executors.twap_executor.data_types import TWAPExecutorConfig
-from hummingbot.strategy_v2.executors.xemm_executor.data_types import XEMMExecutorConfig
 from hummingbot.strategy_v2.models.base import RunnableStatus
 from hummingbot.strategy_v2.models.executors import CloseType
 
+ExecutorConfigType = TypeVar("ExecutorConfigType", bound=ExecutorConfigBase)
 
 class ExecutorInfo(BaseModel):
     id: str
@@ -21,7 +17,7 @@ class ExecutorInfo(BaseModel):
     close_timestamp: Optional[float]
     close_type: Optional[CloseType]
     status: RunnableStatus
-    config: Union[PositionExecutorConfig, XEMMExecutorConfig, ArbitrageExecutorConfig, DCAExecutorConfig, TWAPExecutorConfig, ExecutorConfigBase]
+    config: ExecutorConfigType
     net_pnl_pct: Decimal
     net_pnl_quote: Decimal
     cum_fees_quote: Decimal
