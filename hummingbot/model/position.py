6741427ab4cb98411abdf8ab809aa81986746997
cardosofede <federico.cardoso.e@gmail.com>
2025-03-27 13:04:26 -0300

    (feat) remove filled orders

--- hummingbot/model/position.py
+++ hummingbot/model/position.py
@@ -1,4 +1,4 @@
-from sqlalchemy import JSON, BigInteger, Column, Index, Text
+from sqlalchemy import BigInteger, Column, Index, Text
 
 from . import HummingbotBase
 from .decimal_type_decorator import SqliteDecimal
@@ -24,11 +24,10 @@ class Position(HummingbotBase):
     breakeven_price = Column(SqliteDecimal(6), nullable=False)
     unrealized_pnl_quote = Column(SqliteDecimal(6), nullable=False)
     cum_fees_quote = Column(SqliteDecimal(6), nullable=False)
-    filled_orders = Column(JSON, nullable=False)
 
     def __repr__(self) -> str:
-        return (f"Position(id='{self.id}', config_file_path='{self.config_file_path}', "
-                f"strategy='{self.strategy}', market='{self.market}', "
+        return (f"Position(id='{self.id}', controller_id='{self.controller_id}', "
+                f"connector_name='{self.connector_name}', trading_pair='{self.trading_pair}', "
                 f"trading_pair='{self.trading_pair}', timestamp={self.timestamp}, "
                 f"volume_traded_quote={self.volume_traded_quote}, amount={self.amount}, "
                 f"breakeven_price={self.breakeven_price}, unrealized_pnl_quote={self.unrealized_pnl_quote}, "
