c5d5624e4429ad80c183c6d29b58c4b8a96ba183
isreallee82 <isreallee82@gmail.com>
2025-03-27 02:14:15 +0100

    (feat) remove lyra_v2_action_signing package

--- hummingbot/connector/exchange/derive/derive_exchange.py
+++ hummingbot/connector/exchange/derive/derive_exchange.py
@@ -77,7 +77,7 @@ class DeriveExchange(ExchangePyBase):
 
     @property
     def authenticator(self) -> DeriveAuth:
-        return DeriveAuth(self.derive_api_key, self.derive_secret_key, self._sub_id, self._trading_required)
+        return DeriveAuth(self.derive_api_key, self.derive_secret_key, self._sub_id, self._trading_required, self._domain)
 
     @property
     def rate_limits_rules(self) -> List[RateLimit]:
