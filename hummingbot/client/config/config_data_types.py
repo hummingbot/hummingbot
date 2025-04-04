72fb6aa2347ca3e41054439e874a384719138d61
cardosofede <federico.cardoso.e@gmail.com>
2025-04-04 18:32:38 -0300

    (feat) simplify models

--- hummingbot/client/config/config_data_types.py
+++ hummingbot/client/config/config_data_types.py
@@ -1,13 +1,11 @@
 from dataclasses import dataclass
 from datetime import datetime
-from decimal import Decimal
 from enum import Enum
 from typing import Callable, Optional
 
-from pydantic import BaseModel, ConfigDict, field_validator
-from pydantic.v1 import Field
+from pydantic import BaseModel, ConfigDict, Field, field_validator
 
-from hummingbot.client.config.config_validators import validate_connector, validate_decimal
+from hummingbot.client.config.config_validators import validate_connector
 
 
 class ClientConfigEnum(Enum):
@@ -41,28 +39,14 @@ class BaseClientModel(BaseModel):
         else:
             return False
 
-    def validate_decimal(v: str, field: Field):
-        """Used for client-friendly error output."""
-        field_info = field.field_info
-        inclusive = field_info.ge is not None or field_info.le is not None
-        min_value = field_info.gt if field_info.gt is not None else field_info.ge
-        min_value = Decimal(min_value) if min_value is not None else min_value
-        max_value = field_info.lt if field_info.lt is not None else field_info.le
-        max_value = Decimal(max_value) if max_value is not None else max_value
-        ret = validate_decimal(v, min_value, max_value, inclusive)
-        if ret is not None:
-            raise ValueError(ret)
-        return v
-
 
 class BaseConnectorConfigMap(BaseClientModel):
     connector: str = Field(
         default=...,
-        description="What is your connector?",
-        client_data=ClientFieldData(
-            prompt=lambda mi: "What is your connector?",
-            prompt_on_new=True,
-        ),
+        json_schema_extra={
+            "prompt": "What is your connector?",
+            "prompt_on_new": True,
+        },
     )
 
     @field_validator("connector", mode="before")
