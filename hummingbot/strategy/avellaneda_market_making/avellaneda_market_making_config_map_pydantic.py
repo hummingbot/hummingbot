7263f832d1ec86b11e677ae52a48f4d2eb2a367c
cardosofede <federico.cardoso.e@gmail.com>
2025-04-04 18:42:40 -0300

    (feat) adapt amm

--- hummingbot/strategy/avellaneda_market_making/avellaneda_market_making_config_map_pydantic.py
+++ hummingbot/strategy/avellaneda_market_making/avellaneda_market_making_config_map_pydantic.py
@@ -2,10 +2,9 @@ from datetime import datetime, time
 from decimal import Decimal
 from typing import Dict, Optional, Union
 
-from pydantic import ConfigDict, field_validator, model_validator
-from pydantic.v1 import Field
+from pydantic import ConfigDict, Field, field_validator, model_validator
 
-from hummingbot.client.config.config_data_types import BaseClientModel, ClientFieldData
+from hummingbot.client.config.config_data_types import BaseClientModel
 from hummingbot.client.config.config_validators import (
     validate_bool,
     validate_datetime_iso_string,
@@ -26,18 +25,16 @@ class FromDateToDateModel(BaseClientModel):
     start_datetime: datetime = Field(
         default=...,
         description="The start date and time for date-to-date execution timeframe.",
-        client_data=ClientFieldData(
-            prompt=lambda mi: "Please enter the start date and time (YYYY-MM-DD HH:MM:SS)",
-            prompt_on_new=True,
-        ),
+        json_schema_extra={
+            "prompt": "Please enter the start date and time (YYYY-MM-DD HH:MM:SS)", "prompt_on_new": True
+        }
     )
     end_datetime: datetime = Field(
         default=...,
         description="The end date and time for date-to-date execution timeframe.",
-        client_data=ClientFieldData(
-            prompt=lambda mi: "Please enter the end date and time (YYYY-MM-DD HH:MM:SS)",
-            prompt_on_new=True,
-        ),
+        json_schema_extra={
+            "prompt": "Please enter the end date and time (YYYY-MM-DD HH:MM:SS)", "prompt_on_new": True
+        }
     )
     model_config = ConfigDict(title="from_date_to_date")
 
@@ -56,18 +53,12 @@ class DailyBetweenTimesModel(BaseClientModel):
     start_time: time = Field(
         default=...,
         description="The start time for daily-between-times execution timeframe.",
-        client_data=ClientFieldData(
-            prompt=lambda mi: "Please enter the start time (HH:MM:SS)",
-            prompt_on_new=True,
-        ),
+        json_schema_extra={"prompt": "Please enter the start time (HH:MM:SS)", "prompt_on_new": True},
     )
     end_time: time = Field(
         default=...,
         description="The end time for daily-between-times execution timeframe.",
-        client_data=ClientFieldData(
-            prompt=lambda mi: "Please enter the end time (HH:MM:SS)",
-            prompt_on_new=True,
-        ),
+        json_schema_extra={"prompt": "Please enter the end time (HH:MM:SS)", "prompt_on_new": True},
     )
     model_config = ConfigDict(title="daily_between_times")
 
@@ -98,19 +89,13 @@ class MultiOrderLevelModel(BaseClientModel):
         default=2,
         description="The number of orders placed on either side of the order book.",
         ge=2,
-        client_data=ClientFieldData(
-            prompt=lambda mi: "How many orders do you want to place on both sides?",
-            prompt_on_new=True,
-        ),
+        json_schema_extra={"prompt": "How many orders do you want to place on both sides?", "prompt_on_new": True},
     )
     level_distances: Decimal = Field(
         default=Decimal("0"),
         description="The spread between order levels, expressed in % of optimal spread.",
         ge=0,
-        client_data=ClientFieldData(
-            prompt=lambda mi: "How far apart in % of optimal spread should orders on one side be?",
-            prompt_on_new=True,
-        ),
+        json_schema_extra={"prompt": "How far apart in % of optimal spread should orders on one side be?", "prompt_on_new": True},
     )
     model_config = ConfigDict(title="multi_order_level")
 
@@ -143,12 +128,9 @@ class TrackHangingOrdersModel(BaseClientModel):
         description="The spread percentage at which hanging orders will be cancelled.",
         gt=0,
         lt=100,
-        client_data=ClientFieldData(
-            prompt=lambda mi: (
-                "At what spread percentage (from mid price) will hanging orders be canceled?"
-                " (Enter 1 to indicate 1%)"
-            ),
-        )
+        json_schema_extra={
+            "prompt": "At what spread percentage (from mid price) will hanging orders be canceled? (Enter 1 to indicate 1%)",
+        }
     )
     model_config = ConfigDict(title="track_hanging_orders")
 
@@ -172,23 +154,23 @@ HANGING_ORDER_MODELS = {
 
 
 class AvellanedaMarketMakingConfigMap(BaseTradingStrategyConfigMap):
-    strategy: str = Field(default="avellaneda_market_making", client_data=None)
+    strategy: str = Field(default="avellaneda_market_making")
     execution_timeframe_mode: Union[InfiniteModel, FromDateToDateModel, DailyBetweenTimesModel] = Field(
         default=...,
         description="The execution timeframe.",
-        client_data=ClientFieldData(
-            prompt=lambda mi: f"Select the execution timeframe ({'/'.join(EXECUTION_TIMEFRAME_MODELS.keys())})",
-            prompt_on_new=True,
-        ),
+        json_schema_extra={
+            "prompt": f"Select the execution timeframe ({'/'.join(EXECUTION_TIMEFRAME_MODELS.keys())})",
+            "prompt_on_new": True,
+        }
     )
     order_amount: Decimal = Field(
         default=...,
         description="The strategy order amount.",
         gt=0,
-        client_data=ClientFieldData(
-            prompt=lambda mi: AvellanedaMarketMakingConfigMap.order_amount_prompt(mi),
-            prompt_on_new=True,
-        )
+        json_schema_extra={
+            "prompt": lambda mi: AvellanedaMarketMakingConfigMap.order_amount_prompt(mi),
+            "prompt_on_new": True,
+        }
     )
     order_optimization_enabled: bool = Field(
         default=True,
@@ -196,150 +178,97 @@ class AvellanedaMarketMakingConfigMap(BaseTradingStrategyConfigMap):
             "Allows the bid and ask order prices to be adjusted based on"
             " the current top bid and ask prices in the market."
         ),
-        client_data=ClientFieldData(
-            prompt=lambda mi: "Do you want to enable best bid ask jumping? (Yes/No)"
-        ),
+        json_schema_extra={"prompt": "Do you want to enable order optimization? (Yes/No)"}
     )
     risk_factor: Decimal = Field(
         default=Decimal("1"),
         description="The risk factor (\u03B3).",
         gt=0,
-        client_data=ClientFieldData(
-            prompt=lambda mi: "Enter risk factor (\u03B3)",
-            prompt_on_new=True,
-        ),
+        json_schema_extra={"prompt": "Enter risk factor (\u03B3)", "prompt_on_new": True},
     )
     order_amount_shape_factor: Decimal = Field(
         default=Decimal("0"),
         description="The amount shape factor (\u03b7)",
         ge=0,
         le=1,
-        client_data=ClientFieldData(
-            prompt=lambda mi: "Enter order amount shape factor (\u03B7)",
-        ),
+        json_schema_extra={"prompt": "Enter order amount shape factor (\u03B7)"},
     )
     min_spread: Decimal = Field(
         default=Decimal("0"),
         description="The minimum spread limit as percentage of the mid price.",
         ge=0,
-        client_data=ClientFieldData(
-            prompt=lambda mi: "Enter minimum spread limit (as % of mid price)",
-        ),
+        json_schema_extra={"prompt": "Enter minimum spread limit (as % of mid price)"},
     )
     order_refresh_time: float = Field(
         default=...,
         description="The frequency at which the orders' spreads will be re-evaluated.",
         gt=0.,
-        client_data=ClientFieldData(
-            prompt=lambda mi: "How often do you want to cancel and replace bids and asks (in seconds)?",
-            prompt_on_new=True,
-        ),
+        json_schema_extra={"prompt": "How often do you want to refresh orders (in seconds)?", "prompt_on_new": True},
     )
     max_order_age: float = Field(
         default=1800.,
         description="A given order's maximum lifetime irrespective of spread.",
         gt=0.,
-        client_data=ClientFieldData(
-            prompt=lambda mi: (
-                "How long do you want to cancel and replace bids and asks with the same price (in seconds)?"
-            ),
-        ),
+        json_schema_extra={"prompt": "How long do you want to cancel and replace bids and asks with the same price (in seconds)?"}
     )
     order_refresh_tolerance_pct: Decimal = Field(
         default=Decimal("0"),
-        description=(
-            "The range of spreads tolerated on refresh cycles."
-            " Orders over that range are cancelled and re-submitted."
-        ),
-        ge=-10,
-        le=10,
-        client_data=ClientFieldData(
-            prompt=lambda mi: (
-                "Enter the percent change in price needed to refresh orders at each cycle"
-                " (Enter 1 to indicate 1%)"
-            )
-        ),
+        description="The range of spreads tolerated on refresh cycles. Orders over that range are cancelled and re-submitted.",
+        ge=-10, le=10,
+        json_schema_extra={"prompt": "Enter the percent change in price needed to refresh orders at each cycle (Enter 1 to indicate 1%)"},
     )
     filled_order_delay: float = Field(
         default=60.,
         description="The delay before placing a new order after an order fill.",
         gt=0.,
-        client_data=ClientFieldData(
-            prompt=lambda mi: (
-                "How long do you want to wait before placing the next order"
-                " if your order gets filled (in seconds)?"
-            )
-        ),
+        json_schema_extra={"prompt": "How long do you want to wait before placing the next order if your order gets filled (in seconds)"},
     )
     inventory_target_base_pct: Decimal = Field(
         default=Decimal("50"),
         description="Defines the inventory target for the base asset.",
         ge=0,
         le=100,
-        client_data=ClientFieldData(
-            prompt=lambda mi: "What is the inventory target for the base asset? Enter 50 for 50%",
-            prompt_on_new=True,
-        ),
+        json_schema_extra={"prompt": "Enter the inventory target for the base asset (Enter 50 for 50%)", "prompt_on_new": True},
     )
     add_transaction_costs: bool = Field(
         default=False,
         description="If activated, transaction costs will be added to order prices.",
-        client_data=ClientFieldData(
-            prompt=lambda mi: "Do you want to add transaction costs automatically to order prices? (Yes/No)",
-        ),
+        json_schema_extra={"prompt": "Do you want to add transaction costs automatically to order prices? (Yes/No)"},
     )
     volatility_buffer_size: int = Field(
         default=200,
         description="The number of ticks that will be stored to calculate volatility.",
         ge=1,
         le=10_000,
-        client_data=ClientFieldData(
-            prompt=lambda mi: "Enter amount of ticks that will be stored to estimate order book liquidity",
-        ),
+        json_schema_extra={"prompt": "Enter amount of ticks that will be stored to estimate order book liquidity"},
     )
     trading_intensity_buffer_size: int = Field(
         default=200,
         description="The number of ticks that will be stored to calculate order book liquidity.",
         ge=1,
         le=10_000,
-        client_data=ClientFieldData(
-            prompt=lambda mi: "Enter amount of ticks that will be stored to estimate order book liquidity",
-        ),
+        json_schema_extra={"prompt": "Enter amount of ticks that will be stored to estimate order book liquidity"},
     )
     order_levels_mode: Union[SingleOrderLevelModel, MultiOrderLevelModel] = Field(
         default=SingleOrderLevelModel.model_construct(),
         description="Allows activating multi-order levels.",
-        client_data=ClientFieldData(
-            prompt=lambda mi: f"Select the order levels mode ({'/'.join(list(ORDER_LEVEL_MODELS.keys()))})",
-        ),
+        json_schema_extra={"prompt": f"Select the order levels mode ({'/'.join(list(ORDER_LEVEL_MODELS.keys()))})"},
     )
     order_override: Optional[Dict] = Field(
         default=None,
         description="Allows custom specification of the order levels and their spreads and amounts.",
-        client_data=None,
     )
     hanging_orders_mode: Union[IgnoreHangingOrdersModel, TrackHangingOrdersModel] = Field(
         default=IgnoreHangingOrdersModel(),
         description="When tracking hanging orders, the orders on the side opposite to the filled orders remain active.",
-        client_data=ClientFieldData(
-            prompt=(
-                lambda mi: f"How do you want to handle hanging orders? ({'/'.join(list(HANGING_ORDER_MODELS.keys()))})"
-            ),
-        ),
+        json_schema_extra={"prompt": f"Select the hanging orders mode ({'/'.join(list(HANGING_ORDER_MODELS.keys()))})"},
     )
     should_wait_order_cancel_confirmation: bool = Field(
         default=True,
-        description=(
-            "If activated, the strategy will await cancellation confirmation from the exchange"
-            " before placing a new order."
-        ),
-        client_data=ClientFieldData(
-            prompt=lambda mi: (
-                "Should the strategy wait to receive a confirmation for orders cancellation"
-                " before creating a new set of orders?"
-                " (Not waiting requires enough available balance) (Yes/No)"
-            ),
-        )
+        description="If activated, the strategy will await cancellation confirmation from the exchange before placing a new order.",
+        json_schema_extra={
+            "prompt": "Should the strategy wait to receive a confirmation for orders cancellation before creating a new set of orders? (Yes/No)",
+        }
     )
     model_config = ConfigDict(title="avellaneda_market_making")
 
