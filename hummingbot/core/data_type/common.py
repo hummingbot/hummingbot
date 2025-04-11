81ffb3012ee4dcff655a8807f172d400caf5b4bb
WuonParticle <WuonParticle@gmail.com>
2025-04-11 17:49:36 -0600

    Implements add_or_update support for Trading Pair addition. Add similar Get_or_add support for Connector initialization

--- hummingbot/core/data_type/common.py
+++ hummingbot/core/data_type/common.py
@@ -1,6 +1,9 @@
+from collections import defaultdict
 from decimal import Decimal
 from enum import Enum
-from typing import NamedTuple
+from typing import Any, Callable, Dict, Generic, NamedTuple, Set, TypeVar
+
+from pydantic_core import core_schema
 
 
 class OrderType(Enum):
@@ -64,3 +67,50 @@ class LPType(Enum):
     ADD = 1
     REMOVE = 2
     COLLECT = 3
+
+
+_KT = TypeVar('_KT')
+_VT = TypeVar('_TV')
+
+
+class GroupedSetDict(Dict[_KT, Set[_VT]]):
+    def add_or_update(self, key: _KT, *args: _VT) -> None:
+        if key in self:
+            self[key].update(args)
+        else:
+            self[key] = set(args)
+        return self
+
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls,
+        _source_type: Any,
+        _handler: Any,
+    ) -> core_schema.CoreSchema:
+        return core_schema.no_info_after_validator_function(
+            cls,
+            core_schema.dict_schema(
+                core_schema.any_schema(),
+                core_schema.set_schema(core_schema.any_schema())
+            )
+        )
+
+
+MarketDict = GroupedSetDict[str, Set[str]]
+
+
+class LambdaDict(defaultdict[_KT, _VT], Generic[_KT, _VT]):
+    def __init__(self, default_value_factory: Callable[[_KT], _VT] = None):
+        super().__init__()
+        self.default_value_factory = default_value_factory
+
+    def __missing__(self, key: _KT) -> _VT:
+        if self.default_value_factory is None:
+            raise KeyError(f"Key {key} not found in {self} and no default value factory is set")
+        self[key] = self.default_value_factory(key)
+        return self[key]
+
+    def get_or_add(self, key: _KT, value_factory: Callable[[], _VT]) -> _VT:
+        if key not in self:
+            self[key] = value_factory()
+        return self[key]
