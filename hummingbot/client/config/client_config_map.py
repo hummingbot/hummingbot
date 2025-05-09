b9a6c039fff4de00a9dbb3dcc570bb2655fcda22
Javadyakuza <javadyakuzaa@gamil.com>
2025-05-09 16:59:53 +0330

    Feat: Update the file diffs

--- hummingbot/client/config/client_config_map.py
+++ hummingbot/client/config/client_config_map.py
@@ -469,6 +469,11 @@ class BinanceUSRateSourceMode(ExchangeRateSourceModeBase):
     model_config = ConfigDict(title="binance_us")
 
 
+class MexcRateSourceMode(ExchangeRateSourceModeBase):
+    name: str = Field(default="mexc")
+    model_config = ConfigDict(title="mexc")
+
+
 class CubeRateSourceMode(ExchangeRateSourceModeBase):
     name: str = Field(default="cube")
     model_config = ConfigDict(title="cube")
@@ -660,6 +665,7 @@ RATE_SOURCE_MODES = {
     HyperliquidRateSourceMode.model_config["title"]: HyperliquidRateSourceMode,
     DeriveRateSourceMode.model_config["title"]: DeriveRateSourceMode,
     TegroRateSourceMode.model_config["title"]: TegroRateSourceMode,
+    MexcRateSourceMode.model_config["title"]: MexcRateSourceMode,
 }
 
 
