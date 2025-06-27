ca76029bc99fd92e4443017499856165c92006d5
cardosofede <federico.cardoso.e@gmail.com>
2025-06-27 10:52:55 +0200

    (feat) fix initialize markets methods in start

--- hummingbot/strategy/avellaneda_market_making/start.py
+++ hummingbot/strategy/avellaneda_market_making/start.py
@@ -16,7 +16,8 @@ def start(self):
         raw_trading_pair = c_map.market
 
         trading_pair: str = raw_trading_pair
-        maker_assets: Tuple[str, str] = self._initialize_market_assets(exchange, [trading_pair])[0]
+        base, quote = trading_pair.split("-")
+        maker_assets: Tuple[str, str] = (base, quote)
         market_names: List[Tuple[str, List[str]]] = [(exchange, [trading_pair])]
         self.initialize_markets(market_names)
         maker_data = [self.markets[exchange], trading_pair] + list(maker_assets)
