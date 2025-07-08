390efe7df804d963584d7213cb01f60da53a0da9
cardosofede <federico.cardoso.e@gmail.com>
2025-07-08 23:53:04 +0300

    (feat) add clean up for user stream ds and tracker

--- hummingbot/core/data_type/user_stream_tracker.py
+++ hummingbot/core/data_type/user_stream_tracker.py
@@ -30,10 +30,30 @@ class UserStreamTracker:
         return self.data_source.last_recv_time
 
     async def start(self):
+        # Prevent concurrent start() calls
+        if self._user_stream_tracking_task is not None and not self._user_stream_tracking_task.done():
+            return
+
+        # Stop any existing task
+        await self.stop()
+
         self._user_stream_tracking_task = safe_ensure_future(
             self.data_source.listen_for_user_stream(self._user_stream)
         )
         await safe_gather(self._user_stream_tracking_task)
+
+    async def stop(self):
+        """Stop the user stream tracking task and clean up resources."""
+        if self._user_stream_tracking_task is not None and not self._user_stream_tracking_task.done():
+            self._user_stream_tracking_task.cancel()
+            try:
+                await self._user_stream_tracking_task
+            except asyncio.CancelledError:
+                pass
+
+        await self._data_source.stop()
+
+        self._user_stream_tracking_task = None
 
     @property
     def user_stream(self) -> asyncio.Queue:
