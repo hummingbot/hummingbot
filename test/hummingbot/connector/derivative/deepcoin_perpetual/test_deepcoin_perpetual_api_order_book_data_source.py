c04ffba32a36c296f0e89f4ffeade37d0f6d0301
wangxiangjun <wangxiangjun@deepcoin.pro>
2025-10-29 10:33:43 +0800

    add test file ob,user data

--- test/hummingbot/connector/derivative/deepcoin_perpetual/test_deepcoin_perpetual_api_order_book_data_source.py
+++ test/hummingbot/connector/derivative/deepcoin_perpetual/test_deepcoin_perpetual_api_order_book_data_source.py
@@ -54,9 +54,6 @@ class DeepcoinPerpetualAPIOrderBookDataSourceTests(IsolatedAsyncioWrapperTestCas
             domain=self.domain,
         )
 
-        self._original_full_order_book_reset_time = self.data_source.FULL_ORDER_BOOK_RESET_DELTA_SECONDS
-        self.data_source.FULL_ORDER_BOOK_RESET_DELTA_SECONDS = -1
-
         self.data_source.logger().setLevel(1)
         self.data_source.logger().addHandler(self)
 
@@ -68,7 +65,8 @@ class DeepcoinPerpetualAPIOrderBookDataSourceTests(IsolatedAsyncioWrapperTestCas
 
     def tearDown(self) -> None:
         self.listening_task and self.listening_task.cancel()
-        self.data_source.FULL_ORDER_BOOK_RESET_DELTA_SECONDS = self._original_full_order_book_reset_time
+        for task in self.async_tasks:
+            task.cancel()
         super().tearDown()
 
     def handle(self, record):
