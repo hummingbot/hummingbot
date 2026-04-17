55b865c96797f8d7cbfc9197478b108e279ff06e
x19216888-stack <x.192.168.8.8@gmail.com>
2026-04-17 15:18:25 +0800

    Fix: Dexalot rate source decimal conversion syntax and unhandled NoneType in queued processing

--- hummingbot/connector/exchange/dexalot/dexalot_exchange.py
+++ hummingbot/connector/exchange/dexalot/dexalot_exchange.py
@@ -852,7 +852,7 @@ class DexalotExchange(ExchangePyBase):
                 await asyncio.shield(task)
                 sleep_time = (self.clock.tick_size * 0.5
                               if self.clock is not None
-                              else self._orders_processing_delta_time)
+                              else getattr(self, "_orders_processing_delta_time", 1.0))
                 await self._sleep(sleep_time)
             except NotImplementedError:
                 raise
@@ -860,7 +860,10 @@ class DexalotExchange(ExchangePyBase):
                 raise
             except Exception:
                 self.logger().exception("Unexpected error while processing queued individual orders", exc_info=True)
-                await self._sleep(self.clock.tick_size * 0.5)
+                sleep_time = (self.clock.tick_size * 0.5
+                              if self.clock is not None
+                              else getattr(self, "_orders_processing_delta_time", 1.0))
+                await self._sleep(sleep_time)
 
     async def _cancel_and_create_queued_orders(self):
         if len(self._orders_queued_to_cancel) > 0 or len(self._orders_queued_to_create) > 0:
